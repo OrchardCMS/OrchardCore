95d1a90822ad097a59cf87f1937b517cc366c708
Nicholas Mayne <Jetski5822@gmail.com>
2014-11-29 19:44:35 +0000

    Adding another test project. This will not be enabled

---
+++ src/OrchardVNext.Web/Modules/OrchardVNext.Test2/Class.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+
+namespace OrchardVNext.Test2 {
+    public interface IFoo : IDependency {}
+    public class Class : IFoo {
+        public Class() { }
+    }
+}
