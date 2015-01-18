37ebd3ed82b23db16a9057988d4d1a24edb51e39
Nicholas Mayne <Jetski5822@gmail.com>
2015-01-18 22:41:49 +0000

    Creating Context and TransactionManager

---
+++ src/OrchardVNext/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using Microsoft.Data.Entity.Metadata;
+using OrchardVNext.Environment.Configuration;
+
+namespace OrchardVNext.Data
+{
+    public class DataContext : DbContext
+    {
+        private readonly ShellSettings _shellSettings;
+        public DataContext(ShellSettings shellSettings) {
+            _shellSettings = shellSettings;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+        }
+    }
+}
---
+++ src/OrchardVNext/Data/TransactionManager.cs
@@ -0,0 +1,36 @@
+﻿using OrchardVNext.Environment.Configuration;
+using System;
+
+namespace OrchardVNext.Data
+{
+    public interface ITransactionManager : IDependency {
+        void Demand();
+        void Cancel();
+    }
+
+    public class TransactionManager : ITransactionManager, IDisposable {
+        private readonly ShellSettings _shellSettings;
+        private DataContext _dataContext;
+        private bool _isCancel;
+
+        public TransactionManager(ShellSettings shellSettings) {
+            _shellSettings = shellSettings;
+        }
+
+        public void Demand() {
+            _dataContext = new DataContext(_shellSettings);
+            
+        }
+        public void Cancel() {
+            _isCancel = true;
+            _dataContext.Dispose();
+        }
+
+        public void Dispose() {
+            if (!_isCancel) {
+                _dataContext.SaveChanges();
+                _dataContext.Dispose();
+            }
+        }
+    }
+}
