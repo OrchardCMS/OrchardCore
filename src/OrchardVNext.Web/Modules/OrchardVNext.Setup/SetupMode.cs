9cb7acd32b0badb27a1f203f958e39773e69cbef
Nicholas Mayne <Jetski5822@gmail.com>
2015-08-23 20:03:18 +0100

    Fixing Setup

--- src/OrchardVNext.Web/Modules/OrchardVNext.Setup/SetupMode.cs
+++ src/OrchardVNext.Web/Modules/OrchardVNext.Setup/SetupMode.cs
@@ -2,11 +2,13 @@
 using OrchardVNext.DependencyInjection;
 using OrchardVNext.Hosting;
 using OrchardVNext.Hosting.Web.Routing.Routes;
+using OrchardVNext.Web;
 
 namespace OrchardVNext.Setup {
     public class SetupMode : IModule {
         public void Configure(IServiceCollection serviceCollection) {
             new HostingModule().Configure(serviceCollection);
+            new ShellStartup().ConfigureServices(serviceCollection);
 
             serviceCollection.AddScoped<IRoutePublisher, RoutePublisher>();
         }
