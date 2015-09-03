8e594d0890be10d89d4dcbdea17078ad34d305e1
Nicholas Mayne <Jetski5822@gmail.com>
2015-09-03 22:09:19 +0100

    Moving test to talk to real object

--- test/Orchard.Tests/Events/InternalNotifierTests.cs
+++ test/Orchard.Tests/Events/InternalNotifierTests.cs
@@ -15,109 +15,18 @@ namespace Orchard.Tests.Events
         public void CallingNotifyWillInvokeMethod() {
 
             var target = new TestEvent1();
-            var notifier = new Notifier(new []{ target });
+            var notifier = new InternalTelemetrySourceAdapter(new DefaultMethodAdaptor());
 
-            notifier.Subscribe(new EventObserver(target));
+            notifier.EnlistTarget(target);
 
             Assert.Equal(0, target.Counter);
 
-            notifier.Notify("ITestEvent_Add", new Dictionary<string, object> {  { "value", 1 } });
+            notifier.WriteTelemetry("ITestEvent_Add", new Dictionary<string, object> {  { "value", 1 } });
             
             Assert.Equal(1, target.Counter);
         }
     }
-
-    public class Notifier : IObservable<KeyValuePair<string, IDictionary<string, object>>> {
-        private readonly IList<IObserver<KeyValuePair<string, IDictionary<string, object>>>> _observers
-            = new List<IObserver<KeyValuePair<string, IDictionary<string, object>>>>();
-
-        private readonly IEnumerable<IEventHandler> _eventHandlers;
-
-        public Notifier(IEnumerable<IEventHandler> eventHandlers) {
-            _eventHandlers = eventHandlers;
-        }
-
-        public IDisposable Subscribe(IObserver<KeyValuePair<string, IDictionary<string, object>>> observer) {
-            _observers.Add(observer);
-
-            return null;
-        }
-
-        public void Notify(string notificationName, IDictionary<string, object> parameters) {
-            foreach(var observer in _observers) {
-                try {
-
-                    observer.OnNext(
-                        new KeyValuePair<string, IDictionary<string, object>>(notificationName, parameters));
-                }
-                catch (Exception ex) {
-                    observer.OnError(ex);
-                }
-                finally {
-                    observer.OnCompleted();
-                }
-            }
-        }
-    }
-
-
-    public class EventObserver : IObserver<KeyValuePair<string, IDictionary<string, object>>> {
-        private readonly NotificationListenerCache _listeners = new NotificationListenerCache();
-
-        public EventObserver(IEventHandler target) {
-            var typeInfo = target.GetType().GetTypeInfo();
-
-            var methodInfos = typeInfo.DeclaredMethods;
-
-            foreach (var methodInfo in methodInfos) {
-                var parameterNames = methodInfo.GetParameters().Select(x => x.Name);
-                var key = typeInfo.FullName + "_" + methodInfo.Name + "_" + string.Join("_", parameterNames);
-
-                Enlist(key, target, methodInfo);
-            }
-        }
-
-        private void Enlist(string notificationName, object target, MethodInfo methodInfo) {
-            var entries = _listeners.GetOrAdd(
-                notificationName,
-                _ => new ConcurrentBag<ListenerEntry>());
-
-            entries.Add(new ListenerEntry(target, methodInfo));
-        }
-
-        public void OnCompleted() {
-        }
-
-        public void OnError(Exception error) {
-        }
-
-        public void OnNext(KeyValuePair<string, IDictionary<string, object>> value) {
-            var notificationName = value.Key;
-            ConcurrentBag<ListenerEntry> entries;
-            if (_listeners.TryGetValue(notificationName, out entries)) {
-                foreach (var entry in entries) {
-                    // Do Something!
-                }
-            }
-        }
-
-        private class NotificationListenerCache : ConcurrentDictionary<string, ConcurrentBag<ListenerEntry>> {
-            public NotificationListenerCache()
-                : base(StringComparer.Ordinal) {
-            }
-        }
-
-        private class ListenerEntry {
-            public ListenerEntry(object target, MethodInfo methodInfo) {
-                Target = target;
-                MethodInfo = methodInfo;
-            }
-
-            public MethodInfo MethodInfo { get; }
-
-            public object Target { get; }
-        }
-    }
+    
 
     public interface ITestEvent : IEventHandler {
         int Counter { get; set; }
