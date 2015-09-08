a23b5d868b4099fbfb1901814146fcffa010cf08
Nicholas Mayne <Jetski5822@gmail.com>
2015-09-08 22:46:20 +0100

    missed some files

---
+++ src/Orchard.Hosting/HostServiceExtensions.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Dnx.Compilation.Caching;
+using Microsoft.Dnx.Runtime;
+using Microsoft.Framework.DependencyInjection;
+using Orchard.FileSystem;
+using Orchard.DependencyInjection;
+using Orchard.Configuration.Environment;
+using Orchard.Hosting.ShellBuilders;
+using Orchard.Hosting.Extensions;
+using Orchard.Hosting.Extensions.Folders;
+using Microsoft.Framework.DependencyInjection.Extensions;
+using Orchard.Hosting.Extensions.Loaders;
+using Microsoft.Framework.OptionsModel;
+
+namespace Orchard.Hosting {
+    public static class HostServiceExtensions {
+        public static IServiceCollection AddHost(
+            [NotNull] this IServiceCollection services, Action<IServiceCollection> additionalDependencies) {
+
+            services.AddFileSystems();
+
+            // Caching - Move out
+            services.AddInstance<ICacheContextAccessor>(new CacheContextAccessor());
+            services.AddSingleton<ICache, Cache>();
+
+            additionalDependencies(services);
+            
+            services.AddTransient<IOrchardShellHost, DefaultOrchardShellHost>();
+
+            return services.AddFallback();
+        }
+
+        public static IServiceCollection AddHostCore(this IServiceCollection services) {
+            services.AddSingleton<IOrchardHost, DefaultOrchardHost>();
+            {
+                services.AddSingleton<IShellSettingsManager, ShellSettingsManager>();
+
+                services.AddSingleton<IShellContextFactory, ShellContextFactory>();
+                {
+                    services.AddSingleton<ICompositionStrategy, CompositionStrategy>();
+                    {
+                        services.AddSingleton<IOrchardLibraryManager, OrchardLibraryManager>();
+                        services.AddSingleton<IExtensionManager, ExtensionManager>();
+                        {
+                            services.AddSingleton<IExtensionAssemblyLoader, ExtensionAssemblyLoader>();
+
+                            services.AddSingleton<IExtensionHarvester, ExtensionHarvester>();
+
+                            services.TryAddEnumerable(
+                                ServiceDescriptor.Transient<IConfigureOptions<ExtensionHarvestingOptions>, ExtensionHarvestingOptionsSetup>());
+                            services.AddSingleton<IExtensionLocator, ExtensionLocator>();
+
+                            services.AddSingleton<IExtensionLoader, CoreExtensionLoader>();
+                            services.AddSingleton<IExtensionLoader, DynamicExtensionLoader>();
+                        }
+                    }
+
+                    services.AddSingleton<IShellContainerFactory, ShellContainerFactory>();
+                }
+            }
+
+            return services;
+        }
+    }
+}
---
+++ src/Orchard.Hosting/ServiceExtensions.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Dnx.Runtime;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.DependencyInjection.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+#if DNXCORE50
+using System.Threading.Tasks;
+#endif
+
+namespace Orchard.Hosting
+{
+    public static class ServiceExtensions
+    {
+        internal static IServiceCollection AddFallback([NotNull] this IServiceCollection services) {
+
+            services.AddInstance<IRuntimeServices>(new ServiceManifest(services));
+
+            return services;
+        }
+
+        internal static IServiceProvider BuildShellServiceProviderWithHost(
+            [NotNull] this IServiceCollection services,
+            [NotNull] IServiceProvider hostServices) {
+
+            return new WrappingServiceProvider(hostServices, services);
+        }
+
+        private class ServiceManifest : IRuntimeServices {
+            public ServiceManifest(IServiceCollection fallback) {
+
+                var manifestTypes = fallback.Where(t => t.ServiceType.GetTypeInfo().GenericTypeParameters.Length == 0
+                        && t.ServiceType != typeof(IRuntimeServices)
+                        && t.ServiceType != typeof(IServiceProvider))
+                        .Select(t => t.ServiceType).Distinct();
+
+                Services = manifestTypes;
+            }
+
+            public IEnumerable<Type> Services { get; private set; }
+        }
+
+        private class WrappingServiceProvider : IServiceProvider {
+            private readonly IServiceProvider _services;
+
+            // Need full wrap for generics like IOptions
+            public WrappingServiceProvider(IServiceProvider fallback, IServiceCollection replacedServices) {
+                var services = new ServiceCollection();
+                var manifest = fallback.GetRequiredService<IRuntimeServices>();
+                foreach (var service in manifest.Services) {
+                    services.AddTransient(service, sp => fallback.GetService(service));
+                }
+
+                services.AddFallback();
+                services.Add(replacedServices);
+
+                _services = services.BuildServiceProvider();
+            }
+
+            public object GetService(Type serviceType) {
+                return _services.GetService(serviceType);
+            }
+        }
+    }
+}
