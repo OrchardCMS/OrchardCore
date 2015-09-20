d8774c55cbf31eabba1a3cee5903e2b6f79b2fd4
Nicholas Mayne <Jetski5822@gmail.com>
2015-09-20 15:40:38 +0100

    Small plug in to Setup for recipies, not plugged to full pipeline

--- src/Orchard.Web/Modules/Orchard.Setup/Services/ISetupService.cs
+++ src/Orchard.Web/Modules/Orchard.Setup/Services/ISetupService.cs
@@ -1,7 +1,10 @@
 using Orchard.DependencyInjection;
+using Orchard.Environment.Recipes.Models;
+using System.Collections.Generic;
 
 namespace Orchard.Setup.Services {
     public interface ISetupService : IDependency {
+        IEnumerable<Recipe> Recipes();
         string Setup(SetupContext context);
     }
 }
--- src/Orchard.Web/Modules/Orchard.Setup/Services/SetupService.cs
+++ src/Orchard.Web/Modules/Orchard.Setup/Services/SetupService.cs
@@ -8,15 +8,22 @@ using Orchard.Environment.Shell.Descriptor.Models;
 using Orchard.Environment.Shell.Builders;
 using Orchard.Environment.Shell;
 using Orchard.Environment.Shell.Models;
+using Orchard.Environment.Recipes.Models;
+using System.Collections.Generic;
+using Orchard.Environment.Recipes.Services;
+using Orchard.DependencyInjection;
 
 namespace Orchard.Setup.Services {
-    public class SetupService : ISetupService {
+    [OrchardFeature("Orchard.Setup.Services")]
+    public class SetupService : Component, ISetupService {
         private readonly IOrchardHost _orchardHost;
         private readonly IShellSettingsManager _shellSettingsManager;
         private readonly IShellContainerFactory _shellContainerFactory;
         private readonly ICompositionStrategy _compositionStrategy;
         private readonly IExtensionManager _extensionManager;
         private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IRecipeHarvester _recipeHarvester;
+        private IEnumerable<Recipe> _recipes;
 
         public SetupService(
             IOrchardHost orchardHost,
@@ -24,17 +31,25 @@ namespace Orchard.Setup.Services {
             IShellContainerFactory shellContainerFactory,
             ICompositionStrategy compositionStrategy,
             IExtensionManager extensionManager,
-            IHttpContextAccessor httpContextAccessor) {
+            IHttpContextAccessor httpContextAccessor,
+            IRecipeHarvester recipeHarvester) {
             _orchardHost = orchardHost;
             _shellSettingsManager = shellSettingsManager;
             _shellContainerFactory = shellContainerFactory;
             _compositionStrategy = compositionStrategy;
             _extensionManager = extensionManager;
             _httpContextAccessor = httpContextAccessor;
-            T = NullLocalizer.Instance;
+            _recipeHarvester = recipeHarvester;
         }
 
-        public Localizer T { get; set; }
+        public IEnumerable<Recipe> Recipes() {
+            if (_recipes == null) {
+                var recipes = new List<Recipe>();
+                recipes.AddRange(_recipeHarvester.HarvestRecipes().Where(recipe => recipe.IsSetupRecipe));
+                _recipes = recipes;
+            }
+            return _recipes;
+        }
 
         public string Setup(SetupContext context) {
             string executionId = Guid.NewGuid().ToString();
