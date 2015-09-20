7116ae3f68f8369e4edb521d8ac091bbb24f9066
Nicholas Mayne <Jetski5822@gmail.com>
2015-09-20 20:41:38 +0100

    Adding missing file

---
+++ src/Orchard.Data/Migration/Records/DataMigrationRecord.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Orchard.Data.Migration.Records {
+    public class DataMigrationDocument : StorageDocument {
+        public List<DataMigrationRecord> DataMigrationRecords = new List<DataMigrationRecord>();
+
+        public override string Data {
+            get {
+                return JsonConvert.SerializeObject(DataMigrationRecords);
+            }
+
+            set {
+                DataMigrationRecords = JsonConvert.DeserializeObject<List<DataMigrationRecord>>(value);
+            }
+        }
+    }
+
+    public class DataMigrationRecord {
+        public virtual string DataMigrationClass { get; set; }
+        public virtual int? Version { get; set; }
+    }
+}
