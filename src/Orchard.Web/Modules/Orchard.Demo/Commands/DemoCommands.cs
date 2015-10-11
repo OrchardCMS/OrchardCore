de7ddb850f6262a3223113244df40d756bd94262
Nicholas Mayne <Jetski5822@gmail.com>
2015-10-11 10:34:58 +0100

    Adding Demo commands in. demo helloworld /t:foo

---
+++ src/Orchard.Web/Modules/Orchard.Demo/Commands/DemoCommands.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+using Orchard.Environment.Commands;
+
+namespace Orchard.Demo.Commands {
+    public class DemoCommands : DefaultOrchardCommandHandler {
+        private readonly ILogger _logger;
+
+        public DemoCommands(ILoggerFactory loggerFactory) {
+            _logger = loggerFactory.CreateLogger<DemoCommands>();
+        }
+
+        [CommandName("demo helloworld")]
+        [CommandHelp("demo helloworld says hi!")]
+        public void HelloWorld() {
+            _logger.LogInformation("Hi there from Hello World!");
+        }
+    }
+}
