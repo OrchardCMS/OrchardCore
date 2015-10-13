906782101db5e735695d01d3d75f49e8a7334502
Nicholas Mayne <Jetski5822@gmail.com>
2015-10-13 21:38:45 +0100

    Fixing localizer

--- src/Orchard.Abstractions/Localization/NullLocalizer.cs
+++ src/Orchard.Abstractions/Localization/NullLocalizer.cs
@@ -4,7 +4,7 @@ namespace Orchard.Localization {
     public static class NullLocalizer {
 
         static NullLocalizer() {
-            Instance = (format, args) => new LocalizedString(null, (args == null || args.Length == 0) ? format : string.Format(format, args));
+            Instance = (format, args) => new LocalizedString("localizer", (args == null || args.Length == 0) ? format : string.Format(format, args));
         }
 
         public static Localizer Instance { get; }
