e1c4fe9d76513281456bec875a092a71cc4c8f7e
jtkech <jean-thierry.kechichian@wanadoo.fr>
2015-11-25 23:54:05 +0100

    Update bootstrap.cmd

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -7,4 +7,10 @@ SET skipdnvm=%1
     call dnvm upgrade  -r coreclr -arch x64
 )
 
+call dnu clear-http-cache
+cd src/orchard.web/modules
+call dnu restore
+cd../core
+call dnu restore
+cd../../..
 call dnu restore
