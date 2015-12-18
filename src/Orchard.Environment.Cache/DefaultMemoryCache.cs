f4eaf6c993e24116b9ad34e77d568fef79af254c
Sebastien Ros <sebastienros@gmail.com>
2015-12-17 17:30:35 -0800

    Registering IMemoryCache

---
+++ src/Orchard.Environment.Cache/DefaultMemoryCache.cs
@@ -0,0 +1,19 @@
+﻿using Orchard.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Orchard.Environment.Cache
+{
+    public class DefaultMemoryCache : IModule
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddInstance<IMemoryCache>(
+                new MemoryCache(new MemoryCacheOptions()
+                {
+                    CompactOnMemoryPressure = false,
+                })
+            );
+        }
+    }
+}
--- src/Orchard.Web/Core/Orchard.Core/Settings/Services/SiteService.cs
+++ src/Orchard.Web/Core/Orchard.Core/Settings/Services/SiteService.cs
@@ -1,4 +1,5 @@
-﻿using Orchard.ContentManagement;
+﻿using Microsoft.Extensions.Caching.Memory;
+using Orchard.ContentManagement;
 using Orchard.ContentManagement.Records;
 using System;
 using System.Threading.Tasks;
@@ -9,33 +10,50 @@ namespace Orchard.Core.Settings.Services
     public class SiteService : ISiteService
     {
         private readonly IContentManager _contentManager;
+        private readonly IMemoryCache _memoryCache;
         private readonly ISession _session;
+
+        private const string SiteCacheKey = "Site";
 
         public SiteService(
             ISession session,
-            IContentManager contentManager)
+            IContentManager contentManager,
+            IMemoryCache memoryCache)
         {
             _contentManager = contentManager;
             _session = session;
+            _memoryCache = memoryCache;
         }
 
         public async Task<ISite> GetSiteSettingsAsync()
         {
-            var site = await _session.QueryAsync<ContentItem, ContentItemIndex>(x => x.ContentType == "Site").FirstOrDefault();
-            
-            if (site == null)
+            ContentItem site;
+
+            if (!_memoryCache.TryGetValue(SiteCacheKey, out site))
             {
-                site = _contentManager.New("Site");
-                site.Weld(new SiteSettingsPart()
+                site = await _session.QueryAsync<ContentItem, ContentItemIndex>(x => x.ContentType == "Site").FirstOrDefault();
+
+                if (site == null)
                 {
+                    lock (_memoryCache)
+                    {
+                        if (!_memoryCache.TryGetValue(SiteCacheKey, out site))
+                        {
+                            site = _contentManager.New("Site");
+                            site.Weld(new SiteSettingsPart()
+                            {
+                                SiteSalt = Guid.NewGuid().ToString("N"),
+                                SiteName = "My Orchard Project Application",
+                                PageTitleSeparator = " - ",
+                                TimeZone = TimeZoneInfo.Local.Id
+                            });
 
-                    SiteSalt = Guid.NewGuid().ToString("N"),
-                    SiteName = "My Orchard Project Application",
-                    PageTitleSeparator = " - ",
-                    TimeZone = TimeZoneInfo.Local.Id
-                });
+                            _contentManager.Create(site);
+                        }
+                    }
+                }
 
-                _contentManager.Create(site);
+                _memoryCache.Set(SiteCacheKey, site);
             }
 
             return site.As<SiteSettingsPart>();
