ae56f1f02c0b9d90a8935bba234bde813c639cbb
Sebastien Ros <sebastienros@gmail.com>
2016-01-26 11:55:22 -0800

    Working on TitlePart

---
+++ src/Orchard.Web/Core/Orchard.Core/Title/Drivers/TitlePartDriver.cs
@@ -0,0 +1,14 @@
+﻿using Orchard.ContentManagement;
+using Orchard.ContentManagement.Drivers;
+using Orchard.Core.Title.Model;
+
+namespace Orchard.Core.Title.Drivers
+{
+    public class TitlePartDriver : ContentPartDriver<TitlePart>
+    {
+        protected override void GetContentItemMetadata(TitlePart part, ContentItemMetadata metadata)
+        {
+            metadata.DisplayText = part.Title;
+        }
+    }
+}
---
+++ src/Orchard.Web/Core/Orchard.Core/Title/Model/TitlePart.cs
@@ -0,0 +1,9 @@
+﻿using Orchard.ContentManagement;
+
+namespace Orchard.Core.Title.Model
+{
+    public class TitlePart : ContentPart
+    {
+        public string Title { get; set; }
+    }
+}
