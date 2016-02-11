b0da051c4d6d2d3599e716a8fc417f53625d90d7
Sebastien Ros <sebastienros@gmail.com>
2016-02-10 18:12:19 -0800

    Adding CheckMarkModelBinder
    Just for fun

---
+++ src/Orchard.Hosting.Web/Mvc/ModelBinding/CheckMarkModelBinder.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.Mvc.ModelBinding;
+using System.Threading.Tasks;
+
+namespace Orchard.Hosting.Mvc.ModelBinding
+{
+    public class CheckMarkModelBinder : IModelBinder
+    {
+        public Task<ModelBindingResult> BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if(bindingContext.ModelType == typeof(bool))
+            {
+                var valueProviderResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+                if (valueProviderResult == ValueProviderResult.None)
+                {
+                    return ModelBindingResult.NoResultAsync;
+                }
+
+                bindingContext.ModelState.SetModelValue(bindingContext.ModelName, valueProviderResult);
+
+                if (valueProviderResult.Values == "✓")
+                {
+                    return ModelBindingResult.SuccessAsync(bindingContext.ModelName, true);
+                }
+                else if(valueProviderResult.Values == "✗")
+                {
+                    return ModelBindingResult.SuccessAsync(bindingContext.ModelName, false);
+                }
+            }
+
+            return ModelBindingResult.NoResultAsync;
+        }
+    }
+}
