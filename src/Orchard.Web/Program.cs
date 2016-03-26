3b6a79c5c509011cffc814ce10a2a8eaf2919d03
Alexander Bocharov <alex.bocharov@outlook.com>
2016-03-26 18:26:47 +0300

    Reacting to Hosting changes in RC2

--- src/Orchard.Web/Program.cs
+++ src/Orchard.Web/Program.cs
@@ -1,28 +1,23 @@
 ﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Orchard.Hosting;
-using System.Threading.Tasks;
 
 namespace Orchard.Console
 {
     public class Program
     {
-        //https://github.com/aspnet/Hosting/blob/dev/src/Microsoft.AspNetCore.Hosting/Program.cs
-        public Task<int> Main(string[] args)
+        public static void Main(string[] args)
         {
-            //Add command line configuration source to read command line parameters.
-            var builder = new ConfigurationBuilder().AddCommandLine(args);
-            var config = builder.Build();
-
-            var host = new WebHostBuilder(config, true)
+            var host = new WebHostBuilder()
                 .UseServer("Microsoft.AspNetCore.Server.Kestrel")
+                .UseDefaultHostingConfiguration(args)
                 .Build();
-            using (var app = host.Start())
-            {
-                var orchardHost = new OrchardHost(app.Services, System.Console.In, System.Console.Out, args);
 
-                return Task.FromResult(
-                    (int)orchardHost.Run());
+            using (host)
+            {
+                host.Start();
+
+                var orchardHost = new OrchardHost(host.Services, System.Console.In, System.Console.Out, args);
+                orchardHost.Run();
             }
         }
     }
