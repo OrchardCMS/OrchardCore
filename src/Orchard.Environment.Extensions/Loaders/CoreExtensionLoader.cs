776095015c7625ac7e08301b1b97607968b8c7ce
Sebastien Ros <sebastienros@gmail.com>
2016-04-14 11:52:20 -0700

    Loading Core assembly

--- src/Orchard.Environment.Extensions/Loaders/CoreExtensionLoader.cs
+++ src/Orchard.Environment.Extensions/Loaders/CoreExtensionLoader.cs
@@ -6,6 +6,9 @@ using Orchard.DependencyInjection;
 using Orchard.Environment.Extensions.Models;
 using Microsoft.Extensions.Logging;
 using Orchard.FileSystem;
+using System.IO;
+using Microsoft.DotNet.ProjectModel;
+using Microsoft.DotNet.ProjectModel.Loader;
 
 namespace Orchard.Environment.Extensions.Loaders
 {
@@ -15,6 +18,7 @@ namespace Orchard.Environment.Extensions.Loaders
         private readonly IHostEnvironment _hostEnvironment;
         private readonly IOrchardFileSystem _fileSystem;
         private readonly ILogger _logger;
+        private Assembly _assembly;
 
         public CoreExtensionLoader(
             IHostEnvironment hostEnvironment,
@@ -50,9 +54,7 @@ namespace Orchard.Environment.Extensions.Loaders
                 return null;
             }
 
-            var directory = _fileSystem.GetDirectoryInfo("Core");
-
-            var assembly = Assembly.Load(new AssemblyName(CoreAssemblyName));
+            var assembly = LoadCoreAssembly();
 
             if (_logger.IsEnabled(LogLevel.Information))
             {
@@ -81,6 +83,20 @@ namespace Orchard.Environment.Extensions.Loaders
         private static bool IsTypeFromModule(Type type, ExtensionDescriptor descriptor)
         {
             return (type.Namespace + ".").StartsWith(CoreAssemblyName + "." + descriptor.Id + ".");
+        }
+
+        private Assembly LoadCoreAssembly()
+        {
+            if (_assembly == null)
+            {
+                var projectContext = ProjectContext.CreateContextForEachFramework("Core/Orchard.Core").FirstOrDefault();
+
+                var loadContext = projectContext.CreateLoadContext();
+
+                _assembly = loadContext.LoadFromAssemblyName(new AssemblyName(CoreAssemblyName));
+            }
+
+            return _assembly;
         }
     }
 }
