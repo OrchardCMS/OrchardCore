71609abfebac3eea39dd6e26195a2faa59d74d81
Sebastien Ros <sebastienros@gmail.com>
2016-04-22 15:52:45 -0700

    Adding front-end display for List

--- src/Orchard.Web/Modules/Orchard.Lists/Drivers/ListPartDisplayDriver.cs
+++ src/Orchard.Web/Modules/Orchard.Lists/Drivers/ListPartDisplayDriver.cs
@@ -1,15 +1,16 @@
-﻿using Orchard.ContentManagement;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Orchard.ContentManagement;
 using Orchard.ContentManagement.Display;
 using Orchard.ContentManagement.Display.ContentDisplay;
+using Orchard.DisplayManagement;
+using Orchard.DisplayManagement.ModelBinding;
 using Orchard.DisplayManagement.Views;
 using Orchard.Lists.Indexes;
 using Orchard.Lists.Models;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using YesSql.Core.Services;
-using Microsoft.Extensions.DependencyInjection;
-using Orchard.DisplayManagement.ModelBinding;
 
 namespace Orchard.Lists.Drivers
 {
@@ -36,11 +37,9 @@ namespace Orchard.Lists.Drivers
                 Shape("ListPart_DetailAdmin", async shape =>
                 {
                     var contentItemDisplayManager = _serviceProvider.GetService<IContentItemDisplayManager>();
-
-                    var query = _session.QueryAsync<ContentItem, ContainedPartIndex>(x => x.ListContentItemId == listPart.ContentItem.ContentItemId);
-
+                    var containedItems = await QueryListItems(listPart);
                     var containedItemsSummaries = new List<dynamic>();
-                    var containedItems = await query.List();
+
                     foreach (var contentItem in containedItems)
                     {
                         containedItemsSummaries.Add(await contentItemDisplayManager.BuildDisplayAsync(contentItem, updater, "SummaryAdmin"));
@@ -49,8 +48,33 @@ namespace Orchard.Lists.Drivers
                     shape.ContentItems = containedItemsSummaries;
                     shape.ContentItem = listPart.ContentItem;
                 })
-                .Location("DetailAdmin", "Content")
+                .Location("DetailAdmin", "Content"),
+
+                Shape("ListPart_Detail", async shape =>
+                {
+                    var contentItemDisplayManager = _serviceProvider.GetService<IContentItemDisplayManager>();
+                    var containedItems = await QueryListItems(listPart);
+                    var containedItemsSummaries = new List<dynamic>();
+
+                    foreach (var contentItem in containedItems)
+                    {
+                        var itemShape = await contentItemDisplayManager.BuildDisplayAsync(contentItem, updater, "Summary") as IShape;
+                        itemShape.Metadata.Alternates.Add("List_Summary__" + listPart.ContentItem.ContentType);
+                        containedItemsSummaries.Add(itemShape);
+                    }
+
+                    shape.ContentItems = containedItemsSummaries;
+                    shape.ContentItem = listPart.ContentItem;
+                })
+                .Location("Detail", "Content")
             );
+        }
+
+        private async Task<IEnumerable<ContentItem>> QueryListItems(ListPart listPart)
+        {
+            var query = _session.QueryAsync<ContentItem, ContainedPartIndex>(x => x.ListContentItemId == listPart.ContentItem.ContentItemId);
+            var containedItems = await query.List();
+            return containedItems;
         }
     }
 }
