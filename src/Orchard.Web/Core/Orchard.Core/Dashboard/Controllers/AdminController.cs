1132cc6cdbb1abed20a08ce12d042b4fddff4e61
Sebastien Ros <sebastienros@gmail.com>
2016-04-22 17:53:59 -0700

    Adding default dashboard

---
+++ src/Orchard.Web/Core/Orchard.Core/Dashboard/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orchard.DisplayManagement.Admin;
+
+namespace Orchard.Core.Dashboard
+{
+    [Admin]
+    public class AdminController : Controller
+    {
+        public AdminController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Orchard.Web/Core/Orchard.Core/Dashboard/Routes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Orchard.Hosting.Web.Routing.Routes;
+
+namespace Orchard.Core.Dashboard
+{
+    public class Routes : IRouteProvider
+    {
+        public IEnumerable<RouteDescriptor> GetRoutes()
+        {
+            return new[] {
+                new RouteDescriptor {
+                    Route = new Route(
+                        "Dashboard",
+                        "admin",
+                        defaults:  new
+                        {
+                            area = "Dashboard",
+                            controller = "Admin",
+                            action = "Index"
+                        },
+                        dataTokens:  new
+                        {
+                            area = "Dashboard"
+                        })
+                }
+            };
+        }
+    }
+}
