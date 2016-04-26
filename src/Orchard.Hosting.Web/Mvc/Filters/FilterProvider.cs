c3e76ec43b1ff92186b6ad3f56e3fd7b2faec12d
Sebastien Ros <sebastienros@gmail.com>
2016-04-25 17:34:56 -0700

    Upgrading to latest MVC bits for bug fixes

--- src/Orchard.Hosting.Web/Mvc/Filters/FilterProvider.cs
+++ src/Orchard.Hosting.Web/Mvc/Filters/FilterProvider.cs
@@ -28,18 +28,6 @@ namespace Orchard.Hosting.Mvc.Filters
                 foreach (var filter in filters)
                 {
                     var filterItem = new FilterItem(new FilterDescriptor(filter, FilterScope.Global), filter) { IsReusable = false };
-
-                    // TODO: Remove this when https://github.com/aspnet/Mvc/issues/4504
-                    // is fixed
-                    for (var i=0; i < context.Results.Count; i++)
-                    {
-                        if(context.Results[i].Filter.GetType() == filter.GetType())
-                        {
-                            context.Results.RemoveAt(i);
-                            break;
-                        }
-                    }
-
                     context.Results.Add(filterItem);
                 }
             }
