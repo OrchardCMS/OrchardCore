bdd1fe14c394acbe080817aac876d4d3bfbe5abc
Sebastien Ros <sebastienros@gmail.com>
2016-05-13 18:43:39 -0700

    Rendering ContentTypePart settings

--- src/Orchard.Web/Modules/Orchard.ContentTypes/Editors/ContentDefinitionDisplayCoordinator.cs
+++ src/Orchard.Web/Modules/Orchard.ContentTypes/Editors/ContentDefinitionDisplayCoordinator.cs
@@ -478,5 +478,18 @@ namespace Orchard.ContentTypes.Editors
             return Shape("ContentTypePartSettings_Edit", new { ContentPart = model }).Location("Content");
         }
     }
+    
+    public class BodyPartSettingsDisplayDriver : ContentTypePartDisplayDriver
+    {
+        public override IDisplayResult Edit(ContentTypePartDefinition model, IUpdateModel updater)
+        {
+            if (!String.Equals("BodyPart", model.PartDefinition.Name, StringComparison.Ordinal)) 
+            {
+                return null;
+            }
+            
+            return Shape("BodyPartSettings_Edit", new { ContentPart = model }).Location("Content");
+        }
+    }
 
 }
