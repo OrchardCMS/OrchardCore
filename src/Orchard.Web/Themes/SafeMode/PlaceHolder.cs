99e3016a033cbf082580a98347889c4b27f1dee8
Sebastien Ros <sebastienros@gmail.com>
2016-05-13 18:43:13 -0700

    Adding fake class for empty assemblies

---
+++ src/Orchard.Web/Themes/SafeMode/PlaceHolder.cs
@@ -0,0 +1,7 @@
+﻿namespace SafeMode
+{
+    // Just a class so that an assembly gets created 
+    public class Placeholder
+    {
+    }
+}
---
+++ src/Orchard.Web/Themes/TheTheme/PlaceHolder.cs
@@ -0,0 +1,7 @@
+﻿namespace TheTheme
+{
+    // Just a class so that an assembly gets created 
+    public class Placeholder
+    {
+    }
+}
