41cb86931efd8d03f02dab6fdefac9655f51b628
Nicholas Mayne <Jetski5822@gmail.com>
2016-06-03 12:23:25 +0100

    React to configuration changes

--- src/Orchard.Parser.Yaml/YamlConfigurationExtensions.cs
+++ src/Orchard.Parser.Yaml/YamlConfigurationExtensions.cs
@@ -1,53 +1,55 @@
 ﻿using System;
 using Microsoft.Extensions.Configuration;
 using Orchard.Parser.Yaml;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
 
 namespace Orchard.Parser
 {
     public static class YamlConfigurationExtensions
     {
-        public static IConfigurationBuilder AddYamlFile(this IConfigurationBuilder configuration, string path)
+        public static IConfigurationBuilder AddYamlFile(this IConfigurationBuilder builder, string path)
         {
-            if (configuration == null)
-            {
-                throw new ArgumentNullException(nameof(configuration));
-            }
-
-            return AddYamlFile(configuration, source => source.Path = path);
+            return AddYamlFile(builder, provider: null, path: path, optional: false, reloadOnChange: false);
         }
 
-        public static IConfigurationBuilder AddYamlFile(
-            this IConfigurationBuilder builder,
-            string path,
-            bool optional)
+        public static IConfigurationBuilder AddYamlFile(this IConfigurationBuilder builder, string path, bool optional)
+        {
+            return AddYamlFile(builder, provider: null, path: path, optional: optional, reloadOnChange: false);
+        }
+
+        public static IConfigurationBuilder AddYamlFile(this IConfigurationBuilder builder, string path, bool optional, bool reloadOnChange)
+        {
+            return AddYamlFile(builder, provider: null, path: path, optional: optional, reloadOnChange: reloadOnChange);
+        }
+
+        public static IConfigurationBuilder AddYamlFile(this IConfigurationBuilder builder, IFileProvider provider, string path, bool optional, bool reloadOnChange)
         {
             if (builder == null)
             {
                 throw new ArgumentNullException(nameof(builder));
             }
-
             if (string.IsNullOrEmpty(path))
             {
                 throw new ArgumentException("InvalidFilePath", nameof(path));
             }
 
-            return AddYamlFile(builder, source =>
+            if (provider == null && Path.IsPathRooted(path))
             {
-                source.Path = path;
-                source.Optional = optional;
-            });
-        }
-
-        public static IConfigurationBuilder AddYamlFile(
-            this IConfigurationBuilder builder,
-            Action<YamlConfigurationSource> configureSource)
-        {
-            var source = new YamlConfigurationSource();
-            configureSource(source);
+                provider = new PhysicalFileProvider(Path.GetDirectoryName(path));
+                path = Path.GetFileName(path);
+            }
+            var source = new YamlConfigurationSource
+            {
+                FileProvider = provider,
+                Path = path,
+                Optional = optional,
+                ReloadOnChange = reloadOnChange
+            };
             builder.Add(source);
             return builder;
         }
-
+        
         public static string Get(this IConfigurationProvider provider, string key)
         {
             string value;
