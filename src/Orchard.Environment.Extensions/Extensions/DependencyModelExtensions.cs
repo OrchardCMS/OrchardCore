77b96e9f7bbcf3d3a636f3c007c7428c8e5206be
jtkech <jean-thierry.kechichian@wanadoo.fr>
2016-06-22 18:26:23 +0200

    Formatting

--- src/Orchard.Environment.Extensions/Extensions/DependencyModelExtensions.cs
+++ src/Orchard.Environment.Extensions/Extensions/DependencyModelExtensions.cs
@@ -8,7 +8,8 @@ namespace Orchard.Environment.Extensions.DependencyModel
 {
     public static class DependencyContextExtensions
     {
-        public static IEnumerable<CompilationLibrary> GetProjectTypeCompileLibraries(this DependencyContext context) {
+        public static IEnumerable<CompilationLibrary> GetProjectTypeCompileLibraries(this DependencyContext context)
+        {
             return context.CompileLibraries.Where(x => x.Type.Equals(LibraryType.Project.ToString(), StringComparison.OrdinalIgnoreCase));
         }
     }
