3387028dc212e3c9fdda4a4b35a8c47d28be1e43
Sergio Navarro <jersio@hotmail.com>
2016-08-10 14:54:27 +0200

    Refactor

--- src/Orchard.Web/Modules/Orchard.OpenId/Services/OpenIdUserManager.cs
+++ src/Orchard.Web/Modules/Orchard.OpenId/Services/OpenIdUserManager.cs
@@ -26,81 +26,19 @@ namespace Orchard.OpenId.Services
 
         public override async Task<ClaimsIdentity> CreateIdentityAsync(User user, IEnumerable<string> scopes)
         {
-            if (user == null)
-            {
-                throw new ArgumentNullException(nameof(user));
-            }
-
-            if (scopes == null)
-            {
-                throw new ArgumentNullException(nameof(scopes));
-            }
-
-            var identity = new ClaimsIdentity(
-                OpenIdConnectServerDefaults.AuthenticationScheme,
-                Options.ClaimsIdentity.UserNameClaimType,
-                Options.ClaimsIdentity.RoleClaimType);
-
-            // Note: the name identifier is always included in both identity and
-            // access tokens, even if an explicit destination is not specified.
-            identity.AddClaim(ClaimTypes.NameIdentifier, await GetUserIdAsync(user));
-
-            // Resolve the email address associated with the user if the underlying store supports it.
-            var email = SupportsUserEmail ? await GetEmailAsync(user) : null;
-
-            // Only add the name claim if the "profile" scope was granted.
-            if (scopes.Contains(OpenIdConnectConstants.Scopes.Profile))
-            {
-                var username = await GetUserNameAsync(user);
-
-                // Throw an exception if the username corresponds to the registered
-                // email address and if the "email" scope has not been requested.
-                if (!scopes.Contains(OpenIdConnectConstants.Scopes.Email) &&
-                    !string.IsNullOrEmpty(email) &&
-                     string.Equals(username, email, StringComparison.OrdinalIgnoreCase))
-                {
-                    throw new InvalidOperationException("The 'email' scope is required.");
-                }
-
-                identity.AddClaim(ClaimTypes.Name, username,
-                    OpenIdConnectConstants.Destinations.AccessToken,
-                    OpenIdConnectConstants.Destinations.IdentityToken);
-            }
-
-            // Only add the email address if the "email" scope was granted.
-            if (!string.IsNullOrEmpty(email) && scopes.Contains(OpenIdConnectConstants.Scopes.Email))
-            {
-                identity.AddClaim(ClaimTypes.Email, email,
-                    OpenIdConnectConstants.Destinations.AccessToken,
-                    OpenIdConnectConstants.Destinations.IdentityToken);
-            }
-
+            var identity = await base.CreateIdentityAsync(user, scopes);
+            
             if (SupportsUserRole && scopes.Contains(OpenIddictConstants.Scopes.Roles))
             {
                 foreach (var role in await GetRolesAsync(user))
                 {
-                    identity.AddClaim(identity.RoleClaimType, role,
-                        OpenIdConnectConstants.Destinations.AccessToken,
-                        OpenIdConnectConstants.Destinations.IdentityToken);
                     foreach (var claim in await _roleManager.GetClaimsAsync(await _roleManager.FindByNameAsync(role)))
                     {
                         identity.AddClaim(claim.Type, claim.Value, OpenIdConnectConstants.Destinations.IdentityToken);
                     }                   
                 }
             }
-
-            if (SupportsUserSecurityStamp)
-            {
-                var stamp = await GetSecurityStampAsync(user);
-
-                if (!string.IsNullOrEmpty(stamp))
-                {
-                    identity.AddClaim(Options.ClaimsIdentity.SecurityStampClaimType, stamp,
-                        OpenIdConnectConstants.Destinations.AccessToken,
-                        OpenIdConnectConstants.Destinations.IdentityToken);
-                }
-            }
-
+                        
             return identity;
         }
     }
