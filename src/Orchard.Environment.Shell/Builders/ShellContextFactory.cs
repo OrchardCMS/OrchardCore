8081c15fbbe7ce97599df474b2827471e2bc7945
Sergio Navarro <jersio@hotmail.com>
2016-08-22 21:20:51 +0200

    Adding OpenId module for OpenId/OAuth2 support
    * #210 Added OpenId module for OpenId/OAuth2 support
    
    * Refactored token storage
    
    * Fixed bug on OpenIdApplicationStore and added missed Error View
    
    * Overrided OpenIddictUserManager for adding Permision Claims of an user's roles
    
    * Refactor
    
    * Removing async keywork in PermissionHandler
    
    * Set BaseUrl on Setup cause it is needed by OpenId middleware
    
    * Added Permission Claims to AccessToken and a DataProtectionProvider per tenant
    
    * Enabled AllowAuthorizationCodeFlow and disblec ImplicitFlow plu bugfixes
    
    * Extra isolation between tenants at cookie level
    
    * Fix PhysicalAppDataFolderTests (#219)
    
    * [Fixes #220] Fix OS X build on Travis
    
    * Fix ExtensionLocaterTests.cs (#222)
    
    * Fix DefaultDisplayManagerTests (#223)
    
    * Client Credentials flow enabled
    
    * Added roles to OpeniIdApp edit/create views

--- src/Orchard.Environment.Shell/Builders/ShellContextFactory.cs
+++ src/Orchard.Environment.Shell/Builders/ShellContextFactory.cs
@@ -103,6 +103,7 @@ namespace Orchard.Environment.Shell.Builders
                 SerialNumber = -1,
                 Features = new List<ShellFeature>(_shellFeatures),
                     new ShellFeature { Name = "Orchard.OpenId" },
+                    new ShellFeature { Name = "Orchard.OpenId" },
                 Parameters = new List<ShellParameter>()
             };
         }
