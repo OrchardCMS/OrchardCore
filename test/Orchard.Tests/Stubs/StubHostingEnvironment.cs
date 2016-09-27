47cc4ddb2d06b326bd2fe60155b926b00e5eb865
Nicholas Mayne <Jetski5822@gmail.com>
2016-09-28 00:40:49 +0700

    More filesystem jazz

--- test/Orchard.Tests/Stubs/StubHostingEnvironment.cs
+++ test/Orchard.Tests/Stubs/StubHostingEnvironment.cs
@@ -1,11 +1,78 @@
 ﻿using System;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.FileProviders;
 
 namespace Orchard.Tests.Stubs
 {
-    public class StubHttpContextAccessor : IHttpContextAccessor
+    public class StubHostingEnvironment : IHostingEnvironment
     {
-        public HttpContext HttpContext
+        public string ApplicationName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IFileProvider ContentRootFileProvider
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string ContentRootPath
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string EnvironmentName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IFileProvider WebRootFileProvider
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string WebRootPath
         {
             get
             {
