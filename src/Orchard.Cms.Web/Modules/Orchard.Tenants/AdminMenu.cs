241aa97b874651de0acd1e28d81c78fe889d7b4d
Sebastien Ros <sebastienros@gmail.com>
2016-10-26 13:13:35 -0700

    Creating Orchard.Tenants module

---
+++ src/Orchard.Cms.Web/Modules/Orchard.Tenants/AdminMenu.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Localization;
+using Orchard.Environment.Navigation;
+using System;
+
+namespace Orchard.Tenants
+{
+    public class AdminMenu : INavigationProvider
+    {
+        public AdminMenu(IStringLocalizer<AdminMenu> localizer)
+        {
+            T = localizer;
+        }
+
+        public IStringLocalizer T { get; set; }
+
+        public void BuildNavigation(string name, NavigationBuilder builder)
+        {
+            if (!String.Equals(name, "admin", StringComparison.OrdinalIgnoreCase))
+            {
+                return;
+            }
+
+            builder
+                .Add(T["Design"], "10", design => design
+                    .Add(T["Site"], "10", import => import
+                        .Add(T["Tenants"], "5", deployment => deployment
+                            .Action("Index", "Admin", new { area = "Orchard.Tenants" })
+                            .Permission(Permissions.ManageTenants)
+                            .LocalNav()
+                        )
+                    )
+                );
+        }
+    }
+}
