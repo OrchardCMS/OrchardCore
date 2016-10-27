fb2f16550b06acebc1899e4a8f3fa02fe329cf17
Antoine Griffard <agriffard@hotmail.com>
2016-10-27 17:51:28 +0200

    Orchard.Modules : inital commit. see #277

---
+++ src/Orchard.Cms.Web/Modules/Orchard.Modules/AdminMenu.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Localization;
+using Orchard.Environment.Navigation;
+using System;
+
+namespace Orchard.Modules
+{
+    public class AdminMenu : INavigationProvider
+    {
+        public AdminMenu(IStringLocalizer<AdminMenu> localizer)
+        {
+            T = localizer;
+        }
+
+        public IStringLocalizer T { get; set; }
+        
+        public void BuildNavigation(string name, NavigationBuilder builder)
+        {
+            if (!String.Equals(name, "admin", StringComparison.OrdinalIgnoreCase))
+            {
+                return;
+            }
+
+            builder
+                .Add(T["Modules"], "9", modules => modules
+                    .AddClass("modules")
+                    .Add(T["Features"], "0", installed => installed
+                        .Action("Features", "Admin", new { area = "Orchard.Modules" })
+                        .Permission(Permissions.ManageFeatures)
+                        .LocalNav())
+                );
+        }
+    }
+}
