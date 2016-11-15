8eebb838c2903446a7a7f50b984f2009b32ad2cd
Sebastien Ros <sebastienros@gmail.com>
2016-11-15 11:56:43 -0800

    [Fixes #320] Fixing RoleStore
    The updated document was incorrectly loaded from cache after changes.
    Also the RolesDocument was duplicated in database due to missing Id property in the class.
    
    Fixes #320

--- src/Orchard.Cms.Web/Modules/Orchard.Roles/Models/RolesDocument.cs
+++ src/Orchard.Cms.Web/Modules/Orchard.Roles/Models/RolesDocument.cs
@@ -5,6 +5,7 @@ namespace Orchard.Roles.Models
 {
     public class RolesDocument
     {
+        public int Id { get; set; }
         public List<Role> Roles { get; } = new List<Role>();
         public int Serial { get; set; }
     }
