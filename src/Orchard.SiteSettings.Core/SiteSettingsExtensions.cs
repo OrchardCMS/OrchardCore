798a1b732f723a45c461de303c80a7f7a393b37b
Sebastien Ros <sebastienros@gmail.com>
2016-11-22 10:47:24 -0800

    Adding a named helper to read site settings.

--- src/Orchard.SiteSettings.Core/SiteSettingsExtensions.cs
+++ src/Orchard.SiteSettings.Core/SiteSettingsExtensions.cs
@@ -4,12 +4,28 @@ namespace Orchard.Settings
 {
     public static class SiteSettingsExtensions
     {
+        /// <summary>
+        /// Extracts the specified type of porperty.
+        /// </summary>
+        /// <typeparam name="T">The type of the property to extract.</typeparam>
+        /// <returns>The default value of the requested type if the property was not found.</returns>
         public static T As<T>(this ISite site)
         {
-            JToken value;
             var typeName = typeof(T).Name;
+            return site.As<T>(typeName);
+        }
 
-            if (site.Properties.TryGetValue(typeName, out value))
+        /// <summary>
+        /// Extracts the specified named porperty.
+        /// </summary>
+        /// <typeparam name="T">The type of the property to extract.</typeparam>
+        /// <param name="name">The name of the property to extract.</param>
+        /// <returns>The default value of the requested type if the property was not found.</returns>
+        public static T As<T>(this ISite site, string name)
+        {
+            JToken value;
+
+            if (site.Properties.TryGetValue(name, out value))
             {
                 return value.ToObject<T>();
             }
