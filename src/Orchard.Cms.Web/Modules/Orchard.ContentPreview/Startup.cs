98f029d8f50bc8f0bf2df3068105bd144f91e4d7
Sebastien Ros <sebastienros@gmail.com>
2016-11-22 18:24:58 -0800

    Adding Orchard.ContentPreview module

---
+++ src/Orchard.Cms.Web/Modules/Orchard.ContentPreview/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Modules;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Orchard.ContentManagement.Display.ContentDisplay;
+using Orchard.ContentPreview.Drivers;
+
+namespace Orchard.ContentPreview
+{
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IContentDisplayDriver, ContentPreviewDriver>();
+        }
+
+        public override void Configure(IApplicationBuilder builder, IRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+        }
+    }
+}
