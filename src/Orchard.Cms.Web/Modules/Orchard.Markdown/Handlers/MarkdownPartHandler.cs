97e2128617f383ee99ad3978495acb578d4f0b63
Sébastien Ros <sebastienros@gmail.com>
2016-12-01 00:29:05 -0800

    Adding Orchard.Markdown (#372)

---
+++ src/Orchard.Cms.Web/Modules/Orchard.Markdown/Handlers/MarkdownPartHandler.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Html;
+using Orchard.Markdown.Model;
+using Orchard.ContentManagement.Handlers;
+using Orchard.ContentManagement.Models;
+
+namespace Orchard.Markdown.Handlers
+{
+    public class MarkdownPartHandler : ContentPartHandler<MarkdownPart>
+    {
+        public override void GetContentItemAspect(ContentItemAspectContext context, MarkdownPart part)
+        {
+            context.For<BodyAspect>(bodyAspect => bodyAspect.Body = new HtmlString(part.Markdown));
+        }
+    }
+}
---
+++ src/Orchard.Cms.Web/Modules/Orchard.Markdown/Model/MarkdownPart.cs
@@ -0,0 +1,9 @@
+﻿using Orchard.ContentManagement;
+
+namespace Orchard.Markdown.Model
+{
+    public class MarkdownPart : ContentPart
+    {
+        public string Markdown { get; set; }
+    }
+}
