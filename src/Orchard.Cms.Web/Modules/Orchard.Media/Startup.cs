80631fb850b6a4a5f04c6e7a9e99eb3de7f8e394
Antoine Griffard <agriffard@hotmail.com>
2016-12-03 16:52:23 +0100

    Serve static files from Media folder #370

---
+++ src/Orchard.Cms.Web/Modules/Orchard.Media/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Modules;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+
+namespace Orchard.Media
+{
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public override void Configure(IApplicationBuilder app, IRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+            var hostingEnvironment = app.ApplicationServices.GetRequiredService<IHostingEnvironment>();
+
+            // Add static files to the request pipeline.
+            app.UseStaticFiles();
+
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(hostingEnvironment.ContentRootPath, "Media")),
+                RequestPath = new PathString("/Media")
+            });
+        }
+    }
+}
