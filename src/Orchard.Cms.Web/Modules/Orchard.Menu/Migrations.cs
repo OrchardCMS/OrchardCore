a8fd8196e5ea73c49aac96d5f7f532075df78842
Sebastien Ros <sebastienros@gmail.com>
2016-12-09 16:22:52 -0800

    Adding AliasPart to Menu

--- src/Orchard.Cms.Web/Modules/Orchard.Menu/Migrations.cs
+++ src/Orchard.Cms.Web/Modules/Orchard.Menu/Migrations.cs
@@ -18,9 +18,10 @@ namespace Orchard.Menu
             _contentDefinitionManager.AlterTypeDefinition("Menu", menu => menu
                 .Draftable()
                 .Creatable()
-                .WithPart("MenuPart")
-                .WithPart("TitlePart")
-                .WithPart("MenuItemsListPart")
+                .WithPart("TitlePart", part => part.WithPosition("1"))
+                .WithPart("AliasPart", part => part.WithPosition("2").WithSettings(new AliasPartSettings { Pattern = "{{slug}}" }))
+                .WithPart("MenuPart", part => part.WithPosition("3"))
+                .WithPart("MenuItemsListPart", part => part.WithPosition("4"))
             );
 
             _contentDefinitionManager.AlterTypeDefinition("LinkMenuItem", menu => menu
@@ -30,5 +31,10 @@ namespace Orchard.Menu
 
             return 1;
         }
+    }
+
+    class AliasPartSettings
+    {
+        public string Pattern { get; set; }
     }
 }
--- src/Orchard.ContentManagement.Abstractions/MetaData/Settings/ContentTypePartSettings.cs
+++ src/Orchard.ContentManagement.Abstractions/MetaData/Settings/ContentTypePartSettings.cs
@@ -11,5 +11,10 @@
         /// Gets or sets the description name of the type part.
         /// </summary>
         public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the position of the type part in the editor.
+        /// </summary>
+        public string Position { get; set; }
     }
 }
