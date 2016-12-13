d579ff61c70b8352cf4d748aba46c0071ede300b
Nicholas Mayne <Jetski5822@gmail.com>
2016-12-13 13:04:30 +0800

    Fixing Compare

--- src/Orchard.DisplayManagement/Extensions/ThemeExtensionOrderingStrategy.cs
+++ src/Orchard.DisplayManagement/Extensions/ThemeExtensionOrderingStrategy.cs
@@ -1,24 +1,27 @@
-﻿using System;
-using Orchard.Environment.Extensions;
+﻿using Orchard.Environment.Extensions;
 using Orchard.Environment.Extensions.Features;
 
 namespace Orchard.DisplayManagement.Extensions
 {
     public class ThemeExtensionOrderingStrategy : IExtensionOrderingStrategy
     {
-
         public double Priority => 10D;
 
         public int Compare(IFeatureInfo observer, IFeatureInfo subject)
         {
-            if (observer.Extension.Manifest.IsTheme())
+            var isObserverTheme = observer.Extension.Manifest.IsTheme();
+            var isSubjectTheme = subject.Extension.Manifest.IsTheme();
+
+            if (isObserverTheme && !isSubjectTheme)
             {
-                if (subject.Extension.Manifest.IsTheme())
-                {
-                    return -1;
-                }
                 return 1;
             }
+
+            if (!isObserverTheme && isSubjectTheme)
+            {
+                return -1;
+            }
+
             return 0;
         }
     }
