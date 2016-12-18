a3883b4ac2316fdd493c02eefddae2272f56a8c9
Antoine Griffard <agriffard@hotmail.com>
2016-12-18 13:28:13 +0100

    Resources: update bootstrap 3, jQuery, add font awesome. Fix SetVersion in StyleTagHelper.

--- src/Orchard.Cms.Web/Modules/Orchard.Resources/ResourceManifest.cs
+++ src/Orchard.Cms.Web/Modules/Orchard.Resources/ResourceManifest.cs
@@ -10,7 +10,20 @@ namespace Orchard.Resources
 
             manifest
                 .DefineScript("jQuery")
-                .SetCdn("https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js")
+                .SetCdn("https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js")
+                .SetVersion("1.12.4")
+                ;
+
+            manifest
+                .DefineScript("jQuery")
+                .SetCdn("https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js")
+                .SetVersion("2.2.4")
+                ;
+
+            manifest
+                .DefineScript("jQuery")
+                .SetCdn("https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js")
+                .SetVersion("3.1.1")
                 ;
 
             manifest
@@ -19,25 +32,51 @@ namespace Orchard.Resources
                 .SetUrl("/Orchard.Resources/Scripts/jquery-ui.min.js", "/Orchard.Resources/Scripts/jquery-ui.js")
                 .SetCdn("https://code.jquery.com/ui/1.12.1/jquery-ui.min.js", "https://code.jquery.com/ui/1.12.1/jquery-ui.js")
                 ;
+            
+            manifest
+                .DefineScript("bootstrap")
+                .SetDependencies("jQuery")
+                .SetCdn("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js")
+                .SetCdnIntegrity("sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa")
+                .SetVersion("3.3.7")
+                ;
+
+            manifest
+                .DefineStyle("bootstrap")
+                .SetCdn("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css")
+                .SetCdnIntegrity("sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u")
+                .SetVersion("3.3.7")
+                ;
+
+            manifest
+                .DefineStyle("bootstrap-theme")
+                .SetCdn("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css")
+                .SetCdnIntegrity("sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp")
+                .SetVersion("3.3.7")
+                ;
 
             manifest
                 .DefineScript("bootstrap")
                 .SetDependencies("jQuery")
                 .SetUrl("/Orchard.Resources/Scripts/bootstrap.min.js", "/Orchard.Resources/Scripts/bootstrap.js")
-                .SetCdn("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js")
-                .SetCdnIntegrity("sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS")
+                .SetCdn("https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js")
+                .SetCdnIntegrity("sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK")
+                .SetVersion("4.0.0")
                 ;
 
             manifest
                 .DefineStyle("bootstrap")
-                .SetCdn("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css")
-                .SetCdnIntegrity("sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7")
+                .SetCdn("https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css")
+                .SetCdnIntegrity("sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi")
+                .SetVersion("4.0.0")
                 ;
 
             manifest
-                .DefineStyle("bootstrap-theme")
-                .SetCdn("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css")
-                .SetCdnIntegrity("sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r")
+                .DefineStyle("font-awesome")
+                .SetUrl("/Orchard.Resources/Styles/font-awesome.min.css", "/Orchard.Resources/Styles/font-awesome.css")
+                .SetCdn("https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css")
+                .SetCdnIntegrity("sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN")
+                .SetVersion("4.7.0")
                 ;
         }
     }
