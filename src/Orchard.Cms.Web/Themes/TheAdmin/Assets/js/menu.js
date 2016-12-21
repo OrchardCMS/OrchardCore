555f0469b0285ed13df7c45438cc4b1a9e3371fc
Sebastien Ros <sebastienros@gmail.com>
2016-12-21 07:57:54 -0800

    Fixing scrollbar when chevron is rotated
    The corner of the span would go out of the box with the rotation. The solution
    was to rotate the char only.

--- src/Orchard.Cms.Web/Themes/TheAdmin/Assets/js/menu.js
+++ src/Orchard.Cms.Web/Themes/TheAdmin/Assets/js/menu.js
@@ -1,8 +1,3 @@
 ﻿$(function () {
-    $(".dropdown input").change(function () {
-        //alert(this);
-        var input = $(this);
-        var li = input.closest('li');
-        li.toggleClass('active');
-    });
+    
 });
