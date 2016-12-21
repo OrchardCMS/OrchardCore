3a6c8fc775d37897efec2e07b25458ac8bd97155
Sebastien Ros <sebastienros@gmail.com>
2016-12-21 13:14:33 -0800

    Adding flyout menu

--- src/Orchard.Cms.Web/Themes/TheAdmin/Assets/js/menu.js
+++ src/Orchard.Cms.Web/Themes/TheAdmin/Assets/js/menu.js
@@ -1,3 +1,13 @@
 ﻿$(function () {
-    
+    $('#navbar-toggler').on('click', function () {
+        var nav = $('#ta-left-sidebar');
+        if (nav.hasClass('in')) {
+            nav.animate({ 'left': '-260px' }, 300);
+            nav.removeClass('in');
+        }
+        else {
+            nav.animate({ 'left': '0px' }, 300);
+            nav.addClass('in');
+        }
+    });
 });
