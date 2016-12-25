e9f43681199634b5be57f7ed2701e8a977dd5f8b
Nicholas Mayne <Jetski5822@gmail.com>
2016-12-25 17:31:07 +0800

    Starting work on Glimpse

---
+++ src/Orchard.Cms.Web/Modules/Orchard.Glimpse/DefaultAssemblyProvider.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Glimpse.Internal;
+using Microsoft.Extensions.DependencyModel;
+using Orchard.Environment.Shell.Builders.Models;
+
+namespace Orchard.Glimpse
+{
+    public class OrchardAssemblyProvider : IAssemblyProvider
+    {
+        private static HashSet<string> ReferenceAssemblies { get; } = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+        {
+            "Glimpse.Common",
+            "Glimpse.Server",
+            "Glimpse.Agent.AspNet",
+            "Glimpse.Agent.AspNet.Mvc"
+        };
+
+        private readonly ShellBlueprint _shellBlueprint;
+
+        public OrchardAssemblyProvider(ShellBlueprint shellBlueprint)
+        {
+            _shellBlueprint = shellBlueprint;
+        }
+
+        public IEnumerable<Assembly> GetCandidateAssemblies(string coreLibrary)
+        {
+            return GetCandidateLibraries(DependencyContext.Default)
+                .Select(Assembly.Load);
+        }
+
+        // Returns a list of libraries that references the assemblies in <see cref="ReferenceAssemblies"/>.
+        // By default it returns all assemblies that reference any of the primary MVC assemblies
+        // while ignoring MVC assemblies.
+        // Internal for unit testing
+        private IEnumerable<AssemblyName> GetCandidateLibraries(DependencyContext dependencyContext)
+        {
+            if (ReferenceAssemblies == null)
+            {
+                return Enumerable.Empty<AssemblyName>();
+            }
+
+            return ReferenceAssemblies
+                .Select(x => new AssemblyName(x));
+                //.SelectMany(x => dependencyContext.GetRuntimeAssemblyNames(x))
+                //.Distinct()
+                //.Concat();
+        }
+    }
+}
---
+++ src/Orchard.Cms.Web/Modules/Orchard.Glimpse/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Glimpse;
+using Glimpse.Common.Initialization;
+using Glimpse.Initialization;
+using Glimpse.Internal;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Modules;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Orchard.Glimpse
+{
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.Add(GlimpseServices.GetDefaultServices());
+
+            services.AddScoped<IAssemblyProvider, OrchardAssemblyProvider>();
+
+            var servicesProvider = services
+                .BuildServiceProvider();
+
+            var extensionProvider = servicesProvider
+                .GetService<IExtensionProvider<IRegisterServices>>();
+
+            var glimpseServiceCollectionBuilder = new GlimpseServiceCollectionBuilder(services);
+
+            foreach (var registration in extensionProvider.Instances)
+            {
+                registration.RegisterServices(glimpseServiceCollectionBuilder);
+            }
+
+            (servicesProvider as IDisposable).Dispose();
+
+        }
+
+        public override void Configure(IApplicationBuilder app, IRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+            app.UseGlimpse();
+        }
+    }
+}
