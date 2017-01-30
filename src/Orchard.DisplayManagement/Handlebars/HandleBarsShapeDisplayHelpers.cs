da9e65dbeb3aed9e654e92fdf3c8742f29d0ce05
jtkech <jean-thierry.kechichian@wanadoo.fr>
2017-01-30 01:46:35 +0100

    incremental work on Handlebars.

---
+++ src/Orchard.DisplayManagement/Handlebars/HandleBarsShapeDisplayHelpers.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using HandlebarsDotNet;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Orchard.DisplayManagement.Implementation;
+using Orchard.DisplayManagement.Shapes;
+
+namespace Orchard.DisplayManagement.HandleBars
+{
+    public static class HandleBarsShapeDisplayHelpers
+    {
+        public static void RegisterShapeDisplayHelpers()
+        {
+            Handlebars.RegisterHelper("foreach", (output, options, context, arguments) =>
+            {
+                foreach (var shape in (IEnumerable<dynamic>)arguments[0])
+                {
+                    if (shape is Shape)
+                    {
+                        shape.DisplayContext = context.DisplayContext;
+                        shape.Html = context.Html;
+                        shape.Url = context.Url;
+                    }
+
+                    options.Template(output, shape);
+                }
+            });
+
+            Handlebars.RegisterHelper("Display", async (output, context, arguments) =>
+            {
+                var displayContext = context.DisplayContext as DisplayContext;
+                var shape = arguments.Count() > 0 ? arguments[0] : (object)context;
+                output.WriteSafeString(await displayContext.DisplayAsync.ShapeExecuteAsync(shape));
+            });
+
+            Handlebars.RegisterHelper("UrlContent", (output, context, arguments) =>
+            {
+                var urlHelper = context.Url as IUrlHelper;
+                output.WriteSafeString(urlHelper.Content((string)arguments[0]));
+            });
+
+            Handlebars.RegisterHelper("UrlAction", (output, context, arguments) =>
+            {
+                var urlHelper = context.Url as IUrlHelper;
+
+                output.WriteSafeString(urlHelper.Action(
+                    (string)arguments[0], (string)arguments[1],
+                    arguments.Count() > 2 ? arguments[2] : new { }));
+            });
+
+            Handlebars.RegisterHelper("HtmlRaw", (output, context, arguments) =>
+            {
+                var htmlHelper = context.Html as IHtmlHelper;
+                output.WriteSafeString(htmlHelper.Raw((string)arguments[0]));
+            });
+
+            Handlebars.RegisterHelper("HtmlActionLink", (output, context, arguments) =>
+            {
+                var htmlHelper = context.Html as IHtmlHelper;
+                var content = (TagBuilder)htmlHelper.ActionLink(
+                    (string)arguments[0], (string)arguments[1],
+                    new { }, arguments.Count() > 2 ? arguments[2] : new { });
+
+                using (var writer = new StringWriter())
+                {
+                    content.WriteTo(writer, System.Text.Encodings.Web.HtmlEncoder.Default);
+                    output.WriteSafeString(writer.ToString());
+                }
+            });
+        }
+    }
+}
--- src/Orchard.DisplayManagement/Handlebars/HandlebarsShapeTemplateViewEngine.cs
+++ src/Orchard.DisplayManagement/Handlebars/HandlebarsShapeTemplateViewEngine.cs
@@ -2,31 +2,84 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
+using HandlebarsDotNet;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Html;
-using Orchard.DisplayManagement.Descriptors;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.DependencyInjection;
 using Orchard.DisplayManagement.Descriptors.ShapeTemplateStrategy;
 using Orchard.DisplayManagement.Implementation;
+using Orchard.DisplayManagement.Shapes;
 
-namespace Orchard.DisplayManagement.Handlebars
+namespace Orchard.DisplayManagement.HandleBars
 {
-    public class HandlebarsShapeTemplateViewEngine : IHandlebarsShapeTemplateViewEngine
+    public class HandleBarsShapeTemplateViewEngine : IShapeTemplateViewEngine
     {
-        private static ConcurrentDictionary<string, Func<object, string>> _renderers = new ConcurrentDictionary<string, Func<object, string>>();
+        private readonly ConcurrentDictionary<string, Func<object, string>> _renderers;
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public HandleBarsShapeTemplateViewEngine(
+            IHostingEnvironment hostingEnvironment)
+        {
+            _renderers = new ConcurrentDictionary<string, Func<object, string>>();
+            _hostingEnvironment = hostingEnvironment;
+        }
 
         public IEnumerable<string> TemplateFileExtensions
         {
             get {
-                return new[] { "hbs" };
+                return new[] { ".hbs" };
             }
         }
 
-        public Task<IHtmlContent> RenderAsync(ShapeDescriptor shapeDescriptor, DisplayContext displayContext, HarvestShapeInfo harvestShapeInfo)
+        public async Task<IHtmlContent> RenderAsync(string relativePath, DisplayContext displayContext)
         {
-            var render = _renderers.GetOrAdd(harvestShapeInfo.TemplateVirtualPath,
-                HandlebarsDotNet.Handlebars.Compile(File.ReadAllText(harvestShapeInfo.PhysicalPath)));
+            var viewEngineInstance = displayContext.ServiceProvider
+                .GetRequiredService<IEnumerable<IShapeTemplateViewEngine>>()
+                .FirstOrDefault(e => e is HandleBarsShapeTemplateViewEngine);
 
-            return Task.FromResult<IHtmlContent>(new HtmlString(render(displayContext.Value)));
+            return await ((HandleBarsShapeTemplateViewEngine)viewEngineInstance)
+                .RenderAsyncInternal(relativePath, displayContext);
+        }
+
+        private Task<IHtmlContent> RenderAsyncInternal(string relativePath, DisplayContext displayContext)
+        {
+            var physicalPath = _hostingEnvironment.ContentRootFileProvider.GetFileInfo(relativePath).PhysicalPath;
+            var render = _renderers.GetOrAdd(relativePath, Handlebars.Compile(File.ReadAllText(physicalPath)));
+
+            var urlHelperFactory = displayContext.ServiceProvider.GetService<IUrlHelperFactory>();
+            var urlHelper = urlHelperFactory.GetUrlHelper(displayContext.ViewContext);
+
+            dynamic context = new Composite();
+            context.DisplayContext = displayContext;
+            context.Model = displayContext.Value;
+
+            if (displayContext.ViewContext.View != null)
+            {
+                context.Html = MakeHtmlHelper(displayContext.ViewContext, displayContext.ViewContext.ViewData);
+            }
+
+            context.Url = urlHelper;
+
+            return Task.FromResult<IHtmlContent>(new HtmlString(render(context)));
+        }
+
+        private static IHtmlHelper MakeHtmlHelper(ViewContext viewContext, ViewDataDictionary viewData)
+        {
+            var newHelper = viewContext.HttpContext.RequestServices.GetRequiredService<IHtmlHelper>();
+
+            var contextable = newHelper as IViewContextAware;
+            if (contextable != null)
+            {
+                var newViewContext = new ViewContext(viewContext, viewContext.View, viewData, viewContext.Writer);
+                contextable.Contextualize(newViewContext);
+            }
+
+            return newHelper;
         }
     }
 }
