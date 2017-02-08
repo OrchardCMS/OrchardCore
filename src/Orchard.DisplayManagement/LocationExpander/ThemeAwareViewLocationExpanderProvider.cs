b2bd55fe270b931169de493be7515dace8f19d38
Nicholas Mayne <Jetski5822@gmail.com>
2017-02-08 20:02:32 +0000

    more di

--- src/Orchard.DisplayManagement/LocationExpander/ThemeAwareViewLocationExpanderProvider.cs
+++ src/Orchard.DisplayManagement/LocationExpander/ThemeAwareViewLocationExpanderProvider.cs
@@ -11,6 +11,12 @@ namespace Orchard.DisplayManagement.LocationExpander
 {
     public class ThemeAwareViewLocationExpanderProvider : IViewLocationExpanderProvider
     {
+        private readonly IExtensionManager _extensionManager;
+        public ThemeAwareViewLocationExpanderProvider(IExtensionManager extensionManager)
+        {
+            _extensionManager = extensionManager;
+        }
+
         public int Priority => 15;
 
         /// <inheritdoc />
@@ -45,12 +51,6 @@ namespace Orchard.DisplayManagement.LocationExpander
 
             if (themeManager != null)
             {
-                var extensionManager = context
-                    .ActionContext
-                    .HttpContext
-                    .RequestServices
-                    .GetService<IExtensionManager>();
-
                 var currentTheme = themeManager.GetThemeAsync().GetAwaiter().GetResult();
 
                 if (currentTheme == null)
@@ -58,7 +58,7 @@ namespace Orchard.DisplayManagement.LocationExpander
                     return Enumerable.Empty<string>();
                 }
 
-                var currentThemeAndBaseThemesOrdered = extensionManager
+                var currentThemeAndBaseThemesOrdered = _extensionManager
                     .GetFeatures(new[] { currentTheme.Id })
                     .Where(x => x.Extension.Manifest.IsTheme())
                     .Reverse();
