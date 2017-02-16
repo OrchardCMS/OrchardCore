bdf82d9db929cbb69a1a557d22ff8bde85ccd23e
Sebastien Ros <sebastienros@gmail.com>
2017-02-15 16:31:33 -0800

    Supporting precompiled views

--- src/Microsoft.AspNetCore.Mvc.Modules/SharedCompilerCacheProvider.cs
+++ src/Microsoft.AspNetCore.Mvc.Modules/SharedCompilerCacheProvider.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc.ApplicationParts;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using Microsoft.AspNetCore.Mvc.Razor.Compilation;
 using Microsoft.AspNetCore.Mvc.Razor.Internal;
 
@@ -16,15 +20,29 @@ namespace Microsoft.AspNetCore.Mvc.Modules
 
 		public SharedCompilerCacheProvider(
 			ApplicationPartManager applicationPartManager,
-			IRazorViewEngineFileProviderAccessor fileProviderAccessor)
+			IRazorViewEngineFileProviderAccessor fileProviderAccessor,
+			IHostingEnvironment env)
 		{
-			var feature = new ViewsFeature();
-			applicationPartManager.PopulateFeature(feature);
-
 			lock (_synLock)
 			{
 				if (_cache == null)
 				{
+					var feature = new ViewsFeature();
+					applicationPartManager.PopulateFeature(feature);
+
+					// Applying ViewsFeatureProvider to gather any precompiled view
+					var viewInfoContainerTypeName = GetViewInfoContainerType(new AssemblyName(env.ApplicationName));
+
+					if (viewInfoContainerTypeName != null)
+					{
+						var viewContainer = (ViewInfoContainer)Activator.CreateInstance(viewInfoContainerTypeName);
+
+						foreach (var item in viewContainer.ViewInfos)
+						{
+							feature.Views[item.Path] = item.Type;
+						}
+					}
+
 					_cache = new CompilerCache(fileProviderAccessor.FileProvider, feature.Views);
 				}
 			}
@@ -37,6 +55,37 @@ namespace Microsoft.AspNetCore.Mvc.Modules
 			{
 				return _cache;
 			}
+		}
+
+		private Type GetViewInfoContainerType(AssemblyName applicationAssemblyName)
+		{
+
+			var applicationAssembly = Assembly.Load(applicationAssemblyName);
+			
+#if NETSTANDARD1_6
+			var precompiledAssemblyFileName = applicationAssemblyName.Name
+				+ ViewsFeatureProvider.PrecompiledViewsAssemblySuffix
+				+ ".dll";
+			var precompiledAssemblyFilePath = Path.Combine(
+				Path.GetDirectoryName(applicationAssembly.Location),
+				precompiledAssemblyFileName);
+
+			if (File.Exists(precompiledAssemblyFilePath))
+			{
+				try
+				{
+					System.Runtime.Loader.AssemblyLoadContext.Default.LoadFromAssemblyPath(precompiledAssemblyFilePath);
+				}
+				catch (FileLoadException)
+				{
+					// Don't throw if assembly cannot be loaded. This can happen if the file is not a managed assembly.
+				}
+			}
+#endif
+			var precompiledAssemblyName = new AssemblyName(applicationAssemblyName.Name + ViewsFeatureProvider.PrecompiledViewsAssemblySuffix);
+
+			var typeName = $"{ViewsFeatureProvider.ViewInfoContainerNamespace}.{ViewsFeatureProvider.ViewInfoContainerTypeName},{precompiledAssemblyName}";
+			return Type.GetType(typeName);
 		}
 	}
 }
