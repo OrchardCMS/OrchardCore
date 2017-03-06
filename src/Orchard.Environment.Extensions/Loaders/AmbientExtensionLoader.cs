333af3a614416bc4d31ee6c6da051b4649f6cf9d
Sebastien Ros <sebastienros@gmail.com>
2017-03-06 09:53:46 -0800

    Fixing project references

--- src/Orchard.Environment.Extensions/Loaders/AmbientExtensionLoader.cs
+++ src/Orchard.Environment.Extensions/Loaders/AmbientExtensionLoader.cs
@@ -16,24 +16,36 @@ namespace Orchard.Environment.Extensions.Loaders
 
         public ExtensionEntry Load(IExtensionInfo extensionInfo)
         {
-            var assembly = Assembly.Load(new AssemblyName(extensionInfo.Id));
-
-            if (assembly == null)
+            try
             {
+                var assembly = Assembly.Load(new AssemblyName(extensionInfo.Id));
+
+                if (assembly == null)
+                {
+                    return null;
+                }
+
+                if (_logger.IsEnabled(LogLevel.Information))
+                {
+                    _logger.LogInformation("Loaded referenced ambient extension \"{0}\": assembly name=\"{1}\"", extensionInfo.Id, assembly.FullName);
+                }
+
+                return new ExtensionEntry
+                {
+                    ExtensionInfo = extensionInfo,
+                    Assembly = assembly,
+                    ExportedTypes = assembly.ExportedTypes
+                };
+            }
+            catch
+            {
+                if (_logger.IsEnabled(LogLevel.Information))
+                {
+                    _logger.LogInformation("An extension found but was not loaded: \"{0}\". It might denote an extension which was not referenced by the running application project.", extensionInfo.Id);
+                }
+
                 return null;
             }
-
-            if (_logger.IsEnabled(LogLevel.Information))
-            {
-                _logger.LogInformation("Loaded referenced ambient extension \"{0}\": assembly name=\"{1}\"", extensionInfo.Id, assembly.FullName);
-            }
-
-            return new ExtensionEntry
-            {
-                ExtensionInfo = extensionInfo,
-                Assembly = assembly,
-                ExportedTypes = assembly.ExportedTypes
-            };
         }
     }
 }
