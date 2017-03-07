02787381f76453f21935c842cc579ff2e85513ea
jtkech <jean-thierry.kechichian@wanadoo.fr>
2017-03-07 02:46:50 +0100

    Reuses a shared compiler cache.

--- src/Microsoft.AspNetCore.Mvc.Modules/Extensions/ModularServiceCollectionExtensions.cs
+++ src/Microsoft.AspNetCore.Mvc.Modules/Extensions/ModularServiceCollectionExtensions.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using Microsoft.AspNetCore.Mvc.Modules.LocationExpander;
 using Microsoft.AspNetCore.Mvc.Modules.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Razor.Internal;
 using Microsoft.AspNetCore.Mvc.Razor.TagHelpers;
 using Microsoft.AspNetCore.Mvc.TagHelpers;
 using Microsoft.AspNetCore.Mvc.ViewFeatures.Internal;
@@ -48,6 +49,10 @@ namespace Microsoft.AspNetCore.Mvc.Modules
             AddModularFrameworkParts(applicationServices, builder.PartManager);
 
             builder.AddModularRazorViewEngine(applicationServices);
+
+            // Use a custom ICompilerCacheProvider so all tenant reuse the same ICompilerCache instance
+            builder.Services.Replace(new ServiceDescriptor(typeof(ICompilerCacheProvider),
+                typeof(SharedCompilerCacheProvider), ServiceLifetime.Singleton));
 
             AddMvcModuleCoreServices(services);
             AddDefaultFrameworkParts(builder.PartManager);
---
+++ src/Microsoft.AspNetCore.Mvc.Modules/SharedCompilerCacheProvider.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.Mvc.Razor.Internal;
+
+namespace Microsoft.AspNetCore.Mvc.Modules
+{
+    /// <summary>
+    /// This implementation of <see cref="ICompilerCacheProvider"/> shares the same <see cref="ICompilerCache"/>
+    /// instance across all tenants of the same application in order for the compiled view. Otherwise each
+    /// tenant would get its own compiled view.
+    /// </summary>
+    public class SharedCompilerCacheProvider : ICompilerCacheProvider
+    {
+        private static ICompilerCache _cache;
+        private static object _synLock = new object();
+
+        public SharedCompilerCacheProvider(
+            ApplicationPartManager applicationPartManager,
+            IRazorViewEngineFileProviderAccessor fileProviderAccessor)
+        {
+            lock (_synLock)
+            {
+                if (_cache == null)
+                {
+                    _cache = new DefaultCompilerCacheProvider(
+                        applicationPartManager,
+                        fileProviderAccessor)
+                        .Cache;
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        public ICompilerCache Cache
+        {
+            get
+            {
+                return _cache;
+            }
+        }
+    }
+}
