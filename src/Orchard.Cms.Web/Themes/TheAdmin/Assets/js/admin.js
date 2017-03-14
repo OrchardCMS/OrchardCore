e59f55b878e0b77b8648c1f10454eace1256ba47
Sergio Navarro <jersio@hotmail.com>
2017-03-15 00:30:53 +0100

    Allowing behavior: hide on checkbox checked (#563)

--- src/Orchard.Cms.Web/Themes/TheAdmin/Assets/js/admin.js
+++ src/Orchard.Cms.Web/Themes/TheAdmin/Assets/js/admin.js
@@ -87,6 +87,29 @@ $(function () {
     });
 });
 
+$(function () {
+    $('input[data-toggle="collapse active"]').each(function () {
+        // Prevent bootstrap from altering its behavior for inputs that hide target when input value is checked
+        // c.f. https://github.com/twbs/bootstrap/issues/21079
+        $(this).removeAttr("data-toggle");
+
+        // Expand the section if necessary
+        var target = $($(this).data('target'));
+        if (!$(this).prop('checked')) {
+            target.addClass('collapsed');
+        }
+
+        $(this).on('change', function (e) {
+            // During a double-click, ignore state changes while the element is collapsing
+            if (target.hasClass('collapsing')) {
+                console.log('collapsing');
+                $(this).prop('checked', !$(this).prop('checked'));
+            }
+            target.collapse($(this).prop('checked') ? 'hide' : 'show');
+        });
+    });
+});
+
 function getTechnicalName(name){
     var result = "", c;
 
