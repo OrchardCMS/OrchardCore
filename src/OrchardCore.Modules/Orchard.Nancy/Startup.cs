84ff647d425d67ab4b44422ab43998b5aa270d11
Nicholas Mayne <Jetski5822@gmail.com>
2017-03-30 21:19:34 +0100

    fixing nancy

--- src/OrchardCore.Modules/Orchard.Nancy/Startup.cs
+++ src/OrchardCore.Modules/Orchard.Nancy/Startup.cs
@@ -3,11 +3,17 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Modules;
 using Microsoft.AspNetCore.Nancy.Modules;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Orchard.Nancy
 {
     public class Startup : StartupBase
     {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+        }
+
         public override void Configure(IApplicationBuilder app, IRouteBuilder routes, IServiceProvider serviceProvider)
         {
             app.ConfigureModules(apb =>
