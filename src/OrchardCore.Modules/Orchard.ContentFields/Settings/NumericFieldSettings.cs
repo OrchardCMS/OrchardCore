be13f500b2ad65da8ceeecdad2f1aa01fdc562ca
Antoine Griffard <agriffard@hotmail.com>
2017-04-03 22:07:28 +0200

    Add Editor and Range option

--- src/OrchardCore.Modules/Orchard.ContentFields/Settings/NumericFieldSettings.cs
+++ src/OrchardCore.Modules/Orchard.ContentFields/Settings/NumericFieldSettings.cs
@@ -5,6 +5,7 @@ namespace Orchard.ContentFields.Settings
     public class NumericFieldSettings
     {
         public string Hint { get; set; }
+        public string Editor { get; set; }
         public bool Required { get; set; }
         public int Scale { get; set; }
         public decimal? Minimum { get; set; }
