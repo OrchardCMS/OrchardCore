d314acfe663406674413940459a82c764a9ec9d5
Antoine Griffard <agriffard@hotmail.com>
2017-04-03 14:05:58 +0200

    Numeric Field wip

---
+++ src/OrchardCore.Modules/Orchard.ContentFields/Indexing/NumericFieldIndexHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Orchard.ContentFields.Fields;
+using Orchard.Indexing;
+
+namespace Orchard.ContentFields.Indexing
+{
+    public class NumericFieldIndexHandler : ContentFieldIndexHandler<NumericField>
+    {
+        public override Task BuildIndexAsync(NumericField field, BuildFieldIndexContext context)
+        {
+            var options = context.Settings.ToOptions();
+            context.DocumentIndex.Entries.Add(context.Key, new DocumentIndex.DocumentIndexEntry(field.Value, DocumentIndex.Types.Number, options));
+
+            return Task.CompletedTask;
+        }
+    }
+}
