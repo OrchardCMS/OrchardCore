f532ec25366cf15b138d3df6e1de539f9843dd13
Sebastien Ros <sebastienros@gmail.com>
2017-04-03 18:15:20 -0700

    Using precompiled views for Theming

---
+++ src/OrchardCore/Microsoft.AspNetCore.Mvc.Modules.Abstractions/IViewsFeatureAlteration.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor.Compilation;
+
+namespace Microsoft.AspNetCore.Mvc.Modules
+{
+    public interface IViewsFeatureAlteration
+    {
+        void Alter(ViewsFeature viewsFeature);
+    }
+}
---
+++ src/OrchardCore/Orchard.DisplayManagement/Theming/ThemeLayout.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace Orchard.DisplayManagement.Theming
+{
+    public class ThemeLayout : Razor.RazorPage<dynamic>
+    {
+        public ThemeLayout()
+        {
+        }
+
+        [Microsoft.AspNetCore.Mvc.Razor.Internal.RazorInjectAttribute]
+        public IModelExpressionProvider ModelExpressionProvider { get; private set; }
+        [Microsoft.AspNetCore.Mvc.Razor.Internal.RazorInjectAttribute]
+        public IUrlHelper Url { get; private set; }
+        [Microsoft.AspNetCore.Mvc.Razor.Internal.RazorInjectAttribute]
+        public IViewComponentHelper Component { get; private set; }
+        [Microsoft.AspNetCore.Mvc.Razor.Internal.RazorInjectAttribute]
+        public IJsonHelper Json { get; private set; }
+        [Microsoft.AspNetCore.Mvc.Razor.Internal.RazorInjectAttribute]
+        public IHtmlHelper<dynamic> Html { get; private set; }
+
+        public override async Task ExecuteAsync()
+        {
+            var body = RenderLayoutBody();
+            this.ThemeLayout.Content.Add(body);
+            Write(await DisplayAsync(ThemeLayout));
+        }
+    }
+
+}
