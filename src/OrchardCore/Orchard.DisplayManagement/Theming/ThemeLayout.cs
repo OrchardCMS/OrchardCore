df93acee4fb6e3b0f53fc98e870da8ac9a370f85
Sébastien Ros <sebastienros@gmail.com>
2017-04-07 16:14:13 -0700

    Removing unused properties

--- src/OrchardCore/Orchard.DisplayManagement/Theming/ThemeLayout.cs
+++ src/OrchardCore/Orchard.DisplayManagement/Theming/ThemeLayout.cs
@@ -7,17 +7,6 @@ namespace Orchard.DisplayManagement.Theming
 {
     public class ThemeLayout : Razor.RazorPage<dynamic>
     {
-        [Microsoft.AspNetCore.Mvc.Razor.Internal.RazorInjectAttribute]
-        public IModelExpressionProvider ModelExpressionProvider { get; private set; }
-        [Microsoft.AspNetCore.Mvc.Razor.Internal.RazorInjectAttribute]
-        public IUrlHelper Url { get; private set; }
-        [Microsoft.AspNetCore.Mvc.Razor.Internal.RazorInjectAttribute]
-        public IViewComponentHelper Component { get; private set; }
-        [Microsoft.AspNetCore.Mvc.Razor.Internal.RazorInjectAttribute]
-        public IJsonHelper Json { get; private set; }
-        [Microsoft.AspNetCore.Mvc.Razor.Internal.RazorInjectAttribute]
-        public IHtmlHelper<dynamic> Html { get; private set; }
-
         public override async Task ExecuteAsync()
         {
             var body = RenderLayoutBody();
