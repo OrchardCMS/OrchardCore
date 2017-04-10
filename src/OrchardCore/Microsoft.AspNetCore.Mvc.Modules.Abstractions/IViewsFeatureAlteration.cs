9620a459a9778043ed3b08c2450e789a11ca86a1
jtkech <jean-thierry.kechichian@wanadoo.fr>
2017-04-10 07:07:05 +0200

    Implements ThemingViewsFeatureProvider.

--- src/OrchardCore/Microsoft.AspNetCore.Mvc.Modules.Abstractions/IViewsFeatureAlteration.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc.Razor.Compilation;
-
-namespace Microsoft.AspNetCore.Mvc.Modules
-{
-    public interface IViewsFeatureAlteration
-    {
-        void Alter(ViewsFeature viewsFeature);
-    }
-}
