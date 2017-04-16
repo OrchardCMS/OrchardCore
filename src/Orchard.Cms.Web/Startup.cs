5940a41f9db0134ac82e9e80aa0e00bf81659e66
Sergio Navarro <jersio@hotmail.com>
2017-04-16 10:00:27 +0200

    Added extraDefaultFeatureIds param to AddOrchardCms extension

--- src/Orchard.Cms.Web/Startup.cs
+++ src/Orchard.Cms.Web/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
