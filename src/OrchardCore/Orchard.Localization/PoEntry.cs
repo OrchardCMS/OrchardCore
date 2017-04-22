6bde5a8320452253c0ab22032219b64216fcea04
Lukas Kabrt <lukas@kabrt.cz>
2017-04-23 00:51:37 +0200

    po file parser

---
+++ src/OrchardCore/Orchard.Localization/PoEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orchard.Localization
+{
+    public class PoEntry
+    {
+        public string MessageId { get; set; }
+        public string Context { get; set; }
+        public string[] Values { get; set; }
+    }
+}
---
+++ test/Orchard.Tests/Localization/PoParserTests.cs
@@ -0,0 +1,172 @@
+﻿using Orchard.Localization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Orchard.Tests.Localization
+{
+    public class PoParserTests
+    {
+        [Fact]
+        public void ParseRetursSimpleEntry()
+        {
+            // msgid "Unknown system error"
+            // msgstr "Error desconegut del sistema"
+            var entries = ParseText(PoSampleFiles.SimpleEntry);
+
+            Assert.Equal("Unknown system error", entries[0].MessageId);
+            Assert.Equal("Error desconegut del sistema", entries[0].Values[0]);
+        }
+
+        [Fact]
+        public void ParseIgnoresEntryWithoutTranslation()
+        {
+            // "msgid "Unknown system error"
+            // "msgstr ""
+            var entries = ParseText(PoSampleFiles.EntryWithoutTranslation);
+
+            Assert.Empty(entries);
+        }
+
+        [Fact]
+        public void ParseIgnoresPoeditHeader()
+        {
+            // msgid ""
+            // msgstr ""
+            // "POT-Creation-Date: 2016-06-22 17:06+0200\n"
+            // "PO-Revision-Date: 2016-08-01 11:57+0200\n"
+            // "X-Poedit-Basepath: ../../..\n"
+            var entries = ParseText(PoSampleFiles.PoeditHeader);
+
+            Assert.Empty(entries);
+        }
+
+        [Fact]
+        public void ParseSetsContext()
+        {
+            // msgctxt "Orchard.Localization"
+            // msgid "Unknown system error"
+            // msgstr "Error desconegut del sistema"
+            var entries = ParseText(PoSampleFiles.EntryWithContext);
+
+            Assert.Equal("Orchard.Localization", entries[0].Context);
+        }
+
+        [Fact]
+        public void ParseIgnoresComments()
+        {
+            // # translator-comments
+            // #. extracted-comments
+            // #: reference…
+            // #, flag
+
+            // #| msgctxt previous-context
+            // #| msgid previous-untranslated-string
+            // msgctxt "Orchard.Localization"
+            // msgid "Unknown system error"
+            // msgstr "Error desconegut del sistema"
+
+            var entries = ParseText(PoSampleFiles.EntryWithComments);
+
+            Assert.Equal("Orchard.Localization", entries[0].Context);
+            Assert.Equal("Unknown system error", entries[0].MessageId);
+            Assert.Equal("Error desconegut del sistema", entries[0].Values[0]);
+        }
+
+        [Fact]
+        public void ParseOnlyTrimsLeadingAndTrailingQuotes()
+        {
+            // msgid "Foo \"{0}\""
+            // msgstr "Foo \"{0}\""
+
+            var entries = ParseText(PoSampleFiles.EntryWithQuotes);
+
+            Assert.Equal("Foo \"{0}\"", entries[0].MessageId);
+            Assert.Equal("Foo \"{0}\"", entries[0].Values[0]);
+        }
+
+        [Fact]
+        public void ParseHandlesMultilineEntry()
+        {
+            // msgid ""
+            // "Here is an example of how one might continue a very long string\n"
+            // "for the common case the string represents multi-line output."
+            // msgstr ""
+            // "Here is an example of how one might continue a very long translation\n"
+            // "for the common case the string represents multi-line output."
+
+            var entries = ParseText(PoSampleFiles.EntryWithMultilineText);
+
+            Assert.Equal("Here is an example of how one might continue a very long string\nfor the common case the string represents multi-line output.", entries[0].MessageId);
+            Assert.Equal("Here is an example of how one might continue a very long translation\nfor the common case the string represents multi-line output.", entries[0].Values[0]);
+        }
+
+        [Fact]
+        public void ParsePreservesEscapedCharacters()
+        {
+            // msgid "Line:\t\"{0}\"\n"
+            // msgstr "Line:\t\"{0}\"\n"
+
+            var entries = ParseText(PoSampleFiles.EntryWithEscapedCharacters);
+
+            Assert.Equal("Line:\t\"{0}\"\n", entries[0].MessageId);
+            Assert.Equal("Line:\t\"{0}\"\n", entries[0].Values[0]);
+        }
+
+        [Fact]
+        public void ParseReadsPluralTranslations()
+        {
+            // msgid "book"
+            // msgid_plural "books"
+            // msgstr[0] "kniha"
+            // msgstr[1] "knihy"
+            // msgstr[2] "knih"
+
+            var entries = ParseText(PoSampleFiles.EntryWithPlural);
+
+            Assert.Equal("book", entries[0].MessageId);
+            Assert.Equal("kniha", entries[0].Values[0]);
+            Assert.Equal("knihy", entries[0].Values[1]);
+            Assert.Equal("knih", entries[0].Values[2]);
+        }
+
+        [Fact]
+        public void ParseReadsMultipleEntries()
+        {
+            // #. "File {0} does not exist"
+            // msgctxt "Orchard.FileSystems.Media.FileSystemStorageProvider"
+            // msgid "File {0} does not exist"
+            // msgstr "Soubor {0} neexistuje"
+
+            // #. "Directory {0} does not exist"
+            // msgctxt "Orchard.FileSystems.Media.Directory"
+            // msgid "Directory {0} does not exist"
+            // msgstr "Složka {0} neexistuje"
+
+            var entries = ParseText(PoSampleFiles.MultipleEntries);
+
+            Assert.Equal(2, entries.Length);
+
+            Assert.Equal("Orchard.FileSystems.Media.FileSystemStorageProvider", entries[0].Context);
+            Assert.Equal("File {0} does not exist", entries[0].MessageId);
+            Assert.Equal("Soubor {0} neexistuje", entries[0].Values[0]);
+
+            Assert.Equal("Orchard.FileSystems.Media.Directory", entries[1].Context);
+            Assert.Equal("Directory {0} does not exist", entries[1].MessageId);
+            Assert.Equal("Složka {0} neexistuje", entries[1].Values[0]);
+        }
+
+        private PoEntry[] ParseText(string text)
+        {
+            var parser = new PoParser();
+
+            using (var reader = new StringReader(text))
+            {
+                return parser.Parse(reader).ToArray();
+            }
+        }
+    }
+}
