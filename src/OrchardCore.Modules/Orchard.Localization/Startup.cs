1fe59935c50526d690b16bf514297b2f29ea779e
Lukas Kabrt <lukas@kabrt.cz>
2017-05-01 21:09:31 +0200

    Add localization middleware

--- src/OrchardCore.Modules/Orchard.Localization/Startup.cs
+++ src/OrchardCore.Modules/Orchard.Localization/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Modules;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Orchard.Localization.Core;
+using System.Globalization;
 
 namespace Orchard.Localization
 {
@@ -12,6 +13,16 @@ namespace Orchard.Localization
     /// </summary>
     public class Startup : StartupBase
     {
+        public override void Configure(IApplicationBuilder app, IRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+            app.UseRequestLocalization(new RequestLocalizationOptions()
+            {
+                //TODO set supported cultures
+            });
+
+            base.Configure(app, routes, serviceProvider);
+        }
+
         public override void ConfigureServices(IServiceCollection services)
         {
             services.AddOrchardLocalization();
