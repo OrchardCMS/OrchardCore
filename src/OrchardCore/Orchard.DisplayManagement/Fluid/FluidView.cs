de60c1a1a6684c31798109626ee9f3c95cf006e0
jtkech <jean-thierry.kechichian@wanadoo.fr>
2017-06-24 07:45:40 +0200

    Begin to use Memory Cache.

--- src/OrchardCore/Orchard.DisplayManagement/Fluid/FluidView.cs
+++ src/OrchardCore/Orchard.DisplayManagement/Fluid/FluidView.cs
@@ -7,14 +7,18 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Orchard.Liquid;
+
 
 namespace Orchard.DisplayManagement.Fluid
 {
     public class FluidView : Razor.RazorPage<dynamic>
     {
         public static readonly string ViewExtension = ".fluid";
+        private static readonly MemoryCache Cache = new MemoryCache(new MemoryCacheOptions());
 
         private IServiceProvider ServiceProvider => Context.RequestServices;
 
@@ -25,45 +29,67 @@ namespace Orchard.DisplayManagement.Fluid
 
         public override async Task ExecuteAsync()
         {
+            var path = ViewContext.ExecutingFilePath.Replace(RazorViewEngine.ViewExtension, ViewExtension);
             var environment = ServiceProvider.GetRequiredService<IHostingEnvironment>();
 
-            var source = File.ReadAllText(System.IO.Path.Combine(environment.ContentRootPath,
-                ViewContext.ExecutingFilePath.Replace(RazorViewEngine.ViewExtension, ViewExtension)
-                .TrimStart('/')));
+            // Todo: use custom FileProviders through options
+            // E.g to point to Module Projects files while in dev
+            // E.g if some templates are defined through the database
+            var template = ParseFluidFile(path, environment.ContentRootFileProvider);
 
-            if (FluidViewTemplate.TryParse(source, out var template, out var errors))
+            var context = new TemplateContext();
+            context.MemberAccessStrategy.Register(Context.GetType());
+            context.MemberAccessStrategy.Register(Context.Request.GetType());
+
+            context.LocalScope.SetValue("Context", Context);
+            context.LocalScope.SetValue("ViewData", ViewData);
+            context.LocalScope.SetValue("ViewContext", ViewContext);
+
+            if (Model != null)
             {
-                var context = new TemplateContext();
-
-                context.MemberAccessStrategy.Register(Context.GetType());
-                context.MemberAccessStrategy.Register(Context.Request.GetType());
-
-                ViewBag.Title = "This is a title";
-
-                context.LocalScope.SetValue("Context", Context);
-                context.LocalScope.SetValue("ViewData", ViewData);
-                context.LocalScope.SetValue("ViewContext", ViewContext);
-
-                if (Model != null)
-                {
-                    context.LocalScope.SetValue("Model", Model);
-                    context.LocalScope.SetValue("ModelState", ViewContext.ModelState);
-                    context.MemberAccessStrategy.Register(((object)Model).GetType());
-                }
-
-                var urlHelperFactory = ServiceProvider.GetService<IUrlHelperFactory>();
-                context.AmbientValues.Add("UrlHelper", urlHelperFactory.GetUrlHelper(ViewContext));
-                context.AmbientValues.Add("FluidView", this);
-
-                var handlers = ServiceProvider.GetService<IEnumerable<ITemplateContextHandler>>();
-
-                foreach (var handler in handlers)
-                {
-                    handler.OnTemplateProcessing(context);
-                }
-
-                WriteLiteral(await template.RenderAsync(context));
+                context.LocalScope.SetValue("Model", Model);
+                context.LocalScope.SetValue("ModelState", ViewContext.ModelState);
+                context.MemberAccessStrategy.Register(((object)Model).GetType());
             }
+
+            var urlHelperFactory = ServiceProvider.GetService<IUrlHelperFactory>();
+            context.AmbientValues.Add("UrlHelper", urlHelperFactory.GetUrlHelper(ViewContext));
+            context.AmbientValues.Add("FluidView", this);
+
+            var handlers = ServiceProvider.GetService<IEnumerable<ITemplateContextHandler>>();
+
+            foreach (var handler in handlers)
+            {
+                handler.OnTemplateProcessing(context);
+            }
+
+            WriteLiteral(await template.RenderAsync(context));
+        }
+
+        internal static IFluidTemplate ParseFluidFile(string path, IFileProvider fileProvider)
+        {
+            return Cache.GetOrCreate(path, viewEntry =>
+            {
+                viewEntry.SlidingExpiration = TimeSpan.FromHours(1);
+
+                var fileInfo = fileProvider.GetFileInfo(path);
+                viewEntry.ExpirationTokens.Add(fileProvider.Watch(path));
+
+                using (var stream = fileInfo.CreateReadStream())
+                {
+                    using (var sr = new StreamReader(stream))
+                    {
+                        if (FluidViewTemplate.TryParse(sr.ReadToEnd(), out var template, out var errors))
+                        {
+                            return template;
+                        }
+                        else
+                        {
+                            throw new Exception(String.Join(System.Environment.NewLine, errors));
+                        }
+                    }
+                }
+            });
         }
     }
 }
