b50830724d7001b2bda1aa3f5c4ae4d949943383
jtkech <jean-thierry.kechichian@wanadoo.fr>
2017-07-23 02:44:45 +0200

    More checking on a candidate template path + refactoring

--- src/OrchardCore.Modules/Orchard.Templates/Services/TemplateFileProvider.cs
+++ src/OrchardCore.Modules/Orchard.Templates/Services/TemplateFileProvider.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Primitives;
+using Orchard.Admin;
 using Orchard.DisplayManagement.FileProviders;
 
 namespace Orchard.Templates.Services
@@ -32,14 +34,11 @@ namespace Orchard.Templates.Services
         public IDirectoryContents GetDirectoryContents(string subpath)
         {
             var entries = new List<IFileInfo>();
-            var name = Path.GetFileName(subpath);
 
-            if (name == "Views")
+            if (TryGetTemplateFolderName(subpath, out var folderName))
             {
-                var path = string.Format("{0}/{1}/", Path.GetFileName(Path.GetDirectoryName(subpath)), name);
-
-                entries.AddRange(Templates.Where(kv => kv.Key.StartsWith(path)).Select(kvp =>
-                    new ContentFileInfo(kvp.Key.Substring(path.Length), kvp.Value.Content)));
+                entries.AddRange(Templates.Where(kv => kv.Key.StartsWith(folderName)).Select(kvp =>
+                    new ContentFileInfo(kvp.Key.Substring(folderName.Length + 1), kvp.Value.Content)));
             }
 
             return new DirectoryContents(entries);
@@ -47,11 +46,12 @@ namespace Orchard.Templates.Services
 
         public IFileInfo GetFileInfo(string subpath)
         {
-            var template = Templates.FirstOrDefault(kv => subpath.TrimStart('/').EndsWith(kv.Key));
-
-            if (template.Key != null)
+            if (TryGetTemplateFileName(subpath, out var fileName))
             {
-                return new ContentFileInfo(Path.GetFileName(template.Key), template.Value.Content);
+                if (Templates.TryGetValue(fileName, out var template))
+                {
+                    return new ContentFileInfo(Path.GetFileName(fileName), template.Content);
+                }
             }
 
             return null;
@@ -59,14 +59,49 @@ namespace Orchard.Templates.Services
 
         public IChangeToken Watch(string filter)
         {
-            var template = Templates.FirstOrDefault(kv => filter.TrimStart('/').EndsWith(kv.Key));
-
-            if (template.Key != null)
+            if (TryGetTemplateFileName(filter, out var fileName))
             {
-                return TemplatesManager.ChangeToken;
+                if (Templates.TryGetValue(fileName, out var template))
+                {
+                    return TemplatesManager.ChangeToken;
+                }
             }
 
             return null;
+        }
+
+        private bool TryGetTemplateFolderName(string path, out string folderName)
+        {
+            var segments = path.TrimStart('/').Split('/');
+            var index = Array.IndexOf(segments, "Views");
+
+            if (index > 1 && index == segments.Count() - 1)
+            {
+                folderName = string.Format("{0}/{1}", segments[index - 1], segments[index]);
+                return true;
+            }
+
+            folderName = null;
+            return false;
+        }
+
+        private bool TryGetTemplateFileName(string path, out string fileName)
+        {
+            if (!AdminAttribute.IsApplied(_httpContextAccessor.HttpContext))
+            {
+                var segments = path.TrimStart('/').Split('/');
+                var index = Array.IndexOf(segments, "Views");
+
+                if (index > 1 && index < segments.Count() - 1)
+                {
+                    fileName = string.Format("{0}/{1}/{2}", segments[index - 1], segments[index],
+                        string.Join("/", segments, index + 1, segments.Count() - index - 1));
+                    return true;
+                }
+            }
+
+            fileName = null;
+            return false;
         }
     }
 }
