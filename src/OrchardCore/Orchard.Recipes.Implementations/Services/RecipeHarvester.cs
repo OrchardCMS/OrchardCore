296c816c8948816faeb57fcd04476e6a9b791e05
Sebastien Ros <sebastienros@gmail.com>
2017-08-17 16:13:30 -0700

    Moving setup recipes to the application project

--- src/OrchardCore/Orchard.Recipes.Implementations/Services/RecipeHarvester.cs
+++ src/OrchardCore/Orchard.Recipes.Implementations/Services/RecipeHarvester.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Modules;
 using Microsoft.Extensions.FileSystemGlobbing;
 using Microsoft.Extensions.FileSystemGlobbing.Abstractions;
-using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
@@ -21,53 +20,44 @@ namespace Orchard.Recipes.Services
         private readonly IHostingEnvironment _hostingEnvironment;
         private readonly IOptions<RecipeHarvestingOptions> _recipeOptions;
 
-        public RecipeHarvester(IExtensionManager extensionManager,
+        public RecipeHarvester(
+            IExtensionManager extensionManager,
             IHostingEnvironment hostingEnvironment,
             IOptions<RecipeHarvestingOptions> recipeOptions,
-            IStringLocalizer<RecipeHarvester> localizer,
             ILogger<RecipeHarvester> logger)
         {
             _extensionManager = extensionManager;
             _hostingEnvironment = hostingEnvironment;
             _recipeOptions = recipeOptions;
 
-            T = localizer;
             Logger = logger;
         }
 
-        public IStringLocalizer T { get; set; }
         public ILogger Logger { get; set; }
 
         public Task<IEnumerable<RecipeDescriptor>> HarvestRecipesAsync()
         {
-            return _extensionManager.GetExtensions().InvokeAsync(descriptor => {
-                return Task.FromResult(HarvestRecipes(descriptor));
-            }, Logger);
+            return _extensionManager.GetExtensions().InvokeAsync(descriptor =>HarvestRecipes(descriptor), Logger);
         }
-
-        public Task<IEnumerable<RecipeDescriptor>> HarvestRecipesAsync(string extensionId)
-        {
-            var descriptor = _extensionManager.GetExtension(extensionId);
-
-            if (descriptor.Exists)
-            {
-                return Task.FromResult(HarvestRecipes(descriptor));
-            }
-
-            Logger.LogError(T["Could not discover recipes because extension '{0}' was not found.", extensionId]);
-            return Task.FromResult(Enumerable.Empty<RecipeDescriptor>());
-        }
-
-        private IEnumerable<RecipeDescriptor> HarvestRecipes(IExtensionInfo extension)
+        
+        private Task<IEnumerable<RecipeDescriptor>> HarvestRecipes(IExtensionInfo extension)
         {
             var folderSubPath = Path.Combine(extension.SubPath, "Recipes");
-            var recipeContainerFileInfo = _hostingEnvironment
+            return HarvestRecipesAsync(folderSubPath, _recipeOptions.Value, _hostingEnvironment);
+        }
+
+        /// <summary>
+        /// Returns a list of recipes for a content path.
+        /// </summary>
+        /// <param name="path">A path string relative to the content root of the application.</param>
+        /// <returns>The list of <see cref="RecipeDescriptor"/> instances.</returns>
+        public static Task<IEnumerable<RecipeDescriptor>> HarvestRecipesAsync(string path, RecipeHarvestingOptions options, IHostingEnvironment hostingEnvironment)
+        {
+            var recipeContainerFileInfo = hostingEnvironment
                 .ContentRootFileProvider
-                .GetFileInfo(folderSubPath);
+                .GetFileInfo(path);
 
-            var recipeOptions = _recipeOptions.Value;
-
-            List<RecipeDescriptor> recipeDescriptors = new List<RecipeDescriptor>();
+            var recipeDescriptors = new List<RecipeDescriptor>();
 
             var matcher = new Matcher(System.StringComparison.OrdinalIgnoreCase);
             matcher.AddInclude("*.recipe.json");
@@ -79,9 +69,9 @@ namespace Orchard.Recipes.Services
             if (matches.Any())
             {
                 var result = matches
-                    .Select(match => _hostingEnvironment
+                    .Select(match => hostingEnvironment
                         .ContentRootFileProvider
-                        .GetFileInfo(Path.Combine(folderSubPath, match.Path))).ToArray();
+                        .GetFileInfo(Path.Combine(path, match.Path))).ToArray();
 
                 recipeDescriptors.AddRange(result.Select(recipeFile =>
                 {
@@ -90,7 +80,7 @@ namespace Orchard.Recipes.Services
                     {
                         using (var reader = new JsonTextReader(file))
                         {
-                            JsonSerializer serializer = new JsonSerializer();
+                            var serializer = new JsonSerializer();
                             var recipeDescriptor = serializer.Deserialize<RecipeDescriptor>(reader);
                             recipeDescriptor.RecipeFileInfo = recipeFile;
                             return recipeDescriptor;
@@ -99,7 +89,7 @@ namespace Orchard.Recipes.Services
                 }));
             }
 
-            return recipeDescriptors;
+            return Task.FromResult<IEnumerable<RecipeDescriptor>>(recipeDescriptors);
         }
     }
 }
