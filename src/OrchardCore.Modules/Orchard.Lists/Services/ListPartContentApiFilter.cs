cb8a17f223a4c8ffa6e7fffbb8f453b58145fc26
Nick Mayne <Jetski5822@gmail.com>
2017-08-29 14:15:27 +0100

    cleanup

---
+++ src/OrchardCore.Modules/Orchard.Lists/Services/ListPartContentApiFilter.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Orchard.ContentManagement;
+using Orchard.Contents.Services;
+using Orchard.Lists.Indexes;
+using YesSql;
+
+namespace Orchard.Lists.Services
+{
+    public class ListPartContentApiFilter : IContentApiFilter
+    {
+        public Task FilterAsync(IQuery<ContentItem> query, string parentContentItemId)
+        {
+            if (parentContentItemId != null)
+            {
+                query.With<ContainedPartIndex>(x => x.ListContentItemId == parentContentItemId);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
--- src/OrchardCore.Modules/OrchardCore.Contents/Controllers/ApiController.cs
+++ src/OrchardCore.Modules/OrchardCore.Contents/Controllers/ApiController.cs
@@ -1,5 +1,4 @@
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Modules;
