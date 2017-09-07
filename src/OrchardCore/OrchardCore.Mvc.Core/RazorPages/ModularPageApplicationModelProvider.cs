6e86bbae3be7579e05c645b0a88a72afbd056de4
jtkech <jean-thierry.kechichian@wanadoo.fr>
2017-09-07 06:08:17 +0200

    Optimize Page Filter + refactoring (add newline at end of some files).

--- src/OrchardCore/OrchardCore.Mvc.Core/RazorPages/ModularPageApplicationModelProvider.cs
+++ src/OrchardCore/OrchardCore.Mvc.Core/RazorPages/ModularPageApplicationModelProvider.cs
@@ -1,18 +1,47 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Environment.Shell;
 
 namespace OrchardCore.Mvc.RazorPages
 {
     public class ModularPageApplicationModelProvider : IPageApplicationModelProvider
     {
+        private IEnumerable<string> _paths;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ModularPageApplicationModelProvider(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
         public int Order => -1000 + 10;
 
         public void OnProvidersExecuting(PageApplicationModelProviderContext context)
         {
-            context.PageApplicationModel.Filters.Add(new ModularPageViewEnginePathFilter());
+            if (_paths != null)
+            {
+                return;
+            }
+
+            lock (this)
+            {
+                if (_paths == null)
+                {
+                    var shellFeaturesManager = _httpContextAccessor.HttpContext.RequestServices.GetService<IShellFeaturesManager>();
+                    _paths = shellFeaturesManager.GetEnabledFeaturesAsync().GetAwaiter().GetResult()
+                        .Select(f => '/' + f.Extension.SubPath + "/Pages/").Distinct();
+                }
+            }
         }
 
         public void OnProvidersExecuted(PageApplicationModelProviderContext context)
         {
+            var viewEnginePath = context.ActionDescriptor.ViewEnginePath;
+            var found = _paths.Any(p => viewEnginePath.StartsWith(p)) ? true : false;
+            context.PageApplicationModel.Filters.Add(new ModularPageViewEnginePathFilter(found));
         }
     }
 }
--- src/OrchardCore/OrchardCore.Mvc.Core/RazorPages/ModularPageViewEnginePathFilter.cs
+++ src/OrchardCore/OrchardCore.Mvc.Core/RazorPages/ModularPageViewEnginePathFilter.cs
@@ -1,38 +1,27 @@
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.Extensions.DependencyInjection;
-using OrchardCore.Environment.Shell;
 
 namespace OrchardCore.Mvc.RazorPages
 {
     public class ModularPageViewEnginePathFilter : IAsyncPageFilter
     {
+        private readonly bool _found;
+
+        public ModularPageViewEnginePathFilter(bool found)
+        {
+            _found = found;
+        }
+
         public async Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context, PageHandlerExecutionDelegate next)
         {
-            var viewEnginePath = context.ActionDescriptor.ViewEnginePath;
-
-            if (!viewEnginePath.Contains("/Pages/") || viewEnginePath.StartsWith("/Pages/"))
+            if (!_found)
             {
                 context.Result = new NotFoundResult();
                 return;
             }
 
-            var shellFeaturesManager = context.HttpContext.RequestServices.GetRequiredService<IShellFeaturesManager>();
-            var moduleIds = (await shellFeaturesManager.GetEnabledFeaturesAsync()).Select(f => f.Extension.Id).Distinct();
-
-            var moduleFolder = viewEnginePath.Substring(0, viewEnginePath.LastIndexOf("/Pages/"));
-            var moduleId = moduleFolder.Substring(moduleFolder.LastIndexOf("/") + 1);
-
-            if (!moduleIds.Contains(moduleId))
-            {
-                context.Result = new NotFoundResult();
-            }
-            else
-            {
-                await next();
-            }
+            await next();
         }
 
         public Task OnPageHandlerSelectionAsync(PageHandlerSelectedContext context)
