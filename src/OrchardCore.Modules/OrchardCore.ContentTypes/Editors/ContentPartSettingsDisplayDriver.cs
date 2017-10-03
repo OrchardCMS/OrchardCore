81f641d8152f977998335d58791d0c7cec4d630e
Sébastien Ros <sebastienros@gmail.com>
2017-10-03 09:23:44 -0700

    Fixing part display name edition (#1057)
    Fixes #1049

--- src/OrchardCore.Modules/OrchardCore.ContentTypes/Editors/ContentPartSettingsDisplayDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.ContentTypes/Editors/ContentPartSettingsDisplayDriver.cs
@@ -18,6 +18,8 @@ namespace OrchardCore.ContentTypes.Editors
                 model.Attachable = settings.Attachable;
                 model.Reusable = settings.Reusable;
                 model.Description = settings.Description;
+                model.DisplayName = settings.DisplayName;
+                model.ContentPartDefinition = contentPartDefinition;
 
                 return Task.CompletedTask;
             }).Location("Content");
@@ -32,6 +34,7 @@ namespace OrchardCore.ContentTypes.Editors
                 context.Builder.Attachable(model.Attachable);
                 context.Builder.Reusable(model.Reusable);
                 context.Builder.WithDescription(model.Description);
+                context.Builder.WithDisplayName(model.DisplayName);
             }
 
             return Edit(contentPartDefinition, context.Updater);
--- src/OrchardCore.Modules/OrchardCore.ContentTypes/ViewModels/ContentPartSettingsViewModel.cs
+++ src/OrchardCore.Modules/OrchardCore.ContentTypes/ViewModels/ContentPartSettingsViewModel.cs
@@ -1,9 +1,16 @@
-﻿namespace OrchardCore.ContentTypes.ViewModels
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using OrchardCore.ContentManagement.Metadata.Models;
+
+namespace OrchardCore.ContentTypes.ViewModels
 {
     public class ContentPartSettingsViewModel
     {
         public bool Attachable { get; set; }
         public bool Reusable { get; set; }
         public string Description { get; set; }
+        public string DisplayName { get; set; }
+
+        [BindNever]
+        public ContentPartDefinition ContentPartDefinition { get; set; } 
     }
 }
