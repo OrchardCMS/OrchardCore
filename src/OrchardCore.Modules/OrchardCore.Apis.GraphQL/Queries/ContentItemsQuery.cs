2d92eea9802f5819c4e7457db4a7724cf2e0e0b5
Nick Mayne <Jetski5822@gmail.com>
2017-10-24 16:22:30 +0100

    removing duplicaited code

--- src/OrchardCore.Modules/OrchardCore.Apis.GraphQL/Queries/ContentItemsQuery.cs
+++ src/OrchardCore.Modules/OrchardCore.Apis.GraphQL/Queries/ContentItemsQuery.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using GraphQL.Resolvers;
 using GraphQL.Types;
@@ -11,50 +12,39 @@ namespace OrchardCore.Apis.GraphQL.Queries
     public class ContentItemsQuery : QueryFieldType
     {
         public ContentItemsQuery(IContentManager contentManager,
-            ISession session) {
+            ISession session)
+        {
             Name = "ContentItems";
 
             Type = typeof(ListGraphType<ContentItemType>);
 
             Arguments = new QueryArguments(
-                    new QueryArgument<IntGraphType> { Name = "id", Description = "id of the content item" },
-                    new QueryArgument<BooleanGraphType> { Name = "published", Description = "is the content item published" },
-                    new QueryArgument<StringGraphType> { Name = "latest", Description = "is the content item the latest version" },
-                    new QueryArgument<IntGraphType> { Name = "number", Description = "version number, 1,2,3 etc" },
-                    new QueryArgument<StringGraphType> { Name = "contentType", Description = "type of content item" },
-                    new QueryArgument<StringGraphType> { Name = "contentItemId", Description = "content item id" },
-                    new QueryArgument<StringGraphType> { Name = "contentItemVersionId", Description = "the id of the version" }
-                );
+                new QueryArgument<IntGraphType> { Name = "id", Description = "id of the content item" },
+                new QueryArgument<BooleanGraphType> { Name = "published", Description = "is the content item published", DefaultValue = true },
+                new QueryArgument<StringGraphType> { Name = "latest", Description = "is the content item the latest version", DefaultValue = true },
+                new QueryArgument<IntGraphType> { Name = "number", Description = "version number, 1,2,3 etc" },
+                new QueryArgument<StringGraphType> { Name = "contentType", Description = "type of content item" },
+                new QueryArgument<StringGraphType> { Name = "contentItemId", Description = "same as id" },
+                new QueryArgument<StringGraphType> { Name = "contentItemVersionId", Description = "the id of the version" }
+            );
 
-            Resolver = new FuncFieldResolver<object, Task<object>>(async (context) => {
+            Resolver = new FuncFieldResolver<Task<IEnumerable<ContentItem>>>(async context => {
                 if (context.HasPopulatedArgument("contentItemId"))
                 {
                     return new[] { await contentManager.GetAsync(context.GetArgument<string>("contentItemId")) };
                 }
 
-                var query = session.Query<ContentItem, ContentItemIndex>();
+                var isPublished = context.GetArgument<bool>("published");
+                var isLatest = context.GetArgument<bool>("latest");
 
-                //foreach (var argument in context.Arguments.Where(qa => qa.Value != null))
-                //{
-                //    query = query.WithParameter(argument.Key, argument.Value);
-                //}
+                var query = session.Query<ContentItem, ContentItemIndex>().Where(q =>
+                    q.Published == isPublished &&
+                    q.Latest == isLatest);
 
                 if (context.HasPopulatedArgument("id"))
                 {
                     var value = context.GetArgument<int>("id");
                     query = query.Where(q => q.Id == value);
-                }
-
-                if (context.HasPopulatedArgument("published"))
-                {
-                    var value = context.GetArgument<bool>("published");
-                    query = query.Where(q => q.Published == value);
-                }
-
-                if (context.HasPopulatedArgument("latest"))
-                {
-                    var value = context.GetArgument<bool>("latest");
-                    query = query.Where(q => q.Latest == value);
                 }
 
                 if (context.HasPopulatedArgument("number"))
@@ -66,6 +56,11 @@ namespace OrchardCore.Apis.GraphQL.Queries
                 if (context.HasPopulatedArgument("contentType"))
                 {
                     var value = context.GetArgument<string>("contentType");
+                    query = query.Where(q => q.ContentType == value);
+                }
+                else
+                {
+                    var value = (context.ReturnType as ListGraphType).ResolvedType.Name;
                     query = query.Where(q => q.ContentType == value);
                 }
 
--- src/OrchardCore.Modules/OrchardCore.Apis.GraphQL/Queries/DynamicQueryFieldTypeProvider.cs
+++ src/OrchardCore.Modules/OrchardCore.Apis.GraphQL/Queries/DynamicQueryFieldTypeProvider.cs
@@ -86,71 +86,11 @@ namespace OrchardCore.Apis.GraphQL.Queries
                     }
                 }
 
-                fieldTypes.Add(new FieldType
+
+                fieldTypes.Add(new ContentItemsQuery(_contentManager, _session)
                 {
                     Name = typeDefinition.Name,
-                    ResolvedType = new ListGraphType(typeType),
-
-                    Arguments = new QueryArguments(
-                        new QueryArgument<IntGraphType> { Name = "id", Description = "id of the content item" },
-                        new QueryArgument<BooleanGraphType> { Name = "published", Description = "is the content item published", DefaultValue = true },
-                        new QueryArgument<StringGraphType> { Name = "latest", Description = "is the content item the latest version", DefaultValue = true },
-                        new QueryArgument<IntGraphType> { Name = "number", Description = "version number, 1,2,3 etc" },
-                        new QueryArgument<StringGraphType> { Name = "contentType", Description = "type of content item" },
-                        new QueryArgument<StringGraphType> { Name = "contentItemId", Description = "same as id" },
-                        new QueryArgument<StringGraphType> { Name = "contentItemVersionId", Description = "the id of the version" }
-                    ),
-
-                    Resolver = new FuncFieldResolver<Task<IEnumerable<ContentItem>>>(async context => {
-                        if (context.HasPopulatedArgument("contentItemId"))
-                        {
-                            return new[] { await _contentManager.GetAsync(context.GetArgument<string>("contentItemId")) };
-                        }
-
-                        var isPublished = context.GetArgument<bool>("published");
-                        var isLatest = context.GetArgument<bool>("latest");
-
-                        var query = _session.Query<ContentItem, ContentItemIndex>().Where(q =>
-                            q.Published == isPublished &&
-                            q.Latest == isLatest);
-
-                        if (context.HasPopulatedArgument("id"))
-                        {
-                            var value = context.GetArgument<int>("id");
-                            query = query.Where(q => q.Id == value);
-                        }
-
-                        if (context.HasPopulatedArgument("number"))
-                        {
-                            var value = context.GetArgument<int>("number");
-                            query = query.Where(q => q.Number == value);
-                        }
-
-                        if (context.HasPopulatedArgument("contentType"))
-                        {
-                            var value = context.GetArgument<string>("contentType");
-                            query = query.Where(q => q.ContentType == value);
-                        }
-                        else
-                        {
-                            var value = (context.ReturnType as ListGraphType).ResolvedType.Name;
-                            query = query.Where(q => q.ContentType == value);
-                        }
-
-                        if (context.HasPopulatedArgument("contentItemId"))
-                        {
-                            var value = context.GetArgument<string>("contentItemId");
-                            query = query.Where(q => q.ContentItemId == value);
-                        }
-
-                        if (context.HasPopulatedArgument("contentItemVersionId"))
-                        {
-                            var value = context.GetArgument<string>("contentItemVersionId");
-                            query = query.Where(q => q.ContentItemVersionId == value);
-                        }
-
-                        return await query.ListAsync();
-                    })
+                    ResolvedType = new ListGraphType(typeType)
                 });
             }
 
