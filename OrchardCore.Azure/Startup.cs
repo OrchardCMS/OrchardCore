3c67647f73d78b2e59aa9bc72ea31cf8a1d02422
Daniel Stolt <daniel@ideliverable.com>
2017-11-03 22:28:07 +0100

    Added scaffolding for OrchardCore.Azure module.

---
+++ OrchardCore.Azure/Startup.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Modules;
+
+namespace OrchardCore.Azure
+{
+    public class Startup : StartupBase
+    {
+        static Startup()
+        {
+
+        }
+
+        public override void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public override void Configure(IApplicationBuilder app, IRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+
+        }
+    }
+}
