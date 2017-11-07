61750110b534f25547deeb0fd4c2f5a86cc87e59
Sebastien Ros <sebastienros@gmail.com>
2017-11-07 10:17:15 -0800

    Using Host directly

--- src/OrchardCore/OrchardCore.Modules/Extensions/RunningShellTableExtensions.cs
+++ src/OrchardCore/OrchardCore.Modules/Extensions/RunningShellTableExtensions.cs
@@ -1,4 +1,3 @@
-using System;
 using Microsoft.AspNetCore.Http;
 using OrchardCore.Environment.Shell;
 
@@ -6,39 +5,23 @@ namespace OrchardCore.Modules
 {
     public static class RunningShellTableExtensions
     {
-        private static string XForwardedHost = "X-Forwarded-Host";
+        private const string XForwardedHost = "X-Forwarded-Host";
 
         public static ShellSettings Match(this IRunningShellTable table, HttpContext httpContext)
         {
-            // use Host header to prevent proxy alteration of the orignal request
-            try
+            var httpRequest = httpContext.Request;
+
+            if (httpRequest == null)
             {
-                var httpRequest = httpContext.Request;
-
-                if (httpRequest == null)
-                {
-                    return null;
-                }
-
-                string host;
-
-                // If the "X-Forwarded-Host" header is set, use it as the host that the client requested. 
-                if (httpRequest.Headers.TryGetValue(XForwardedHost, out var forwardedHost) && forwardedHost.Count > 0)
-                {
-                    host = forwardedHost[0];
-                }
-                else
-                {
-                    host = httpRequest.Host.ToString();
-                }
-
-                return table.Match(host ?? string.Empty, httpRequest.Path);
-            }
-            catch (Exception)
-            {
-                // can happen on cloud service for an unknown reason
                 return null;
             }
+
+            // The Host property contains the value as set from the client. It is replaced automatically
+            // to the value of X-Forwarded-Host when UseIISIntegration() is invoked.
+            // The same way .Scheme contains the protocol that the user set and not what a proxy
+            // could be using (see X-Forwarded-Proto).
+
+            return table.Match(httpRequest.Host.ToString(), httpRequest.Path);
         }
     }
 }
