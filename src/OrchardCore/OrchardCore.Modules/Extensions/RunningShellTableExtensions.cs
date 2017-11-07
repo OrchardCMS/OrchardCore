2408f8cfcfcbe87970b42f0ad86add9d23f5ae75
Sebastien Ros <sebastienros@gmail.com>
2017-11-06 16:59:45 -0800

    Support X-Forwarded-Host for tenants
    Fixes #1101

--- src/OrchardCore/OrchardCore.Modules/Extensions/RunningShellTableExtensions.cs
+++ src/OrchardCore/OrchardCore.Modules/Extensions/RunningShellTableExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.AspNetCore.Http;
 using OrchardCore.Environment.Shell;
 
@@ -6,18 +6,31 @@ namespace OrchardCore.Modules
 {
     public static class RunningShellTableExtensions
     {
+        private static string XForwardedHost = "X-Forwarded-Host";
+
         public static ShellSettings Match(this IRunningShellTable table, HttpContext httpContext)
         {
             // use Host header to prevent proxy alteration of the orignal request
             try
             {
                 var httpRequest = httpContext.Request;
+
                 if (httpRequest == null)
                 {
                     return null;
                 }
 
-                var host = httpRequest.Headers["Host"].ToString();
+                string host;
+
+                // If the "X-Forwarded-Host" header is set, use it as the host that the client requested. 
+                if (httpRequest.Headers.TryGetValue(XForwardedHost, out var forwardedHost) && forwardedHost.Count > 0)
+                {
+                    host = forwardedHost[0];
+                }
+                else
+                {
+                    host = httpRequest.Host.ToString();
+                }
 
                 return table.Match(host ?? string.Empty, httpRequest.Path);
             }
