7a50b746858bbcc35504abd05df0408a371aa2be
paynecrl97 <chris.payne@bedegaming.com>
2017-12-01 11:53:34 +0000

    Adds PublicUrls property to DisplayMediaFieldViewModel

--- src/OrchardCore.Modules/OrchardCore.Media/ViewModels/DisplayMediaFieldViewModel.cs
+++ src/OrchardCore.Modules/OrchardCore.Media/ViewModels/DisplayMediaFieldViewModel.cs
@@ -6,6 +6,14 @@ namespace OrchardCore.Media.ViewModels
 {
     public class DisplayMediaFieldViewModel
     {
+        private string[] _publicUrls;
+
+        public string[] PublicUrls
+        {
+            get { return _publicUrls ?? new string[0]; }
+            set { _publicUrls = value; }
+        }
+
         public MediaField Field { get; set; }
         public ContentPart Part { get; set; }
         public ContentPartFieldDefinition PartFieldDefinition { get; set; }
