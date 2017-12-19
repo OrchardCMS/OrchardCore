208849486f34948e2ba0272934d5309c8571a5de
jtkech <jean-thierry.kechichian@wanadoo.fr>
2017-12-19 01:27:31 +0100

    Simpler Versionable option.

--- src/OrchardCore.Modules/OrchardCore.ContentTypes/ViewModels/ContentTypeSettingsViewModel.cs
+++ src/OrchardCore.Modules/OrchardCore.ContentTypes/ViewModels/ContentTypeSettingsViewModel.cs
@@ -1,10 +1,11 @@
-﻿namespace OrchardCore.ContentTypes.ViewModels
+namespace OrchardCore.ContentTypes.ViewModels
 {
     public class ContentTypeSettingsViewModel
     {
         public bool Creatable { get; set; }
         public bool Listable { get; set; }
         public bool Draftable { get; set; }
+        public bool Versionable { get; set; }
         public bool Securable { get; set; }
         public string Stereotype { get; set; }
     }
