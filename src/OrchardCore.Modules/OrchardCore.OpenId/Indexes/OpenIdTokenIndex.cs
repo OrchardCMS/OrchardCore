7528c93f46aeda7eb7f30f2ca72f5cf61464b808
Kévin Chalet <kevinchalet@gmail.com>
2017-12-29 20:13:16 +0100

    Update the OpenID module to target the latest OpenIddict packages (#1286)

--- src/OrchardCore.Modules/OrchardCore.OpenId/Indexes/OpenIdTokenIndex.cs
+++ src/OrchardCore.Modules/OrchardCore.OpenId/Indexes/OpenIdTokenIndex.cs
@@ -1,28 +1,35 @@
-﻿using OrchardCore.OpenId.Models;
+using System;
+using OrchardCore.OpenId.Models;
 using YesSql.Indexes;
 
 namespace OrchardCore.OpenId.Indexes
 {
     public class OpenIdTokenIndex : MapIndex
     {
-        public int AppId { get; set; }
+        public string TokenId { get; set; }
+        public string ApplicationId { get; set; }
+        public string AuthorizationId { get; set; }
+        public DateTimeOffset? ExpirationDate { get; set; }
+        public string ReferenceId { get; set; }
+        public string Status { get; set; }
         public string Subject { get; set; }
-        public int TokenId { get; set; }
     }
 
     public class OpenIdTokenIndexProvider : IndexProvider<OpenIdToken>
     {
         public override void Describe(DescribeContext<OpenIdToken> context)
         {
-            context.For<OpenIdTokenIndex>().Map(token =>
-            {
-                return new OpenIdTokenIndex
+            context.For<OpenIdTokenIndex>()
+                .Map(token => new OpenIdTokenIndex
                 {
-                    AppId = token.AppId,
-                    Subject = token.Subject,
-                    TokenId = token.Id
-                };
-            });
+                    TokenId = token.TokenId,
+                    ApplicationId = token.ApplicationId,
+                    AuthorizationId = token.AuthorizationId,
+                    ExpirationDate = token.ExpirationDate,
+                    ReferenceId = token.ReferenceId,
+                    Status = token.Status,
+                    Subject = token.Subject
+                });
         }
     }
 }
--- src/OrchardCore.Modules/OrchardCore.OpenId/Migrations.cs
+++ src/OrchardCore.Modules/OrchardCore.OpenId/Migrations.cs
@@ -1,3 +1,4 @@
+using System;
 using OrchardCore.Data.Migration;
 using OrchardCore.OpenId.Indexes;
 
@@ -8,20 +9,38 @@ namespace OrchardCore.OpenId
         public int Create()
         {
             SchemaBuilder.CreateMapIndexTable(nameof(OpenIdApplicationIndex), table => table
+                .Column<string>(nameof(OpenIdApplicationIndex.ApplicationId), c => c.WithLength(48))
                 .Column<string>(nameof(OpenIdApplicationIndex.ClientId))
-                .Column<string>(nameof(OpenIdApplicationIndex.LogoutRedirectUri))
-                .Column<string>(nameof(OpenIdApplicationIndex.RedirectUri))
-            );
+                );
+
+            SchemaBuilder.CreateReduceIndexTable(nameof(OpenIdApplicationByPostLogoutRedirectUriIndex), table => table
+                .Column<string>(nameof(OpenIdApplicationByPostLogoutRedirectUriIndex.PostLogoutRedirectUri))
+                .Column<int>(nameof(OpenIdApplicationByPostLogoutRedirectUriIndex.Count)));
+
+            SchemaBuilder.CreateReduceIndexTable(nameof(OpenIdApplicationByRedirectUriIndex), table => table
+                .Column<string>(nameof(OpenIdApplicationByRedirectUriIndex.RedirectUri))
+                .Column<int>(nameof(OpenIdApplicationByPostLogoutRedirectUriIndex.Count)));
 
             SchemaBuilder.CreateReduceIndexTable(nameof(OpenIdApplicationByRoleNameIndex), table => table
                 .Column<string>(nameof(OpenIdApplicationByRoleNameIndex.RoleName))
-                .Column<int>(nameof(OpenIdApplicationByRoleNameIndex.Count))
-            );
+                .Column<int>(nameof(OpenIdApplicationByRoleNameIndex.Count)));
+
+            SchemaBuilder.CreateMapIndexTable(nameof(OpenIdAuthorizationIndex), table => table
+                .Column<string>(nameof(OpenIdAuthorizationIndex.AuthorizationId), c => c.WithLength(48))
+                .Column<string>(nameof(OpenIdAuthorizationIndex.ApplicationId), c => c.WithLength(48))
+                .Column<string>(nameof(OpenIdAuthorizationIndex.Status))
+                .Column<string>(nameof(OpenIdAuthorizationIndex.Subject))
+                .Column<string>(nameof(OpenIdAuthorizationIndex.Type))
+                );
 
             SchemaBuilder.CreateMapIndexTable(nameof(OpenIdTokenIndex), table => table
-                .Column<int>(nameof(OpenIdTokenIndex.AppId))
-                .Column<string>(nameof(OpenIdTokenIndex.Subject))
-                .Column<int>(nameof(OpenIdTokenIndex.TokenId)));
+                .Column<string>(nameof(OpenIdTokenIndex.TokenId), c => c.WithLength(48))
+                .Column<string>(nameof(OpenIdTokenIndex.ApplicationId), c => c.WithLength(48))
+                .Column<string>(nameof(OpenIdTokenIndex.AuthorizationId), c => c.WithLength(48))
+                .Column<DateTimeOffset>(nameof(OpenIdTokenIndex.ExpirationDate))
+                .Column<string>(nameof(OpenIdTokenIndex.ReferenceId))
+                .Column<string>(nameof(OpenIdTokenIndex.Status))
+                .Column<string>(nameof(OpenIdTokenIndex.Subject)));
 
             return 1;
         }
