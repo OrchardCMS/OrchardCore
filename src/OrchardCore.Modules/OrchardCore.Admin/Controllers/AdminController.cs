72e1fb35454e1c595e5329100fa8249274beb541
yiannis kyriazidis <giannkyrias@gmail.com>
2018-01-10 23:12:46 +0200

    default dashboard admin shape

--- src/OrchardCore.Modules/OrchardCore.Admin/Controllers/AdminController.cs
+++ src/OrchardCore.Modules/OrchardCore.Admin/Controllers/AdminController.cs
@@ -1,19 +1,25 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using OrchardCore.DisplayManagement;
 
 namespace OrchardCore.Admin.Controllers
 {
     [Authorize]
     public class AdminController : Controller
     {
-        public AdminController()
+          private readonly IShapeFactory _shapeFactory;
+          
+        public AdminController(IShapeFactory shapeFactory)
         {
-
+            _shapeFactory = shapeFactory;
         }
 
-        public IActionResult Index()
+         public async Task<IActionResult> Index()
         {
-            return View();
+            var adminshape = await _shapeFactory.CreateAsync("AdminContent");
+
+            return View(adminshape);
         }
     }
 }
