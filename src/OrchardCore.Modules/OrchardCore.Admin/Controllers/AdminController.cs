e6b91eb9ceb994cb4c9ebdd558383cf528c00869
Sébastien Ros <sebastienros@gmail.com>
2018-01-12 14:03:27 -0800

    Reverting changes on AdminController

--- src/OrchardCore.Modules/OrchardCore.Admin/Controllers/AdminController.cs
+++ src/OrchardCore.Modules/OrchardCore.Admin/Controllers/AdminController.cs
@@ -1,21 +1,17 @@
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using OrchardCore.DisplayManagement;
 
 namespace OrchardCore.Admin.Controllers
 {
     [Authorize]
     public class AdminController : Controller
     {
-          private readonly IShapeFactory _shapeFactory;
-          
-        public AdminController(IShapeFactory shapeFactory)
+        public AdminController()
         {
-            _shapeFactory = shapeFactory;
+
         }
 
-         public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
             return View();
         }
