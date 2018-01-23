f3f9e40a8589e0091e862f95891bdd9f5d9a7b44
jtkech <jean-thierry.kechichian@wanadoo.fr>
2018-01-23 02:21:43 +0100

    Only add locations of modules which defines the requested view component.

--- src/OrchardCore/OrchardCore.Mvc.Core/LocationExpander/ModularViewLocationExpanderProvider.cs
+++ src/OrchardCore/OrchardCore.Mvc.Core/LocationExpander/ModularViewLocationExpanderProvider.cs
@@ -1,8 +1,9 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 using Microsoft.Extensions.Caching.Memory;
 using OrchardCore.Environment.Extensions;
 using OrchardCore.Environment.Shell.Descriptor.Models;
@@ -11,19 +12,22 @@ namespace OrchardCore.Mvc.LocationExpander
 {
     public class ModularViewLocationExpanderProvider : IViewLocationExpanderProvider
     {
-        private const string CacheKey = nameof(ModularViewLocationExpanderProvider);
+        private const string CacheKey = "ModuleViewLocationsByViewComponentName)";
 
-        private readonly ShellDescriptor _shellDescriptor;
+        private readonly ApplicationPartManager _applicationPartManager;
         private readonly IExtensionManager _extensionManager;
+        private readonly ShellDescriptor _shellDescriptor;
         private readonly IMemoryCache _memoryCache;
 
         public ModularViewLocationExpanderProvider(
+            ApplicationPartManager applicationPartManager,
             ShellDescriptor shellDescriptor,
             IExtensionManager extensionManager,
             IMemoryCache memoryCache)
         {
-            _shellDescriptor = shellDescriptor;
+            _applicationPartManager = applicationPartManager;
             _extensionManager = extensionManager;
+            _shellDescriptor = shellDescriptor;
             _memoryCache = memoryCache;
         }
 
@@ -73,18 +77,54 @@ namespace OrchardCore.Mvc.LocationExpander
             }
             else
             {
-                if (!_memoryCache.TryGetValue(CacheKey, out IEnumerable<string>  moduleViewsPaths))
+                if (!_memoryCache.TryGetValue(CacheKey, out IDictionary<string,
+                    IEnumerable<string>> moduleViewLocationsByViewComponentName))
                 {
-                    moduleViewsPaths = _extensionManager.GetFeatures()
-                        .Where(f => f.Id == f.Extension.Id && _shellDescriptor.Features.Any(sf => sf.Id == f.Id))
-                        .Where(f => f.Extension.Manifest?.Type?.Equals("module", StringComparison.OrdinalIgnoreCase) ?? false)
-                        .Select(f => '/' + f.Extension.SubPath + "/Views" + "/Shared/{0}" + RazorViewEngine.ViewExtension)
-                        .Reverse();
+                    var modules = _extensionManager.GetFeatures()
+                        .Where(f => f.Id == f.Extension.Id &&
+                            _shellDescriptor.Features.Any(sf => sf.Id == f.Id))
+                        .Select(f => f.Extension);
 
-                    _memoryCache.Set(CacheKey, moduleViewsPaths);
+                    var feature = new ViewComponentFeature();
+                    _applicationPartManager.PopulateFeature(feature);
+
+                    var modulesByComponentName = new Dictionary<string, IList<IExtensionInfo>>();
+
+                    foreach (var component in feature.ViewComponents.Select(vc => vc.AsType()))
+                    {
+                        var module = modules.FirstOrDefault(e => e.Id == component.Assembly.GetName().Name);
+
+                        if (module == null)
+                        {
+                            continue;
+                        }
+
+                        if (!modulesByComponentName.ContainsKey(component.Name))
+                        {
+                            modulesByComponentName[component.Name] = new List<IExtensionInfo>();
+                        }
+
+                        modulesByComponentName[component.Name].Add(module);
+                    }
+
+                    var orderedModuleNames = modules.Select(e => e.Id).Reverse().ToList();
+
+                    moduleViewLocationsByViewComponentName = modulesByComponentName
+                        .ToDictionary(kvp => kvp.Key, kvp => kvp.Value
+                            .OrderBy(module => orderedModuleNames.IndexOf(module.Id))
+                            .Select(module => '/' + module.SubPath + "/Views" + "/Shared/{0}"
+                                + RazorViewEngine.ViewExtension));
+
+                    _memoryCache.Set(CacheKey, moduleViewLocationsByViewComponentName);
                 }
 
-                result.AddRange(moduleViewsPaths);
+                var viewComponentName = context.ViewName.Substring(0, context.ViewName.LastIndexOf('/'))
+                    .Substring("Components/".Length) + "ViewComponent";
+
+                if (moduleViewLocationsByViewComponentName.ContainsKey(viewComponentName))
+                {
+                    result.AddRange(moduleViewLocationsByViewComponentName[viewComponentName]);
+                }
             }
 
             result.AddRange(viewLocations);
