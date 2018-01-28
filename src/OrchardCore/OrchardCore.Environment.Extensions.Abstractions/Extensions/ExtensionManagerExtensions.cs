35a8063052af409b9a6772866d5fe7a788cc6b00
jtkech <jean-thierry.kechichian@wanadoo.fr>
2018-01-29 00:43:19 +0100

    Finally don't rely on obj reference for equality comparison.

--- src/OrchardCore/OrchardCore.Environment.Extensions.Abstractions/Extensions/ExtensionManagerExtensions.cs
+++ src/OrchardCore/OrchardCore.Environment.Extensions.Abstractions/Extensions/ExtensionManagerExtensions.cs
@@ -8,14 +8,14 @@ namespace OrchardCore.Environment.Extensions
         public static IEnumerable<IExtensionInfo> GetOrderedExtensions(this IExtensionManager extensionManager)
         {
             return extensionManager.GetFeatures()
-                .Where(f => f == f.Extension.Features.First())
+                .Where(f => f.Id == f.Extension.Features.First().Id)
                 .Select(f => f.Extension);
         }
 
         public static IEnumerable<IExtensionInfo> GetOrderedModules(this IExtensionManager extensionManager)
         {
             return extensionManager.GetFeatures()
-                .Where(f => f == f.Extension.Features.First())
+                .Where(f => f.Id == f.Extension.Features.First().Id)
                 .Where(f => f.Extension.Manifest.IsModule())
                 .Select(f => f.Extension);
         }
@@ -23,7 +23,7 @@ namespace OrchardCore.Environment.Extensions
         public static IEnumerable<IExtensionInfo> GetOrderedThemes(this IExtensionManager extensionManager)
         {
             return extensionManager.GetFeatures()
-                .Where(f => f == f.Extension.Features.First())
+                .Where(f => f.Id == f.Extension.Features.First().Id)
                 .Where(f => f.Extension.Manifest.IsTheme())
                 .Select(f => f.Extension);
         }
--- src/OrchardCore/OrchardCore.Environment.Shell.Abstractions/Extensions/ShellFeaturesManagerExtensions.cs
+++ src/OrchardCore/OrchardCore.Environment.Shell.Abstractions/Extensions/ShellFeaturesManagerExtensions.cs
@@ -10,14 +10,14 @@ namespace OrchardCore.Environment.Shell
         public static IEnumerable<IExtensionInfo> GetEnabledExtensions(this IShellFeaturesManager shellFeaturesManager)
         {
             return shellFeaturesManager.GetEnabledFeaturesAsync().GetAwaiter().GetResult()
-                .Where(f => f == f.Extension.Features.First())
+                .Where(f => f.Id == f.Extension.Features.First().Id)
                 .Select(f => f.Extension);
         }
 
         public static IEnumerable<IExtensionInfo> GetEnabledModules(this IShellFeaturesManager shellFeaturesManager)
         {
             return shellFeaturesManager.GetEnabledFeaturesAsync().GetAwaiter().GetResult()
-                .Where(f => f == f.Extension.Features.First())
+                .Where(f => f.Id == f.Extension.Features.First().Id)
                 .Where(f => f.Extension.Manifest.IsModule())
                 .Select(f => f.Extension);
         }
@@ -25,7 +25,7 @@ namespace OrchardCore.Environment.Shell
         public static IEnumerable<IExtensionInfo> GetEnabledThemes(this IShellFeaturesManager shellFeaturesManager)
         {
             return shellFeaturesManager.GetEnabledFeaturesAsync().GetAwaiter().GetResult()
-                .Where(f => f == f.Extension.Features.First())
+                .Where(f => f.Id == f.Extension.Features.First().Id)
                 .Where(f => f.Extension.Manifest.IsTheme())
                 .Select(f => f.Extension);
         }
@@ -33,14 +33,14 @@ namespace OrchardCore.Environment.Shell
         public static async Task<IEnumerable<IExtensionInfo>> GetEnabledExtensionsAsync(this IShellFeaturesManager shellFeaturesManager)
         {
             return (await shellFeaturesManager.GetEnabledFeaturesAsync())
-                .Where(f => f == f.Extension.Features.First())
+                .Where(f => f.Id == f.Extension.Features.First().Id)
                 .Select(f => f.Extension);
         }
 
         public static async Task<IEnumerable<IExtensionInfo>> GetEnabledModulesAsync(this IShellFeaturesManager shellFeaturesManager)
         {
             return (await shellFeaturesManager.GetEnabledFeaturesAsync())
-                .Where(f => f == f.Extension.Features.First())
+                .Where(f => f.Id == f.Extension.Features.First().Id)
                 .Where(f => f.Extension.Manifest.IsModule())
                 .Select(f => f.Extension);
         }
@@ -48,7 +48,7 @@ namespace OrchardCore.Environment.Shell
         public static async Task<IEnumerable<IExtensionInfo>> GetEnabledThemesAsync(this IShellFeaturesManager shellFeaturesManager)
         {
             return (await shellFeaturesManager.GetEnabledFeaturesAsync())
-                .Where(f => f == f.Extension.Features.First())
+                .Where(f => f.Id == f.Extension.Features.First().Id)
                 .Where(f => f.Extension.Manifest.IsTheme())
                 .Select(f => f.Extension);
         }
