34e3479d65d6adcd2d25f374c3d8a713da7670a6
Keith Raven <keith@buzzinteractive.co.uk>
2018-01-31 21:38:53 +0000

    moved resizing to separate tag helper

--- src/OrchardCore.Modules/OrchardCore.Media/TagHelpers/ImageTagHelper.cs
+++ src/OrchardCore.Modules/OrchardCore.Media/TagHelpers/ImageTagHelper.cs
@@ -5,37 +5,22 @@ using OrchardCore.Media.Processing;
 namespace OrchardCore.Media.TagHelpers
 {
     [HtmlTargetElement("img", Attributes = ImageSrcAttributeName)]
-    [HtmlTargetElement("img", Attributes = ImageSrcAttributeName + "," + ImageSizeWidthAttributeName)]
-    [HtmlTargetElement("img", Attributes = ImageSrcAttributeName + "," + ImageSizeHeightAttributeName)]
-    [HtmlTargetElement("img", Attributes = ImageSrcAttributeName + "," + ImageSizeWidthAttributeName + "," + ImageSizeModeAttributeName)]
-    [HtmlTargetElement("img", Attributes = ImageSrcAttributeName + "," + ImageSizeHeightAttributeName + "," + ImageSizeModeAttributeName)]
     public class ImageTagHelper : TagHelper
     {
         private const string ImageSizeAttributePrefix = "media-";
-
         private const string ImageSrcAttributeName = ImageSizeAttributePrefix + "src";
-        private const string ImageSizeWidthAttributeName = ImageSizeAttributePrefix + "width";
-        private const string ImageSizeHeightAttributeName = ImageSizeAttributePrefix + "height";
-        private const string ImageSizeModeAttributeName = ImageSizeAttributePrefix + "resize-mode";
 
         private readonly IMediaFileStore _mediaFileStore;
+
+        public override int Order => -10;
+
+        [HtmlAttributeName(ImageSrcAttributeName)]
+        public string MediaSrc { get; set; }
 
         public ImageTagHelper(IMediaFileStore mediaFileStore)
         {
             _mediaFileStore = mediaFileStore;
         }
-
-        [HtmlAttributeName(ImageSizeWidthAttributeName)]
-        public int? ImageWidth { get; set; }
-
-        [HtmlAttributeName(ImageSizeHeightAttributeName)]
-        public int? ImageHeight { get; set; }
-
-        [HtmlAttributeName(ImageSizeModeAttributeName)]
-        public ResizeMode ResizeMode { get; set; }
-
-        [HtmlAttributeName(ImageSrcAttributeName)]
-        public string MediaSrc { get; set; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
@@ -45,16 +30,7 @@ namespace OrchardCore.Media.TagHelpers
             }
 
             var resolvedSrc = _mediaFileStore != null ? _mediaFileStore.MapPathToPublicUrl(MediaSrc) : MediaSrc;
-
-            if (!ImageWidth.HasValue && !ImageHeight.HasValue)
-            {
-                output.Attributes.SetAttribute("src", resolvedSrc);
-                return;
-            }
-
-            var resizedSrc = ImageSharpUrlFormatter.GetMediaResizeUrl(resolvedSrc, ImageWidth, ImageHeight, ResizeMode);
-
-            output.Attributes.SetAttribute("src", resizedSrc);
+            output.Attributes.SetAttribute("src", resolvedSrc);
         }
     }
 }
