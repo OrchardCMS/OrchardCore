432c1824f18b9d25f667931cc67f83459405bfd7
Nicholas Mayne <Jetski5822@gmail.com>
2018-02-04 22:14:15 +0000

    adding detail

--- src/OrchardCore.Modules/OrchardCore.Apis.OpenApi/OpenApiMiddleware.cs
+++ src/OrchardCore.Modules/OrchardCore.Apis.OpenApi/OpenApiMiddleware.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
+using OrchardCore.Environment.Shell;
 
 namespace OrchardCore.Apis.OpenApi
 {
@@ -10,13 +13,16 @@ namespace OrchardCore.Apis.OpenApi
     {
         private readonly RequestDelegate _next;
         private readonly OpenApiSettings _settings;
+        private readonly ShellSettings _shellSettings;
 
         public OpenApiMiddleware(
             RequestDelegate next,
-            OpenApiSettings settings)
+            OpenApiSettings settings,
+            ShellSettings shellSettings)
         {
             _next = next;
             _settings = settings;
+            _shellSettings = shellSettings;
         }
 
         public Task Invoke(HttpContext context)
@@ -39,23 +45,46 @@ namespace OrchardCore.Apis.OpenApi
 
         private Task ExecuteAsync(HttpContext context)
         {
+            var descriptionProvider = context
+                .RequestServices
+                .GetService<IApiDescriptionGroupCollectionProvider>();
+
+
             var document = new OpenApiDocument();
 
             document.Info = new OpenApiInfo
             {
-
+                Title = _shellSettings.Name
             };
 
             document.Servers = new List<OpenApiServer>
             {
-
+                new OpenApiServer { Url = context.Request.Path }
             };
 
             document.Paths = new OpenApiPaths
             {
-                [""] = new OpenApiPathItem {  }
             };
 
+            foreach (var group in descriptionProvider.ApiDescriptionGroups.Items)
+            {
+                foreach (var description in group.Items)
+                {
+                    document.Paths.Add(
+                        description.RelativePath,
+                        new OpenApiPathItem
+                        {
+                            Operations = new Dictionary<OperationType, OpenApiOperation>
+                            {
+                                [(OperationType)Enum.Parse(typeof(OperationType), description.HttpMethod)] = new OpenApiOperation {
+                                    
+                                } 
+                            }
+                        }
+                    );
+                }
+            }
+            
             return Task.FromResult(document);
         }
     }
