0ecf836cbbf297e1be91034ae40f0597b43b918b
Jean-Thierry Kéchichian <jean-thierry.kechichian@wanadoo.fr>
2018-02-15 21:48:22 +0100

    Fixes a circular dependency and manifest cleanup. (#1422)

--- src/OrchardCore.Modules/OrchardCore.Features/Controllers/AdminController.cs
+++ src/OrchardCore.Modules/OrchardCore.Features/Controllers/AdminController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Localization;
 using OrchardCore.Admin;
@@ -62,8 +62,7 @@ namespace OrchardCore.Features.Controllers
                 .GetFeatures()
                 .Where(f => !f.Extension.Manifest.IsTheme()))
             {
-                var dependentFeatures = _extensionManager
-                    .GetDependentFeatures(moduleFeatureInfo.Id);
+                var featureDependencies = _extensionManager.GetFeatureDependencies(moduleFeatureInfo.Id);
 
                 var moduleFeature = new ModuleFeature
                 {
@@ -71,7 +70,8 @@ namespace OrchardCore.Features.Controllers
                     IsEnabled = enabledFeatures.Contains(moduleFeatureInfo),
                     //IsRecentlyInstalled = _moduleService.IsRecentlyInstalled(f.Extension),
                     //NeedsUpdate = featuresThatNeedUpdate.Contains(f.Id),
-                    DependentFeatures = dependentFeatures.Where(x => x.Id != moduleFeatureInfo.Id).ToList()
+                    //DependentFeatures = dependentFeatures.Where(f => f.Id != moduleFeatureInfo.Id).ToList(),
+                    FeatureDependencies = featureDependencies.Where(d => d.Id != moduleFeatureInfo.Id).ToList()
                 };
 
                 moduleFeatures.Add(moduleFeature);
