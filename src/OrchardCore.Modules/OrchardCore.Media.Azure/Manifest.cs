96a764105e9d073ffe95b902a7f9a6629ab1c53c
Jean-Thierry Kéchichian <jean-thierry.kechichian@wanadoo.fr>
2018-02-28 07:26:05 +0100

    Update Manifest.cs (#1536)

--- src/OrchardCore.Modules/OrchardCore.Media.Azure/Manifest.cs
+++ src/OrchardCore.Modules/OrchardCore.Media.Azure/Manifest.cs
@@ -8,7 +8,7 @@ using OrchardCore.Modules.Manifest;
 )]
 
 [assembly: Feature(
-    Id = "OrchardCore.Media.Azure",
+    Id = "OrchardCore.Media.Azure.Storage",
     Name = "Azure Media Storage",
     Description = "Enables support for storing media files in, and serving them to clients directly from, Microsoft Azure Blob Storage.",
     Category = "Hosting"
