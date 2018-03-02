90b437d778280a817ccfac40e9ca99de764a16b8
jtkech <jean-thierry.kechichian@wanadoo.fr>
2018-03-02 01:15:31 +0100

    Provide a default Theme attribute if not defined.

--- src/OrchardCore/OrchardCore.Modules.Abstractions/Manifest/ManifestAttributes.cs
+++ src/OrchardCore/OrchardCore.Modules.Abstractions/Manifest/ManifestAttributes.cs
@@ -81,14 +81,20 @@ namespace OrchardCore.Modules.Manifest
     }
 
     /// <summary>
-    /// Marks an assembly as a module, auto generated on building.
+    /// Marks an assembly as a module of a given type, auto generated on building.
     /// </summary>
     [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = false, Inherited = false)]
-    public class ModuleMarkerAttribute : Attribute
+    public class ModuleMarkerAttribute : ModuleAttribute
     {
-        public ModuleMarkerAttribute()
+        private string _type;
+
+        public ModuleMarkerAttribute(string name, string type)
         {
+            Name = name;
+            _type = type;
         }
+
+        public override string Type => _type;
     }
 
     /// <summary>
