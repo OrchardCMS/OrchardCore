28dc515fbaae7c13c0a940e2270525dcb036df3c
Eternity <zero.do@qq.com>
2018-03-19 21:20:44 +0800

    custom cms targets

---
+++ src/OrchardCore/EternityCore.Application.Cms.Targets/ServiceExtensions.cs
@@ -0,0 +1,27 @@
+using OrchardCore.DisplayManagement;
+using OrchardCore.Environment.Commands;
+using OrchardCore.Environment.Extensions.Manifests;
+using OrchardCore.Environment.Shell.Data;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddOrchardCms(this IServiceCollection services)
+        {
+            services.AddThemingHost();
+            services.AddManifestDefinition("theme");
+            services.AddSitesFolder();
+            services.AddCommands();
+            services.AddAuthentication();
+            services.AddModules(modules => 
+            {
+                modules.WithDefaultFeatures(
+                    "OrchardCore.Mvc", "OrchardCore.Settings", "OrchardCore.Setup",
+                    "OrchardCore.Recipes", "OrchardCore.Commons");
+            });
+
+            return services;
+        }
+    }
+}
