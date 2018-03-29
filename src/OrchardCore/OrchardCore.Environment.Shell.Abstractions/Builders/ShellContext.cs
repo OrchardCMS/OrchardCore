90dddb58431db111fa2cf97bc1213da7cef3e745
Sébastien Ros <sebastienros@gmail.com>
2018-03-28 17:54:17 -0700

    Update ShellContext.cs

--- src/OrchardCore/OrchardCore.Environment.Shell.Abstractions/Builders/ShellContext.cs
+++ src/OrchardCore/OrchardCore.Environment.Shell.Abstractions/Builders/ShellContext.cs
@@ -31,11 +31,6 @@ namespace OrchardCore.Hosting.ShellBuilders
         public bool IsActivated { get; set; }
 
         /// <summary>
-        /// Whether the shell is activating. 
-        /// </summary>
-        public bool IsActivating { get; set; }
-
-        /// <summary>
         /// Creates a standalone service scope that can be used to resolve local services and
         /// replaces <see cref="HttpContext.RequestServices"/> with it.
         /// </summary>
@@ -136,8 +131,6 @@ namespace OrchardCore.Hosting.ShellBuilders
             }
 
             IsActivated = false;
-            IsActivating = false;
-
             Settings = null;
             Blueprint = null;
 
