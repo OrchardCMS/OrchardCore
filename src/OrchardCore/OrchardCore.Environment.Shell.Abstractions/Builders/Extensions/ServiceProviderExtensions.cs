52317a5884d2f2188cbfae705271fb69f0f47cb6
jtkech <jean-thierry.kechichian@wanadoo.fr>
2018-03-30 06:53:03 +0200

    Remove child container changes moved to another PR.

--- src/OrchardCore/OrchardCore.Environment.Shell.Abstractions/Builders/Extensions/ServiceProviderExtensions.cs
+++ src/OrchardCore/OrchardCore.Environment.Shell.Abstractions/Builders/Extensions/ServiceProviderExtensions.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Linq;
+using System.Collections.Generic;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace OrchardCore.Environment.Shell.Builders
@@ -14,91 +14,46 @@ namespace OrchardCore.Environment.Shell.Builders
         public static IServiceCollection CreateChildContainer(this IServiceProvider serviceProvider, IServiceCollection serviceCollection)
         {
             IServiceCollection clonedCollection = new ServiceCollection();
-            var servicesByType = serviceCollection.GroupBy(s => s.ServiceType);
-
-            foreach (var services in servicesByType)
+            var serviceTypes = new HashSet<Type>();
+            
+            foreach (var service in serviceCollection)
             {
-                // if only one service of a given type
-                if (services.Count() == 1)
+                // Register the singleton instances to all containers
+                if (service.Lifetime == ServiceLifetime.Singleton)
                 {
-                    var service = services.First();
-
-                    // Register the singleton instances to all containers
-                    if (service.Lifetime == ServiceLifetime.Singleton)
+                    // Treat open-generic registrations differently
+                    if (service.ServiceType.IsGenericType && service.ServiceType.GenericTypeArguments.Length == 0)
                     {
-                        // Treat open-generic registrations differently
-                        if (service.ServiceType.IsGenericType && service.ServiceType.GenericTypeArguments.Length == 0)
-                        {
-                            // There is no Func based way to register an open-generic type, instead of
-                            // tenantServiceCollection.AddSingleton(typeof(IEnumerable<>), typeof(List<>));
-                            // Right now, we regsiter them as singleton per cloned scope even though it's wrong
-                            // but in the actual examples it won't matter.
-                            clonedCollection.AddSingleton(service.ServiceType, service.ImplementationType);
-                        }
-                        else
-                        {
-                            // When a service from the main container is resolved, just add its instance to the container.
-                            // It will be shared by all tenant service providers.
-                            clonedCollection.AddSingleton(service.ServiceType, serviceProvider.GetService(service.ServiceType));
-
-                            // Ideally the service should be resolved when first requested, but ASP.NET DI will call Dispose()
-                            // and this would fail reusability of the instance across tenants' containers.
-                            // clonedCollection.AddSingleton(service.ServiceType, sp => serviceProvider.GetService(service.ServiceType));
-                        }
+                        // There is no Func based way to register an open-generic type, instead of
+                        // tenantServiceCollection.AddSingleton(typeof(IEnumerable<>), typeof(List<>));
+                        // Right now, we regsiter them as singleton per cloned scope even though it's wrong
+                        // but in the actual examples it won't matter.
+                        clonedCollection.AddSingleton(service.ServiceType, service.ImplementationType);
                     }
                     else
                     {
-                        clonedCollection.Add(service);
-                    }
-                }
+                        // When a service from the main container is resolved, just add its instance to the container.
+                        // It will be shared by all tenant service providers.
+                        clonedCollection.AddSingleton(service.ServiceType, serviceProvider.GetService(service.ServiceType));
 
-                // If multiple services of the same type
-                else
-                {
-                    // If all services of the same type are not singletons.
-                    if (services.All(s => s.Lifetime != ServiceLifetime.Singleton))
-                    {
-                        // We don't need to resolve them.
-                        foreach (var service in services)
+                        if (!serviceTypes.Add(service.ServiceType))
                         {
-                            clonedCollection.Add(service);
-                        }
-                    }
+                            var serviceType = typeof(IEnumerable<>).MakeGenericType(service.ServiceType);
 
-                    // If all services of the same type are singletons.
-                    else if (services.All(s => s.Lifetime == ServiceLifetime.Singleton))
-                    {
-                        // We can resolve them from the main container.
-                        var instances = serviceProvider.GetServices(services.Key);
-
-                        foreach (var instance in instances)
-                        {
-                            clonedCollection.AddSingleton(services.Key, instance);
-                        }
-                    }
-
-                    // If singletons and scoped services are mixed.
-                    else
-                    {
-                        // We need a service scope to resolve them.
-                        using (var scope = serviceProvider.CreateScope())
-                        {
-                            var instances = scope.ServiceProvider.GetServices(services.Key);
-
-                            // Then we only keep singleton instances.
-                            for (var i = 0; i < services.Count(); i++)
+                            if (serviceTypes.Add(serviceType))
                             {
-                                if (services.ElementAt(i).Lifetime == ServiceLifetime.Singleton)
-                                {
-                                    clonedCollection.AddSingleton(services.Key, instances.ElementAt(i));
-                                }
-                                else
-                                {
-                                    clonedCollection.Add(services.ElementAt(i));
-                                }
+                                clonedCollection.AddSingleton(serviceType, serviceProvider.GetServices(service.ServiceType));
                             }
                         }
+
+                        // Ideally the service should be resolved when first requested, but ASP.NET DI will call Dispose()
+                        // and this would fail reusability of the instance across tenants' containers.
+                        // clonedCollection.AddSingleton(service.ServiceType, sp => serviceProvider.GetService(service.ServiceType));
                     }
+                }
+                else
+                {
+                    clonedCollection.Add(service);
                 }
             }
 
