91999dd7e339e99200b5076a966e26d304ef27fe
Sebastien Ros <sebastienros@gmail.com>
2018-04-03 09:46:12 -0700

    Fixing Unprotect exception

--- src/OrchardCore.Modules/OrchardCore.Email/Services/SmtpSettingsConfiguration.cs
+++ src/OrchardCore.Modules/OrchardCore.Email/Services/SmtpSettingsConfiguration.cs
@@ -1,8 +1,8 @@
 using System;
 using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using OrchardCore.Entities;
-using OrchardCore.Environment.Shell;
 using OrchardCore.Settings;
 
 namespace OrchardCore.Email.Services
@@ -11,13 +11,16 @@ namespace OrchardCore.Email.Services
     {
         private readonly ISiteService _site;
         private readonly IDataProtectionProvider _dataProtectionProvider;
+        private readonly ILogger<SmtpSettingsConfiguration> _logger;
 
         public SmtpSettingsConfiguration(
             ISiteService site,
-            IDataProtectionProvider dataProtectionProvider)
+            IDataProtectionProvider dataProtectionProvider,
+            ILogger<SmtpSettingsConfiguration> logger)
         {
             _site = site;
             _dataProtectionProvider = dataProtectionProvider;
+            _logger = logger;
         }
 
         public void Configure(SmtpSettings options)
@@ -39,8 +42,15 @@ namespace OrchardCore.Email.Services
             // Decrypt the password
             if (!String.IsNullOrWhiteSpace(settings.Password))
             {
-                var protector = _dataProtectionProvider.CreateProtector(nameof(SmtpSettingsConfiguration));
-                options.Password = protector.Unprotect(settings.Password);
+                try
+                {
+                    var protector = _dataProtectionProvider.CreateProtector(nameof(SmtpSettingsConfiguration));
+                    options.Password = protector.Unprotect(settings.Password);
+                }
+                catch
+                {
+                    _logger.LogError("The Smtp password could not be decrypted. It must have been encrypted using a different key.");
+                }
             }
         }
     }
