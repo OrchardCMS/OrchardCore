cb304159a2cbc8fed7633834cb78c84f693f1a3d
matiasmolleja@outlook.com <matiasmolleja@outlook.com>
2018-05-09 19:57:11 +0200

    Refactoring, extract folder component to its own file.

--- src/OrchardCore.Modules/OrchardCore.Media/wwwroot/Scripts/media.js
+++ src/OrchardCore.Modules/OrchardCore.Media/wwwroot/Scripts/media.js
@@ -408,6 +408,29 @@ function initializeMediaApplication(displayMediaApplication, mediaApplicationUrl
         }
     });
 }
+$(document).on('mediaApp:ready', function () {
+    $('#fileupload').fileupload({
+        dataType: 'json',
+        url: $('#uploadFiles').val(),
+        formData: function () {
+            var antiForgeryToken = $("input[name=__RequestVerificationToken]").val();
+
+            return [
+                { name: 'path', value: mediaApp.selectedFolder.path },
+                { name: '__RequestVerificationToken', value: antiForgeryToken },
+            ]
+        },
+        done: function (e, data) {
+            $.each(data.result.files, function (index, file) {
+                mediaApp.mediaItems.push(file)
+            });
+            $('#progress .progress-bar').css(
+                'width',
+                0 + '%'
+            );
+        }
+    });
+});
 
 // <folder> component
 Vue.component('folder', {
@@ -568,30 +591,6 @@ Vue.component('folder', {
             });
         }
     }
-});
-
-$(document).on('mediaApp:ready', function () {
-    $('#fileupload').fileupload({
-        dataType: 'json',
-        url: $('#uploadFiles').val(),
-        formData: function () {
-            var antiForgeryToken = $("input[name=__RequestVerificationToken]").val();
-
-            return [
-                { name: 'path', value: mediaApp.selectedFolder.path },
-                { name: '__RequestVerificationToken', value: antiForgeryToken },
-            ]
-        },
-        done: function (e, data) {
-            $.each(data.result.files, function (index, file) {
-                mediaApp.mediaItems.push(file)
-            });
-            $('#progress .progress-bar').css(
-                'width',
-                0 + '%'
-            );
-        }
-    });
 });
 
 /*
