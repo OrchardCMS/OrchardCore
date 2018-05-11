4955bb87641e425837531b71614eda2be2c26d4d
Sebastien Ros <sebastienros@gmail.com>
2018-05-11 09:55:14 -0700

    Testing failing test

--- test/Functional/test-run.cmd
+++ test/Functional/test-run.cmd
@@ -1,7 +1,1 @@
-rd /S /Q ".\publish\App_Data"
-
-start "TESTORCHARD" /D .\publish /B dotnet "OrchardCore.Cms.Web.dll
-
-CALL npm "test"
-
-taskkill /IM "dotnet.exe" /F
+npm test
--- test/Functional/test-setup.cmd
+++ test/Functional/test-setup.cmd
@@ -1,1 +1,2 @@
-npm install jest jest-cli puppeteer --save-dev
+CALL npm install jest jest-cli puppeteer --save-dev
+dotnet publish -c Release -o %cd%/publish ../../src/OrchardCore.Cms.Web/OrchardCore.Cms.Web.csproj
