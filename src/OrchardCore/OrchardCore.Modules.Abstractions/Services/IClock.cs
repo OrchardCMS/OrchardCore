dd2749c3ea995eec9ca117ea18bba0d0703533e7
Jasmin Savard <jasmin.savard@gmail.com>
2018-05-18 21:10:11 -0400

    Nodatime (#1659)
    Fixes #1577

--- src/OrchardCore/OrchardCore.Modules.Abstractions/Services/IClock.cs
+++ src/OrchardCore/OrchardCore.Modules.Abstractions/Services/IClock.cs
@@ -1,17 +1,37 @@
-﻿using System;
+using System;
 
 namespace OrchardCore.Modules
 {
-	/// <summary>
-	/// Provides the current Utc <see cref="DateTime"/>, and time related method for cache management.
-	/// This service should be used whenever the current date and time are needed, instead of <seealso cref="DateTime"/> directly.
-	/// It also makes implementations more testable, as time can be mocked.
-	/// </summary>
-	public interface IClock
+    /// <summary>
+    /// Provides the current Utc <see cref="DateTime"/>, and time related method for cache management.
+    /// This service should be used whenever the current date and time are needed, instead of <seealso cref="DateTime"/> directly.
+    /// It also makes implementations more testable, as time can be mocked.
+    /// </summary>
+    public interface IClock
     {
-		/// <summary>
-		/// Gets the current <see cref="DateTime"/> of the system, expressed in Utc
-		/// </summary>
-		DateTime UtcNow { get; }
+        /// <summary>
+        /// Gets the current <see cref="DateTime"/> of the system, expressed in Utc
+        /// </summary>
+        DateTime UtcNow { get; }
+
+        /// <summary>
+        /// Returns the list of all available <see cref="ITimeZone" />.
+        /// </summary>
+        ITimeZone[] GetTimeZones();
+
+        /// <summary>
+        /// Returns a <see cref="ITimeZone" /> from a time zone id or the local system's one if not found.
+        /// </summary>
+        ITimeZone GetTimeZone(string timeZoneId);
+
+        /// <summary>
+        /// Returns a default <see cref="ITimeZone" /> for the system.
+        /// </summary>
+        ITimeZone GetSystemTimeZone();
+
+        /// <summary>
+        /// Converts a <see cref="DateTimeOffset" /> to the specified <see cref="ITimeZone" /> instance.
+        /// </summary>
+        DateTimeOffset ConvertToTimeZone(DateTimeOffset dateTimeOffset, ITimeZone timeZone);
     }
 }
