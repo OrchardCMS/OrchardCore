24ba07cfbfe9f40992e7ad5df2d855375580e916
Jean-Thierry Kéchichian <jean-thierry.kechichian@wanadoo.fr>
2018-05-23 00:48:08 +0000

    Update MVC module order (#1830)

--- src/OrchardCore.Modules/OrchardCore.Mvc/Manifest.cs
+++ src/OrchardCore.Modules/OrchardCore.Mvc/Manifest.cs
@@ -6,6 +6,6 @@ using OrchardCore.Modules.Manifest;
     Website = "http://orchardproject.net",
     Version = "2.0.0",
     Description = "The mvc module.",
-    Priority = "-1",
+    Priority = "-10",
     Category = "Infrastructure"
 )]
