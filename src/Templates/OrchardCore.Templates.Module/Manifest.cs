8ffdd4a7a713b7219c7667f996800fbcaa33f7cb
Antoine Griffard <agriffard@hotmail.com>
2018-06-01 20:21:04 +0200

    OrchardCore dotnet new templates

---
+++ src/Templates/OrchardCore.Templates.Module/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.Templates.Modules.Manifest;
+
+[assembly: Module(
+    Name = "OrchardCore.Templates.Module",
+    Author = "The Orchard Team",
+    Website = "http://orchardproject.net",
+    Version = "0.0.1",
+    Description = "OrchardCore.Templates.Module"
+)]
