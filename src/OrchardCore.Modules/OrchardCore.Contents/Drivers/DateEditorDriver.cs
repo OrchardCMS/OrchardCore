979075ea0c9c80282e4dbf116102d9ea3738b393
Antoine Griffard <agriffard@hotmail.com>
2018-06-02 10:22:36 +0200

    Display Created date as local time in Edit page and store it as utc on update using ILocalClock

--- src/OrchardCore.Modules/OrchardCore.Contents/Drivers/DateEditorDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.Contents/Drivers/DateEditorDriver.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using OrchardCore.ContentManagement.Display.ContentDisplay;
@@ -7,16 +7,19 @@ using OrchardCore.Contents.Models;
 using OrchardCore.Contents.ViewModels;
 using OrchardCore.DisplayManagement.ModelBinding;
 using OrchardCore.DisplayManagement.Views;
+using OrchardCore.Modules;
 
 namespace OrchardCore.Contents.Drivers
 {
     public class DateEditorDriver : ContentPartDisplayDriver<CommonPart>
     {
         private readonly IContentDefinitionManager _contentDefinitionManager;
+        private readonly ILocalClock _localClock;
 
-        public DateEditorDriver(IContentDefinitionManager contentDefinitionManager)
+        public DateEditorDriver(IContentDefinitionManager contentDefinitionManager, ILocalClock localClock)
         {
             _contentDefinitionManager = contentDefinitionManager;
+            _localClock = localClock;
         }
 
         public override IDisplayResult Edit(CommonPart part)
@@ -25,9 +28,9 @@ namespace OrchardCore.Contents.Drivers
 
             if (settings.DisplayDateEditor)
             {
-                return Initialize<DateEditorViewModel>("CommonPart_Edit__Date", model =>
+                return Initialize<DateEditorViewModel>("CommonPart_Edit__Date", async model =>
                 {
-                    model.CreatedUtc = part.ContentItem.CreatedUtc;
+                    model.CreatedUtc = part.ContentItem.CreatedUtc.Value == null ? (DateTime?)null : (await _localClock.ConvertToLocalAsync(part.ContentItem.CreatedUtc.Value)).DateTime;
                 });
             }
 
@@ -43,7 +46,14 @@ namespace OrchardCore.Contents.Drivers
                 var model = new DateEditorViewModel();
                 await updater.TryUpdateModelAsync(model, Prefix);
 
-                part.ContentItem.CreatedUtc = model.CreatedUtc;
+                if (model.CreatedUtc == null)
+                {
+                    part.ContentItem.CreatedUtc = null;
+                }
+                else
+                {
+                    part.ContentItem.CreatedUtc = await _localClock.ConvertToUtcAsync(model.CreatedUtc.Value);
+                }
             }
 
             return Edit(part);
