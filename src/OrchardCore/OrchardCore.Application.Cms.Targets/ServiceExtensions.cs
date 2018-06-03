f392cb49000c663faeabfb200bb447741f35da86
jtkech <jean-thierry.kechichian@wanadoo.fr>
2018-06-04 01:57:55 +0200

    Remove testing code

--- src/OrchardCore/OrchardCore.Application.Cms.Targets/ServiceExtensions.cs
+++ src/OrchardCore/OrchardCore.Application.Cms.Targets/ServiceExtensions.cs
@@ -21,12 +21,6 @@ namespace Microsoft.Extensions.DependencyInjection
                     .AddBackgroundTasks()
                     .AddDeferredTasks()
 
-                    .Startup.ConfigureServices((tenant, sp) =>
-                    {
-                    })
-                    .Builder
-
-
                     .AddTheming()
                     .AddLiquidViews()
                     .AddResourceManagement()
