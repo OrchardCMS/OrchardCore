35fd934b33c23a9120cdd00498d65ebef7aee66b
jtkech <jean-thierry.kechichian@wanadoo.fr>
2018-06-08 00:16:22 +0200

    Update after merging dev.

--- src/OrchardCore.Modules/OrchardCore.Users/Manifest.cs
+++ src/OrchardCore.Modules/OrchardCore.Users/Manifest.cs
@@ -11,7 +11,6 @@ using OrchardCore.Modules.Manifest;
     Id = "OrchardCore.Users",
     Name = "Users",
     Description = "The users module enables authentication UI and user management.",
-    Dependencies = new[] { "OrchardCore.Authentication", "OrchardCore.DataProtection" },
     Category = "Security"
 )]
 
