9cc25c4125359083065ebee6c59b0c3eb6f6102f
Michael Petrinolis <MichaelPetrinolis@users.noreply.github.com>
2018-06-13 02:25:20 +0300

    Open id connect Client (#1622)

--- src/OrchardCore.Modules/OrchardCore.OpenId/Manifest.cs
+++ src/OrchardCore.Modules/OrchardCore.OpenId/Manifest.cs
@@ -9,6 +9,19 @@ using OrchardCore.OpenId;
 )]
 
 [assembly: Feature(
+    Id = OpenIdConstants.Features.Client,
+    Name = "OpenID Client",
+    Category = "OpenID Connect",
+    Description = "Auhenticates users from an external OpenID Connect identity provider.",
+    Dependencies = new[]
+    {
+        OpenIdConstants.Features.Core,
+        "OrchardCore.Authentication",
+        "OrchardCore.DataProtection"
+    }
+)]
+
+[assembly: Feature(
     Id = OpenIdConstants.Features.Core,
     Name = "OpenID Core Components",
     Category = "OpenID Connect",
