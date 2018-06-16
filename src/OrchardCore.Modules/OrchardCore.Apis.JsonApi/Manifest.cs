1f5115e6d51ea6b0ef3a17dc718d257058acb431
Nick Mayne <Jetski5822@gmail.com>
2018-06-16 09:50:34 +0100

    Update libs and manifests

---
+++ src/OrchardCore.Modules/OrchardCore.Apis.JsonApi/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "JsonApi",
+    Author = "The Orchard Team",
+    Website = "http://orchardproject.net",
+    Version = "2.0.0",
+    Description = "Apis using the JsonApi Specification.",
+    Category = "Api"
+)]
---
+++ src/OrchardCore.Modules/OrchardCore.Apis.OpenApi/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "OpenApi",
+    Author = "The Orchard Team",
+    Website = "http://orchardproject.net",
+    Version = "2.0.0",
+    Description = "Apis using the OpenApi Specification.",
+    Category = "Api"
+)]
