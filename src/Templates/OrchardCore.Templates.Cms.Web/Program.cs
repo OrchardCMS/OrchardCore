c15f4c10abe2f3e1a576fd25cc0f854f733a909f
Antoine Griffard <agriffard@hotmail.com>
2018-06-19 20:18:41 +0200

    OrchardCore dotnet new templates (#1877)

---
+++ src/Templates/OrchardCore.Templates.Cms.Web/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+#if (AddNLog)
+using OrchardCore.Logging;
+#endif
+using OrchardCore.Modules;
+
+namespace OrchardCore.Templates.Cms.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+#if (AddNLog)
+                .UseNLogWeb()
+#endif
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
---
+++ src/Templates/OrchardCore.Templates.Module/Handlers/MyTestPartHandler.cs
@@ -0,0 +1,16 @@
+﻿using OrchardCore.ContentManagement.Handlers;
+using System.Threading.Tasks;
+using OrchardCore.Templates.Module.Models;
+
+namespace OrchardCore.Templates.Module.Handlers
+{
+    public class MyTestPartHandler : ContentPartHandler<MyTestPart>
+    {
+        public override Task InitializingAsync(InitializingContentContext context, MyTestPart part)
+        {
+            part.Show = true;
+
+            return Task.CompletedTask;
+        }
+    }
+}
