6a021641bd76dceb100eba9e804a79b994b81809
Matias Molleja <matiasmolleja@outlook.com>
2018-06-21 00:29:00 +0200

    Fixes #2023 media added twice on several markdown editors (#2030)

--- src/OrchardCore.Modules/OrchardCore.Markdown/wwwroot/Scripts/simplemde.media.js
+++ src/OrchardCore.Modules/OrchardCore.Markdown/wwwroot/Scripts/simplemde.media.js
@@ -8,21 +8,22 @@
 
     $(document).on('simplemde:initialize', function (event, simplemde) {
         // Replace the image button action.
-        simplemde.toolbar[9].action = function (editor) {
+        var editor;
+        simplemde.toolbar[9].action = function (e) {
+            editor = e;
             $("#mediaApp").detach().appendTo('#mediaModalMarkdown .modal-body');
             $("#mediaApp").show();
             mediaApp.selectedMedias = [];
             var modal = $('#mediaModalMarkdown').modal();
-            $('#mediaMarkdownSelectButton').on('click', function (v) {
-                var mediaMarkdownContent = "";
-                for (i = 0; i < mediaApp.selectedMedias.length; i++) {
-                    mediaMarkdownContent += ' {{ "' + mediaApp.selectedMedias[i].mediaPath + '" | asset_url | img_tag }}';
-                }
-
-                var cm = editor.codemirror;
-                cm.replaceSelection(mediaMarkdownContent)
-                $('#mediaModalMarkdown').modal('hide');
-            });
         };
+        $('#mediaMarkdownSelectButton').on('click', function (v) {
+            var mediaMarkdownContent = "";
+            for (i = 0; i < mediaApp.selectedMedias.length; i++) {
+                mediaMarkdownContent += ' {{ "' + mediaApp.selectedMedias[i].mediaPath + '" | asset_url | img_tag }}';
+            }
+            var cm = editor.codemirror;
+            cm.replaceSelection(mediaMarkdownContent)
+            $('#mediaModalMarkdown').modal('hide');
+        });
     });
 })(jQuery);
