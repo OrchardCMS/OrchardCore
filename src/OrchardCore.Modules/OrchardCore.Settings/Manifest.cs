1024b6175d39a80cd5f531d16ccb97140c1951bc
Jasmin Savard <jasmin.savard@gmail.com>
2018-07-04 18:14:32 -0400

    Add dependency on OrchardCore.Localization for OrchardCore.Settings module

--- src/OrchardCore.Modules/OrchardCore.Settings/Manifest.cs
+++ src/OrchardCore.Modules/OrchardCore.Settings/Manifest.cs
@@ -6,5 +6,6 @@ using OrchardCore.Modules.Manifest;
     Website = "http://orchardproject.net",
     Version = "2.0.0",
     Description = "The settings module creates site settings that other modules can contribute to.",
-    Category = "Configuration"
+    Category = "Configuration",
+    Dependencies = new[] { "OrchardCore.Localization" }
 )]
