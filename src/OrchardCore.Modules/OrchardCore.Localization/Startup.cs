0470e6cb528d9f0740cc81e0033e74e08095c73c
jtkech <jean-thierry.kechichian@wanadoo.fr>
2018-07-06 18:10:52 +0200

    Add RouteDataRequestCultureProvider.

--- src/OrchardCore.Modules/OrchardCore.Localization/Startup.cs
+++ src/OrchardCore.Modules/OrchardCore.Localization/Startup.cs
@@ -1,11 +1,12 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Globalization;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Localization.Routing;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Options;
 using OrchardCore.Localization.Services;
 using OrchardCore.Modules;
 
@@ -24,9 +25,13 @@ namespace OrchardCore.Localization
 
             // Override the default localization file locations with Orchard specific ones
             services.Replace(ServiceDescriptor.Singleton<ILocalizationFileLocationProvider, ModularPoFileLocationProvider>());
+        }
 
-            // Configure supported cultures and localization options
-            services.Configure<RequestLocalizationOptions>(options =>
+        public override void Configure(IApplicationBuilder app, IRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+            var options = new RequestLocalizationOptions();
+
+            options.RequestCultureProviders = new List<IRequestCultureProvider>
             {
                 // You can change which providers are configured to determine the culture for requests, or even add a custom
                 // provider with your own logic. The providers will be asked in order to provide a culture for each request,
@@ -34,22 +39,24 @@ namespace OrchardCore.Localization
                 // By default, the following built-in providers are configured:
                 // - QueryStringRequestCultureProvider, sets culture via "culture" and "ui-culture" query string values, useful for testing
                 // - CookieRequestCultureProvider, sets culture via "ASPNET_CULTURE" cookie
+                // - RouteDataRequestCultureProvider, determines the culture information for a request via values in the route data.
                 // - AcceptLanguageHeaderRequestCultureProvider, sets culture via the "Accept-Language" request header
-                //options.RequestCultureProviders.Clear();
-                options.RequestCultureProviders.Insert(0, new DefaultRequestCultureProvider());
-            });
-        }
 
-        public override void Configure(IApplicationBuilder app, IRouteBuilder routes, IServiceProvider serviceProvider)
-        {
+                new DefaultRequestCultureProvider { Options = options },
+                new QueryStringRequestCultureProvider { Options = options },
+                new CookieRequestCultureProvider { Options = options },
+                new RouteDataRequestCultureProvider { Options = options },
+                new AcceptLanguageHeaderRequestCultureProvider { Options = options }
+            };
+
             var cultureManager = serviceProvider.GetService<ICultureManager>();
-            var options = serviceProvider.GetService<IOptions<RequestLocalizationOptions>>().Value;
 
             if (cultureManager != null)
             {
                 var siteCultures = cultureManager.ListCultures().Select(c => c.Culture).ToArray();
 
-                options.SetDefaultCulture(cultureManager.GetSiteCulture())
+                options
+                    .SetDefaultCulture(cultureManager.GetSiteCulture())
                     .AddSupportedCultures(siteCultures)
                     .AddSupportedUICultures(siteCultures);
             }
