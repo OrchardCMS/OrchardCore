26ea3ab0be9b489341515ffcc33cef71973eaeb1
GLOBALREFUND\sayubi <sayubi@globalblue.com>
2018-07-12 14:08:40 +0200

    Convert "Is Youtube" checkbox to a combobox with different streaming types

--- src/OrchardCore.Modules/OrchardCore.ContentFields/Settings/VideoFieldSettings.cs
+++ src/OrchardCore.Modules/OrchardCore.ContentFields/Settings/VideoFieldSettings.cs
@@ -5,7 +5,7 @@ namespace OrchardCore.ContentFields.Settings
         public string Hint { get; set; }
         public string Label { get; set; }
         public string Editor { get; set; }
-        public bool IsYoutubeVideo { get; set; }
+        public VideoStreamingType StreamingType { get; set; }
         public int Width { get; set; }
         public int Height { get; set; }
     }
---
+++ src/OrchardCore.Modules/OrchardCore.ContentFields/Settings/VideoStreamingType.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrchardCore.ContentFields.Settings
+{
+    public enum VideoStreamingType
+    {
+        SelfHosted = 1,
+        Youtube = 2
+    }
+}
