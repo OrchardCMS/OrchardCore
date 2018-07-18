526b944ba16cae3c728bd1a659ba3604efb0cf1d
jtkech <jean-thierry.kechichian@wanadoo.fr>
2018-07-18 03:49:59 +0200

    Tests.

--- test/OrchardCore.Tests/Apis/JsonApi/BlogTests.cs
+++ test/OrchardCore.Tests/Apis/JsonApi/BlogTests.cs
@@ -14,7 +14,7 @@ namespace OrchardCore.Tests.Apis.JsonApi
         {
         }
 
-        [Fact(Skip = "For testing")]
+        [Fact]
         public async Task ShouldCreateABlog() {
 
             var initialize = false;
