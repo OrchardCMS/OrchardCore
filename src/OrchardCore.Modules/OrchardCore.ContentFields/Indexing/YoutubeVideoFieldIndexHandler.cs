20edb0b1a72f4e26eb4d8847d3dca82030a06f8b
Siyamand Ayubi <sayubi@globalblue.com>
2018-07-27 16:00:41 +0200

    Imrpove YoutubeVideoField editor by being able to paste Youtube urls easily

--- src/OrchardCore.Modules/OrchardCore.ContentFields/Indexing/YoutubeVideoFieldIndexHandler.cs
+++ src/OrchardCore.Modules/OrchardCore.ContentFields/Indexing/YoutubeVideoFieldIndexHandler.cs
@@ -10,7 +10,7 @@ namespace OrchardCore.ContentFields.Indexing
         public override Task BuildIndexAsync(YoutubeVideoField field, BuildFieldIndexContext context)
         {
             var options = context.Settings.ToOptions();
-            context.DocumentIndex.Entries.Add(context.Key, new DocumentIndex.DocumentIndexEntry(field.Address, DocumentIndex.Types.Text, options));
+            context.DocumentIndex.Entries.Add(context.Key, new DocumentIndex.DocumentIndexEntry(field.EmbededAddress, DocumentIndex.Types.Text, options));
 
             return Task.CompletedTask;
         }
