a72213da67b760a163d8aa1060ecfc61e688b1ef
Chris Payne <2872984+paynecrl97@users.noreply.github.com>
2018-07-31 19:33:04 +0100

    Some small recipe UI changes (#2155)

--- src/OrchardCore.Modules/OrchardCore.Recipes/AdminMenu.cs
+++ src/OrchardCore.Modules/OrchardCore.Recipes/AdminMenu.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 using Microsoft.Extensions.Localization;
 using OrchardCore.Environment.Navigation;
 using OrchardCore.Security;
