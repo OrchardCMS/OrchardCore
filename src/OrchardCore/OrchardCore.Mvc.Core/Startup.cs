c38c8263bbb52dd57d6be41990d89ed53da520bc
Sebastien Ros <sebastienros@gmail.com>
2018-08-24 16:12:01 -0700

    Reusing standard AddMvc call

--- src/OrchardCore/OrchardCore.Mvc.Core/Startup.cs
+++ src/OrchardCore/OrchardCore.Mvc.Core/Startup.cs
@@ -1,14 +1,10 @@
 using System;
-using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.AspNetCore.Mvc.Razor.Compilation;
-using Microsoft.AspNetCore.Mvc.Razor.TagHelpers;
-using Microsoft.AspNetCore.Mvc.TagHelpers;
 using Microsoft.AspNetCore.Mvc.ViewFeatures.Internal;
-using Microsoft.CodeAnalysis;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
@@ -32,37 +28,32 @@ namespace OrchardCore.Mvc
 
         public override void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton(new ApplicationPartManager());
-
-            var builder = services.AddMvcCore(options =>
+            var builder = services.AddMvc(options =>
             {
-                // Do we need this?
+                // AntiForgery Token Valide should be on by default
                 options.Filters.Add(typeof(AutoValidateAntiforgeryTokenAuthorizationFilter));
+
+                // Custom model binder to testing purpose
                 options.ModelBinderProviders.Insert(0, new CheckMarkModelBinderProvider());
             });
 
-            builder.AddAuthorization();
-            // builder.AddViews();
-            // builder.AddRazorViewEngine();
-            // the above is called here
+            builder.SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Latest);
+
+            services.AddMvcCore().AddModularRazorPages();
+
+            AddModularFrameworkParts(_serviceProvider, builder.PartManager);
+            
+            // Adding localization
             builder.AddViewLocalization();
             builder.AddDataAnnotationsLocalization();
 
-            AddModularFrameworkParts(_serviceProvider, builder.PartManager);
-
             builder.Services.TryAddEnumerable(
                 ServiceDescriptor.Transient<IConfigureOptions<RazorViewEngineOptions>, ModularRazorViewEngineOptionsSetup>());
-
-            builder.AddModularRazorPages();
-
+            
             // Use a custom IViewCompilerProvider so that all tenants reuse the same ICompilerCache instance
             builder.Services.Replace(new ServiceDescriptor(typeof(IViewCompilerProvider), typeof(SharedViewCompilerProvider), ServiceLifetime.Singleton));
 
             AddMvcModuleCoreServices(services);
-            AddDefaultFrameworkParts(builder.PartManager);
-
-            // Order important
-            builder.AddJsonFormatters();
         }
 
         private void AddModularFrameworkParts(IServiceProvider services, ApplicationPartManager manager)
@@ -71,21 +62,6 @@ namespace OrchardCore.Mvc
             manager.ApplicationParts.Add(new ShellFeatureApplicationPart(httpContextAccessor));
         }
         
-        private static void AddDefaultFrameworkParts(ApplicationPartManager partManager)
-        {
-            var mvcTagHelpersAssembly = typeof(InputTagHelper).Assembly;
-            if (!partManager.ApplicationParts.OfType<AssemblyPart>().Any(p => p.Assembly == mvcTagHelpersAssembly))
-            {
-                partManager.ApplicationParts.Add(new AssemblyPart(mvcTagHelpersAssembly));
-            }
-
-            var mvcRazorAssembly = typeof(UrlResolutionTagHelper).Assembly;
-            if (!partManager.ApplicationParts.OfType<AssemblyPart>().Any(p => p.Assembly == mvcRazorAssembly))
-            {
-                partManager.ApplicationParts.Add(new AssemblyPart(mvcRazorAssembly));
-            }
-        }
-
         internal static void AddMvcModuleCoreServices(IServiceCollection services)
         {
             services.Replace(
