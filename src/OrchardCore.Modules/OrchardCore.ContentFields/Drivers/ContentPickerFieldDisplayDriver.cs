a1e5129dd1cbd63185f9bdfac1142c11df00ef16
James Hawkins <jhawkins@bethany.org>
2018-08-27 17:01:17 -0400

    Add content items to view model

--- src/OrchardCore.Modules/OrchardCore.ContentFields/Drivers/ContentPickerFieldDisplayDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.ContentFields/Drivers/ContentPickerFieldDisplayDriver.cs
@@ -1,22 +1,38 @@
 using System;
 using System.Threading.Tasks;
 using OrchardCore.ContentFields.ViewModels;
+using OrchardCore.ContentManagement;
 using OrchardCore.ContentManagement.Display.ContentDisplay;
 using OrchardCore.ContentManagement.Display.Models;
+using OrchardCore.ContentManagement.Records;
 using OrchardCore.DisplayManagement.ModelBinding;
 using OrchardCore.DisplayManagement.Views;
+using YesSql;
+using YesSql.Services;
 
 namespace OrchardCore.ContentFields.Fields
 {
     public class ContentPickerFieldDisplayDriver : ContentFieldDisplayDriver<ContentPickerField>
     {
+        private readonly ISession _session;
+
+        public ContentPickerFieldDisplayDriver(ISession session)
+        {
+            _session = session;
+        }
+
         public override IDisplayResult Display(ContentPickerField field, BuildFieldDisplayContext context)
         {
-            return Initialize<DisplayContentPickerFieldViewModel>("ContentPickerField", model =>
+            return Initialize<DisplayContentPickerFieldViewModel>("ContentPickerField", async model =>
             {
                 model.Field = field;
                 model.Part = context.ContentPart;
                 model.PartFieldDefinition = context.PartFieldDefinition;
+                model.Updater = context.Updater;
+                model.SelectedContentItemIds = string.Join(",", field.ContentItemIds);
+                model.ContentItems = await _session.Query<ContentItem, ContentItemIndex>()
+                    .With<ContentItemIndex>(x => x.ContentItemId.IsIn(field.ContentItemIds))
+                    .ListAsync();
             })
             .Location("Content")
             .Location("SummaryAdmin", "");
