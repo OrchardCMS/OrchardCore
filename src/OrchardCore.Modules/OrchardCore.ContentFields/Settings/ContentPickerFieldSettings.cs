63846c83dce2229a208cb8157e48f817f0460029
James Hawkins <jhawkins@bethany.org>
2018-08-28 11:06:39 -0400

    Add setting for displayed content types

--- src/OrchardCore.Modules/OrchardCore.ContentFields/Settings/ContentPickerFieldSettings.cs
+++ src/OrchardCore.Modules/OrchardCore.ContentFields/Settings/ContentPickerFieldSettings.cs
@@ -5,5 +5,6 @@ namespace OrchardCore.ContentFields.Settings
         public string Hint { get; set; }
         public bool Required { get; set; }
         public bool Multiple { get; set; }
+        public string[] DisplayedContentTypes { get; set; } = new string[0];
     }
 }
