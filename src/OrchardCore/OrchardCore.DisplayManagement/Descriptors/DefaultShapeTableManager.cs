857ea12104caef8c1ad8e46977abddcf9b2bba0f
jtkech <jean-thierry.kechichian@wanadoo.fr>
2018-08-29 23:26:55 +0200

    Don't use IHostingEnvironment because of unit tests.

--- src/OrchardCore/OrchardCore.DisplayManagement/Descriptors/DefaultShapeTableManager.cs
+++ src/OrchardCore/OrchardCore.DisplayManagement/Descriptors/DefaultShapeTableManager.cs
@@ -2,8 +2,8 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using Microsoft.Extensions.Caching.Memory;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using OrchardCore.DisplayManagement.Extensions;
 using OrchardCore.Environment.Extensions;
@@ -20,7 +20,7 @@ namespace OrchardCore.DisplayManagement.Descriptors
     {
         private static ConcurrentDictionary<string, FeatureShapeDescriptor> _shapeDescriptors = new ConcurrentDictionary<string, FeatureShapeDescriptor>();
 
-        private readonly IHostingEnvironment _hostingEnvironment;
+        private readonly string _applicationName;
         private readonly IEnumerable<IShapeTableProvider> _bindingStrategies;
         private readonly IShellFeaturesManager _shellFeaturesManager;
         private readonly IExtensionManager _extensionManager;
@@ -30,7 +30,6 @@ namespace OrchardCore.DisplayManagement.Descriptors
         private readonly IMemoryCache _memoryCache;
 
         public DefaultShapeTableManager(
-            IHostingEnvironment hostingEnvironment,
             IEnumerable<IShapeTableProvider> bindingStrategies,
             IShellFeaturesManager shellFeaturesManager,
             IExtensionManager extensionManager,
@@ -38,7 +37,7 @@ namespace OrchardCore.DisplayManagement.Descriptors
             ILogger<DefaultShapeTableManager> logger,
             IMemoryCache memoryCache)
         {
-            _hostingEnvironment = hostingEnvironment;
+            _applicationName = Assembly.GetEntryAssembly().GetName().Name;
             _bindingStrategies = bindingStrategies;
             _shellFeaturesManager = shellFeaturesManager;
             _extensionManager = extensionManager;
@@ -84,9 +83,9 @@ namespace OrchardCore.DisplayManagement.Descriptors
                     .ToList();
 
                 // let the application acting as a super theme for shapes rendering.
-                if (enabledAndOrderedFeatureIds.Remove(_hostingEnvironment.ApplicationName))
+                if (enabledAndOrderedFeatureIds.Remove(_applicationName))
                 {
-                    enabledAndOrderedFeatureIds.Add(_hostingEnvironment.ApplicationName);
+                    enabledAndOrderedFeatureIds.Add(_applicationName);
                 }
 
                 var descriptors = _shapeDescriptors
