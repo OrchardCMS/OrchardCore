757e297fa5b8af481b09bddaaa53bf7aa6b29250
Michael Petrinolis <michael.petrinolis@outlook.com>
2018-09-02 23:59:37 +0300

    Initial Commit

---
+++ src/OrchardCore.Themes/TheComingSoonTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "The Coming Soon Theme",
+    Author = "The Orchard Team",
+    Website = "http://orchardproject.net",
+    Version = "2.0.0",
+    Description = "A theme adapted for coming soon websites."
+)]
---
+++ src/OrchardCore.Themes/TheComingSoonTheme/wwwroot/js/coming-soon.min.js
@@ -0,0 +1,1 @@
+!function(t){"use strict"}(jQuery);
