c4468cf0b606b0f564b7fbb76de1135cfe4a135b
Michael Petrinolis <michael.petrinolis@outlook.com>
2018-09-15 23:08:19 +0300

    Added RegisterUserTask in Users Added Register Widget in Theme Updated the recipe

---
+++ src/OrchardCore.Modules/OrchardCore.Users/Workflows/Activities/RegisterUserTask.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.Extensions.Localization;
+using OrchardCore.DisplayManagement.ModelBinding;
+using OrchardCore.Users.Models;
+using OrchardCore.Users.Services;
+using OrchardCore.Workflows.Abstractions.Models;
+using OrchardCore.Workflows.Activities;
+using OrchardCore.Workflows.Models;
+
+namespace OrchardCore.Users.Workflows.Activities
+{
+    public class RegisterUserTask : TaskActivity
+    {
+        private readonly IUserService _userService;
+        private readonly IStringLocalizer T;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IUpdateModelAccessor _updateModelAccessor;
+
+        public RegisterUserTask(IUserService userService, IHttpContextAccessor httpContextAccessor, IUpdateModelAccessor updateModelAccessor, IStringLocalizer<RegisterUserTask> t)
+        {
+            _userService = userService;
+            _httpContextAccessor = httpContextAccessor;
+            _updateModelAccessor = updateModelAccessor;
+            T = t;
+        }
+
+        // The technical name of the activity. Activities on a workflow definition reference this name.
+        public override string Name => nameof(RegisterUserTask);
+
+        // The category to which this activity belongs. The activity picker groups activities by this category.
+        public override LocalizedString Category => T["Content"];
+
+        // Returns the possible outcomes of this activity.
+        public override IEnumerable<Outcome> GetPossibleOutcomes(WorkflowExecutionContext workflowContext, ActivityContext activityContext)
+        {
+            return Outcomes(T["Done"], T["Valid"], T["Invalid"]);
+        }
+
+        // This is the heart of the activity and actually performs the work to be done.
+        public override async Task<ActivityExecutionResult> ExecuteAsync(WorkflowExecutionContext workflowContext, ActivityContext activityContext)
+        {
+
+            var form = _httpContextAccessor.HttpContext.Request.Form;
+            var email = form["Email"];
+            var isValid = !string.IsNullOrWhiteSpace(email);
+            var outcome = isValid ? "Valid" : "Invalid";
+
+
+            if (isValid)
+            {
+                var userName = form["UserName"];
+                if (string.IsNullOrWhiteSpace(userName))
+                    userName = email;
+
+                IUser user = new User() { UserName = userName, Email = email };
+                Dictionary<string, string> errors = new Dictionary<string, string>();
+                user = await _userService.CreateUserAsync(user, null, (key, message) => errors.Add(key, message));
+                if (errors.Count > 0)
+                {
+                    var updater = _updateModelAccessor.ModelUpdater;
+                    if (updater != null)
+                    {
+                        foreach (var item in errors)
+                        {
+                            updater.ModelState.TryAddModelError(item.Key, T[item.Value]);
+                        }
+                    }
+                    outcome = "Invalid";
+                }
+            }
+
+            return Outcomes("Done", outcome);
+        }
+    }
+}
---
+++ src/OrchardCore.Modules/OrchardCore.Users/Workflows/Drivers/RegisterUserTaskDisplay.cs
@@ -0,0 +1,9 @@
+using OrchardCore.Users.Workflows.Activities;
+using OrchardCore.Workflows.Display;
+
+namespace OrchardCore.Users.Workflows.Drivers
+{
+    public class RegisterUserTaskDisplay : ActivityDisplayDriver<RegisterUserTask>
+    {
+    }
+}
