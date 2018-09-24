ac9c1ecbb0c3aa772071702b2e2a9592f0be0c60
Sebastien Ros <sebastienros@gmail.com>
2018-09-24 15:26:21 -0700

    Missing using statement

--- src/OrchardCore/OrchardCore.Navigation.Core/NavigationManager.cs
+++ src/OrchardCore/OrchardCore.Navigation.Core/NavigationManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Routing;
