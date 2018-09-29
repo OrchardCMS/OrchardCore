3467db6a20e7138c8e506a1eb529f811fbfcfe2d
Jasmin Savard <jasmin.savard@gmail.com>
2018-09-29 03:33:30 -0400

    adding Predefined Fields options editor
    TODO: fix gulpfile and uglify which fails compiling

---
+++ src/OrchardCore.Modules/OrchardCore.ContentFields/Assets/js/OptionsEditor/optionsEditor.js
@@ -0,0 +1,56 @@
+function initializeOptionsEditor(el, data) {
+
+    var optionsEditor = $(el);
+    var previouslyChecked;
+
+    var optionsTable = {
+        template: '#options-table',
+        props: ['value'],
+        name: 'options-table',
+        computed: {
+            list: {
+                get() {
+                    return this.value;
+                },
+                set(value) {
+                    this.$emit('input', value);
+                }
+            }
+        },
+        methods: {
+            add: function () {
+                this.list.push({ name: '', value: ''});
+            },
+            remove: function (index) {
+                this.list.splice(index, 1);
+            },
+            uncheck: function (index) {
+                if (index == previouslyChecked) {
+                    $('#customRadio_' + index)[0].checked = false;
+                    previouslyChecked = null;
+                }
+                else {
+                    previouslyChecked = index;
+                }
+            }
+        }
+    };
+
+    new Vue({
+        components: {
+            optionsTable: optionsTable
+        },
+        el: optionsEditor.get(0),
+        data: {
+            option: data,
+            dragging: false
+        },
+        methods: {
+            getData: function () {
+                return JSON.stringify(data);
+            }
+        }
+
+    });
+
+}
