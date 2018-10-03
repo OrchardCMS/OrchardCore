4e6e2bcc0c2ba344e88fb91d3472a7728fca8294
Peter Davis <peter.w.davis@gmail.com>
2018-10-03 22:44:47 +0930

    Add Required setting option for coordinate field

--- src/OrchardCore.Modules/OrchardCore.Spatial/Settings/CoordinateFieldSettings.cs
+++ src/OrchardCore.Modules/OrchardCore.Spatial/Settings/CoordinateFieldSettings.cs
@@ -3,5 +3,6 @@ namespace OrchardCore.Spatial.Settings
     public class CoordinateFieldSettings
     {
         public string Hint { get; set; }
+        public bool Required { get; set; }
     }
 }
