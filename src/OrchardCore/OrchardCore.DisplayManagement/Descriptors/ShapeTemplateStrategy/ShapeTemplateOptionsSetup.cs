e2072e174a6b5a5f7c6eb580a2e980662272aeeb
Jean-Thierry Kéchichian <jean-thierry.kechichian@wanadoo.fr>
2018-10-18 15:19:14 +0000

    Fixes Application'module shapes discovering. (#2524)

--- src/OrchardCore/OrchardCore.DisplayManagement/Descriptors/ShapeTemplateStrategy/ShapeTemplateOptionsSetup.cs
+++ src/OrchardCore/OrchardCore.DisplayManagement/Descriptors/ShapeTemplateStrategy/ShapeTemplateOptionsSetup.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Options;
+using OrchardCore.Modules;
 
 namespace OrchardCore.DisplayManagement.Descriptors.ShapeTemplateStrategy
 {
@@ -10,14 +11,16 @@ namespace OrchardCore.DisplayManagement.Descriptors.ShapeTemplateStrategy
     public class ShapeTemplateOptionsSetup : IConfigureOptions<ShapeTemplateOptions>
     {
         private readonly IHostingEnvironment _hostingEnvironment;
+        private readonly IApplicationContext _applicationContext;
 
         /// <summary>
         /// Initializes a new instance of <see cref="ShapeTemplateViewOptions"/>.
         /// </summary>
         /// <param name="hostingEnvironment"><see cref="IHostingEnvironment"/> for the application.</param>
-        public ShapeTemplateOptionsSetup(IHostingEnvironment hostingEnvironment)
+        public ShapeTemplateOptionsSetup(IHostingEnvironment hostingEnvironment, IApplicationContext applicationContext)
         {
             _hostingEnvironment = hostingEnvironment ?? throw new ArgumentNullException(nameof(hostingEnvironment));
+            _applicationContext = applicationContext;
         }
 
         public void Configure(ShapeTemplateOptions options)
@@ -26,6 +29,10 @@ namespace OrchardCore.DisplayManagement.Descriptors.ShapeTemplateStrategy
             {
                 options.FileProviders.Add(_hostingEnvironment.ContentRootFileProvider);
             }
+
+            // To let the application behave as a module, its views are requested under the virtual
+            // "Areas" folder, but they are still served from the file system by this custom provider.
+            options.FileProviders.Insert(0, new ApplicationViewFileProvider(_applicationContext));
         }
     }
 }
