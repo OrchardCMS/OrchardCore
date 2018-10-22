87f6a8aa082d43f085a30e26520df7345043c0e3
Sebastien Ros <sebastienros@gmail.com>
2018-10-22 15:31:19 -0700

    Adding tenants APIs

---
+++ src/OrchardCore.Modules/OrchardCore.Tenants/ViewModels/SetupApiViewModel.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace OrchardCore.Tenants.ViewModels
+{
+    public class SetupApiViewModel
+    {
+
+        [Required]
+        public string Tenant { get; set; }
+
+        [Required]
+        public string SiteName { get; set; }
+
+        public string DatabaseProvider { get; set; }
+
+        public string ConnectionString { get; set; }
+
+        public string TablePrefix { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string RecipeName { get; set; }
+        public IFormFile Recipe { get; set; }
+
+        public string SiteTimeZone { get; set; }
+    }
+}
