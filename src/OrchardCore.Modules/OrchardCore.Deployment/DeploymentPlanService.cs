06a5414538f89945ef683805b0c8b62edd109f8c
Himanshu Swami <himanshu@swami.ws>
2018-10-26 12:03:44 -0700

    Added deployment plan and recipe support for delpoyment plans.

---
+++ src/OrchardCore.Modules/OrchardCore.Deployment/DeploymentPlanService.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using OrchardCore.Deployment.Indexes;
+using YesSql;
+
+namespace OrchardCore.Deployment
+{
+    public class DeploymentPlanService
+    {
+        private readonly YesSql.ISession _session;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IAuthorizationService _authorizationService;
+        private readonly Lazy<Task<Dictionary<string, DeploymentPlan>>> _deploymentPlans;
+
+        public DeploymentPlanService(
+            YesSql.ISession session,
+            IHttpContextAccessor httpContextAccessor,
+            IAuthorizationService authorizationService)
+        {
+            _session = session;
+            _httpContextAccessor = httpContextAccessor;
+            _authorizationService = authorizationService;
+            _deploymentPlans = new Lazy<Task<Dictionary<string, DeploymentPlan>>>(() =>
+                Task.Factory.StartNew(async () =>
+                {
+                    var deploymentPlanQuery = _session.Query<DeploymentPlan, DeploymentPlanIndex>();
+                    var deploymentPlans = await deploymentPlanQuery.ListAsync();
+                    return deploymentPlans.ToDictionary(x => x.Name);
+                }).Unwrap());
+        }
+
+        public async Task<bool> DoesUserHavePermissionsAsync()
+        {
+            var user = _httpContextAccessor.HttpContext.User;
+
+            var result = await _authorizationService.AuthorizeAsync(user, Permissions.ManageDeploymentPlan);
+
+            if (result)
+            {
+                result = await _authorizationService.AuthorizeAsync(user, Permissions.Export);
+            }
+
+            return result;
+        }
+
+        public async Task<IEnumerable<string>> GetAllDeploymentPlanNamesAsync()
+        {
+            var deploymentPlans = await _deploymentPlans.Value;
+
+            return deploymentPlans.Keys;
+        }
+
+        public async Task<IEnumerable<DeploymentPlan>> GetAllDeploymentPlansAsync()
+        {
+            var deploymentPlans = await _deploymentPlans.Value;
+
+            return deploymentPlans.Values;
+        }
+
+        public async Task<IEnumerable<DeploymentPlan>> GetDeploymentPlansAsync(params string[] deploymentPlanNames)
+        {
+            var deploymentPlans = await _deploymentPlans.Value;
+
+            return GetDeploymentPlans(deploymentPlans, deploymentPlanNames);
+        }
+
+        private static IEnumerable<DeploymentPlan> GetDeploymentPlans(IDictionary<string, DeploymentPlan> deploymentPlans, params string[] deploymentPlanNames)
+        {
+            foreach (var deploymentPlanName in deploymentPlanNames)
+            {
+                DeploymentPlan deploymentPlan;
+                if (deploymentPlans.TryGetValue(deploymentPlanName, out deploymentPlan))
+                {
+                    yield return deploymentPlan;
+                }
+            }
+        }
+
+        public async Task CreateOrUpdateDeploymentPlans(IEnumerable<DeploymentPlan> deploymentPlans)
+        {
+            foreach (var deploymentPlan in deploymentPlans)
+            {
+                _session.Save(deploymentPlan);
+            }
+
+            await _session.CommitAsync();
+        }
+    }
+}
