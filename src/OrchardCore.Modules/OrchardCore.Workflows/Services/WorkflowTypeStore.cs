635a37f6ae502580904e34778cd79f8ebef4947b
Michael Petrinolis <MichaelPetrinolis@users.noreply.github.com>
2018-11-20 21:49:47 +0200

    The Coming Soon theme (#2321)

--- src/OrchardCore.Modules/OrchardCore.Workflows/Services/WorkflowTypeStore.cs
+++ src/OrchardCore.Modules/OrchardCore.Workflows/Services/WorkflowTypeStore.cs
@@ -27,19 +27,19 @@ namespace OrchardCore.Workflows.Services
             return _session.GetAsync<WorkflowType>(id);
         }
 
-        public async Task<IEnumerable<WorkflowType>> GetAsync(IEnumerable<int> ids)
+        public Task<IEnumerable<WorkflowType>> GetAsync(IEnumerable<int> ids)
         {
-            return await _session.GetAsync<WorkflowType>(ids.ToArray());
+            return _session.GetAsync<WorkflowType>(ids.ToArray());
         }
 
-        public async Task<WorkflowType> GetAsync(string workflowTypeId)
+        public Task<WorkflowType> GetAsync(string workflowTypeId)
         {
-            return await _session.Query<WorkflowType, WorkflowTypeIndex>(x => x.WorkflowTypeId == workflowTypeId).FirstOrDefaultAsync();
+            return _session.Query<WorkflowType, WorkflowTypeIndex>(x => x.WorkflowTypeId == workflowTypeId).FirstOrDefaultAsync();
         }
 
-        public async Task<IEnumerable<WorkflowType>> ListAsync()
+        public Task<IEnumerable<WorkflowType>> ListAsync()
         {
-            return await _session.Query<WorkflowType, WorkflowTypeIndex>().ListAsync();
+            return _session.Query<WorkflowType, WorkflowTypeIndex>().ListAsync();
         }
 
         public async Task<IList<WorkflowType>> GetByStartActivityAsync(string activityName)
