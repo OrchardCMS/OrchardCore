6fd11268fa72ce9c83eabb0fcb4662f554c88323
Jean-Thierry Kéchichian <jean-thierry.kechichian@wanadoo.fr>
2018-11-21 01:51:36 +0000

    Background tasks (#1572)

---
+++ src/OrchardCore/OrchardCore.Abstractions/BackgroundTasks/BackgroundTaskAttribute.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace OrchardCore.BackgroundTasks
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+    public class BackgroundTaskAttribute : Attribute
+    {
+        public bool Enable { get; set; } = true;
+        public string Schedule { get; set; } = "*/5 * * * *";
+        public string Description { get; set; } = String.Empty;
+    }
+}
