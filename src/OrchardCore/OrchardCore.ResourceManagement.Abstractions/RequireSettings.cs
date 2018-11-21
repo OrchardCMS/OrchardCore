c14175f49207885607293602e1a49ae34c5c5db7
Sébastien Ros <sebastienros@gmail.com>
2018-11-21 14:19:51 -0800

    Implementing resource management configuration (#2716)
    Fixes #2161

--- src/OrchardCore/OrchardCore.ResourceManagement.Abstractions/RequireSettings.cs
+++ src/OrchardCore/OrchardCore.ResourceManagement.Abstractions/RequireSettings.cs
@@ -21,7 +21,19 @@ namespace OrchardCore.ResourceManagement
         public Dictionary<string, string> Attributes
         {
             get { return _attributes ?? (_attributes = new Dictionary<string, string>()); }
-            set { _attributes = value; }
+            private set { _attributes = value; }
+        }
+
+        public RequireSettings()
+        {
+
+        }
+
+        public RequireSettings(ResourceManagementOptions options)
+        {
+            CdnMode = options.UseCdn;
+            DebugMode = options.DebugMode;
+            Culture = options.Culture;
         }
 
         public bool HasAttributes
