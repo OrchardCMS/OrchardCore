6229aeb5e761cc97d229abdb914a4df01aff3e91
jtkech <jean-thierry.kechichian@wanadoo.fr>
2018-11-27 05:30:31 +0100

    Fixes user claims based on roles.

--- src/OrchardCore/OrchardCore.Users.Core/Services/DefaultUserClaimsPrincipalFactory.cs
+++ src/OrchardCore/OrchardCore.Users.Core/Services/DefaultUserClaimsPrincipalFactory.cs
@@ -2,6 +2,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
+using OrchardCore.Security.Services;
 
 namespace OrchardCore.Users.Services
 {
@@ -10,13 +11,16 @@ namespace OrchardCore.Users.Services
     /// </summary>
     public class DefaultUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<IUser>
     {
+        private readonly IRoleProvider _roleProvider;
         private readonly UserManager<IUser> _userManager;
         private readonly IOptions<IdentityOptions> _identityOptions;
 
         public DefaultUserClaimsPrincipalFactory(
-            UserManager<IUser> userManager, 
+            IRoleProvider roleProvider,
+            UserManager<IUser> userManager,
             IOptions<IdentityOptions> identityOptions) : base(userManager, identityOptions)
         {
+            _roleProvider = roleProvider;
             _userManager = userManager;
             _identityOptions = identityOptions;
         }
@@ -31,10 +35,18 @@ namespace OrchardCore.Users.Services
             {
                 claims.AddClaim(new Claim("email", email));
             }
-                       
+
             if (await _userManager.IsEmailConfirmedAsync(user))
             {
                 claims.AddClaim(new Claim("email_verified", "true"));
+            }
+
+            foreach (var role in await _userManager.GetRolesAsync(user))
+            {
+                foreach (var claim in await _roleProvider.GetRoleClaimsAsync(_userManager.NormalizeKey(role)))
+                {
+                    claims.AddClaim(claim);
+                }
             }
 
             return claims;
