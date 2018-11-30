bc75c0a60b529ae9fc1bbc8ad2e0b1c50fdd6c4a
Peter Davis <peter.w.davis@gmail.com>
2018-11-30 20:54:54 +1030

    Remove unused usings

--- src/OrchardCore.Modules/OrchardCore.Users/Controllers/AccountController.cs
+++ src/OrchardCore.Modules/OrchardCore.Users/Controllers/AccountController.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
-using System.Text.RegularExpressions;
-using System.Threading;
 using System.Threading.Tasks;
 using AspNet.Security.OpenIdConnect.Primitives;
 using Microsoft.AspNetCore.Authentication;
