a197af75ca8c9eea7697f52bc3fcc9555837999c
Sébastien Ros <sebastienros@gmail.com>
2018-12-04 17:26:43 -0800

    Update IDbConnectionAccessor.cs

--- src/OrchardCore/OrchardCore.Data.Abstractions/IDbConnectionAccessor.cs
+++ src/OrchardCore/OrchardCore.Data.Abstractions/IDbConnectionAccessor.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace OrchardCore.Data.Abstractions
 {
-    public interface IDbConnectionAccessor : IDisposable
+    public interface IDbConnectionAccessor
     {
         Task<IDbConnection> GetConnectionAsync();
     }
