8df947a8b888dd89376f05665539abf845e71e08
Jasmin Savard <jasmin.savard@gmail.com>
2018-12-17 20:02:03 -0500

    adding separator after first menu item.
    (wip) make button for collapsing menu to stand in bottom.

--- src/OrchardCore.Themes/TheAdmin/wwwroot/Scripts/TheAdmin.js
+++ src/OrchardCore.Themes/TheAdmin/wwwroot/Scripts/TheAdmin.js
@@ -6674,7 +6674,18 @@ function isNumber(str) {
     return str.length === 1 && str.match(/[0-9]/i);
 }
 
-
+var stringToColour = function (str) {
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    var colour = '#';
+    for (var i = 0; i < 3; i++) {
+        var value = (hash >> (i * 8)) & 0xFF;
+        colour += ('00' + value.toString(16)).substr(-2);
+    }
+    return colour;
+}
 // When we load compact status from preferences we need to do some other tasks besides adding the class to the body.
 // UserPreferencesLoader has already added the needed class.
 $(function () {
@@ -6688,6 +6699,8 @@ $(function () {
         || (($('body').hasClass('no-admin-preferences') && $(window).width() < 768))){
         setCompactStatus();
     }
+
+    $('#ta-left-sidebar .persona-content').css('background-color', stringToColour($('#ta-left-sidebar .persona-content').data("title")));
 });
 
 
