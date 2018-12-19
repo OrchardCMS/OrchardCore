d08e15cca17c7e32f3e57432d951689d8398b159
Jasmin Savard <jasmin.savard@gmail.com>
2018-12-19 18:50:56 -0500

    removing function (not part of this PR)

--- src/OrchardCore.Themes/TheAdmin/Assets/js/admin.js
+++ src/OrchardCore.Themes/TheAdmin/Assets/js/admin.js
@@ -142,16 +142,3 @@ function isLetter(str) {
 function isNumber(str) {
     return str.length === 1 && str.match(/[0-9]/i);
 }
-
-var stringToColour = function (str) {
-    var hash = 0;
-    for (var i = 0; i < str.length; i++) {
-        hash = str.charCodeAt(i) + ((hash << 5) - hash);
-    }
-    var colour = '#';
-    for (var i = 0; i < 3; i++) {
-        var value = (hash >> (i * 8)) & 0xFF;
-        colour += ('00' + value.toString(16)).substr(-2);
-    }
-    return colour;
-}
