842324f27988af8eb294e45284a7d1b6f131c61b
andy <zgofmontreal@gmail.com>
2019-01-08 13:42:01 -0500

    Basic health check (#2976)

---
+++ src/OrchardCore.Modules/OrchardCore.HealthChecks/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "Health Check",
+    Author = "The Orchard Team",
+    Website = "http://orchardproject.net",
+    Version = "2.0.0",
+    Description = "This module provides health check for the website.",
+    Category = "Infrastructure"
+)]
