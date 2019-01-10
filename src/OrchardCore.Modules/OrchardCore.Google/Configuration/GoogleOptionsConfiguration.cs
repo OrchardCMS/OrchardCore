87f7f7c9adb30b6c08b091cc33c7ad63df09d32b
Michael Petrinolis <MichaelPetrinolis@users.noreply.github.com>
2019-01-10 21:43:13 +0200

    Patching Google+ (#2996)

--- src/OrchardCore.Modules/OrchardCore.Google/Configuration/GoogleOptionsConfiguration.cs
+++ src/OrchardCore.Modules/OrchardCore.Google/Configuration/GoogleOptionsConfiguration.cs
@@ -2,6 +2,7 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Google;
@@ -75,6 +76,17 @@ namespace OrchardCore.Google.Configuration
             {
                 options.CallbackPath = settings.CallbackPath;
             }
+
+            // should be removed after February 2.2 patches
+            options.UserInformationEndpoint = "https://www.googleapis.com/oauth2/v2/userinfo";
+            options.ClaimActions.Clear();
+            options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+            options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
+            options.ClaimActions.MapJsonKey(ClaimTypes.GivenName, "given_name");
+            options.ClaimActions.MapJsonKey(ClaimTypes.Surname, "family_name");
+            options.ClaimActions.MapJsonKey("urn:google:profile", "link");
+            options.ClaimActions.MapJsonKey(ClaimTypes.Email, "email");
+
         }
 
         public void Configure(GoogleOptions options) => Debug.Fail("This infrastructure method shouldn't be called.");
