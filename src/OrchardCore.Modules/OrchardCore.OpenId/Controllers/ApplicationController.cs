3bd601f4c8657190c3cb5decfaf6298c5a8d45a2
Kévin Chalet <kevinchalet@gmail.com>
2019-01-10 20:42:33 +0100

    Temporarily reintroduce the null propagation operators in ApplicationController to unblock CI builds

--- src/OrchardCore.Modules/OrchardCore.OpenId/Controllers/ApplicationController.cs
+++ src/OrchardCore.Modules/OrchardCore.OpenId/Controllers/ApplicationController.cs
@@ -96,7 +96,7 @@ namespace OrchardCore.OpenId.Controllers
 
             var model = new CreateOpenIdApplicationViewModel();
 
-            var roleService = HttpContext.RequestServices.GetService<IRoleService>();
+            var roleService = HttpContext.RequestServices?.GetService<IRoleService>();
             if (roleService != null)
             {
                 foreach (var role in await roleService.GetRoleNamesAsync())
@@ -250,7 +250,7 @@ namespace OrchardCore.OpenId.Controllers
                 Type = await _applicationManager.GetClientTypeAsync(application)
             };
 
-            var roleService = HttpContext.RequestServices.GetService<IRoleService>();
+            var roleService = HttpContext.RequestServices?.GetService<IRoleService>();
             if (roleService != null)
             {
                 foreach (var role in await roleService.GetRoleNamesAsync())
