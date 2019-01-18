6b9d972160179e0b1d95e02c9046255781d9b3dc
Nick Mayne <Jetski5822@gmail.com>
2019-01-18 16:15:54 +0000

    Initial models

--- src/OrchardCore/OrchardCore.ContentManagement.GraphQL/Queries/Models/GraphQLContentTypePartSettings.cs
+++ src/OrchardCore/OrchardCore.ContentManagement.GraphQL/Queries/Models/GraphQLContentTypePartSettings.cs
@@ -1,7 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace OrchardCore.ContentManagement.GraphQL.Queries.Models
 {
     public class GraphQLContentTypePartSettings
     {
         public bool CollapseFieldsToParent { get; set; }
+    }
+
+    public class GraphQLContentOptions
+    {
+        public IEnumerable<GraphQLContentTypeOption> ContentTypeOptions { get; set; }
+            = Enumerable.Empty<GraphQLContentTypeOption>();
+    }
+
+    public class GraphQLContentTypeOption
+    {
+        public string ContentType { get; set; }
+
+        public bool Collapse { get; set; }
+
+        public IEnumerable<GraphQLContentPartOption> PartOptions { get; set; }
+            = Enumerable.Empty<GraphQLContentPartOption>();
+    }
+
+    public class GraphQLContentPartOption
+    {
+        public Type Type { get; set; }
+
+        public bool Collapse { get; set; }
     }
 }
