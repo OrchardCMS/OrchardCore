dbe98c4dabf20dfe6f408210a41bbf55530844e8
jtkech <jean-thierry.kechichian@wanadoo.fr>
2019-02-08 00:21:14 +0100

    React to review - AddAsync for another missing signature.

--- src/OrchardCore/OrchardCore.Navigation.Core/NavigationBuilder.cs
+++ src/OrchardCore/OrchardCore.Navigation.Core/NavigationBuilder.cs
@@ -36,19 +36,34 @@ namespace OrchardCore.Navigation
             return this;
         }
 
+        public NavigationBuilder Add(LocalizedString caption, string position, Action<NavigationItemBuilder> itemBuilder, IEnumerable<string> classes = null, int priority = 0)
+        {
+            var childBuilder = new NavigationItemBuilder();
+
+            childBuilder.Caption(caption);
+            childBuilder.Position(position);
+            childBuilder.Priority(priority);
+            itemBuilder(childBuilder);
+            Contained.AddRange(childBuilder.Build());
+
+            if (classes != null)
+            {
+                foreach (var className in classes)
+                {
+                    childBuilder.AddClass(className);
+                }
+            }
+
+            return this;
+        }
+
         public Task<NavigationBuilder> AddAsync(LocalizedString caption, Func<NavigationItemBuilder, Task> itemBuilder, IEnumerable<string> classes = null)
         {
             return AddAsync(caption, null, itemBuilder, classes);
         }
-
-        public NavigationBuilder Add(LocalizedString caption, string position, Action<NavigationItemBuilder> itemBuilder, IEnumerable<string> classes = null, int priority = 0)
+        public Task<NavigationBuilder> AddAsync(Func<NavigationItemBuilder, Task> itemBuilder, IEnumerable<string> classes = null)
         {
-            return AddAsync(caption, position, builder =>
-            {
-                itemBuilder(builder);
-                return Task.CompletedTask;
-            },
-            classes, priority).GetAwaiter().GetResult();
+            return AddAsync(new LocalizedString(null, null), null, itemBuilder, classes);
         }
 
         public NavigationBuilder Add(LocalizedString caption, Action<NavigationItemBuilder> itemBuilder, IEnumerable<string> classes = null)
@@ -59,6 +74,7 @@ namespace OrchardCore.Navigation
         {
             return Add(new LocalizedString(null, null), null, itemBuilder, classes);
         }
+
         public NavigationBuilder Add(LocalizedString caption, string position, IEnumerable<string> classes = null)
         {
             return Add(caption, position, x => { }, classes);
