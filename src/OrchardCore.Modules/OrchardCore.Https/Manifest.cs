5fbb87936ba2e39145f7df7f63ef0d9778456092
Sebastien Ros <sebastienros@gmail.com>
2019-02-28 11:15:30 -0800

    Simplifying theme template
    Fixes #3196

--- src/OrchardCore.Modules/OrchardCore.Https/Manifest.cs
+++ src/OrchardCore.Modules/OrchardCore.Https/Manifest.cs
@@ -3,7 +3,7 @@ using OrchardCore.Modules.Manifest;
 [assembly: Module(
     Name = "HTTPS",
     Author = "The Orchard Team",
-    Website = "http://orchardproject.net",
+    Website = "https://orchardproject.net",
     Version = "2.0.0",
     Description = "This module will ensure HTTPS is used when accessing the website.",
     Category = "Security"
