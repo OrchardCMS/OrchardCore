49764357dc08eac699a9aacda450e9f9a27ad4e2
jtkech <jean-thierry.kechichian@wanadoo.fr>
2019-03-18 00:46:18 +0100

    Revert OC.Media.Azure changes.

--- src/OrchardCore.Modules/OrchardCore.Media.Azure/Startup.cs
+++ src/OrchardCore.Modules/OrchardCore.Media.Azure/Startup.cs
@@ -1,11 +1,9 @@
 using System;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using OrchardCore.Environment.Shell;
 using OrchardCore.Environment.Shell.Configuration;
 using OrchardCore.FileStorage.AzureBlob;
 using OrchardCore.Media.Services;
@@ -16,11 +14,6 @@ namespace OrchardCore.Media.Azure
     [Feature("OrchardCore.Media.Azure.Storage")]
     public class Startup : StartupBase
     {
-        /// <summary>
-        /// The url prefix used to route asset files
-        /// </summary>
-        private const string AssetsUrlPrefix = "/media";
-
         private ILogger<Startup> _logger;
         private readonly IShellConfiguration _configuration;
 
@@ -46,23 +39,13 @@ namespace OrchardCore.Media.Azure
                     var options = serviceProvider.GetRequiredService<IOptions<MediaBlobStorageOptions>>().Value;
                     var clock = serviceProvider.GetRequiredService<IClock>();
 
-                    var shellSettings = serviceProvider.GetRequiredService<ShellSettings>();
-
-                    // To make the 'BlobFileStore' tenant aware.
-                    if (shellSettings.RequestUrlPrefix != null)
-                    {
-                        options.BasePath = '/' + shellSettings.RequestUrlPrefix;
-                    }
-
                     var fileStore = new BlobFileStore(options, clock);
 
-                    var httpContextAccessor = serviceProvider.GetRequiredService<IHttpContextAccessor>();
-                    var pathBase = httpContextAccessor.HttpContext.Request.PathBase;
+                    var mediaBaseUri = fileStore.BaseUri;
+                    if (!String.IsNullOrEmpty(options.PublicHostName))
+                        mediaBaseUri = new UriBuilder(mediaBaseUri) { Host = options.PublicHostName }.Uri;
 
-                    // 'PathBase' includes the 'RequestUrlPrefix' and may start by a virtual folder.
-                    var mediaUrlBase = pathBase.Add(AssetsUrlPrefix);
-
-                    return new MediaFileStore(fileStore, mediaUrlBase);
+                    return new MediaFileStore(fileStore, mediaBaseUri.ToString());
                 }));
             }
 
