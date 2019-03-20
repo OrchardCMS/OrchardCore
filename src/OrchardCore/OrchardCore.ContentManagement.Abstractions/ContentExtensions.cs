a9786f429a080ee372cfc1f8ecd19f473e3613de
Sébastien Ros <sebastienros@gmail.com>
2019-03-20 12:18:35 -0700

    Update ContentExtensions.cs

--- src/OrchardCore/OrchardCore.ContentManagement.Abstractions/ContentExtensions.cs
+++ src/OrchardCore/OrchardCore.ContentManagement.Abstractions/ContentExtensions.cs
@@ -148,12 +148,18 @@ namespace OrchardCore.ContentManagement
             if (elementData != null)
             {
                 elementData.Merge(JObject.FromObject(element), JsonMergeSettings);
-                contentElement.Elements.Remove(name);
             }
             else
             {
-                contentElement.Data[name] = JObject.FromObject(element, ContentBuilderSettings.IgnoreDefaultValuesSerializer);
+                elementData = JObject.FromObject(element, ContentBuilderSettings.IgnoreDefaultValuesSerializer);
+                contentElement.Data[name] = elementData;
             }
+
+            element.Data = elementData;
+            element.ContentItem = contentElement.ContentItem;
+
+            // Replace the existing content element with the new one
+            contentElement.Elements[name] = element;
 
             return contentElement;
         }
@@ -174,6 +180,7 @@ namespace OrchardCore.ContentManagement
                 contentElement.Data = JObject.FromObject(element.Data, ContentBuilderSettings.IgnoreDefaultValuesSerializer);
             }
 
+            contentElement.Elements.Clear();
             return contentElement;
         }
 
