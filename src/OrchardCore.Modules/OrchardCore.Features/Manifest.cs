804ff4fae87e66e0a8e21a2b820f0c3bc1f6f247
Antoine Griffard <agriffard@hotmail.com>
2019-03-25 19:49:25 +0100

    Missing description and name in manifests (#3385)
    Fixes #3384

--- src/OrchardCore.Modules/OrchardCore.Features/Manifest.cs
+++ src/OrchardCore.Modules/OrchardCore.Features/Manifest.cs
@@ -9,6 +9,7 @@ using OrchardCore.Modules.Manifest;
 
 [assembly: Feature(
     Id = "OrchardCore.Features",
+    Name = "Features",
     Description = "The Features module enables the administrator of the site to manage the installed modules as well as activate and de-activate features.",
     Dependencies = new [] { "OrchardCore.Resources" },
     Category = "Infrastructure"
