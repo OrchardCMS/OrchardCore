c3a869580347b37b1b4f38219489b42ab9a5fd46
Seth Cleaver <seth@cleaver.id.au>
2019-03-27 07:40:21 +1000

    Moved the registering of meta tags from the view to the Display Async method so that adding of metadata isn't accidentally overriden by changes to content type templates. Fix a couple of bugs in the UI.

--- src/OrchardCore.Modules/OrchardCore.Metadata/Drivers/MetadataPartDisplayDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.Metadata/Drivers/MetadataPartDisplayDriver.cs
@@ -2,12 +2,13 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using OrchardCore.ContentManagement.Display.ContentDisplay;
+using OrchardCore.ContentManagement.Display.Models;
 using OrchardCore.ContentManagement.Metadata;
 using OrchardCore.DisplayManagement.ModelBinding;
 using OrchardCore.DisplayManagement.Views;
 using OrchardCore.Metadata.Models;
-using OrchardCore.Metadata.Settings;
 using OrchardCore.Metadata.ViewModels;
+using OrchardCore.ResourceManagement;
 using OrchardCore.Settings;
 
 namespace OrchardCore.Metadata.Drivers
@@ -16,19 +17,143 @@ namespace OrchardCore.Metadata.Drivers
     {
         private readonly IContentDefinitionManager _contentDefinitionManager;
         private readonly ISiteService _siteService;
+        private readonly IResourceManager _resourceManager;
 
         public MetadataPartDisplayDriver(
             IContentDefinitionManager contentDefinitionManager,
-            ISiteService siteService)
+            ISiteService siteService, IResourceManager resourceManager)
         {
             _contentDefinitionManager = contentDefinitionManager;
             _siteService = siteService;
+            _resourceManager = resourceManager;
         }
 
         public override IDisplayResult Display(MetadataPart metadataPart)
         {
             return Initialize<MetadataPartViewModel>("MetadataPart", async model => await BuildViewModelAsync(model, metadataPart))
             .Location("Detail", "Content:20");
+        }
+
+        public override async Task<IDisplayResult> DisplayAsync(MetadataPart part, BuildPartDisplayContext context)
+        {
+            var viewModel = new MetadataPartViewModel();
+            await BuildViewModelAsync(viewModel, part);
+
+            var settings = GetSettings(part);
+
+            if (!String.IsNullOrWhiteSpace(viewModel.MetaDescription))
+            {
+                _resourceManager.RegisterMeta(new MetaEntry
+                {
+                    Name = "description",
+                    Content = viewModel.MetaDescription
+                });
+            }
+
+            if (settings.SupportMetaKeywords && !String.IsNullOrWhiteSpace(viewModel.MetaKeywords))
+            {
+                _resourceManager.RegisterMeta(new MetaEntry
+                {
+                    Name = "keywords",
+                    Content = viewModel.MetaKeywords
+                });
+            }
+            // Both Twitter and Facebook share some Open Graph tags so use these if possible rather than the platform specific ones.
+            if (settings.SupportOpenGraph || settings.SupportTwitterCards)
+            {
+                if (!String.IsNullOrWhiteSpace(viewModel.OpenGraphTitle))
+                {
+                    _resourceManager.RegisterMeta(new MetaEntry
+                    {
+                        Property = "og:title",
+                        Content = viewModel.OpenGraphTitle
+                    });
+                }
+                if (!String.IsNullOrWhiteSpace(viewModel.OpenGraphDescription))
+                {
+                    _resourceManager.RegisterMeta(new MetaEntry
+                    {
+                        Property = "og:description",
+                        Content = viewModel.OpenGraphDescription
+                    });
+                }
+                if (!String.IsNullOrWhiteSpace(viewModel.OpenGraphImage))
+                {
+                    _resourceManager.RegisterMeta(new MetaEntry
+                    {
+                        Property = "og:image",
+                        Content = viewModel.OpenGraphImage
+                    });
+                }
+            }
+            // Extended metatags for Facebook Open Graph 
+            if (settings.SupportOpenGraph)
+            {
+                if (!String.IsNullOrWhiteSpace(viewModel.OpenGraphType))
+                {
+                    _resourceManager.RegisterMeta(new MetaEntry
+                    {
+                        Property = "og:type",
+                        Content = viewModel.OpenGraphType
+                    });
+                }
+                if (!String.IsNullOrWhiteSpace(viewModel.OpenGraphUrl))
+                {
+                    _resourceManager.RegisterMeta(new MetaEntry
+                    {
+                        Property = "og:url",
+                        Content = viewModel.OpenGraphUrl
+                    });
+                }
+                if (!String.IsNullOrWhiteSpace(viewModel.OpenGraphImageAlt) && !String.IsNullOrWhiteSpace(viewModel.OpenGraphImage))
+                {
+                    _resourceManager.RegisterMeta(new MetaEntry
+                    {
+                        Property = "og:image:alt",
+                        Content = viewModel.OpenGraphImageAlt
+                    });
+                }
+            }
+            // Extended metatags for Twitter
+            if (settings.SupportTwitterCards)
+            {
+                if (!String.IsNullOrWhiteSpace(viewModel.TwitterCard))
+                {
+                    _resourceManager.RegisterMeta(new MetaEntry
+                    {
+                        Name = "twitter:card",
+                        Content = viewModel.TwitterCard
+                    });
+                }
+                // Twitter does not support the Open Graph Image Alt tag so we have to add Twitter's own tag
+                if (!String.IsNullOrWhiteSpace(viewModel.OpenGraphImageAlt) && !String.IsNullOrWhiteSpace(viewModel.OpenGraphImage))
+                {
+                    _resourceManager.RegisterMeta(new MetaEntry
+                    {
+                        Name = "twitter:image:alt",
+                        Content = viewModel.OpenGraphImageAlt
+                    });
+                }
+            }
+            // If an Facebook App id is registered in the site settings
+            if (!String.IsNullOrWhiteSpace(viewModel.FacebookAppId))
+            {
+                _resourceManager.RegisterMeta(new MetaEntry
+                {
+                    Property = "fb:app_id",
+                    Content = viewModel.FacebookAppId
+                });
+            }
+            // If a Twitter site id is registered in the site settings
+            if (!String.IsNullOrWhiteSpace(viewModel.TwitterSite))
+            {
+                _resourceManager.RegisterMeta(new MetaEntry
+                {
+                    Name = "twitter:site",
+                    Content = viewModel.TwitterSite
+                });
+            }
+            return await base.DisplayAsync(part, context);
         }
 
         public override IDisplayResult Edit(MetadataPart metadataPart)
@@ -39,6 +164,7 @@ namespace OrchardCore.Metadata.Drivers
 
                 model.MetaTitle = metadataPart.MetaTitle;
                 model.MetaDescription = metadataPart.MetaDescription;
+                model.MetaKeywords = metadataPart.MetaKeywords;
                 model.OpenGraphTitle = metadataPart.OpenGraphTitle;
                 model.OpenGraphDescription = metadataPart.OpenGraphDescription;
                 model.OpenGraphImage = metadataPart.OpenGraphImage;
@@ -75,6 +201,7 @@ namespace OrchardCore.Metadata.Drivers
             // If blank get title from Content items display text
             viewModel.MetaTitle = GenerateText(part.MetaTitle, part.ContentItem.DisplayText);
             viewModel.MetaDescription = part.MetaDescription;
+            viewModel.MetaKeywords = part.MetaKeywords;
 
             // If blank get title from MetaTitle
             viewModel.OpenGraphTitle = GenerateText(part.OpenGraphTitle, viewModel.MetaTitle);
@@ -98,6 +225,7 @@ namespace OrchardCore.Metadata.Drivers
             viewModel.OpenGraphUrl = siteSettings.BaseUrl;
             viewModel.MetadataPart = part;
             viewModel.Settings = settings;
+
         }
 
         private string GenerateText(string text, string alternateText)
