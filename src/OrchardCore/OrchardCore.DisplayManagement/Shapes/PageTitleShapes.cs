a26ee5b08d3c3bba8399d966fe6c4993ad04137a
Seth Cleaver <seth@cleaver.id.au>
2019-03-29 07:10:36 +1000

    Working on parsing the page title format

--- src/OrchardCore/OrchardCore.DisplayManagement/Shapes/PageTitleShapes.cs
+++ src/OrchardCore/OrchardCore.DisplayManagement/Shapes/PageTitleShapes.cs
@@ -1,36 +1,30 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
+using Fluid;
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc.Localization;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Localization;
 using OrchardCore.DisplayManagement.Descriptors;
 using OrchardCore.DisplayManagement.Title;
+using OrchardCore.Liquid;
 using OrchardCore.Settings;
 
 namespace OrchardCore.DisplayManagement.Shapes
 {
-    
+
     public class PageTitleShapes : IShapeAttributeProvider
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly ISiteService _siteService;
+        private readonly ILiquidTemplateManager _liquidTemplateManager;
 
-        public PageTitleShapes(IHttpContextAccessor httpContextAccessor, ISiteService siteService, IStringLocalizer<PageTitleShapes> localizer, IHtmlLocalizer<PageTitleShapes> htmlLocalizer)
+        public PageTitleShapes(IHttpContextAccessor httpContextAccessor, ISiteService siteService, ILiquidTemplateManager liquidTemplateManager)
         {
+            _liquidTemplateManager = liquidTemplateManager;
             _httpContextAccessor = httpContextAccessor;
             _siteService = siteService;
-            T = localizer;
-            H = htmlLocalizer;
         }
-
-        IStringLocalizer T { get; }
-        IHtmlLocalizer H { get; }
-
 
         private IPageTitleBuilder _pageTitleBuilder;
         public IPageTitleBuilder Title
@@ -52,14 +46,28 @@ namespace OrchardCore.DisplayManagement.Shapes
             var siteSettings = await _siteService.GetSiteSettingsAsync();
 
             // We must return a page title so if the format setting is blank just use the current title unformatted
-            if(String.IsNullOrWhiteSpace(siteSettings.PageTitleFormat))
+            if (String.IsNullOrWhiteSpace(siteSettings.PageTitleFormat))
             {
                 return Title.GenerateTitle(null);
             }
+            else
+            {
+                return GenerateTitleUsingFormat(siteSettings.PageTitleFormat);
+            }
+        }
 
-            //TODO: parse the page title format with liquid to construct the users required page title format
-            // for now just use my site setting as is for feedback
-            return Html.Raw(siteSettings.PageTitleFormat); ;
+        private IHtmlContent GenerateTitleUsingFormat(string pageTitleFormat)
+        {
+            var htmlContentBuilder = new HtmlContentBuilder();
+
+            // Need to parse the title format somehow
+            //var templateContext = new TemplateContext();
+
+            //var test = await _liquidTemplateManager.RenderAsync(pageTitleFormat, NullEncoder.Default, templateContext);
+
+            htmlContentBuilder.AppendHtml(pageTitleFormat);
+
+            return htmlContentBuilder;
         }
     }
 }
