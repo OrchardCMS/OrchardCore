b48595df108df720ff61874d74092822b939cbd4
Dean Marcussen <dean.marcussen@gmail.com>
2019-05-06 07:33:47 +0100

    add support for tenant static file provider, and change tenant static file provider manifest to once per tenant = false

---
+++ src/OrchardCore.Modules/OrchardCore.Tenants/Services/ITenantFileProvider.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.FileProviders;
+
+namespace OrchardCore.Tenants.Services
+{
+    public interface ITenantFileProvider : IFileProvider
+    {
+    }
+}
