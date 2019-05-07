19e325d79255a218710c822c3ebe48c4fa375ca9
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2019-05-07 23:25:48 +0300

    Address feedback

--- src/OrchardCore.Modules/OrchardCore.Localization/Startup.cs
+++ src/OrchardCore.Modules/OrchardCore.Localization/Startup.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -22,15 +21,6 @@ namespace OrchardCore.Localization
     /// </summary>
     public class Startup : StartupBase
     {
-        public Startup(IHostingEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json");
-
-            Configuration = builder.Build();
-        }
-
         public IConfiguration Configuration { get; }
 
         public override void ConfigureServices(IServiceCollection services)
