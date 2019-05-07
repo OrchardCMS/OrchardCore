5c8db3c06963156a5921c8125a02c92e2526d7bd
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2019-05-07 22:41:19 +0300

    Use localization middleware inside setup module

--- src/OrchardCore.Modules/OrchardCore.Setup/Startup.cs
+++ src/OrchardCore.Modules/OrchardCore.Setup/Startup.cs
@@ -1,20 +1,59 @@
 using System;
+using System.Linq;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using OrchardCore.Modules;
 
 namespace OrchardCore.Setup
 {
     public class Startup : StartupBase
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
         public override void ConfigureServices(IServiceCollection services)
         {
             services.AddSetup();
 		}
 
-		public override void Configure(IApplicationBuilder builder, IRouteBuilder routes, IServiceProvider serviceProvider)
+		public override void Configure(IApplicationBuilder app, IRouteBuilder routes, IServiceProvider serviceProvider)
         {
+            var options = serviceProvider.GetService<IOptions<RequestLocalizationOptions>>().Value;
+            var defaultCulture = Configuration["OrchardCore:OrchardCore.Setup:DefaultCulture"];
+            var supportedCultures = Configuration.GetSection("OrchardCore:OrchardCore.Setup:SupportedCultures").Get<string[]>();
+
+            if (!string.IsNullOrEmpty(defaultCulture))
+            {
+                options.SetDefaultCulture(defaultCulture);
+            }
+
+            if (supportedCultures.Length > 0)
+            {
+                var supportedCulture = new[] { options.DefaultRequestCulture.Culture.Name }
+                    .Concat(supportedCultures)
+                    .Distinct()
+                    .ToArray();
+
+                options
+                    .AddSupportedCultures(supportedCultures)
+                    .AddSupportedUICultures(supportedCultures)
+                    ;
+            }
+
+            app.UseRequestLocalization(options);
+
             routes.MapAreaRoute(
                 name: "Setup",
                 areaName: "OrchardCore.Setup",
