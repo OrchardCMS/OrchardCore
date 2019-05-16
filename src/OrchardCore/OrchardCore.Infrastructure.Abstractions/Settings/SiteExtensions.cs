90d493477d1da31f80a5aa362bc8a88824d1c8ef
Jasmin Savard <jasmin.savard@gmail.com>
2019-05-16 18:01:52 -0400

    fix formatting

--- src/OrchardCore/OrchardCore.Infrastructure.Abstractions/Settings/SiteExtensions.cs
+++ src/OrchardCore/OrchardCore.Infrastructure.Abstractions/Settings/SiteExtensions.cs
@@ -4,7 +4,8 @@ namespace OrchardCore.Settings
 {
     public static class SiteExtensions
     {
-        public static string[] GetManageableCultures(this ISite site) {
+        public static string[] GetManageableCultures(this ISite site)
+        {
             return new[] { site.Culture }.Concat(site.SupportedCultures).Distinct().ToArray();
         }
     }
