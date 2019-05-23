d5371c5b9b2044133438bae783fd3675a31e0c77
Carl Woodhouse <carl.woodhouse@gmail.com>
2019-05-23 22:49:20 +0100

    GraphQL fixes where case sensity issues in postgres (#3695)

--- src/OrchardCore/OrchardCore.ContentManagement.GraphQL/Queries/Predicates/PredicateQuery.cs
+++ src/OrchardCore/OrchardCore.ContentManagement.GraphQL/Queries/Predicates/PredicateQuery.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Reflection;
+using OrchardCore.ContentManagement.Records;
 using OrchardCore.Environment.Shell;
 using YesSql;
 
@@ -7,8 +9,9 @@ namespace OrchardCore.ContentManagement.GraphQL.Queries.Predicates
 {
     public class PredicateQuery : IPredicateQuery
     {
-		private readonly HashSet<string> _usedAliases = new HashSet<string>();
-        private readonly IDictionary<string, string> _aliases = new Dictionary<string, string>();
+        private static Dictionary<string, string> _contentItemIndexProperties = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+        private readonly HashSet<string> _usedAliases = new HashSet<string>();
+        private readonly Dictionary<string, string> _aliases = new Dictionary<string, string>();
         private readonly string _tablePrefix;
 
         public PredicateQuery(ISqlDialect dialect, ShellSettings shellSettings)
@@ -22,6 +25,14 @@ namespace OrchardCore.ContentManagement.GraphQL.Queries.Predicates
         public ISqlDialect Dialect { get; set; }
 
         public IDictionary<string, object> Parameters { get; } = new Dictionary<string, object>();
+
+        static PredicateQuery()
+        {
+            foreach(var property in typeof(ContentItemIndex).GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase))
+            {
+                _contentItemIndexProperties[property.Name] = property.Name;
+            }            
+        }
 
         public string NewQueryParameter(object value)
         {
@@ -46,22 +57,26 @@ namespace OrchardCore.ContentManagement.GraphQL.Queries.Predicates
             if (propertyPath == null) throw new ArgumentNullException(nameof(propertyPath));
 
             // Check if there's an alias for the full path
-			// aliasPart.Alias -> AliasFieldIndex.Alias
+            // aliasPart.Alias -> AliasFieldIndex.Alias
             if (_aliases.TryGetValue(propertyPath, out string alias))
             {
                 _usedAliases.Add(alias);
                 return Dialect.QuoteForColumnName(alias);
             }
 
-            var values = propertyPath.Split(new []{'.'}, 2);
+            var values = propertyPath.Split(new[] { '.' }, 2);
             if (values.Length == 1)
             {
                 if (_aliases.TryGetValue(string.Empty, out alias))
                 {
                     // Return the default alias
-                    // ContentItemId -> ContentItemIndex.ContentItemId
-                    _usedAliases.Add(alias);
-                    return Dialect.QuoteForTableName($"{_tablePrefix}{alias}") + "." + Dialect.QuoteForColumnName(values[0]);
+                    // contentItemId -> ContentItemIndex.ContentItemId
+
+                    if (_contentItemIndexProperties.TryGetValue(values[0], out var columnName))
+                    {
+                        _usedAliases.Add(alias);
+                        return Dialect.QuoteForTableName($"{_tablePrefix}{alias}") + "." + Dialect.QuoteForColumnName(columnName);
+                    }
                 }
             }
             else
