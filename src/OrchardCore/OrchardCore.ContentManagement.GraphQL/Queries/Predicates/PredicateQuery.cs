913104336db57ff03decb1e00b2bb0e5651693bf
Sébastien Ros <sebastienros@gmail.com>
2019-05-23 14:36:02 -0700

    Update PredicateQuery.cs

--- src/OrchardCore/OrchardCore.ContentManagement.GraphQL/Queries/Predicates/PredicateQuery.cs
+++ src/OrchardCore/OrchardCore.ContentManagement.GraphQL/Queries/Predicates/PredicateQuery.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using OrchardCore.ContentManagement.Records;
 using OrchardCore.Environment.Shell;
@@ -11,9 +9,9 @@ namespace OrchardCore.ContentManagement.GraphQL.Queries.Predicates
 {
     public class PredicateQuery : IPredicateQuery
     {
-        private static ConcurrentDictionary<string, string> _indexMapping = new ConcurrentDictionary<string, string>();
-		private readonly HashSet<string> _usedAliases = new HashSet<string>();
-        private readonly IDictionary<string, string> _aliases = new Dictionary<string, string>();
+        private static Dictionary<string, string> _contentItemIndexProperties = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+        private readonly HashSet<string> _usedAliases = new HashSet<string>();
+        private readonly Dictionary<string, string> _aliases = new Dictionary<string, string>();
         private readonly string _tablePrefix;
 
         public PredicateQuery(ISqlDialect dialect, ShellSettings shellSettings)
@@ -27,6 +25,14 @@ namespace OrchardCore.ContentManagement.GraphQL.Queries.Predicates
         public ISqlDialect Dialect { get; set; }
 
         public IDictionary<string, object> Parameters { get; } = new Dictionary<string, object>();
+
+        static PredicateQuery()
+        {
+            foreach(var property in typeof(ContentItemIndex).GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase))
+            {
+                _contentItemIndexProperties[property.Name] = property.Name;
+            }            
+        }
 
         public string NewQueryParameter(object value)
         {
@@ -51,32 +57,23 @@ namespace OrchardCore.ContentManagement.GraphQL.Queries.Predicates
             if (propertyPath == null) throw new ArgumentNullException(nameof(propertyPath));
 
             // Check if there's an alias for the full path
-			// aliasPart.Alias -> AliasFieldIndex.Alias
+            // aliasPart.Alias -> AliasFieldIndex.Alias
             if (_aliases.TryGetValue(propertyPath, out string alias))
             {
                 _usedAliases.Add(alias);
                 return Dialect.QuoteForColumnName(alias);
             }
 
-            var values = propertyPath.Split(new []{'.'}, 2);
+            var values = propertyPath.Split(new[] { '.' }, 2);
             if (values.Length == 1)
             {
                 if (_aliases.TryGetValue(string.Empty, out alias))
                 {
                     // Return the default alias
-                    // ContentItemId -> ContentItemIndex.ContentItemId
-                    // Ensures its actually a property of ContentItemIndex and gets the correct casing.
-                    var propertyName = values[0].ToLower();
+                    // contentItemId -> ContentItemIndex.ContentItemId
 
-                    var indexKey = nameof(ContentItemIndex) + propertyName;
-
-                    if (!_indexMapping.TryGetValue(indexKey, out var columnName))
+                    if (_contentItemIndexProperties.TryGetValue(values[0], out var columnName))
                     {
-                        columnName = typeof(ContentItemIndex).GetProperty(propertyName, BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase)?.Name;
-                        _indexMapping.TryAdd(indexKey, columnName);
-                    }
-
-                    if (columnName is object) { 
                         _usedAliases.Add(alias);
                         return Dialect.QuoteForTableName($"{_tablePrefix}{alias}") + "." + Dialect.QuoteForColumnName(columnName);
                     }
