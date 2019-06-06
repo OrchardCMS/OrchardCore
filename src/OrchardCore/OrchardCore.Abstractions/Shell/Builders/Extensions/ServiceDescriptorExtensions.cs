8db81f6d69dfc6b5cf7a37538005bbb444f18078
Sébastien Ros <sebastienros@gmail.com>
2019-06-06 13:01:51 -0700

    Update ServiceDescriptorExtensions.cs

--- src/OrchardCore/OrchardCore.Abstractions/Shell/Builders/Extensions/ServiceDescriptorExtensions.cs
+++ src/OrchardCore/OrchardCore.Abstractions/Shell/Builders/Extensions/ServiceDescriptorExtensions.cs
@@ -13,17 +13,17 @@ namespace OrchardCore.Environment.Shell.Builders
                 return cloned.Parent.GetImplementationType();
             }
 
-            if (descriptor.ImplementationType is object)
+            if (descriptor.ImplementationType != null)
             {
                 return descriptor.ImplementationType;
             }
 
-            if (descriptor.ImplementationInstance is object)
+            if (descriptor.ImplementationInstance != null)
             {
                 return descriptor.ImplementationInstance.GetType();
             }
 
-            if (descriptor.ImplementationFactory is object)
+            if (descriptor.ImplementationFactory != null)
             {
                 return descriptor.ImplementationFactory.GetType().GenericTypeArguments[1];
             }
