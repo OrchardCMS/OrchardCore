f1c1683731a7eaa08e31d471d3fc4e511b2ed295
jtkech <jean-thierry.kechichian@wanadoo.fr>
2019-07-07 01:53:06 +0200

    Fixes building after merging dev.

--- src/OrchardCore/OrchardCore/Modules/Extensions/ServiceCollectionExtensions.cs
+++ src/OrchardCore/OrchardCore/Modules/Extensions/ServiceCollectionExtensions.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Localization;
