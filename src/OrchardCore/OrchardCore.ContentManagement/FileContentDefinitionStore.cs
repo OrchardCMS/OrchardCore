f9d00bd14062d5f8d635a1cba40d102fa0495223
jtkech <jean-thierry.kechichian@wanadoo.fr>
2019-07-21 03:09:25 +0200

    More async

--- src/OrchardCore/OrchardCore.ContentManagement/FileContentDefinitionStore.cs
+++ src/OrchardCore/OrchardCore.ContentManagement/FileContentDefinitionStore.cs
@@ -2,6 +2,7 @@ using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using OrchardCore.ContentManagement.Metadata.Records;
 using OrchardCore.Environment.Shell;
 
@@ -18,25 +19,20 @@ namespace OrchardCore.ContentManagement
             _shellSettings = shellSettings;
         }
 
-        public Task<ContentDefinitionRecord> LoadContentDefinitionAsync()
+        public async Task<ContentDefinitionRecord> LoadContentDefinitionAsync()
         {
-            
-            ContentDefinitionRecord result;
-
             if (!File.Exists(Filename))
             {
-                result = new ContentDefinitionRecord();
-            }
-            else
-            {
-                using (var file = File.OpenText(Filename))
-                {
-                    var serializer = new JsonSerializer();
-                    result = (ContentDefinitionRecord)serializer.Deserialize(file, typeof(ContentDefinitionRecord));
-                }
+                return new ContentDefinitionRecord();
             }
 
-            return Task.FromResult(result);
+            using (var file = File.OpenText(Filename))
+            {
+                using (var reader = new JsonTextReader(file))
+                {
+                    return (await JObject.LoadAsync(reader)).ToObject<ContentDefinitionRecord>();
+                }
+            }
         }
 
         public Task SaveContentDefinitionAsync(ContentDefinitionRecord contentDefinitionRecord)
