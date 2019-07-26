cda6877dec89e3a6914074cb26ca2a5f284362a6
jtkech <jean-thierry.kechichian@wanadoo.fr>
2019-07-26 23:04:28 +0200

    Simpler way to not html encode an 'integrity' attribute.

--- src/OrchardCore/OrchardCore.ResourceManagement.Abstractions/ResourceRequiredContext.cs
+++ src/OrchardCore/OrchardCore.ResourceManagement.Abstractions/ResourceRequiredContext.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace OrchardCore.ResourceManagement
 {
@@ -16,9 +19,27 @@ namespace OrchardCore.ResourceManagement
         {
             var tagBuilder = Resource.GetTagBuilder(Settings, appPath, FileVersionProvider);
 
+            var attributes = new TagHelperAttributeList();
+            foreach (var attribute in tagBuilder.Attributes)
+            {
+                if (attribute.Key != "integrity")
+                {
+                    attributes.Add(attribute.Key, attribute.Value);
+                    continue;
+                }
+
+                attributes.Add(new TagHelperAttribute(attribute.Key, new HtmlString(attribute.Value)));
+            }
+
+            var tagHelperOutput = new TagHelperOutput(tagBuilder.TagName, attributes,
+                (useCachedResult, encoder) => Task.FromResult<TagHelperContent>(null))
+            {
+                TagMode = tagBuilder.TagRenderMode == TagRenderMode.SelfClosing ? TagMode.SelfClosing : TagMode.StartTagAndEndTag
+            };
+
             if (String.IsNullOrEmpty(Settings.Condition))
             {
-                return new TagBuilderWrapper(tagBuilder);
+                return tagHelperOutput;
             }
 
             var builder = new HtmlContentBuilder();
@@ -32,7 +53,7 @@ namespace OrchardCore.ResourceManagement
                 builder.AppendHtml("<!--[if " + Settings.Condition + "]>");
             }
 
-            builder.AppendHtml(new TagBuilderWrapper(tagBuilder));
+            builder.AppendHtml(tagHelperOutput);
 
             if (!string.IsNullOrEmpty(Settings.Condition))
             {
