f1bd00976da3a80592a3bfee6ace1d47c2f115ca
Dean Marcussen <dean.marcussen@gmail.com>
2019-08-28 10:19:03 +0100

    Keep PublicHostName as Obsolete, as removing this can cause hard to diagnose Process Failed to Start errors on IIS, due to how Configure<IOptions<T>> handles missing properties.

--- src/OrchardCore.Modules/OrchardCore.Media.Azure/MediaBlobStorageOptions.cs
+++ src/OrchardCore.Modules/OrchardCore.Media.Azure/MediaBlobStorageOptions.cs
@@ -1,9 +1,13 @@
+using System;
 using OrchardCore.FileStorage.AzureBlob;
 
 namespace OrchardCore.Media.Azure
 {
     public class MediaBlobStorageOptions : BlobStorageOptions
     {
+        [Obsolete("PublicHostName is obsolete. Use MediaOptions.CdnBaseUrl instead.")]
+        public string PublicHostName { get; set; }
+
         /// <summary>
         /// The path in the wwwroot folder containing the Azure Media Blob asset cache.
         /// The tenants name will be appended to this folder path.
