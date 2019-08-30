0f35d806f452aa32251f21a1aa1a2ba8318792d4
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2019-08-30 16:54:21 +0300

    Address feedback

--- src/OrchardCore/OrchardCore.Localization.Abstractions/CultureAspect.cs
+++ src/OrchardCore/OrchardCore.Localization.Abstractions/CultureAspect.cs
@@ -7,9 +7,11 @@ namespace OrchardCore.Localization
     /// </summary>
     public class CultureAspect
     {
+        private static readonly CultureInfo _defaultCulture = CultureInfo.InstalledUICulture ?? CultureInfo.InvariantCulture;
+
         /// <summary>
         /// Gets or sets the culture.
         /// </summary>
-        public CultureInfo Culture { get; set; } = CultureInfo.InstalledUICulture;
+        public CultureInfo Culture { get; set; } = _defaultCulture;
     }
 }
