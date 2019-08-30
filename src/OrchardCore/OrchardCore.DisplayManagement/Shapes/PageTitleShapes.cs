ddffadfcaec55ee40ba3af555bab88fb191d86db
jtkech <jean-thierry.kechichian@wanadoo.fr>
2019-08-30 04:19:33 +0200

    Fixes setup and then unit tests

--- src/OrchardCore/OrchardCore.DisplayManagement/Shapes/PageTitleShapes.cs
+++ src/OrchardCore/OrchardCore.DisplayManagement/Shapes/PageTitleShapes.cs
@@ -2,11 +2,11 @@ using System;
 using System.Threading.Tasks;
 using Fluid;
 using Microsoft.AspNetCore.Html;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.DisplayManagement.Descriptors;
 using OrchardCore.DisplayManagement.Title;
+using OrchardCore.Environment.Shell.Scope;
 using OrchardCore.Liquid;
 using OrchardCore.Settings;
 
@@ -15,17 +15,6 @@ namespace OrchardCore.DisplayManagement.Shapes
 
     public class PageTitleShapes : IShapeAttributeProvider
     {
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        private readonly ISiteService _siteService;
-        private readonly ILiquidTemplateManager _liquidTemplateManager;
-
-        public PageTitleShapes(IHttpContextAccessor httpContextAccessor, ISiteService siteService, ILiquidTemplateManager liquidTemplateManager)
-        {
-            _liquidTemplateManager = liquidTemplateManager;
-            _httpContextAccessor = httpContextAccessor;
-            _siteService = siteService;
-        }
-
         private IPageTitleBuilder _pageTitleBuilder;
         public IPageTitleBuilder Title
         {
@@ -33,7 +22,7 @@ namespace OrchardCore.DisplayManagement.Shapes
             {
                 if (_pageTitleBuilder == null)
                 {
-                    _pageTitleBuilder = _httpContextAccessor.HttpContext.RequestServices.GetRequiredService<IPageTitleBuilder>();
+                    _pageTitleBuilder = ShellScope.Services.GetRequiredService<IPageTitleBuilder>();
                 }
 
                 return _pageTitleBuilder;
@@ -43,7 +32,7 @@ namespace OrchardCore.DisplayManagement.Shapes
         [Shape]
         public async Task<IHtmlContent> PageTitle(IHtmlHelper Html)
         {
-            var siteSettings = await _siteService.GetSiteSettingsAsync();
+            var siteSettings = await ShellScope.Services.GetRequiredService<ISiteService>().GetSiteSettingsAsync();
 
             // We must return a page title so if the format setting is blank just use the current title unformatted
             if (String.IsNullOrWhiteSpace(siteSettings.PageTitleFormat))
@@ -52,7 +41,8 @@ namespace OrchardCore.DisplayManagement.Shapes
             }
             else
             {
-                var result = await _liquidTemplateManager.RenderAsync(siteSettings.PageTitleFormat, System.Text.Encodings.Web.HtmlEncoder.Default, new TemplateContext());
+                var liquidTemplateManager = ShellScope.Services.GetRequiredService<ILiquidTemplateManager>();
+                var result = await liquidTemplateManager.RenderAsync(siteSettings.PageTitleFormat, System.Text.Encodings.Web.HtmlEncoder.Default, new TemplateContext());
                 return new HtmlString(result);
             }
         }
