fefc9ab65be3f0695122ba0e3107c9445671ab86
Sébastien Ros <sebastienros@gmail.com>
2019-08-31 12:02:46 -0700

    Update IShape.cs

--- src/OrchardCore/OrchardCore.DisplayManagement/IShape.cs
+++ src/OrchardCore/OrchardCore.DisplayManagement/IShape.cs
@@ -3,17 +3,17 @@ using OrchardCore.DisplayManagement.Shapes;
 
 namespace OrchardCore.DisplayManagement
 {
-	/// <summary>
-	/// Interface present on dynamic shapes.
-	/// May be used to access attributes in a strongly typed fashion.
-	/// Note: Anything on this interface is a reserved word for the purpose of shape properties
-	/// </summary>
-	public interface IShape
-	{
-		ShapeMetadata Metadata { get; }
-		string Id { get; set; }
-		IList<string> Classes { get; }
-		IDictionary<string, string> Attributes { get; }
+    /// <summary>
+    /// Interface present on dynamic shapes.
+    /// May be used to access attributes in a strongly typed fashion.
+    /// Note: Anything on this interface is a reserved word for the purpose of shape properties
+    /// </summary>
+    public interface IShape
+    {
+        ShapeMetadata Metadata { get; }
+        string Id { get; set; }
+        IList<string> Classes { get; }
+        IDictionary<string, string> Attributes { get; }
         IDictionary<object, object> Properties { get; }
 	}
 }
