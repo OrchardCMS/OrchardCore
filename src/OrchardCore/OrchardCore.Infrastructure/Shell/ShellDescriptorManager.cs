1138e251e490b549b5788cbae89405a3af10722f
jtkech <jean-thierry.kechichian@wanadoo.fr>
2019-09-06 02:33:21 +0200

    Remove 'UpgradeFromBeta2'

--- src/OrchardCore/OrchardCore.Infrastructure/Shell/ShellDescriptorManager.cs
+++ src/OrchardCore/OrchardCore.Infrastructure/Shell/ShellDescriptorManager.cs
@@ -6,7 +6,6 @@ using Dapper;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using OrchardCore.Data;
 using OrchardCore.Environment.Shell.Configuration;
 using OrchardCore.Environment.Shell.Descriptor;
 using OrchardCore.Environment.Shell.Descriptor.Models;
@@ -56,8 +55,6 @@ namespace OrchardCore.Environment.Shell.Data.Descriptors
 
                 if (_shellDescriptor == null)
                 {
-                    // If no descriptor was found, try to update from Beta2
-                    await UpgradeFromBeta2();
                     _shellDescriptor = await _session.Query<ShellDescriptor>().FirstOrDefaultAsync();
                 }
 
@@ -124,47 +121,6 @@ namespace OrchardCore.Environment.Shell.Data.Descriptors
         private class ConfiguredFeatures
         {
             public string[] Features { get; set; } = Array.Empty<string>();
-        }
-
-        private async Task UpgradeFromBeta2()
-        {
-            // TODO: Can be removed when going RC as users are not supposed to go from beta2 to RC
-            // c.f. https://github.com/OrchardCMS/OrchardCore/issues/2439
-
-            var connectionAccessor = _serviceProvider.GetRequiredService<IDbConnectionAccessor>();
-
-            using (var connection = connectionAccessor.CreateConnection())
-            {
-                await connection.OpenAsync();
-
-                using (var transaction = connection.BeginTransaction())
-                {
-                    var dialect = SqlDialectFactory.For(connection);
-                    var tablePrefix = _shellSettings["TablePrefix"];
-
-                    if (!String.IsNullOrEmpty(tablePrefix))
-                    {
-                        tablePrefix += '_';
-                    }
-
-                    var documentTable = dialect.QuoteForTableName($"{tablePrefix}{nameof(Document)}");
-
-                    var oldShellDescriptorType = "OrchardCore.Environment.Shell.Descriptor.Models.ShellDescriptor, OrchardCore.Environment.Shell.Abstractions";
-                    var newShellDescriptorType = "OrchardCore.Environment.Shell.Descriptor.Models.ShellDescriptor, OrchardCore.Abstractions";
-
-                    var updateShellDescriptorCmd = $"UPDATE {documentTable} SET {dialect.QuoteForColumnName(nameof(Document.Type))} = {dialect.GetSqlValue(newShellDescriptorType)} WHERE {dialect.QuoteForColumnName(nameof(Document.Type))} = {dialect.GetSqlValue(oldShellDescriptorType)}";
-
-                    var oldShellStateType = "OrchardCore.Environment.Shell.State.ShellState, OrchardCore.Environment.Shell.Abstractions";
-                    var newShellStateType = "OrchardCore.Environment.Shell.State.ShellState, OrchardCore.Abstractions";
-
-                    var updateShellStateCmd = $"UPDATE {documentTable} SET {dialect.QuoteForColumnName(nameof(Document.Type))} = {dialect.GetSqlValue(newShellStateType)} WHERE {dialect.QuoteForColumnName(nameof(Document.Type))} = {dialect.GetSqlValue(oldShellStateType)}";
-
-                    await connection.ExecuteAsync(updateShellDescriptorCmd, null, transaction);
-                    await connection.ExecuteAsync(updateShellStateCmd, null, transaction);
-
-                    transaction.Commit();
-                }
-            }
         }
     }
 }
