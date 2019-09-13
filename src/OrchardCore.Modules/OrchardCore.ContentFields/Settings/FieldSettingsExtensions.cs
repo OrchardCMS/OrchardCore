5855affe29ce84fb141a3a52b570f563f45626df
Dean Marcussen <dean.marcussen@gmail.com>
2019-09-13 16:18:12 +0100

    Remove Hacky Hint<T>. After testing the existing extension is not very useful either. Not all field settings have hints, and a migration builder should use WithSettings(new TextFieldSettings { Hint = "the hint", Required = true })

--- src/OrchardCore.Modules/OrchardCore.ContentFields/Settings/FieldSettingsExtensions.cs
+++ src/OrchardCore.Modules/OrchardCore.ContentFields/Settings/FieldSettingsExtensions.cs
@@ -5,18 +5,10 @@ namespace OrchardCore.ContentFields.Settings
 {
     public static class FieldSettingsExtensions
     {
-        //TODO maybe just remove now.
-        [Obsolete("Please migrate to use Hint<T>. This will be removed in future versions.")]
+        [Obsolete("Please migrate to use WithSettings<T>. This will be removed in future versions.")]
         public static ContentPartFieldDefinitionBuilder Hint(this ContentPartFieldDefinitionBuilder builder, string hint)
         {
             return builder.WithSetting(nameof(Hint), hint);
-        }
-
-        //HACK. do better. settings hint base class? Probably
-        public static ContentPartFieldDefinitionBuilder Hint<T>(this ContentPartFieldDefinitionBuilder builder, string hint)
-            where T: class, new()
-        {
-            return builder.MergeSettings<T>((new T() as dynamic).Hint = hint);
         }
     }
 }
