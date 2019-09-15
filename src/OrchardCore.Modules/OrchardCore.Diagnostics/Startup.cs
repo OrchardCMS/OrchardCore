18300d4003dae3dad53f6c9a9e6368491f0a5cfd
Jean-Thierry Kéchichian <jean-thierry.kechichian@wanadoo.fr>
2019-09-15 02:21:03 +0200

    Diagnostics ISartupFilter to add a middleware before the routing one (#4264)
    * ConfigureBeforeRouting.
    
    * Use "IStartupFilter" in place of "ConfigureBeforeRouting".
    
    * emove testing code
    
    * React to review, use inline variable

--- src/OrchardCore.Modules/OrchardCore.Diagnostics/Startup.cs
+++ src/OrchardCore.Modules/OrchardCore.Diagnostics/Startup.cs
@@ -1,18 +1,15 @@
 using System;
-using System.Linq;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
-using OrchardCore.Modules;
 
 namespace OrchardCore.Diagnostics
 {
-    public class Startup : StartupBase
+    public class Startup : Modules.StartupBase
     {
         private readonly FileExtensionContentTypeProvider _contentTypeProvider = new FileExtensionContentTypeProvider();
 
@@ -23,58 +20,19 @@ namespace OrchardCore.Diagnostics
             _hostingEnvironment = hostingEnvironment;
         }
 
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.TryAddEnumerable(ServiceDescriptor.Singleton<IStartupFilter, DiagnosticsStartupFilter>());
+        }
+
         public override void Configure(IApplicationBuilder app, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
         {
-            if (!_hostingEnvironment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-            }
-
-            // c.f. https://docs.asp.net/en/latest/fundamentals/error-handling.html#id3
-            app.UseStatusCodePagesWithReExecute("/Error", "?status={0}");
-
-            app.Use(async (context, next) =>
-            {
-                await next();
-
-                if (context.Response.StatusCode < 200 || context.Response.StatusCode >= 400)
-                {
-                    string contentType;
-                    if (_contentTypeProvider.TryGetContentType(context.Request.Path, out contentType))
-                    {
-                        var statusCodePagesFeature = context.Features.Get<IStatusCodePagesFeature>();
-                        if (statusCodePagesFeature != null)
-                        {
-                            statusCodePagesFeature.Enabled = false;
-                        }
-                    }
-                    else
-                    {
-                        // Workaround of c.f. https://github.com/aspnet/AspNetCore/issues/11555
-                        var endpointDataSource = context.RequestServices.GetRequiredService<EndpointDataSource>();
-
-                        var endpoint = endpointDataSource.Endpoints
-                            .Where(e => e.Metadata.GetMetadata<RouteNameMetadata>().RouteName == "Diagnostics.Error")
-                            .FirstOrDefault();
-
-                        context.SetEndpoint(endpoint);
-
-                        var routeValues = endpoint.Metadata.GetMetadata<ControllerActionDescriptor>().RouteValues;
-                        context.Request.RouteValues = new RouteValueDictionary(routeValues);
-                    }
-                }
-            });
-
             routes.MapAreaControllerRoute(
                 name: "Diagnostics.Error",
                 areaName: "OrchardCore.Diagnostics",
                 pattern: "Error/{status?}",
                 defaults: new { controller = "Diagnostics", action = "Error" }
             );
-        }
-
-        public override void ConfigureServices(IServiceCollection services)
-        {
         }
     }
 }
