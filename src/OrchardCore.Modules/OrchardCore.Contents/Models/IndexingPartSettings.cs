893b6e41e1e7a6030e0d4ec0c8335c00e0dff3cd
Jasmin Savard <jasmin.savard@gmail.com>
2019-09-19 11:00:44 -0400

    Fix default values for IndexingPartSettings

--- src/OrchardCore.Modules/OrchardCore.Contents/Models/IndexingPartSettings.cs
+++ src/OrchardCore.Modules/OrchardCore.Contents/Models/IndexingPartSettings.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+
 namespace OrchardCore.Contents.Models
 {
     public class IndexingPartSettings
@@ -5,8 +7,10 @@ namespace OrchardCore.Contents.Models
         //TODO don't index full content item #3809
         public bool IsNotIndexingFullTextOrAll { get; set; }
 
-        public bool IndexBodyAspect { get; set; }
+        [DefaultValue(true)]
+        public bool IndexBodyAspect { get; set; } = true;
 
-        public bool IndexDisplayText { get; set; }
+        [DefaultValue(true)]
+        public bool IndexDisplayText { get; set; } = true;
     }
 }
