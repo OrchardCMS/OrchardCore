80a585889aa9eb014a53662dd43b63d593a0dae2
Jean-Philippe Tissot <jeanphilippe.tissot@gmail.com>
2019-09-21 21:40:54 -0400

    Add contentitem tag helper and liquid tag

---
+++ src/OrchardCore.Modules/OrchardCore.Contents/TagHelpers/ContentItemTagHelper.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using OrchardCore.DisplayManagement;
+using OrchardCore.DisplayManagement.TagHelpers;
+
+namespace OrchardCore.Contents.TagHelpers
+{
+    [HtmlTargetElement("contentitem")]
+    [HtmlTargetElement("contentitem")]
+    public class ContentItemTagHelper : BaseShapeTagHelper
+    {
+        public ContentItemTagHelper(IShapeFactory shapeFactory, IDisplayHelper displayHelper)
+            : base(shapeFactory, displayHelper)
+        {
+            Type = "ContentItem";
+        }
+    }
+}
