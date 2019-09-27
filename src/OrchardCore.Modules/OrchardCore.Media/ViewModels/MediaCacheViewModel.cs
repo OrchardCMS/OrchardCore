1dea3b6f7d9594abf9557ec7c3047c190d1368e4
Dean Marcussen <dean.marcussen@gmail.com>
2019-09-27 16:32:09 +0100

    Resolve IMediaFileStoreCache from service provider, in case the Azure Feature is disabled, or not configured. Provide error message to that effect.

---
+++ src/OrchardCore.Modules/OrchardCore.Media/ViewModels/MediaCacheViewModel.cs
@@ -0,0 +1,11 @@
+using OrchardCore.ContentManagement;
+using OrchardCore.ContentManagement.Metadata.Models;
+using OrchardCore.Media.Fields;
+
+namespace OrchardCore.Media.ViewModels
+{
+    public class MediaCacheViewModel
+    {
+        public bool IsConfigured { get; set;}
+    }
+}
