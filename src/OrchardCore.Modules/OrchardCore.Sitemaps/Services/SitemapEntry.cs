527ac1c918a173815add80d399a7a6fa1fafde55
Dean Marcussen <dean.marcussen@gmail.com>
2019-09-30 07:26:17 +0100

    Cleanup

--- src/OrchardCore.Modules/OrchardCore.Sitemaps/Services/SitemapEntry.cs
+++ src/OrchardCore.Modules/OrchardCore.Sitemaps/Services/SitemapEntry.cs
@@ -3,7 +3,6 @@ namespace OrchardCore.Sitemaps.Services
     public struct SitemapEntry
     {
         public string SitemapNodeId;
-        //TODO PathString I think is better
         public string Path;
     }
 }
--- src/OrchardCore.Modules/OrchardCore.Sitemaps/ViewModels/SitemapNodeListViewModel.cs
+++ src/OrchardCore.Modules/OrchardCore.Sitemaps/ViewModels/SitemapNodeListViewModel.cs
@@ -6,8 +6,6 @@ namespace OrchardCore.Sitemaps.ViewModels
     public class SitemapNodeListViewModel
     {
         public SitemapSet SitemapSet { get; set; }
-
-        // TODO probably don't need this (was it for icons?)
         public IDictionary<string, dynamic> Thumbnails { get; set; }
     }
 }
