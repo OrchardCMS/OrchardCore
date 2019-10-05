17ece29edefb90005428bc04371f7b2a9fa0480e
Dean Marcussen <dean.marcussen@gmail.com>
2019-10-05 07:52:52 +0100

    WIP. Move IShapeExtensions back to DisplayManagement. Still w/testing code in, to see how useful this is

--- src/OrchardCore/OrchardCore.DisplayManagement/IShape.cs
+++ src/OrchardCore/OrchardCore.DisplayManagement/IShape.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
-using System.Text;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using OrchardCore.DisplayManagement.Shapes;
 
@@ -18,15 +19,64 @@ namespace OrchardCore.DisplayManagement
         IList<string> Classes { get; }
         IDictionary<string, string> Attributes { get; }
         IDictionary<string, object> Properties { get; }
-	}
+    }
 
-    //public static class IShapeExtensions
-    //{
-    //    public static JObject ShapeDump(this IShape shape)
-    //    {
-    //        var jObject = new JObject();
-    //        jObject.Add("ShapeType", JToken.FromObject(shape.Metadata.DisplayType));
-    //        return jObject;
-    //    }
-    //}
+
+    public static class IShapeExtensions
+    {
+        public static readonly JsonSerializer ShapeSerializer = new JsonSerializer
+        {
+            DefaultValueHandling = DefaultValueHandling.Ignore
+        };
+
+        public static JObject ShapeDump(this IShape shape)
+        {
+            if (shape == null)
+            {
+                return new JObject();
+            }
+
+            var jObject = new JObject();
+            var metadata = JObject.FromObject(shape.Metadata, ShapeSerializer);
+            if (metadata.HasValues)
+            {
+                jObject.Add("Metadata", metadata);
+            }
+
+            if (shape.Classes != null && shape.Classes.Any())
+            {
+                jObject.Add(nameof(shape.Classes), JArray.FromObject(shape.Classes, ShapeSerializer));
+            }
+            if (shape.Attributes != null && shape.Attributes.Any())
+            {
+                jObject.Add("Attributes", JObject.FromObject(shape.Attributes, ShapeSerializer));
+            }
+            if (shape.Properties != null && shape.Properties.Any())
+            {
+                jObject.Add("Properties", JObject.FromObject(shape.Properties, ShapeSerializer));
+            }
+
+            var actualShape = shape as Shape;
+            if (actualShape != null && actualShape.HasItems)
+            {
+                var items = new JArray();
+                // Because items can be mutated during shape execution.
+                var shapeItems = actualShape.Items.ToImmutableArray();
+                foreach (IShape item in shapeItems)
+                {
+                    var itemResult = item.ShapeDump();
+                    if (itemResult.HasValues)
+                    {
+                        items.Add(itemResult);
+                    }
+                }
+                if (items.HasValues)
+                {
+                    jObject.Add("Items", items);
+                }
+            }
+
+            return jObject;
+        }
+    }
 }
