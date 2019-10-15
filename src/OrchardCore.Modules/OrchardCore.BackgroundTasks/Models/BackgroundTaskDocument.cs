c73d921b6edda5f8c92690e517b3957c34545907
jtkech <jean-thierry.kechichian@wanadoo.fr>
2019-10-16 00:52:52 +0200

    Minor change to restart the CI

--- src/OrchardCore.Modules/OrchardCore.BackgroundTasks/Models/BackgroundTaskDocument.cs
+++ src/OrchardCore.Modules/OrchardCore.BackgroundTasks/Models/BackgroundTaskDocument.cs
@@ -4,7 +4,7 @@ namespace OrchardCore.BackgroundTasks.Models
 {
     public class BackgroundTaskDocument
     {
-        public int Id { get; set; } // An identifier so that updates don't create new documents
+        public int Id { get; set; } // An identifier so that updates don't create new documents.
 
         public ImmutableDictionary<string, BackgroundTaskSettings> Settings { get; set; } = ImmutableDictionary.Create<string, BackgroundTaskSettings>();
     }
