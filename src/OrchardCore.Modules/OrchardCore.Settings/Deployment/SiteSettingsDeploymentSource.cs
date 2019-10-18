28bd5a9c5e7c399071288673d858b346545cf51a
Dean Marcussen <dean.marcussen@gmail.com>
2019-10-18 20:11:27 +0100

    Fix SiteSettings defaults (#4565)

--- src/OrchardCore.Modules/OrchardCore.Settings/Deployment/SiteSettingsDeploymentSource.cs
+++ src/OrchardCore.Modules/OrchardCore.Settings/Deployment/SiteSettingsDeploymentSource.cs
@@ -62,6 +62,10 @@ namespace OrchardCore.Settings.Deployment
                         data.Add(new JProperty(nameof(ISite.SiteName), site.SiteName));
                         break;
 
+                    case "PageTitleFormat":
+                        data.Add(new JProperty(nameof(ISite.PageTitleFormat), site.PageTitleFormat));
+                        break;
+
                     case "SiteSalt":
                         data.Add(new JProperty(nameof(ISite.SiteSalt), site.SiteSalt));
                         break;
@@ -76,6 +80,14 @@ namespace OrchardCore.Settings.Deployment
 
                     case "UseCdn":
                         data.Add(new JProperty(nameof(ISite.UseCdn), site.UseCdn));
+                        break;
+
+                    case "CdnBaseUrl":
+                        data.Add(new JProperty(nameof(ISite.CdnBaseUrl), site.CdnBaseUrl));
+                        break;
+
+                    case "AppendVersion":
+                        data.Add(new JProperty(nameof(ISite.AppendVersion), site.AppendVersion));
                         break;
 
                     case "HomeRoute":
