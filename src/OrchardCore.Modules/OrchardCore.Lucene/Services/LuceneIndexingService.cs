41ffb408006ab5d34b635194fbe71005f2a74fb3
Jasmin Savard <jasmin.savard@gmail.com>
2019-10-25 00:09:09 -0400

    Add missing using

--- src/OrchardCore.Modules/OrchardCore.Lucene/Services/LuceneIndexingService.cs
+++ src/OrchardCore.Modules/OrchardCore.Lucene/Services/LuceneIndexingService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
