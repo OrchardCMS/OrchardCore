4a99ffcc4860cd5a87681776c28a55b15551c234
jtkech <jean-thierry.kechichian@wanadoo.fr>
2019-11-02 03:19:13 +0100

    Fixes attribute name conflicts in BaseShapeTagHelper

--- src/OrchardCore/OrchardCore.DisplayManagement/TagHelpers/BaseShapeTagHelper.cs
+++ src/OrchardCore/OrchardCore.DisplayManagement/TagHelpers/BaseShapeTagHelper.cs
@@ -12,7 +12,7 @@ namespace OrchardCore.DisplayManagement.TagHelpers
     {
         private static readonly HashSet<string> InternalProperties = new HashSet<string>
         {
-            "id", "type", "cache-id", "cache-context", "cache-dependency", "cache-tag", "cache-fixed-duration", "cache-sliding-duration"
+            "id", "alternate", "wrapper", "cache-id", "cache-context", "cache-tag", "cache-fixed-duration", "cache-sliding-duration"
         };
 
         protected const string PropertyPrefix = "prop-";
@@ -24,11 +24,11 @@ namespace OrchardCore.DisplayManagement.TagHelpers
         protected IDisplayHelper _displayHelper;
 
         public string Type { get; set; }
-        public string Cache { get; set; }
-        public TimeSpan? FixedDuration { get; set; }
-        public TimeSpan? SlidingDuration { get; set; }
-        public string Context { get; set; }
-        public string Tag { get; set; }
+        internal string Cache { get; set; }
+        internal TimeSpan? FixedDuration { get; set; }
+        internal TimeSpan? SlidingDuration { get; set; }
+        internal string Context { get; set; }
+        internal string Tag { get; set; }
 
         [HtmlAttributeNotBound]
         [ViewContext]
@@ -51,7 +51,7 @@ namespace OrchardCore.DisplayManagement.TagHelpers
             var properties = new Dictionary<string, object>();
 
             // These prefixed properties are bound with their original type and not converted as IHtmlContent
-            foreach(var property in Properties)
+            foreach (var property in Properties)
             {
                 var normalizedName = property.Key.ToPascalCaseDash();
                 properties.Add(normalizedName, property.Value);
