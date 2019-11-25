376e7b0f346ebfaccbfe5441a37da32f4892adc7
Matthijs Krempel <matthijs.krempel@hotmail.com>
2019-11-25 13:23:47 +0100

    Potential fix for disabeling core modules (#4259)

--- src/OrchardCore/OrchardCore.Abstractions/Extensions/Features/IFeatureInfo.cs
+++ src/OrchardCore/OrchardCore.Abstractions/Extensions/Features/IFeatureInfo.cs
@@ -10,5 +10,7 @@ namespace OrchardCore.Environment.Extensions.Features
         bool DefaultTenantOnly { get; }
         IExtensionInfo Extension { get; }
         string[] Dependencies { get; }
+
+        bool IsAlwaysEnabled { get; }
     }
 }
