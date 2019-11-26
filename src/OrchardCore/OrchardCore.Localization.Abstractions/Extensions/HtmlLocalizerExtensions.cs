c99c087c5dba0f32a80dfb076d44daef9c60a693
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2019-11-26 03:10:49 +0300

    the -> The

--- src/OrchardCore/OrchardCore.Localization.Abstractions/Extensions/HtmlLocalizerExtensions.cs
+++ src/OrchardCore/OrchardCore.Localization.Abstractions/Extensions/HtmlLocalizerExtensions.cs
@@ -12,7 +12,7 @@ namespace Microsoft.AspNetCore.Mvc.Localization
         /// Gets the pluralization form.
         /// </summary>
         /// <param name="localizer">The <see cref="IHtmlLocalizer"/>.</param>
-        /// <param name="count">the number to be used for selecting the pluralization form.</param>
+        /// <param name="count">The number to be used for selecting the pluralization form.</param>
         /// <param name="singular">The singular form key.</param>
         /// <param name="plural">The plural form key.</param>
         /// <param name="arguments">The parameters used in the key.</param>
@@ -30,7 +30,7 @@ namespace Microsoft.AspNetCore.Mvc.Localization
         /// Gets the pluralization form.
         /// </summary>
         /// <param name="localizer">The <see cref="IHtmlLocalizer"/>.</param>
-        /// <param name="count">the number to be used for selecting the pluralization form.</param>
+        /// <param name="count">The number to be used for selecting the pluralization form.</param>
         /// <param name="pluralForms">A list of pluralization forms.</param>
         /// <param name="arguments">The parameters used in the key.</param>
         public static LocalizedHtmlString Plural(this IHtmlLocalizer localizer, int count, string[] pluralForms, params object[] arguments)
--- src/OrchardCore/OrchardCore.Localization.Abstractions/Extensions/StringLocalizerExtensions.cs
+++ src/OrchardCore/OrchardCore.Localization.Abstractions/Extensions/StringLocalizerExtensions.cs
@@ -12,7 +12,7 @@ namespace Microsoft.Extensions.Localization
         /// Gets the pluralization form.
         /// </summary>
         /// <param name="localizer">The <see cref="IStringLocalizer"/>.</param>
-        /// <param name="count">the number to be used for selecting the pluralization form.</param>
+        /// <param name="count">The number to be used for selecting the pluralization form.</param>
         /// <param name="singular">The singular form key.</param>
         /// <param name="plural">The plural form key.</param>
         /// <param name="arguments">The parameters used in the key.</param>
@@ -30,7 +30,7 @@ namespace Microsoft.Extensions.Localization
         /// Gets the pluralization form.
         /// </summary>
         /// <param name="localizer">The <see cref="IStringLocalizer"/>.</param>
-        /// <param name="count">the number to be used for selecting the pluralization form.</param>
+        /// <param name="count">The number to be used for selecting the pluralization form.</param>
         /// <param name="pluralForms">A list of pluralization forms.</param>
         /// <param name="arguments">The parameters used in the key.</param>
         public static LocalizedString Plural(this IStringLocalizer localizer, int count, string[] pluralForms, params object[] arguments)
