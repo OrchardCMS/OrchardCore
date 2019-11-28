00f5df8ba382680b8ec208f89e4561dce2bb78ac
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2019-11-29 00:16:46 +0300

    Add email docs

--- src/OrchardCore/OrchardCore.Email.Abstractions/ISmtpService.cs
+++ src/OrchardCore/OrchardCore.Email.Abstractions/ISmtpService.cs
@@ -2,11 +2,16 @@ using System.Threading.Tasks;
 
 namespace OrchardCore.Email
 {
+    /// <summary>
+    /// Represents a contract for SMTP service.
+    /// </summary>
     public interface ISmtpService
     {
         /// <summary>
         /// Sends the specified message to an SMTP server for delivery.
         /// </summary>
+        /// <param name="message">The message to be sent.</param>
+        /// <returns>A <see cref="SmtpResult"/> that hold information about the sent message, for intsnace if it's sent successfully or it fails,</returns>
         Task<SmtpResult> SendAsync(MailMessage message);
     }
 }
