eb7c3b2b5d7ec1e66ab4ad374130fae0064ef911
Duncan <duncan.hoggan@nologostudios.com>
2019-12-02 16:50:22 +0200

    Updated a summary

--- src/OrchardCore.Modules/OrchardCore.Contents/Workflows/ViewModels/RetrieveContentTaskViewModel.cs
+++ src/OrchardCore.Modules/OrchardCore.Contents/Workflows/ViewModels/RetrieveContentTaskViewModel.cs
@@ -5,7 +5,7 @@ namespace OrchardCore.Contents.Workflows.ViewModels
     public class RetrieveContentTaskViewModel : ContentTaskViewModel<RetrieveContentTask>
     {
         /// <summary>
-        /// The expression resulting into a content item or content item ID to unpublish.
+        /// The expression resulting into a content item ID to retrieve.
         /// </summary>
         public string ContentItemId { get; set; }
     }
