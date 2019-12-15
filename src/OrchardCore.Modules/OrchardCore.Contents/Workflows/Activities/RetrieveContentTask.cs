a00f31851a8fb9d3c01c4e3120ccad55b2e57550
Duncan Hoggan <duncan.hoggan@gmail.com>
2019-12-15 12:48:00 +0200

    Workflow Content Item ID functionality (#4936)

---
+++ src/OrchardCore.Modules/OrchardCore.Contents/Workflows/Activities/RetrieveContentTask.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Localization;
+using OrchardCore.ContentManagement;
+using OrchardCore.Workflows.Abstractions.Models;
+using OrchardCore.Workflows.Activities;
+using OrchardCore.Workflows.Models;
+using OrchardCore.Workflows.Services;
+
+namespace OrchardCore.Contents.Workflows.Activities
+{
+    public class RetrieveContentTask : ContentTask
+    {
+        public RetrieveContentTask(IContentManager contentManager, IWorkflowScriptEvaluator scriptEvaluator, IStringLocalizer<RetrieveContentTask> localizer) : base(contentManager, scriptEvaluator, localizer)
+        {
+        }
+
+        public override string Name => nameof(RetrieveContentTask);
+        public override LocalizedString DisplayText => T["Retrieve Content Task"];
+        public override LocalizedString Category => T["Content"];
+
+        public override IEnumerable<Outcome> GetPossibleOutcomes(WorkflowExecutionContext workflowContext, ActivityContext activityContext)
+        {
+            return Outcomes(T["Retrieved"]);
+        }
+
+        public override async Task<ActivityExecutionResult> ExecuteAsync(WorkflowExecutionContext workflowContext, ActivityContext activityContext)
+        {
+            var contentItemId = await GetContentItemIdAsync(workflowContext);
+            var contentItem = await ContentManager.GetAsync(contentItemId, VersionOptions.Latest);
+            workflowContext.LastResult = contentItem;
+            return Outcomes("Retrieved");
+        }
+    }
+}
