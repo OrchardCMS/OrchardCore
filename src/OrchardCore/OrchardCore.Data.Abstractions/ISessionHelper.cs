24fc5de8e1c54aa8d8b0de06552c8ba45da96e8d
Antoine Griffard <agriffard@hotmail.com>
2019-12-15 15:35:46 +0100

    typo

--- src/OrchardCore/OrchardCore.Data.Abstractions/ISessionHelper.cs
+++ src/OrchardCore/OrchardCore.Data.Abstractions/ISessionHelper.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 namespace OrchardCore.Data
 {
     /// <summary>
-    /// Represents a contract that provides a helper methods for <see cref="YesSql.ISession"/>.
+    /// Represents a contract that provides helper methods for <see cref="YesSql.ISession"/>.
     /// </summary>
     public interface ISessionHelper
     {
@@ -20,7 +20,7 @@ namespace OrchardCore.Data
         /// Gets a single document (or create a new one) for caching and that should not be updated.
         /// For a full isolation, it needs to be used in pair with <see cref="LoadForUpdateAsync"/>.
         /// </summary>
-        /// <typeparam name="T">The.</typeparam>
+        /// <typeparam name="T"></typeparam>
         /// <param name="factory">A factory method to get or create a document for caching.</param>
         /// <returns></returns>
         Task<T> GetForCachingAsync<T>(Func<T> factory = null) where T : class, new();
