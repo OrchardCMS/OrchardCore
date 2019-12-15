85b163b8a88b05a456d8bf4053183fabfceb3326
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2019-12-15 18:30:56 +0300

    Add users docs

--- src/OrchardCore/OrchardCore.Users.Abstractions/Events/IPasswordRecoveryFormEvents.cs
+++ src/OrchardCore/OrchardCore.Users.Abstractions/Events/IPasswordRecoveryFormEvents.cs
@@ -5,14 +5,31 @@ using System.Threading.Tasks;
 
 namespace OrchardCore.Users.Events
 {
+    /// <summary>
+    /// Contract for password recovery events.
+    /// </summary>
     public interface IPasswordRecoveryFormEvents
     {
+        /// <summary>
+        /// Occures during the user password resetting.
+        /// </summary>
+        /// <param name="reportError">The reported error if fauilar happened during the ressting process.</param>
         Task ResettingPasswordAsync(Action<string, string> reportError);
 
+        /// <summary>
+        /// Occures after the user password has been reset.
+        /// </summary>
         Task PasswordResetAsync();
 
+        /// <summary>
+        /// Occures during the user password recovery.
+        /// </summary>
+        /// <param name="reportError">The reported error if fauilar happened during the recovery process.</param>
         Task RecoveringPasswordAsync(Action<string, string> reportError);
 
+        /// <summary>
+        /// Occures after the user password has been recovered.
+        /// </summary>
         Task PasswordRecoveredAsync();
     }
 }
--- src/OrchardCore/OrchardCore.Users.Abstractions/Handlers/IExternalLoginEventHandler.cs
+++ src/OrchardCore/OrchardCore.Users.Abstractions/Handlers/IExternalLoginEventHandler.cs
@@ -5,12 +5,22 @@ using System.Threading.Tasks;
 namespace OrchardCore.Users.Handlers
 {
     /// <summary>
-    /// Called when a tenant is set up.
+    /// Contract for external login events, this occurs when a tenant is set up.
     /// </summary>
     public interface IExternalLoginEventHandler
     {
+        /// <summary>
+        /// Occurs when the username is generated.
+        /// </summary>
+        /// <param name="provider">The login provider.</param>
+        /// <param name="claims">A <see cref="SerializableClaim"/>s.</param>
+        /// <return>A username.</return>
         Task<string> GenerateUserName(string provider, IEnumerable<SerializableClaim> claims);
 
+        /// <summary>
+        /// Occurs when the user roles has been updated.
+        /// </summary>
+        /// <param name="context">The <see cref="UpdateRolesContext"/>.</param>
         Task UpdateRoles(UpdateRolesContext context);
     }
 }
