dcc5ebe934b5155d5540425182af67bf519c2fe1
Jasmin Savard <jasmin.savard@gmail.com>
2019-12-20 20:33:03 -0500

    Cleanup code

--- src/OrchardCore.Modules/OrchardCore.Lucene/Services/LuceneIndexSettingsService.cs
+++ src/OrchardCore.Modules/OrchardCore.Lucene/Services/LuceneIndexSettingsService.cs
@@ -1,11 +1,6 @@
-using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.IO;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
-using OrchardCore.Environment.Shell;
 using OrchardCore.Lucene.Model;
 using YesSql;
 
