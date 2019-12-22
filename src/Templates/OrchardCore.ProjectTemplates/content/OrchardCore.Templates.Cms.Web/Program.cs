d559cf3dae9453606e89d69079caf11664eac604
Michael Petrinolis <michael.petrinolis@outlook.com>
2019-12-22 12:08:07 +0200

    make UseSerilogWeb obsolete make UseSerilogTenantNameLoggingMiddleware add UseSerilogTenantNameLogging instead remove deprecated Serilog.RollingFile modify readme.md to show up to date info only keep dependency to Serilog.AspnetCore update program.cs and startup.cs in template

--- src/Templates/OrchardCore.ProjectTemplates/content/OrchardCore.Templates.Cms.Web/Program.cs
+++ src/Templates/OrchardCore.ProjectTemplates/content/OrchardCore.Templates.Cms.Web/Program.cs
@@ -11,50 +11,20 @@ namespace OrchardCore.Templates.Cms.Web
 {
     public class Program
     {
-#if (UseNLog)
         public static Task Main(string[] args)
             => BuildHost(args).RunAsync();
-#else
-        public static async Task Main(string[] args)
-        {
-            Log.Logger = new LoggerConfiguration()
-                .Enrich.FromLogContext()
-                .WriteTo.Console()
-                .CreateLogger();
 
-            try
-            {
-                Log.Information("Orchard starting up");
-                await BuildHost(args).RunAsync();
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "Orchard start-up failed");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
-        }
-#endif
-        public static IHost BuildHost(string[] args)
-        {
-            var host = Host.CreateDefaultBuilder(args)
+        public static IHost BuildHost(string[] args) =>
+            Host.CreateDefaultBuilder(args)
                 .ConfigureLogging(logging => logging.ClearProviders())
 #if (UseSerilog)
                 .UseSerilog()
 #endif
                 .ConfigureWebHostDefaults(webBuilder => webBuilder
 #if (UseNLog)
-                        .UseNLogWeb()
+                    .UseNLogWeb()
 #endif
-#if (UseSerilog)
-                        .UseSerilogWeb()
-#endif
-                        .UseStartup<Startup>())
-                .Build();
-
-            return host;
-        }
+                    .UseStartup<Startup>()
+                ).Build();
     }
 }
