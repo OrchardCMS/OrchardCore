3dd1c0955990002c846752beb64b9c3ee9016aeb
Jean-Philippe Tissot <4681586+jptissot@users.noreply.github.com>
2019-12-23 15:15:21 -0500

    Fixing LocalizationSet cloning (#5117)
    LocalizationSet was not being cleared when cloning so a duplicate entry
    would be created for a locale when cloning

--- src/OrchardCore/OrchardCore.ContentManagement.Abstractions/Handlers/CloneContentContext.cs
+++ src/OrchardCore/OrchardCore.ContentManagement.Abstractions/Handlers/CloneContentContext.cs
@@ -3,6 +3,7 @@ namespace OrchardCore.ContentManagement.Handlers
     public class CloneContentContext : ContentContextBase
     {
         public ContentItem CloneContentItem { get; set; }
+
         public CloneContentContext(ContentItem contentItem, ContentItem cloneContentItem)
             : base(contentItem)
         {
