3f25e75996971ca221f6fd85677e14633a1bd297
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2019-12-24 17:10:15 +0300

    Remove NullLocalizer & NullLocalizer<T> Stubs

--- test/OrchardCore.Tests/DisplayManagement/DefaultDisplayManagerTests.cs
+++ test/OrchardCore.Tests/DisplayManagement/DefaultDisplayManagerTests.cs
@@ -10,6 +10,7 @@ using OrchardCore.DisplayManagement.Implementation;
 using OrchardCore.DisplayManagement.Shapes;
 using OrchardCore.DisplayManagement.Theming;
 using OrchardCore.Environment.Extensions;
+using OrchardCore.Localization;
 using OrchardCore.Tests.Stubs;
 using Xunit;
 
@@ -38,7 +39,9 @@ namespace OrchardCore.Tests.DisplayManagement
             serviceCollection.AddScoped<IShapeBindingResolver, TestShapeBindingResolver>();
             serviceCollection.AddScoped<IShapeDisplayEvents, TestDisplayEvents>();
             serviceCollection.AddScoped<IExtensionManager, StubExtensionManager>();
-            serviceCollection.AddScoped<IStringLocalizer<DefaultHtmlDisplay>, NullStringLocalizer<DefaultHtmlDisplay>>();
+            serviceCollection.AddSingleton<IStringLocalizerFactory, NullStringLocalizerFactory>();
+            serviceCollection.AddTransient(typeof(IStringLocalizer<>), typeof(StringLocalizer<>));
+
             serviceCollection.AddLogging();
 
             serviceCollection.AddSingleton(_defaultShapeTable);
