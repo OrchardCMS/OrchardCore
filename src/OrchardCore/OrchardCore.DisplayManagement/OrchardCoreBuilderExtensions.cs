edeed1d4df9c17e5b83269f9870bd6cff2a317da
Dean Marcussen <dean.marcussen@gmail.com>
2019-12-27 00:54:04 +0000

    Liquid console_log filter and docs (#5060)

--- src/OrchardCore/OrchardCore.DisplayManagement/OrchardCoreBuilderExtensions.cs
+++ src/OrchardCore/OrchardCore.DisplayManagement/OrchardCoreBuilderExtensions.cs
@@ -83,7 +83,6 @@ namespace Microsoft.Extensions.DependencyInjection
 
                     services.AddShapeAttributes<DateTimeShapes>();
                     services.AddShapeAttributes<PageTitleShapes>();
-                    services.AddShapeAttributes<ConsoleLogWrapperShapes>();
 
                     services.AddTagHelpers<AddAlternateTagHelper>();
                     services.AddTagHelpers<AddClassTagHelper>();
