38b7a54063bcd745b6c31f9a7716b075fe768589
Dean Marcussen <dean.marcussen@gmail.com>
2019-12-28 10:56:27 +0000

    Remove obsolete property as configuration is now through IConfigureOptions so will not cause startup error due to unmappable json

--- src/OrchardCore.Modules/OrchardCore.Media.Azure/MediaBlobStorageOptions.cs
+++ src/OrchardCore.Modules/OrchardCore.Media.Azure/MediaBlobStorageOptions.cs
@@ -5,9 +5,6 @@ namespace OrchardCore.Media.Azure
 {
     public class MediaBlobStorageOptions : BlobStorageOptions
     {
-        [Obsolete("PublicHostName is obsolete. Use MediaOptions.CdnBaseUrl instead.")]
-        public string PublicHostName { get; set; }
-
         /// <summary>
         /// Create blob container on startup if it does not exist.
         /// </summary>
