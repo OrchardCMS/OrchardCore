5ceb3a1ac22fb41e6d94b6553040fe932f4054a3
yiannis kyriazidis <giannkyrias@gmail.com>
2020-01-02 05:13:21 +0200

    register services in correct module. (#5178)

--- src/OrchardCore.Modules/OrchardCore.Templates/Startup.cs
+++ src/OrchardCore.Modules/OrchardCore.Templates/Startup.cs
@@ -47,9 +47,6 @@ namespace OrchardCore.Templates
             services.AddTransient<IDeploymentSource, AllTemplatesDeploymentSource>();
             services.AddSingleton<IDeploymentStepFactory>(new DeploymentStepFactory<AllTemplatesDeploymentStep>());
             services.AddScoped<IDisplayDriver<DeploymentStep>, AllTemplatesDeploymentStepDriver>();
-
-            services.AddScoped<AdminTemplatesManager>();
-            services.AddScoped<IPermissionProvider, AdminTemplatesPermissions>();
         }
 
         public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
@@ -99,6 +96,9 @@ namespace OrchardCore.Templates
             services.AddTransient<IDeploymentSource, AllAdminTemplatesDeploymentSource>();
             services.AddSingleton<IDeploymentStepFactory>(new DeploymentStepFactory<AllAdminTemplatesDeploymentStep>());
             services.AddScoped<IDisplayDriver<DeploymentStep>, AllAdminTemplatesDeploymentStepDriver>();
+            
+            services.AddScoped<AdminTemplatesManager>();
+            services.AddScoped<IPermissionProvider, AdminTemplatesPermissions>();
         }
 
         public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
