34f61139f4671d7d5aba51c489ba75e9b3d15fdf
Sébastien Ros <sebastienros@gmail.com>
2020-01-03 13:32:53 -0800

    Reducing ShellSettings allocations

--- src/OrchardCore/OrchardCore.Abstractions/Shell/ShellSettings.cs
+++ src/OrchardCore/OrchardCore.Abstractions/Shell/ShellSettings.cs
@@ -16,16 +16,24 @@ namespace OrchardCore.Environment.Shell
         private ShellConfiguration _settings;
         private ShellConfiguration _configuration;
 
+        private string _requestUrlHost;
+        private string _requestUrlPrefix;
+        private TenantState _state;
+
         public ShellSettings()
         {
             _settings = new ShellConfiguration();
             _configuration = new ShellConfiguration();
+
+            InitLocals();
         }
 
         public ShellSettings(ShellConfiguration settings, ShellConfiguration configuration)
         {
             _settings = settings;
             _configuration = configuration;
+
+            InitLocals();
         }
 
         public ShellSettings(ShellSettings settings)
@@ -34,27 +42,36 @@ namespace OrchardCore.Environment.Shell
             _configuration = new ShellConfiguration(settings.Name, settings._configuration);
 
             Name = settings.Name;
+
+            InitLocals();
+        }
+
+        private void InitLocals()
+        {
+            _state = _settings.GetValue<TenantState>("State");
+            _requestUrlHost = _settings["RequestUrlHost"];
+            _requestUrlPrefix = _settings["RequestUrlPrefix"]?.Trim(' ', '/');
         }
 
         public string Name { get; set; }
 
         public string RequestUrlHost
         {
-            get => _settings["RequestUrlHost"];
-            set => _settings["RequestUrlHost"] = value;
+            get => _requestUrlHost;
+            set => _settings["RequestUrlHost"] = _requestUrlHost = value;
         }
 
         public string RequestUrlPrefix
         {
-            get => _settings["RequestUrlPrefix"]?.Trim(' ', '/');
-            set => _settings["RequestUrlPrefix"] = value;
+            get => _requestUrlPrefix;
+            set => _settings["RequestUrlPrefix"] = _requestUrlPrefix = value?.Trim(' ', '/');
         }
 
         [JsonConverter(typeof(StringEnumConverter))]
         public TenantState State
         {
-            get => _settings.GetValue<TenantState>("State");
-            set => _settings["State"] = value.ToString();
+            get => _state;
+            set => _settings["State"] = (_state = value).ToString();
         }
 
         [JsonIgnore]
