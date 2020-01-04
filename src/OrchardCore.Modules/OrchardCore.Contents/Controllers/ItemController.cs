7d337e538d5e87895688e562803fb8d776f58148
Sébastien Ros <sebastienros@gmail.com>
2020-01-03 17:10:52 -0800

    No-op Display

--- src/OrchardCore.Modules/OrchardCore.Contents/Controllers/ItemController.cs
+++ src/OrchardCore.Modules/OrchardCore.Contents/Controllers/ItemController.cs
@@ -28,32 +28,32 @@ namespace OrchardCore.Contents.Controllers
 
         private static ConcurrentDictionary<string, ContentItem> _cache = new ConcurrentDictionary<string, ContentItem>();
 
-        public async Task<IActionResult> Display(string contentItemId)
+        public IActionResult Display(string contentItemId)
         {
-            if (!_cache.TryGetValue(contentItemId, out var contentItem))
-            {
-                contentItem = await _contentManager.GetAsync(contentItemId);
+            return Content("Hello");
 
-                if (contentItem == null)
-                {
-                    return NotFound();
-                }
+            //if (!_cache.TryGetValue(contentItemId, out var contentItem))
+            //{
+            //    contentItem = await _contentManager.GetAsync(contentItemId);
 
-                _cache.TryAdd(contentItemId, contentItem);
-            }
+            //    if (contentItem == null)
+            //    {
+            //        return NotFound();
+            //    }
 
-            if (contentItem == null)
-            {
-                return NotFound();
-            }
+            //    _cache.TryAdd(contentItemId, contentItem);
+            //}
 
-            if (!await _authorizationService.AuthorizeAsync(User, Permissions.ViewContent, contentItem))
-            {
-                return User.Identity.IsAuthenticated ? (IActionResult)Forbid() : Challenge();
-            }
+            //if (contentItem == null)
+            //{
+            //    return NotFound();
+            //}
 
+            //if (!await _authorizationService.AuthorizeAsync(User, Permissions.ViewContent, contentItem))
+            //{
+            //    return User.Identity.IsAuthenticated ? (IActionResult)Forbid() : Challenge();
+            //}
 
-            return Content(contentItem.DisplayText);
 
             //var model = await _contentItemDisplayManager.BuildDisplayAsync(contentItem, this);
 
