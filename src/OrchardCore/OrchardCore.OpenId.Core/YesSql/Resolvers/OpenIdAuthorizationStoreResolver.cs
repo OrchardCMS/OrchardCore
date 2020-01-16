964186f9c8bce81d7c24f06ec385e6e78aab6494
Kévin Chalet <kevinchalet@gmail.com>
2020-01-16 14:48:14 +0100

    Remove license headers from the stores resolvers

--- src/OrchardCore/OrchardCore.OpenId.Core/YesSql/Resolvers/OpenIdAuthorizationStoreResolver.cs
+++ src/OrchardCore/OrchardCore.OpenId.Core/YesSql/Resolvers/OpenIdAuthorizationStoreResolver.cs
@@ -1,9 +1,3 @@
-/*
- * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
- * See https://github.com/openiddict/openiddict-core for more information concerning
- * the license and the contributors participating to this project.
- */
-
 using System;
 using System.Collections.Concurrent;
 using System.Text;
