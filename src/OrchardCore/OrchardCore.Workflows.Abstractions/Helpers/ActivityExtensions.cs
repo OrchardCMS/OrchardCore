02a1a93d15691d18e2e8c6598f74a8cf78ea2891
Ludovic Thauvin <ludovic.thauvin@sikim.fr>
2020-01-16 16:38:05 +0100

    Workflow status localization

--- src/OrchardCore/OrchardCore.Workflows.Abstractions/Helpers/ActivityExtensions.cs
+++ src/OrchardCore/OrchardCore.Workflows.Abstractions/Helpers/ActivityExtensions.cs
@@ -18,5 +18,22 @@ namespace OrchardCore.Workflows.Helpers
             var title = activity.As<ActivityMetadata>().Title;
             return !string.IsNullOrEmpty(title) ? new LocalizedHtmlString(title, title) : defaultTitle();
         }
+
+        public static LocalizedHtmlString GetLocalizedStatus(this WorkflowStatus status, IViewLocalizer localizer)
+        {
+            // A switch is used to allow string collection for translations.
+            return status switch
+            {
+                WorkflowStatus.Aborted => localizer["Aborted"],
+                WorkflowStatus.Executing => localizer["Executing"],
+                WorkflowStatus.Faulted => localizer["Faulted"],
+                WorkflowStatus.Finished => localizer["Finished"],
+                WorkflowStatus.Halted => localizer["Halted"],
+                WorkflowStatus.Idle => localizer["Idle"],
+                WorkflowStatus.Resuming => localizer["Resuming"],
+                WorkflowStatus.Starting => localizer["Starting"],
+                _ => localizer[status.ToString()],
+            };
+        }
     }
 }
