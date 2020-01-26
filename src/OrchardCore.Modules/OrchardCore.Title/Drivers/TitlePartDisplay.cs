8f23c9edd7cf47dfc08ada69abfbf32a31418f35
yiannis kyriazidis <giannkyrias@gmail.com>
2020-01-26 09:30:19 +0200

    simplify part settings retreival for TitlePartDisplay
    #5365

--- src/OrchardCore.Modules/OrchardCore.Title/Drivers/TitlePartDisplay.cs
+++ src/OrchardCore.Modules/OrchardCore.Title/Drivers/TitlePartDisplay.cs
@@ -1,7 +1,6 @@
-using System.Linq;
 using System.Threading.Tasks;
 using OrchardCore.ContentManagement.Display.ContentDisplay;
-using OrchardCore.ContentManagement.Metadata;
+using OrchardCore.ContentManagement.Display.Models;
 using OrchardCore.DisplayManagement.ModelBinding;
 using OrchardCore.DisplayManagement.Views;
 using OrchardCore.Title.Models;
@@ -11,31 +10,24 @@ namespace OrchardCore.Title.Drivers
 {
     public class TitlePartDisplay : ContentPartDisplayDriver<TitlePart>
     {
-        private readonly IContentDefinitionManager _contentDefinitionManager;
-
-        public TitlePartDisplay(IContentDefinitionManager contentDefinitionManager)
-        {
-            _contentDefinitionManager = contentDefinitionManager;
-        }
-
         public override IDisplayResult Display(TitlePart titlePart)
         {
             return Initialize<TitlePartViewModel>("TitlePart", model =>
-            {
-                model.Title = titlePart.ContentItem.DisplayText;
-                model.TitlePart = titlePart;
-            })
-            .Location("Detail", "Header:5")
-            .Location("Summary", "Header:5");
+                {
+                    model.Title = titlePart.ContentItem.DisplayText;
+                    model.TitlePart = titlePart;
+                })
+                .Location("Detail", "Header:5")
+                .Location("Summary", "Header:5");
         }
 
-        public override IDisplayResult Edit(TitlePart titlePart)
+        public override IDisplayResult Edit(TitlePart titlePart, BuildPartEditorContext context)
         {
             return Initialize<TitlePartViewModel>("TitlePart_Edit", model =>
             {
                 model.Title = titlePart.ContentItem.DisplayText;
                 model.TitlePart = titlePart;
-                model.Settings = GetSettings(titlePart);
+                model.Settings = context.TypePartDefinition.GetSettings<TitlePartSettings>();
             });
         }
 
@@ -46,13 +38,6 @@ namespace OrchardCore.Title.Drivers
             model.ContentItem.DisplayText = model.Title;
 
             return Edit(model);
-        }
-
-        private TitlePartSettings GetSettings(TitlePart titlePart)
-        {
-            var contentTypeDefinition = _contentDefinitionManager.GetTypeDefinition(titlePart.ContentItem.ContentType);
-            var contentTypePartDefinition = contentTypeDefinition.Parts.FirstOrDefault(x => string.Equals(x.PartDefinition.Name, nameof(TitlePart)));
-            return contentTypePartDefinition?.GetSettings<TitlePartSettings>();
         }
     }
 }
