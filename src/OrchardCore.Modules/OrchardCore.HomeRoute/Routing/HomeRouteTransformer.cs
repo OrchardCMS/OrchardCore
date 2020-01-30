25d98a652a7729a2dcb8e2eccbd3d12fd6b0057c
Sébastien Ros <sebastienros@gmail.com>
2020-01-30 13:17:58 -0800

    Update HomeRouteTransformer.cs

--- src/OrchardCore.Modules/OrchardCore.HomeRoute/Routing/HomeRouteTransformer.cs
+++ src/OrchardCore.Modules/OrchardCore.HomeRoute/Routing/HomeRouteTransformer.cs
@@ -18,10 +18,12 @@ namespace OrchardCore.HomeRoute.Routing
         public override async ValueTask<RouteValueDictionary> TransformAsync(HttpContext httpContext, RouteValueDictionary values)
         {
             var homeRoute = (await _siteService.GetSiteSettingsAsync()).HomeRoute;
+            
             if (homeRoute.Count > 0)
             {
                 return new RouteValueDictionary(homeRoute);
-            } else
+            } 
+            else
             {
                 return null;
             }
