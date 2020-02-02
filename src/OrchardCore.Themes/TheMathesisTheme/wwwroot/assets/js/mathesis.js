787201c46426ad5dce83d76b978a8b0af20479b4
Antonello Bianchi <antonello@nementis.eu>
2020-02-02 22:21:52 +0100

    Blog Section Complete

--- src/OrchardCore.Themes/TheMathesisTheme/wwwroot/assets/js/mathesis.js
+++ src/OrchardCore.Themes/TheMathesisTheme/wwwroot/assets/js/mathesis.js
@@ -34,22 +34,5 @@ $(function () {
 		$.ui.fancytree.getTree().filterNodes($(this).val());
 	});
 
-	var words = [
-		{ text: "Lorem", weight: 13 },
-		{ text: "Ipsum", weight: 10.5 },
-		{ text: "Dolor", weight: 9.4 },
-		{ text: "Sit", weight: 8 },
-		{ text: "Amet", weight: 6.2 },
-		{ text: "Consectetur", weight: 5 },
-		{ text: "Adipiscing", weight: 5 },
-		{ text: "Loremss", weight: 13 },
-		{ text: "Ipsumss", weight: 10.5 },
-		{ text: "Dolorss", weight: 9.4 },
-		{ text: "Siss", weight: 8 },
-		{ text: "Ametss", weight: 6.2 },
-		{ text: "Consecteturss", weight: 5 },
-		{ text: "Adipiscingss", weight: 5 },
-	];
-
 	$("#tagcloud").jQCloud(words);
 });
