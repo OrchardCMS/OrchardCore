f0256798e06033faa8ebaf9dfc1e7a419a2ee06e
Jeff Olmstead <jeff.olmstead@endlessmountainsolutions.com>
2020-02-14 09:02:39 -0500

    Adding in "datetime" and "tagspan" support

---
+++ src/OrchardCore/OrchardCore.DisplayManagement/TagHelpers/DateTimeTagHelper.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace OrchardCore.DisplayManagement.TagHelpers
+{
+    [HtmlTargetElement("datetime")]
+    public class DateTimeTagHelper : TagHelper
+    {
+        private const string UtcAttribute = "utc";
+        private const string FormatAttribute = "format";
+
+        protected IShapeFactory _shapeFactory;
+        protected IDisplayHelper _displayHelper;
+
+        public DateTimeTagHelper(IShapeFactory shapeFactory, IDisplayHelper displayHelper)
+        {
+            _shapeFactory = shapeFactory;
+            _displayHelper = displayHelper;
+        }
+
+        [HtmlAttributeName(UtcAttribute)]
+        public DateTime? Utc { set; get; }
+
+        [HtmlAttributeName(FormatAttribute)]
+        public string Format { set; get; }
+
+        public async override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var shapeType = "DateTime";
+            dynamic shape = await _shapeFactory.CreateAsync(shapeType);
+            shape.Utc = Utc;
+            shape.Format = Format;
+            
+            output.Content.SetHtmlContent(await _displayHelper.ShapeExecuteAsync(shape));
+
+            // We don't want any encapsulating tag around the shape
+            output.TagName = null;
+        }
+    }
+}
