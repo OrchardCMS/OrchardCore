2b531f6fbe0baf86eef69b00e12bd0be7d049474
Sébastien Ros <sebastienros@gmail.com>
2020-02-24 12:19:02 -0800

    Update MediaFilters.cs

--- src/OrchardCore.Modules/OrchardCore.Media/Filters/MediaFilters.cs
+++ src/OrchardCore.Modules/OrchardCore.Media/Filters/MediaFilters.cs
@@ -1,8 +1,8 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.WebUtilities;
 using Fluid;
 using Fluid.Values;
-using Microsoft.AspNetCore.WebUtilities;
 using OrchardCore.Liquid;
 
 namespace OrchardCore.Media.Filters
