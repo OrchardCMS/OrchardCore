d5d50411fe8ada0a0b5a16b4f3c6ed9e488cc306
jtkech <jean-thierry.kechichian@wanadoo.fr>
2020-02-26 23:43:19 +0100

    IStringLocalizer static field named "S"

--- src/OrchardCore.Modules/OrchardCore.OpenId/Validators/OpenIdUrlValidator.cs
+++ src/OrchardCore.Modules/OrchardCore.OpenId/Validators/OpenIdUrlValidator.cs
@@ -1,23 +1,31 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Threading;
 using Microsoft.Extensions.Localization;
 
 namespace OrchardCore.OpenId.Validators
 {
     public static class OpenIdUrlValidator
     {
+        private static readonly AsyncLocal<IStringLocalizer> _localizer = new AsyncLocal<IStringLocalizer>();
+
+        private static IStringLocalizer S => _localizer.Value;
+
         public static IEnumerable<ValidationResult> ValidateUrls(this ValidationContext context, string memberName, string member)
         {
             if (member != null)
             {
-                var localizer = (IStringLocalizer)context.GetService(typeof(IStringLocalizer<Startup>));
+                if (_localizer.Value == null)
+                {
+                    _localizer.Value = (IStringLocalizer)context.GetService(typeof(IStringLocalizer<Startup>));
+                }
 
                 foreach (var url in member.Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries))
                 {
                     if (!Uri.TryCreate(url, UriKind.Absolute, out var uri) || !uri.IsWellFormedOriginalString())
                     {
-                        yield return new ValidationResult(localizer["{0} is not wellformed", url], new[] { memberName });
+                        yield return new ValidationResult(S["{0} is not wellformed", url], new[] { memberName });
                     }
                 }
             }
