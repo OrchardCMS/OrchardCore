bfea789181cf0181549ed474acc703bce214e5e3
Lev Platonov <47658822+levpl@users.noreply.github.com>
2020-03-08 14:36:47 +0300

    AdminController.cs commented

--- src/OrchardCore.Modules/OrchardCore.Admin/Controllers/AdminController.cs
+++ src/OrchardCore.Modules/OrchardCore.Admin/Controllers/AdminController.cs
@@ -1,14 +1,21 @@
+/*
+	defines the class AdminController <- Controller
+		implementing method Index
+			returning the IActionResult result of the View method
+*/
+
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace OrchardCore.Admin.Controllers
 {
-    [Authorize]
-    public class AdminController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+	[Authorize] // locks the controller
+	public class AdminController: Controller
+	{
+		// returns an View() result
+		public IActionResult Index ()
+		{
+			return View ();
+		}
+	}
 }
