fc9c6d1625a3e518b6bb4fe9dfb21c7b1b59d903
Lev Platonov <47658822+levpl@users.noreply.github.com>
2020-03-08 21:46:56 +0300

    AdminSiteSettingsDisplayDriver.cs commented

--- src/OrchardCore.Modules/OrchardCore.Admin/Drivers/AdminSiteSettingsDisplayDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.Admin/Drivers/AdminSiteSettingsDisplayDriver.cs
@@ -1,3 +1,9 @@
+/*
+    defines the class AdminSiteSettingsDisplayDriver (<- SectionDisplayDriver <ISite, AdminSettings>)
+        allows you to change the model (.EditAsync) or update the settings (.UpdateAsync)
+            returning the task with a display result
+*/
+
 using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
@@ -11,54 +17,74 @@ using OrchardCore.Settings;
 
 namespace OrchardCore.Admin.Drivers
 {
-    public class AdminSiteSettingsDisplayDriver : SectionDisplayDriver<ISite, AdminSettings>
-    {
-        public const string GroupId = "admin";
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        private readonly IAuthorizationService _authorizationService;
+    public class AdminSiteSettingsDisplayDriver: SectionDisplayDriver <ISite, AdminSettings>
+    {
+        // (???) id in the user group OR group id
+        public const string GroupId = "admin";
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IAuthorizationService _authorizationService;
 
-        public AdminSiteSettingsDisplayDriver(
-            IHttpContextAccessor httpContextAccessor,
-            IAuthorizationService authorizationService)
-        {
-            _httpContextAccessor = httpContextAccessor;
-            _authorizationService = authorizationService;
-        }
+        // constructor
+        public AdminSiteSettingsDisplayDriver (
+            IHttpContextAccessor httpContextAccessor,
+            IAuthorizationService authorizationService)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _authorizationService = authorizationService;
+        }
 
-        public override async Task<IDisplayResult> EditAsync(AdminSettings settings, BuildEditorContext context)
-        {
-            var user = _httpContextAccessor.HttpContext?.User;
+        /*
+            returns a task with a display result
+        */
+        public override async Task <IDisplayResult> EditAsync (AdminSettings settings, BuildEditorContext context)
+        {
+            var user = _httpContextAccessor.HttpContext? .User;
+            // if "user" does not have administrator privileges, then returns null
+            if (! await _authorizationService.AuthorizeAsync (user, PermissionsAdminSettings.ManageAdminSettings))
+            {
+                return null;
+            }
 
-            if (!await _authorizationService.AuthorizeAsync(user, PermissionsAdminSettings.ManageAdminSettings))
-            {
-                return null;
-            }
+            /*
+                creates and returns a "ShapeResult" object with arguments $model and $GroupId
+            */
+            return Initialize <AdminSettingsViewModel> ("AdminSettings_Edit",
+                model =>
+                // initialize $model to $settings
+                {
+                    model.DisplayMenuFilter = settings.DisplayMenuFilter;
+                })
+                .Location ("Content: 3")
+                .OnGroup (GroupId);
+        }
 
-            return Initialize<AdminSettingsViewModel>("AdminSettings_Edit", model =>
-                {
-                    model.DisplayMenuFilter = settings.DisplayMenuFilter;
-                }).Location("Content:3").OnGroup(GroupId);
-        }
+        /*
+            returns a task with a display result
+        */
+        public override async Task <IDisplayResult> UpdateAsync (AdminSettings settings, BuildEditorContext context)
+        {
+            var user = _httpContextAccessor.HttpContext? .User;
 
-        public override async Task<IDisplayResult> UpdateAsync(AdminSettings settings, BuildEditorContext context)
-        {
-            var user = _httpContextAccessor.HttpContext?.User;
+            // if user does not have administrator privileges, then returns null
+            if (! await _authorizationService.AuthorizeAsync (user, PermissionsAdminSettings.ManageAdminSettings))
+            {
+                return null;
+            }
 
-            if (!await _authorizationService.AuthorizeAsync(user, PermissionsAdminSettings.ManageAdminSettings))
-            {
-                return null;
-            }
+            // for administrators
+            if (context.GroupId == GroupId)
+            {
+                var model = new AdminSettingsViewModel ();
 
-            if (context.GroupId == GroupId)
-            {
-                var model = new AdminSettingsViewModel();
+                // tries to update the $context by $model
+                await context.Updater.TryUpdateModelAsync (model, Prefix);
 
-                await context.Updater.TryUpdateModelAsync(model, Prefix);
+                // update the settings by $model
+                settings.DisplayMenuFilter = model.DisplayMenuFilter;
+            }
 
-                settings.DisplayMenuFilter = model.DisplayMenuFilter;
-            }
-
-            return await EditAsync(settings, context);
-        }
-    }
+            // creates and returns an task with a display result
+            return await EditAsync (settings, context);
+        }
+    }
 }
