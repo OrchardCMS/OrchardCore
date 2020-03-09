eba7b26102f93947527885e5a88b355a23e2987a
Antoine Griffard <agriffard@hotmail.com>
2020-03-09 20:42:51 +0100

    Fix test

--- test/OrchardCore.Tests/OrchardCore.Users/RegistrationControllerTests.cs
+++ test/OrchardCore.Tests/OrchardCore.Users/RegistrationControllerTests.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
@@ -99,6 +100,9 @@ namespace OrchardCore.Tests.OrchardCore.Users
             mockServiceProvider
                 .Setup(x => x.GetService(typeof(ITempDataDictionaryFactory)))
                 .Returns(Mock.Of<ITempDataDictionaryFactory>());
+            mockServiceProvider
+                .Setup(x => x.GetService(typeof(IObjectModelValidator)))
+                .Returns(Mock.Of<IObjectModelValidator>());
 
             var mockHttpContext = new Mock<HttpContext>();
             mockHttpContext
