bf2fc438a9fe10bc094bc46d71faeb38f357d560
Lev Platonov <47658822+levpl@users.noreply.github.com>
2020-03-09 10:34:17 +0300

    AdminSettingsViewModel.cs commented

--- src/OrchardCore.Modules/OrchardCore.Admin/ViewModels/AdminSettingsViewModel.cs
+++ src/OrchardCore.Modules/OrchardCore.Admin/ViewModels/AdminSettingsViewModel.cs
@@ -1,9 +1,15 @@
+/*
+	defines the class "AdminSettignsViewModel"
+		having a flag field "DisplayMenuFilter"
+*/
+
 using System;
 
 namespace OrchardCore.Admin.ViewModels
 {
-    public class AdminSettingsViewModel
-    {
-        public bool DisplayMenuFilter { get; set; }
-    }
+	public class AdminSettingsViewModel
+	{
+		// flag field, (apparently) to display the menu filter
+		public bool DisplayMenuFilter {get; set; }
+	}
 }
