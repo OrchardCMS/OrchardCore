e77d30967a46891b586d2423369492c54f1b38ec
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2020-03-13 17:40:34 +0300

    Add docs

--- src/OrchardCore/OrchardCore.Infrastructure/Email/EmailAddressValidator.cs
+++ src/OrchardCore/OrchardCore.Infrastructure/Email/EmailAddressValidator.cs
@@ -2,8 +2,12 @@ using MimeKit;
 
 namespace OrchardCore.Email
 {
+    /// <summary>
+    /// Represents a service for e-mail address validation.
+    /// </summary>
     public class EmailAddressValidator : IEmailAddressValidator
     {
+        /// <inheritdoc/>
         public bool Validate(string emailAddress)
             => emailAddress.IndexOf('@') > -1 && MailboxAddress.TryParse(emailAddress, out _);
     }
--- src/OrchardCore/OrchardCore.Infrastructure/Email/OrchardCoreBuilderExtensions.cs
+++ src/OrchardCore/OrchardCore.Infrastructure/Email/OrchardCoreBuilderExtensions.cs
@@ -2,11 +2,15 @@ using OrchardCore.Email;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
+    /// <summary>
+    /// Provides an extension method for <see cref="OrchardCoreBuilder"/>.
+    /// </summary>
     public static partial class OrchardCoreBuilderExtensions
     {
         /// <summary>
-        /// Adds email address validator service.
+        /// Adds e-mail address validator service.
         /// </summary>
+        /// <param name="builder">The <see cref="OrchardCoreBuilder"/>.</param>
         public static OrchardCoreBuilder AddEmailAddressValidator(this OrchardCoreBuilder builder)
         {
             builder.ConfigureServices(services =>
