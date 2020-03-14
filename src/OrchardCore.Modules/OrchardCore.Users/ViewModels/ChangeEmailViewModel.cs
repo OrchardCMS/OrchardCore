ee008c82bc5f60307c041463dc93e7d584dd8060
Antoine Griffard <agriffard@hotmail.com>
2020-03-14 17:23:57 +0100

    Use IEmailAddressValidator

--- src/OrchardCore.Modules/OrchardCore.Users/ViewModels/ChangeEmailViewModel.cs
+++ src/OrchardCore.Modules/OrchardCore.Users/ViewModels/ChangeEmailViewModel.cs
@@ -1,13 +1,21 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Localization;
-using MimeKit;
+using Microsoft.Extensions.Localization;    
+using OrchardCore.Email;
 
 namespace OrchardCore.Users.ViewModels
 {
     public class ChangeEmailViewModel : IValidatableObject
     {
+        private readonly IEmailAddressValidator _emailAddressValidator;
+
+        public ChangeEmailViewModel(IEmailAddressValidator emailAddressValidator)
+        {
+            _emailAddressValidator = emailAddressValidator ?? throw new ArgumentNullException(nameof(emailAddressValidator));
+        }
+
         public string Email { get; set; }
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
@@ -17,7 +25,7 @@ namespace OrchardCore.Users.ViewModels
             {
                 yield return new ValidationResult(S["Email is required."], new[] { nameof(Email) });
             }
-            else if (Email.IndexOf('@') == -1 || !MailboxAddress.TryParse(Email, out var emailAddress))
+            else if (!_emailAddressValidator.Validate(Email))
             {
                 yield return new ValidationResult(S["Invalid Email."], new[] { nameof(Email) });
             }
