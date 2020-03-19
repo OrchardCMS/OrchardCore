903930c8cb339c3a3f8b90de79b57bc199b4e237
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2020-03-19 23:35:39 +0300

    Add another unit test

--- test/OrchardCore.Tests/OrchardCore.Deployment/DeploymentSourceOrderTests.cs
+++ test/OrchardCore.Tests/OrchardCore.Deployment/DeploymentSourceOrderTests.cs
@@ -15,9 +15,11 @@ namespace OrchardCore.Tests.Deployment
         [Fact]
         public async Task ExecuteDeploymentPlan_ShouldRespect_DeployementSourceOrder()
         {
-            var deploymentSources = new IDeploymentSource[] { new DeploymentSource1(), new DeploymentSource2() };
+            var deploymentSources = new IDeploymentSource[] { new DeploymentSource1(), new DeploymentSource2(), new DeploymentSource3() };
             var deploymentManager = new DeploymentManager(deploymentSources, null, null);
             var recipeDescriptor = new RecipeDescriptor();
+
+            _executedDeploymentSteps.Clear();
 
             using (var fileBuilder = new TemporaryFileBuilder())
             {
@@ -30,6 +32,29 @@ namespace OrchardCore.Tests.Deployment
 
             Assert.Equal("Deployment Step 2", _executedDeploymentSteps[0]);
             Assert.Equal("Deployment Step 1", _executedDeploymentSteps[1]);
+            Assert.Equal("Deployment Step 3", _executedDeploymentSteps[2]);
+        }
+
+        [Fact]
+        public async Task ExecuteDeploymentPlan_ExecuteDeployementSource_BeforeOrderedDeployementSource_WithZeroOrder()
+        {
+            var deploymentSources = new IDeploymentSource[] { new DeploymentSource1(), new DeploymentSource4() };
+            var deploymentManager = new DeploymentManager(deploymentSources, null, null);
+            var recipeDescriptor = new RecipeDescriptor();
+
+            _executedDeploymentSteps.Clear();
+
+            using (var fileBuilder = new TemporaryFileBuilder())
+            {
+                var deploymentPlanResult = new DeploymentPlanResult(fileBuilder, recipeDescriptor);
+                var deploymentPlan = new DeploymentPlan();
+
+                deploymentPlan.DeploymentSteps.AddRange(new DeploymentStep[] { new DeploymentStep1(), new DeploymentStep2() });
+                await deploymentManager.ExecuteDeploymentPlanAsync(deploymentPlan, deploymentPlanResult);
+            }
+
+            Assert.Equal("Deployment Step 1", _executedDeploymentSteps[0]);
+            Assert.Equal("Deployment Step 4", _executedDeploymentSteps[1]);
         }
 
         public class DeploymentStep1 : DeploymentStep
@@ -81,6 +106,46 @@ namespace OrchardCore.Tests.Deployment
 
                 result.Steps.Add(new JObject(new JProperty("name", "Deployment Step 2")));
                 _executedDeploymentSteps.Add("Deployment Step 2");
+
+                await Task.CompletedTask;
+            }
+        }
+
+        public class DeploymentSource3 : IOrderedDeploymentSource
+        {
+            public int Order => 10;
+
+            public async Task ProcessDeploymentStepAsync(DeploymentStep step, DeploymentPlanResult result)
+            {
+                var deploymentStep = step as DeploymentStep2;
+
+                if (deploymentStep == null)
+                {
+                    return;
+                }
+
+                result.Steps.Add(new JObject(new JProperty("name", "Deployment Step 3")));
+                _executedDeploymentSteps.Add("Deployment Step 3");
+
+                await Task.CompletedTask;
+            }
+        }
+
+        public class DeploymentSource4 : IOrderedDeploymentSource
+        {
+            public int Order => 0;
+
+            public async Task ProcessDeploymentStepAsync(DeploymentStep step, DeploymentPlanResult result)
+            {
+                var deploymentStep = step as DeploymentStep2;
+
+                if (deploymentStep == null)
+                {
+                    return;
+                }
+
+                result.Steps.Add(new JObject(new JProperty("name", "Deployment Step 4")));
+                _executedDeploymentSteps.Add("Deployment Step 4");
 
                 await Task.CompletedTask;
             }
