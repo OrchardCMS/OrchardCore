d5429403d81101c039802dadfac222bb7035c48c
Jean-Thierry Kéchichian <jean-thierry.kechichian@wanadoo.fr>
2020-03-22 23:48:57 +0100

    Update TodoModel.cs

--- src/OrchardCore.Modules/OrchardCore.Demo/Models/TodoModel.cs
+++ src/OrchardCore.Modules/OrchardCore.Demo/Models/TodoModel.cs
@@ -1,45 +1,12 @@
 using System;
-using System.ComponentModel.DataAnnotations;
-using OrchardCore.DisplayManagement.Views;
 
 namespace OrchardCore.Demo.Models
 {
-    public class TodoModel : ShapeViewModel
+    public class TodoModel
     {
-        public TodoModel()
-        {
-            Metadata.Type = "Todo";
-        }
-
-
-        [Required]
+        public string TodoId { get; set; }
         public string Text { get; set; }
-        [Required]
         public DateTime DueDate { get; set; }
         public bool IsCompleted { get; set; }
-
-
-        public string DisplayMode
-        {
-            get
-            {
-                return Metadata.DisplayType;
-            }
-            set
-            {
-                var alternate = $"Todo_{value}";
-                if (Metadata.Alternates.Contains(alternate))
-                {
-                    if (Metadata.Alternates.Last == alternate)
-                    {
-                        return;
-                    }
-
-                    Metadata.Alternates.Remove(alternate);
-                }
-                Metadata.Alternates.Add(alternate);
-                Metadata.DisplayType = value;
-            }
-        }
     }
 }
