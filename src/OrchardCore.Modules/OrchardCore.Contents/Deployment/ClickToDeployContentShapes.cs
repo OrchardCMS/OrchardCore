2a272b43f90e560ee89a9f569a700caa32ab4ec8
Dean Marcussen <dean.marcussen@gmail.com>
2020-04-06 15:17:22 +0100

    Remove buttons on content page

--- src/OrchardCore.Modules/OrchardCore.Contents/Deployment/ClickToDeployContentShapes.cs
+++ src/OrchardCore.Modules/OrchardCore.Contents/Deployment/ClickToDeployContentShapes.cs
@@ -15,44 +15,13 @@ namespace OrchardCore.Contents.Deployment
 
         public void Discover(ShapeTableBuilder builder)
         {
-            builder.Describe("Content_PublishButton")
-                .OnDisplaying(async displaying =>
-                {
-                    if (await _deploymentPlanService.DoesUserHavePermissionsAsync())
-                    {
-                        var deploymentPlans = await _deploymentPlanService.GetAllDeploymentPlansAsync();
-                        var clickToDeployPlan = deploymentPlans.FirstOrDefault(x => x.DeploymentSteps.Any(x => x.Name == nameof(ClickToDeployContentDeploymentStep)));
-
-                        if (clickToDeployPlan != null)
-                        {
-                            displaying.Shape.Metadata.Alternates.Add("ClickToDeploy_PublishButton__Content");
-                            displaying.Shape.Metadata.Wrappers.Add("ClickToDeploy_Wrapper__DeploymentTarget");
-                        }
-                    }
-                });
-
-            builder.Describe("Content_SaveDraftButton")
-                .OnDisplaying(async displaying =>
-                {
-                    if (await _deploymentPlanService.DoesUserHavePermissionsAsync())
-                    {
-                        var deploymentPlans = await _deploymentPlanService.GetAllDeploymentPlansAsync();
-                        var clickToDeployPlan = deploymentPlans.FirstOrDefault(x => x.DeploymentSteps.Any(x => x.Name == nameof(ClickToDeployContentDeploymentStep)));
-
-                        if (clickToDeployPlan != null)
-                        {
-                            displaying.Shape.Metadata.Alternates.Add("ClickToDeploy_SaveDraftButton__Content");
-                            displaying.Shape.Metadata.Wrappers.Add("ClickToDeploy_Wrapper__DeploymentTarget");
-                        }
-                    }
-                });
-
             builder.Describe("Contents_SummaryAdmin__Button__Actions")
                 .OnDisplaying(async displaying =>
                 {
                     if (await _deploymentPlanService.DoesUserHavePermissionsAsync())
                     {
                         var deploymentPlans = await _deploymentPlanService.GetAllDeploymentPlansAsync();
+                        //TODO this could be a setting. This would store the id, then no need to retrieve all from the database.
                         var clickToDeployPlan = deploymentPlans.FirstOrDefault(x => x.DeploymentSteps.Any(x => x.Name == nameof(ClickToDeployContentDeploymentStep)));
 
                         if (clickToDeployPlan != null)
