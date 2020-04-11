95b33018bad36c431cadf7c638851a4237a4903e
Dean Marcussen <dean.marcussen@gmail.com>
2020-04-11 17:32:33 +0100

    Autoroute container routing (#5665)

--- src/OrchardCore.Modules/OrchardCore.Autoroute/ViewModels/AutoroutePartSettingsViewModel.cs
+++ src/OrchardCore.Modules/OrchardCore.Autoroute/ViewModels/AutoroutePartSettingsViewModel.cs
@@ -8,6 +8,10 @@ namespace OrchardCore.Autoroute.ViewModels
         public string Pattern { get; set; }
         public bool ShowHomepageOption { get; set; }
         public bool AllowUpdatePath { get; set; }
+        public bool AllowDisabled { get; set; }
+        public bool AllowRouteContainedItems { get; set; }
+        public bool ManageContainedItemRoutes { get; set; }
+        public bool AllowAbsolutePath { get; set; }
         public AutoroutePartSettings AutoroutePartSettings { get; set; }
     }
 }
