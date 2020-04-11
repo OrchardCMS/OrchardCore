35ffd41dc2c57c1ac37c555b7376bb6aacc53c90
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2020-04-11 17:30:02 +0200

    Updating manifest

--- src/OrchardCore.Modules/OrchardCore.PublishLater/Manifest.cs
+++ src/OrchardCore.Modules/OrchardCore.PublishLater/Manifest.cs
@@ -3,8 +3,8 @@ using OrchardCore.Modules.Manifest;
 [assembly: Module(
     Name = "Publish Later",
     Author = "The Orchard Team",
-    Website = "https://orchardproject.net",
-    Version = "2.0.0",
+    Website = "https://orchardcore.ne",
+    Version = "1.0.0-rc2",
     Description = "The Publish Later module adds the ability to schedule content items to be published at a given future date and time.",
     Dependencies = new[] { "OrchardCore.Contents" },
     Category = "Content Management"
