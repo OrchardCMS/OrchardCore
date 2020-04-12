ae2fc682747150ad182b1155736b87228e87cdee
Antoine Griffard <agriffard@hotmail.com>
2020-04-12 18:45:33 +0200

    Admin Head Setting

--- src/OrchardCore.Modules/OrchardCore.Admin/Admin/AdminSettings.cs
+++ src/OrchardCore.Modules/OrchardCore.Admin/Admin/AdminSettings.cs
@@ -3,5 +3,7 @@ namespace OrchardCore.Admin.Models
     public class AdminSettings
     {
         public bool DisplayMenuFilter { get; set; }
+
+        public string Head { get; set; }
     }
 }
--- src/OrchardCore.Modules/OrchardCore.Admin/Drivers/AdminSiteSettingsDisplayDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.Admin/Drivers/AdminSiteSettingsDisplayDriver.cs
@@ -36,6 +36,7 @@ namespace OrchardCore.Admin.Drivers
             return Initialize<AdminSettingsViewModel>("AdminSettings_Edit", model =>
                 {
                     model.DisplayMenuFilter = settings.DisplayMenuFilter;
+                    model.Head = settings.Head;
                 }).Location("Content:3").OnGroup(GroupId);
         }
 
@@ -55,6 +56,7 @@ namespace OrchardCore.Admin.Drivers
                 await context.Updater.TryUpdateModelAsync(model, Prefix);
 
                 settings.DisplayMenuFilter = model.DisplayMenuFilter;
+                settings.Head = model.Head;
             }
 
             return await EditAsync(settings, context);
