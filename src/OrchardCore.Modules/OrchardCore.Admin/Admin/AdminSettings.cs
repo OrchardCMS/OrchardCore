36f800e1793e97faee9fd7ccf0bd34ead5a441d0
Antoine Griffard <agriffard@hotmail.com>
2020-04-13 12:37:56 +0200

    Add media picker

--- src/OrchardCore.Modules/OrchardCore.Admin/Admin/AdminSettings.cs
+++ src/OrchardCore.Modules/OrchardCore.Admin/Admin/AdminSettings.cs
@@ -1,3 +1,7 @@
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 namespace OrchardCore.Admin.Models
 {
     public class AdminSettings
@@ -6,5 +10,47 @@ namespace OrchardCore.Admin.Models
         public string BrandImageUrl { get; set; }
         public string FaviconUrl { get; set; }
         public string Head { get; set; }
+
+        public string BrandImage
+        {
+            get
+            {
+                string path = null;
+
+                if (!string.IsNullOrEmpty(BrandImageUrl))
+                {
+                    var media = JArray.Parse(BrandImageUrl);
+                    path = media.FirstOrDefault()?["Path"]?.Value<string>();
+
+                    if (!string.IsNullOrEmpty(path))
+                    {
+                        path = "/media/" + path;
+                    }
+                }
+
+                return path;
+            }
+        }
+
+        public string Favicon
+        {
+            get
+            {
+                string path = null;
+
+                if (!string.IsNullOrEmpty(FaviconUrl))
+                {
+                    var media = JArray.Parse(FaviconUrl);
+                    path = media.FirstOrDefault()?["Path"]?.Value<string>();
+
+                    if (!string.IsNullOrEmpty(path))
+                    {
+                        path = "/media/" + path;
+                    }
+                }
+
+                return path;
+            }
+        }
     }
 }
