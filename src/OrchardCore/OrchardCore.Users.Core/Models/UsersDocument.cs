7d7cb3b7f7c73c49849e9ebb388e1e9bd2eb64eb
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2020-04-13 18:02:04 +0300

    Fix property name

--- src/OrchardCore/OrchardCore.Users.Core/Models/UsersDocument.cs
+++ src/OrchardCore/OrchardCore.Users.Core/Models/UsersDocument.cs
@@ -6,7 +6,7 @@ namespace OrchardCore.Users.Models
     {
         public int Id { get; set; }
 
-        public List<User> Roles { get; } = new List<User>();
+        public List<User> Users { get; } = new List<User>();
 
         public int Serial { get; set; }
     }
