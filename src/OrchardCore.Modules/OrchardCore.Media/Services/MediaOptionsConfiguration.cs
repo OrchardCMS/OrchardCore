65fa3c5b8013748e479769ba2eb8a2d305c8dc9a
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2020-04-14 21:02:31 +0200

    Remove unused namespaces

--- src/OrchardCore.Modules/OrchardCore.Media/Services/MediaOptionsConfiguration.cs
+++ src/OrchardCore.Modules/OrchardCore.Media/Services/MediaOptionsConfiguration.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
-using OrchardCore.Environment.Extensions.Utility;
 using OrchardCore.Environment.Shell.Configuration;
 
 namespace OrchardCore.Media.Services
