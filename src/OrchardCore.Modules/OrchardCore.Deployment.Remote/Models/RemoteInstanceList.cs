fd87ce6f5ce74bd5ededb93f47e55368bfbf6751
jtkech <jean-thierry.kechichian@wanadoo.fr>
2020-04-17 05:25:10 +0200

    Make RemoteInstanceService distributed

--- src/OrchardCore.Modules/OrchardCore.Deployment.Remote/Models/RemoteInstanceList.cs
+++ src/OrchardCore.Modules/OrchardCore.Deployment.Remote/Models/RemoteInstanceList.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using OrchardCore.Data.Documents;
 
 namespace OrchardCore.Deployment.Remote.Models
 {
-    public class RemoteInstanceList
+    public class RemoteInstanceList : Document
     {
         public List<RemoteInstance> RemoteInstances { get; set; } = new List<RemoteInstance>();
+        public Dictionary<string, RemoteInstance> RemoteInstances { get; set; } = new Dictionary<string, RemoteInstance>(StringComparer.OrdinalIgnoreCase);
     }
 }
