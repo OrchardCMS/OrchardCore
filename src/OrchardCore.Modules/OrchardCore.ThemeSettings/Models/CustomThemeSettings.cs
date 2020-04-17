06ca6999524348d0e25ae9d40d30ee9a8a004eae
Antoine Griffard <agriffard@hotmail.com>
2020-04-17 19:23:00 +0200

    Remove ThemeSettings

--- src/OrchardCore.Modules/OrchardCore.ThemeSettings/Models/CustomThemeSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace OrchardCore.ThemeSettings.Models
-{
-    public class CustomThemeSettings
-    {
-        public string Head { get; set; }
-        public string Foot { get; set; }
-    }
-}
