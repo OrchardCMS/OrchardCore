ebf26b7e6d6e1d14a13e86dad296e4101c16ec05
ns8482e <23270244+ns8482e@users.noreply.github.com>
2020-04-28 18:49:25 -0500

    removed unused usings

--- src/OrchardCore.Modules/OrchardCore.Forms/Drivers/FormContentDisplayDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.Forms/Drivers/FormContentDisplayDriver.cs
@@ -1,37 +1,25 @@
-using System;
-using System.Collections.Generic;
+using System.Threading.Tasks;
 using OrchardCore.ContentManagement;
 using OrchardCore.ContentManagement.Display.ContentDisplay;
-using OrchardCore.ContentManagement.Display.ViewModels;
 using OrchardCore.ContentManagement.Metadata;
-using OrchardCore.ContentManagement.Metadata.Settings;
-using OrchardCore.DisplayManagement.ModelBinding;
 using OrchardCore.DisplayManagement.Views;
 using OrchardCore.DisplayManagement.Handlers;
-using System.Threading.Tasks;
 using OrchardCore.Forms.Models;
+
 namespace OrchardCore.Forms.Drivers
 {
     public class FormContentDisplayDriver : ContentDisplayDriver
     {
-        //private readonly IContentDefinitionManager _contentDefinitionManager;
-
-        public FormContentDisplayDriver(IContentDefinitionManager contentDefinitionManager)
-        {
-            //_contentDefinitionManager = contentDefinitionManager;
-        }
-
         public override Task<IDisplayResult> DisplayAsync(ContentItem model, BuildDisplayContext context)
         {
             var formItemShape = context.Shape;
-
+            //If content item contains FormPart add Form Wrapper.
             var formPart = model.As<FormPart>();
             if (formPart != null)
             {
-                // var contentTypeDefinition = _contentDefinitionManager.GetTypeDefinition(model.ContentType);
                 context.Shape.Metadata.Wrappers.Add($"Form_Wrapper");
             }
-
+                        
             //We don't need to return a shape result
             return Task.FromResult<IDisplayResult>(null);
         }
