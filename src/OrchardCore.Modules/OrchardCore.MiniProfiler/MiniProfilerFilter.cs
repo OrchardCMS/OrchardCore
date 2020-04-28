4511008a17ba9179d11f5d97e2e90b2ed4ee3e01
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2020-04-29 00:11:55 +0200

    Adding option to enable Mini Profiler on the admin too

--- src/OrchardCore.Modules/OrchardCore.MiniProfiler/MiniProfilerFilter.cs
+++ src/OrchardCore.Modules/OrchardCore.MiniProfiler/MiniProfilerFilter.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
 using OrchardCore.Admin;
 using OrchardCore.DisplayManagement;
 using OrchardCore.DisplayManagement.Layout;
@@ -12,20 +13,23 @@ namespace OrchardCore.MiniProfiler
     {
         private readonly ILayoutAccessor _layoutAccessor;
         private readonly IShapeFactory _shapeFactory;
+        private readonly MiniProfilerSettings _settings;
 
         public MiniProfilerFilter(
             ILayoutAccessor layoutAccessor,
-            IShapeFactory shapeFactory)
+            IShapeFactory shapeFactory,
+            IOptions<MiniProfilerSettings> options)
         {
             _layoutAccessor = layoutAccessor;
             _shapeFactory = shapeFactory;
+            _settings = options.Value;
         }
 
         public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
         {
             // Should only run on the front-end for a full view
             if ((context.Result is ViewResult || context.Result is PageResult) &&
-                !AdminAttribute.IsApplied(context.HttpContext))
+                (_settings.EnableOnAdmin || !AdminAttribute.IsApplied(context.HttpContext)))
             {
                 dynamic layout = await _layoutAccessor.GetLayoutAsync();
                 var footerZone = layout.Zones["Footer"];
---
+++ src/OrchardCore.Modules/OrchardCore.MiniProfiler/MiniProfilerSettings.cs
@@ -0,0 +1,10 @@
+namespace OrchardCore.MiniProfiler
+{
+    public class MiniProfilerSettings
+    {
+        /// <summary>
+        /// Gets or sets whether to enable Mini Profiler on the admin too, not just the frontend. Defaults to <c>false</c>.
+        /// </summary>
+        public bool EnableOnAdmin { get; set; }
+    }
+}
