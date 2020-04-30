e1a7ecd2f157a4747756df023e91fc4ea251a270
Dean Marcussen <dean.marcussen@gmail.com>
2020-04-30 18:10:06 +0100

    Add a markdown test

--- test/OrchardCore.Tests/Script/HtmlScriptSanitizerTests.cs
+++ test/OrchardCore.Tests/Script/HtmlScriptSanitizerTests.cs
@@ -5,16 +5,31 @@ namespace OrchardCore.Tests.Script
 {
     public class HtmlScriptSanitizerTests
     {
+        private static HtmlScriptSanitizer _sanitizer = new HtmlScriptSanitizer();
+
         [Theory]
         [InlineData("<script>alert('xss')</script><div onload=\"alert('xss')\">Test<img src=\"test.gif\" style=\"background-image: url(javascript:alert('xss')); margin: 10px\"></div>", "<div>Test<img src=\"test.gif\" style=\"margin: 10px\"></div>")]
         [InlineData("<IMG SRC=javascript:alert(\"XSS\")>", @"<img>")]
+        [InlineData("<a href=\"javascript: alert('xss')\">Click me</a>", @"<a>Click me</a>")]
         public void ShouldSanitizeHTML(string html, string sanitized)
         {
             // Setup
-            var sanitizer = new HtmlScriptSanitizer();
+            var output = _sanitizer.Sanitize(html);
 
             // Test
-            Assert.Equal(sanitizer.Sanitize(html), sanitized);
+            Assert.Equal(output, sanitized);
+        }
+
+
+        [Theory]
+        [InlineData("[Click me](javascript:alert('xss'))", "[Click me](javascript:alert('xss'))")]
+        public void DoesNotSanitizeMarkdown(string markdown, string sanitized)
+        {
+            // Setup
+            var output = _sanitizer.Sanitize(markdown);
+
+            // Test
+            Assert.Equal(output, sanitized);
         }
     }
 }
