25de31f84a593013f483b30926e9cefe4cc59a79
hist <hist@outlook.com>
2020-05-02 18:04:56 +0800

    minor name update

--- src/OrchardCore.Modules/OrchardCore.Menu/Models/ContentMenuItemPart.cs
+++ src/OrchardCore.Modules/OrchardCore.Menu/Models/ContentMenuItemPart.cs
@@ -5,7 +5,7 @@ namespace OrchardCore.Menu.Models
     public class ContentMenuItemPart : ContentPart
     {
         /// <summary>
-        /// The name of the link
+        /// The name of the content menu
         /// </summary>
         public string Name { get; set; }
     }
