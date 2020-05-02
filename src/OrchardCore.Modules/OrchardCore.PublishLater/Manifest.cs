4dc2d3e7719a699a422bc4f99182b24aa64ae081
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2020-05-02 20:59:00 +0200

    Using ManifestConstants in Manifest

--- src/OrchardCore.Modules/OrchardCore.PublishLater/Manifest.cs
+++ src/OrchardCore.Modules/OrchardCore.PublishLater/Manifest.cs
@@ -2,9 +2,9 @@ using OrchardCore.Modules.Manifest;
 
 [assembly: Module(
     Name = "Publish Later",
-    Author = "The Orchard Team",
-    Website = "https://orchardcore.ne",
-    Version = "1.0.0-rc2",
+    Author = ManifestConstants.OrchardCoreTeam,
+    Website = ManifestConstants.OrchardCoreWebsite,
+    Version = ManifestConstants.OrchardCoreVersion,
     Description = "The Publish Later module adds the ability to schedule content items to be published at a given future date and time.",
     Dependencies = new[] { "OrchardCore.Contents" },
     Category = "Content Management"
