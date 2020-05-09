9723526760eef5f26df273e5d84693a4b55f47f6
jtkech <jean-thierry.kechichian@wanadoo.fr>
2020-05-10 01:13:31 +0200

    Update before merging dev

--- src/OrchardCore.Modules/OrchardCore.Autoroute/Services/AutorouteEntries.cs
+++ src/OrchardCore.Modules/OrchardCore.Autoroute/Services/AutorouteEntries.cs
@@ -66,7 +66,7 @@ namespace OrchardCore.Autoroute.Services
         public void AddEntries(AutorouteDocument document, IEnumerable<AutorouteEntry> entries)
         {
             // Evict all entries related to a container item from autoroute entries.
-            // This is necesary to account for deletions, disabling of an item, or disabling routing of contained items.
+            // This is necessary to account for deletions, disabling of an item, or disabling routing of contained items.
             foreach (var entry in entries.Where(x => String.IsNullOrEmpty(x.ContainedContentItemId)))
             {
                 var entriesToRemove = document.Paths.Values.Where(x => x.ContentItemId == entry.ContentItemId && !String.IsNullOrEmpty(x.ContainedContentItemId));
