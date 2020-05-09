0bd0ea593bb9e03d46b4a0617eeeb3446526ae66
jtkech <jean-thierry.kechichian@wanadoo.fr>
2020-05-09 09:36:33 +0200

    Fixes MessagePack serialization

--- src/OrchardCore.Modules/OrchardCore.Workflows/Http/Models/WorkflowRoutesEntry.cs
+++ src/OrchardCore.Modules/OrchardCore.Workflows/Http/Models/WorkflowRoutesEntry.cs
@@ -1,14 +1,27 @@
+using System.Collections.Generic;
+using System.Linq;
+using MessagePack;
 using Microsoft.AspNetCore.Routing;
+using Newtonsoft.Json;
 using OrchardCore.Workflows.Helpers;
 
 namespace OrchardCore.Workflows.Http.Models
 {
-    public class WorkflowRoutesEntry
+    public class WorkflowRoutesEntry : IMessagePackSerializationCallbackReceiver
     {
         public string WorkflowId { get; set; }
         public string ActivityId { get; set; }
         public string HttpMethod { get; set; }
-        public RouteValueDictionary RouteValues { get; set; }
+
+        [IgnoreMember]
+        public RouteValueDictionary RouteValues { get; set; } = new RouteValueDictionary();
+
+        [JsonIgnore]
+        public Dictionary<string, object> RouteDataValues { get; set; }
+
+        // 'MessagePack' can't serialize a 'RouteValueDictionary'.
+        public void OnAfterDeserialize() => RouteValues = new RouteValueDictionary(RouteDataValues);
+        public void OnBeforeSerialize() => RouteDataValues = RouteValues.ToDictionary(kv => kv.Key, kv => kv.Value);
 
         public string ControllerName => RouteValues.GetValue<string>("controller");
         public string ActionName => RouteValues.GetValue<string>("action");
