a749c09ec144c5b6c6e74495f26abd87c158b0b9
jtkech <jean-thierry.kechichian@wanadoo.fr>
2020-05-10 08:55:58 +0200

    Implement WorkflowRoutesEntry CorrelationId

--- src/OrchardCore.Modules/OrchardCore.Workflows/Http/Services/WorkflowRouteEntries.cs
+++ src/OrchardCore.Modules/OrchardCore.Workflows/Http/Services/WorkflowRouteEntries.cs
@@ -1,8 +1,5 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.AspNetCore.Routing;
-using OrchardCore.Workflows.Helpers;
 using OrchardCore.Workflows.Http.Activities;
 using OrchardCore.Workflows.Http.Models;
 using OrchardCore.Workflows.Models;
@@ -12,20 +9,6 @@ namespace OrchardCore.Workflows.Http.Services
 {
     internal class WorkflowRouteEntries : WorkflowRouteEntriesBase, IWorkflowInstanceRouteEntries
     {
-        public override IEnumerable<WorkflowRoutesEntry> GetWorkflowRouteEntries(string httpMethod, RouteValueDictionary routeValues)
-        {
-            var entries = base.GetWorkflowRouteEntries(httpMethod, routeValues);
-
-            var correlationId = routeValues.GetValue<string>("correlationid");
-
-            if (String.IsNullOrWhiteSpace(correlationId))
-            {
-                return entries;
-            }
-
-            return entries.Where(x => x.CorrelationId == correlationId).ToArray();
-        }
-
         internal static IEnumerable<WorkflowRoutesEntry> GetWorkflowRoutesEntries(WorkflowType workflowType, Workflow workflow, IActivityLibrary activityLibrary)
         {
             var awaitingActivityIds = workflow.BlockingActivities.Select(x => x.ActivityId).ToDictionary(x => x);
@@ -38,7 +21,6 @@ namespace OrchardCore.Workflows.Http.Services
                     ActivityId = x.ActivityId,
                     HttpMethod = activity.HttpMethod,
                     RouteValues = activity.RouteValues,
-                    CorrelationId = workflow.CorrelationId
                 };
 
                 return entry;
