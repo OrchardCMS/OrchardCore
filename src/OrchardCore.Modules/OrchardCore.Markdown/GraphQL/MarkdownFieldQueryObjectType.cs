885a36b36c9a804970f5435d5d3a7dcc981fb253
Dean Marcussen <dean.marcussen@gmail.com>
2020-05-13 17:31:12 +0100

    graphql markdown html output field

--- src/OrchardCore.Modules/OrchardCore.Markdown/GraphQL/MarkdownFieldQueryObjectType.cs
+++ src/OrchardCore.Modules/OrchardCore.Markdown/GraphQL/MarkdownFieldQueryObjectType.cs
@@ -1,14 +1,23 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using GraphQL.Builders;
 using GraphQL.Types;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Localization;
+using Newtonsoft.Json.Linq;
 using OrchardCore.Apis.GraphQL;
+using OrchardCore.ContentManagement;
 using OrchardCore.ContentManagement.Metadata;
 using OrchardCore.Infrastructure.SafeCodeFilters;
+using OrchardCore.Infrastructure.Script;
 using OrchardCore.Liquid;
 using OrchardCore.Markdown.Fields;
 using OrchardCore.Markdown.Services;
+using OrchardCore.Markdown.Settings;
+using OrchardCore.Markdown.ViewModels;
 
 namespace OrchardCore.Markdown.GraphQL
 {
@@ -18,15 +27,19 @@ namespace OrchardCore.Markdown.GraphQL
         {
             Name = nameof(MarkdownField);
             Description = S["Content stored as Markdown. You can also query the HTML interpreted version of Markdown."];
-
+            
             Field("markdown", x => x.Markdown, nullable: true)
                 .Description(S["the markdown value"]);
 
             Field<StringGraphType>()
                 .Name("html")
                 .Description(S["the HTML representation of the markdown content"])
+                //.me
+                //.FieldWithMeta()
+
                 .ResolveLockedAsync(ToHtml);
         }
+
 
         private static async Task<object> ToHtml(ResolveFieldContext<MarkdownField> ctx)
         {
@@ -36,28 +49,51 @@ namespace OrchardCore.Markdown.GraphQL
             }
 
             var serviceProvider = ctx.ResolveServiceProvider();
-
             var markdownService = serviceProvider.GetRequiredService<IMarkdownService>();
             var safeCodeFilterManager = serviceProvider.GetRequiredService<ISafeCodeFilterManager>();
-            //var contentDefinitionManager = serviceProvider.GetRequiredService<IContentDefinitionManager>();
 
-            //TODO this will not work, because we don not know which part is it on.
-            // need more context. Probably there is something in the registration of graphql to give us that.
-            //var contentTypeDefinition = contentDefinitionManager.GetTypeDefinition(ctx.Source.ContentItem.ContentType);
-            //var contentTypePartDefinition = contentTypeDefinition.Parts.FirstOrDefault(x => string.Equals(x.PartDefinition.Name, "MarkdownBodyPart"));
-            //var settings = contentTypePartDefinition.GetSettings<MarkdownFieldSettings>();
+            var contentDefinitionManager = serviceProvider.GetRequiredService<IContentDefinitionManager>();
+
+            var jObject = ctx.Source.Content as JObject;
+            // The JObject.Path is consistent here even when contained in a bag part.
+            var jsonPath = jObject.Path;
+            var paths = jsonPath.Split('.');
+            var partName = paths[0];
+            var fieldName = paths[1];
+            var contentTypeDefinition = contentDefinitionManager.GetTypeDefinition(ctx.Source.ContentItem.ContentType);
+            var contentPartDefinition = contentTypeDefinition.Parts.FirstOrDefault(x => string.Equals(x.Name, partName));
+            var contentPartFieldDefintion = contentPartDefinition.PartDefinition.Fields.FirstOrDefault(x => string.Equals(x.Name, fieldName));
+
+            var settings = contentPartFieldDefintion.GetSettings<MarkdownFieldSettings>();
 
             var markdown = ctx.Source.Markdown;
-            var liquidTemplateManager = serviceProvider.GetService<ILiquidTemplateManager>();
-            var htmlEncoder = serviceProvider.GetService<HtmlEncoder>();
 
-            markdown = await liquidTemplateManager.RenderAsync(markdown, htmlEncoder);
+            if (settings.AllowCustomScripts)
+            {
+                var liquidTemplateManager = serviceProvider.GetService<ILiquidTemplateManager>();
+                var htmlEncoder = serviceProvider.GetService<HtmlEncoder>();
+
+                var model = new MarkdownFieldViewModel()
+                {
+                    Markdown = ctx.Source.Markdown,
+                    Field = ctx.Source,
+                    Part = ctx.Source.ContentItem.Get<ContentPart>(partName),
+                    PartFieldDefinition = contentPartFieldDefintion
+                };
+
+                markdown = await liquidTemplateManager.RenderAsync(markdown, htmlEncoder, model,
+                    scope => scope.SetValue("ContentItem", ctx.Source.ContentItem));
+            }
 
             markdown = await safeCodeFilterManager.ProcessAsync(markdown);
 
             markdown = markdownService.ToHtml(markdown);
 
-            //TODO sanitize.
+            if (!settings.AllowCustomScripts)
+            {
+                var htmlSanitizer = serviceProvider.GetRequiredService<IHtmlScriptSanitizer>();
+                markdown = htmlSanitizer.Sanitize(markdown);
+            }
 
             return markdown;
         }
