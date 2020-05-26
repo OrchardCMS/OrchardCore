737406f35f33adcf318a3655227b7165f9750712
Kavli-Kaffel <63543200+Kavli-Kaffel@users.noreply.github.com>
2020-05-26 04:05:19 +0300

    Update GoogleAuthenticationSettingsViewModel.cs

--- src/OrchardCore.Modules/OrchardCore.Google/Authentication/ViewModels/GoogleAuthenticationSettingsViewModel.cs
+++ src/OrchardCore.Modules/OrchardCore.Google/Authentication/ViewModels/GoogleAuthenticationSettingsViewModel.cs
@@ -4,12 +4,14 @@ namespace OrchardCore.Google.Authentication.ViewModels
 {
     public class GoogleAuthenticationSettingsViewModel
     {
+        //Error messages for empty fields
         [Required(AllowEmptyStrings = false, ErrorMessage = "ClientID key is required")]
         public string ClientID { get; set; }
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "ClientSecret is required")]
         public string ClientSecret { get; set; }
-
+        
+        //Error message for wrong path and unacceptable symbols
         [RegularExpression(@"\/[-A-Za-z0-9+&@#\/%?=~_|!:,.;]+[-A-Za-z0-9+&@#\/%=~_|]", ErrorMessage = "Invalid path")]
         public string CallbackPath { get; set; }
     }
