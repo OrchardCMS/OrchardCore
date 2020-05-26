e002a533d4d62e07ddede5c8b4f465c95af78408
Antoine Griffard <agriffard@hotmail.com>
2020-05-26 13:11:10 +0200

    Add tags

--- src/OrchardCore.Themes/TheAdmin/Manifest.cs
+++ src/OrchardCore.Themes/TheAdmin/Manifest.cs
@@ -2,10 +2,10 @@ using OrchardCore.DisplayManagement.Manifest;
 using OrchardCore.Modules.Manifest;
 
 [assembly: Theme(
-    Name = "TheAdmin Theme",
+    Name = "The Admin Theme",
     Author = ManifestConstants.OrchardCoreTeam,
     Website = ManifestConstants.OrchardCoreWebsite,
     Version = ManifestConstants.OrchardCoreVersion,
     Description = "The default Admin theme.",
-    Tags = new [] { "admin" }
+    Tags = new [] { "Admin" }
 )]
