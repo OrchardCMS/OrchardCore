49450ea11869cfa0dcc29a2b0b6a71e8644539d1
Lev Platonov <47658822+levplatonov@users.noreply.github.com>
2020-05-27 20:43:05 +0300

    Comment BackgroundTaskIndexViewModel.cs (BackgroundTasks)

--- src/OrchardCore.Modules/OrchardCore.BackgroundTasks/ViewModels/BackgroundTaskIndexViewModel.cs
+++ src/OrchardCore.Modules/OrchardCore.BackgroundTasks/ViewModels/BackgroundTaskIndexViewModel.cs
@@ -1,10 +1,17 @@
+/*
+	The data model (~) of the 'background tasks' list
+	and a pager (~ something like optimizing memory usage) for them
+*/
+
 using System.Collections.Generic;
 
 namespace OrchardCore.BackgroundTasks.ViewModels
 {
     public class BackgroundTaskIndexViewModel
     {
+    	// tasks - list of 'background task settings'
         public IList<BackgroundTaskEntry> Tasks { get; set; }
+        // (~) implements 'swap memory'
         public dynamic Pager { get; set; }
     }
 
