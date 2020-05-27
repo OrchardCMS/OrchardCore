0ddd664d41d269d95667e5f895aa8bfb6eebee2f
Lev Platonov <47658822+levplatonov@users.noreply.github.com>
2020-05-27 20:46:09 +0300

    Comment BackgroundTaskViewModel (BackgroundTasks)

--- src/OrchardCore.Modules/OrchardCore.BackgroundTasks/ViewModels/BackgroundTaskViewModel.cs
+++ src/OrchardCore.Modules/OrchardCore.BackgroundTasks/ViewModels/BackgroundTaskViewModel.cs
@@ -2,6 +2,7 @@ namespace OrchardCore.BackgroundTasks.ViewModels
 {
     public class BackgroundTaskViewModel : BackgroundTaskSettings
     {
+    	// (~) placeholder
         public string DefaultSchedule { get; set; } = "* * * * *";
     }
 }
