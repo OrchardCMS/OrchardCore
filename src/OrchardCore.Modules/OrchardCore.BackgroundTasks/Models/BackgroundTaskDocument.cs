10ee133bd968c74ff736a6ef3779f0c5ee6cbc5c
Lev Platonov <47658822+levplatonov@users.noreply.github.com>
2020-05-27 19:46:59 +0300

    Comment BackgroundTaskDocument (BackgroundTasks)

--- src/OrchardCore.Modules/OrchardCore.BackgroundTasks/Models/BackgroundTaskDocument.cs
+++ src/OrchardCore.Modules/OrchardCore.BackgroundTasks/Models/BackgroundTaskDocument.cs
@@ -1,9 +1,14 @@
+/*
+	background task data model
+*/
+
 using System.Collections.Generic;
 
 namespace OrchardCore.BackgroundTasks.Models
 {
     public class BackgroundTaskDocument
     {
+    	// set of relations 'string (~name) : settings'
         public Dictionary<string, BackgroundTaskSettings> Settings { get; } = new Dictionary<string, BackgroundTaskSettings>();
     }
 }
