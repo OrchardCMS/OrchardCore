36829260a41bdf84cdbb5114111eb5a36dda3189
Lev Platonov <47658822+levplatonov@users.noreply.github.com>
2020-05-27 17:04:16 +0300

    Comment (BackgroundTasks) AdminMenu

--- src/OrchardCore.Modules/OrchardCore.BackgroundTasks/AdminMenu.cs
+++ src/OrchardCore.Modules/OrchardCore.BackgroundTasks/AdminMenu.cs
@@ -1,8 +1,17 @@
+/*
+    AdminMenu class with the specified locale
+
+    The BuildNavigationAsync method (~) adds elements to the navigation panel:
+        
+        Configuration/Tasks/background tasks...
+*/
+
 using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Localization;
 using OrchardCore.Navigation;
 
+// Background tasks
 namespace OrchardCore.BackgroundTasks
 {
     public class AdminMenu : INavigationProvider
@@ -14,13 +23,18 @@ namespace OrchardCore.BackgroundTasks
             S = localizer;
         }
 
+        // method for (~) constructing the navigation layout
+        // @return always 'completed task', (~) followed the signature
         public Task BuildNavigationAsync(string name, NavigationBuilder builder)
         {
-            if (!String.Equals(name, "admin", StringComparison.OrdinalIgnoreCase))
+            // admin check
+            // if FALSE, then 'end'
+            if (!String.Equals(name, "admin", StringComparison.OrdinalIgnoreCase)) 
             {
                 return Task.CompletedTask;
             }
 
+            // build (adding elements) to the layout
             builder
                 .Add(S["Configuration"], configuration => configuration
                     .Add(S["Tasks"], "10", tasks => tasks
