8d490ef7b3a5b9b646f3826cc0742c108c52867e
Lev Platonov <47658822+levplatonov@users.noreply.github.com>
2020-05-27 17:51:39 +0300

    Comment (BackgoundTasks) Permissoins.cs

--- src/OrchardCore.Modules/OrchardCore.BackgroundTasks/Permissions.cs
+++ src/OrchardCore.Modules/OrchardCore.BackgroundTasks/Permissions.cs
@@ -1,3 +1,8 @@
+/*
+	Permission class
+		Gives the 'ManageBackgoundTasks' right for the 'admin' role
+*/
+
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using OrchardCore.Security.Permissions;
@@ -8,16 +13,19 @@ namespace OrchardCore.BackgroundTasks
     {
         public static readonly Permission ManageBackgroundTasks = new Permission("ManageBackgroundTasks", "Manage background tasks");
 
+        // @return 'Permission' object (permission/access/set of rights)
         public Task<IEnumerable<Permission>> GetPermissionsAsync()
         {
             return Task.FromResult(GetPermissions());
         }
 
+        // @return collection of roles including name and set of rights,
+        //         in this case, the role of 'Administrator' with the right 'ManageBackgroundTasks'
         public IEnumerable<PermissionStereotype> GetDefaultStereotypes()
         {
-            return new[]
-            {
-                new PermissionStereotype
+            return new[] 
+            { 
+                new PermissionStereotype 
                 {
                     Name = "Administrator",
                     Permissions = new[] { ManageBackgroundTasks }
@@ -25,6 +33,7 @@ namespace OrchardCore.BackgroundTasks
             };
         }
 
+        // @return set of rights set by all objects of this class
         private IEnumerable<Permission> GetPermissions()
         {
             return new[] { ManageBackgroundTasks };
