b7cd4f3e26dc06a6c474bb34b7376e3f9d49d67a
Dean Marcussen <dean.marcussen@gmail.com>
2020-07-06 09:43:20 +0100

    Update ShortcodeService.cs

--- src/OrchardCore.Modules/OrchardCore.Shortcodes/Services/ShortcodeService.cs
+++ src/OrchardCore.Modules/OrchardCore.Shortcodes/Services/ShortcodeService.cs
@@ -7,13 +7,10 @@ namespace OrchardCore.Shortcodes.Services
     public class ShortcodeService : IShortcodeService
     {
         private readonly ShortcodesProcessor _shortcodesProcessor;
-        private readonly IEnumerable<IShortcodeProvider> _shortcodeProviders;
 
         public ShortcodeService(IEnumerable<IShortcodeProvider> shortcodeProviders)
         {
-            _shortcodeProviders = shortcodeProviders;
             _shortcodesProcessor = new ShortcodesProcessor(shortcodeProviders);
-
         }
 
         public ValueTask<string> ProcessAsync(string input)
