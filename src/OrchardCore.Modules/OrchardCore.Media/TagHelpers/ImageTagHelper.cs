35d98f03f7161904483e98c68f7ecff0bcd42acc
Marcelo Barsotti <mbarsott@gmail.com>
2020-07-08 23:16:06 -0400

    Add behavior to anchor tag with asset-href attribute

--- src/OrchardCore.Modules/OrchardCore.Media/TagHelpers/ImageTagHelper.cs
+++ src/OrchardCore.Modules/OrchardCore.Media/TagHelpers/ImageTagHelper.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
@@ -5,9 +6,12 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
 namespace OrchardCore.Media.TagHelpers
 {
     [HtmlTargetElement("img", Attributes = AssetSrcAttributeName)]
+    [HtmlTargetElement("a", Attributes = AssetHrefAttributeName)]
     public class ImageTagHelper : TagHelper
     {
         private const string AssetSrcAttributeName = "asset-src";
+
+        private const string AssetHrefAttributeName = "asset-href";
 
         private const string AppendVersionAttributeName = "asp-append-version";
 
@@ -22,8 +26,11 @@ namespace OrchardCore.Media.TagHelpers
         [HtmlAttributeName(AssetSrcAttributeName)]
         public string AssetSrc { get; set; }
 
+        [HtmlAttributeName(AssetHrefAttributeName)]
+        public string AssetHref { get; set; }
+
         /// <summary>
-        /// Value indicating if file version should be appended to the src urls.
+        /// Value indicating if file version should be appended to the src or href urls.
         /// </summary>
         /// <remarks>
         /// If <c>true</c> then a query string "v" with the encoded content of the file is added.
@@ -44,17 +51,40 @@ namespace OrchardCore.Media.TagHelpers
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            if (string.IsNullOrEmpty(AssetSrc))
+            string assetPath;
+            string attributeName;
+            if ("img".Equals(context.TagName, StringComparison.InvariantCultureIgnoreCase))
+            {
+                if (string.IsNullOrEmpty(AssetSrc))
+                {
+                    return;
+                }
+                assetPath = AssetSrc;
+                attributeName = "src";
+            }
+            else if ("a".Equals(context.TagName, StringComparison.InvariantCultureIgnoreCase))
+            {
+                if (string.IsNullOrEmpty(AssetHref))
+                {
+                    return;
+                }
+                assetPath = AssetHref;
+                attributeName = "href";
+            }
+            else
             {
                 return;
             }
 
-            var resolvedSrc = _mediaFileStore != null ? _mediaFileStore.MapPathToPublicUrl(AssetSrc) : AssetSrc;
-            output.Attributes.SetAttribute("src", resolvedSrc);
+            var resolvedUrl = _mediaFileStore != null ? _mediaFileStore.MapPathToPublicUrl(assetPath) : assetPath;
 
             if (AppendVersion && _fileVersionProvider != null)
             {
-                output.Attributes.SetAttribute("src", _fileVersionProvider.AddFileVersionToPath(_httpContextAccessor.HttpContext.Request.PathBase, resolvedSrc));
+                output.Attributes.SetAttribute(attributeName, _fileVersionProvider.AddFileVersionToPath(_httpContextAccessor.HttpContext.Request.PathBase, resolvedUrl));
+            }
+            else
+            {
+                output.Attributes.SetAttribute(attributeName, resolvedUrl);
             }
         }
     }
