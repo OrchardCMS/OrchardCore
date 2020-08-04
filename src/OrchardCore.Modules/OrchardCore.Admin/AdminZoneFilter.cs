250ac7f5bb8ccbd7e2bcfd43cf03e67a362d7e62
hishamco <hishamco_2007@yahoo.com>
2020-08-04 10:59:34 +0300

    Use admin url prefix instead of magi string

--- src/OrchardCore.Modules/OrchardCore.Admin/AdminZoneFilter.cs
+++ src/OrchardCore.Modules/OrchardCore.Admin/AdminZoneFilter.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
 
 namespace OrchardCore.Admin
 {
@@ -11,6 +12,12 @@ namespace OrchardCore.Admin
     /// </summary>
     public class AdminZoneFilter : IAsyncResourceFilter
     {
+        private readonly string _adminUrlPrefix;
+
+        public AdminZoneFilter(IOptions<AdminOptions> adminOptions)
+        {
+            _adminUrlPrefix = adminOptions.Value.AdminUrlPrefix;
+        }
         public Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
         {
             if (context.ActionDescriptor is ControllerActionDescriptor action)
@@ -22,7 +29,7 @@ namespace OrchardCore.Admin
             }
             else if (context.ActionDescriptor is PageActionDescriptor page)
             {
-                if (page.ViewEnginePath.Contains("/Admin/"))
+                if (page.ViewEnginePath.Contains(_adminUrlPrefix))
                 {
                     AdminAttribute.Apply(context.HttpContext);
                 }
