764c6112ca9a5f1d0073a93157a2363047bccaff
Antoine Griffard <agriffard@hotmail.com>
2020-08-16 15:39:39 +0200

    React to review

--- src/OrchardCore.Modules/OrchardCore.Apis.GraphQL/SchemaMiddleware.cs
+++ src/OrchardCore.Modules/OrchardCore.Apis.GraphQL/SchemaMiddleware.cs
@@ -7,25 +7,16 @@ namespace OrchardCore.Apis.GraphQL
 {
     public class SchemaMiddleware
     {
-        private readonly RequestDelegate _next;
-
-        public SchemaMiddleware(RequestDelegate next)
-        {
-            _next = next;
-        }
-
         public async Task Invoke(HttpContext context, ISchemaFactory schemaService)
         {
             var schema = await schemaService.GetSchemaAsync();
 
             using (var printer = new SchemaPrinter(schema))
             {
-                context.Response.ContentType = "application/text";
+                context.Response.ContentType = "application/json";
                 context.Response.StatusCode = (int)HttpStatusCode.OK;
 
                 await context.Response.WriteAsync(printer.Print());
-
-                return;
             }
         }
     }
