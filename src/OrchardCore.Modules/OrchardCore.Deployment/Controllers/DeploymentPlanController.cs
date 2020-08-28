dafae70046720ef89faaeaa6e93c997510da2f9d
Dean Marcussen <dean.marcussen@gmail.com>
2020-08-28 09:00:31 +0100

    Ignore case when comparing plannames

--- src/OrchardCore.Modules/OrchardCore.Deployment/Controllers/DeploymentPlanController.cs
+++ src/OrchardCore.Modules/OrchardCore.Deployment/Controllers/DeploymentPlanController.cs
@@ -283,7 +283,7 @@ namespace OrchardCore.Deployment.Controllers
                 {
                     ModelState.AddModelError(nameof(EditDeploymentPlanViewModel.Name), S["The name is mandatory."]);
                 }
-                if (model.Name != deploymentPlan.Name)
+                if (!String.Equals(model.Name, deploymentPlan.Name, StringComparer.OrdinalIgnoreCase))
                 {
                     var count = await _session.QueryIndex<DeploymentPlanIndex>(x => x.Name == model.Name && x.DocumentId != model.Id).CountAsync();
                     if (count > 0)
