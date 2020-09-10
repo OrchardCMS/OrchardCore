b8c9dc07b4237cfa9e101e6db1531332cb59027c
Antoine Griffard <agriffard@hotmail.com>
2020-09-10 11:36:35 +0200

    ChallengeOrForbid in Api controllers
    Fixes #7039

--- src/OrchardCore/OrchardCore.Mvc.Core/Utilities/ControllerExtensions.cs
+++ src/OrchardCore/OrchardCore.Mvc.Core/Utilities/ControllerExtensions.cs
@@ -8,11 +8,18 @@ namespace OrchardCore.Mvc.Utilities
         /// Returns the proper actionresult for unauthorized or unauthenticated users.
         /// Will return a forbid when the user is authenticated.
         /// Will return a challenge when the user is not authenticated.
+        /// If a authentication scheme is specified, will return a challenge to it.
         /// </summary>
         /// <param name="controller"></param>
+        /// <param name="authenticationScheme"></param>
         /// <returns>The proper actionresult based upon if the user is authenticated</returns>
-        public static ActionResult ChallengeOrForbid(this Controller controller)
+        public static ActionResult ChallengeOrForbid(this Controller controller, string authenticationScheme = null)
         {
+            if (!string.IsNullOrEmpty(authenticationScheme))
+            {
+                return controller.User.Identity.IsAuthenticated ? (ActionResult)controller.Forbid() : (ActionResult)controller.Challenge(new string[] { authenticationScheme });
+            }
+
             return controller.User.Identity.IsAuthenticated ? (ActionResult)controller.Forbid() : (ActionResult)controller.Challenge();
         }
     }
