9ee424a76dd8a564613ff50668169f9413a9dd1f
ns8482e <23270244+ns8482e@users.noreply.github.com>
2020-09-18 10:08:35 -0500

    removed oc- prefix

--- src/OrchardCore.Modules/OrchardCore.Templates/ResourceManifest.cs
+++ src/OrchardCore.Modules/OrchardCore.Templates/ResourceManifest.cs
@@ -9,9 +9,9 @@ namespace OrchardCore.Templates
             var manifest = builder.Add();
 
             manifest
-                .DefineScript("oc-templatepreview-edit")
+                .DefineScript("templatepreview-edit")
                 .SetUrl("~/OrchardCore.Templates/Scripts/templatepreview.edit.min.js", "~/OrchardCore.Templates/Scripts/templatepreview.edit.js")
-                .SetVersion("1.0");
+                .SetVersion("1.0.0");
         }
     }
 }
