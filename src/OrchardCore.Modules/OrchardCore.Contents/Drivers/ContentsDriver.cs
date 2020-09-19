e035e26071302314b47e5d7bacd8f546ca33c258
Jasmin Savard <jasmin.savard@gmail.com>
2020-09-19 02:27:02 -0400

    WIP

--- src/OrchardCore.Modules/OrchardCore.Contents/Drivers/ContentsDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.Contents/Drivers/ContentsDriver.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using OrchardCore.ContentManagement;
 using OrchardCore.ContentManagement.Display.ContentDisplay;
 using OrchardCore.ContentManagement.Display.ViewModels;
 using OrchardCore.ContentManagement.Metadata;
 using OrchardCore.ContentManagement.Metadata.Settings;
+using OrchardCore.Contents.Core;
 using OrchardCore.DisplayManagement.ModelBinding;
 using OrchardCore.DisplayManagement.Views;
 
@@ -13,20 +16,25 @@ namespace OrchardCore.Contents.Drivers
     public class ContentsDriver : ContentDisplayDriver
     {
         private readonly IContentDefinitionManager _contentDefinitionManager;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public ContentsDriver(IContentDefinitionManager contentDefinitionManager)
+        public ContentsDriver(
+            IContentDefinitionManager contentDefinitionManager,
+            IHttpContextAccessor httpContextAccessor)
         {
             _contentDefinitionManager = contentDefinitionManager;
+            _httpContextAccessor = httpContextAccessor;
         }
 
-        public override IDisplayResult Display(ContentItem model, IUpdateModel updater)
+        public override async Task<IDisplayResult> DisplayAsync(ContentItem model, IUpdateModel updater)
         {
             // We add custom alternates. This could be done generically to all shapes coming from ContentDisplayDriver but right now it's
             // only necessary on this shape. Otherwise c.f. ContentPartDisplayDriver
 
-            var contentsMetadataShape = Shape("ContentsMetadata", new ContentItemViewModel(model)).Location("Detail", "Content:before");
-
+            var context = _httpContextAccessor.HttpContext;
+            var results = new List<IDisplayResult>();
             var contentTypeDefinition = _contentDefinitionManager.GetTypeDefinition(model.ContentType);
+            var contentsMetadataShape = Shape("ContentsMetadata", new ContentItemViewModel(model)).Location("Detail", "Content:before");
 
             if (contentTypeDefinition != null)
             {
@@ -44,34 +52,56 @@ namespace OrchardCore.Contents.Drivers
                         ctx.Shape.Metadata.Alternates.Add($"{stereotype}__ContentsMetadata");
                     }
                 });
+
+                results.Add(contentsMetadataShape);
+
+                var hasEditPermission = await ContentTypeAuthorizationHelper.AuthorizeDynamicPermissionAsync(context, Permissions.EditContent, model);
+
+                if (hasEditPermission)
+                {
+                    results.Add(Shape("Contents_SummaryAdmin__Button__Edit", new ContentItemViewModel(model)).Location("SummaryAdmin", "Actions:10"));
+                }
+
+                var hasPublishPermission = await ContentTypeAuthorizationHelper.AuthorizeDynamicPermissionAsync(context, Permissions.PublishContent, model);
+
+                if (hasPublishPermission)
+                {
+                    results.Add(Shape("Contents_SummaryAdmin__Button__Actions", new ContentItemViewModel(model)).Location("SummaryAdmin", "Actions:10"));
+                }
             }
 
-            return Combine(
-                contentsMetadataShape,
-                Shape("Contents_SummaryAdmin__Tags", new ContentItemViewModel(model)).Location("SummaryAdmin", "Tags:10"),
-                Shape("Contents_SummaryAdmin__Meta", new ContentItemViewModel(model)).Location("SummaryAdmin", "Meta:20"),
-                Shape("Contents_SummaryAdmin__Button__Edit", new ContentItemViewModel(model)).Location("SummaryAdmin", "Actions:10"),
-                Shape("Contents_SummaryAdmin__Button__Actions", new ContentItemViewModel(model)).Location("SummaryAdmin", "ActionsMenu:10")
-            );
+            results.Add(Shape("Contents_SummaryAdmin__Tags", new ContentItemViewModel(model)).Location("SummaryAdmin", "Tags:10"));
+            results.Add(Shape("Contents_SummaryAdmin__Meta", new ContentItemViewModel(model)).Location("SummaryAdmin", "Meta:20"));
+
+            return Combine(results.ToArray());
         }
 
-        public override IDisplayResult Edit(ContentItem contentItem, IUpdateModel updater)
+        public override async Task<IDisplayResult> EditAsync(ContentItem contentItem, IUpdateModel updater)
         {
+            var context = _httpContextAccessor.HttpContext;
             var contentTypeDefinition = _contentDefinitionManager.GetTypeDefinition(contentItem.ContentType);
+            var results = new List<IDisplayResult>();
 
             if (contentTypeDefinition == null)
             {
                 return null;
             }
 
-            var results = new List<IDisplayResult>
-            {
-                Dynamic("Content_PublishButton").Location("Actions:10"),
-            };
+            var hasPublishPermission = await ContentTypeAuthorizationHelper.AuthorizeDynamicPermissionAsync(context, Permissions.PublishContent, contentItem);
 
-            if (contentTypeDefinition.GetSettings<ContentTypeSettings>().Draftable)
+            if (hasPublishPermission)
             {
-                results.Add(Dynamic("Content_SaveDraftButton").Location("Actions:20"));
+                results.Add(Dynamic("Content_PublishButton").Location("Actions:10"));
+            }
+
+            var hasEditPermission = await ContentTypeAuthorizationHelper.AuthorizeDynamicPermissionAsync(context, Permissions.EditContent, contentItem);
+
+            if (hasEditPermission)
+            {
+                if (contentTypeDefinition.GetSettings<ContentTypeSettings>().Draftable)
+                {
+                    results.Add(Dynamic("Content_SaveDraftButton").Location("Actions:20"));
+                }
             }
 
             return Combine(results.ToArray());
