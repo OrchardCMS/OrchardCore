5f81a4d6113fdf37abacdab71a32e2e88b976573
Carl Woodhouse <carl.woodhouse@patient.info>
2020-09-21 23:58:41 +0100

    cleanup usings

--- src/OrchardCore/OrchardCore.ContentManagement.GraphQL/Queries/Predicates/PredicateQuery.cs
+++ src/OrchardCore/OrchardCore.ContentManagement.GraphQL/Queries/Predicates/PredicateQuery.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using OrchardCore.ContentManagement.Records;
 using OrchardCore.Environment.Shell;
 using YesSql;
 
