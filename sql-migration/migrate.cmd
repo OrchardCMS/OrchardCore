63d2b72b4dd41e25322dc22a9badb4b8deadf334
Jasmin Savard <jasmin.savard@gmail.com>
2020-09-26 04:25:14 -0400

    wip

---
+++ sql-migration/migrate.cmd
@@ -0,0 +1,4 @@
+.\sqlite3
+.open E:/Repositories/OrchardCore/src/OrchardCore.Cms.Web/App_Data/Sites/AutoSetupExample1/yessql.db
+
+.\sqlite3 E:/Repositories/OrchardCore/src/OrchardCore.Cms.Web/App_Data/Sites/AutoSetupExample1/yessql.db > sqlite-1.sql
