32b06c4433ebb644d7ffba93caaeaa6ba546f77d
jtkech <jean-thierry.kechichian@wanadoo.fr>
2020-09-30 23:56:45 +0200

    Revert another change

--- src/OrchardCore/OrchardCore.Infrastructure/Shells.Database/Extensions/DatabaseShellsOrchardCoreBuilderExtensions.cs
+++ src/OrchardCore/OrchardCore.Infrastructure/Shells.Database/Extensions/DatabaseShellsOrchardCoreBuilderExtensions.cs
@@ -7,7 +7,7 @@ namespace Microsoft.Extensions.DependencyInjection
     public static class DatabaseShellsOrchardCoreBuilderExtensions
     {
         /// <summary>
-        /// Registers host services to load shells settings and configuration from the database.
+        /// Host services to load shells settings and configuration from database.
         /// </summary>
         public static OrchardCoreBuilder AddDatabaseShellsConfiguration(this OrchardCoreBuilder builder)
         {
