d6d9a7e8cc1b575f0db0925e609db84d0b48af97
Jasmin Savard <jasmin.savard@gmail.com>
2020-09-30 16:36:31 -0400

    Change comment

--- src/OrchardCore.Modules/OrchardCore.Users/Drivers/UserDisplayDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.Users/Drivers/UserDisplayDriver.cs
@@ -98,7 +98,7 @@ namespace OrchardCore.Users.Drivers
             {
                 if (usersOfAdminRole.Count == 1 && usersOfAdminRole.First().UserName == user.UserName)
                 {
-                    _notifier.Warning(H["Cannot disable the super administrator."]);
+                    _notifier.Warning(H["Cannot disable the only administrator."]);
                 }
                 else
                 {
