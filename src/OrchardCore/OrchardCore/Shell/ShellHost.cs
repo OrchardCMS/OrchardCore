cc88b71c53d38c01e87cd8baa45de128d9c64fa7
Jean-Thierry Kéchichian <jean-thierry.kechichian@wanadoo.fr>
2020-10-07 08:07:42 +0200

    Ensures shell terminating and disposing (#7158)

--- src/OrchardCore/OrchardCore/Shell/ShellHost.cs
+++ src/OrchardCore/OrchardCore/Shell/ShellHost.cs
@@ -92,7 +92,6 @@ namespace OrchardCore.Environment.Shell
                     finally
                     {
                         semaphore.Release();
-                        _shellSemaphores.TryRemove(settings.Name, out semaphore);
                     }
                 }
 
