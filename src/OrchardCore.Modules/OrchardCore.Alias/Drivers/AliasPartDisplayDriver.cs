261863f3b3edbc0e10543721d700fc8d1e5aa0eb
Michael Petrinolis <michael.petrinolis@outlook.com>
2020-10-14 16:10:44 +0300

    use validating handlers from Content Display Manager (#6195)

--- src/OrchardCore.Modules/OrchardCore.Alias/Drivers/AliasPartDisplayDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.Alias/Drivers/AliasPartDisplayDriver.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Localization;
 using OrchardCore.Alias.Indexes;
@@ -39,7 +40,8 @@ namespace OrchardCore.Alias.Drivers
         {
             await updater.TryUpdateModelAsync(model, Prefix, t => t.Alias);
 
-            await ValidateAsync(model, updater);
+            var errors = await context.ValidateAsync(model);
+            updater.ModelState.BindValidationResults(Prefix, errors);
 
             return Edit(model, context);
         }
@@ -52,17 +54,5 @@ namespace OrchardCore.Alias.Drivers
             model.Settings = settings;
         }
 
-        private async Task ValidateAsync(AliasPart alias, IUpdateModel updater)
-        {
-            if (alias.Alias?.Length > MaxAliasLength)
-            {
-                updater.ModelState.AddModelError(Prefix, nameof(alias.Alias), S["Your alias is too long. The alias can only be up to {0} characters.", MaxAliasLength]);
-            }
-
-            if (alias.Alias != null && (await _session.QueryIndex<AliasPartIndex>(o => o.Alias == alias.Alias && o.ContentItemId != alias.ContentItem.ContentItemId).CountAsync()) > 0)
-            {
-                updater.ModelState.AddModelError(Prefix, nameof(alias.Alias), S["Your alias is already in use."]);
-            }
-        }
     }
 }
