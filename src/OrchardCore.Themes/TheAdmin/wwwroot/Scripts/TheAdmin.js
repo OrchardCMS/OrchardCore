5b4d5e21cd12c054162d50be08539c15739647fb
Jasmin Savard <jasmin.savard@gmail.com>
2020-10-24 03:13:59 -0400

    renaming assets

--- src/OrchardCore.Themes/TheAdmin/wwwroot/Scripts/TheAdmin.js
+++ src/OrchardCore.Themes/TheAdmin/wwwroot/Scripts/TheAdmin.js
@@ -7265,15 +7265,15 @@ function isNumber(str) {
 var darkmode = darkmode === undefined ? false : darkmode;
 $(function () {
   if ($('body').hasClass('darkmode') || darkmode) {
-    $('#admin-css').attr('href', $('#admin-css').attr('href').replace('/TheAdmin.css', '/TheAdmin-dark.css'));
+    $('#admin-css').attr('href', $('#admin-css').attr('href').replace('/TheAdmin.css', '/TheAdmin-darkmode.css'));
   }
 
   $("#btn-darkmode").click(function () {
     if (darkmode) {
-      $('#admin-css').attr('href', $('#admin-css').attr('href').replace('/TheAdmin-dark.css', '/TheAdmin.css'));
+      $('#admin-css').attr('href', $('#admin-css').attr('href').replace('/TheAdmin-darkmode.css', '/TheAdmin.css'));
       darkmode = false;
     } else {
-      $('#admin-css').attr('href', $('#admin-css').attr('href').replace('/TheAdmin.css', '/TheAdmin-dark.css'));
+      $('#admin-css').attr('href', $('#admin-css').attr('href').replace('/TheAdmin.css', '/TheAdmin-darkmode.css'));
       darkmode = true;
     }
 
