1909e9bdd9371f7b53718ed22ed57a170f74114a
hishamco <hishamco_2007@yahoo.com>
2020-10-27 03:47:56 +0300

    Add proper icon while switching between themes

--- src/OrchardCore.Themes/TheAdmin/Assets/js/darkmode.js
+++ src/OrchardCore.Themes/TheAdmin/Assets/js/darkmode.js
@@ -3,12 +3,14 @@ $("#btn-darkmode").click(function() {
     {
         $('#admin-default').attr('media', 'all');
         $('#admin-darkmode').attr('media', 'not all');
+        $(this).children(':first').addClass('fa-moon');
         darkMode = false;
     }
     else
     {
         $('#admin-default').attr('media', 'not all');
         $('#admin-darkmode').attr('media', 'all');
+        $(this).children(':first').addClass('fa-sun');
         darkMode = true;
     }
 
