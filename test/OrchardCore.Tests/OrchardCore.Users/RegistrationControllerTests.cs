f274bdcc61ee84e97b71eaaac4247c5e93b815ce
hishamco <hishamco_2007@yahoo.com>
2020-10-31 16:44:54 +0300

    Refactoring

--- test/OrchardCore.Tests/OrchardCore.Users/RegistrationControllerTests.cs
+++ test/OrchardCore.Tests/OrchardCore.Users/RegistrationControllerTests.cs
@@ -31,29 +31,15 @@ namespace OrchardCore.Tests.OrchardCore.Users
         [Fact]
         public async Task UsersShouldNotBeAbleToRegisterIfNotAllowed()
         {
-            var mockUserManager = UsersMockHelper.MockUserManager<IUser>().Object;
-            var settings = new RegistrationSettings { UsersCanRegister = UserRegistrationType.NoRegistration };
-            var mockSiteService = Mock.Of<ISiteService>(ss =>
-                ss.GetSiteSettingsAsync() == Task.FromResult(
-                    Mock.Of<ISite>(s => s.Properties == JObject.FromObject(new { RegistrationSettings = settings }))
-                    )
-            );
-            var mockSmtpService = Mock.Of<ISmtpService>();
+            // Arrange
+            var controller = SetupRegistrationController(new RegistrationSettings {
+                UsersCanRegister = UserRegistrationType.NoRegistration
+            });
 
-            var controller = new RegistrationController(
-                mockUserManager,
-                Mock.Of<IAuthorizationService>(),
-                mockSiteService,
-                Mock.Of<INotifier>(),
-                Mock.Of<IEmailAddressValidator>(),
-                Mock.Of<ILogger<RegistrationController>>(),
-                Mock.Of<IHtmlLocalizer<RegistrationController>>(),
-                Mock.Of<IStringLocalizer<RegistrationController>>());
-
+            // Act & Assert
             var result = await controller.Register();
             Assert.IsType<NotFoundResult>(result);
 
-            // Post
             result = await controller.Register(new RegisterViewModel());
             Assert.IsType<NotFoundResult>(result);
         }
@@ -61,65 +47,13 @@ namespace OrchardCore.Tests.OrchardCore.Users
         [Fact]
         public async Task UsersShouldBeAbleToRegisterIfAllowed()
         {
-            var mockUserManager = UsersMockHelper.MockUserManager<IUser>().Object;
-            var settings = new RegistrationSettings { UsersCanRegister = UserRegistrationType.AllowRegistration };
-            var mockSiteService = Mock.Of<ISiteService>(ss =>
-                ss.GetSiteSettingsAsync() == Task.FromResult(
-                    Mock.Of<ISite>(s => s.Properties == JObject.FromObject(new { RegistrationSettings = settings }))
-                    )
-            );
-            var mockSmtpService = Mock.Of<ISmtpService>();
-            var mockStringLocalizer = new Mock<IStringLocalizer<RegistrationController>>();
-            mockStringLocalizer.Setup(l => l[It.IsAny<string>()])
-                .Returns<string>(s => new LocalizedString(s, s));
+            // Arrange
+            var controller = SetupRegistrationController();
 
-            var controller = new RegistrationController(
-                mockUserManager,
-                Mock.Of<IAuthorizationService>(),
-                mockSiteService,
-                Mock.Of<INotifier>(),
-                Mock.Of<IEmailAddressValidator>(),
-                Mock.Of<ILogger<RegistrationController>>(),
-                Mock.Of<IHtmlLocalizer<RegistrationController>>(),
-                mockStringLocalizer.Object);
-
-            var mockServiceProvider = new Mock<IServiceProvider>();
-            mockServiceProvider
-                .Setup(x => x.GetService(typeof(ISmtpService)))
-                .Returns(mockSmtpService);
-            mockServiceProvider
-                .Setup(x => x.GetService(typeof(UserManager<IUser>)))
-                .Returns(mockUserManager);
-            mockServiceProvider
-                .Setup(x => x.GetService(typeof(ISiteService)))
-                .Returns(mockSiteService);
-            mockServiceProvider
-                .Setup(x => x.GetService(typeof(IEnumerable<IRegistrationFormEvents>)))
-                .Returns(Enumerable.Empty<IRegistrationFormEvents>());
-            mockServiceProvider
-                .Setup(x => x.GetService(typeof(IUserService)))
-                .Returns(Mock.Of<IUserService>());
-            mockServiceProvider
-                .Setup(x => x.GetService(typeof(SignInManager<IUser>)))
-                .Returns(MockSignInManager(mockUserManager).Object);
-            mockServiceProvider
-                .Setup(x => x.GetService(typeof(ITempDataDictionaryFactory)))
-                .Returns(Mock.Of<ITempDataDictionaryFactory>());
-            mockServiceProvider
-                .Setup(x => x.GetService(typeof(IObjectModelValidator)))
-                .Returns(Mock.Of<IObjectModelValidator>());
-
-            var mockHttpContext = new Mock<HttpContext>();
-            mockHttpContext
-                .Setup(x => x.RequestServices)
-                .Returns(mockServiceProvider.Object);
-
-            controller.ControllerContext.HttpContext = mockHttpContext.Object;
-
+            // Act & Assert
             var result = await controller.Register();
             Assert.IsType<ViewResult>(result);
 
-            // Post
             result = await controller.Register(new RegisterViewModel());
             Assert.IsType<ViewResult>(result);
         }
@@ -128,6 +62,42 @@ namespace OrchardCore.Tests.OrchardCore.Users
         public async Task UsersShouldNotBeAbleToRegisterIfEmailIsDuplicate()
         {
             // Arrange
+            var controller = SetupRegistrationController();
+
+            // Act
+            var result = await controller.Register(new RegisterViewModel { UserName = "SuoerAdmin", Email = "admin@orchardcore.net" });
+            result = await controller.Register(new RegisterViewModel { UserName = "Admin", Email = "admin@orchardcore.net" });
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+            Assert.False(controller.ViewData.ModelState.IsValid);
+            Assert.True(controller.ViewData.ModelState["Email"].Errors.Count == 1);
+            Assert.Equal("A user with the same email already exists.", controller.ViewData.ModelState["Email"].Errors[0].ErrorMessage);
+        }
+
+        private static Mock<SignInManager<TUser>> MockSignInManager<TUser>(UserManager<TUser> userManager = null) where TUser : class
+        {
+            var context = new Mock<HttpContext>();
+            var manager = userManager ?? UsersMockHelper.MockUserManager<TUser>().Object;
+
+            return new Mock<SignInManager<TUser>>(
+                manager,
+                new HttpContextAccessor { HttpContext = context.Object },
+                Mock.Of<IUserClaimsPrincipalFactory<TUser>>(),
+                null,
+                null,
+                null,
+                null)
+            { CallBase = true };
+        }
+
+        private static RegistrationController SetupRegistrationController()
+            => SetupRegistrationController(new RegistrationSettings {
+                UsersCanRegister = UserRegistrationType.AllowRegistration
+            });
+
+        private static RegistrationController SetupRegistrationController(RegistrationSettings registrationSettings)
+        {
             var users = new List<IUser>();
             var mockUserManager = UsersMockHelper.MockUserManager<IUser>();
             mockUserManager.Setup(um => um.FindByEmailAsync(It.IsAny<string>()))
@@ -137,10 +107,9 @@ namespace OrchardCore.Tests.OrchardCore.Users
                     return Task.FromResult(user);
                 });
 
-            var settings = new RegistrationSettings { UsersCanRegister = UserRegistrationType.AllowRegistration };
             var mockSiteService = Mock.Of<ISiteService>(ss =>
                 ss.GetSiteSettingsAsync() == Task.FromResult(
-                    Mock.Of<ISite>(s => s.Properties == JObject.FromObject(new { RegistrationSettings = settings }))
+                    Mock.Of<ISite>(s => s.Properties == JObject.FromObject(new { RegistrationSettings = registrationSettings }))
                     )
             );
             var mockSmtpService = Mock.Of<ISmtpService>();
@@ -195,30 +164,7 @@ namespace OrchardCore.Tests.OrchardCore.Users
 
             controller.ControllerContext.HttpContext = mockHttpContext.Object;
 
-            // Act
-            var result = await controller.Register(new RegisterViewModel { UserName = "SuoerAdmin", Email = "admin@orchardcore.net" });
-            result = await controller.Register(new RegisterViewModel { UserName = "Admin", Email = "admin@orchardcore.net" });
-
-            // Assert
-            Assert.IsType<ViewResult>(result);
-            Assert.False(controller.ViewData.ModelState.IsValid);
-            Assert.True(controller.ViewData.ModelState["Email"].Errors.Count == 1);
-            Assert.Equal("A user with the same email already exists.", controller.ViewData.ModelState["Email"].Errors[0].ErrorMessage);
-        }
-
-        public static Mock<SignInManager<TUser>> MockSignInManager<TUser>(UserManager<TUser> userManager = null) where TUser : class
-        {
-            var context = new Mock<HttpContext>();
-            var manager = userManager ?? UsersMockHelper.MockUserManager<TUser>().Object;
-            return new Mock<SignInManager<TUser>>(
-                manager,
-                new HttpContextAccessor { HttpContext = context.Object },
-                Mock.Of<IUserClaimsPrincipalFactory<TUser>>(),
-                null,
-                null,
-                null,
-                null)
-            { CallBase = true };
+            return controller;
         }
     }
 }
