29bbc87aac023f4e6455b1b1eb94a0373c616b75
Jean-Philippe Tissot <dev@tissotweb.ca>
2020-11-02 09:18:31 -0500

    Two ways of accessing object

--- test/OrchardCore.Tests/Scripting/JIntTests.cs
+++ test/OrchardCore.Tests/Scripting/JIntTests.cs
@@ -11,13 +11,22 @@ namespace OrchardCore.Tests.Html
 {
     public class JintTests
     {
+
+        private object Evaluate(IEnumerable<GlobalMethod> methods, string script)
+        {
+            var mockServiceProvider = new Mock<IServiceProvider>();
+            var engine = new JavaScriptEngine(new MemoryCache(new MemoryCacheOptions()));
+            var scope = engine.CreateScope(methods, mockServiceProvider.Object, null, null);
+            return engine.Evaluate(scope, script);
+        }
         [Fact]
-        public void CanAccessJObjectProperties()
+        public void CanAccessPropertiesWithAccessor()
         {
             var methods = new List<GlobalMethod>();
 
-            methods.Add(new GlobalMethod(){
-                Name= "GetJObject",
+            methods.Add(new GlobalMethod()
+            {
+                Name = "GetJObject",
                 Method = serviceProvider => (Func<JObject>)(() =>
                 {
                     return new JObject
@@ -26,13 +35,26 @@ namespace OrchardCore.Tests.Html
                     };
                 })
             });
-            var mockServiceProvider = new Mock<IServiceProvider>();
-            var engine = new JavaScriptEngine(new MemoryCache(new MemoryCacheOptions()));
-            var scope = engine.CreateScope(methods, mockServiceProvider.Object, null, null);
 
-           var returnValue = engine.Evaluate(scope, "return GetJObject().name == 'test-name';");
+            Assert.Equal(true, Evaluate(methods, "return GetJObject()['name'] == 'test-name';"));
+        }
+        [Fact]
+        public void CanAccessPropertiesDotValue()
+        {
+            var methods = new List<GlobalMethod>();
 
-           Assert.Equal(true, returnValue);
+            methods.Add(new GlobalMethod()
+            {
+                Name = "GetJObject",
+                Method = serviceProvider => (Func<JObject>)(() =>
+                {
+                    return new JObject
+                    {
+                        new JProperty("name", "test-name")
+                    };
+                })
+            });
+            Assert.Equal(true, Evaluate(methods, "return GetJObject().name== 'test-name';"));
         }
     }
 }
