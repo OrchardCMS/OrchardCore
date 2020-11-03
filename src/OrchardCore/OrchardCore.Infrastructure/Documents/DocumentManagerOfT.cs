f16eb299376f1af4f256affa0403cab9145bb222
Jean-Thierry Kéchichian <jean-thierry.kechichian@wanadoo.fr>
2020-11-03 07:29:59 +0100

    Improves Document Options and Compression (#7393)
    Improves document options (per document type) configuration management
    Allows to define through document options a custom serializer (per document type)
    The default serializer compresses data if > configurable threshold (10Kb by default)

--- src/OrchardCore/OrchardCore.Infrastructure/Documents/DocumentManagerOfT.cs
+++ src/OrchardCore/OrchardCore.Infrastructure/Documents/DocumentManagerOfT.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
 using OrchardCore.Data.Documents;
 using OrchardCore.Documents.Options;
 
@@ -15,7 +16,7 @@ namespace OrchardCore.Documents
             TDocumentStore documentStore,
             IDistributedCache distributedCache,
             IMemoryCache memoryCache,
-            DocumentOptions<TDocument> options)
+            IOptionsSnapshot<DocumentOptions> options)
             : base(documentStore, distributedCache, memoryCache, options)
         {
         }
