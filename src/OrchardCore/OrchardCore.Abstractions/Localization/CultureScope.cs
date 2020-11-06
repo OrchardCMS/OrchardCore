7013d7f0e396b27de743ae4c85397729bb7fa149
hishamco <hishamco_2007@yahoo.com>
2020-11-07 00:34:46 +0300

    Use CultureScope in unit tests

--- src/OrchardCore/OrchardCore.Abstractions/Localization/CultureScope.cs
+++ src/OrchardCore/OrchardCore.Abstractions/Localization/CultureScope.cs
@@ -5,19 +5,40 @@ namespace OrchardCore.Localization
 {
     public sealed class CultureScope : IDisposable
     {
-        private CultureScope(CultureInfo culture)
+        private readonly CultureInfo _originalCulture;
+        private readonly CultureInfo _originalUICulture;
+
+        private CultureScope(CultureInfo culture, CultureInfo uiCulture)
         {
             Culture = culture;
-            CultureInfo.CurrentUICulture = culture;
+            UICulture = uiCulture;
+            _originalCulture = CultureInfo.CurrentCulture;
+            _originalUICulture = CultureInfo.CurrentUICulture;
+
+            SetCultures(culture, uiCulture);
         }
 
-        public CultureInfo Culture { get; private set; }
+        public CultureInfo Culture { get; }
 
-        public static CultureScope Create(CultureInfo culture) => new CultureScope(culture);
+        public CultureInfo UICulture { get; }
+
+        public static CultureScope Create(string culture) => Create(culture, culture);
+
+        public static CultureScope Create(string culture, string uiCulture)
+            => Create(CultureInfo.GetCultureInfo(culture), CultureInfo.GetCultureInfo(uiCulture));
+
+        public static CultureScope Create(CultureInfo culture) => Create(culture, culture);
+
+        public static CultureScope Create(CultureInfo culture, CultureInfo uiCulture) => new CultureScope(culture, uiCulture);
 
         public void Dispose()
         {
-            CultureInfo.CurrentUICulture = Culture;
+            SetCultures(_originalCulture, _originalUICulture);
+        }
+        private static void SetCultures(CultureInfo culture, CultureInfo uiCulture)
+        {
+            CultureInfo.CurrentCulture = culture;
+            CultureInfo.CurrentUICulture = uiCulture;
         }
     }
 }
