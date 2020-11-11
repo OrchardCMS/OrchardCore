da209cf56746418a13b90da2d3f5b137db8ede11
Jean-Thierry Kéchichian <jean-thierry.kechichian@wanadoo.fr>
2020-11-11 04:26:31 +0100

    Update AdminController.cs

--- src/OrchardCore.Modules/OrchardCore.Contents/Controllers/AdminController.cs
+++ src/OrchardCore.Modules/OrchardCore.Contents/Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
