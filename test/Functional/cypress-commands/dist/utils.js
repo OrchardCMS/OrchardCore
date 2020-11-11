5f01815f592f256c592da355d129ed0fefeaca04
Jean-Philippe Tissot <dev@tissotweb.ca>
2020-11-11 14:39:40 -0500

    static prefix for tenants

--- test/Functional/cypress-commands/dist/utils.js
+++ test/Functional/cypress-commands/dist/utils.js
@@ -3,11 +3,11 @@
 Object.defineProperty(exports, '__esModule', { value: true });
 
 // https://stackoverflow.com/questions/105034/how-to-create-guid-uuid
-const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
 function generateUniqueName() {
     var date = new Date();
     var today = new Date(date.getFullYear(), date.getMonth(), date.getDay());
-    return alphabet[Math.floor(Math.random() * alphabet.length)] + (date - today).toString(32);
+    return 't' + (date - today).toString(32);
 }
 
 function generateTenantInfo(setupRecipeName, description) {
