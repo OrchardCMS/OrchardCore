3fa511fcc8700c9849edc672d8e328b3e20cff0f
Antoine Griffard <agriffard@hotmail.com>
2020-11-13 13:04:50 +0100

    Only use email in ResetPasword
    Fixes #7540

--- src/OrchardCore/OrchardCore.Users.Core/Services/UserService.cs
+++ src/OrchardCore/OrchardCore.Users.Core/Services/UserService.cs
@@ -179,7 +179,7 @@ namespace OrchardCore.Users.Services
                 return result;
             }
 
-            var user = await FindByUsernameOrEmailAsync(userIdentifier) as User;
+            var user = await FindByEmailAsync(userIdentifier) as User;
 
             if (user == null)
             {
