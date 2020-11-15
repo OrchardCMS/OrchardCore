b039ce2f76162c99efaddd6fc47bc119e8a15af5
hs <h.schoemaker@ridder.nl>
2020-11-15 14:23:26 +0100

    Update of openID application Step
     - not using viewmode
     - recipe can be run multiple times
    Update of openID scope Step
     - not using viewmode
     - recipe can be run multiple times ScopeController
    
    Update of openID scope page
     - Give user feedback when spaces are added in the name

--- src/OrchardCore.Modules/OrchardCore.OpenId/Recipes/OpenIdClientSettingsStep.cs
+++ src/OrchardCore.Modules/OrchardCore.OpenId/Recipes/OpenIdClientSettingsStep.cs
@@ -43,21 +43,4 @@ namespace OrchardCore.OpenId.Recipes
             await _clientService.UpdateSettingsAsync(settings);
         }
     }
-
-    public class OpenIdClientSettingsStepModel
-    {
-        public string DisplayName { get; set; }
-
-        [Url]
-        public string Authority { get; set; }
-
-        public string ClientId { get; set; }
-        public string ClientSecret { get; set; }
-        public string CallbackPath { get; set; }
-        public string SignedOutRedirectUri { get; set; }
-        public string SignedOutCallbackPath { get; set; }
-        public string Scopes { get; set; }
-        public string ResponseType { get; set; }
-        public string ResponseMode { get; set; }
-    }
 }
