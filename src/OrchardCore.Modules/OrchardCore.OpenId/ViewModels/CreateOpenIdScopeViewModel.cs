05ed9243ae4f4d7f70e6d0321c26d644d5b817f6
hs <h.schoemaker@ridder.nl>
2020-11-15 20:19:15 +0100

    Moved name validation to IValidateObject of the ViewModels

--- src/OrchardCore.Modules/OrchardCore.OpenId/ViewModels/CreateOpenIdScopeViewModel.cs
+++ src/OrchardCore.Modules/OrchardCore.OpenId/ViewModels/CreateOpenIdScopeViewModel.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
 
 namespace OrchardCore.OpenId.ViewModels
 {
-    public class CreateOpenIdScopeViewModel
+    public class CreateOpenIdScopeViewModel : IValidatableObject
     {
         public string Description { get; set; }
 
@@ -16,6 +18,16 @@ namespace OrchardCore.OpenId.ViewModels
         public string Resources { get; set; }
 
         public List<TenantEntry> Tenants { get; } = new List<TenantEntry>();
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var S = validationContext.GetService<IStringLocalizer<EditOpenIdScopeViewModel>>();
+
+            if (Name.Contains(' '))
+            {
+                yield return new ValidationResult(S["The scope name cannot contain spaces."], new[] { nameof(Name) });
+            }
+        }
 
         public class TenantEntry
         {
