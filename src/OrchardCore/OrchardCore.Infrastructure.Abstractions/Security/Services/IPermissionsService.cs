fd71f5ee9d5ed4889abd12b33c4116b76d50b6f5
Jasmin Savard <jasmin.savard@gmail.com>
2020-11-19 13:59:08 -0500

    Move BasePermissionsService and rename it DefaultPermissionsService in OrchardCore.Infrastructure project

--- src/OrchardCore/OrchardCore.Infrastructure.Abstractions/Security/Services/IPermissionsService.cs
+++ src/OrchardCore/OrchardCore.Infrastructure.Abstractions/Security/Services/IPermissionsService.cs
@@ -11,32 +11,4 @@ namespace OrchardCore.Security.Services
     {
         Task<IEnumerable<Permission>> GetInstalledPermissionsAsync();
     }
-
-    public class BasePermissionsService : IPermissionsService
-    {
-
-        private readonly IEnumerable<IPermissionProvider> _permissionProviders;
-
-        public BasePermissionsService(IEnumerable<IPermissionProvider> permissionProviders)
-        {
-            _permissionProviders = permissionProviders;
-        }
-
-        public async Task<IEnumerable<Permission>> GetInstalledPermissionsAsync()
-        {
-            var installedPermissions = new List<Permission>();
-
-            foreach (var permissionProvider in _permissionProviders)
-            {
-                var permissions = await permissionProvider.GetPermissionsAsync();
-
-                foreach (var permission in permissions)
-                {
-                    installedPermissions.Add(permission);
-                }
-            }
-
-            return installedPermissions;
-        }
-    }
 }
