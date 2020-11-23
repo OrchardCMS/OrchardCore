8c89a49acc2144bcb205fdd9966b79ec97be5217
Eternity <14150259+EternityTeam@users.noreply.github.com>
2020-11-23 14:59:31 +0800

    Remove duplicate event handler (#7720)

--- src/OrchardCore.Modules/OrchardCore.Users/Startup.cs
+++ src/OrchardCore.Modules/OrchardCore.Users/Startup.cs
@@ -182,8 +182,6 @@ namespace OrchardCore.Users
 
             services.AddScoped<IDisplayDriver<ISite>, LoginSettingsDisplayDriver>();
 
-            services.AddScoped<ILiquidTemplateEventHandler, UserLiquidTemplateEventHandler>();
-
             services.AddScoped<IDisplayManager<User>, DisplayManager<User>>();
             services.AddScoped<IDisplayDriver<User>, UserDisplayDriver>();
             services.AddScoped<IDisplayDriver<User>, UserInformationDisplayDriver>();
