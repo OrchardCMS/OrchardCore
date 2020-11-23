7fd5bf89b5e6ce53acab4055798652083ca54402
Jasmin Savard <jasmin.savard@gmail.com>
2020-11-23 08:06:07 -0500

    wip

--- src/OrchardCore.Themes/TheAdmin/wwwroot/Scripts/TheAdmin.js
+++ src/OrchardCore.Themes/TheAdmin/wwwroot/Scripts/TheAdmin.js
@@ -7278,6 +7278,14 @@ $('#btn-darkmode').click(function () {
 
   persistAdminPreferences();
 });
+
+if ($('html').attr('data-theme') === 'darkmode') {
+  $('#btn-darkmode').children(':first').removeClass('fa-moon');
+  $('#btn-darkmode').children(':first').addClass('fa-sun');
+} else if ($('html').attr('data-theme') === 'default') {
+  $(this).children(':first').removeClass('fa-sun');
+  $(this).children(':first').addClass('fa-moon');
+}
 // When we load compact status from preferences we need to do some other tasks besides adding the class to the body.
 // UserPreferencesLoader has already added the needed class.
 $(function () {
