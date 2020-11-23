583451a6020f51ea83ec772db3ae71151782ff7b
Jasmin Savard <jasmin.savard@gmail.com>
2020-11-22 22:36:33 -0500

    wip

--- src/OrchardCore.Modules/OrchardCore.Themes/Services/DarkModeService.cs
+++ src/OrchardCore.Modules/OrchardCore.Themes/Services/DarkModeService.cs
@@ -1,25 +1,48 @@
 using Microsoft.AspNetCore.Http;
+using OrchardCore.Admin.Models;
+using OrchardCore.Entities;
+using OrchardCore.Settings;
 using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace OrchardCore.Themes.Services
 {
     public class DarkModeService
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ISiteService _siteService;
 
-        public DarkModeService(IHttpContextAccessor httpContextAccessor)
+        public DarkModeService(
+            IHttpContextAccessor httpContextAccessor,
+            ISiteService siteService)
         {
             _httpContextAccessor = httpContextAccessor;
+            _siteService = siteService;
         }
 
-        public bool IsDarkMode()
-        {
-            var adminPreferences = JsonDocument.Parse(_httpContextAccessor.HttpContext.Request.Cookies["adminPreferences"]);
-            var result = false;
+        public string MediaDefault { get; set; } = "all";
 
-            if(adminPreferences.RootElement.TryGetProperty("darkMode", out var darkMode))
+        public string MediaDark { get; set; } = "not all";
+
+        public async Task<bool> IsDarkModeAsync()
+        {
+            var result = false;
+            var adminSettings = (await _siteService.GetSiteSettingsAsync()).As<AdminSettings>();
+
+            if(adminSettings.DisplayDarkMode)
             {
-                result = darkMode.GetBoolean();
+                var adminPreferences = JsonDocument.Parse(_httpContextAccessor.HttpContext.Request.Cookies["adminPreferences"]);
+
+                if (adminPreferences.RootElement.TryGetProperty("darkMode", out var darkMode))
+                {
+                    result = darkMode.GetBoolean();
+                }
+            }
+
+            if(result)
+            {
+                MediaDefault = "not all";
+                MediaDark = "all";
             }
 
             return result;
