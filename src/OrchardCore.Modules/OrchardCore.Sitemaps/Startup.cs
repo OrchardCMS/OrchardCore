d7e092809e3823f65a6a36f652aefc29fe0acd41
Luca Temponi <piemp@hotmail.it>
2020-11-24 18:08:23 +0100

    Revert "added interfaces and classes to allow custom url as a sitemap entry"
    This reverts commit c41ee0b761f20ae150fd991228c101a5719cc4f9.

--- src/OrchardCore.Modules/OrchardCore.Sitemaps/Startup.cs
+++ src/OrchardCore.Modules/OrchardCore.Sitemaps/Startup.cs
@@ -8,7 +8,6 @@ using OrchardCore.ContentManagement;
 using OrchardCore.ContentManagement.Display.ContentDisplay;
 using OrchardCore.Data.Migration;
 using OrchardCore.DisplayManagement;
-using OrchardCore.DisplayManagement.Handlers;
 using OrchardCore.Entities;
 using OrchardCore.Modules;
 using OrchardCore.Mvc.Core.Utilities;
@@ -80,13 +79,6 @@ namespace OrchardCore.Sitemaps
             services.AddContentPart<SitemapPart>()
                 .UseDisplayDriver<SitemapPartDisplay>()
                 .AddHandler<SitemapPartHandler>();
-
-            //custom url in sitemap
-            services.AddScoped<ISitemapSourceBuilder, CustomUrlsSitemapSourceBuilder>();
-            services.AddScoped<ISitemapSourceUpdateHandler, CustomUrlsSitemapSourceUpdateHandler>();
-            services.AddScoped<ISitemapSourceModifiedDateProvider, CustomUrlsSitemapSourceModifiedDateProvider>();
-            services.AddScoped<IDisplayDriver<SitemapSource>, CustomUrlsSitemapSourceDriver>();
-            services.AddScoped<ISitemapSourceFactory, SitemapSourceFactory<CustomUrlSitemapSource>>();
         }
 
         public override void Configure(IApplicationBuilder app, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
