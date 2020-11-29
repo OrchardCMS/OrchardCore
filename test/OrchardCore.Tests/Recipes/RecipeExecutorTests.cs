74835081bee1d45a03853baca6dd1df780549f74
jtkech <jean-thierry.kechichian@wanadoo.fr>
2020-11-29 15:18:01 +0100

    Fixes Unit tests

--- test/OrchardCore.Tests/Recipes/RecipeExecutorTests.cs
+++ test/OrchardCore.Tests/Recipes/RecipeExecutorTests.cs
@@ -1,20 +1,22 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Moq;
+using OrchardCore.Environment.Extensions;
 using OrchardCore.Environment.Shell;
 using OrchardCore.Environment.Shell.Builders;
+using OrchardCore.Environment.Shell.Models;
 using OrchardCore.Environment.Shell.Scope;
 using OrchardCore.Recipes.Events;
 using OrchardCore.Recipes.Models;
 using OrchardCore.Recipes.Services;
 using OrchardCore.Scripting;
+using OrchardCore.Tests.Stubs;
 using Xunit;
 
 namespace OrchardCore.Recipes
@@ -27,31 +29,69 @@ namespace OrchardCore.Recipes
         [InlineData("recipe3", "js: valiables('now')")]
         [InlineData("recipe4", "[locale en]This text contain a colon ':' symbol[/locale][locale fr]Ce texte contient un symbole du côlon ':'[/locale]")]
         [InlineData("recipe5", "[sc text='some : text'/]")]
-        public async Task ShouldTrimValidScriptExpression(string recipeName, string expected)
+        public Task ShouldTrimValidScriptExpression(string recipeName, string expected)
         {
-            // Arrange
-            var shellHostMock = new Mock<IShellHost>();
-            var shellSettings = new ShellSettings { Name = "Test" };
-            var services = new ServiceCollection()
+            return CreateShellContext().CreateScope().UsingAsync(async scope =>
+            {
+                var recipeExecutor = scope.ServiceProvider.GetRequiredService<IRecipeExecutor>();
+
+                // Act
+                var executionId = Guid.NewGuid().ToString("n");
+                var recipeDescriptor = new RecipeDescriptor { RecipeFileInfo = GetRecipeFileInfo(recipeName) };
+                await recipeExecutor.ExecuteAsync(executionId, recipeDescriptor, new object(), CancellationToken.None);
+
+                // Assert
+                var recipeStep = scope.ServiceProvider
+                    .GetServices<IRecipeEventHandler>()
+                    .OfType<RecipeEventHandler>()
+                    .FirstOrDefault()
+                    .Context
+                    .Step;
+
+                Assert.Equal(expected, recipeStep.SelectToken("data.[0].TitlePart.Title").ToString());
+            });
+        }
+
+        private static ShellContext CreateShellContext()
+        {
+            var settings = new ShellSettings() { Name = ShellHelper.DefaultShellName, State = TenantState.Running };
+
+            return new ShellContext()
+            {
+                Settings = new ShellSettings() { Name = ShellHelper.DefaultShellName, State = TenantState.Running },
+                ServiceProvider = CreateServiceProvider(settings),
+            };
+        }
+
+        private static IServiceProvider CreateServiceProvider(ShellSettings settings)
+        {
+            var services = new ServiceCollection();
+
+            services.AddOrchardCore();
+
+            services.AddSingleton(settings)
+                .AddSingleton<IShellHost, StubShellHost>()
+                .AddSingleton<IHostEnvironment, StubHostingEnvironment>()
+                .AddScoped<IRecipeEventHandler, RecipeEventHandler>()
+                .AddTransient<IRecipeExecutor, RecipeExecutor>()
                 .AddScripting();
-            shellHostMock.Setup(h => h.GetScopeAsync(It.IsAny<ShellSettings>()))
-                .Returns(Task.FromResult(new ShellScope(new ShellContext {
-                    ServiceProvider = services.BuildServiceProvider(),
-                    Settings = shellSettings
-                })));
-            var recipeEventHandlers = new List<IRecipeEventHandler> { new RecipeEventHandler() };
-            var loggerMock = new Mock<ILogger<RecipeExecutor>>();
-            var recipeExecutor = new RecipeExecutor(shellHostMock.Object, shellSettings, recipeEventHandlers, loggerMock.Object);
-            var executionId = Guid.NewGuid().ToString("n");
-            var recipeDescriptor = new RecipeDescriptor { RecipeFileInfo = GetRecipeFileInfo(recipeName) };
 
-            // Act
-            await recipeExecutor.ExecuteAsync(executionId, recipeDescriptor, new object(), CancellationToken.None);
+            return services.BuildServiceProvider();
+        }
 
-            // Assert
-            var recipeContext = (recipeEventHandlers.Single() as RecipeEventHandler).Context;
-            var recipeStep = recipeContext.Step;
-            Assert.Equal(expected, recipeStep.SelectToken("data.[0].TitlePart.Title").ToString());
+        private class StubShellHost : ShellHost
+        {
+            public StubShellHost(
+                IShellSettingsManager shellSettingsManager,
+                IShellContextFactory shellContextFactory,
+                IRunningShellTable runningShellTable,
+                IExtensionManager extensionManager,
+                ILogger<ShellHost> logger)
+                : base (shellSettingsManager, shellContextFactory, runningShellTable, extensionManager, logger)
+            {
+            }
+
+            public new Task<ShellScope> GetScopeAsync(ShellSettings _) => Task.FromResult(ShellScope.Context.CreateScope());
         }
 
         private IFileInfo GetRecipeFileInfo(string recipeName)
@@ -67,21 +107,16 @@ namespace OrchardCore.Recipes
             public RecipeExecutionContext Context { get; private set; }
 
             Task IRecipeEventHandler.ExecutionFailedAsync(string executionId, RecipeDescriptor descriptor) => Task.CompletedTask;
-
             Task IRecipeEventHandler.RecipeExecutedAsync(string executionId, RecipeDescriptor descriptor) => Task.CompletedTask;
-
             Task IRecipeEventHandler.RecipeExecutingAsync(string executionId, RecipeDescriptor descriptor) => Task.CompletedTask;
+            Task IRecipeEventHandler.RecipeStepExecutingAsync(RecipeExecutionContext context) => Task.CompletedTask;
 
             Task IRecipeEventHandler.RecipeStepExecutedAsync(RecipeExecutionContext context)
             {
-                Context = context;
-
-                return Task.CompletedTask;
-            }
-
-            Task IRecipeEventHandler.RecipeStepExecutingAsync(RecipeExecutionContext context)
-            {
-                Context = context;
+                if (String.Equals(context.Name, "Content", StringComparison.OrdinalIgnoreCase))
+                {
+                    Context = context;
+                }
 
                 return Task.CompletedTask;
             }
