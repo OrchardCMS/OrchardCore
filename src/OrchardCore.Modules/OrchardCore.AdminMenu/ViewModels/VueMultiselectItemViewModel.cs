5e3400ec87cea2d7cd8faf552b537573fc396377
tempcode <piemp@hotmail.it>
2020-12-01 18:31:13 +0100

    Refactor admin menu permissions (#7777)

--- src/OrchardCore.Modules/OrchardCore.AdminMenu/ViewModels/VueMultiselectItemViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace OrchardCore.AdminMenu.ViewModels
-{
-    public class VueMultiselectItemViewModel
-    {
-        public string Id { get; set; }
-        public string DisplayText { get; set; }
-    }
-}
