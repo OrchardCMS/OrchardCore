a2f0b749b68c98ec2fe42fa2544d5e3d81a4ab8f
hs <h.schoemaker@ridder.nl>
2020-12-10 22:11:10 +0100

    Update the applications management UI to support setting scope permissions

--- src/OrchardCore.Modules/OrchardCore.OpenId/Configuration/OpenIdServerConfiguration.cs
+++ src/OrchardCore.Modules/OrchardCore.OpenId/Configuration/OpenIdServerConfiguration.cs
@@ -54,7 +54,7 @@ namespace OrchardCore.OpenId.Configuration
                 return;
             }
 
-            options.IgnoreScopePermissions = true;
+            options.IgnoreScopePermissions = false;
             options.Issuer = settings.Authority;
             options.DisableAccessTokenEncryption = settings.DisableAccessTokenEncryption;
             options.DisableRollingRefreshTokens = settings.DisableRollingRefreshTokens;
