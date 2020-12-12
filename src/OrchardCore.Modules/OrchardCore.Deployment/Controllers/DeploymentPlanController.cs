a7773d038efcc475fd6775902a17769f3560be8d
Antoine Griffard <agriffard@hotmail.com>
2020-12-12 23:27:36 +0100

    Revert the YesSqlQuery ones

--- src/OrchardCore.Modules/OrchardCore.Deployment/Controllers/DeploymentPlanController.cs
+++ src/OrchardCore.Modules/OrchardCore.Deployment/Controllers/DeploymentPlanController.cs
@@ -79,7 +79,7 @@ namespace OrchardCore.Deployment.Controllers
 
             if (!string.IsNullOrWhiteSpace(options.Search))
             {
-                deploymentPlans = deploymentPlans.Where(x => x.Name.Contains(options.Search, StringComparison.OrdinalIgnoreCase));
+                deploymentPlans = deploymentPlans.Where(x => x.Name.Contains(options.Search));
             }
 
             var count = await deploymentPlans.CountAsync();
--- src/OrchardCore.Modules/OrchardCore.Workflows/Controllers/WorkflowTypeController.cs
+++ src/OrchardCore.Modules/OrchardCore.Workflows/Controllers/WorkflowTypeController.cs
@@ -108,7 +108,7 @@ namespace OrchardCore.Workflows.Controllers
 
             if (!string.IsNullOrWhiteSpace(options.Search))
             {
-                query = query.Where(x => x.Name.Contains(options.Search, StringComparison.OrdinalIgnoreCase));
+                query = query.Where(x => x.Name.Contains(options.Search));
             }
 
             switch (options.Order)
