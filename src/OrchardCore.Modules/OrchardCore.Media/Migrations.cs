52044a2fe71c0cf23f35a102d4c8019ed58ce6ee
Dean Marcussen <dean.marcussen@gmail.com>
2020-12-12 11:53:00 +0000

    Refactor media token service to use HMAC signature instead of DP

--- src/OrchardCore.Modules/OrchardCore.Media/Migrations.cs
+++ src/OrchardCore.Modules/OrchardCore.Media/Migrations.cs
@@ -1,17 +1,24 @@
+using System.Security.Cryptography;
+using System.Threading.Tasks;
 using OrchardCore.ContentManagement.Metadata;
 using OrchardCore.Data.Migration;
+using OrchardCore.Entities;
 using OrchardCore.Media.Fields;
+using OrchardCore.Media.Processing;
 using OrchardCore.Media.Settings;
+using OrchardCore.Settings;
 
 namespace OrchardCore.Media
 {
     public class Migrations : DataMigration
     {
         private readonly IContentDefinitionManager _contentDefinitionManager;
+        private readonly ISiteService _siteService;
 
-        public Migrations(IContentDefinitionManager contentDefinitionManager)
+        public Migrations(IContentDefinitionManager contentDefinitionManager, ISiteService siteService)
         {
             _contentDefinitionManager = contentDefinitionManager;
+            _siteService = siteService;
         }
 
         // This migration does not need to run on new installations, but because there is no
@@ -21,6 +28,22 @@ namespace OrchardCore.Media
             _contentDefinitionManager.MigrateFieldSettings<MediaField, MediaFieldSettings>();
 
             return 1;
+        }
+
+        public async Task<int> UpdateFrom1Async()
+        {
+            var siteSettings = await _siteService.LoadSiteSettingsAsync();
+            var mediaTokenSettings = siteSettings.As<MediaTokenSettings>();
+
+            var rng = RandomNumberGenerator.Create();
+
+            mediaTokenSettings.HashKey = new byte[64];
+            rng.GetBytes(mediaTokenSettings.HashKey);
+            siteSettings.Put(mediaTokenSettings);
+
+            await _siteService.UpdateSiteSettingsAsync(siteSettings);
+
+            return 2;
         }
     }
 }
