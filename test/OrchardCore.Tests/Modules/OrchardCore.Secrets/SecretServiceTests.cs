ae0a9a6cca1435cda2a6bca0c6b75ce0765d5c66
jtkech <jean-thierry.kechichian@wanadoo.fr>
2020-12-14 14:24:35 +0100

    Fixes unit tests

--- test/OrchardCore.Tests/Modules/OrchardCore.Secrets/SecretServiceTests.cs
+++ test/OrchardCore.Tests/Modules/OrchardCore.Secrets/SecretServiceTests.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Threading.Tasks;
 using Moq;
+using OrchardCore.Documents;
 using OrchardCore.Secrets;
+using OrchardCore.Secrets.Models;
 using OrchardCore.Secrets.Services;
 using Xunit;
 
@@ -12,14 +15,25 @@ namespace OrchardCore.Tests.Modules.OrchardCore.Secrets
         public async Task ShouldGetTextSecret()
         {
             var store = Mock.Of<ISecretStore>();
+
             var textSecret = new TextSecret()
             {
                 Text = "myemailpassword"
-            };            
+            };
 
-            Mock.Get(store).Setup(s => s.GetSecretAsync("email", typeof(TextSecret))).ReturnsAsync(textSecret);            
+            var documentManager = Mock.Of<IDocumentManager<SecretBindingsDocument>>();
 
-            var coordinator = new DefaultSecretCoordinator(null, new [] { store });
+            Mock.Get(store).Setup(s => s.GetSecretAsync("email", typeof(TextSecret))).ReturnsAsync(textSecret);
+
+            Mock.Get(documentManager).Setup(m => m.GetOrCreateImmutableAsync(It.IsAny<Func<Task<SecretBindingsDocument>>>()))
+                .ReturnsAsync(() =>
+                {
+                    var document = new SecretBindingsDocument();
+                    document.SecretBindings["email"] = new SecretBinding();
+                    return document;
+                });
+
+            var coordinator = new DefaultSecretCoordinator(new SecretBindingsManager(documentManager), new[] { store });
 
             var service = new SecretService<TextSecret>(coordinator);
             var secret = await service.GetSecretAsync("email");
