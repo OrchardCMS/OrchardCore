75170e9aae51b4c1662c6d3d652663b565cf9362
jtkech <jean-thierry.kechichian@wanadoo.fr>
2020-12-26 20:01:15 +0100

    Minor change

--- src/OrchardCore/OrchardCore.Abstractions/Shell/Builders/ShellContextExtensions.cs
+++ src/OrchardCore/OrchardCore.Abstractions/Shell/Builders/ShellContextExtensions.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
@@ -12,19 +11,22 @@ namespace OrchardCore.Environment.Shell.Builders
     public static class ShellContextExtensions
     {
         /// <summary>
-        /// Tries to acquire a lock on this shell, a distributed lock if it is not initializing, otherwise a local lock.
+        /// Tries to acquire a lock for shell activation, a local lock if it is initializing, otherwise a distributed lock.
         /// </summary>
         public static Task<(ILocker locker, bool locked)> TryAcquireShellActivateLockAsync(this ShellContext shellContext)
         {
+            // If the shell is initializing, force the usage of a local lock.
             var lockService = shellContext.Settings.State == TenantState.Initializing
                 ? (ILock)shellContext.ServiceProvider.GetRequiredService<ILocalLock>()
                 : shellContext.ServiceProvider.GetRequiredService<IDistributedLock>();
 
+            // If it is a local lock, use a maximum timeout and no expiration.
             if (lockService is ILocalLock localLock)
             {
-                return localLock.TryAcquireLockAsync("SHELL_ACTIVATE_LOCK", timeout: TimeSpan.MaxValue);
+                return localLock.TryAcquireLockAsync("SHELL_ACTIVATE_LOCK", TimeSpan.MaxValue);
             }
 
+            // If it is a distributed lock, we use the configured locking times.
             var options = shellContext.ServiceProvider.GetRequiredService<IOptions<ShellContextOptions>>().Value;
 
             return lockService.TryAcquireLockAsync(
