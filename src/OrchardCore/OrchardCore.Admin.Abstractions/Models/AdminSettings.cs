14bf8749505e61b5a12225aa07eaba6698a70fcc
Antoine Griffard <agriffard@hotmail.com>
2020-12-31 11:24:03 +0100

    Display New menu setting

--- src/OrchardCore/OrchardCore.Admin.Abstractions/Models/AdminSettings.cs
+++ src/OrchardCore/OrchardCore.Admin.Abstractions/Models/AdminSettings.cs
@@ -2,9 +2,11 @@ namespace OrchardCore.Admin.Models
 {
     public class AdminSettings
     {
+        public bool DisplayDarkMode { get; set; } = true;
+
         public bool DisplayMenuFilter { get; set; } = true;
 
-        public bool DisplayDarkMode { get; set; } = true;
+        public bool DisplayNewMenu { get; set; } = true;
 
         public bool DisplayTitlesInTopbar { get; set; }
     }
