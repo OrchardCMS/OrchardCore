71601cbcf60b8edafcc7e6f41c3375dc4758df66
jtkech <jean-thierry.kechichian@wanadoo.fr>
2021-01-02 01:51:10 +0100

    Missing reverting

--- src/OrchardCore.Modules/OrchardCore.Workflows/ViewModels/WorkflowTypePropertiesViewModel.cs
+++ src/OrchardCore.Modules/OrchardCore.Workflows/ViewModels/WorkflowTypePropertiesViewModel.cs
@@ -11,8 +11,6 @@ namespace OrchardCore.Workflows.ViewModels
 
         public bool IsEnabled { get; set; }
         public bool IsSingleton { get; set; }
-        public int LockTimeout { get; set; } = 30_000;
-        public int LockExpiration { get; set; } = 30_000;
         public bool DeleteFinishedWorkflows { get; set; }
         public string ReturnUrl { get; set; }
     }
