fb04d3d777334db556ea0b6fa7d49ddbf86877ae
Dean Marcussen <dean.marcussen@gmail.com>
2021-01-23 16:36:49 +0000

    Add missing using

--- src/OrchardCore.Modules/OrchardCore.Deployment/Deployment/DeploymentPlanDeploymentSource.cs
+++ src/OrchardCore.Modules/OrchardCore.Deployment/Deployment/DeploymentPlanDeploymentSource.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Linq;
 
 namespace OrchardCore.Deployment.Deployment
