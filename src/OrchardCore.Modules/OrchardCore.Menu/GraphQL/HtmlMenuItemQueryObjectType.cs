7c926541b6b11f128c503b6c2419bf2b40d9b7f1
Titus Anderson <titus.anderson@jefferson.kyschools.us>
2021-01-26 14:53:24 -0500

    added graphql object type for html menu item

---
+++ src/OrchardCore.Modules/OrchardCore.Menu/GraphQL/HtmlMenuItemQueryObjectType.cs
@@ -0,0 +1,17 @@
+using GraphQL.Types;
+using OrchardCore.Menu.Models;
+
+namespace OrchardCore.Menu.GraphQL
+{
+    public class HtmlMenuItemQueryObjectType : ObjectGraphType<HtmlMenuItemPart>
+    {
+        public HtmlMenuItemQueryObjectType()
+        {
+            Name = "HtmlMenuItemPart";
+
+            Field(x => x.Name, nullable: true);
+            Field(x => x.Url, nullable: true);
+            Field(x => x.Html, nullable: true);
+        }
+    }
+}
