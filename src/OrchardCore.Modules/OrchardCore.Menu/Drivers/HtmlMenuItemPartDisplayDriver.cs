913329d088c329f27ec47511545302c080fc21c8
Titus Anderson <titus.anderson@jefferson.kyschools.us>
2021-01-27 09:14:32 -0500

    Add HTML sanitization and part settings for htmlMenuItemPart.

--- src/OrchardCore.Modules/OrchardCore.Menu/Drivers/HtmlMenuItemPartDisplayDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.Menu/Drivers/HtmlMenuItemPartDisplayDriver.cs
@@ -3,16 +3,31 @@ using OrchardCore.ContentManagement.Display.ContentDisplay;
 using OrchardCore.ContentManagement.Display.Models;
 using OrchardCore.DisplayManagement.ModelBinding;
 using OrchardCore.DisplayManagement.Views;
+using OrchardCore.Infrastructure.Html;
 using OrchardCore.Menu.Models;
+using OrchardCore.Menu.Settings;
 using OrchardCore.Menu.ViewModels;
 
 namespace OrchardCore.Menu.Drivers
 {
     public class HtmlMenuItemPartDisplayDriver : ContentPartDisplayDriver<HtmlMenuItemPart>
     {
+        private readonly IHtmlSanitizerService _htmlSanitizerService;
+
+        public HtmlMenuItemPartDisplayDriver(IHtmlSanitizerService htmlSanitizerService)
+        {
+            _htmlSanitizerService = htmlSanitizerService;
+        }
 
         public override IDisplayResult Display(HtmlMenuItemPart part, BuildPartDisplayContext context)
         {
+            var settings = context.TypePartDefinition.GetSettings<HtmlMenuItemPartSettings>();
+
+            if (settings.SanitizeHtml)
+            {
+                part.Html = _htmlSanitizerService.Sanitize(part.Html);
+            }
+
             return Combine(
                 Dynamic("HtmlMenuItemPart_Admin", shape =>
                 {
@@ -38,11 +53,16 @@ namespace OrchardCore.Menu.Drivers
             });
         }
 
-        public override async Task<IDisplayResult> UpdateAsync(HtmlMenuItemPart part, IUpdateModel updater)
+        public override async Task<IDisplayResult> UpdateAsync(HtmlMenuItemPart part, IUpdateModel updater, UpdatePartEditorContext context)
         {
-            await updater.TryUpdateModelAsync(part, Prefix, x => x.Name, x => x.Url, x => x.Html);
+            var settings = context.TypePartDefinition.GetSettings<HtmlMenuItemPartSettings>();
 
-            return Edit(part);
+            if (await updater.TryUpdateModelAsync(part, Prefix, x => x.Name, x => x.Url, x => x.Html))
+            {
+                part.Html = settings.SanitizeHtml ? _htmlSanitizerService.Sanitize(part.Html) : part.Html;
+            }
+
+            return Edit(part, context);
         }
     }
 }
--- src/OrchardCore.Modules/OrchardCore.Menu/Startup.cs
+++ src/OrchardCore.Modules/OrchardCore.Menu/Startup.cs
@@ -7,12 +7,14 @@ using OrchardCore.Admin;
 using OrchardCore.ContentManagement;
 using OrchardCore.ContentManagement.Display.ContentDisplay;
 using OrchardCore.ContentManagement.Handlers;
+using OrchardCore.ContentTypes.Editors;
 using OrchardCore.Data.Migration;
 using OrchardCore.DisplayManagement.Descriptors;
 using OrchardCore.Menu.Controllers;
 using OrchardCore.Menu.Drivers;
 using OrchardCore.Menu.Handlers;
 using OrchardCore.Menu.Models;
+using OrchardCore.Menu.Settings;
 using OrchardCore.Menu.TagHelpers;
 using OrchardCore.Modules;
 using OrchardCore.Mvc.Core.Utilities;
@@ -55,6 +57,8 @@ namespace OrchardCore.Menu
             // HtmlMenuItemPart
             services.AddContentPart<HtmlMenuItemPart>()
                 .UseDisplayDriver<HtmlMenuItemPartDisplayDriver>();
+            services.AddScoped<IContentTypePartDefinitionDisplayDriver, HtmlMenuItemPartSettingsDisplayDriver>();
+
 
             services.AddTagHelpers<MenuTagHelper>();
         }
