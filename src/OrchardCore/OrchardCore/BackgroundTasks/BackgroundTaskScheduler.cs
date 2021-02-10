6ed9601062441f5c0d50226429156c4216dca7f8
Jean-Thierry Kéchichian <jean-thierry.kechichian@wanadoo.fr>
2021-02-10 04:20:50 +0100

    Revert "Using ILocalClock instead of IClock (BackgroundTaskScheduler) (#8552)"
    This reverts commit 1a442207654f242ac276155f9e291d76c2c60fcd.

--- src/OrchardCore/OrchardCore/BackgroundTasks/BackgroundTaskScheduler.cs
+++ src/OrchardCore/OrchardCore/BackgroundTasks/BackgroundTaskScheduler.cs
@@ -6,14 +6,15 @@ namespace OrchardCore.BackgroundTasks
 {
     public class BackgroundTaskScheduler
     {
-        public BackgroundTaskScheduler(string tenant, string name, DateTime referenceTime, ILocalClock localClock)
+        public BackgroundTaskScheduler(string tenant, string name, DateTime referenceTime, IClock clock, string timeZoneId)
         {
             Name = name;
             Tenant = tenant;
             ReferenceTime = referenceTime;
             Settings = new BackgroundTaskSettings() { Name = name };
             State = new BackgroundTaskState() { Name = name };
-            LocalClock = localClock;
+            Clock = clock;
+            TimeZone = Clock.GetTimeZone(timeZoneId);
         }
 
         public string Name { get; }
@@ -23,13 +24,14 @@ namespace OrchardCore.BackgroundTasks
         public BackgroundTaskState State { get; set; }
         public bool Released { get; set; }
         public bool Updated { get; set; }
-        private ILocalClock LocalClock { get; set; }
+        public ITimeZone TimeZone { get; set; }
+        private IClock Clock { get; set; }
 
         public bool CanRun()
         {
-            var referenceTimeLocal = LocalClock.ConvertToLocalAsync(ReferenceTime).Result.DateTime;
+            var referenceTimeLocal = Clock.ConvertToTimeZone(ReferenceTime, TimeZone).DateTime;
             var nextStartTime = CrontabSchedule.Parse(Settings.Schedule).GetNextOccurrence(referenceTimeLocal);
-            var nowLocal = LocalClock.LocalNowAsync.Result.DateTime;
+            var nowLocal = Clock.ConvertToTimeZone(DateTime.UtcNow, TimeZone).DateTime;
 
             if (nowLocal >= nextStartTime)
             {
