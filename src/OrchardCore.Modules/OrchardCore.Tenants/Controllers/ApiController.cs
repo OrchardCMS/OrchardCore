ed4bad93b6696668338e874213397607d943f844
Jean-Thierry Kéchichian <jean-thierry.kechichian@wanadoo.fr>
2021-02-20 17:00:28 +0100

    Update ApiController.cs

--- src/OrchardCore.Modules/OrchardCore.Tenants/Controllers/ApiController.cs
+++ src/OrchardCore.Modules/OrchardCore.Tenants/Controllers/ApiController.cs
@@ -319,10 +319,10 @@ namespace OrchardCore.Tenants.Controllers
                 pathString = pathString.Add('/' + shellSettings.RequestUrlPrefix);
             }
 
-            var queryString = String.Empty;
+            QueryString queryString;
             if (!String.IsNullOrEmpty(token))
             {
-                queryString = QueryString.Create("token", token).ToString();
+                queryString = QueryString.Create("token", token);
             }
 
             return $"{Request.Scheme}://{hostString + pathString + queryString}";
