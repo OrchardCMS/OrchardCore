91bfcea3253521d6629297868f88183ac3a41da3
spike <906990711@qq.com>
2021-02-22 23:56:59 +0800

    Update ValidateRuleFormTask.cs

--- src/OrchardCore.Modules/OrchardCore.Forms/Workflows/Activities/ValidateRuleFormTask.cs
+++ src/OrchardCore.Modules/OrchardCore.Forms/Workflows/Activities/ValidateRuleFormTask.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Localization;
 using Newtonsoft.Json;
 using OrchardCore.DisplayManagement.ModelBinding;
+using OrchardCore.Environment.Shell.Scope;
+using OrchardCore.Scripting;
 using OrchardCore.Workflows.Abstractions.Models;
 using OrchardCore.Workflows.Activities;
 using OrchardCore.Workflows.Models;
@@ -17,13 +19,16 @@ namespace OrchardCore.Forms.Workflows.Activities
         private readonly IUpdateModelAccessor _updateModelAccessor;
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly IStringLocalizer S;
+        private readonly IScriptingManager _scriptingManager;
 
         public ValidateRuleFormTask(
+            IScriptingManager scriptingManager,
             IHttpContextAccessor httpContextAccessor,
             IUpdateModelAccessor updateModelAccessor,
             IStringLocalizer<ValidateRuleFormTask> localizer
         )
         {
+            _scriptingManager = scriptingManager;
             _updateModelAccessor = updateModelAccessor;
             _httpContextAccessor = httpContextAccessor;
             S = localizer;
@@ -52,12 +57,13 @@ namespace OrchardCore.Forms.Workflows.Activities
             }
 
             var httpContext = _httpContextAccessor.HttpContext;
+            bool validateRule = true;
             var rules = new List<dynamic>();
             foreach (var item in httpContext.Request.Form)
             {
                 if (item.Key.Equals("validateRules", StringComparison.InvariantCultureIgnoreCase))
                 {
-                    if (string.IsNullOrEmpty(item.Value[0]))
+                    if (!string.IsNullOrEmpty(item.Value[0]))
                     {
                         rules = JsonConvert.DeserializeObject<List<dynamic>>(item.Value[0]);
                     }
@@ -67,21 +73,27 @@ namespace OrchardCore.Forms.Workflows.Activities
                     updater.ModelState.SetModelValue(item.Key, item.Value, item.Value);
                 }
             }
+         
             if (rules.Count > 0)
             {
                 foreach (var item in rules)
                 {
-                    var type = item.type;
-                    var option = item.option;
-                    var formItem = httpContext.Request.Form.FirstOrDefault(a => a.Key == item.elementId);
-                    var formItemValue = formItem.Value;
-                    // validate formItem.Value
-
+                    var type = item.type.ToString();
+                    var option = item.option.ToString();
+                    var formItemValue = httpContext.Request.Form[item.elementId.ToString()];
+                    var engine = _scriptingManager.GetScriptingEngine("js");
+                    var scope = engine.CreateScope(_scriptingManager.GlobalMethodProviders.SelectMany(x => x.GetMethods()), ShellScope.Services, null, null);
+                    var jsScriptStr = $"js: {type}('{formItemValue}','{option}')";
+                    if (!Convert.ToBoolean(engine.Evaluate(scope, jsScriptStr)))
+                    {
+                        validateRule = false;
+                        break;
+                    }
                 }
             }
 
             var isValid = updater.ModelState.ErrorCount == 0;
-            var outcome = isValid ? "Valid" : "Invalid";
+            var outcome = isValid && validateRule ? "Valid" : "Invalid";
             return Outcomes(outcome);
         }
     }
