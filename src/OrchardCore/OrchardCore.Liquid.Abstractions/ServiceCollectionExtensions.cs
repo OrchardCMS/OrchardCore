e7c72c07886d7f39743231cd7c6be9d16c1d2be3
Dean Marcussen <dean.marcussen@gmail.com>
2021-02-22 08:15:12 +0000

    Update ServiceCollectionExtensions.cs

--- src/OrchardCore/OrchardCore.Liquid.Abstractions/ServiceCollectionExtensions.cs
+++ src/OrchardCore/OrchardCore.Liquid.Abstractions/ServiceCollectionExtensions.cs
@@ -7,7 +7,6 @@ namespace OrchardCore.Liquid
     {
         public static IServiceCollection AddLiquidFilter<TLiquidFilter>(this IServiceCollection services, string name) where TLiquidFilter : class, ILiquidFilter
         {
-            // services.Configure<TemplateOptions>(o => o.Filters.AddFilter(name, new LiquidFilterResolver(typeof(T)).ResolveThenProcessAsync));
             services.Configure<TemplateOptions>(o => o.Filters.AddFilter(name, new LiquidFilterResolver<TLiquidFilter>().ResolveThenProcessAsync));
             services.AddScoped<TLiquidFilter>();
 
