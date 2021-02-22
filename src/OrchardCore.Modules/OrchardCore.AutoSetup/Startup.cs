51b51cac4c12fe84ef1e5647434cc6d105d98bb5
rserajedinov <rserajedinov@accesssoftek.com>
2021-02-22 11:06:18 -0800

    Move AutoSetupFilter logic to Startup

--- src/OrchardCore.Modules/OrchardCore.AutoSetup/Startup.cs
+++ src/OrchardCore.Modules/OrchardCore.AutoSetup/Startup.cs
@@ -1,5 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using OrchardCore.AutoSetup.Options;
+using OrchardCore.Environment.Shell;
 using OrchardCore.Environment.Shell.Configuration;
 using OrchardCore.Modules;
 
@@ -14,6 +23,11 @@ namespace OrchardCore.AutoSetup
         /// The Shell/Tenant configuration.
         /// </summary>
         private readonly IShellConfiguration _shellConfiguration;
+
+        /// <summary>
+        /// AutoSetup Configuration Section Name 
+        /// </summary>
+        private const string ConfigSectionName = "OrchardCore_AutoSetup";
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Startup"/> class.
@@ -34,9 +48,63 @@ namespace OrchardCore.AutoSetup
         /// </param>
         public override void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<Microsoft.AspNetCore.Hosting.IStartupFilter, AutoSetupStartupFilter>();
-            var configuration = _shellConfiguration.GetSection("OrchardCore_AutoSetup");
+            var configuration = _shellConfiguration.GetSection(ConfigSectionName);
             services.Configure<AutoSetupOptions>(configuration);
+        }
+
+        /// <summary>
+        /// Configure application pipeline.
+        /// </summary>
+        /// <param name="app">
+        /// The app.
+        /// </param>
+        /// <param name="routes">
+        /// The routes.
+        /// </param>
+        /// <param name="serviceProvider">
+        /// The "Shell Scope" service provider.
+        /// </param>
+        public override void Configure(IApplicationBuilder app, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+            var currentShellSettings = serviceProvider.GetRequiredService<ShellSettings>();
+            var logger = serviceProvider.GetRequiredService<ILogger<Startup>>();
+
+
+            if (currentShellSettings.State == Environment.Shell.Models.TenantState.Uninitialized)
+            {
+                var optionsAccessor = serviceProvider.GetRequiredService<IOptions<AutoSetupOptions>>();
+                var options = optionsAccessor.Value;
+
+                if (options != null)
+                {
+                    var validationContext = new ValidationContext(options, serviceProvider, null);
+                    var validationErrors = options.Validate(validationContext);
+
+                    if (validationErrors.Any())
+                    {
+                        var stringBuilder = new StringBuilder();
+                        foreach (var error in validationErrors)
+                        {
+                            stringBuilder.Append(error.ErrorMessage);
+                        }
+
+                        logger.LogError("AutoSetup did not start, configuration has following errors: {errors}", stringBuilder.ToString());
+                    }
+                    else
+                    {
+                        if (string.IsNullOrEmpty(options.AutoSetupPath))
+                        {
+                            app.UseMiddleware<AutoSetupMiddleware>();
+                        }
+                        else
+                        {
+                            app.Map(options.AutoSetupPath, appBuilder => appBuilder.UseMiddleware<AutoSetupMiddleware>());
+                        }
+                    }
+                }
+            }
+
+            base.Configure(app, routes, serviceProvider);
         }
     }
 }
