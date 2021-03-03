a45ba11beacf750497c74bbf2866724ca20b5e59
Antoine Griffard <agriffard@hotmail.com>
2021-03-03 12:16:59 +0100

    Rule Benchmark

--- test/OrchardCore.Benchmarks/RuleBenchmark.cs
+++ test/OrchardCore.Benchmarks/RuleBenchmark.cs
@@ -4,17 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 using Moq;
-using OrchardCore.Benchmark.Support;
-using OrchardCore.Modules.FileProviders;
-using OrchardCore.Mvc;
-using OrchardCore.ResourceManagement;
-using OrchardCore.Layers;
 using OrchardCore.Layers.Services;
-using OrchardCore.Localization;
 using OrchardCore.Rules;
 using OrchardCore.Rules.Models;
 using OrchardCore.Rules.Services;
@@ -68,19 +60,38 @@ namespace OrchardCore.Benchmark
             };
         }
 
-// Summary 21st Feb 2021
+        // Summary 3rd March 2021: dotnet run -c Release --filter RuleBenchmark --framework netcoreapp3.1 --job short
 
-// BenchmarkDotNet=v0.12.1, OS=macOS Catalina 10.15.7 (19H15) [Darwin 19.6.0]
-// Intel Core i7-6700K CPU 4.00GHz (Skylake), 1 CPU, 8 logical and 4 physical cores
-// .NET Core SDK=5.0.103
-//   [Host]     : .NET Core 3.1.8 (CoreCLR 4.700.20.41105, CoreFX 4.700.20.41903), X64 RyuJIT
-//   DefaultJob : .NET Core 3.1.8 (CoreCLR 4.700.20.41105, CoreFX 4.700.20.41903), X64 RyuJIT
+        //BenchmarkDotNet=v0.12.1, OS=Windows 10.0.19042
+        //Intel Core i9-9980HK CPU 2.40GHz, 1 CPU, 16 logical and 8 physical cores
+        //.NET Core SDK = 5.0.200
+        //  [Host]   : .NET Core 3.1.12 (CoreCLR 4.700.21.6504, CoreFX 4.700.21.6905), X64 RyuJIT
+        //  ShortRun : .NET Core 3.1.12 (CoreCLR 4.700.21.6504, CoreFX 4.700.21.6905), X64 RyuJIT
+
+        //Job=ShortRun IterationCount = 3  LaunchCount=1
+        //WarmupCount=3
+
+        //|                           Method |     Mean |    Error |    StdDev | Ratio | RatioSD |  Gen 0 |  Gen 1 |  Gen 2 | Allocated |
+        //|--------------------------------- |---------:|---------:|----------:|------:|--------:|-------:|-------:|-------:|----------:|
+        //| EvaluateIsHomepageWithJavascript | 3.991 us | 3.824 us | 0.2096 us |  1.00 |    0.00 | 0.1373 | 0.0381 |      - |    1168 B |
+        //|       EvaluateIsHomepageWithRule | 1.038 us | 4.620 us | 0.2532 us |  0.26 |    0.05 | 0.0277 | 0.0105 | 0.0010 |     224 B |
 
 
-// |                           Method |       Mean |    Error |   StdDev | Ratio |  Gen 0 |  Gen 1 | Gen 2 | Allocated |
-// |--------------------------------- |-----------:|---------:|---------:|------:|-------:|-------:|------:|----------:|
-// | EvaluateIsHomepageWithJavascript | 2,397.3 ns | 31.24 ns | 27.69 ns |  1.00 | 0.2365 | 0.0763 |     - |    1168 B |
-// |       EvaluateIsHomepageWithRule |   880.8 ns | 16.97 ns | 22.07 ns |  0.37 | 0.0343 | 0.0095 |     - |     224 B |
+        // Summary 3rd March 2021: dotnet run -c Release --filter RuleBenchmark --framework net5.0 --job short
+
+        //BenchmarkDotNet=v0.12.1, OS=Windows 10.0.19042
+        //Intel Core i9-9980HK CPU 2.40GHz, 1 CPU, 16 logical and 8 physical cores
+        //.NET Core SDK = 5.0.200
+        //  [Host]   : .NET Core 5.0.3 (CoreCLR 5.0.321.7212, CoreFX 5.0.321.7212), X64 RyuJIT
+        //  ShortRun : .NET Core 5.0.3 (CoreCLR 5.0.321.7212, CoreFX 5.0.321.7212), X64 RyuJIT
+
+        //Job=ShortRun IterationCount = 3  LaunchCount=1
+        //WarmupCount=3
+
+        //|                           Method |       Mean |      Error |    StdDev | Ratio | RatioSD |  Gen 0 |  Gen 1 | Gen 2 | Allocated |
+        //|--------------------------------- |-----------:|-----------:|----------:|------:|--------:|-------:|-------:|------:|----------:|
+        //| EvaluateIsHomepageWithJavascript | 4,122.9 ns | 6,853.9 ns | 375.69 ns |  1.00 |    0.00 | 0.1221 | 0.0305 |     - |    1040 B |
+        //|       EvaluateIsHomepageWithRule |   772.6 ns |   750.9 ns |  41.16 ns |  0.19 |    0.02 | 0.0219 | 0.0086 |     - |     184 B |
 
         [Benchmark(Baseline = true)]
         public void EvaluateIsHomepageWithJavascript()
