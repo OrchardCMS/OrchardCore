e88b22a0ef0b7602ba593140b2573fa272594828
Gábor Domonkos <gabor.domonkos@lombiq.com>
2021-03-03 19:16:19 +0100

    Code styling

--- src/OrchardCore.Modules/OrchardCore.AuditTrail/Settings/AuditTrailSettings.cs
+++ src/OrchardCore.Modules/OrchardCore.AuditTrail/Settings/AuditTrailSettings.cs
@@ -8,10 +8,13 @@ namespace OrchardCore.AuditTrail.Settings
     public class AuditTrailSettings
     {
         public bool EnableClientIpAddressLogging { get; set; }
+
         public IEnumerable<AuditTrailEventSetting> EventSettings { get; set; } = 
             Enumerable.Empty<AuditTrailEventSetting>();
+
         public IEnumerable<IgnoredContentTypesViewModel> IgnoredContentTypes { get; set; } = 
             Enumerable.Empty<IgnoredContentTypesViewModel>();
+
         public string[] IgnoredContentTypeNames { get; set; } = Array.Empty<string>();
     }
 }
