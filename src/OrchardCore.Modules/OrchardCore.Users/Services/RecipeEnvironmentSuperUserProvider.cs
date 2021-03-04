c4d7b38085874fcc4102e424dbfdd95e16110025
Titus Anderson <flew2bits@gmail.com>
2021-03-04 13:14:16 -0500

    Added try-catch block around SuperUser lookup for recipe migrations. (#8748)
    Fixes #8747

--- src/OrchardCore.Modules/OrchardCore.Users/Services/RecipeEnvironmentSuperUserProvider.cs
+++ src/OrchardCore.Modules/OrchardCore.Users/Services/RecipeEnvironmentSuperUserProvider.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using OrchardCore.Recipes.Services;
 using OrchardCore.Settings;
 
@@ -10,11 +11,16 @@ namespace OrchardCore.Users.Services
     {
         private readonly ISiteService _siteService;
         private readonly IUserService _userService;
+        private readonly ILogger _logger;
 
-        public RecipeEnvironmentSuperUserProvider(ISiteService siteService, IUserService userService)
+        public RecipeEnvironmentSuperUserProvider(
+            ISiteService siteService,
+            IUserService userService,
+            ILogger<RecipeEnvironmentSuperUserProvider> logger)
         {
             _siteService = siteService;
             _userService = userService;
+            _logger = logger;
         }
 
         public int Order => 0;
@@ -24,10 +30,17 @@ namespace OrchardCore.Users.Services
             var siteSettings = await _siteService.GetSiteSettingsAsync();
             if (!String.IsNullOrEmpty(siteSettings.SuperUser))
             {
-                var superUser = await _userService.GetUserByUniqueIdAsync(siteSettings.SuperUser);
-                if (superUser != null)
+                try
                 {
-                    environment["AdminUserId"] = siteSettings.SuperUser;
+                    var superUser = await _userService.GetUserByUniqueIdAsync(siteSettings.SuperUser);
+                    if (superUser != null)
+                    {
+                        environment["AdminUserId"] = siteSettings.SuperUser;
+                    }
+                } 
+                catch 
+                {
+                    _logger.LogWarning("Could not lookup AdminUserId; User migrations may not have run yet");
                 }
             }
         }
