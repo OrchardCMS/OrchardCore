d5608b8c726cab10d7ef88f287119596b1d5b5bd
Jasmin Savard <jasmin.savard@gmail.com>
2021-03-05 21:21:50 -0500

    For Jean-Thierry

--- src/OrchardCore.Modules/OrchardCore.Media/Services/MediaCacheBackgroundTask.cs
+++ src/OrchardCore.Modules/OrchardCore.Media/Services/MediaCacheBackgroundTask.cs
@@ -134,16 +134,18 @@ namespace OrchardCore.Media.Services
             }
             catch (IOException)
             {
-                //the file is unavailable because it is:
-                //still being written to
-                //or being processed by another thread
-                //or does not exist (has already been processed)
+                // The file is unavailable because it is:
+                // still being written to
+                // or being processed by another thread
+                // or does not exist (has already been processed)
                 return true;
             }
             finally
             {
                 if (stream != null)
+                {
                     stream.Close();
+                }
             }
 
             //file is not locked
