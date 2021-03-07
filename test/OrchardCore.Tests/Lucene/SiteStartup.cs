bbbcce58aab395b922e4addf445ff46ded2a4398
Mário Nunes <mariojsnunes@gmail.com>
2021-03-07 15:17:26 +0000

    Setup lucene test environment. Added test recipe. Test boost and wildcard on SimpleQueryString and Wildcard queries.

---
+++ test/OrchardCore.Tests/Lucene/SiteStartup.cs
@@ -0,0 +1,79 @@
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OrchardCore.Modules;
+using OrchardCore.Modules.Manifest;
+using OrchardCore.Security;
+using OrchardCore.Tests.Apis.Context;
+
+namespace OrchardCore.Tests.Lucene
+{
+    public class SiteStartup
+    {
+        public static ConcurrentDictionary<string, PermissionsContext> PermissionsContexts;
+
+        static SiteStartup()
+        {
+            PermissionsContexts = new ConcurrentDictionary<string, PermissionsContext>();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOrchardCms(builder =>
+                builder.AddSetupFeatures(
+                    "OrchardCore.Tenants"
+                )
+                .AddTenantFeatures(
+
+                )
+                .ConfigureServices(collection =>
+                {
+                    collection.AddScoped<IAuthorizationHandler, AlwaysLoggedInAuthHandler>();
+                    collection.AddAuthentication((options) =>
+                    {
+                        options.AddScheme<AlwaysLoggedInApiAuthenticationHandler>("Api", null);
+                    });
+                })
+                .Configure(appBuilder => appBuilder.UseAuthorization()));
+
+            services.AddSingleton<IModuleNamesProvider, ModuleNamesProvider>();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseOrchardCore();
+        }
+
+        private class ModuleNamesProvider : IModuleNamesProvider
+        {
+            private readonly string[] _moduleNames;
+
+            public ModuleNamesProvider()
+            {
+                Assembly assembly = Assembly.Load(new AssemblyName(typeof(Cms.Web.Startup).Assembly.GetName().Name));
+                _moduleNames = assembly.GetCustomAttributes<ModuleNameAttribute>().Select(m => m.Name).ToArray();
+            }
+
+            public IEnumerable<string> GetModuleNames()
+            {
+                return _moduleNames;
+            }
+        }
+    }
+
+    public class AlwaysLoggedInAuthHandler : AuthorizationHandler<PermissionRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, PermissionRequirement requirement)
+        {
+            context.Succeed(requirement);
+            return Task.CompletedTask;
+        }
+    }
+}
