1ecb141da7bf01c7f560e78c73b933655bf1d87f
Dean Marcussen <dean.marcussen@gmail.com>
2021-03-10 10:52:08 +0000

    wip. role index markers

--- src/OrchardCore/OrchardCore.Users.Core/Indexes/UserByRoleNameIndex.cs
+++ src/OrchardCore/OrchardCore.Users.Core/Indexes/UserByRoleNameIndex.cs
@@ -23,12 +23,28 @@ namespace OrchardCore.Users.Indexes
         public override void Describe(DescribeContext<User> context)
         {
             context.For<UserByRoleNameIndex, string>()
-                .Map(user => user.RoleNames.Select(x => new UserByRoleNameIndex
+                .Map(user => 
                 {
-                    RoleName = NormalizeKey(x),
-                    Count = 1
-                }))
-                .Group(user => user.RoleName)
+                    // Include a marker that the user does not have any roles, i.e. is Authenticated only.
+                    if (!user.RoleNames.Any())
+                    {
+                        return new UserByRoleNameIndex[]
+                        {
+                            new UserByRoleNameIndex
+                            {
+                                RoleName = NormalizeKey("Authenticated"),
+                                Count = 1
+                            }
+                        };
+                    }
+
+                    return user.RoleNames.Select(x => new UserByRoleNameIndex
+                    {
+                        RoleName = NormalizeKey(x),
+                        Count = 1
+                    });
+                })
+                .Group(index => index.RoleName)
                 .Reduce(group => new UserByRoleNameIndex
                 {
                     RoleName = group.Key,
