af1353d6d6194a73040bf5a1d756962db2578e6d
jtkech <jean-thierry.kechichian@wanadoo.fr>
2021-03-24 02:48:46 +0100

    Fixes #8948 Liquid ShapeTag

--- src/OrchardCore.Modules/OrchardCore.Contents/Liquid/ContentItemTag.cs
+++ src/OrchardCore.Modules/OrchardCore.Contents/Liquid/ContentItemTag.cs
@@ -1,12 +1,21 @@
-using OrchardCore.DisplayManagement.Liquid.Tags;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Fluid;
+using Fluid.Ast;
+using Fluid.Values;
 
 namespace OrchardCore.Contents.Liquid
 {
-    public class ContentItemTag : ShapeTag
+    public class ContentItemTag
     {
-        public ContentItemTag() : base("contentitem")
-        {
+        private static readonly FilterArgument _typeArgument = new FilterArgument("type", new LiteralExpression(StringValue.Create("contentitem")));
 
+        public static ValueTask<Completion> WriteToAsync(List<FilterArgument> argumentsList, TextWriter writer, TextEncoder encoder, TemplateContext context)
+        {
+            argumentsList.Add(_typeArgument);
+            return WriteToAsync(argumentsList, writer, encoder, context);
         }
     }
 }
