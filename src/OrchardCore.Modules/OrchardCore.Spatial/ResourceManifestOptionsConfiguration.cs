d59ac55949034eae29c978027464b85236567034
Jasmin Savard <jasmin.savard@gmail.com>
2021-03-25 19:51:50 -0400

    Update CDN to 1.7.1

--- src/OrchardCore.Modules/OrchardCore.Spatial/ResourceManifestOptionsConfiguration.cs
+++ src/OrchardCore.Modules/OrchardCore.Spatial/ResourceManifestOptionsConfiguration.cs
@@ -14,16 +14,16 @@ namespace OrchardCore.Spatial
             _manifest
                 .DefineScript("leaflet")
                 .SetUrl("/OrchardCore.Spatial/Scripts/leaflet/leaflet.js", "/OrchardCore.Spatial/Scripts/leaflet/leaflet-src.js")
-                .SetCdn("https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/leaflet.js", "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/leaflet.js")
-                .SetCdnIntegrity("sha256-CNm+7c26DTTCGRQkM9vp7aP85kHFMqs9MhPEuytF+fQ=", "sha256-CNm+7c26DTTCGRQkM9vp7aP85kHFMqs9MhPEuytF+fQ=")
+                .SetCdn("https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.min.js", "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet-src.js")
+                .SetCdnIntegrity("sha512-SeiQaaDh73yrb56sTW/RgVdi/mMqNeM2oBwubFHagc5BkixSpP1fvqF47mKzPGWYSSy4RwbBunrJBQ4Co8fRWA==", "sha512-I5Hd7FcJ9rZkH7uD01G3AjsuzFy3gqz7HIJvzFZGFt2mrCS4Piw9bYZvCgUE0aiJuiZFYIJIwpbNnDIM6ohTrg==")
                 .SetVersion("1.7.1")
                 ;
 
             _manifest
                 .DefineStyle("leaflet")
                 .SetUrl("/OrchardCore.Spatial/Styles/leaflet.min.css", "/OrchardCore.Spatial/Styles/leaflet.css")
-                .SetCdn("https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/leaflet.css", "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/leaflet.css")
-                .SetCdnIntegrity("sha256-iYUgmrapfDGvBrePJPrMWQZDcObdAcStKBpjP3Az+3s=", "sha256-iYUgmrapfDGvBrePJPrMWQZDcObdAcStKBpjP3Az+3s=")
+                .SetCdn("https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.min.css", "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.css")
+                .SetCdnIntegrity("sha512-1xoFisiGdy9nvho8EgXuXvnpR5GAMSjFwp40gSRE3NwdUdIMIKuPa7bqoUhLD0O/5tPNhteAsE5XyyMi5reQVA==", "sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==")
                 .SetVersion("1.7.1")
                 ;
         }
