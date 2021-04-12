1cd04bbe7c7592a4387b4d2d9c6dbbe769ecb4f3
jtkech <jean-thierry.kechichian@wanadoo.fr>
2021-04-12 04:48:06 +0200

    Litle refactoring

--- src/OrchardCore.Modules/OrchardCore.Sitemaps/Models/SitemapDocument.cs
+++ src/OrchardCore.Modules/OrchardCore.Sitemaps/Models/SitemapDocument.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using OrchardCore.Data.Documents;
 
@@ -7,8 +6,5 @@ namespace OrchardCore.Sitemaps.Models
     public class SitemapDocument : Document
     {
         public IDictionary<string, SitemapType> Sitemaps { get; set; } = new Dictionary<string, SitemapType>();
-        public Dictionary<string, string> SitemapIds { get; set; } = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
-        public Dictionary<string, string> SitemapPaths { get; set; } = new Dictionary<string, string>();
-        public bool IsBuilt { get; set; }
     }
 }
--- src/OrchardCore.Modules/OrchardCore.Sitemaps/Services/SitemapManager.cs
+++ src/OrchardCore.Modules/OrchardCore.Sitemaps/Services/SitemapManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,6 +11,11 @@ namespace OrchardCore.Sitemaps.Services
     {
         private readonly IDocumentManager<SitemapDocument> _documentManager;
 
+        private Dictionary<string, string> _sitemapIds;
+        private Dictionary<string, string> _sitemapPaths;
+
+        private string _identifier;
+
         public SitemapManager(IDocumentManager<SitemapDocument> documentManager)
         {
             _documentManager = documentManager;
@@ -19,17 +25,12 @@ namespace OrchardCore.Sitemaps.Services
         {
             var document = await GetDocumentAsync();
 
-            // For backward compatibility, run only once.
-            if (!document.IsBuilt)
+            if (_sitemapIds == null || _identifier != document.Identifier)
             {
-                // Load, build and update the document.
-                await UpdateSitemapAsync();
-
-                // Retrieve the updated scoped document.
-                document = await LoadDocumentAsync();
+                BuildEntries(document);
             }
 
-            if (document.SitemapIds.TryGetValue(path, out var sitemapId))
+            if (_sitemapIds.TryGetValue(path, out var sitemapId))
             {
                 return (true, sitemapId);
             }
@@ -41,17 +42,12 @@ namespace OrchardCore.Sitemaps.Services
         {
             var document = await GetDocumentAsync();
 
-            // For backward compatibility, run only once.
-            if (!document.IsBuilt)
+            if (_sitemapPaths == null || _identifier != document.Identifier)
             {
-                // Load, build and update the document.
-                await UpdateSitemapAsync();
-
-                // Retrieve the updated scoped document.
-                document = await LoadDocumentAsync();
+                BuildEntries(document);
             }
 
-            if (document.SitemapPaths.TryGetValue(sitemapId, out var path))
+            if (_sitemapPaths.TryGetValue(sitemapId, out var path))
             {
                 return (true, path);
             }
@@ -95,34 +91,27 @@ namespace OrchardCore.Sitemaps.Services
         {
             var existing = await LoadDocumentAsync();
             existing.Sitemaps.Remove(sitemapId);
-
-            BuildEntries(existing);
             await _documentManager.UpdateAsync(existing);
         }
 
         public async Task UpdateSitemapAsync(SitemapType sitemap)
         {
             var existing = await LoadDocumentAsync();
-
             existing.Sitemaps[sitemap.SitemapId] = sitemap;
             sitemap.Identifier = IdGenerator.GenerateId();
-
-            BuildEntries(existing);
             await _documentManager.UpdateAsync(existing);
         }
 
         public async Task UpdateSitemapAsync()
         {
             var existing = await LoadDocumentAsync();
-
-            BuildEntries(existing);
             await _documentManager.UpdateAsync(existing);
         }
 
-        private static void BuildEntries(SitemapDocument document)
+        private void BuildEntries(SitemapDocument document)
         {
-            document.SitemapIds.Clear();
-            document.SitemapPaths.Clear();
+            var sitemapIds = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+            var sitemapPaths = new Dictionary<string, string>();
 
             foreach (var sitemap in document.Sitemaps.Values)
             {
@@ -131,23 +120,26 @@ namespace OrchardCore.Sitemaps.Services
                     continue;
                 }
 
-                document.SitemapIds[sitemap.Path] = sitemap.SitemapId;
-                document.SitemapPaths[sitemap.SitemapId] = sitemap.Path;
+                sitemapIds[sitemap.Path] = sitemap.SitemapId;
+                sitemapPaths[sitemap.SitemapId] = sitemap.Path;
             }
 
-            document.IsBuilt = true;
+            lock (this)
+            {
+                _sitemapIds = sitemapIds;
+                _sitemapPaths = sitemapPaths;
+                _identifier = document.Identifier;
+            }
         }
 
         /// <summary>
         /// Loads the sitemap document from the store for updating and that should not be cached.
         /// </summary>
-        private Task<SitemapDocument> LoadDocumentAsync() => _documentManager.GetOrCreateMutableAsync(CreateDocumentAsync);
+        private Task<SitemapDocument> LoadDocumentAsync() => _documentManager.GetOrCreateMutableAsync();
 
         /// <summary>
         /// Gets the sitemap document from the cache for sharing and that should not be updated.
         /// </summary>
-        private Task<SitemapDocument> GetDocumentAsync() => _documentManager.GetOrCreateImmutableAsync(CreateDocumentAsync);
-
-        private Task<SitemapDocument> CreateDocumentAsync() => Task.FromResult(new SitemapDocument() { IsBuilt = true });
+        private Task<SitemapDocument> GetDocumentAsync() => _documentManager.GetOrCreateImmutableAsync();
     }
 }
