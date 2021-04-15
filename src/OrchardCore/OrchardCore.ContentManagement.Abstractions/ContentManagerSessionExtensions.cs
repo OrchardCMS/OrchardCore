64cb2595372fa56cbf7ef09dd6adf8e7d9a1fa18
jtkech <jean-thierry.kechichian@wanadoo.fr>
2021-04-15 03:07:39 +0200

    Missing extensions methods

--- src/OrchardCore/OrchardCore.ContentManagement.Abstractions/ContentManagerSessionExtensions.cs
+++ src/OrchardCore/OrchardCore.ContentManagement.Abstractions/ContentManagerSessionExtensions.cs
@@ -7,6 +7,13 @@ namespace OrchardCore.ContentManagement
 {
     public static class ContentManagerSessionExtensions
     {
+        public static async Task<ContentItem> FirstOrDefaultAsync(this IQuery<ContentItem> query, IContentManager contentManager)
+            => await contentManager.LoadAsync(await query.FirstOrDefaultAsync());
+
+        public static async Task<ContentItem> FirstOrDefaultAsync<TIndex>(this IQuery<ContentItem, TIndex> query, IContentManager contentManager)
+            where TIndex : class, IIndex
+            => await contentManager.LoadAsync(await query.FirstOrDefaultAsync());
+
         public static async Task<IEnumerable<ContentItem>> ListAsync(this IQuery<ContentItem> query, IContentManager contentManager)
             => await contentManager.LoadAsync(await query.ListAsync());
 
