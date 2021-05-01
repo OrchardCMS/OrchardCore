b3092991c523c8aa93eb71b355208967274861c1
hishamco <hishamco_2007@yahoo.com>
2021-05-01 21:50:08 +0300

    Try to use using block

--- test/OrchardCore.Tests/Modules/OrchardCore.Email/MailMessageTests.cs
+++ test/OrchardCore.Tests/Modules/OrchardCore.Email/MailMessageTests.cs
@@ -9,13 +9,12 @@ namespace OrchardCore.Tests.Modules.OrchardCore.Email
         [Fact]
         public void MailMessageDispose_DisposesItsAttachments()
         {
-            // Arrange
-            var message = new MailMessage();
-            message.Attachments.Add(new MailMessageAttachment { Stream = new MemoryStream() });
-            message.Attachments.Add(new MailMessageAttachment { Stream = new MemoryStream() });
-
-            // Act
-            message.Dispose();
+            // Arrange & Act
+            using(var message = new MailMessage())
+            {
+                message.Attachments.Add(new MailMessageAttachment { Stream = new MemoryStream() });
+                message.Attachments.Add(new MailMessageAttachment { Stream = new MemoryStream() });
+            }
 
             // Assert
             Assert.All(message.Attachments, a => Assert.False(a.Stream.CanRead));
