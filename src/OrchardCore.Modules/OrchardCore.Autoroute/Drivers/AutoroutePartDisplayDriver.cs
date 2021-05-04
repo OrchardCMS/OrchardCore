0cf4327ad2891a47285429cc888e6561ff78f883
Dean Marcussen <dean.marcussen@gmail.com>
2021-05-04 11:28:23 +0100

    Update handler to use the same logic

--- src/OrchardCore.Modules/OrchardCore.Autoroute/Drivers/AutoroutePartDisplayDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.Autoroute/Drivers/AutoroutePartDisplayDriver.cs
@@ -118,9 +118,8 @@ namespace OrchardCore.Autoroute.Drivers
                 {
                     var path = model.Path.Trim('/');
                     var paths = new string[] { path, "/" + path, path + "/", "/" + path + "/" };
-
+                    
                     var possibleConflicts = await _session.QueryIndex<AutoroutePartIndex>(o => (o.Published || o.Latest) && o.Path.IsIn(paths)).ListAsync();
-
                     if (possibleConflicts.Any(x => x.ContentItemId != model.ContentItem.ContentItemId && x.ContainedContentItemId != model.ContentItem.ContentItemId))
                     {
                         updater.ModelState.AddModelError(Prefix, nameof(model.Path), S["Your permalink is already in use."]);
