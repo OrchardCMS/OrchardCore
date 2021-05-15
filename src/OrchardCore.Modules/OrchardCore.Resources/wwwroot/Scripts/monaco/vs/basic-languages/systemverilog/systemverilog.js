5de843666988fcda73793dd40449847939e197c1
Antoine Griffard <agriffard@hotmail.com>
2021-05-15 21:18:31 +0200

    Monaco 0.24.0 (#9443)

--- src/OrchardCore.Modules/OrchardCore.Resources/wwwroot/Scripts/monaco/vs/basic-languages/systemverilog/systemverilog.js
+++ src/OrchardCore.Modules/OrchardCore.Resources/wwwroot/Scripts/monaco/vs/basic-languages/systemverilog/systemverilog.js
@@ -1,6 +1,6 @@
 /*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * monaco-languages version: 2.3.0(57af10ae0184db4e0f7f9a92ff972629c39ccb53)
+ * monaco-languages version: 2.4.0(2e51a900146f672d53d88b6c63355fb2b22d40c2)
  * Released under the MIT license
  * https://github.com/Microsoft/monaco-languages/blob/master/LICENSE.md
  *-----------------------------------------------------------------------------*/
