01a516adaf9b6f59c7359108066c166ddf90b711
Dean Marcussen <dean.marcussen@gmail.com>
2021-05-16 16:07:08 +0100

    Missing routing

--- src/OrchardCore.Modules/OrchardCore.AuditTrail/Startup.cs
+++ src/OrchardCore.Modules/OrchardCore.AuditTrail/Startup.cs
@@ -1,5 +1,11 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
+using OrchardCore.Admin;
+using OrchardCore.AuditTrail.Controllers;
 using OrchardCore.AuditTrail.Drivers;
 using OrchardCore.AuditTrail.Indexes;
 using OrchardCore.AuditTrail.Models;
@@ -16,6 +22,7 @@ using OrchardCore.Data.Migration;
 using OrchardCore.Deployment;
 using OrchardCore.DisplayManagement.Handlers;
 using OrchardCore.Modules;
+using OrchardCore.Mvc.Core.Utilities;
 using OrchardCore.Navigation;
 using OrchardCore.Security.Permissions;
 using OrchardCore.Settings;
@@ -26,6 +33,13 @@ namespace OrchardCore.AuditTrail
 {
     public class Startup : StartupBase
     {
+        private readonly AdminOptions _adminOptions;
+
+        public Startup(IOptions<AdminOptions> adminOptions)
+        {
+            _adminOptions = adminOptions.Value;
+        }
+
         public override void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IAuditTrailManager, AuditTrailManager>();
@@ -54,8 +68,8 @@ namespace OrchardCore.AuditTrail
             services.AddTransient<IDeploymentSource, SiteSettingsPropertyDeploymentSource<AuditTrailSettings>>();
             services.AddScoped<IDisplayDriver<DeploymentStep>>(sp =>
             {
-                var T = sp.GetService<IStringLocalizer<Startup>>();
-                return new SiteSettingsPropertyDeploymentStepDriver<AuditTrailSettings>(T["Audit Trail settings"], T["Exports the audit trail settings."]);
+                var S = sp.GetService<IStringLocalizer<Startup>>();
+                return new SiteSettingsPropertyDeploymentStepDriver<AuditTrailSettings>(S["Audit Trail settings"], S["Exports the audit trail settings."]);
             });
 
             services.AddSingleton<IDeploymentStepFactory>(new SiteSettingsPropertyDeploymentStepFactory<AuditTrailSettings>());
@@ -63,11 +77,29 @@ namespace OrchardCore.AuditTrail
             services.AddTransient<IDeploymentSource, SiteSettingsPropertyDeploymentSource<AuditTrailTrimmingSettings>>();
             services.AddScoped<IDisplayDriver<DeploymentStep>>(sp =>
             {
-                var T = sp.GetService<IStringLocalizer<Startup>>();
-                return new SiteSettingsPropertyDeploymentStepDriver<AuditTrailTrimmingSettings>(T["Audit Trail Trimming settings"], T["Exports the audit trail trimming settings."]);
+                var S = sp.GetService<IStringLocalizer<Startup>>();
+                return new SiteSettingsPropertyDeploymentStepDriver<AuditTrailTrimmingSettings>(S["Audit Trail Trimming settings"], S["Exports the audit trail trimming settings."]);
             });
 
             services.AddSingleton<IDeploymentStepFactory>(new SiteSettingsPropertyDeploymentStepFactory<AuditTrailTrimmingSettings>());
+        }
+
+        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+            var adminControllerName = typeof(AdminController).ControllerName();
+
+            routes.MapAreaControllerRoute(
+                name: "AuditTrailIndex",
+                areaName: "OrchardCore.AuditTrail",
+                pattern: _adminOptions.AdminUrlPrefix + "/AuditTrail",
+                defaults: new { controller = adminControllerName, action = nameof(AdminController.Index) }
+            );
+            routes.MapAreaControllerRoute(
+                name: "AuditTrailIndex",
+                areaName: "OrchardCore.AdminMenu",
+                pattern: _adminOptions.AdminUrlPrefix + "/AuditTrail/Create",
+                defaults: new { controller = adminControllerName, action = nameof(AdminController.Detail) }
+            );
         }
     }
 }
