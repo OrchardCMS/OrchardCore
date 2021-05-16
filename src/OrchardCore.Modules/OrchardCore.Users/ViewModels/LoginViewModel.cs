2cbb935cb42fe4cc432938d7b4bc794308a30b12
hishamco <hishamco_2007@yahoo.com>
2021-05-16 09:50:50 +0300

    DataAnnotations localization should work in LoginViewModel

--- src/OrchardCore.Modules/OrchardCore.Users/ViewModels/LoginViewModel.cs
+++ src/OrchardCore.Modules/OrchardCore.Users/ViewModels/LoginViewModel.cs
@@ -1,31 +1,17 @@
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Localization;
 
 namespace OrchardCore.Users.ViewModels
 {
-    public class LoginViewModel : IValidatableObject
+    public class LoginViewModel
     {
+        [Required(ErrorMessage = "Username is required.")]
+        [Display(Name = "Username")]
         public string UserName { get; set; }
 
+        [Required(ErrorMessage = "Password is required."]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
         public bool RememberMe { get; set; }
-
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            var S = validationContext.GetService<IStringLocalizer<LoginViewModel>>();
-            if (string.IsNullOrWhiteSpace(UserName))
-            {
-                yield return new ValidationResult(S["Username is required."], new[] { "UserName" });
-            }
-
-            if (string.IsNullOrWhiteSpace(Password))
-            {
-                yield return new ValidationResult(S["Password is required."], new[] { "Password" });
-            }
-        }
     }
 }
