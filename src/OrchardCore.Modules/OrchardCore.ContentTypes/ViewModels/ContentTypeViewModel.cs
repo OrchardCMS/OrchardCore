83f7ff2c874bb7d03ba51cdcc830a5388a3f7dbf
Kshitiz Shakya <kshitiz.shakya22@gmail.com>
2021-05-16 07:35:57 +0545

    Show technical name of content type and display name of Content Parts in Edit Content Type page

--- src/OrchardCore.Modules/OrchardCore.ContentTypes/ViewModels/ContentTypeViewModel.cs
+++ src/OrchardCore.Modules/OrchardCore.ContentTypes/ViewModels/ContentTypeViewModel.cs
@@ -3,5 +3,6 @@ namespace OrchardCore.ContentTypes.ViewModels
     public class ContentTypeViewModel
     {
         public string DisplayName { get; set; }
+        public string Name { get; set; }
     }
 }
