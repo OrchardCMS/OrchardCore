9c8cbf12d0d70b3a2b096e76b581690baee0495e
Antoine Griffard <agriffard@hotmail.com>
2021-05-26 10:36:14 +0200

    StartBootstrap CleanBlog 6 (#9506)

--- src/OrchardCore.Themes/TheBlogTheme/ResourceManifestOptionsConfiguration.cs
+++ src/OrchardCore.Themes/TheBlogTheme/ResourceManifestOptionsConfiguration.cs
@@ -12,17 +12,10 @@ namespace OrchardCore.Themes.TheBlogTheme
             _manifest = new ResourceManifest();
 
             _manifest
-                .DefineScript("TheBlogTheme-jQuery")
-                .SetCdn("https://code.jquery.com/jquery-3.5.1.min.js", "https://code.jquery.com/jquery-3.5.1.js")
-                .SetCdnIntegrity("sha384-ZvpUoO/+PpLXR1lu4jmpXWu80pZlYUAfxl5NsBMWOEPSjUn/6Z/hRTt8+pR6L4N2", "sha384-/LjQZzcpTzaYn7qWqRIWYC5l8FWEZ2bIHIz0D73Uzba4pShEcdLdZyZkI4Kv676E")
-                .SetVersion("3.5.1");
-
-            _manifest
                 .DefineScript("TheBlogTheme-bootstrap-bundle")
-                .SetDependencies("TheBlogTheme-jQuery")
-                .SetCdn("https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js", "https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.js")
-                .SetCdnIntegrity("sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns", "sha384-ZEiV3j24mJ99uunGq2LXkicfbHO18KBrDHpdTjD1NUT07kWB6B8u6U5t6UBimJ7w")
-                .SetVersion("4.6.0");
+                .SetCdn("https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js", "https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.js")
+                .SetCdnIntegrity("sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4", "sha384-zlQmapo6noJSGz1A/oxylOFtN0k8EiXX45sOWv3x9f/RGYG0ECMxTbMao6+OLt2e")
+                .SetVersion("5.0.1");
 
             _manifest
                 .DefineStyle("TheBlogTheme-bootstrap-oc")
@@ -31,14 +24,14 @@ namespace OrchardCore.Themes.TheBlogTheme
 
             _manifest
                 .DefineScript("TheBlogTheme")
-                .SetDependencies("TheBlogTheme-jQuery")
+                .SetDependencies("TheBlogTheme-bootstrap-bundle")
                 .SetUrl("~/TheBlogTheme/js/scripts.min.js", "~/TheBlogTheme/js/scripts.js")
-                .SetVersion("5.1.0");
+                .SetVersion("6.0.0");
 
             _manifest
                 .DefineStyle("TheBlogTheme")
                 .SetUrl("~/TheBlogTheme/css/styles.min.css", "~/TheBlogTheme/css/styles.css")
-                .SetVersion("5.1.0");
+                .SetVersion("6.0.0");
         }
 
         public void Configure(ResourceManagementOptions options)
--- src/OrchardCore.Themes/TheBlogTheme/wwwroot/js/scripts.js
+++ src/OrchardCore.Themes/TheBlogTheme/wwwroot/js/scripts.js
@@ -4,45 +4,34 @@
 */
 
 /*!
-* Start Bootstrap - Clean Blog v5.1.0 (https://startbootstrap.com/theme/clean-blog)
+* Start Bootstrap - Clean Blog v6.0.0 (https://startbootstrap.com/theme/clean-blog)
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
 */
-(function ($) {
-  "use strict"; // Start of use strict
-  // Floating label headings for the contact form
+window.addEventListener('DOMContentLoaded', function () {
+  var scrollPos = 0;
+  var mainNav = document.getElementById('mainNav');
+  var headerHeight = mainNav.clientHeight;
+  window.addEventListener('scroll', function () {
+    var currentTop = document.body.getBoundingClientRect().top * -1;
 
-  $("body").on("input propertychange", ".floating-label-form-group", function (e) {
-    $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-  }).on("focus", ".floating-label-form-group", function () {
-    $(this).addClass("floating-label-form-group-with-focus");
-  }).on("blur", ".floating-label-form-group", function () {
-    $(this).removeClass("floating-label-form-group-with-focus");
-  }); // Show the navbar when the page is scrolled up
-
-  var MQL = 992; //primary navigation slide-in effect
-
-  if ($(window).width() > MQL) {
-    var headerHeight = $('#mainNav').height();
-    $(window).on('scroll', {
-      previousTop: 0
-    }, function () {
-      var currentTop = $(window).scrollTop(); //check if user is scrolling up
-
-      if (currentTop < this.previousTop) {
-        //if scrolling up...
-        if (currentTop > 0 && $('#mainNav').hasClass('is-fixed')) {
-          $('#mainNav').addClass('is-visible');
-        } else {
-          $('#mainNav').removeClass('is-visible is-fixed');
-        }
-      } else if (currentTop > this.previousTop) {
-        //if scrolling down...
-        $('#mainNav').removeClass('is-visible');
-        if (currentTop > headerHeight && !$('#mainNav').hasClass('is-fixed')) $('#mainNav').addClass('is-fixed');
+    if (currentTop < scrollPos) {
+      // Scrolling Up
+      if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
+        mainNav.classList.add('is-visible');
+      } else {
+        console.log(123);
+        mainNav.classList.remove('is-visible', 'is-fixed');
       }
+    } else {
+      // Scrolling Down
+      mainNav.classList.remove(['is-visible']);
 
-      this.previousTop = currentTop;
-    });
-  }
-})(jQuery); // End of use strict
+      if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
+        mainNav.classList.add('is-fixed');
+      }
+    }
+
+    scrollPos = currentTop;
+  });
+});
