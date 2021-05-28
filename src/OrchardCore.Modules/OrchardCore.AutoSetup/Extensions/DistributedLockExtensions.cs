6382d7afb66eaf8bbb29ec9edcc3000307817b22
Jean-Thierry Kéchichian <jean-thierry.kechichian@wanadoo.fr>
2021-05-29 00:34:31 +0200

    Update DistributedLockExtensions.cs

--- src/OrchardCore.Modules/OrchardCore.AutoSetup/Extensions/DistributedLockExtensions.cs
+++ src/OrchardCore.Modules/OrchardCore.AutoSetup/Extensions/DistributedLockExtensions.cs
@@ -7,33 +7,40 @@ using OrchardCore.Locking.Distributed;
 namespace OrchardCore.AutoSetup.Extensions
 {
     /// <summary>
-    /// The distributed lock extensions for Auto Setup.
+    /// The distributed lock extensions for auto setup.
     /// </summary>
     public static class DistributedLockExtensions
     {
         /// <summary>
-        /// Tries to acquire a setup lock 
+        /// Tries to acquire an auto setup lock.
         /// </summary>
         /// <param name="distributedLock">
-        /// The distributed Lock.
+        /// The distributed lock.
         /// </param>
         /// <param name="lockOptions">
-        /// The setup options.
+        /// The auto setup lock options.
         /// </param>
         /// <returns>
-        /// The <see cref="Task"/>.
+        /// The <see cref="ILocker"/> and <c>true</c> if successfully acquired.
         /// </returns>
         public static Task<(ILocker locker, bool locked)> TryAcquireAutoSetupLockAsync(this IDistributedLock distributedLock, LockOptions lockOptions)
         {
-            if (lockOptions == null)
+            var timeout = lockOptions?.LockTimeout ?? 0;
+            if (timeout <= 0)
             {
-                throw new ArgumentNullException(nameof(lockOptions));
+                timeout = 30_000;
+            }
+
+            var expiration = lockOptions?.LockExpiration ?? 0;
+            if (expiration <= 0)
+            {
+                expiration = 30_000;
             }
 
             return distributedLock.TryAcquireLockAsync(
                 "AUTOSETUP_LOCK",
-                TimeSpan.FromMilliseconds(lockOptions.LockTimeout),
-                TimeSpan.FromMilliseconds(lockOptions.LockExpiration));
+                TimeSpan.FromMilliseconds(timeout),
+                TimeSpan.FromMilliseconds(expiration));
         }
     }
 }
