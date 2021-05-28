28403ba36f1b6dbbccb009e9b6a3ea7f99c72e61
Jean-Thierry Kéchichian <jean-thierry.kechichian@wanadoo.fr>
2021-05-29 00:33:24 +0200

    Update LockOptions.cs

--- src/OrchardCore.Modules/OrchardCore.AutoSetup/Options/LockOptions.cs
+++ src/OrchardCore.Modules/OrchardCore.AutoSetup/Options/LockOptions.cs
@@ -1,20 +1,10 @@
 namespace OrchardCore.AutoSetup.Options
 {
     /// <summary>
-    /// The AutoSetup lock options.
+    /// The auto setup lock options.
     /// </summary>
     public class LockOptions
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="LockOptions"/> class with Default Lock Option Values.
-        /// </summary>
-        public LockOptions()
-        {
-            LockExpiration = 20_000;
-            LockTimeout = 20_000;
-            LockName = "AUTOSETUP_LOCK";
-        }
-
         /// <summary>
         /// The timeout in milliseconds to acquire a distributed setup lock.
         /// </summary>
@@ -24,10 +14,5 @@ namespace OrchardCore.AutoSetup.Options
         /// The expiration in milliseconds of the distributed setup lock.
         /// </summary>
         public int LockExpiration { get; set; }
-
-        /// <summary>
-        /// The distributed lock key which used while setup.
-        /// </summary>
-        public string LockName { get; set; }
     }
 }
