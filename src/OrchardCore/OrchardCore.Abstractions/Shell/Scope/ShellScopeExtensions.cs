6d25cd862cbc116dd3cf155c95211f7add4873f7
Jean-Thierry Kéchichian <jean-thierry.kechichian@wanadoo.fr>
2021-05-29 03:18:44 +0200

    Minor tweak to ensure BeforeDisposeAsync is called (#9580)

--- src/OrchardCore/OrchardCore.Abstractions/Shell/Scope/ShellScopeExtensions.cs
+++ src/OrchardCore/OrchardCore.Abstractions/Shell/Scope/ShellScopeExtensions.cs
@@ -6,11 +6,25 @@ namespace OrchardCore.Environment.Shell.Scope
     public static class ShellScopeExtensions
     {
         /// <summary>
-        /// Registers a delegate to be invoked before this shell scope will be disposed.
+        /// Registers a delegate task to be invoked before this shell scope will be disposed.
         /// </summary>
         public static ShellScope RegisterBeforeDispose(this ShellScope scope, Func<ShellScope, Task> callback)
         {
             scope?.BeforeDispose(callback);
+            return scope;
+        }
+
+        /// <summary>
+        /// Registers a delegate action to be invoked before this shell scope will be disposed.
+        /// </summary>
+        public static ShellScope RegisterBeforeDispose(this ShellScope scope, Action<ShellScope> callback)
+        {
+            scope?.BeforeDispose(scope =>
+            {
+                callback(scope);
+                return Task.CompletedTask;
+            });
+
             return scope;
         }
 
@@ -33,11 +47,39 @@ namespace OrchardCore.Environment.Shell.Scope
         }
 
         /// <summary>
-        /// Adds an handler to be invoked if an exception is thrown while executing in this shell scope.
+        /// Adds an Action to be executed in a new scope once this shell scope has been disposed.
+        /// </summary>
+        public static ShellScope AddDeferredTask(this ShellScope scope, Action<ShellScope> callback)
+        {
+            scope?.DeferredTask(scope =>
+            {
+                callback(scope);
+                return Task.CompletedTask;
+            });
+
+            return scope;
+        }
+
+        /// <summary>
+        /// Adds an handler task to be invoked if an exception is thrown while executing in this shell scope.
         /// </summary>
         public static ShellScope AddExceptionHandler(this ShellScope scope, Func<ShellScope, Exception, Task> handler)
         {
             scope?.ExceptionHandler(handler);
+            return scope;
+        }
+
+        /// <summary>
+        /// Adds an handler action to be invoked if an exception is thrown while executing in this shell scope.
+        /// </summary>
+        public static ShellScope AddExceptionHandler(this ShellScope scope, Action<ShellScope, Exception> handler)
+        {
+            scope?.ExceptionHandler((scope, e) =>
+            {
+                handler(scope, e);
+                return Task.CompletedTask;
+            });
+
             return scope;
         }
     }
