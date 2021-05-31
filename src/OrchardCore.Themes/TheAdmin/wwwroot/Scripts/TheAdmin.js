f91cede9b061a78f22270267c738ac52a8b0e9da
Antoine Griffard <agriffard@hotmail.com>
2021-05-31 10:05:24 +0200

    jsplumb 2.15.6

--- src/OrchardCore.Themes/TheAdmin/wwwroot/Scripts/TheAdmin.js
+++ src/OrchardCore.Themes/TheAdmin/wwwroot/Scripts/TheAdmin.js
@@ -7053,6 +7053,8 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
     value: true
   });
 });
+var _excluded = ["callback"];
+
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) { symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); } keys.push.apply(keys, symbols); } return keys; }
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
@@ -7065,7 +7067,7 @@ function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) r
 
 function confirmDialog(_ref) {
   var callback = _ref.callback,
-      options = _objectWithoutProperties(_ref, ["callback"]);
+      options = _objectWithoutProperties(_ref, _excluded);
 
   var defaultOptions = $('#confirmRemoveModalMetadata').data();
 
