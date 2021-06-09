e877e77e3c4011c91f32c6c5acbf950411d2bcf6
Dean Marcussen <dean.marcussen@gmail.com>
2021-06-09 08:24:05 +0100

    Remove unused extension

--- src/OrchardCore/OrchardCore.AuditTrail.Abstractions/Extensions/CollectionExtensions.cs
+++ src/OrchardCore/OrchardCore.AuditTrail.Abstractions/Extensions/CollectionExtensions.cs
@@ -4,14 +4,6 @@ namespace OrchardCore.AuditTrail.Extensions
 {
     public static class CollectionExtensions
     {
-        public static void AddToListIfNotNull<T>(this List<T> list, T value) where T : class
-        {
-            if (value != null)
-            {
-                list.Add(value);
-            }
-        }
-
         public static T Get<T>(this Dictionary<string, object> data, string key)
         {
             if (data != null && data.TryGetValue(key, out var value))
