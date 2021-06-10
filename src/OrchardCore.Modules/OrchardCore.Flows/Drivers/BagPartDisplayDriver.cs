eda7738ae2c7fcd799f253728b0d2ca9bfcc37de
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2021-06-10 17:51:33 +0200

    Typo

--- src/OrchardCore.Modules/OrchardCore.Flows/Drivers/BagPartDisplayDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.Flows/Drivers/BagPartDisplayDriver.cs
@@ -69,7 +69,7 @@ namespace OrchardCore.Flows.Drivers
             {
                 var contentItem = await _contentManager.NewAsync(model.ContentTypes[i]);
                 var existingContentItem = part.ContentItems.FirstOrDefault(x => String.Equals(x.ContentItemId, model.Prefixes[i], StringComparison.OrdinalIgnoreCase));
-                // When the content item already exists merge its elements to preverse nested content item ids.
+                // When the content item already exists merge its elements to preserve nested content item ids.
                 // All of the data for these merged items is then replaced by the model values on update, while a nested content item id is maintained.
                 // This prevents nested items which rely on the content item id, i.e. the media attached field, losing their reference point.
                 if (existingContentItem != null)
