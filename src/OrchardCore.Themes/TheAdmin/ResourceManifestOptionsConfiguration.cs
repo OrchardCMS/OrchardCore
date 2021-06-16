82079bb53dbe8b0c5e3f0113392645d9266f5ec2
Jasmin Savard <jasmin.savard@gmail.com>
2021-06-16 00:48:54 -0400

    Cleanup and minify

--- src/OrchardCore.Themes/TheAdmin/ResourceManifestOptionsConfiguration.cs
+++ src/OrchardCore.Themes/TheAdmin/ResourceManifestOptionsConfiguration.cs
@@ -14,7 +14,7 @@ namespace OrchardCore.Themes.TheAdmin
             _manifest
                 .DefineStyle("material-icons")
                 .SetCdn("https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp")
-                .SetUrl("~/TheAdmin/fonts/material-design-icons/material-icons.css")
+                .SetUrl("~/TheAdmin/fonts/material-design-icons/material-icons.min.css", "~/TheAdmin/fonts/material-design-icons/material-icons.css")
                 .SetVersion("3.0.1");
 
             _manifest
