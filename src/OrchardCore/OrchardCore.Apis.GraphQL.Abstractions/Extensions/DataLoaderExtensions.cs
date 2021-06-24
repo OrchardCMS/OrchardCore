ea0d3a1a0260770baea1c6c6dbe3bfea70174726
Sébastien Ros <sebastienros@gmail.com>
2021-06-24 10:42:16 -0700

    Fix DataLoader concurrent issues
    Fixes #9651

--- src/OrchardCore/OrchardCore.Apis.GraphQL.Abstractions/Extensions/DataLoaderExtensions.cs
+++ src/OrchardCore/OrchardCore.Apis.GraphQL.Abstractions/Extensions/DataLoaderExtensions.cs
@@ -7,16 +7,16 @@ namespace OrchardCore.Apis.GraphQL
 {
     public static class DataLoaderExtensions
     {
-        public static Task<T[]> LoadAsync<TKey, T>(this IDataLoader<TKey, T> dataLoader, IEnumerable<TKey> keys)
+        public static async Task<T[]> LoadAsync<TKey, T>(this IDataLoader<TKey, T> dataLoader, IEnumerable<TKey> keys)
         {
-            var tasks = new List<Task<T>>();
+            var items = new List<T>();
 
             foreach (var key in keys)
             {
-                tasks.Add(dataLoader.LoadAsync(key));
+                items.Add(await dataLoader.LoadAsync(key));
             }
 
-            return Task.WhenAll(tasks);
+            return items.ToArray();
         }
 
         public static Task<T[]> LoadAsync<TKey, T>(this IDataLoader<TKey, T> dataLoader, params TKey[] keys)
