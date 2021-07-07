c8002585c4a59f3bffbdf0a7b686c8c13d4d68d6
Jasmin Savard <jasmin.savard@gmail.com>
2021-07-07 06:27:35 -0400

    add html comment for scripts

--- src/OrchardCore.Modules/OrchardCore.Google/TagManager/GoogleTagManagerFilter.cs
+++ src/OrchardCore.Modules/OrchardCore.Google/TagManager/GoogleTagManagerFilter.cs
@@ -38,7 +38,7 @@ namespace OrchardCore.Google.TagManager
 
                     if (!string.IsNullOrWhiteSpace(settings?.ContainerID))
                     {
-                        _scriptsCache = new HtmlString("<!-- Google Tag Manager -->\n<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\nnew Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\nj=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n})(window,document,'script','dataLayer','" + settings.ContainerID + "');</script>\n<!-- End Google Tag Manager -->");
+                        _scriptsCache = new HtmlString("<!-- Google Tag Manager -->\n<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\n  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\n  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n  })(window,document,'script','dataLayer','" + settings.ContainerID + "');</script>\n<!-- End Google Tag Manager -->");
                     }
                 }
 
