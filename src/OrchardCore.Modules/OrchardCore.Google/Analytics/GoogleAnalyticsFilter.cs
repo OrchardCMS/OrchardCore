76178263a1f35c20027eb086951b51e339098db1
Jasmin Savard <jasmin.savard@gmail.com>
2021-07-12 02:31:57 -0400

    wip

--- src/OrchardCore.Modules/OrchardCore.Google/Analytics/GoogleAnalyticsFilter.cs
+++ src/OrchardCore.Modules/OrchardCore.Google/Analytics/GoogleAnalyticsFilter.cs
@@ -40,7 +40,7 @@ namespace OrchardCore.Google.Analytics
 
                     if (settings.SettingEntries.Length > 0)
                     {
-                        var defaultMeasurementId = settings.SettingEntries.Where(x => x.IsDefault == true);
+                        var defaultMeasurementId = settings.SettingEntries.FirstOrDefault(x => x.IsDefault == true).MeasurementId;
                         var sb = new StringBuilder($"<!-- Global site tag (gtag.js) - Google Analytics -->\n<script async src=\"https://www.googletagmanager.com/gtag/js?id={defaultMeasurementId}\"></script>\n<script>window.dataLayer = window.dataLayer || [];function gtag() {{ dataLayer.push(arguments); }}gtag('js', new Date());\n");
 
                         foreach(SettingEntry config in settings.SettingEntries)
