a9df63ba55a1510f44c568af23925a95a2a3f7c6
hishamco <hishamco_2007@yahoo.com>
2021-07-12 08:18:44 +0300

    Address feedback

--- src/OrchardCore.Modules/OrchardCore.Users/Workflows/Drivers/RegisterUserTaskDisplayDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.Users/Workflows/Drivers/RegisterUserTaskDisplayDriver.cs
@@ -1,3 +1,7 @@
+using System.Threading.Tasks;
+using Microsoft.Extensions.Localization;
+using OrchardCore.DisplayManagement.ModelBinding;
+using OrchardCore.DisplayManagement.Views;
 using OrchardCore.Users.Workflows.Activities;
 using OrchardCore.Users.Workflows.ViewModels;
 using OrchardCore.Workflows.Display;
@@ -7,26 +11,37 @@ namespace OrchardCore.Users.Workflows.Drivers
 {
     public class RegisterUserTaskDisplayDriver : ActivityDisplayDriver<RegisterUserTask, RegisterUserTaskViewModel>
     {
+        private readonly IStringLocalizer S;
+
+        public RegisterUserTaskDisplayDriver(IStringLocalizer<RegisterUserTaskDisplayDriver> localizer)
+        {
+            S = localizer;
+        }
+
         protected override void EditActivity(RegisterUserTask activity, RegisterUserTaskViewModel model)
         {
             model.SendConfirmationEmail = activity.SendConfirmationEmail;
-
-            if (activity.SendConfirmationEmail)
-            {
-                model.ConfirmationEmailSubject = activity.ConfirmationEmailSubject.Expression;
-                model.ConfirmationEmailTemplate = activity.ConfirmationEmailTemplate.Expression;
-            }
+            model.ConfirmationEmailSubject = activity.ConfirmationEmailSubject.Expression;
+            model.ConfirmationEmailTemplate = activity.ConfirmationEmailTemplate.Expression;
         }
 
-        protected override void UpdateActivity(RegisterUserTaskViewModel model, RegisterUserTask activity)
+        public async override Task<IDisplayResult> UpdateAsync(RegisterUserTask model, IUpdateModel updater)
         {
-            activity.SendConfirmationEmail = model.SendConfirmationEmail;
-
-            if (model.SendConfirmationEmail)
+            var viewModel = new RegisterUserTaskViewModel();
+            if (await updater.TryUpdateModelAsync(viewModel, Prefix))
             {
-                activity.ConfirmationEmailSubject = new WorkflowExpression<string>(model.ConfirmationEmailSubject);
-                activity.ConfirmationEmailTemplate = new WorkflowExpression<string>(model.ConfirmationEmailTemplate);
+                model.SendConfirmationEmail = viewModel.SendConfirmationEmail;
+                model.ConfirmationEmailSubject = new WorkflowExpression<string>(viewModel.ConfirmationEmailSubject);
+                model.ConfirmationEmailTemplate = new WorkflowExpression<string>(viewModel.ConfirmationEmailTemplate);
+
+                if (model.SendConfirmationEmail)
+                {
+                    updater.ModelState.AddModelError(nameof(viewModel.ConfirmationEmailSubject), S["A value is required for {0}.", nameof(viewModel.ConfirmationEmailSubject)]);
+                    updater.ModelState.AddModelError(nameof(viewModel.ConfirmationEmailTemplate), S["A value is required for {0}.", nameof(viewModel.ConfirmationEmailTemplate)]);
+                }
             }
+
+            return Edit(model);
         }
     }
 }
