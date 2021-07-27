e4362576e0d2c6c590e486fd9ee5383577369448
aaronamm <theeranitp@gmail.com>
2021-07-27 13:39:04 +0700

    Use IndexOf with StringComparison.OrdinalIgnoreCase when checking invalid Autoroute Path

--- src/OrchardCore.Modules/OrchardCore.Autoroute/Models/AutoroutePartExtensions.cs
+++ src/OrchardCore.Modules/OrchardCore.Autoroute/Models/AutoroutePartExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -14,16 +15,26 @@ namespace OrchardCore.Autoroute.Models
                 yield return new ValidationResult(S["Your permalink can't be set to the homepage, please use the homepage option instead."], new[] { nameof(autoroute.Path) });
             }
 
-            if (autoroute.Path?.IndexOfAny(AutoroutePart.InvalidCharactersForPath) > -1 || autoroute.Path?.IndexOf(' ') > -1 || autoroute.Path?.IndexOf("//") > -1)
+            if (HasInvalidCharacters(autoroute.Path))
             {
                 var invalidCharactersForMessage = string.Join(", ", AutoroutePart.InvalidCharactersForPath.Select(c => $"\"{c}\""));
-                yield return new ValidationResult(S["Please do not use any of the following characters in your permalink: {0}. No spaces, or consecutive slashes, are allowed (please use dashes or underscores instead).", invalidCharactersForMessage], new[] { nameof(autoroute.Path) });
+                const string errorMessage = "Please do not use any of the following characters in your permalink: {0}. No spaces, or consecutive slashes, are allowed (please use dashes or underscores instead).";
+                yield return new ValidationResult(S[errorMessage, invalidCharactersForMessage], new[] { nameof(autoroute.Path) });
             }
 
             if (autoroute.Path?.Length > AutoroutePart.MaxPathLength)
             {
                 yield return new ValidationResult(S["Your permalink is too long. The permalink can only be up to {0} characters.", AutoroutePart.MaxPathLength], new[] { nameof(autoroute.Path) });
             }
+        }
+
+        private static bool HasInvalidCharacters(string path)
+        {
+            if (path?.IndexOfAny(AutoroutePart.InvalidCharactersForPath) > -1) return true; // IndexOfAny performs culture-insensitive search.
+            if (path?.IndexOf(' ', StringComparison.Ordinal) > -1) return true;
+            if (path?.IndexOf("//", StringComparison.Ordinal) > -1) return true;
+
+            return false;
         }
     }
 }
