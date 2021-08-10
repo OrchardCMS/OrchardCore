1264938e55abe020fb8eff72a2dde8928b2b9120
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2021-08-11 01:08:19 +0200

    Add memory file builder for unit testing

---
+++ test/OrchardCore.Tests/Stubs/MemoryFileBuilder.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using OrchardCore.Deployment;
+
+namespace OrchardCore.Tests.Stubs
+{
+    /// <summary>
+    /// In memory file builder that uses a dictionary as virtual file system.
+    /// Intended for unit testing.
+    /// </summary>
+    public class MemoryFileBuilder
+        : IFileBuilder
+    {
+        public Dictionary<string, byte[]> VirtualFiles { get; private set; } = new();
+
+        public async Task SetFileAsync(string subpath, Stream stream)
+        {
+            using var ms = new MemoryStream();
+            await stream.CopyToAsync(ms);
+            VirtualFiles[subpath] = ms.ToArray();
+        }
+
+        /// <summary>
+        /// Read the contents of a file built with this file builder, using the specified encoding.
+        /// </summary>
+        /// <param name="subpath">Path and/or file name</param>
+        /// <param name="encoding">Encoding used to convert the byte array to string</param>
+        /// <returns></returns>
+        public string GetFileContents(string subpath, Encoding encoding)
+        {
+            return encoding.GetString(VirtualFiles[subpath]);
+        }
+    }
+}
