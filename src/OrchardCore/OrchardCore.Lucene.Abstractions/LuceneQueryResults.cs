162a62a10aa3f7829852148ff760e2b0ee052ea6
Jasmin Savard <jasmin.savard@gmail.com>
2021-10-04 22:07:54 -0400

    Refactor IQueryResult usage (#10312)

--- src/OrchardCore/OrchardCore.Lucene.Abstractions/LuceneQueryResults.cs
+++ src/OrchardCore/OrchardCore.Lucene.Abstractions/LuceneQueryResults.cs
@@ -1,10 +1,8 @@
-using System;
 using System.Collections.Generic;
 using OrchardCore.Queries;
 
-namespace OrchardCore.Queries
+namespace OrchardCore.Lucene
 {
-    [Obsolete("This class has been deprecated and we will be removed in the next major release, please use OrchardCore.Lucene.Abstractions instead.", false)]
     public class LuceneQueryResults : IQueryResults
     {
         public IEnumerable<object> Items { get; set; }
