7ee348987370b669bdb68fa6761cbd4f41aaf40a
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2021-10-08 22:08:30 +0300

    Remove resources depedency from OrchardCore.Email

--- src/OrchardCore.Modules/OrchardCore.Email/Manifest.cs
+++ src/OrchardCore.Modules/OrchardCore.Email/Manifest.cs
@@ -6,6 +6,5 @@ using OrchardCore.Modules.Manifest;
     Website = ManifestConstants.OrchardCoreWebsite,
     Version = ManifestConstants.OrchardCoreVersion,
     Description = "Provides email settings configuration and a default email service based on SMTP.",
-    Dependencies = new[] { "OrchardCore.Resources" },
     Category = "Messaging"
 )]
