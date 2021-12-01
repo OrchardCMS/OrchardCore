6434fd6df5f702d39aa1fbf84253521e79861251
Michal Kužela <michal.kuzela@puxdesign.cz>
2021-12-01 10:59:09 +0100

    fix MaxNumberOfResultsValidationRule

--- src/OrchardCore.Modules/OrchardCore.Apis.GraphQL/ValidationRules/MaxNumberOfResultsValidationRule.cs
+++ src/OrchardCore.Modules/OrchardCore.Apis.GraphQL/ValidationRules/MaxNumberOfResultsValidationRule.cs
@@ -31,7 +31,7 @@ namespace OrchardCore.Apis.GraphQL.ValidationRules
             {
                 if ((arg.Name == "first" || arg.Name == "last") && arg.Value != null)
                 {
-                    var context = (GraphQLContext)validationContext.UserContext;
+                    var context = (GraphQLUserContext)validationContext.UserContext;
 
                     int? value = null;
 
