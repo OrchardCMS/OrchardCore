8d1d8d3ba314917d9e2ad97ef5b814d089063867
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2021-12-10 01:40:59 +0100

    Back to C# 8

--- src/OrchardCore/OrchardCore.Media.Abstractions/Indexing/MediaFileIndexingOptions.cs
+++ src/OrchardCore/OrchardCore.Media.Abstractions/Indexing/MediaFileIndexingOptions.cs
@@ -6,7 +6,7 @@ namespace OrchardCore.Media.Indexing
 {
     public class MediaFileIndexingOptions
     {
-        private readonly Dictionary<string, Type> _mediaFileTextProviderRegistrations = new();
+        private readonly Dictionary<string, Type> _mediaFileTextProviderRegistrations = new Dictionary<string, Type>();
 
         public MediaFileIndexingOptions RegisterMediaFileTextProvider<TMediaFileTextProvider>(string fileExtension)
             where TMediaFileTextProvider : class, IMediaFileTextProvider
