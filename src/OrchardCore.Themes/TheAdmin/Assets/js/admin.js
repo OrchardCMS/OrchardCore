58c2d016023b3c212ca421818f2db72f76692f79
Jasmin Savard <jasmin.savard@gmail.com>
2021-12-15 02:03:07 -0500

    Add back tooltips

--- src/OrchardCore.Themes/TheAdmin/Assets/js/admin.js
+++ src/OrchardCore.Themes/TheAdmin/Assets/js/admin.js
@@ -178,6 +178,12 @@ $(function () {
         });
     });
 
+    // Tooltips
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
 });
 
 function getTechnicalName(name) {
--- src/OrchardCore.Themes/TheAdmin/wwwroot/js/TheAdmin.js
+++ src/OrchardCore.Themes/TheAdmin/wwwroot/js/TheAdmin.js
@@ -1936,7 +1936,7 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
     value: true
   });
 });
-function _get(target, property, receiver) { if (typeof Reflect !== "undefined" && Reflect.get) { _get = Reflect.get; } else { _get = function _get(target, property, receiver) { var base = _superPropBase(target, property); if (!base) return; var desc = Object.getOwnPropertyDescriptor(base, property); if (desc.get) { return desc.get.call(receiver); } return desc.value; }; } return _get(target, property, receiver || target); }
+function _get() { if (typeof Reflect !== "undefined" && Reflect.get) { _get = Reflect.get; } else { _get = function _get(target, property, receiver) { var base = _superPropBase(target, property); if (!base) return; var desc = Object.getOwnPropertyDescriptor(base, property); if (desc.get) { return desc.get.call(arguments.length < 3 ? target : receiver); } return desc.value; }; } return _get.apply(this, arguments); }
 
 function _superPropBase(object, property) { while (!Object.prototype.hasOwnProperty.call(object, property)) { object = _getPrototypeOf(object); if (object === null) break; } return object; }
 
@@ -7808,6 +7808,11 @@ $(function () {
 
       target.collapse($(this).prop('checked') ? 'hide' : 'show');
     });
+  }); // Tooltips
+
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
   });
 });
 
