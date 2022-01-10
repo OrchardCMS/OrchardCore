e43cff894858debbe40f9e8916927e55233f6b70
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2022-01-10 15:09:47 +0300

    EditTenantViewModel -> TenantViewModel

--- src/OrchardCore.Modules/OrchardCore.Tenants/Controllers/ControllerExtensions.cs
+++ src/OrchardCore.Modules/OrchardCore.Tenants/Controllers/ControllerExtensions.cs
@@ -24,12 +24,12 @@ namespace OrchardCore.Tenants.Controllers
 
             if (selectedProvider != null && selectedProvider.HasConnectionString && String.IsNullOrWhiteSpace(model.ConnectionString))
             {
-                controller.ModelState.AddModelError(nameof(EditTenantViewModel.ConnectionString), S["The connection string is mandatory for this provider."]);
+                controller.ModelState.AddModelError(nameof(TenantViewModel.ConnectionString), S["The connection string is mandatory for this provider."]);
             }
 
             if (String.IsNullOrWhiteSpace(model.Name))
             {
-                controller.ModelState.AddModelError(nameof(EditTenantViewModel.Name), S["The tenant name is mandatory."]);
+                controller.ModelState.AddModelError(nameof(TenantViewModel.Name), S["The tenant name is mandatory."]);
             }
 
             if (!String.IsNullOrWhiteSpace(model.FeatureProfile))
@@ -37,7 +37,7 @@ namespace OrchardCore.Tenants.Controllers
                 var featureProfiles = await featureProfilesService.GetFeatureProfilesAsync();
                 if (!featureProfiles.ContainsKey(model.FeatureProfile))
                 {
-                    controller.ModelState.AddModelError(nameof(EditTenantViewModel.FeatureProfile), S["The feature profile does not exist.", model.FeatureProfile]);
+                    controller.ModelState.AddModelError(nameof(TenantViewModel.FeatureProfile), S["The feature profile does not exist.", model.FeatureProfile]);
                 }
             }
 
@@ -45,30 +45,30 @@ namespace OrchardCore.Tenants.Controllers
 
             if (newTenant && allSettings.Any(tenant => String.Equals(tenant.Name, model.Name, StringComparison.OrdinalIgnoreCase)))
             {
-                controller.ModelState.AddModelError(nameof(EditTenantViewModel.Name), S["A tenant with the same name already exists.", model.Name]);
+                controller.ModelState.AddModelError(nameof(TenantViewModel.Name), S["A tenant with the same name already exists.", model.Name]);
             }
 
             if (!String.IsNullOrEmpty(model.Name) && !Regex.IsMatch(model.Name, @"^\w+$"))
             {
-                controller.ModelState.AddModelError(nameof(EditTenantViewModel.Name), S["Invalid tenant name. Must contain characters only and no spaces."]);
+                controller.ModelState.AddModelError(nameof(TenantViewModel.Name), S["Invalid tenant name. Must contain characters only and no spaces."]);
             }
 
             if (!IsDefaultShell(controller) && String.IsNullOrWhiteSpace(model.RequestUrlHost) && String.IsNullOrWhiteSpace(model.RequestUrlPrefix))
             {
-                controller.ModelState.AddModelError(nameof(EditTenantViewModel.RequestUrlPrefix), S["Host and url prefix can not be empty at the same time."]);
+                controller.ModelState.AddModelError(nameof(TenantViewModel.RequestUrlPrefix), S["Host and url prefix can not be empty at the same time."]);
             }
 
             var allOtherShells = allSettings.Where(t => !String.Equals(t.Name, model.Name, StringComparison.OrdinalIgnoreCase));
             if (allOtherShells.Any(t => String.Equals(t.RequestUrlPrefix, model.RequestUrlPrefix?.Trim(), StringComparison.OrdinalIgnoreCase) && (t.RequestUrlHost?.Contains(model.RequestUrlHost, StringComparison.OrdinalIgnoreCase) ?? false)))
             {
-                controller.ModelState.AddModelError(nameof(EditTenantViewModel.RequestUrlPrefix), S["A tenant with the same host and prefix already exists.", model.Name]);
+                controller.ModelState.AddModelError(nameof(TenantViewModel.RequestUrlPrefix), S["A tenant with the same host and prefix already exists.", model.Name]);
             }
 
             if (!String.IsNullOrWhiteSpace(model.RequestUrlPrefix))
             {
                 if (model.RequestUrlPrefix.Contains('/'))
                 {
-                    controller.ModelState.AddModelError(nameof(EditTenantViewModel.RequestUrlPrefix), S["The url prefix can not contain more than one segment."]);
+                    controller.ModelState.AddModelError(nameof(TenantViewModel.RequestUrlPrefix), S["The url prefix can not contain more than one segment."]);
                 }
             }
         }
