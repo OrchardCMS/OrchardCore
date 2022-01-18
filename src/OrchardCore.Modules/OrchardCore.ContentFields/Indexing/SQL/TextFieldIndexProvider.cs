c3149e8e4a1d2fd2fa3a9d8daddc876171631049
Oceania2018 <haiping008@gmail.com>
2022-01-18 17:43:07 -0600

    Fix index flag doesn't work in SQL #11035

--- src/OrchardCore.Modules/OrchardCore.ContentFields/Indexing/SQL/TextFieldIndexProvider.cs
+++ src/OrchardCore.Modules/OrchardCore.ContentFields/Indexing/SQL/TextFieldIndexProvider.cs
@@ -6,6 +6,7 @@ using Newtonsoft.Json.Linq;
 using OrchardCore.ContentFields.Fields;
 using OrchardCore.ContentManagement;
 using OrchardCore.ContentManagement.Metadata;
+using OrchardCore.Indexing;
 using YesSql.Indexes;
 
 namespace OrchardCore.ContentFields.Indexing.SQL
@@ -90,6 +91,10 @@ namespace OrchardCore.ContentFields.Indexing.SQL
                         {
                             continue;
                         }
+
+                        var contentIndexSettings = fieldDefinition.GetSettings<ContentIndexSettings>();
+                        if (!contentIndexSettings.Included)
+                            continue;
 
                         var field = jField.ToObject<TextField>();
 
