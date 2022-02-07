77e2768391a89c935b02464f426c1efd48b3511c
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2022-02-08 01:32:44 +0300

    Add docs for ThemeSelectorResult

--- src/OrchardCore/OrchardCore.DisplayManagement/Theming/ThemeSelectorResult.cs
+++ src/OrchardCore/OrchardCore.DisplayManagement/Theming/ThemeSelectorResult.cs
@@ -1,8 +1,19 @@
 namespace OrchardCore.DisplayManagement.Theming
 {
+    /// <summary>
+    /// Represents a result for a selected theme.
+    /// </summary>
     public class ThemeSelectorResult
     {
+        /// <summary>
+        /// Gets or sets the priority for the selected theme.
+        /// </summary>
         public int Priority { get; set; }
+
+        /// <summary>
+        /// Gets or sets the theme name.
+        /// </summary>
+        /// <remarks>Avoid to use the theme name acciedently. <see cref="ThemeSelectorResult"/> is using the theme identifier instead.</remarks>
         public string ThemeName { get; set; }
     }
 }
