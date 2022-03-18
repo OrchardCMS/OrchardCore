18a32dbf233042911d5663b4a55561641cf18e08
Antoine Griffard <agriffard@hotmail.com>
2022-03-18 18:10:06 +0100

    @popperjs/core 2.11.4

--- src/OrchardCore.Themes/TheAdmin/wwwroot/js/TheAdmin.js
+++ src/OrchardCore.Themes/TheAdmin/wwwroot/js/TheAdmin.js
@@ -6,7 +6,7 @@
 function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
 
 /**
- * @popperjs/core v2.11.3 - MIT License
+ * @popperjs/core v2.11.4 - MIT License
  */
 (function (global, factory) {
   (typeof exports === "undefined" ? "undefined" : _typeof(exports)) === 'object' && typeof module !== 'undefined' ? factory(exports) : typeof define === 'function' && define.amd ? define(['exports'], factory) : (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.Popper = {}));
