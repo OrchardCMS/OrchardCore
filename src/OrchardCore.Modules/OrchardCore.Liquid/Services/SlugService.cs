5c9eeaed4f04af2ce2a9d125151ea884ad448e3d
Jasmin Savard <jasmin.savard@gmail.com>
2022-04-09 00:54:39 -0400

    Mark class as obsolete

---
+++ src/OrchardCore.Modules/OrchardCore.Liquid/Services/SlugService.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Globalization;
+using System.Text;
+using Cysharp.Text;
+using OrchardCore.Autoroute.Abstractions.Services;
+
+namespace OrchardCore.Liquid.Services
+{
+    [Obsolete("This class has been deprecated and we will be removed in the next major release, please use OrchardCore.Autoroute.Core.Services instead.", false)]
+    public class SlugService : ISlugService
+    {
+        private const char Hyphen = '-';
+        private const int MaxLength = 1000;
+
+        public string Slugify(string text)
+        {
+            if (String.IsNullOrEmpty(text))
+            {
+                return text;
+            }
+
+            var appendHyphen = false;
+            var normalizedText = text.Normalize(NormalizationForm.FormKD);
+
+            using var slug = ZString.CreateStringBuilder();
+
+            for (var i = 0; i < normalizedText.Length; i++)
+            {
+                var currentChar = Char.ToLowerInvariant(normalizedText[i]);
+
+                if (CharUnicodeInfo.GetUnicodeCategory(currentChar) == UnicodeCategory.NonSpacingMark)
+                {
+                    continue;
+                }
+
+                if (Char.IsLetterOrDigit(currentChar))
+                {
+                    slug.Append(currentChar);
+
+                    appendHyphen = true;
+                }
+                else if (currentChar is Hyphen)
+                {
+                    if (appendHyphen && i != normalizedText.Length - 1)
+                    {
+                        slug.Append(currentChar);
+                        appendHyphen = false;
+                    }
+                }
+                else if (currentChar == '_' || currentChar == '~')
+                {
+                    slug.Append(currentChar);
+                }
+                else
+                {
+                    if (appendHyphen)
+                    {
+                        slug.Append(Hyphen);
+
+                        appendHyphen = false;
+                    }
+                }
+            }
+
+            return new string(slug.AsSpan()[..Math.Min(slug.Length, MaxLength)]).Normalize(NormalizationForm.FormC);
+        }
+    }
+}
