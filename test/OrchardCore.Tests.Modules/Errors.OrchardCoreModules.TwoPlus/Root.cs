9af60180f99efe5e7c2ddc42fae90647f957df7f
Michael W Powell <mwpowelllde@gmail.com>
2022-04-14 13:59:57 -0400

    Module and feature constructors (#11514)

---
+++ test/OrchardCore.Tests.Modules/Errors.OrchardCoreModules.TwoPlus/Root.cs
@@ -0,0 +1,12 @@
+// This project intentional left out of the build configuration, it SHOULD error, that is expected
+namespace Errors.OrchardCoreModules.TwoPlus
+{
+    /// <summary>
+    /// Hooks provided for purposes of identifying the class and assembly context.
+    /// </summary>
+    internal class Root
+    {
+    }
+}
+// TODO: MWP: would be better if perhaps we could verify a BUILD cycle from code
+// TODO: MWP: but this would also require a bit of xUnit scaffold to support
