f1b18f9f5fb276390b8b5a952e7d526e0e6f147d
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2022-04-14 04:29:50 +0300

    Split middleware and ApplicationExtension tests

--- test/OrchardCore.Tests/Modules/OrchardCore.Security/ReferrerPolicyMiddlewareTests.cs
+++ test/OrchardCore.Tests/Modules/OrchardCore.Security/ReferrerPolicyMiddlewareTests.cs
@@ -1,52 +1,27 @@
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Builder;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Security.Middlewares;
 using Xunit;
 
 namespace OrchardCore.Security.Tests
 {
     public class ReferrerPolicyMiddlewareTests
     {
-        public static IEnumerable<object[]> Policies =>
-            new List<object[]>
-            {
-                new object[] { ReferrerPolicy.NoReferrer },
-                new object[] { ReferrerPolicy.NoReferrerWhenDowngrade },
-                new object[] { ReferrerPolicy.Origin },
-                new object[] { ReferrerPolicy.OriginWhenCrossOrigin },
-                new object[] { ReferrerPolicy.SameOrigin },
-                new object[] { ReferrerPolicy.StrictOrigin },
-                new object[] { ReferrerPolicy.StrictOriginWhenCrossOrigin },
-                new object[] { ReferrerPolicy.UnsafeUrl }
-            };
-
-        [Theory]
-        [MemberData(nameof(Policies))]
-        public void UseReferrerPolicy_ShouldAddReferrerPolicyHeaderWithProperValue(ReferrerPolicy policy)
+        [Fact]
+        public async Task AddReferrerPolicyHeader()
         {
             // Arrange
+            var middleware = new ReferrerPolicyMiddleware(ReferrerPolicy.SameOrigin, request);
             var context = new DefaultHttpContext();
-            var applicationBuilder = CreateApplicationBuilder();
 
             // Act
-            applicationBuilder.UseReferrerPolicy(policy);
-
-            applicationBuilder
-                .Build()
-                .Invoke(context);
+            await middleware.Invoke(context);
 
             // Assert
             Assert.True(context.Response.Headers.ContainsKey(SecurityHeader.ReferrerPolicy));
-            Assert.Equal(policy, context.Response.Headers[SecurityHeader.ReferrerPolicy]);
-        }
+            Assert.Equal(ReferrerPolicy.SameOrigin, context.Response.Headers[SecurityHeader.ReferrerPolicy]);
 
-        private static IApplicationBuilder CreateApplicationBuilder()
-        {
-            var services = new ServiceCollection();
-            var serviceProvider = services.BuildServiceProvider();
-
-            return new ApplicationBuilder(serviceProvider);
+            static Task request(HttpContext context) => Task.CompletedTask;
         }
     }
 }
