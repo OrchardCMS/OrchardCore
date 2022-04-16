126424f42322a5f7614882a7c6cf8e53a2ac3591
Jean-Thierry Kéchichian <jean-thierry.kechichian@wanadoo.fr>
2022-04-16 05:07:29 +0200

    Revert "Non awaited task in GraphQL DataLoaderExtensions (#11536)" (#11548)
    This reverts commit 5ba889e56d9e6e8ff817ae24f48d0135094143cf.

--- src/OrchardCore/OrchardCore.Apis.GraphQL.Abstractions/Extensions/DataLoaderExtensions.cs
+++ src/OrchardCore/OrchardCore.Apis.GraphQL.Abstractions/Extensions/DataLoaderExtensions.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using GraphQL.DataLoader;
 
@@ -18,15 +19,9 @@ namespace OrchardCore.Apis.GraphQL
             return Task.WhenAll(tasks);
         }
 
-        public static async Task<T[]> LoadAsync<TKey, T>(this IDataLoader<TKey, T> dataLoader, params TKey[] keys)
+        public static Task<T[]> LoadAsync<TKey, T>(this IDataLoader<TKey, T> dataLoader, params TKey[] keys)
         {
-            var tasks = new T[keys.Length];
-            for (var i = 0; i < keys.Length; i++)
-            {
-                tasks[i] = await dataLoader.LoadAsync(keys[i]);
-            }
-
-            return tasks;
+            return dataLoader.LoadAsync(keys.AsEnumerable());
         }
     }
 }
