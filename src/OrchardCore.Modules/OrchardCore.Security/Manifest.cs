ddf54acf57f3acae4c66c613927aabaa755b4785
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2022-04-21 16:51:47 +0300

    Remove IsAlwaysEnabled

--- src/OrchardCore.Modules/OrchardCore.Security/Manifest.cs
+++ src/OrchardCore.Modules/OrchardCore.Security/Manifest.cs
@@ -6,6 +6,5 @@ using OrchardCore.Modules.Manifest;
     Website = ManifestConstants.OrchardCoreWebsite,
     Version = ManifestConstants.OrchardCoreVersion,
     Description = "The Security module adds HTTP headers to follow security best practices.",
-    Category = "Security",
-    IsAlwaysEnabled = true
+    Category = "Security"
 )]
