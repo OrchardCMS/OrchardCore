99659d5948991c39d2a4018fcd8c4784ad4a4059
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2022-05-11 12:47:28 +0300

    Override FrameOptions by CSP if FrameAncestors exists

--- src/OrchardCore.Modules/OrchardCore.Security/Services/FrameOptionsHeaderPolicyProvider.cs
+++ src/OrchardCore.Modules/OrchardCore.Security/Services/FrameOptionsHeaderPolicyProvider.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Http;
+using OrchardCore.Security.Options;
 
 namespace OrchardCore.Security.Services
 {
@@ -6,7 +7,21 @@ namespace OrchardCore.Security.Services
     {
         public override void ApplyPolicy(HttpContext httpContext)
         {
-            httpContext.Response.Headers[SecurityHeaderNames.XFrameOptions] = Options.FrameOptions;
+            var overrideHeaderByCSP = false;
+            if (httpContext.Response.Headers.ContainsKey(SecurityHeaderNames.ContentSecurityPolicy))
+            {
+                var contentSecurityHeaderValue = httpContext.Response.Headers[SecurityHeaderNames.ContentSecurityPolicy].ToString();
+                // Override FramOptions when FrameAncestors exists
+                if (contentSecurityHeaderValue.Contains(ContentSecurityPolicyValue.FrameAncestors, System.StringComparison.CurrentCulture))
+                {
+                    overrideHeaderByCSP = true;
+                }
+            }
+
+            if (!overrideHeaderByCSP)
+            {
+                httpContext.Response.Headers[SecurityHeaderNames.XFrameOptions] = Options.FrameOptions;
+            }
         }
     }
 }
---
+++ test/OrchardCore.Tests/Modules/OrchardCore.Security/FrameOptionsHeaderPolicyProviderTests.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Http;
+using OrchardCore.Security.Options;
+using OrchardCore.Security.Services;
+using Xunit;
+
+namespace OrchardCore.Security.Tests
+{
+    public class FrameOptionsHeaderPolicyProviderTests
+    {
+        [Fact]
+        public void AddFrameOptionsHeader()
+        {
+            // Arrange
+            var provider = new FrameOptionsHeaderPolicyProvider { Options = new SecurityHeadersOptions()};
+            var context = new DefaultHttpContext();
+
+            // Act
+            provider.ApplyPolicy(context);
+
+            // Assert
+            Assert.True(context.Response.Headers.ContainsKey(SecurityHeaderNames.XFrameOptions));
+        }
+
+        [Fact]
+        public void OverrideFrameOptionsHeaderWhenContentSecurityPolicyHeaderExists()
+        {
+            // Arrange
+            var provider = new FrameOptionsHeaderPolicyProvider();
+            var context = new DefaultHttpContext();
+            context.Response.Headers[SecurityHeaderNames.ContentSecurityPolicy] = $"{ContentSecurityPolicyValue.FrameAncestors} {ContentSecurityPolicyOriginValue.Self}";
+
+            // Act
+            provider.ApplyPolicy(context);
+
+            // Assert
+            Assert.False(context.Response.Headers.ContainsKey(SecurityHeaderNames.XFrameOptions));
+        }
+    }
+}
