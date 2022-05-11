9822a435d111091afc0ca8c2df49befb2725a18a
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2022-05-11 18:07:34 +0300

    Include all permissons policy directives

--- src/OrchardCore.Modules/OrchardCore.Security/Options/PermissionsPolicyValue.cs
+++ src/OrchardCore.Modules/OrchardCore.Security/Options/PermissionsPolicyValue.cs
@@ -8,15 +8,27 @@ namespace OrchardCore.Security.Options
 
         public const string Autoplay = "autoplay";
 
+        public const string Battery = "battery";
+
         public const string Camera = "camera";
+
+        public const string DisplayCapture = "display-capture";
+
+        public const string DocumentDomain = "document-domain";
 
         public const string EncryptedMedia = "encrypted-media";
 
         public const string FullScreen = "fullscreen";
 
+        public const string GamePad = "gamepad";
+
         public const string Geolocation = "geolocation";
 
         public const string Gyroscope = "gyroscope";
+
+        public const string LayoutAnimations = "layout-animations";
+
+        public const string LegacyImageFormat = "legacy-image-format";
 
         public const string Magnetometer = "magnetometer";
 
@@ -24,22 +36,30 @@ namespace OrchardCore.Security.Options
 
         public const string Midi = "midi";
 
-        public const string Notifications = "notifications";
+        public const string OversizedImages = "oversized-images";
 
         public const string Payment = "payment";
 
         public const string PictureInPicture = "picture-in-picture";
 
+        public const string PublicKeyRetrieval = "publickey-credentials-get";
+
         public const string Push = "push";
 
-        public const string Speaker = "speaker";
+        public const string SpeakerSelection = "speaker-selection";
+
+        public const string ScreenWakeLock = "screen-wake-lock";
 
         public const string SyncXhr = "sync-xhr";
 
+        public const string UnoptimizedImages = "unoptimized-images";
+
+        public const string UnsizedMedia = "unsized-media";
+
         public const string Usb = "usb";
 
-        public const string Vibrate = "vibrate";
+        public const string WebShare = "web-share";
 
-        public const string VR = "vr";
+        public const string WebXR = "xr-spatial-tracking";
     }
 }
