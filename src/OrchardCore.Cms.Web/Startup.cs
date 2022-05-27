43811a70093f70a34006f5241de7a10f6a24b7bb
Zoltán Máté <zoltan.mate@lombiq.com>
2022-05-27 17:42:06 +0200

    Lowering MaxFileSize for testing

--- src/OrchardCore.Cms.Web/Startup.cs
+++ src/OrchardCore.Cms.Web/Startup.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OrchardCore.Media;
 
 namespace OrchardCore.Cms.Web
 {
@@ -9,6 +11,9 @@ namespace OrchardCore.Cms.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddOrchardCms().AddSetupFeatures("OrchardCore.AutoSetup");
+            services.Configure<IISServerOptions>(options => options.MaxRequestBodySize = 1000000);
+            services.Configure<KestrelServerOptions>(options => options.Limits.MaxRequestBodySize = 1000000);
+            services.PostConfigure<MediaOptions>(options => options.MaxFileSize = 1000000);
         }
 
         public void Configure(IApplicationBuilder app, IHostEnvironment env)
