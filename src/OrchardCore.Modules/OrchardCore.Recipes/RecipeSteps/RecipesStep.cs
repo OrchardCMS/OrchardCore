6c85ddd21685b49f3ca1741cd1716bdb30c108e6
BrunoToti <bruno@altibiz.com>
2022-06-01 13:21:49 +0200

    Removed instanceId which is not used for anything important

--- src/OrchardCore.Modules/OrchardCore.Recipes/RecipeSteps/RecipesStep.cs
+++ src/OrchardCore.Modules/OrchardCore.Recipes/RecipeSteps/RecipesStep.cs
@@ -29,23 +29,16 @@ namespace OrchardCore.Recipes.RecipeSteps
             var step = context.Step.ToObject<InternalStep>();
             var recipesDictionary = new Dictionary<string, IDictionary<string, RecipeDescriptor>>();
             IList<RecipeDescriptor> innerRecipes = new List<RecipeDescriptor>();
+            IDictionary<string, RecipeDescriptor> recipes;
+            var recipeCollections = await Task.WhenAll(_recipeHarvesters.Select(x => x.HarvestRecipesAsync()));
+            recipes = recipeCollections.SelectMany(x => x).ToDictionary(x => x.Name);
 
             foreach (var recipe in step.Values)
             {
-                IDictionary<string, RecipeDescriptor> recipes;
-
-                if (!recipesDictionary.TryGetValue(recipe.ExecutionId, out recipes))
-                {
-                    var recipeCollections = await Task.WhenAll(_recipeHarvesters.Select(x => x.HarvestRecipesAsync()));
-                    recipes = recipeCollections.SelectMany(x => x).ToDictionary(x => x.Name);
-                    recipesDictionary[recipe.ExecutionId] = recipes;
-                }
-
                 if (!recipes.ContainsKey(recipe.Name))
                 {
-                    throw new ArgumentException($"No recipe named '{recipe.Name}' was found in extension '{recipe.ExecutionId}'.");
+                    throw new ArgumentException($"No recipe named '{recipe.Name}' was found.");
                 }
-
                 innerRecipes.Add(recipes[recipe.Name]);
             }
 
@@ -59,7 +52,6 @@ namespace OrchardCore.Recipes.RecipeSteps
 
         private class InternalStepValue
         {
-            public string ExecutionId { get; set; }
             public string Name { get; set; }
         }
     }
