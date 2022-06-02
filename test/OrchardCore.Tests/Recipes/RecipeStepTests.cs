6355ae138cddc2a3a246883577d8503b48423f5d
BrunoToti <bruno@altibiz.com>
2022-06-02 23:00:56 +0200

    added test

---
+++ test/OrchardCore.Tests/Recipes/RecipeStepTests.cs
@@ -0,0 +1,40 @@
+using System.Threading.Tasks;
+using Moq;
+using Newtonsoft.Json.Linq;
+using OrchardCore.Recipes.Models;
+using OrchardCore.Recipes.RecipeSteps;
+using OrchardCore.Recipes.Services;
+using Xunit;
+
+namespace OrchardCore.Tests.Email
+{
+    public class RecipeStepTests
+    {
+        [Fact]
+        public async Task ReplyTo_ShouldHaveAuthors_IfNotSet()
+        {
+            var step = CreateRecipeStepExecutor();
+            var context = new RecipeExecutionContext()
+            {
+                Name = "Recipes",
+                Step = JObject.Parse(@"{""values"":[
+                                                    {""name"":""subrecipe"",""executionid"":""something""},
+                                                    {""name"":""subrecipe2""},
+                                                ]}")
+            };
+            await step.ExecuteAsync(context);
+            Assert.Collection(context.InnerRecipes,
+                item => Assert.Equal("subrecipe", item.Name),
+                item2 => Assert.Equal("subrecipe2", item2.Name)
+            );
+        }
+
+        private static RecipesStep CreateRecipeStepExecutor()
+        {
+            var options = new Mock<IRecipeHarvester>();
+            options.Setup(o => o.HarvestRecipesAsync()).ReturnsAsync(new[] { new RecipeDescriptor { Name = "subrecipe" }, new RecipeDescriptor { Name = "subrecipe2" } });
+            var smtp = new RecipesStep(new[] { options.Object });
+            return smtp;
+        }
+    }
+}
