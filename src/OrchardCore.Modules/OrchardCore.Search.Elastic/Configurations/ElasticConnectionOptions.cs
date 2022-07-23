f52151e520672a152cbb3ece82bf2fe44b71f33e
Jasmin Savard <jasmin.savard@gmail.com>
2022-07-23 12:21:47 -0400

    Cleanup/comment

--- src/OrchardCore.Modules/OrchardCore.Search.Elastic/Configurations/ElasticConnectionOptions.cs
+++ src/OrchardCore.Modules/OrchardCore.Search.Elastic/Configurations/ElasticConnectionOptions.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace OrchardCore.Search.Elastic.Configurations
 {
     public class ElasticConnectionOptions
@@ -29,7 +23,7 @@ namespace OrchardCore.Search.Elastic.Configurations
         public string CertificateFingerprint { get; set; }
 
         /// <summary>
-        /// 
+        /// Enables compatibility mode for Elastic Search 8.x
         /// </summary>
         public bool EnableApiVersioningHeader { get; set; } = false;
 
