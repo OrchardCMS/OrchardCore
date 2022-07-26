c3155018eeae1272da20b12867d7459107ec16d6
Jasmin Savard <jasmin.savard@gmail.com>
2022-07-26 16:38:04 -0400

    Comment

--- src/OrchardCore.Modules/OrchardCore.Indexing/ViewModels/ContentIndexSettingsViewModel.cs
+++ src/OrchardCore.Modules/OrchardCore.Indexing/ViewModels/ContentIndexSettingsViewModel.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc.ModelBinding;
-using OrchardCore.ContentManagement.Metadata.Models;
 
 namespace OrchardCore.Indexing.ViewModels
 {
@@ -7,6 +6,10 @@ namespace OrchardCore.Indexing.ViewModels
     {
         public ContentIndexSettings ContentIndexSettings { get; set; }
 
+        /// <summary>
+        /// TODO : Provide a way to find ContentElements that can be stored or not.
+        /// Should be based on StorableAttribute and also should only be available for indexing Text.
+        /// </summary>
         [BindNever]
         public bool IsStorable { get; set; } = true;
     }
