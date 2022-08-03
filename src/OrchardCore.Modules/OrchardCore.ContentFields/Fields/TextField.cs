5714b2e1a937ea08652fe4b92b29defed45e4915
Jasmin Savard <jasmin.savard@gmail.com>
2022-08-03 14:32:01 -0400

    cleanup

--- src/OrchardCore.Modules/OrchardCore.ContentFields/Fields/TextField.cs
+++ src/OrchardCore.Modules/OrchardCore.ContentFields/Fields/TextField.cs
@@ -3,7 +3,6 @@ using OrchardCore.Indexing;
 
 namespace OrchardCore.ContentFields.Fields
 {
-    [Storable(typeof(TextField))]
     public class TextField : ContentField
     {
         public string Text { get; set; }
