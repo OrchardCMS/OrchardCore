3466cc192b3c4493101de37d9ab0db615490d1f8
Jean-Thierry Kéchichian <jean-thierry.kechichian@wanadoo.fr>
2022-08-05 01:13:59 +0200

    Update src/OrchardCore/OrchardCore.Abstractions/Shell/Removing/IShellRemovingManager.cs
    Co-authored-by: Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>

--- src/OrchardCore/OrchardCore.Abstractions/Shell/Removing/IShellRemovingManager.cs
+++ src/OrchardCore/OrchardCore.Abstractions/Shell/Removing/IShellRemovingManager.cs
@@ -10,7 +10,7 @@ public interface IShellRemovingManager
     /// <summary>
     /// Removes the provided tenant.
     /// </summary>
-    /// <param name="shellSettings">The tenant <see cref="ShellSettings"/>.</param>
+    /// <param name="shellSettings">The tenant's <see cref="ShellSettings"/>.</param>
     /// <param name="localResourcesOnly">
     /// Indicates that only local (not shared among server nodes in case of a multi-node hosting evironment) resources should be removed.
     /// Used to sync locally a tenant that was removed by another instance.
