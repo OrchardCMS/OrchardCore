b9648b6cb96b9004e1f53da5e777bff242949d12
Jasmin Savard <jasmin.savard@gmail.com>
2022-08-05 13:52:14 -0400

    Comments

--- src/OrchardCore/OrchardCore.Search.Elasticsearch.Abstractions/IElasticsearchSearchQueryService.cs
+++ src/OrchardCore/OrchardCore.Search.Elasticsearch.Abstractions/IElasticsearchSearchQueryService.cs
@@ -14,7 +14,7 @@ namespace OrchardCore.Search.Elasticsearch
         /// <param name="sort"></param>
         /// <param name="start"></param>
         /// <param name="end"></param>
-        /// <returns><see cref="IList{String}"/></returns>
+        /// <returns><see cref="IList{String}">IList&lt;string&gt;</see></returns>
         Task<IList<string>> ExecuteQueryAsync(string indexName, QueryContainer query, List<ISort> sort, int start = 0, int end = 20);
     }
 }
