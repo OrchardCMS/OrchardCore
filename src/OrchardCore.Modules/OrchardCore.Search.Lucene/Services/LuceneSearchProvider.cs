e4a92fef1ed04a82cf0ac697322f0afbead1e598
Jasmin Savard <jasmin.savard@gmail.com>
2022-08-06 17:56:33 -0400

    Comments

--- src/OrchardCore.Modules/OrchardCore.Search.Lucene/Services/LuceneSearchProvider.cs
+++ src/OrchardCore.Modules/OrchardCore.Search.Lucene/Services/LuceneSearchProvider.cs
@@ -2,6 +2,9 @@ using OrchardCore.Search.Abstractions;
 
 namespace OrchardCore.Search.Lucene.Services
 {
+    /// <summary>
+    /// Provides a way to determine that a Lucene SearchProvider is available to the current tenant.
+    /// </summary>
     internal class LuceneSearchProvider : SearchProvider
     {
         public LuceneSearchProvider() : base("Lucene")
--- src/OrchardCore/OrchardCore.Search.Elasticsearch.Core/Providers/ElasticSearchProvider.cs
+++ src/OrchardCore/OrchardCore.Search.Elasticsearch.Core/Providers/ElasticSearchProvider.cs
@@ -2,6 +2,9 @@ using OrchardCore.Search.Abstractions;
 
 namespace OrchardCore.Search.Elasticsearch.Core.Providers
 {
+    /// <summary>
+    /// Provides a way to determine that an Elasticsearch SearchProvider is available to the current tenant.
+    /// </summary>
     internal class ElasticSearchProvider : SearchProvider
     {
         public ElasticSearchProvider() : base("Elasticsearch") { }
