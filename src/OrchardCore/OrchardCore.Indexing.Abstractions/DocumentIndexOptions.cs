cae7ce2c0f34f9973549077039849ddfb339c896
Jasmin Savard <jasmin.savard@gmail.com>
2022-08-11 20:55:13 -0400

    Comments

--- src/OrchardCore/OrchardCore.Indexing.Abstractions/DocumentIndexOptions.cs
+++ src/OrchardCore/OrchardCore.Indexing.Abstractions/DocumentIndexOptions.cs
@@ -3,10 +3,11 @@ using System;
 namespace OrchardCore.Indexing
 {
     /// <summary>
-    /// Store means that the value will be stored in the index with it's original value.
+    /// Store means that the value will be stored in the index.
     /// Keyword means that the value will be indexed and tokenized as a single term. See Lucene StringField.
-    /// We should use keyword and store options for technical value. This way it will be using a StringField but stored.
-    /// Stored and Keyword are not the same. Stored is an override to say that we want to store the original value in the index.
+    /// We should use keyword and store options together for technical values. This way it will be using a StringField but also be stored.
+    /// Stored and Keyword are not the same.
+    /// If you absolutely need to only store the original value of a field then you should use a Lucene StoredField.
     /// See Elasticsearch documentation : <see href="https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping-store.html#mapping-store"/>
     /// </summary>
     [Flags]
