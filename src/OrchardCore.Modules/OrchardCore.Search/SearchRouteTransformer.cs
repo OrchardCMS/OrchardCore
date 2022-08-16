b373fcfb5857f3af7f58d376e381f40d553a8698
Jasmin Savard <jasmin.savard@gmail.com>
2022-08-15 22:18:15 -0400

    Use current SearchProvider if only one is enabled

--- src/OrchardCore.Modules/OrchardCore.Search/SearchRouteTransformer.cs
+++ src/OrchardCore.Modules/OrchardCore.Search/SearchRouteTransformer.cs
@@ -1,8 +1,12 @@
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.Entities;
+using OrchardCore.Search.Abstractions;
 using OrchardCore.Search.Model;
 using OrchardCore.Settings;
 
@@ -11,18 +15,42 @@ namespace OrchardCore.Search.Routing
     public class SearchRouteTransformer : DynamicRouteValueTransformer
     {
         private readonly ISiteService _siteService;
+        private readonly IServiceProvider _serviceProvider;
 
-        public SearchRouteTransformer(ISiteService siteService)
+        public SearchRouteTransformer(
+            ISiteService siteService,
+            IServiceProvider serviceProvider)
         {
             _siteService = siteService;
+            _serviceProvider = serviceProvider;
         }
 
         public override async ValueTask<RouteValueDictionary> TransformAsync(HttpContext httpContext, RouteValueDictionary values)
         {
-            var site = await _siteService.GetSiteSettingsAsync();
-            var settings = site.As<SearchSettings>();
+            var searchProviders = _serviceProvider.GetServices<SearchProvider>();
+            RouteValueDictionary routeValue;
 
-            var routeValue = new RouteValueDictionary( new { Name = "Search", Area = settings.SearchProviderAreaName, Action = "Search", Controller = "Search" });
+            if (searchProviders.Count() == 1)
+            {
+                routeValue = new RouteValueDictionary(new {
+                    Name = "Search",
+                    Area = searchProviders.FirstOrDefault().AreaName,
+                    Action = "Search",
+                    Controller = "Search" }
+                );
+            }
+            else
+            {
+                var site = await _siteService.GetSiteSettingsAsync();
+                var settings = site.As<SearchSettings>();
+
+                routeValue = new RouteValueDictionary(new {
+                    Name = "Search",
+                    Area = settings.SearchProviderAreaName,
+                    Action = "Search",
+                    Controller = "Search" }
+                );
+            }
 
             if (routeValue.Count > 0)
             {
