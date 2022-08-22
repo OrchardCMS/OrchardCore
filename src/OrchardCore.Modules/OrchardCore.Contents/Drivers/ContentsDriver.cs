2d2fdf14fbf0b3384d9b76d4baaa1486379bd647
Mike Alhayek <mike@crestapps.com>
2022-08-22 08:18:39 -0700

    Fix a Typo

--- src/OrchardCore.Modules/OrchardCore.Contents/Drivers/ContentsDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.Contents/Drivers/ContentsDriver.cs
@@ -46,7 +46,7 @@ namespace OrchardCore.Contents.Drivers
                 {
                     var hasStereotype = contentTypeDefinition.TryGetStereotype(out var stereotype);
 
-                    if (hasSteretype && !String.Equals("Content", stereotype, StringComparison.OrdinalIgnoreCase))
+                    if (hasStereotype && !String.Equals("Content", stereotype, StringComparison.OrdinalIgnoreCase))
                     {
                         ctx.Shape.Metadata.Alternates.Add($"{stereotype}__ContentsMetadata");
                     }
@@ -57,12 +57,11 @@ namespace OrchardCore.Contents.Drivers
                     {
                         ctx.Shape.Metadata.Alternates.Add($"ContentsMetadata_{ctx.Shape.Metadata.DisplayType}");
 
-                        if (hasSteretype && !String.Equals("Content", stereotype, StringComparison.OrdinalIgnoreCase))
+                        if (hasStereotype && !String.Equals("Content", stereotype, StringComparison.OrdinalIgnoreCase))
                         {
                             ctx.Shape.Metadata.Alternates.Add($"{stereotype}_{displayType}__ContentsMetadata");
                         }
                     }
-
                 });
 
                 results.Add(contentsMetadataShape);
