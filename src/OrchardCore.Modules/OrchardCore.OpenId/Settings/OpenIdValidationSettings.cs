3fe19b3014472b0730c7f3d41c1ff4a684f005cf
arra@cloudconstruct.com <arra@cloudconstruct.com>
2022-08-25 07:28:49 -0400

    Adding code for ability to set metadata address in openid validation options. Helpful for Azure B2C scenarios.

--- src/OrchardCore.Modules/OrchardCore.OpenId/Settings/OpenIdValidationSettings.cs
+++ src/OrchardCore.Modules/OrchardCore.OpenId/Settings/OpenIdValidationSettings.cs
@@ -8,5 +8,8 @@ namespace OrchardCore.OpenId.Settings
         public Uri Authority { get; set; }
         public bool DisableTokenTypeValidation { get; set; }
         public string Tenant { get; set; }
+#nullable enable
+        public Uri? MetadataAddress { get; set; }
+#nullable disable
     }
 }
