a90b36980b52ff1755d73fe69980b6bbac8b8c10
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2022-08-26 19:54:30 +0300

    Use QueryIndex() for better performance in OC.PublishLater

--- src/OrchardCore.Modules/OrchardCore.PublishLater/Migrations.cs
+++ src/OrchardCore.Modules/OrchardCore.PublishLater/Migrations.cs
@@ -1,59 +1,84 @@
 using System;
 using OrchardCore.ContentManagement.Metadata;
 using OrchardCore.ContentManagement.Metadata.Settings;
+using OrchardCore.ContentManagement.Records;
 using OrchardCore.Data.Migration;
 using OrchardCore.PublishLater.Indexes;
 using OrchardCore.PublishLater.Models;
 using YesSql.Sql;
 
-namespace OrchardCore.PublishLater
+namespace OrchardCore.PublishLater;
+
+public class Migrations : DataMigration
 {
-    public class Migrations : DataMigration
+    private readonly IContentDefinitionManager _contentDefinitionManager;
+
+    public Migrations(IContentDefinitionManager contentDefinitionManager)
     {
-        private readonly IContentDefinitionManager _contentDefinitionManager;
+        _contentDefinitionManager = contentDefinitionManager;
+    }
 
-        public Migrations(IContentDefinitionManager contentDefinitionManager)
+    public int Create()
+    {
+        _contentDefinitionManager.AlterPartDefinition(nameof(PublishLaterPart), builder => builder
+            .Attachable()
+            .WithDescription("Adds the ability to schedule content items to be published at a given future date and time."));
+
+        SchemaBuilder.CreateMapIndexTable<PublishLaterPartIndex>(table => table
+            .Column<DateTime>(nameof(PublishLaterPartIndex.ScheduledPublishDateTimeUtc))
+        );
+
+        SchemaBuilder.AlterIndexTable<PublishLaterPartIndex>(table => table
+            .CreateIndex($"IDX_{nameof(PublishLaterPartIndex)}_{nameof(PublishLaterPartIndex.ScheduledPublishDateTimeUtc)}",
+                "DocumentId",
+                nameof(PublishLaterPartIndex.ScheduledPublishDateTimeUtc))
+        );
+
+        // Shortcut other migration steps on new content definition schemas.
+        return 2;
+    }
+
+    // This code can be removed in a later version.
+    public int UpdateFrom1()
+    {
+        // The 'ScheduledPublishUtc' column and related index are kept on existing databases,
+        // this because dropping an index and altering a column don't work on all providers.
+
+        SchemaBuilder.AlterIndexTable<PublishLaterPartIndex>(table => table
+            .AddColumn<DateTime>(nameof(PublishLaterPartIndex.ScheduledPublishDateTimeUtc))
+        );
+
+        SchemaBuilder.AlterIndexTable<PublishLaterPartIndex>(table => table
+            .CreateIndex($"IDX_{nameof(PublishLaterPartIndex)}_{nameof(PublishLaterPartIndex.ScheduledPublishDateTimeUtc)}",
+                "DocumentId",
+                nameof(PublishLaterPartIndex.ScheduledPublishDateTimeUtc))
+        );
+
+        return 2;
+    }
+
+    public int UpdateFrom2()
+    {
+        SchemaBuilder.CreateMapIndexTable<PublishLaterPartIndex>(table => table
+            .Column<string>(nameof(PublishLaterPartIndex.ContentItemId))
+            .Column<bool>(nameof(PublishLaterPartIndex.Latest))
+            .Column<bool>(nameof(PublishLaterPartIndex.Published))
+            .Column<DateTime>(nameof(PublishLaterPartIndex.ScheduledPublishDateTimeUtc))
+        );
+
+        SchemaBuilder.AlterIndexTable<PublishLaterPartIndex>(table =>
         {
-            _contentDefinitionManager = contentDefinitionManager;
-        }
+            table.DropIndex($"IDX_{nameof(PublishLaterPartIndex)}_{nameof(PublishLaterPartIndex.ScheduledPublishDateTimeUtc)}");
 
-        public int Create()
-        {
-            _contentDefinitionManager.AlterPartDefinition(nameof(PublishLaterPart), builder => builder
-                .Attachable()
-                .WithDescription("Adds the ability to schedule content items to be published at a given future date and time."));
+            table.CreateIndex($"IDX_{nameof(PublishLaterPartIndex)}_{nameof(ContentItemIndex.DocumentId)}",
+                "Id",
+                nameof(ContentItemIndex.DocumentId),
+                nameof(PublishLaterPartIndex.ContentItemId),
+                nameof(PublishLaterPartIndex.Latest),
+                nameof(PublishLaterPartIndex.Published),
+                nameof(PublishLaterPartIndex.ScheduledPublishDateTimeUtc));
+        });
 
-            SchemaBuilder.CreateMapIndexTable<PublishLaterPartIndex>(table => table
-                .Column<DateTime>(nameof(PublishLaterPartIndex.ScheduledPublishDateTimeUtc))
-            );
-
-            SchemaBuilder.AlterIndexTable<PublishLaterPartIndex>(table => table
-                .CreateIndex($"IDX_{nameof(PublishLaterPartIndex)}_{nameof(PublishLaterPartIndex.ScheduledPublishDateTimeUtc)}",
-                    "DocumentId",
-                    nameof(PublishLaterPartIndex.ScheduledPublishDateTimeUtc))
-            );
-
-            // Shortcut other migration steps on new content definition schemas.
-            return 2;
-        }
-
-        // This code can be removed in a later version.
-        public int UpdateFrom1()
-        {
-            // The 'ScheduledPublishUtc' column and related index are kept on existing databases,
-            // this because dropping an index and altering a column don't work on all providers.
-
-            SchemaBuilder.AlterIndexTable<PublishLaterPartIndex>(table => table
-                .AddColumn<DateTime>(nameof(PublishLaterPartIndex.ScheduledPublishDateTimeUtc))
-            );
-
-            SchemaBuilder.AlterIndexTable<PublishLaterPartIndex>(table => table
-                .CreateIndex($"IDX_{nameof(PublishLaterPartIndex)}_{nameof(PublishLaterPartIndex.ScheduledPublishDateTimeUtc)}",
-                    "DocumentId",
-                    nameof(PublishLaterPartIndex.ScheduledPublishDateTimeUtc))
-            );
-
-            return 2;
-        }
+        return 3;
     }
 }
