001d717938ef4054056f3cfbc71cb2ec7aa8a476
Mike Alhayek <mike@crestapps.com>
2022-09-05 18:58:54 -0700

    Update DbConnectionValidatorResult comments

--- src/OrchardCore/OrchardCore.Data.YesSql.Abstractions/DbConnectionValidatorResult.cs
+++ src/OrchardCore/OrchardCore.Data.YesSql.Abstractions/DbConnectionValidatorResult.cs
@@ -1,25 +1,21 @@
 namespace OrchardCore.Data;
 
+/// <summary>
+/// 'Unknown' indicates that the connection string status is unknown or was not validated.
+/// 'NoProvider' indicated that the database provider is missing.
+/// 'DocumentTableNotFound' indicates that the connection string was valid, while the Document table does not exists.
+/// 'DocumentFound' indicates that the connection string was valid, while the Document table exists.
+/// 'ShellDescriptorDocumentFound' indicates that the connection string was valid and the document table exists with at least one shell-descriptor document.
+/// 'InvalidConnection' unable to open a connection to the given connection string.
+/// 'UnsupportedProvider' indicates invalid or unsupported database provider.
+/// </summary>
 public enum DbConnectionValidatorResult
 {
-    // 'Unknown' indicates that the connection string status is unknown or was not yet validated.
     Unknown,
-
-    // 'NoProvider' indicated that the provider is missing.
     NoProvider,
-
-    // 'DocumentTableNotFound' indicates that the connection string was valid, yet the Document table does not exist.
     DocumentTableNotFound,
-
-    // 'DocumentFound' indicates that the connection string was valid, yet the Document table exist.
     DocumentTableFound,
-
-    // 'ShellDescriptorDocumentFound' indicates that the connection string was valid and the document table exists with at least one entry of shell-descriptor document.
     ShellDescriptorDocumentFound,
-
-    // 'InvalidConnection' unable to open a connection to the given connection string.
     InvalidConnection,
-
-    // 'UnsupportedProvider' indicated invalid or unsupported database provider.
     UnsupportedProvider
 }
