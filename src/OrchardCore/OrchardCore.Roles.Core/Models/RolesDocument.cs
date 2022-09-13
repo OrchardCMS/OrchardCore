b1ca65f81974544d6d41f62946a1b77b52e6a07a
Mike Alhayek <mike@crestapps.com>
2022-09-13 14:35:11 -0700

    Add SimplifiedTypeName to RolesDocument since the assumbly name changed

--- src/OrchardCore/OrchardCore.Roles.Core/Models/RolesDocument.cs
+++ src/OrchardCore/OrchardCore.Roles.Core/Models/RolesDocument.cs
@@ -4,6 +4,7 @@ using OrchardCore.Security;
 
 namespace OrchardCore.Roles.Models
 {
+    [YesSql.SimplifiedTypeName("OrchardCore.Roles.Models.RolesDocument, OrchardCore.Roles")]
     public class RolesDocument : Document
     {
         public List<Role> Roles { get; set; } = new List<Role>();
