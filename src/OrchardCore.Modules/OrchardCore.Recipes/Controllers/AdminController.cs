b5f0300e9412d6849e83d8203130eb7a4e00397c
Jasmin Savard <jasmin.savard@gmail.com>
2022-09-14 00:26:56 -0400

    Revert changes on AdminController

--- src/OrchardCore.Modules/OrchardCore.Recipes/Controllers/AdminController.cs
+++ src/OrchardCore.Modules/OrchardCore.Recipes/Controllers/AdminController.cs
@@ -105,7 +105,19 @@ namespace OrchardCore.Recipes.Controllers
 
             var executionId = Guid.NewGuid().ToString("n");
 
-            await _recipeExecutor.ExecuteAsync(executionId, recipe, environment, CancellationToken.None);
+            // Set shell state to "Initializing" so that subsequent HTTP requests
+            // are responded to with "Service Unavailable" while running the recipe.
+            _shellSettings.State = TenantState.Initializing;
+
+            try
+            {
+                await _recipeExecutor.ExecuteAsync(executionId, recipe, environment, CancellationToken.None);
+            }
+            finally
+            {
+                // Don't lock the tenant if the recipe fails.
+                _shellSettings.State = TenantState.Running;
+            }
 
             await _shellHost.ReleaseShellContextAsync(_shellSettings);
 
