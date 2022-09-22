aec12f5dfb302cf6c3d3b9765e17df5200efad43
Sébastien Ros <sebastienros@gmail.com>
2022-09-22 10:40:26 -0700

    Update CultureScope.cs

--- src/OrchardCore/OrchardCore.Abstractions/Localization/CultureScope.cs
+++ src/OrchardCore/OrchardCore.Abstractions/Localization/CultureScope.cs
@@ -1,48 +1,49 @@
 using System;
 using System.Globalization;
 
-namespace OrchardCore.Localization;
-
-public sealed class CultureScope : IDisposable
+namespace OrchardCore.Localization
 {
-    private readonly CultureInfo _originalCulture;
-    private readonly CultureInfo _originalUICulture;
-
-    private CultureScope(string culture, string uiCulture, CultureSettings cultureSettings)
+    public sealed class CultureScope : IDisposable
     {
-        var useUserOveride = cultureSettings == CultureSettings.User;
-        Culture = new CultureInfo(culture, useUserOveride);
-        UICulture = new CultureInfo(uiCulture, useUserOveride);
-        _originalCulture = CultureInfo.CurrentCulture;
-        _originalUICulture = CultureInfo.CurrentUICulture;
+        private readonly CultureInfo _originalCulture;
+        private readonly CultureInfo _originalUICulture;
 
-        SetCultures(Culture, UICulture);
-    }
+        private CultureScope(string culture, string uiCulture, CultureSettings cultureSettings)
+        {
+            var useUserOveride = cultureSettings == CultureSettings.User;
+            Culture = new CultureInfo(culture, useUserOveride);
+            UICulture = new CultureInfo(uiCulture, useUserOveride);
+            _originalCulture = CultureInfo.CurrentCulture;
+            _originalUICulture = CultureInfo.CurrentUICulture;
 
-    public CultureInfo Culture { get; }
+            SetCultures(Culture, UICulture);
+        }
 
-    public CultureInfo UICulture { get; }
+        public CultureInfo Culture { get; }
 
-    public static CultureScope Create(string culture, CultureSettings cultureSettings = CultureSettings.Default)
-        => Create(culture, culture, cultureSettings);
+        public CultureInfo UICulture { get; }
 
-    public static CultureScope Create(string culture, string uiCulture, CultureSettings cultureSettings = CultureSettings.Default)
-        => new(culture, uiCulture, cultureSettings);
+        public static CultureScope Create(string culture, CultureSettings cultureSettings = CultureSettings.Default)
+            => Create(culture, culture, cultureSettings);
 
-    public static CultureScope Create(CultureInfo culture, CultureSettings cultureSettings = CultureSettings.Default)
-        => Create(culture, culture, cultureSettings);
+        public static CultureScope Create(string culture, string uiCulture, CultureSettings cultureSettings = CultureSettings.Default)
+            => new(culture, uiCulture, cultureSettings);
 
-    public static CultureScope Create(CultureInfo culture, CultureInfo uiCulture, CultureSettings cultureSettings = CultureSettings.Default)
-        => new(culture.Name, uiCulture.Name, cultureSettings);
+        public static CultureScope Create(CultureInfo culture, CultureSettings cultureSettings = CultureSettings.Default)
+            => Create(culture, culture, cultureSettings);
 
-    public void Dispose()
-    {
-        SetCultures(_originalCulture, _originalUICulture);
-    }
+        public static CultureScope Create(CultureInfo culture, CultureInfo uiCulture, CultureSettings cultureSettings = CultureSettings.Default)
+            => new(culture.Name, uiCulture.Name, cultureSettings);
 
-    private static void SetCultures(CultureInfo culture, CultureInfo uiCulture)
-    {
-        CultureInfo.CurrentCulture = culture;
-        CultureInfo.CurrentUICulture = uiCulture;
+        public void Dispose()
+        {
+            SetCultures(_originalCulture, _originalUICulture);
+        }
+
+        private static void SetCultures(CultureInfo culture, CultureInfo uiCulture)
+        {
+            CultureInfo.CurrentCulture = culture;
+            CultureInfo.CurrentUICulture = uiCulture;
+        }
     }
 }
