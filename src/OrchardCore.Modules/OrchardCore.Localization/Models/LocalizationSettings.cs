a6814bb0f47aea0e4ce31bd33e6eeeeb82621253
Sébastien Ros <sebastienros@gmail.com>
2022-09-22 10:39:15 -0700

    Update LocalizationSettings.cs

--- src/OrchardCore.Modules/OrchardCore.Localization/Models/LocalizationSettings.cs
+++ src/OrchardCore.Modules/OrchardCore.Localization/Models/LocalizationSettings.cs
@@ -1,36 +1,37 @@
 using System.Globalization;
 
-namespace OrchardCore.Localization.Models;
-
-/// <summary>
-/// Represents an object to store the localization settings.
-/// </summary>
-public class LocalizationSettings
+namespace OrchardCore.Localization.Models
 {
-    private readonly static string[] DefaultSupportedCultures = new[] { CultureInfo.InstalledUICulture.Name };
-
     /// <summary>
-    /// Creates a new instance of the <see cref="LocalizationSettings"/>.
+    /// Represents an object to store the localization settings.
     /// </summary>
-    public LocalizationSettings()
+    public class LocalizationSettings
     {
-        CultureSettings = CultureSettings.Default;
-        DefaultCulture = CultureInfo.InstalledUICulture.Name;
-        SupportedCultures = DefaultSupportedCultures;
+        private readonly static string[] DefaultSupportedCultures = new[] { CultureInfo.InstalledUICulture.Name };
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="LocalizationSettings"/>.
+        /// </summary>
+        public LocalizationSettings()
+        {
+            CultureSettings = CultureSettings.Default;
+            DefaultCulture = CultureInfo.InstalledUICulture.Name;
+            SupportedCultures = DefaultSupportedCultures;
+        }
+
+        /// <summary>
+        /// Gets or sets the culture settings of the site.
+        /// </summary>
+        public CultureSettings CultureSettings { get; set; }
+
+        /// <summary>
+        /// Gets or sets the default culture of the site.
+        /// </summary>
+        public string DefaultCulture { get; set; }
+
+        /// <summary>
+        /// Gets or sets all the supported cultures of the site. It also contains the default culture.
+        /// </summary>
+        public string[] SupportedCultures { get; set; }
     }
-
-    /// <summary>
-    /// Gets or sets the culture settings of the site.
-    /// </summary>
-    public CultureSettings CultureSettings { get; set; }
-
-    /// <summary>
-    /// Gets or sets the default culture of the site.
-    /// </summary>
-    public string DefaultCulture { get; set; }
-
-    /// <summary>
-    /// Gets or sets all the supported cultures of the site. It also contains the default culture.
-    /// </summary>
-    public string[] SupportedCultures { get; set; }
 }
