046d3978f6eea69cd30b8344fe16cac4c087cced
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-10-17 21:01:43 +0200

    Revert "Perhaps TheTheme has a lang attribute?"
    This reverts commit b7e50fe559399302b1d2af2d5b83e53790035250.

--- test/OrchardCore.Tests.UI/Helpers/SetupHelpers.cs
+++ test/OrchardCore.Tests.UI/Helpers/SetupHelpers.cs
@@ -9,7 +9,7 @@ namespace OrchardCore.Tests.UI.Helpers
 {
     public static class SetupHelpers
     {
-        public const string RecipeId = "TheTheme";
+        public const string RecipeId = "Blog";
 
         public static async Task<Uri> RunSetupAsync(UITestContext context)
         {
