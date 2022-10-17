e80515f828023c40e7110bd09238b559fd957a13
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-10-18 00:55:26 +0200

    Blank and Headless recipe tests

---
+++ test/OrchardCore.Tests.UI/Tests/BlankRecipeTests.cs
@@ -0,0 +1,56 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Lombiq.Tests.UI.Attributes;
+using Lombiq.Tests.UI.Extensions;
+using Lombiq.Tests.UI.Pages;
+using Lombiq.Tests.UI.Services;
+using OpenQA.Selenium;
+using Shouldly;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace OrchardCore.Tests.UI.Tests
+{
+    public class BlankRecipeTests : UITestBase
+    {
+        public BlankRecipeTests(ITestOutputHelper testOutputHelper)
+            : base(testOutputHelper)
+        {
+        }
+
+        [Theory, Chrome]
+        public Task BasicOrchardFeaturesShouldWorkWithBlank(Browser browser) =>
+            ExecuteTestAsync(
+                async context =>
+                {
+                    await context.TestSetupWithInvalidAndValidDataAsync(new OrchardCoreSetupParameters(context)
+                    {
+                        RecipeId = "Blank.Tests",
+                    });
+
+                    await context.TestLoginWithInvalidDataAsync();
+                    await context.TestLoginAsync();
+                    await context.TestTurningFeatureOnAndOffAsync();
+                    await context.TestLogoutAsync();
+                },
+                browser,
+                configuration =>
+                {
+                    configuration.HtmlValidationConfiguration.RunHtmlValidationAssertionOnAllPageChanges = false;
+                    configuration.AccessibilityCheckingConfiguration.RunAccessibilityCheckingAssertionOnAllPageChanges = false;
+
+                    configuration.AssertBrowserLog = logEntries =>
+                        logEntries.ShouldNotContain(
+                            logEntry => IsValidBrowserLogEntry(logEntry),
+                            logEntries.Where(IsValidBrowserLogEntry).ToFormattedString());
+
+                    return Task.CompletedTask;
+                });
+
+        private static bool IsValidBrowserLogEntry(LogEntry logEntry)
+        {
+            return OrchardCoreUITestExecutorConfiguration.IsValidBrowserLogMessage(logEntry) &&
+                !logEntry.IsNotFoundMessage("/");
+        }
+    }
+}
---
+++ test/OrchardCore.Tests.UI/Tests/HeadlessRecipeTests.cs
@@ -0,0 +1,41 @@
+using System.Threading.Tasks;
+using Lombiq.Tests.UI.Attributes;
+using Lombiq.Tests.UI.Extensions;
+using Lombiq.Tests.UI.Pages;
+using Lombiq.Tests.UI.Services;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace OrchardCore.Tests.UI.Tests
+{
+    public class HeadlessRecipeTests : UITestBase
+    {
+        public HeadlessRecipeTests(ITestOutputHelper testOutputHelper)
+            : base(testOutputHelper)
+        {
+        }
+
+        [Theory, Chrome]
+        public Task BasicOrchardFeaturesShouldWorkWithHeadless(Browser browser) =>
+            ExecuteTestAsync(
+                async context =>
+                {
+                    await context.TestSetupWithInvalidAndValidDataAsync(new OrchardCoreSetupParameters(context)
+                    {
+                        RecipeId = "Headless.Tests",
+                    });
+
+                    await context.TestLoginWithInvalidDataAsync();
+                    await context.TestLoginAsync();
+                    await context.TestTurningFeatureOnAndOffAsync();
+                    await context.TestLogoutAsync();
+                },
+                browser,
+                configuration =>
+                {
+                    configuration.HtmlValidationConfiguration.RunHtmlValidationAssertionOnAllPageChanges = false;
+                    configuration.AccessibilityCheckingConfiguration.RunAccessibilityCheckingAssertionOnAllPageChanges = false;
+                    return Task.CompletedTask;
+                });
+    }
+}
