fd22b944c931af0d61b9d14836fc4df9c139e804
hishamco <hishamco_2007@yahoo.com>
2022-10-21 14:07:02 +0300

    Enable / disable RSS feed per list

--- src/OrchardCore.Modules/OrchardCore.Lists/Feeds/ListFeedEditViewModel.cs
+++ src/OrchardCore.Modules/OrchardCore.Lists/Feeds/ListFeedEditViewModel.cs
@@ -1,11 +1,15 @@
-﻿using OrchardCore.ContentManagement;
+using OrchardCore.ContentManagement;
 
 namespace OrchardCore.Lists.Feeds
 {
     public class ListFeedEditViewModel
     {
+        public bool EnableFeedProxyUrl { get; set; } = true;
+
         public string FeedProxyUrl { get; set; }
+
         public int FeedItemsCount { get; set; } = ListFeedQuery.DefaultItemsCount;
+
         public ContentItem ContentItem { get; set; }
     }
 }
--- src/OrchardCore/OrchardCore.Feeds.Abstractions/Models/FeedMetadata.cs
+++ src/OrchardCore/OrchardCore.Feeds.Abstractions/Models/FeedMetadata.cs
@@ -1,10 +1,12 @@
-﻿namespace OrchardCore.Feeds.Models
+namespace OrchardCore.Feeds.Models
 {
     /// <summary>
     /// Used to gather custom Feed properties for a content item.
     /// </summary>
     public class FeedMetadata
     {
+        public bool EnableFeedProxyUrl { get; set; } = true;
+
         public string FeedProxyUrl { get; set; }
     }
 }
