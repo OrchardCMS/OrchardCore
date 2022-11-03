61427f89b737f9be2567e472d82aa4688b4e3f69
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-11-03 22:15:20 +0100

    Revert "Revert "Re-enabling all tests""
    This reverts commit 8eea409ca6acbff112060cebb61ec59ffb3042a0.

--- test/OrchardCore.Tests.UI/Tests/InvalidSetupTests.cs
+++ test/OrchardCore.Tests.UI/Tests/InvalidSetupTests.cs
@@ -15,7 +15,7 @@ namespace OrchardCore.Tests.UI.Tests
         {
         }
 
-        [Theory(Skip = "Minimal test suite for multi-DB testing."), Chrome]
+        [Theory, Chrome]
         public Task SetupWithInvalidDataShouldFail(Browser browser) =>
             ExecuteTestAsync(
                 context => context.TestSetupWithInvalidDataAsync(
