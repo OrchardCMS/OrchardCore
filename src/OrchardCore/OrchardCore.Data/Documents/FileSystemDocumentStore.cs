f7d8b0048abee561df17c357ee396027513a194d
Thierry Fleury <thierry@codinlab.fr>
2022-12-13 15:02:15 +0100

    Make FileDocument store independant of DocumentStoreIntroduces IDocumentFileStore with FileSystemDocumentStore default implementation

---
+++ src/OrchardCore/OrchardCore.Data/Documents/FileSystemDocumentStore.cs
@@ -0,0 +1,83 @@
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using OrchardCore.Environment.Shell;
+
+namespace OrchardCore.Data.Documents
+{
+    /// <summary>
+    /// A singleton <see cref="IDocumentFileStore"/> implementation using
+    /// file system to persist documents.
+    /// </summary>
+    public class FileSystemDocumentStore : IDocumentFileStore
+    {
+        private readonly string _tenantPath;
+
+        private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1);
+
+        public FileSystemDocumentStore(IOptions<ShellOptions> shellOptions, ShellSettings shellSettings)
+        {
+            _tenantPath = Path.Combine(
+                shellOptions.Value.ShellsApplicationDataPath,
+                shellOptions.Value.ShellsContainerName,
+                shellSettings.Name) + "/";
+
+            Directory.CreateDirectory(_tenantPath);
+        }
+
+        /// <inheritdoc />
+        public async Task<object> GetDocumentAsync(Type documentType)
+        {
+            var filename = GetFilename(documentType);
+
+            if (!File.Exists(filename))
+            {
+                return null;
+            }
+
+            await _semaphore.WaitAsync();
+            try
+            {
+                using var file = File.OpenText(filename);
+                var serializer = new JsonSerializer();
+
+                return serializer.Deserialize(file, documentType);
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+
+        /// <inheritdoc />
+        public async Task SaveDocumentAsync(Type documentType, object document)
+        {
+            var filename = GetFilename(documentType);
+
+            await _semaphore.WaitAsync();
+            try
+            {
+                using var file = File.CreateText(filename);
+                var serializer = new JsonSerializer
+                {
+                    Formatting = Formatting.Indented
+                };
+
+                serializer.Serialize(file, document);
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+
+        private string GetFilename(Type documentType)
+        {
+            return _tenantPath + (documentType.GetCustomAttribute<FileDocumentStoreAttribute>()?.FileName ?? documentType.Name) + ".json";
+        }
+    }
+}
