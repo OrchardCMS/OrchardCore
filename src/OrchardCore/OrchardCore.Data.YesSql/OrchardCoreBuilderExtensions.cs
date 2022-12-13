6fe73f516eef3b5cd2cace9e59147f8a37845166
Thierry Fleury <thierry@codinlab.fr>
2022-12-13 19:23:57 +0100

    Ensure ISession changes are saved before shell dispose

--- src/OrchardCore/OrchardCore.Data.YesSql/OrchardCoreBuilderExtensions.cs
+++ src/OrchardCore/OrchardCore.Data.YesSql/OrchardCoreBuilderExtensions.cs
@@ -155,6 +155,16 @@ namespace Microsoft.Extensions.DependencyInjection
 
                     session.RegisterIndexes(scopedServices.ToArray());
 
+                    ShellScope.Current
+                        .RegisterBeforeDispose(scope =>
+                        {
+                            return session.SaveChangesAsync();
+                        })
+                        .AddExceptionHandler((scope, e) =>
+                        {
+                            return session.CancelAsync();
+                        });
+
                     return session;
                 });
 
