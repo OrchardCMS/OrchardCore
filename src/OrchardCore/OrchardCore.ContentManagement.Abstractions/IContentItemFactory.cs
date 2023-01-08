08e75b4a23dde06fc771164a8e94b14819e43135
Mike Alhayek <mike@crestapps.com>
2023-01-07 17:59:30 -0800

    Remove the IContentItemFactory

--- src/OrchardCore/OrchardCore.ContentManagement.Abstractions/IContentItemFactory.cs
+++
@@ -1,8 +0,0 @@
-using System.Threading.Tasks;
-
-namespace OrchardCore.ContentManagement;
-
-public interface IContentItemFactory
-{
-    Task<ContentItem> CreateAsync(string contentTypeId, string ownerId = null);
-}
