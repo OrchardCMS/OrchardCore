4e994a28c3934f6bb134db1df9e46bad4cf39999
Mike Alhayek <mike@crestapps.com>
2023-01-15 14:00:43 -0800

    Remove ununsed class

--- src/OrchardCore.Modules/OrchardCore.Users/ViewModels/UserProfileSettingsViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace OrchardCore.Users.ViewModels;
-
-public class UserProfileSettingsViewModel
-{
-    public bool AllowChangingUsername { get; set; }
-
-    public bool AllowChangingEmail { get; set; }
-}
