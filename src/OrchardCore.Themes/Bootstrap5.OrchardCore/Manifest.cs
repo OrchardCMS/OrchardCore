726eb32edf6dbfab51bc3f5d17b3fa0165216962
urbanit <sroussos@urbanit.gr>
2023-02-14 09:51:40 +0200

    Bootstrap 5 Theme

---
+++ src/OrchardCore.Themes/Bootstrap5.OrchardCore/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "Bootstrap5.OrchardCore",
+    Author = "The Orchard Core Team",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "Bootstrap5.OrchardCore"
+)]
