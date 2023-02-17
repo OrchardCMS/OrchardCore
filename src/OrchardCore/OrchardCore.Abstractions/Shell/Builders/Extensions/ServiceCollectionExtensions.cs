75053beae5024ad9f0fb7e8bd2d732cf5bcdcf61
jtkech <jean-thierry.kechichian@wanadoo.fr>
2023-02-17 23:38:27 +0100

    comments

--- src/OrchardCore/OrchardCore.Abstractions/Shell/Builders/Extensions/ServiceCollectionExtensions.cs
+++ src/OrchardCore/OrchardCore.Abstractions/Shell/Builders/Extensions/ServiceCollectionExtensions.cs
@@ -4,12 +4,18 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace OrchardCore.Environment.Shell.Builders;
 
+/// <summary>
+/// Provides a mechanism for initializing tenant container services asynchronously.
+/// </summary>
 public static class ServiceCollectionExtensions
 {
+    /// <summary>
+    /// Adds an async initializer being called on each tenant container creation.
+    /// </summary>
     public static IServiceCollection AddShellContainerAsyncInitializer(
         this IServiceCollection services, Func<IServiceProvider, Task> _initializeAsync)
     {
-        services.AddSingleton<IShellContainerAsyncInitializer>(
+        services.AddTransient<IShellContainerAsyncInitializer>(
             sp => new ShellContainerAsyncInitializer(_initializeAsync));
 
         return services;
