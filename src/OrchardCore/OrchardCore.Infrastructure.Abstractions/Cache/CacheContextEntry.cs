ceffe86a5057780d99f17402c1ca709b112ec077
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2023-02-28 08:43:00 +0300

    CacheContextEntry properties should be readonly

--- src/OrchardCore/OrchardCore.Infrastructure.Abstractions/Cache/CacheContextEntry.cs
+++ src/OrchardCore/OrchardCore.Infrastructure.Abstractions/Cache/CacheContextEntry.cs
@@ -8,7 +8,8 @@
             Value = value;
         }
 
-        public string Key { get; set; }
-        public string Value { get; set; }
+        public string Key { get; }
+
+        public string Value { get; }
     }
 }
