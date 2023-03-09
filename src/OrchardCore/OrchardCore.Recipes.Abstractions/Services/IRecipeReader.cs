ac128cb4abb476a0ba0313534f12956a8b51ce5b
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2023-03-09 18:52:53 +0300

    Remove async .. await from IRecipeReader

--- src/OrchardCore/OrchardCore.Recipes.Abstractions/Services/IRecipeReader.cs
+++ src/OrchardCore/OrchardCore.Recipes.Abstractions/Services/IRecipeReader.cs
@@ -8,8 +8,8 @@ namespace OrchardCore.Recipes.Services
     public interface IRecipeReader
     {
         [Obsolete("This method has been deprecated, please use GetRecipeDescriptorAsync(string, IFileInfo, IFileProvider).")]
-        async Task<RecipeDescriptor> GetRecipeDescriptor(string recipeBasePath, IFileInfo recipeFileInfo, IFileProvider fileProvider)
-            => await GetRecipeDescriptorAsync(recipeBasePath, recipeFileInfo, fileProvider);
+        Task<RecipeDescriptor> GetRecipeDescriptor(string recipeBasePath, IFileInfo recipeFileInfo, IFileProvider fileProvider)
+            => GetRecipeDescriptorAsync(recipeBasePath, recipeFileInfo, fileProvider);
 
         Task<RecipeDescriptor> GetRecipeDescriptorAsync(string recipeBasePath, IFileInfo recipeFileInfo, IFileProvider fileProvider);
     }
