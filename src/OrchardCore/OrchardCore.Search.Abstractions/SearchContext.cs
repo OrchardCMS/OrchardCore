17e80e817b526624b56b7b4ad4923b39451b6cb4
Mike Alhayek <mike@crestapps.com>
2023-03-12 10:21:27 -0700

    Add search handler

---
+++ src/OrchardCore/OrchardCore.Search.Abstractions/SearchContext.cs
@@ -0,0 +1,10 @@
+namespace OrchardCore.Search.Abstractions;
+
+public class SearchContext
+{
+    public string Index { get; set; }
+
+    public string Terms { get; set; }
+
+    public int Hits { get; set; }
+}
