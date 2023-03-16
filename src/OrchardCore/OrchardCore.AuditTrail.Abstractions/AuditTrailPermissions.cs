7e66d1076dd31df8dd72f76ed845c17ceeb8f9db
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2023-03-17 02:05:53 +0300

    Make AuditTrailPermissions static (#13314)

--- src/OrchardCore/OrchardCore.AuditTrail.Abstractions/AuditTrailPermissions.cs
+++ src/OrchardCore/OrchardCore.AuditTrail.Abstractions/AuditTrailPermissions.cs
@@ -2,9 +2,10 @@ using OrchardCore.Security.Permissions;
 
 namespace OrchardCore.AuditTrail
 {
-    public class AuditTrailPermissions
+    public static class AuditTrailPermissions
     {
         public static readonly Permission ViewAuditTrail = new Permission(nameof(ViewAuditTrail), "View audit trail");
+
         public static readonly Permission ManageAuditTrailSettings = new Permission(nameof(ManageAuditTrailSettings), "Manage audit trail settings");
     }
 }
