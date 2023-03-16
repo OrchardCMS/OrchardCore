8d664257bd0b88b032585ee0bf34b3ad81489d8e
Mike Alhayek <mike@crestapps.com>
2023-03-16 08:44:59 -0700

    saving

--- src/OrchardCore/OrchardCore.Search.Elasticsearch.Core/ElasticsearchConstants.cs
+++ src/OrchardCore/OrchardCore.Search.Elasticsearch.Core/ElasticsearchConstants.cs
@@ -4,19 +4,5 @@ public class ElasticsearchConstants
 {
     public static string DefaultAnalyzer = StandardAnalyzer;
 
-    public static string StandardAnalyzer = "Standard Analyzer";
-
-    public static string SimpleAnalyzer = "Simple Analyzer";
-
-    public static string WhitespaceAnalyzer = "Whitespace Analyzer";
-
-    public static string StopAnalyzer = "Stop Analyzer";
-
-    public static string KeywordAnalyzer = "Keyword Analyzer";
-
-    public static string PatternAnalyzer = "Pattern Analyzer";
-
-    public static string LanguageAnalyzers = "Language Analyzers";
-
-    public static string FingerprintAnalyzer = "Fingerprint Analyzer";
+    public static string StandardAnalyzer = "standard";
 }
