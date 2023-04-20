208dd296e2f64066708cc669b7c9239013eaff34
Mike Alhayek <mike@crestapps.com>
2023-04-20 10:42:46 -0700

    adding project specific install/update command

---
+++ .scripts/project.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+'use strict';
+
+console.log(process.argv.length, process.argv);
+
+if(process.argv.length !== 4) {
+
+	console.log('Invalid arguments. This command accepts a single project-name argument.');
+
+	return;
+}
+
+var actionName = process.argv[2];
+
+if(actionName != 'install' && actionName != 'update') {
+
+	console.log('Invalid arguments. The first argument should be either "install" or "udpate".');
+
+	return;
+}
+
+const projectName = process.argv[3];
+
+const glob = require("glob"),
+      exec = require('child_process').exec;
+
+const projects = glob.sync("./src/OrchardCore.{Modules,Themes}/*" + projectName + "*/package.json", {});
+
+if(projects.length == 0) {
+
+	console.log('Unable to find a project with a name that contains the name ' + process.argv[2]);
+
+	return;
+}
+
+
+if(projects.length > 1) {
+
+	console.log('Found multiple projects that contains the name ' + process.argv[2] + '. Please specify a more specific name.');
+	console.log('Here is a list of the matches found');
+	console.log(projects);
+
+	return;
+}
+
+const path = projects[0].substring(0, projects[0].length - 12);
+
+console.log(actionName + 'ing: ' + projects[0]);
+
+exec('npm ' + actionName, {
+    'cwd': path
+}, (error, stdout, stderr) => {
+     console.log(error, stdout, stderr);
+});
