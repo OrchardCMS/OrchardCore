44b0c4f98fa402d2c1153485188968bdd4840d17
Antoine Griffard <agriffard@hotmail.com>
2023-04-30 12:16:28 +0200

    Monac 0.37.1

--- src/OrchardCore.Modules/OrchardCore.Resources/wwwroot/Scripts/monaco/vs/base/common/worker/simpleWorker.nls.es.js
+++ src/OrchardCore.Modules/OrchardCore.Resources/wwwroot/Scripts/monaco/vs/base/common/worker/simpleWorker.nls.es.js
@@ -1,6 +1,6 @@
 /*!-----------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.35.0(71ac097e6155a405f6be52b0b368a04508c31e31)
+ * Version: 0.37.1(20a8d5a651d057aaed7875ad1c1f2ecf13c4e773)
  * Released under the MIT license
  * https://github.com/microsoft/vscode/blob/main/LICENSE.txt
  *-----------------------------------------------------------*/define("vs/base/common/worker/simpleWorker.nls.es",{"vs/base/common/platform":["_"]});
--- src/OrchardCore.Modules/OrchardCore.Resources/wwwroot/Scripts/monaco/vs/basic-languages/javascript/javascript.js
+++ src/OrchardCore.Modules/OrchardCore.Resources/wwwroot/Scripts/monaco/vs/basic-languages/javascript/javascript.js
@@ -1,6 +1,6 @@
 "use strict";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.35.0(71ac097e6155a405f6be52b0b368a04508c31e31)
+ * Version: 0.37.1(20a8d5a651d057aaed7875ad1c1f2ecf13c4e773)
  * Released under the MIT license
  * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
  *-----------------------------------------------------------------------------*/
