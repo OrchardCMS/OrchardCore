6f5a06c50fd4058ccd8e44a368f5eadeea08c7e2
jtkech <jean-thierry.kechichian@wanadoo.fr>
2023-05-03 02:54:39 +0200

    Refactoring and comments

--- src/OrchardCore/OrchardCore.Abstractions/Clusters/ClustersOptions.cs
+++ src/OrchardCore/OrchardCore.Abstractions/Clusters/ClustersOptions.cs
@@ -3,9 +3,23 @@ using System.Collections.Generic;
 
 namespace OrchardCore.Clusters;
 
+/// <summary>
+/// Tenant Clusters options.
+/// </summary>
 public class ClustersOptions
 {
+    /// <summary>
+    /// Wether tenant clustering is enabled or not.
+    /// </summary>
     public bool Enabled { get; set; }
+
+    /// <summary>
+    /// Hosts of instances running as clusters proxy.
+    /// </summary>
     public string[] Hosts { get; set; } = Array.Empty<string>();
+
+    /// <summary>
+    /// List of <see cref="ClusterOptions"/> for each cluster.
+    /// </summary>
     public List<ClusterOptions> Clusters { get; set; } = new();
 }
