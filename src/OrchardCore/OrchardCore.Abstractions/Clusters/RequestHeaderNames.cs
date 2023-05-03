711d665a5f4b071ff95a98ab65a5add463aa5acc
jtkech <jean-thierry.kechichian@wanadoo.fr>
2023-05-03 08:47:56 +0200

    minor changes

--- src/OrchardCore/OrchardCore.Abstractions/Clusters/RequestHeaderNames.cs
+++ src/OrchardCore/OrchardCore.Abstractions/Clusters/RequestHeaderNames.cs
@@ -5,7 +5,18 @@ namespace OrchardCore.Clusters;
 /// </summary>
 public class RequestHeaderNames
 {
+    /// <summary>
+    /// Request header sent by any clusters proxy request.
+    /// </summary>
     public static readonly string FromClustersProxy = "From-Clusters-Proxy";
+
+    /// <summary>
+    /// Request header used to check clusters proxy requests while they are disabled.
+    /// </summary>
     public static readonly string CheckClustersProxy = "Check-Clusters-Proxy";
+
+    /// <summary>
+    /// Request header used to retrieve the original host while behind a clusters proxy.
+    /// </summary>
     public static readonly string XOriginalHost = "X-Original-Host";
 }
