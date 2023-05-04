cc4bdaf6f40b8ed28e9a55cc8668652b669353e7
jtkech <jean-thierry.kechichian@wanadoo.fr>
2023-05-04 04:46:46 +0200

    Tweak

--- src/OrchardCore/OrchardCore.Abstractions/Shell/ShellSettings.cs
+++ src/OrchardCore/OrchardCore.Abstractions/Shell/ShellSettings.cs
@@ -48,8 +48,106 @@ public class ShellSettings : IDisposable
     /// </summary>
     public ShellSettings()
     {
+<<<<<<< HEAD
         _settings = new ShellConfiguration();
         _configuration = new ShellConfiguration();
+=======
+        public const int ClusterSlotCount = 16383;
+        private static readonly char[] _hostSeparators = new[] { ',', ' ' };
+
+        private readonly ShellConfiguration _settings;
+        private readonly ShellConfiguration _configuration;
+        private volatile int _clusterSlot = -1;
+
+        public ShellSettings()
+        {
+            _settings = new ShellConfiguration();
+            _configuration = new ShellConfiguration();
+        }
+
+        public ShellSettings(ShellConfiguration settings, ShellConfiguration configuration)
+        {
+            _settings = settings;
+            _configuration = configuration;
+        }
+
+        public ShellSettings(ShellSettings settings)
+        {
+            _settings = new ShellConfiguration(settings._settings);
+            _configuration = new ShellConfiguration(settings.Name, settings._configuration);
+            Name = settings.Name;
+        }
+
+        public string Name { get; set; }
+
+        public string VersionId
+        {
+            get => _settings["VersionId"];
+            set
+            {
+                _settings["TenantId"] ??= _settings["VersionId"] ?? value;
+                _settings["VersionId"] = value;
+            }
+        }
+
+        public string TenantId => _settings["TenantId"] ?? _settings["VersionId"];
+
+        [JsonIgnore]
+        public int ClusterSlot
+        {
+            get
+            {
+                if (_clusterSlot == -1)
+                {
+                    var tenantId = TenantId;
+                    if (tenantId is not null)
+                    {
+                        Interlocked.Exchange(ref _clusterSlot, Crc16XModem.Compute(tenantId) % ClusterSlotCount);
+                    }
+                }
+
+                return _clusterSlot;
+            }
+        }
+
+        public string RequestUrlHost
+        {
+            get => _settings["RequestUrlHost"];
+            set => _settings["RequestUrlHost"] = value;
+        }
+
+        [JsonIgnore]
+        public string[] RequestUrlHosts => _settings["RequestUrlHost"]
+            ?.Split(_hostSeparators, StringSplitOptions.RemoveEmptyEntries)
+            ?? Array.Empty<string>();
+
+        public string RequestUrlPrefix
+        {
+            get => _settings["RequestUrlPrefix"]?.Trim(' ', '/');
+            set => _settings["RequestUrlPrefix"] = value;
+        }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public TenantState State
+        {
+            get => _settings.GetValue<TenantState>("State");
+            set => _settings["State"] = value.ToString();
+        }
+
+        [JsonIgnore]
+        public IShellConfiguration ShellConfiguration => _configuration;
+
+        [JsonIgnore]
+        public string this[string key]
+        {
+            get => _configuration[key];
+            set => _configuration[key] = value;
+        }
+
+        public Task EnsureConfigurationAsync() => _configuration.EnsureConfigurationAsync();
+
+        public bool IsDefaultShell() => Name == ShellHelper.DefaultShellName;
+>>>>>>> b0328b022 (Tweak)
     }
 
     /// <summary>
@@ -115,9 +213,13 @@ public class ShellSettings : IDisposable
     {
         get
         {
-            if (_clusterSlot == -1 && TenantId is not null)
+            if (_clusterSlot == -1)
             {
-                Interlocked.Exchange(ref _clusterSlot, Crc16XModem.Compute(TenantId) % ClusterSlotsCount);
+                var tenantId = TenantId;
+                if (tenantId is not null)
+                {
+                    Interlocked.Exchange(ref _clusterSlot, Crc16XModem.Compute(tenantId) % ClusterSlotsCount);
+                }
             }
 
             return _clusterSlot;
