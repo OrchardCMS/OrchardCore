b2d3d4a502031b4ea9399d533b48b1a4f00a9670
Antoine Griffard <agriffard@hotmail.com>
2023-05-06 12:25:47 +0200

    StringExtensions Unit Tests

---
+++ test/OrchardCore.Abstractions.Tests/Modules/StringExtensionsTests.cs
@@ -0,0 +1,79 @@
+using System;
+using Xunit;
+
+namespace OrchardCore.Modules
+{
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void ToHexString_ReturnsCorrectHexString()
+        {
+            // Arrange
+            byte[] bytes = { 10, 20, 30 };
+
+            // Act
+            var result = bytes.ToHexString();
+
+            // Assert
+            Assert.Equal("0A141E", result);
+        }
+
+        [Fact]
+        public void ToHexString_NullInput_ThrowsArgumentNullException()
+        {
+            // Arrange
+            byte[] bytes = null;
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => bytes.ToHexString());
+        }
+
+        [Fact]
+        public void ToHexString_EmptyInput_ReturnsEmptyString()
+        {
+            // Arrange
+            var bytes = new byte[0];
+
+            // Act
+            var result = bytes.ToHexString();
+
+            // Assert
+            Assert.Equal("", result);
+        }
+
+        [Theory]
+        [InlineData("414243", new byte[] { 0x41, 0x42, 0x43 })]
+        [InlineData("48656C6C6F20576F726C64", new byte[] { 0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x20, 0x57, 0x6F, 0x72, 0x6C, 0x64 })]
+        [InlineData("", new byte[0])]
+        public void ToByteArray_ShouldConvertToByteArray(string hex, byte[] expected)
+        {
+            // Arrange
+
+            // Act
+            var result = hex.ToByteArray();
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void ToByteArray_ShouldThrowException_WhenHexContainsOddNumberOfCharacters()
+        {
+            // Arrange
+            var hex = "41424";
+
+            // Act & Assert
+            Assert.Throws<FormatException>(() => hex.ToByteArray());
+        }
+
+        [Fact]
+        public void ToByteArray_ShouldThrowException_WhenHexIsNotValid()
+        {
+            // Arrange
+            var hex = "GHIJK";
+
+            // Act & Assert
+            Assert.Throws<FormatException>(() => hex.ToByteArray());
+        }
+    }
+}
