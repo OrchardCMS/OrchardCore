b06cd0227b0f912ffeffeefd32a134e291ce1f8a
jtkech <jean-thierry.kechichian@wanadoo.fr>
2023-05-07 03:58:28 +0200

    Manages ShellContext RequestsCount

--- src/OrchardCore/OrchardCore.Abstractions/Shell/Builders/ShellPipeline.cs
+++ src/OrchardCore/OrchardCore.Abstractions/Shell/Builders/ShellPipeline.cs
@@ -3,17 +3,26 @@ using System.Threading.Tasks;
 
 namespace OrchardCore.Environment.Shell.Builders;
 
+/// <summary>
+/// An <see cref="IShellPipeline"/> that manages the <see cref="ShellContext.RequestsCount"/>.
+/// </summary>
 public class ShellPipeline : IShellPipeline
 {
     private readonly ShellContext _shellContext;
     private readonly IShellPipeline _pipeline;
 
+    /// <summary>
+    /// Builds an <see cref="IShellPipeline"/> that manages the <see cref="ShellContext.RequestsCount"/>.
+    /// </summary>
     public ShellPipeline(ShellContext shellcontext, IShellPipeline pipeline)
     {
         _shellContext = shellcontext;
         _pipeline = pipeline;
     }
 
+    /// <summary>
+    /// Manages the <see cref="ShellContext.RequestsCount"/> before executing the shell pipeline.
+    /// </summary>
     public Task Invoke(object context)
     {
         Interlocked.Increment(ref _shellContext._requestsCount);
