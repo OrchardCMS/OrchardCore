f93bf50703ec926ce9b12f49838d0ea56e8c8a16
jtkech <jean-thierry.kechichian@wanadoo.fr>
2023-05-14 00:56:42 +0200

    FeatureEventHandler base class

--- src/OrchardCore/OrchardCore.Abstractions/Shell/IFeatureEventHandler.cs
+++ src/OrchardCore/OrchardCore.Abstractions/Shell/IFeatureEventHandler.cs
@@ -1,17 +1,35 @@
 using System.Threading.Tasks;
 using OrchardCore.Environment.Extensions.Features;
 
-namespace OrchardCore.Environment.Shell
+namespace OrchardCore.Environment.Shell;
+
+public interface IFeatureEventHandler
 {
-    public interface IFeatureEventHandler
-    {
-        Task InstallingAsync(IFeatureInfo feature);
-        Task InstalledAsync(IFeatureInfo feature);
-        Task EnablingAsync(IFeatureInfo feature);
-        Task EnabledAsync(IFeatureInfo feature);
-        Task DisablingAsync(IFeatureInfo feature);
-        Task DisabledAsync(IFeatureInfo feature);
-        Task UninstallingAsync(IFeatureInfo feature);
-        Task UninstalledAsync(IFeatureInfo feature);
-    }
+    Task InstallingAsync(IFeatureInfo feature);
+    Task InstalledAsync(IFeatureInfo feature);
+    Task EnablingAsync(IFeatureInfo feature);
+    Task EnabledAsync(IFeatureInfo feature);
+    Task DisablingAsync(IFeatureInfo feature);
+    Task DisabledAsync(IFeatureInfo feature);
+    Task UninstallingAsync(IFeatureInfo feature);
+    Task UninstalledAsync(IFeatureInfo feature);
+}
+
+public class FeatureEventHandler : IFeatureEventHandler
+{
+    public virtual Task DisabledAsync(IFeatureInfo feature) => Task.CompletedTask;
+
+    public virtual Task DisablingAsync(IFeatureInfo feature) => Task.CompletedTask;
+
+    public virtual Task EnabledAsync(IFeatureInfo feature) => Task.CompletedTask;
+
+    public virtual Task EnablingAsync(IFeatureInfo feature) => Task.CompletedTask;
+
+    public virtual Task InstalledAsync(IFeatureInfo feature) => Task.CompletedTask;
+
+    public virtual Task InstallingAsync(IFeatureInfo feature) => Task.CompletedTask;
+
+    public virtual Task UninstalledAsync(IFeatureInfo feature) => Task.CompletedTask;
+
+    public virtual Task UninstallingAsync(IFeatureInfo feature) => Task.CompletedTask;
 }
