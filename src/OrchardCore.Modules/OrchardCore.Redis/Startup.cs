4c16f1448d96680c6b9dd2c869ff11f83895ccb4
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2023-05-14 20:11:32 +0300

    Add a separate Startup

--- src/OrchardCore.Modules/OrchardCore.Redis/Startup.cs
+++ src/OrchardCore.Modules/OrchardCore.Redis/Startup.cs
@@ -55,10 +55,6 @@ namespace OrchardCore.Redis
             services.AddSingleton<IRedisService, RedisService>();
             services.AddSingleton<IModularTenantEvents>(sp => sp.GetRequiredService<IRedisService>());
             services.AddSingleton<IRedisDatabaseFactory, RedisDatabaseFactory>();
-
-            services
-                .AddHealthChecks()
-                .AddRedisCheck();
         }
     }
 
