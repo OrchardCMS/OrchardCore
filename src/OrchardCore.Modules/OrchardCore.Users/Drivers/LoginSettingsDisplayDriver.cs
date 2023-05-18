e295fae626347cc3d993c060460c2377e50ea968
Mike Alhayek <mike@crestapps.com>
2023-05-18 12:27:27 -0700

    Add two-factor authentication (#13704)

--- src/OrchardCore.Modules/OrchardCore.Users/Drivers/LoginSettingsDisplayDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.Users/Drivers/LoginSettingsDisplayDriver.cs
@@ -2,9 +2,12 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Localization;
 using OrchardCore.DisplayManagement.Entities;
 using OrchardCore.DisplayManagement.Handlers;
 using OrchardCore.DisplayManagement.Views;
+using OrchardCore.Mvc.ModelBinding;
+using OrchardCore.Security.Services;
 using OrchardCore.Settings;
 using OrchardCore.Users.Models;
 
@@ -15,13 +18,17 @@ namespace OrchardCore.Users.Drivers
         public const string GroupId = "userLogin";
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly IAuthorizationService _authorizationService;
+        private readonly IStringLocalizer S;
 
         public LoginSettingsDisplayDriver(
             IHttpContextAccessor httpContextAccessor,
-            IAuthorizationService authorizationService)
+            IAuthorizationService authorizationService,
+            IStringLocalizer<LoginSettingsDisplayDriver> stringLocalizer,
+            IRoleService roleService)
         {
             _httpContextAccessor = httpContextAccessor;
             _authorizationService = authorizationService;
+            S = stringLocalizer;
         }
         public override async Task<IDisplayResult> EditAsync(LoginSettings settings, BuildEditorContext context)
         {
@@ -32,7 +39,7 @@ namespace OrchardCore.Users.Drivers
                 return null;
             }
 
-            return Initialize<LoginSettings>("LoginSettings_Edit", model =>
+            var contentResult = Initialize<LoginSettings>("LoginSettings_Edit", model =>
             {
                 model.UseSiteTheme = settings.UseSiteTheme;
                 model.UseExternalProviderIfOnlyOneDefined = settings.UseExternalProviderIfOnlyOneDefined;
@@ -41,20 +48,53 @@ namespace OrchardCore.Users.Drivers
                 model.SyncRolesScript = settings.SyncRolesScript;
                 model.AllowChangingEmail = settings.AllowChangingEmail;
                 model.AllowChangingUsername = settings.AllowChangingUsername;
-            }).Location("Content:5").OnGroup(GroupId);
+            }).Location("Content:5#General")
+            .OnGroup(GroupId);
+
+            var enableTwoFaResult = Initialize<LoginSettings>("LoginSettingsEnableTwoFactorAuthentication_Edit", model =>
+            {
+                model.EnableTwoFactorAuthentication = settings.EnableTwoFactorAuthentication;
+            }).Location("Content:5#Two-factor Authentication")
+            .OnGroup(GroupId);
+
+            var twoFaResult = Initialize<LoginSettings>("LoginSettingsTwoFactorAuthentication_Edit", model =>
+            {
+                model.EnableTwoFactorAuthentication = settings.EnableTwoFactorAuthentication;
+                model.NumberOfRecoveryCodesToGenerate = settings.NumberOfRecoveryCodesToGenerate;
+                model.UseEmailAsAuthenticatorDisplayName = settings.UseEmailAsAuthenticatorDisplayName;
+                model.RequireTwoFactorAuthentication = settings.RequireTwoFactorAuthentication;
+                model.AllowRememberClientTwoFactorAuthentication = settings.AllowRememberClientTwoFactorAuthentication;
+                model.TokenLength = settings.TokenLength;
+            }).Location("Content:10#Two-factor Authentication")
+            .OnGroup(GroupId);
+
+            return Combine(contentResult, enableTwoFaResult, twoFaResult);
         }
 
         public override async Task<IDisplayResult> UpdateAsync(LoginSettings section, BuildEditorContext context)
         {
-            if (!await _authorizationService.AuthorizeAsync(_httpContextAccessor.HttpContext?.User, CommonPermissions.ManageUsers))
+            if (!context.GroupId.Equals(GroupId, StringComparison.OrdinalIgnoreCase)
+                || !await _authorizationService.AuthorizeAsync(_httpContextAccessor.HttpContext?.User, CommonPermissions.ManageUsers))
             {
                 return null;
             }
 
-            if (context.GroupId.Equals(GroupId, StringComparison.OrdinalIgnoreCase))
+            await context.Updater.TryUpdateModelAsync(section, Prefix);
+
+            if (section.NumberOfRecoveryCodesToGenerate < 1)
             {
-                await context.Updater.TryUpdateModelAsync(section, Prefix);
+                context.Updater.ModelState.AddModelError(Prefix, nameof(section.NumberOfRecoveryCodesToGenerate), S["Number of Recovery Codes to Generate should be grater than 0."]);
             }
+
+            // A possible issue in Identity prevents from validation token that are not 6 in length.
+            // If this limitation is lifted, the following block can be uncommented.
+            // For more info read https://github.com/dotnet/aspnetcore/issues/48317
+            /*
+            if (section.TokenLength != 6 && section.TokenLength != 8)
+            {
+                context.Updater.ModelState.AddModelError(Prefix, nameof(section.TokenLength), S["The token length should be either 6 or 8."]);
+            }
+            */
 
             return await EditAsync(section, context);
         }
