29122164ca7ca054c5da7f280db78a47671d8682
Mike Alhayek <mike@crestapps.com>
2023-05-29 11:42:25 -0700

    adding release notes.

--- src/OrchardCore.Modules/OrchardCore.Contents/Services/StereotypeFilterNode.cs
+++ src/OrchardCore.Modules/OrchardCore.Contents/Services/StereotypeFilterNode.cs
@@ -1,19 +1,18 @@
-﻿using System;
+using System;
 using YesSql.Filters.Abstractions.Nodes;
 
-namespace OrchardCore.Contents.Services
+namespace OrchardCore.Contents.Services;
+
+public class StereotypeFilterNode : TermOperationNode
 {
-    public class StereotypeFilterNode : TermOperationNode
+    public StereotypeFilterNode(string stereotype)
+        : base("stereotype", new UnaryNode(stereotype, OperateNodeQuotes.None))
     {
-        public StereotypeFilterNode(string stereotype)
-            : base("stereotype", new UnaryNode(stereotype, OperateNodeQuotes.None))
-        {
-        }
-
-        public override string ToNormalizedString()
-            => String.Empty;
-
-        public override string ToString()
-            => String.Empty;
     }
+
+    public override string ToNormalizedString()
+        => String.Empty;
+
+    public override string ToString()
+        => String.Empty;
 }
