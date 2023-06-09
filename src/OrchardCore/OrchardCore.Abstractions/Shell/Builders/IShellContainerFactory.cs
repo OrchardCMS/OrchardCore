a88584cf836821de37aa7cb9e74bb77c74c0fb61
jtkech <jean-thierry.kechichian@wanadoo.fr>
2023-06-09 04:55:33 +0200

    Missing updates

--- src/OrchardCore/OrchardCore.Abstractions/Shell/Builders/IShellContainerFactory.cs
+++ src/OrchardCore/OrchardCore.Abstractions/Shell/Builders/IShellContainerFactory.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.Environment.Shell.Builders.Models;
 
 namespace OrchardCore.Environment.Shell.Builders
@@ -8,6 +7,6 @@ namespace OrchardCore.Environment.Shell.Builders
     public interface IShellContainerFactory
     {
         IServiceProvider CreateContainer(ShellSettings settings, ShellBlueprint blueprint);
-        Task<ServiceProvider> CreateContainerAsync(ShellSettings settings, ShellBlueprint blueprint);
+        Task<IServiceProvider> CreateContainerAsync(ShellSettings settings, ShellBlueprint blueprint);
     }
 }
