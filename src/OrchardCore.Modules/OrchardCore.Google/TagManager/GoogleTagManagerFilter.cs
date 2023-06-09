f2c8d9923a7f98bbc02f720a863e9f9bd0742c3b
Mike Alhayek <mike@crestapps.com>
2023-06-09 16:28:26 -0700

    Include Google Analytics and Tag Manage when user concents. Fix #13825

--- src/OrchardCore.Modules/OrchardCore.Google/TagManager/GoogleTagManagerFilter.cs
+++ src/OrchardCore.Modules/OrchardCore.Google/TagManager/GoogleTagManagerFilter.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -32,11 +34,13 @@ namespace OrchardCore.Google.TagManager
             if ((context.Result is ViewResult || context.Result is PageResult) &&
                 !AdminAttribute.IsApplied(context.HttpContext))
             {
-                if (_scriptsCache == null)
+                var canTrack = context.HttpContext.Features.Get<ITrackingConsentFeature>()?.CanTrack ?? true;
+
+                if (_scriptsCache == null && canTrack)
                 {
                     var settings = (await _siteService.GetSiteSettingsAsync()).As<GoogleTagManagerSettings>();
 
-                    if (!string.IsNullOrWhiteSpace(settings?.ContainerID))
+                    if (!String.IsNullOrWhiteSpace(settings?.ContainerID))
                     {
                         _scriptsCache = new HtmlString("<!-- Google Tag Manager -->\n<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\n  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\n  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n  })(window,document,'script','dataLayer','" + settings.ContainerID + "');</script>\n<!-- End Google Tag Manager -->");
                     }
