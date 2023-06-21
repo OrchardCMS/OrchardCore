0a10df36465c7f9f1195a87daacb6df7b9bda3d9
jtkech <jean-thierry.kechichian@wanadoo.fr>
2023-06-21 05:52:30 +0200

    Only get definitions of provided contained types

--- src/OrchardCore.Modules/OrchardCore.Lists/Drivers/ContainedPartDisplayDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.Lists/Drivers/ContainedPartDisplayDriver.cs
@@ -164,16 +164,10 @@ namespace OrchardCore.Lists.Drivers
             }).Location("Content:1");
         }
 
-        private IEnumerable<ContentTypeDefinition> GetContainedContentTypes(ListPartSettings settings)
-        {
-            var contentTypes = settings.ContainedContentTypes ?? Enumerable.Empty<string>();
-
-            if (!contentTypes.Any())
-            {
-                return Enumerable.Empty<ContentTypeDefinition>();
-            }
-
-            return _contentDefinitionManager.ListTypeDefinitions().Where(x => contentTypes.Contains(x.Name));
-        }
+        private IEnumerable<ContentTypeDefinition> GetContainedContentTypes(ListPartSettings settings) =>
+            settings.ContainedContentTypes
+                ?.Where(contentType => !String.IsNullOrEmpty(contentType))
+                .Select(contentType => _contentDefinitionManager.GetTypeDefinition(contentType))
+                ?? Enumerable.Empty<ContentTypeDefinition>();
     }
 }
