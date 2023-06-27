9525492a8c6c8197dda920bcc27bd9a86735f9d7
Mike Alhayek <mike@crestapps.com>
2023-06-27 16:06:44 -0700

    add docs

--- src/OrchardCore/OrchardCore.Contents.Core/ContentSearchOption.cs
+++ src/OrchardCore/OrchardCore.Contents.Core/ContentSearchOption.cs
@@ -7,11 +7,22 @@ public class ContentSearchOption
 {
     private readonly Dictionary<string, string> _maps = new(StringComparer.OrdinalIgnoreCase);
 
+    /// <summary>
+    /// Gets the term-name to use by the given content type.
+    /// </summary>
+    /// <param name="contentType"></param>
+    /// <param name="termName"></param>
+    /// <returns></returns>
     public bool TryGetValue(string contentType, out string termName)
     {
         return _maps.TryGetValue(contentType, out termName);
     }
 
+    /// <summary>
+    /// Adds a custom term name for the given content type if one does not exists.
+    /// </summary>
+    /// <param name="contentType"></param>
+    /// <param name="termName"></param>
     public void AddTerm(string contentType, string termName)
     {
         ArgumentNullException.ThrowIfNull(contentType, nameof(contentType));
@@ -19,6 +30,10 @@ public class ContentSearchOption
         _maps.TryAdd(contentType, termName);
     }
 
+    /// <summary>
+    /// Removes the content type from from the options if exists.
+    /// </summary>
+    /// <param name="contentType"></param>
     public void RemoveTerm(string contentType)
     {
         ArgumentNullException.ThrowIfNull(contentType, nameof(contentType));
