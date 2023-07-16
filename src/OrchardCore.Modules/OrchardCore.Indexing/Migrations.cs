8a2ac812c580f13b7c05cd5b19e67e0f852e1cf5
Mike Alhayek <mike@crestapps.com>
2023-07-16 10:30:18 -0700

    update migration

--- src/OrchardCore.Modules/OrchardCore.Indexing/Migrations.cs
+++ src/OrchardCore.Modules/OrchardCore.Indexing/Migrations.cs
@@ -8,7 +8,7 @@ namespace OrchardCore.Indexing
         public int Create()
         {
             SchemaBuilder.CreateTable(nameof(IndexingTask), table => table
-                .Column<int>(nameof(IndexingTask.Id), col => col.PrimaryKey().Identity())
+                .Column<long>(nameof(IndexingTask.Id), col => col.PrimaryKey().Identity())
                 .Column<string>(nameof(IndexingTask.ContentItemId), c => c.WithLength(26))
                 .Column<DateTime>(nameof(IndexingTask.CreatedUtc), col => col.NotNull())
                 .Column<int>(nameof(IndexingTask.Type))
