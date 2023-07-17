321bcc501d69d3309f12b0a9a877a6d656ed7233
Jean-Thierry Kéchichian <jean-thierry.kechichian@wanadoo.fr>
2023-07-17 03:07:40 +0200

    Revert IndexingTask change

--- src/OrchardCore.Modules/OrchardCore.Indexing/Migrations.cs
+++ src/OrchardCore.Modules/OrchardCore.Indexing/Migrations.cs
@@ -1,23 +1,14 @@
 using System;
-using OrchardCore.Data;
 using OrchardCore.Data.Migration;
-using OrchardCore.Environment.Shell;
-using YesSql;
 
 namespace OrchardCore.Indexing
 {
     public class Migrations : DataMigration
     {
-        private readonly ShellSettings _shellSettings;
-
-        public Migrations(ShellSettings shellSettings) => _shellSettings = shellSettings;
-
         public int Create()
         {
-            var identityColumnSize = Enum.Parse<IdentityColumnSize>(_shellSettings.GetIdentityColumnSize());
-
             SchemaBuilder.CreateTable(nameof(IndexingTask), table => table
-                .Column(identityColumnSize, nameof(IndexingTask.Id), col => col.PrimaryKey().Identity())
+                .Column<int>(nameof(IndexingTask.Id), col => col.PrimaryKey().Identity())
                 .Column<string>(nameof(IndexingTask.ContentItemId), c => c.WithLength(26))
                 .Column<DateTime>(nameof(IndexingTask.CreatedUtc), col => col.NotNull())
                 .Column<int>(nameof(IndexingTask.Type))
