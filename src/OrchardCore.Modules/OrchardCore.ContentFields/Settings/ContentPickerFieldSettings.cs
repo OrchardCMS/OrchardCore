dac63cfe3bdbc0d36d58b8d201fd50a833e9ccb1
Mike Alhayek <mike@crestapps.com>
2023-08-11 15:08:25 -0700

    Use display_text helper to generate TitlePattern

--- src/OrchardCore.Modules/OrchardCore.ContentFields/Settings/ContentPickerFieldSettings.cs
+++ src/OrchardCore.Modules/OrchardCore.ContentFields/Settings/ContentPickerFieldSettings.cs
@@ -6,16 +6,21 @@ namespace OrchardCore.ContentFields.Settings
     public class ContentPickerFieldSettings
     {
         public string Hint { get; set; }
+
         public bool Required { get; set; }
+
         public bool Multiple { get; set; }
+
         public bool DisplayAllContentTypes { get; set; }
+
         public string[] DisplayedContentTypes { get; set; } = Array.Empty<string>();
+
         public string[] DisplayedStereotypes { get; set; } = Array.Empty<string>();
 
         /// <summary>
-        /// The pattern used to build custom title
+        /// The pattern used to build title.
         /// </summary>
-        [DefaultValue("{{ Model.ContentItem.DisplayText }}")]
-        public string TitlePattern { get; set; } = "{{ Model.ContentItem.DisplayText }}";
+        [DefaultValue("{{ Model.ContentItem | display_text }}")]
+        public string TitlePattern { get; set; } = "{{ Model.ContentItem | display_text }}";
     }
 }
