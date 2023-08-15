b10177d8b08dda9f8cbf4ba9b92f44bd49308845
jtkech <jean-thierry.kechichian@wanadoo.fr>
2023-08-15 23:49:46 +0200

    Remove using

--- src/OrchardCore/OrchardCore.Media.Core/DefaultMediaFileStore.cs
+++ src/OrchardCore/OrchardCore.Media.Core/DefaultMediaFileStore.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
