f20f7d884d9dd2462e4809eb0a2c41abd796e133
Mike Alhayek <mike@crestapps.com>
2023-08-28 19:28:06 -0700

    Move EncodeAlternateElement into one common place. and cleanup strings.

--- src/OrchardCore.Modules/OrchardCore.Users/UserMenuShapeTableProvider.cs
+++ src/OrchardCore.Modules/OrchardCore.Users/UserMenuShapeTableProvider.cs
@@ -1,16 +1,21 @@
 using System;
 using OrchardCore.DisplayManagement.Descriptors;
+using OrchardCore.DisplayManagement.Utilities;
 
 namespace OrchardCore.Users;
 
 public class UserMenuShapeTableProvider : IShapeTableProvider
 {
-    private const string _shapePrefix = "UserMenuItems";
+    public const string ShapeTypePrefix = $"{ShapePrefix}__";
+
+    private const string ShapePrefix = "UserMenuItems";
+
+    private const string ShapeAlternatePrefix = $"{ShapePrefix}_";
 
     public void Discover(ShapeTableBuilder builder)
     {
         // Describe any shape-type that starts with 'UserMenuItems__'.
-        builder.Describe(_shapePrefix + "__*")
+        builder.Describe(ShapeTypePrefix + "*")
             .OnDisplaying(context =>
             {
                 if (String.IsNullOrEmpty(context.Shape.Metadata.DisplayType) || context.Shape.Metadata.DisplayType == "Detail")
@@ -19,13 +24,13 @@ public class UserMenuShapeTableProvider : IShapeTableProvider
                 }
 
                 // UserMenuItems_{displayType} > UserMenuItems.{displayType}.cshtml.
-                context.Shape.Metadata.Alternates.Add(_shapePrefix + '_' + context.Shape.Metadata.DisplayType);
+                context.Shape.Metadata.Alternates.Add($"{ShapeAlternatePrefix}{context.Shape.Metadata.DisplayType}");
 
                 // The value of 'subType' is the string that comes after 'UserMenuItems__'.
-                var subType = context.Shape.Metadata.Type[(_shapePrefix.Length + 2)..];
+                var subType = context.Shape.Metadata.Type[ShapeTypePrefix.Length..].EncodeAlternateElement();
 
                 // UserMenuItems_{displaType}__{subType} > UserMenuItems-{subType}.{displayType}.cshtml.
-                context.Shape.Metadata.Alternates.Add(_shapePrefix + '_' + context.Shape.Metadata.DisplayType + "__" + subType);
+                context.Shape.Metadata.Alternates.Add($"{ShapeAlternatePrefix}{context.Shape.Metadata.DisplayType}__{subType}");
             });
     }
 }
