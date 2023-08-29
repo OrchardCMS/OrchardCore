e316f3d86e01cb2b01fbfab31e10171ce11aa4f2
Mike Alhayek <mike@crestapps.com>
2023-08-29 14:23:02 -0700

    cleanup

--- src/OrchardCore/OrchardCore.DisplayManagement.Abstractions/Extensions/ResultExecutingContextExtensions.cs
+++ src/OrchardCore/OrchardCore.DisplayManagement.Abstractions/Extensions/ResultExecutingContextExtensions.cs
@@ -5,11 +5,5 @@ namespace Microsoft.AspNetCore.Mvc.Filters;
 public static class ResultExecutingContextExtensions
 {
     public static bool IsViewOrPageResult(this ResultExecutingContext context)
-        => context.IsViewResult() || context.IsPageResult();
-
-    public static bool IsViewResult(this ResultExecutingContext context)
-        => context.Result is ViewResult;
-
-    public static bool IsPageResult(this ResultExecutingContext context)
-        => context.Result is PageResult;
+        => context.Result is ViewResult or PageResult;
 }
