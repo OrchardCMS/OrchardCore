41c8f0fbc512db429912979def41cb5e7016cba0
Mike Alhayek <mike@crestapps.com>
2023-09-19 13:37:32 -0700

    Add As/Put method to ISite to allow caching

--- src/OrchardCore.Modules/OrchardCore.Settings/SiteSettings.cs
+++ src/OrchardCore.Modules/OrchardCore.Settings/SiteSettings.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Routing;
 using OrchardCore.Documents;
+using OrchardCore.Entities;
 
 namespace OrchardCore.Settings
 {
@@ -7,20 +9,64 @@ namespace OrchardCore.Settings
     public class SiteSettings : DocumentEntity, ISite
     {
         public string BaseUrl { get; set; }
+
         public string Calendar { get; set; }
+
         public int MaxPagedCount { get; set; }
+
         public int MaxPageSize { get; set; }
+
         public int PageSize { get; set; }
+
         public string TimeZoneId { get; set; }
+
         public ResourceDebugMode ResourceDebugMode { get; set; }
+
         public string SiteName { get; set; }
+
         public string SiteSalt { get; set; }
+
         public string PageTitleFormat { get; set; }
+
         public string SuperUser { get; set; }
+
         public bool UseCdn { get; set; }
+
         public string CdnBaseUrl { get; set; }
+
         public RouteValueDictionary HomeRoute { get; set; } = new RouteValueDictionary();
+
         public bool AppendVersion { get; set; } = true;
+
         public CacheMode CacheMode { get; set; }
+
+        public T As<T>() where T : new()
+        {
+            var name = typeof(T).Name;
+
+            if (_cache.TryGetValue(name, out var obj) && obj is T value)
+            {
+                return value;
+            }
+
+            var settings = this.As<T>(name);
+
+            _cache[name] = settings;
+
+            return settings;
+        }
+
+        public ISite Put<T>(T aspect) where T : new()
+        {
+            var name = typeof(T).Name;
+
+            this.Put(name, aspect);
+
+            _cache.Remove(name);
+
+            return this;
+        }
+
+        private readonly Dictionary<string, object> _cache = new();
     }
 }
