93825b13632fb6fd7d91866e5a9e27df6ac5952a
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2023-10-06 12:43:04 +0300

    SMTP should send the email if the SSL certificate is invalid

--- src/OrchardCore/OrchardCore.Email.Abstractions/SmtpSettings.cs
+++ src/OrchardCore/OrchardCore.Email.Abstractions/SmtpSettings.cs
@@ -79,6 +79,11 @@ namespace OrchardCore.Email
         /// </summary>
         public int ProxyPort { get; set; }
 
+        /// <summary>
+        /// Gets or sets whether the SSL certificate should be ignored once its invalid.
+        /// </summary>
+        public bool IgnoreInvalidSslCertificate { get; set; }
+
         /// <inheritdocs />
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
