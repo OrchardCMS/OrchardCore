f6366f93aca12668d65f7f969284c39a6d12db3e
Sebastien Ros <sebastienros@gmail.com>
2023-10-17 08:37:58 -0700

    Fix MySQL index length

--- test/OrchardCore.Tests.Functional/cms-tests/cypress/integration/migrations-test.js
+++ test/OrchardCore.Tests.Functional/cms-tests/cypress/integration/migrations-test.js
@@ -2,20 +2,21 @@
 
 import { generateTenantInfo } from 'cypress-orchardcore/dist/utils';
 
-describe('Agency Tests', function () {
+describe('Migrations Tests', function () {
     let tenant;
 
     before(() => {
-        tenant = generateTenantInfo("Agency")
+        // generate the migrations tenant
+        tenant = generateTenantInfo("Migrations")
         cy.newTenant(tenant);
     })
 
-    it('Displays the home page of the Agency theme', function(){
+    it('Displays the home page of the migrations recipe', function () {
         cy.visit(`${tenant.prefix}`);
-        cy.get('#services').should('contain.text', 'Lorem ipsum dolor sit amet consectetur');
+        cy.get('.subheading').should('contain.text', 'This is the description of your blog');
     })
 
-    it('Agency admin login should work', function(){
+    it('Migrations admin login should work', function () {
         cy.login(tenant);
         cy.visit(`${tenant.prefix}/Admin`);
         cy.get('.menu-admin').should('have.id', 'adminMenu')
