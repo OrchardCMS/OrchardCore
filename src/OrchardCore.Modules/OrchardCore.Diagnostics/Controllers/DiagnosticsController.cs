329408823b9e56456b9ae8d1875139573a825cd4
Mike Alhayek <mike@crestapps.com>
2023-10-19 12:58:40 -0700

    Use shape when rendering Http errors to allow cusomization from the UI

--- src/OrchardCore.Modules/OrchardCore.Diagnostics/Controllers/DiagnosticsController.cs
+++ src/OrchardCore.Modules/OrchardCore.Diagnostics/Controllers/DiagnosticsController.cs
@@ -1,27 +1,23 @@
 using System;
 using System.Net;
 using Microsoft.AspNetCore.Mvc;
+using OrchardCore.Diagnostics.ViewModels;
 
-namespace OrchardCore.Diagnostics.Controllers
+namespace OrchardCore.Diagnostics.Controllers;
+
+public class DiagnosticsController : Controller
 {
-    public class DiagnosticsController : Controller
+    [IgnoreAntiforgeryToken]
+    public IActionResult Error(int? status)
     {
-        [IgnoreAntiforgeryToken]
-        public IActionResult Error(int? status)
+        // Most commonly used error messages.
+        ViewData["StatusCode"] = status;
+
+        if (!status.HasValue || !Enum.TryParse<HttpStatusCode>(status.ToString(), out var httpStatusCode))
         {
-            // Most commonly used error messages.
-            ViewData["StatusCode"] = status;
-            Enum.TryParse((status ?? 500).ToString(), true, out HttpStatusCode httpStatusCode);
-
-
-            return httpStatusCode switch
-            {
-                HttpStatusCode.Forbidden => View("Forbidden"),
-                HttpStatusCode.NotFound => View("NotFound"),
-                HttpStatusCode.BadRequest => View("BadRequest"),
-                HttpStatusCode.Unauthorized => View("Unauthorized"),
-                _ => View("Error"),
-            };
+            httpStatusCode = HttpStatusCode.InternalServerError;
         }
+
+        return View(new ErrorShapeViewModel(httpStatusCode));
     }
 }
