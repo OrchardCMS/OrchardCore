18a5cdfbfb813163a7f9a01cfe1f2467d22f655c
lampersky <lampersky@gmail.com>
2023-10-20 13:15:05 +0200

    PR fix

--- src/OrchardCore/OrchardCore.ContentManagement.Abstractions/Metadata/Models/ContentPartFieldDefinitionExtensions.cs
+++ src/OrchardCore/OrchardCore.ContentManagement.Abstractions/Metadata/Models/ContentPartFieldDefinitionExtensions.cs
@@ -3,23 +3,16 @@ namespace OrchardCore.ContentManagement.Metadata.Models
     public static class ContentPartFieldDefinitionExtensions
     {
         public static bool IsNamedPart(this ContentPartFieldDefinition fieldDefinition)
-        {
-            return fieldDefinition.ContentTypePartDefinition.Name != fieldDefinition.PartDefinition.Name;
-        }
+            => fieldDefinition.ContentTypePartDefinition.Name != fieldDefinition.PartDefinition.Name;
 
-        public static string UniqueNameRegularPart(this ContentPartFieldDefinition fieldDefinition)
-        {
-            return $"{fieldDefinition.PartDefinition.Name}-{fieldDefinition.Name}";
-        }
-
-        public static string UniqueNameNamedPart(this ContentPartFieldDefinition fieldDefinition)
-        {
-            return $"{fieldDefinition.ContentTypePartDefinition.Name}-{fieldDefinition.Name}";
-        }
-
-        public static string UniqueName(this ContentPartFieldDefinition fieldDefinition)
-        {
-            return fieldDefinition.IsNamedPart() ? fieldDefinition.UniqueNameNamedPart() : fieldDefinition.UniqueNameRegularPart();
-        }
+        /// <summary>
+        /// Returns full field name, if field belongs to named part technical name is used.
+        /// </summary>
+        /// <param name="fieldDefinition"></param>
+        /// <returns></returns>
+        public static string GetFullName(this ContentPartFieldDefinition fieldDefinition)
+            => fieldDefinition.IsNamedPart()
+                ? $"{fieldDefinition.ContentTypePartDefinition.Name}-{fieldDefinition.Name}"
+                : $"{fieldDefinition.PartDefinition.Name}-{fieldDefinition.Name}";
     }
 }
