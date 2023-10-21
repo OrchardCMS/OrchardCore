5aa11ecfe639d889f973c959472153d9e150ac47
jtkech <jean-thierry.kechichian@wanadoo.fr>
2023-10-21 03:36:21 +0200

    Useless property

--- src/OrchardCore.Modules/OrchardCore.Secrets/ViewModels/SecretBindingEntry.cs
+++ src/OrchardCore.Modules/OrchardCore.Secrets/ViewModels/SecretBindingEntry.cs
@@ -6,6 +6,5 @@ public class SecretBindingEntry
 {
     public string Name { get; set; }
     public SecretBinding SecretBinding { get; set; }
-    public bool IsChecked { get; set; }
     public dynamic Summary { get; set; }
 }
