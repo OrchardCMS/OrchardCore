fddb800c7a2790f24010d4a38b54e661fd585f6a
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2023-10-21 11:08:05 +0300

    Update src/OrchardCore.Modules/OrchardCore.Users/ViewModels/CustomUserSettingsDeploymentStepViewModel.cs

--- src/OrchardCore.Modules/OrchardCore.Users/ViewModels/CustomUserSettingsDeploymentStepViewModel.cs
+++ src/OrchardCore.Modules/OrchardCore.Users/ViewModels/CustomUserSettingsDeploymentStepViewModel.cs
@@ -3,6 +3,7 @@ namespace OrchardCore.Users.ViewModels;
 public class CustomUserSettingsDeploymentStepViewModel
 {
     public bool IncludeAll { get; set; }
+
     public string[] SettingsTypeNames { get; set; }
     public string[] AllSettingsTypeNames { get; set; }
 }
