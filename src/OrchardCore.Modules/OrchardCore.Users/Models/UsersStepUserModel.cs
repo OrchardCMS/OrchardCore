28ac1df21deec26d8303fcc77467e8344eb9425e
Szymon Seliga <SzymonSel@users.noreply.github.com>
2023-10-21 10:17:51 +0200

    Update src/OrchardCore.Modules/OrchardCore.Users/Models/UsersStepUserModel.cs
    Co-authored-by: Hisham Bin Ateya <hishamco_2007@yahoo.com>

--- src/OrchardCore.Modules/OrchardCore.Users/Models/UsersStepUserModel.cs
+++ src/OrchardCore.Modules/OrchardCore.Users/Models/UsersStepUserModel.cs
@@ -7,6 +7,7 @@ public class UsersStepUserModel
     public long Id { get; set; }
 
     public string UserId { get; set; }
+
     public string UserName { get; set; }
     public string Email { get; set; }
     public string PasswordHash { get; set; }
