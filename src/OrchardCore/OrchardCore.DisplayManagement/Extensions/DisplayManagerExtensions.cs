2aa9409486fa605df56b59c4d30168ca79dc700e
Mike Alhayek <mike@crestapps.com>
2023-10-26 07:27:31 -0700

    cleanup

--- src/OrchardCore/OrchardCore.DisplayManagement/Extensions/DisplayManagerExtensions.cs
+++ src/OrchardCore/OrchardCore.DisplayManagement/Extensions/DisplayManagerExtensions.cs
@@ -5,11 +5,9 @@ namespace OrchardCore.DisplayManagement;
 
 public static class DisplayManagerExtensions
 {
-    public static Task<IShape> BuildDisplayAsync<TModel>(this IDisplayManager<TModel> displayManager, IUpdateModel updater, string displayType = "", string groupId = "")
-        where TModel : new()
+    public static Task<IShape> BuildDisplayAsync<TModel>(this IDisplayManager<TModel> displayManager, IUpdateModel updater, string displayType = "", string groupId = "") where TModel : new()
         => displayManager.BuildDisplayAsync(new TModel(), updater, displayType, groupId);
 
-    public static Task<IShape> BuildEditorAsync<TModel>(this IDisplayManager<TModel> displayManager, IUpdateModel updater, bool isNew, string groupId = "", string htmlPrefix = "")
-        where TModel : new()
+    public static Task<IShape> BuildEditorAsync<TModel>(this IDisplayManager<TModel> displayManager, IUpdateModel updater, bool isNew, string groupId = "", string htmlPrefix = "") where TModel : new()
         => displayManager.BuildEditorAsync(new TModel(), updater, isNew, groupId, htmlPrefix);
 }
