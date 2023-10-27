e800bcb6d69fac81e81eb0fbcd7510d4fd9204f8
Mike Alhayek <mike@crestapps.com>
2023-10-27 10:37:05 -0700

    Update GetPartWrapperCssClasses and GetFieldWrapperCssClasses helpers (#14591)

--- src/OrchardCore/OrchardCore.DisplayManagement/Html/CssOrchardHelperExtensions.cs
+++ src/OrchardCore/OrchardCore.DisplayManagement/Html/CssOrchardHelperExtensions.cs
@@ -6,7 +6,7 @@ using OrchardCore.DisplayManagement.Html;
 
 namespace OrchardCore;
 
-public static class CssOrchardHelper
+public static class CssOrchardHelperExtensions
 {
     public static string GetLimitedWidthWrapperCssClasses(this IOrchardHelper helper, params string[] additionalClasses)
     {
@@ -74,7 +74,8 @@ public static class CssOrchardHelper
         return string.Join(' ', Combine(options.OffsetClasses, additionalClasses));
     }
 
-    private static TheAdminThemeOptions GetThemeOptions(IOrchardHelper helper) => helper.HttpContext.RequestServices.GetService<IOptions<TheAdminThemeOptions>>().Value;
+    private static TheAdminThemeOptions GetThemeOptions(IOrchardHelper helper)
+        => helper.HttpContext.RequestServices.GetService<IOptions<TheAdminThemeOptions>>().Value;
 
     private static IEnumerable<string> Combine(string optionClasses, string[] additionalClasses)
     {
