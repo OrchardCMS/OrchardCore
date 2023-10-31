b2d58eb09083c2331b307511762c168b0f0c8e2a
Mike Alhayek <mike@crestapps.com>
2023-10-31 15:08:17 -0700

    Improve performance for CssClass extensions

--- src/OrchardCore/OrchardCore.ContentManagement.Display/Razor/CssOrchardHelperExtensions.cs
+++ src/OrchardCore/OrchardCore.ContentManagement.Display/Razor/CssOrchardHelperExtensions.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Html;
 using OrchardCore.ContentManagement.Metadata.Models;
 using OrchardCore.Mvc.Utilities;
 
@@ -6,13 +7,16 @@ namespace OrchardCore;
 
 public static class CssOrchardHelperExtensions
 {
-    private const string FieldWrapperPrefix = "field-wrapper";
     private const string PartWrapperPrefix = "content-part-wrapper";
+    private const string FieldWrapperPrefix = "field-wrapper";
 
-    public static string GetPartWrapperCssClasses(this IOrchardHelper helper, ContentTypePartDefinition partDefinition, params string[] additionalClasses)
+    public static IHtmlContent GetPartWrapperClasses(this IOrchardHelper helper, ContentTypePartDefinition partDefinition, params string[] additionalClasses)
     {
+        var options = helper.GetThemeOptions();
+
         var items = new List<string>()
         {
+            options.WrapperClasses,
             PartWrapperPrefix,
         };
 
@@ -26,18 +30,21 @@ public static class CssOrchardHelperExtensions
             }
         }
 
-        if (additionalClasses?.Length > 0)
+        if (additionalClasses != null)
         {
             items.AddRange(additionalClasses);
         }
 
-        return helper.GetWrapperCssClasses(items.ToArray());
+        return GetHtmlContentBuilder(items);
     }
 
-    public static string GetFieldWrapperCssClasses(this IOrchardHelper helper, ContentPartFieldDefinition fieldDefinition, params string[] additionalClasses)
+    public static IHtmlContent GetFieldWrapperClasses(this IOrchardHelper helper, ContentPartFieldDefinition fieldDefinition, params string[] additionalClasses)
     {
+        var options = helper.GetThemeOptions();
+
         var items = new List<string>()
         {
+            options.WrapperClasses,
             FieldWrapperPrefix
         };
 
@@ -51,11 +58,38 @@ public static class CssOrchardHelperExtensions
             }
         }
 
-        if (additionalClasses?.Length > 0)
+        if (additionalClasses != null)
         {
             items.AddRange(additionalClasses);
         }
 
-        return helper.GetWrapperCssClasses(items.ToArray());
+        return GetHtmlContentBuilder(items);
+    }
+
+    private static HtmlContentBuilder GetHtmlContentBuilder(IEnumerable<string> values)
+    {
+        var builder = new HtmlContentBuilder();
+
+        foreach (var value in values)
+        {
+            if (builder.Count == 0)
+            {
+                // The 'Append' method performs a string.IsNullOrEmpty() check. So, as long as the builder has no entries, we append with no spaces.
+                builder.Append(value);
+
+                continue;
+            }
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                continue;
+            }
+
+            // At this point, we already know that the builder has at least one entry, so we append a single space to the class name.
+            // We pass create HtmlString here to prevent the builder from preforming string.IsNullOrWhiteSpace again for performance reason.
+            builder.AppendHtml(new HtmlString(' ' + value));
+        }
+
+        return builder;
     }
 }
--- src/OrchardCore/OrchardCore.DisplayManagement/Html/CssOrchardHelperExtensions.cs
+++ src/OrchardCore/OrchardCore.DisplayManagement/Html/CssOrchardHelperExtensions.cs
@@ -1,58 +1,134 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Html;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Linq;
 using OrchardCore.DisplayManagement.Html;
 
 namespace OrchardCore;
 
 public static class CssOrchardHelperExtensions
 {
+    public static IHtmlContent GetLimitedWidthWrapperClasses(this IOrchardHelper helper, params string[] additionalClasses)
+    {
+        var options = helper.GetThemeOptions();
+
+        return GetHtmlContentBuilder(options.LimitedWidthWrapperClasses, additionalClasses);
+    }
+
+    public static IHtmlContent GetLimitedWidthClasses(this IOrchardHelper helper, params string[] additionalClasses)
+    {
+        var options = helper.GetThemeOptions();
+
+        return GetHtmlContentBuilder(options.LimitedWidthClasses, additionalClasses);
+    }
+
+    public static IHtmlContent GetStartClasses(this IOrchardHelper helper, params string[] additionalClasses)
+    {
+        var options = helper.GetThemeOptions();
+
+        return GetHtmlContentBuilder(options.StartClasses, additionalClasses);
+    }
+
+    public static IHtmlContent GetEndClasses(this IOrchardHelper helper, params string[] additionalClasses)
+    {
+        var options = helper.GetThemeOptions();
+
+        return GetHtmlContentBuilder(options.EndClasses, additionalClasses);
+    }
+
+    public static IHtmlContent GetLabelClasses(this IOrchardHelper helper, params string[] additionalClasses)
+    {
+        var options = helper.GetThemeOptions();
+
+        return GetHtmlContentBuilder(options.LabelClasses, additionalClasses);
+    }
+
+    public static IHtmlContent GetWrapperClasses(this IOrchardHelper helper, params string[] additionalClasses)
+    {
+        var options = helper.GetThemeOptions();
+
+        return GetHtmlContentBuilder(options.WrapperClasses, additionalClasses);
+    }
+
+    public static IHtmlContent GetEndClasses(this IOrchardHelper helper, bool withOffset, params string[] additionalClasses)
+    {
+        var options = helper.GetThemeOptions();
+
+        if (withOffset && !string.IsNullOrEmpty(options.OffsetClasses))
+        {
+            var cssClasses = new List<string>(additionalClasses)
+            {
+                options.OffsetClasses
+            };
+
+            return GetHtmlContentBuilder(options.EndClasses, cssClasses);
+        }
+
+        return GetHtmlContentBuilder(options.EndClasses, additionalClasses);
+    }
+
+    public static IHtmlContent GetOffsetClasses(this IOrchardHelper helper, params string[] additionalClasses)
+    {
+        var options = helper.GetThemeOptions();
+
+        return GetHtmlContentBuilder(options.OffsetClasses, additionalClasses);
+    }
+
+    [Obsolete($"Please use {nameof(GetLimitedWidthWrapperClasses)} instead, as this method is deprecated and will be removed in upcoming versions.")]
     public static string GetLimitedWidthWrapperCssClasses(this IOrchardHelper helper, params string[] additionalClasses)
     {
-        var options = GetThemeOptions(helper);
+        var options = helper.GetThemeOptions();
 
         return string.Join(' ', Combine(options.LimitedWidthWrapperClasses, additionalClasses));
     }
 
+    [Obsolete($"Please use {nameof(GetLimitedWidthClasses)} instead, as this method is deprecated and will be removed in upcoming versions.")]
     public static string GetLimitedWidthCssClasses(this IOrchardHelper helper, params string[] additionalClasses)
     {
-        var options = GetThemeOptions(helper);
+        var options = helper.GetThemeOptions();
 
         return string.Join(' ', Combine(options.LimitedWidthClasses, additionalClasses));
     }
 
+    [Obsolete($"Please use {nameof(GetLabelClasses)} instead, as this method is deprecated and will be removed in upcoming versions.")]
     public static string GetLabelCssClasses(this IOrchardHelper helper, params string[] additionalClasses)
     {
-        var options = GetThemeOptions(helper);
+        var options = helper.GetThemeOptions();
 
         return string.Join(' ', Combine(options.LabelClasses, additionalClasses));
     }
 
+    [Obsolete($"Please use {nameof(GetStartClasses)} instead, as this method is deprecated and will be removed in upcoming versions.")]
     public static string GetStartCssClasses(this IOrchardHelper helper, params string[] additionalClasses)
     {
-        var options = GetThemeOptions(helper);
+        var options = helper.GetThemeOptions();
 
         return string.Join(' ', Combine(options.StartClasses, additionalClasses));
     }
 
+    [Obsolete($"Please use {nameof(GetEndClasses)} instead, as this method is deprecated and will be removed in upcoming versions.")]
     public static string GetEndCssClasses(this IOrchardHelper helper, params string[] additionalClasses)
     {
-        var options = GetThemeOptions(helper);
+        var options = helper.GetThemeOptions();
 
         return string.Join(' ', Combine(options.EndClasses, additionalClasses));
     }
 
+    [Obsolete($"Please use {nameof(GetWrapperClasses)} instead, as this method is deprecated and will be removed in upcoming versions.")]
     public static string GetWrapperCssClasses(this IOrchardHelper helper, params string[] additionalClasses)
     {
-        var options = GetThemeOptions(helper);
+        var options = helper.GetThemeOptions();
 
         return string.Join(' ', Combine(options.WrapperClasses, additionalClasses));
     }
 
+    [Obsolete($"Please use {nameof(GetEndClasses)} instead, as this method is deprecated and will be removed in upcoming versions.")]
     public static string GetEndCssClasses(this IOrchardHelper helper, bool withOffset, params string[] additionalClasses)
     {
-        var options = GetThemeOptions(helper);
+        var options = helper.GetThemeOptions();
 
         if (withOffset && !string.IsNullOrEmpty(options.OffsetClasses))
         {
@@ -67,14 +143,15 @@ public static class CssOrchardHelperExtensions
         return string.Join(' ', Combine(options.EndClasses, additionalClasses.ToArray()));
     }
 
+    [Obsolete($"Please use {nameof(GetOffsetClasses)} instead, as this method is deprecated and will be removed in upcoming versions.")]
     public static string GetOffsetCssClasses(this IOrchardHelper helper, params string[] additionalClasses)
     {
-        var options = GetThemeOptions(helper);
+        var options = helper.GetThemeOptions();
 
         return string.Join(' ', Combine(options.OffsetClasses, additionalClasses));
     }
 
-    private static TheAdminThemeOptions GetThemeOptions(IOrchardHelper helper)
+    public static TheAdminThemeOptions GetThemeOptions(this IOrchardHelper helper)
         => helper.HttpContext.RequestServices.GetService<IOptions<TheAdminThemeOptions>>().Value;
 
     private static IEnumerable<string> Combine(string optionClasses, string[] additionalClasses)
@@ -85,5 +162,37 @@ public static class CssOrchardHelperExtensions
         }
 
         return additionalClasses.Concat(new[] { optionClasses });
+    }
+
+    private static HtmlContentBuilder GetHtmlContentBuilder(string optionClasses, IEnumerable<string> values)
+    {
+        var builder = new HtmlContentBuilder();
+
+        builder.Append(optionClasses);
+
+        if (values != null)
+        {
+            foreach (var value in values)
+            {
+                if (builder.Count == 0)
+                {
+                    // The 'Append' method performs a string.IsNullOrEmpty() check. So, as long as the builder has no entries, we append with no spaces.
+                    builder.Append(value);
+
+                    continue;
+                }
+
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    continue;
+                }
+
+                // At this point, we already know that the builder has at least one entry, so we append a single space to the class name.
+                // We pass create HtmlString here to prevent the builder from preforming string.IsNullOrWhiteSpace again for performance reason.
+                builder.AppendHtml(new HtmlString(' ' + value));
+            }
+        }
+
+        return builder;
     }
 }
