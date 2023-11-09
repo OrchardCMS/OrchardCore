3c71429efab4bad44101faf17756ca27a4ba3c4f
Mike Alhayek <mike@crestapps.com>
2023-11-09 15:55:29 -0800

    Cleanup

--- src/OrchardCore.Modules/OrchardCore.Forms/Activities/HttpRedirectToFormLocationTask.cs
+++ src/OrchardCore.Modules/OrchardCore.Forms/Activities/HttpRedirectToFormLocationTask.cs
@@ -1,46 +1,34 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.Extensions.Localization;
 using OrchardCore.Workflows;
 using OrchardCore.Workflows.Abstractions.Models;
 using OrchardCore.Workflows.Activities;
 using OrchardCore.Workflows.Models;
-using OrchardCore.Workflows.Services;
 
 namespace OrchardCore.Forms.Activities;
 
-public class HttpRedirectToFormLocationTask : TaskActivity
+public class HttpRedirectToFormLocationTask : TaskActivity<HttpRedirectToFormLocationTask>
 {
     private readonly IHttpContextAccessor _httpContextAccessor;
-    private readonly IActionContextAccessor _actionContextAccessor;
-    private readonly IWorkflowExpressionEvaluator _workflowExpressionEvaluator;
     protected readonly IStringLocalizer S;
 
     public HttpRedirectToFormLocationTask(
-        IStringLocalizer<HttpRedirectToFormLocationTask> localizer,
-        IActionContextAccessor actionContextAccessor,
-        IWorkflowExpressionEvaluator workflowExpressionEvaluator,
-        IHttpContextAccessor httpContextAccessor
+        IHttpContextAccessor httpContextAccessor,
+        IStringLocalizer<HttpRedirectToFormLocationTask> stringLocalizer
     )
     {
-        S = localizer;
-        _actionContextAccessor = actionContextAccessor;
-        _workflowExpressionEvaluator = workflowExpressionEvaluator;
         _httpContextAccessor = httpContextAccessor;
+        S = stringLocalizer;
     }
-
-    public override string Name => nameof(HttpRedirectToFormLocationTask);
 
     public override LocalizedString DisplayText => S["Http Redirect To Form Location Task"];
 
     public override LocalizedString Category => S["HTTP"];
 
     public override IEnumerable<Outcome> GetPossibleOutcomes(WorkflowExecutionContext workflowContext, ActivityContext activityContext)
-    {
-        return Outcomes(S["Done"], S["Failed"]);
-    }
+        => Outcomes(S["Done"], S["Failed"]);
 
     public string FormLocationKey
     {
@@ -50,7 +38,8 @@ public class HttpRedirectToFormLocationTask : TaskActivity
 
     public override Task<ActivityExecutionResult> ExecuteAsync(WorkflowExecutionContext workflowContext, ActivityContext activityContext)
     {
-        if (workflowContext.Output.TryGetValue(WorkflowConstants.HttpFormLocationOutputKeyName, out var obj) && obj is Dictionary<string, string> formLocations)
+        if (workflowContext.Output.TryGetValue(WorkflowConstants.HttpFormLocationOutputKeyName, out var obj)
+            && obj is Dictionary<string, string> formLocations)
         {
             // if no custom location-key was provided, we use empty string as the default key.
             var location = FormLocationKey ?? string.Empty;
