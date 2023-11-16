d968d418938f5c90d1e08b72ad5e2976e8ab13d4
Jean-Thierry Kéchichian <jean-thierry.kechichian@wanadoo.fr>
2023-11-16 22:11:04 +0100

    Update src/OrchardCore.Modules/OrchardCore.Deployment/Models/FileDownloadDeploymentTargetSettings.cs
    Co-authored-by: Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>

--- src/OrchardCore.Modules/OrchardCore.Deployment/Models/FileDownloadDeploymentTargetSettings.cs
+++ src/OrchardCore.Modules/OrchardCore.Deployment/Models/FileDownloadDeploymentTargetSettings.cs
@@ -3,12 +3,12 @@ namespace OrchardCore.Deployment.Models
     public class FileDownloadDeploymentTargetSettings
     {
         /// <summary>
-        /// The name of the rsa secret that can be used to encrypt exported data.
+        /// The name of the RSA secret that can be used to encrypt exported data.
         /// </summary>
         public string RsaEncryptionSecret { get; set; }
 
         /// <summary>
-        /// The name of the rsa secret that can be used to sign encrypted exported data.
+        /// The name of the RSA secret that can be used to sign encrypted exported data.
         /// </summary>
         public string RsaSigningSecret { get; set; }
     }
