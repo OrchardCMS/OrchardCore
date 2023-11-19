81d6499b73af028e193f1e68fe2d121e37330694
jtkech <jean-thierry.kechichian@wanadoo.fr>
2023-11-19 04:49:13 +0100

    Tweak on type discriminator for backward compatibility

--- src/OrchardCore/OrchardCore.Abstractions/Json/Serialization/JsonDerivedTypeInfo.cs
+++ src/OrchardCore/OrchardCore.Abstractions/Json/Serialization/JsonDerivedTypeInfo.cs
@@ -5,6 +5,9 @@ namespace System.Text.Json.Serialization;
 public class JsonDerivedTypeInfo<TDerived, TBase> : IJsonDerivedTypeInfo
         where TDerived : class where TBase : class
 {
-    public JsonDerivedType DerivedType => new(typeof(TDerived), typeof(TDerived).FullName);
+    public JsonDerivedType DerivedType => new(
+        typeof(TDerived),
+        $"{typeof(TDerived).FullName}, {typeof(TDerived).Assembly.GetName().Name}");
+
     public Type BaseType => typeof(TBase);
 }
