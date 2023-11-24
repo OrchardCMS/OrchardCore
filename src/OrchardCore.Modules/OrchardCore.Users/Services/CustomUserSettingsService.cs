4e13b339f6b48277da589de8262bde3b9b6b0424
jtkech <jean-thierry.kechichian@wanadoo.fr>
2023-11-24 04:25:56 +0100

    Minor change

--- src/OrchardCore.Modules/OrchardCore.Users/Services/CustomUserSettingsService.cs
+++ src/OrchardCore.Modules/OrchardCore.Users/Services/CustomUserSettingsService.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Newtonsoft.Json.Linq;
 using OrchardCore.ContentManagement;
 using OrchardCore.ContentManagement.Metadata;
 using OrchardCore.ContentManagement.Metadata.Models;
