cfc1327b16eafb4aa2ebc623e00f483f63cab802
Mike Alhayek <mike@crestapps.com>
2023-11-26 09:35:59 -0800

    Cleanup

--- src/OrchardCore.Modules/OrchardCore.DataProtection.Azure/Startup.cs
+++ src/OrchardCore.Modules/OrchardCore.DataProtection.Azure/Startup.cs
@@ -6,51 +6,51 @@ using Microsoft.Extensions.Logging;
 using OrchardCore.Environment.Shell.Configuration;
 using OrchardCore.Modules;
 
-namespace OrchardCore.DataProtection.Azure
+namespace OrchardCore.DataProtection.Azure;
+
+public class Startup : StartupBase
 {
-    public class Startup : StartupBase
+    private readonly IShellConfiguration _configuration;
+    private readonly ILogger _logger;
+
+    public Startup(IShellConfiguration configuration, ILogger<Startup> logger)
     {
-        private readonly IShellConfiguration _configuration;
-        private readonly ILogger _logger;
+        _configuration = configuration;
+        _logger = logger;
+    }
 
-        public Startup(IShellConfiguration configuration, ILogger<Startup> logger)
+    // Assume that this module will override default configuration, so set the Order to a value above the default.
+    public override int Order => 10;
+
+    public override void ConfigureServices(IServiceCollection services)
+    {
+        var connectionString = _configuration.GetValue<string>("OrchardCore_DataProtection_Azure:ConnectionString");
+
+        if (!string.IsNullOrWhiteSpace(connectionString))
         {
-            _configuration = configuration;
-            _logger = logger;
-        }
-
-        public override void ConfigureServices(IServiceCollection services)
-        {
-            var connectionString = _configuration.GetValue<string>("OrchardCore_DataProtection_Azure:ConnectionString");
-            if (!string.IsNullOrWhiteSpace(connectionString))
-            {
-                services
-                    .AddSingleton(new BlobOptions())
-                    .AddTransient<BlobOptionsSetup>()
-                    .AddDataProtection()
-                    .PersistKeysToAzureBlobStorage(sp =>
-                    {
-                        var options = sp.GetRequiredService<BlobOptions>();
-                        return new BlobClient(
-                            options.ConnectionString,
-                            options.ContainerName,
-                            options.BlobName);
-                    });
-
-                services.Initialize(async sp =>
+            services
+                .AddSingleton(new BlobOptions())
+                .AddTransient<BlobOptionsSetup>()
+                .AddDataProtection()
+                .PersistKeysToAzureBlobStorage(sp =>
                 {
                     var options = sp.GetRequiredService<BlobOptions>();
-                    var setup = sp.GetRequiredService<BlobOptionsSetup>();
-                    await setup.ConfigureAsync(options);
+                    return new BlobClient(
+                        options.ConnectionString,
+                        options.ContainerName,
+                        options.BlobName);
                 });
-            }
-            else
-            {
-                _logger.LogCritical("No connection string was supplied for OrchardCore.DataProtection.Azure. Ensure that an application setting containing a valid Azure Storage connection string is available at `Modules:OrchardCore.DataProtection.Azure:ConnectionString`.");
-            }
-        }
 
-        // Assume that this module will override default configuration, so set the Order to a value above the default.
-        public override int Order => 10;
+            services.Initialize(async sp =>
+            {
+                var options = sp.GetRequiredService<BlobOptions>();
+                var setup = sp.GetRequiredService<BlobOptionsSetup>();
+                await setup.ConfigureAsync(options);
+            });
+        }
+        else
+        {
+            _logger.LogCritical("No connection string was supplied for OrchardCore.DataProtection.Azure. Ensure that an application setting containing a valid Azure Storage connection string is available at `Modules:OrchardCore.DataProtection.Azure:ConnectionString`.");
+        }
     }
 }
