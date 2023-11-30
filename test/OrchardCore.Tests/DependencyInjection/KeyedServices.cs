3aa3e89f3d3928e8b36abe6330cdda373f7e61c5
Mike Alhayek <mike@crestapps.com>
2023-11-30 11:40:31 -0800

    Add test cases

---
+++ test/OrchardCore.Tests/DependencyInjection/KeyedServices.cs
@@ -0,0 +1,54 @@
+namespace OrchardCore.Tests.DependencyInjection;
+
+public class KeyedServices
+{
+    [Fact]
+    public void AbleToResolveKeyedServiceAsDictionary()
+    {
+        var services = new ServiceCollection();
+
+        services.AddOrchardCore();
+
+        services.AddKeyedTransient<IService, Service1>("a");
+        services.AddKeyedTransient<IService, Service2>("b");
+        services.AddKeyedTransient<IService, Service3>("c");
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        var keyService = serviceProvider.GetService<IDictionary<string, IService>>();
+
+        Assert.NotNull(keyService);
+        Assert.Equal(3, keyService.Count);
+        Assert.True(keyService.ContainsKey("a"));
+        Assert.True(keyService.ContainsKey("b"));
+        Assert.True(keyService.ContainsKey("c"));
+    }
+
+    [Fact]
+    public void AbleToResolveKeyedServiceAsReadOnlyDictionary()
+    {
+        var services = new ServiceCollection();
+
+        services.AddOrchardCore();
+
+        services.AddKeyedTransient<IService, Service1>("a");
+        services.AddKeyedTransient<IService, Service2>("b");
+        services.AddKeyedTransient<IService, Service3>("c");
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        var keyService = serviceProvider.GetService<IReadOnlyDictionary<string, IService>>();
+
+        Assert.NotNull(keyService);
+        Assert.Equal(3, keyService.Count);
+        Assert.True(keyService.ContainsKey("a"));
+        Assert.True(keyService.ContainsKey("b"));
+        Assert.True(keyService.ContainsKey("c"));
+    }
+}
+
+public class IService { }
+
+public class Service1 : IService { }
+public class Service2 : IService { }
+public class Service3 : IService { }
