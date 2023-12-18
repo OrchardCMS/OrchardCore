1665cd33b1e8325ea3550b560264a40c3c04114f
ggkoning <koninggido@gmail.com>
2023-12-18 16:36:29 +0100

    Add comment

--- src/OrchardCore.Modules/OrchardCore.Lists/GraphQL/ListQueryObjectType.cs
+++ src/OrchardCore.Modules/OrchardCore.Lists/GraphQL/ListQueryObjectType.cs
@@ -29,6 +29,7 @@ namespace OrchardCore.Lists.GraphQL
                 .Description("the content items")
                 .Argument<IntGraphType, int>("first", "the first n elements (10 by default)", 10)
                 .Argument<IntGraphType, int>("skip", "the number of elements to skip", 0)
+                // Important to use ResolveLockedAsync to prevent concurrency error on database query, when using nested content items with List part
                 .ResolveLockedAsync(async g =>
                 {
                     var serviceProvider = g.RequestServices;
