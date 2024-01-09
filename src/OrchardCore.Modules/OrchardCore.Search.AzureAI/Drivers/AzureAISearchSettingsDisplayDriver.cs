71bdec23c314aea6312aedd1ff40d32d4f028105
Mike Alhayek <mike@crestapps.com>
2024-01-09 12:30:04 -0800

    fix settings

--- src/OrchardCore.Modules/OrchardCore.Search.AzureAI/Drivers/AzureAISearchSettingsDisplayDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.Search.AzureAI/Drivers/AzureAISearchSettingsDisplayDriver.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Localization;
 using OrchardCore.DisplayManagement.Entities;
 using OrchardCore.DisplayManagement.Handlers;
 using OrchardCore.DisplayManagement.Views;
+using OrchardCore.Environment.Shell;
 using OrchardCore.Modules;
 using OrchardCore.Mvc.ModelBinding;
 using OrchardCore.Search.AzureAI.Models;
@@ -24,19 +25,24 @@ public class AzureAISearchSettingsDisplayDriver : SectionDisplayDriver<ISite, Az
     private readonly AzureAISearchIndexSettingsService _indexSettingsService;
     private readonly IHttpContextAccessor _httpContextAccessor;
     private readonly IAuthorizationService _authorizationService;
-
+    private readonly IShellHost _shellHost;
+    private readonly ShellSettings _shellSettings;
     protected readonly IStringLocalizer S;
 
     public AzureAISearchSettingsDisplayDriver(
         AzureAISearchIndexSettingsService indexSettingsService,
         IHttpContextAccessor httpContextAccessor,
         IAuthorizationService authorizationService,
+        IShellHost shellHost,
+        ShellSettings shellSettings,
         IStringLocalizer<AzureAISearchSettingsDisplayDriver> stringLocalizer
         )
     {
         _indexSettingsService = indexSettingsService;
         _httpContextAccessor = httpContextAccessor;
         _authorizationService = authorizationService;
+        _shellHost = shellHost;
+        _shellSettings = shellSettings;
         S = stringLocalizer;
     }
 
@@ -83,11 +89,38 @@ public class AzureAISearchSettingsDisplayDriver : SectionDisplayDriver<ISite, Az
                 }
             }
 
-            section.SearchIndex = model.SearchIndex;
-            section.DefaultSearchFields = model.SearchFields?.Split(_separator, StringSplitOptions.RemoveEmptyEntries);
+            var fields = model.SearchFields?.Split(_separator, StringSplitOptions.RemoveEmptyEntries);
+
+            if (section.SearchIndex != model.SearchIndex || !AreSame(section.DefaultSearchFields, fields))
+            {
+                section.SearchIndex = model.SearchIndex;
+                section.DefaultSearchFields = fields;
+
+                if (context.Updater.ModelState.IsValid)
+                {
+                    await _shellHost.ReleaseShellContextAsync(_shellSettings);
+                }
+            }
         }
 
-        return await EditAsync(section, context);
+        return Edit(section);
+    }
+
+    private static bool AreSame(string[] a, string[] b)
+    {
+        if (a == null && b == null)
+        {
+            return false;
+        }
+
+        if ((a is null && b is not null) || (a is not null && b is null))
+        {
+            return true;
+        }
+
+        var combine = a.Intersect(b).ToList();
+
+        return combine.Count == a.Length && combine.Count == b.Length;
     }
 
     protected override void BuildPrefix(ISite model, string htmlFieldPrefix)
