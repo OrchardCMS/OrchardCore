15e3a776873c9572077a68bc4939cb34b94501a0
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2024-01-12 20:43:24 +0300

    Add TimeZones to UserTimeZoneViewModel

--- src/OrchardCore.Modules/OrchardCore.Users/TimeZone/Drivers/UserTimeZoneDisplayDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.Users/TimeZone/Drivers/UserTimeZoneDisplayDriver.cs
@@ -1,8 +1,11 @@
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using OrchardCore.DisplayManagement.Entities;
 using OrchardCore.DisplayManagement.Handlers;
 using OrchardCore.DisplayManagement.ModelBinding;
 using OrchardCore.DisplayManagement.Views;
+using OrchardCore.Modules;
 using OrchardCore.Settings;
 using OrchardCore.Users.Models;
 using OrchardCore.Users.TimeZone.Models;
@@ -15,11 +18,13 @@ public class UserTimeZoneDisplayDriver : SectionDisplayDriver<User, UserTimeZone
 {
     private readonly UserTimeZoneService _userTimeZoneService;
     private readonly ISiteService _siteService;
+    private readonly IClock _clock;
 
-    public UserTimeZoneDisplayDriver(UserTimeZoneService userTimeZoneService, ISiteService siteService)
+    public UserTimeZoneDisplayDriver(UserTimeZoneService userTimeZoneService, ISiteService siteService, IClock clock)
     {
         _userTimeZoneService = userTimeZoneService;
         _siteService = siteService;
+        _clock = clock;
     }
 
     public override IDisplayResult Edit(UserTimeZone userTimeZone, BuildEditorContext context)
@@ -35,6 +40,13 @@ public class UserTimeZoneDisplayDriver : SectionDisplayDriver<User, UserTimeZone
             }
 
             model.TimeZoneId = timeZoneId;
+            model.TimeZones = _clock.GetTimeZones().Select(tz =>
+            new SelectListItem
+            {
+                Text = tz.ToString(),
+                Value = tz.TimeZoneId,
+                Selected = tz.TimeZoneId == timeZoneId
+            });
         }).Location("Content:3");
     }
 
--- src/OrchardCore.Modules/OrchardCore.Users/TimeZone/ViewModel/UserTimeZoneViewModel.cs
+++ src/OrchardCore.Modules/OrchardCore.Users/TimeZone/ViewModel/UserTimeZoneViewModel.cs
@@ -1,6 +1,13 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 namespace OrchardCore.Users.TimeZone.ViewModels;
 
 public class UserTimeZoneViewModel
 {
     public string TimeZoneId { get; set; }
+
+    [BindNever]
+    public IEnumerable<SelectListItem> TimeZones { get; set; } = new List<SelectListItem>();
 }
