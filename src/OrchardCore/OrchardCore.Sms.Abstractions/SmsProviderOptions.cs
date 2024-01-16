b1771061af0e1a2657f82d4c64d58b4e350105d9
Mike Alhayek <mike@crestapps.com>
2024-01-16 08:30:36 -0800

    Add ISmsService and support multiple SMS Providers (#14774)

--- src/OrchardCore/OrchardCore.Sms.Abstractions/SmsProviderOptions.cs
+++ src/OrchardCore/OrchardCore.Sms.Abstractions/SmsProviderOptions.cs
@@ -6,26 +6,26 @@ namespace OrchardCore.Sms;
 
 public class SmsProviderOptions
 {
-    private Dictionary<string, Type> _providers { get; } = [];
+    private Dictionary<string, SmsProviderTypeOptions> _providers { get; } = [];
 
-    private FrozenDictionary<string, Type> _readonlyProviders;
+    private FrozenDictionary<string, SmsProviderTypeOptions> _readonlyProviders;
 
     /// <summary>
     /// This read-only collections contains all registered SMS providers.
     /// The 'Key' is the technical name of the provider.
     /// The 'Value' is the type of the SMS provider. The type will always be an implementation of <see cref="ISmsProvider"></see> interface.
     /// </summary>
-    public IReadOnlyDictionary<string, Type> Providers
+    public IReadOnlyDictionary<string, SmsProviderTypeOptions> Providers
         => _readonlyProviders ??= _providers.ToFrozenDictionary(x => x.Key, x => x.Value);
 
     /// <summary>
     /// Adds a provider if one does not exist.
     /// </summary>
     /// <param name="name">The technical name of the provider.</param>
-    /// <param name="type">The type of the provider.</param>
+    /// <param name="options">The type options of the provider.</param>
     /// <returns cref="SmsProviderOptions"></returns>
     /// <exception cref="ArgumentException"></exception>
-    public SmsProviderOptions TryAddProvider(string name, Type type)
+    public SmsProviderOptions TryAddProvider(string name, SmsProviderTypeOptions options)
     {
         if (string.IsNullOrEmpty(name))
         {
@@ -37,12 +37,7 @@ public class SmsProviderOptions
             return this;
         }
 
-        if (!typeof(ISmsProvider).IsAssignableFrom(type))
-        {
-            throw new ArgumentException($"The type must implement the '{nameof(ISmsProvider)}' interface.");
-        }
-
-        _providers.Add(name, type);
+        _providers.Add(name, options);
         _readonlyProviders = null;
 
         return this;
@@ -73,13 +68,30 @@ public class SmsProviderOptions
     /// Replaces existing or adds a new provider.
     /// </summary>
     /// <param name="name">The technical name of the provider.</param>
-    /// <param name="type">The type of the provider.</param>
+    /// <param name="options">The type-options of the provider.</param>
     /// <returns cref="SmsProviderOptions"></returns>
     /// <exception cref="ArgumentException"></exception>
-    public SmsProviderOptions ReplaceProvider(string name, Type type)
+    public SmsProviderOptions ReplaceProvider(string name, SmsProviderTypeOptions options)
     {
         _providers.Remove(name);
 
-        return TryAddProvider(name, type);
+        return TryAddProvider(name, options);
     }
+}
+
+public class SmsProviderTypeOptions
+{
+    public Type Type { get; }
+
+    public SmsProviderTypeOptions(Type type)
+    {
+        if (!typeof(ISmsProvider).IsAssignableFrom(type))
+        {
+            throw new ArgumentException($"The type must implement the '{nameof(ISmsProvider)}' interface.");
+        }
+
+        Type = type;
+    }
+
+    public bool IsEnabled { get; set; }
 }
