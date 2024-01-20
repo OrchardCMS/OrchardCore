7f6b5a0684364f874269886d4579d0c44deac975
Magnus Pettersson <magnus.pettersson@micropos.nu>
2024-01-20 18:44:47 +0100

    Sqllite DatabaseName was not read from appsettings

--- src/OrchardCore/OrchardCore.Data.YesSql/Options/SqliteOptionsConfiguration.cs
+++ src/OrchardCore/OrchardCore.Data.YesSql/Options/SqliteOptionsConfiguration.cs
@@ -6,7 +6,6 @@ namespace OrchardCore.Data
 {
     public class SqliteOptionsConfiguration : IConfigureOptions<SqliteOptions>
     {
-        private static readonly bool _defaultUseConnectionPooling = true;
         private readonly IShellConfiguration _shellConfiguration;
 
         public SqliteOptionsConfiguration(IShellConfiguration shellConfiguration)
@@ -18,7 +17,8 @@ namespace OrchardCore.Data
         {
             var section = _shellConfiguration.GetSection("OrchardCore_Data_Sqlite");
 
-            options.UseConnectionPooling = section.GetValue(nameof(options.UseConnectionPooling), _defaultUseConnectionPooling);
+            options.DatabaseName = section.GetValue(nameof(options.DatabaseName), options.DatabaseName);
+            options.UseConnectionPooling = section.GetValue(nameof(options.UseConnectionPooling), options.UseConnectionPooling);
         }
     }
 }
