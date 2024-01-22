61c0ca7f2bfc5f9ef4a1c8a5fc37a70d9920d372
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2024-01-22 12:12:47 +0300

    Revert HttpClient changes

--- test/OrchardCore.Tests/Modules/OrchardCore.Resources/SubResourceIntegrityTests.cs
+++ test/OrchardCore.Tests/Modules/OrchardCore.Resources/SubResourceIntegrityTests.cs
@@ -2,73 +2,73 @@ using System.Security.Cryptography;
 using OrchardCore.ResourceManagement;
 using OrchardCore.Resources;
 
-namespace OrchardCore.Tests.Modules.OrchardCore.Resources
+namespace OrchardCore.Tests.Modules.OrchardCore.Resources;
+
+public class SubResourceIntegrityTests
 {
-    public class SubResourceIntegrityTests
+    [Fact]
+    public async Task SavedSubResourceIntegritiesShouldMatchCurrentResources()
     {
-        private static readonly HttpClient _httpClient = new();
+        // Arrange
+        var resourceOptions = Options.Create(new ResourceOptions());
+        var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+        httpContextAccessorMock
+            .Setup(a => a.HttpContext)
+            .Returns(new DefaultHttpContext());
+        var configurationOptions = new ResourceManagementOptionsConfiguration(
+            resourceOptions,
+            Mock.Of<IHostEnvironment>(),
+            httpContextAccessorMock.Object);
+        var resourceManagementOptions = new ResourceManagementOptions();
 
-        [Fact]
-        public async Task SavedSubResourceIntegritiesShouldMatchCurrentResources()
+        // Act
+        configurationOptions.Configure(resourceManagementOptions);
+        
+        // Assert
+        var resourceManifest = resourceManagementOptions.ResourceManifests.First();
+
+        await ValidateSubResourceIntegrityAsync("script");
+        await ValidateSubResourceIntegrityAsync("style");
+
+        async Task ValidateSubResourceIntegrityAsync(string resourceType)
         {
-            // Arrange
-            var resourceOptions = Options.Create(new ResourceOptions());
-            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
-            httpContextAccessorMock
-                .Setup(a => a.HttpContext)
-                .Returns(new DefaultHttpContext());
-            var configurationOptions = new ResourceManagementOptionsConfiguration(
-                resourceOptions,
-                Mock.Of<IHostEnvironment>(),
-                httpContextAccessorMock.Object);
-            var resourceManagementOptions = new ResourceManagementOptions();
-
-            // Act
-            configurationOptions.Configure(resourceManagementOptions);
-            
-            // Assert
-            var resourceManifest = resourceManagementOptions.ResourceManifests.First();
-
-            await ValidateSubResourceIntegrityAsync("script");
-            await ValidateSubResourceIntegrityAsync("style");
-
-            async Task ValidateSubResourceIntegrityAsync(string resourceType)
+            foreach (var resource in resourceManifest.GetResources(resourceType))
             {
-                foreach (var resource in resourceManifest.GetResources(resourceType))
+                foreach (var resourceDefinition in resource.Value)
                 {
-                    foreach (var resourceDefinition in resource.Value)
+                    if (!string.IsNullOrEmpty(resourceDefinition.CdnIntegrity) && !string.IsNullOrEmpty(resourceDefinition.UrlCdnDebug))
                     {
-                        if (!string.IsNullOrEmpty(resourceDefinition.CdnIntegrity) && !string.IsNullOrEmpty(resourceDefinition.UrlCdnDebug))
-                        {
-                            var resourceIntegrity = await GetSubResourceIntegrityAsync(resourceDefinition.UrlCdnDebug);
+                        var resourceIntegrity = await GetSubResourceIntegrityAsync(resourceDefinition.UrlCdnDebug);
 
-                            Assert.True(resourceIntegrity.Equals(resourceDefinition.CdnDebugIntegrity),
-                                $"The {resourceType} {resourceDefinition.UrlCdnDebug} has invalid SRI hash, please use '{resourceIntegrity}' instead.");
-                        }
+                        Assert.True(resourceIntegrity.Equals(resourceDefinition.CdnDebugIntegrity),
+                            $"The {resourceType} {resourceDefinition.UrlCdnDebug} has invalid SRI hash, please use '{resourceIntegrity}' instead.");
+                    }
 
-                        if (!string.IsNullOrEmpty(resourceDefinition.CdnIntegrity) && !string.IsNullOrEmpty(resourceDefinition.UrlCdn))
-                        {
-                            var resourceIntegrity = await GetSubResourceIntegrityAsync(resourceDefinition.UrlCdn);
+                    if (!string.IsNullOrEmpty(resourceDefinition.CdnIntegrity) && !string.IsNullOrEmpty(resourceDefinition.UrlCdn))
+                    {
+                        var resourceIntegrity = await GetSubResourceIntegrityAsync(resourceDefinition.UrlCdn);
 
-                            Assert.True(resourceIntegrity.Equals(resourceDefinition.CdnIntegrity),
-                                $"The {resourceType} {resourceDefinition.UrlCdn} has invalid SRI hash, please use '{resourceIntegrity}' instead.");
-                        }
+                        Assert.True(resourceIntegrity.Equals(resourceDefinition.CdnIntegrity),
+                            $"The {resourceType} {resourceDefinition.UrlCdn} has invalid SRI hash, please use '{resourceIntegrity}' instead.");
                     }
                 }
             }
         }
+    }
 
-        private static async Task<string> GetSubResourceIntegrityAsync(string url)
+    private static async Task<string> GetSubResourceIntegrityAsync(string url)
+    {
+        var httpClient = new HttpClient
         {
-            _httpClient.BaseAddress = new Uri(url);
+            BaseAddress = new Uri(url)
+        };
 
-            var data = await _httpClient.GetByteArrayAsync(url);
+        var data = await httpClient.GetByteArrayAsync(url);
 
-            using var memoryStream = new MemoryStream(data);
-            using var sha384Hash = SHA384.Create();
-            var hash = sha384Hash.ComputeHash(memoryStream);
+        using var memoryStream = new MemoryStream(data);
+        using var sha384Hash = SHA384.Create();
+        var hash = sha384Hash.ComputeHash(memoryStream);
 
-            return "sha384-" + Convert.ToBase64String(hash);
-        }
+        return "sha384-" + Convert.ToBase64String(hash);
     }
 }
