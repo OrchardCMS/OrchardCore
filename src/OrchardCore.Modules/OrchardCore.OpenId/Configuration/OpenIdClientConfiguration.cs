5835d2a4be544124f9ce96c058f58173df55c829
Tony Han <huarui2219@qq.com>
2024-01-24 21:26:00 +0800

    Update src/OrchardCore.Modules/OrchardCore.OpenId/Configuration/OpenIdClientConfiguration.cs
    Co-authored-by: Kévin Chalet <kevinchalet@gmail.com>

--- src/OrchardCore.Modules/OrchardCore.OpenId/Configuration/OpenIdClientConfiguration.cs
+++ src/OrchardCore.Modules/OrchardCore.OpenId/Configuration/OpenIdClientConfiguration.cs
@@ -129,7 +129,7 @@ namespace OrchardCore.OpenId.Configuration
                         var errors = result.Where(x => x != ValidationResult.Success)
                             .Select(x => x.ErrorMessage);
 
-                        _logger.LogWarning("The Client Settings of OpenID Connect module is not correctly configured:{Error}", String.Join("\r\n;", errors));
+                        _logger.LogWarning("The OpenID client settings are invalid: {Errors}", string.Join("\r\n;", errors));
                     }
 
                     return null;
