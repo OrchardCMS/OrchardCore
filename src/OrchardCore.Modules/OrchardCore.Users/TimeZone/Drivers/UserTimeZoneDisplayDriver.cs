4c264018b620698231f389e65a68bd65382a41a1
Jasmin Savard <jasmin.savard@gmail.com>
2024-01-25 18:02:09 -0500

    User Timezone setting (Hisham refactor)

--- src/OrchardCore.Modules/OrchardCore.Users/TimeZone/Drivers/UserTimeZoneDisplayDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.Users/TimeZone/Drivers/UserTimeZoneDisplayDriver.cs
@@ -1,45 +1,67 @@
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using OrchardCore.DisplayManagement.Entities;
 using OrchardCore.DisplayManagement.Handlers;
 using OrchardCore.DisplayManagement.ModelBinding;
 using OrchardCore.DisplayManagement.Views;
+using OrchardCore.Modules;
+using OrchardCore.Settings;
 using OrchardCore.Users.Models;
 using OrchardCore.Users.TimeZone.Models;
 using OrchardCore.Users.TimeZone.Services;
 using OrchardCore.Users.TimeZone.ViewModels;
 
-namespace OrchardCore.Users.TimeZone.Drivers
+namespace OrchardCore.Users.TimeZone.Drivers;
+
+public class UserTimeZoneDisplayDriver : SectionDisplayDriver<User, UserTimeZone>
 {
-    public class UserTimeZoneDisplayDriver : SectionDisplayDriver<User, UserTimeZone>
+    private readonly UserTimeZoneService _userTimeZoneService;
+    private readonly ISiteService _siteService;
+    private readonly IClock _clock;
+
+    public UserTimeZoneDisplayDriver(UserTimeZoneService userTimeZoneService, ISiteService siteService, IClock clock)
     {
-        private readonly UserTimeZoneService _userTimeZoneService;
+        _userTimeZoneService = userTimeZoneService;
+        _siteService = siteService;
+        _clock = clock;
+    }
 
-        public UserTimeZoneDisplayDriver(UserTimeZoneService userTimeZoneService)
+    public override IDisplayResult Edit(UserTimeZone userTimeZone, BuildEditorContext context)
+    {
+        return Initialize<UserTimeZoneViewModel>("UserTimeZone_Edit", async model =>
         {
-            _userTimeZoneService = userTimeZoneService;
-        }
-
-        public override IDisplayResult Edit(UserTimeZone userTimeZone, BuildEditorContext context)
-        {
-            return Initialize<UserTimeZoneViewModel>("UserTimeZone_Edit", model =>
+            var timeZoneId = userTimeZone.TimeZoneId;
+            if (timeZoneId == null)
             {
-                model.TimeZoneId = userTimeZone.TimeZoneId;
-            }).Location("Content:2");
-        }
+                var siteSettings = await _siteService.GetSiteSettingsAsync();
 
-        public override async Task<IDisplayResult> UpdateAsync(User user, UserTimeZone userTimeZone, IUpdateModel updater, BuildEditorContext context)
-        {
-            var model = new UserTimeZoneViewModel();
-
-            if (await context.Updater.TryUpdateModelAsync(model, Prefix))
-            {
-                userTimeZone.TimeZoneId = model.TimeZoneId;
-
-                // Remove the cache entry, don't update it, as the form might still fail validation for other reasons.
-                await _userTimeZoneService.UpdateUserTimeZoneAsync(user);
+                timeZoneId = siteSettings.TimeZoneId;
             }
 
-            return await EditAsync(userTimeZone, context);
+            model.TimeZoneId = timeZoneId;
+            model.TimeZones = _clock.GetTimeZones().Select(tz =>
+                new SelectListItem
+                {
+                    Text = tz.ToString(),
+                    Value = tz.TimeZoneId,
+                    Selected = tz.TimeZoneId == timeZoneId
+                });
+        }).Location("Content:3");
+    }
+
+    public override async Task<IDisplayResult> UpdateAsync(User user, UserTimeZone userTimeZone, IUpdateModel updater, BuildEditorContext context)
+    {
+        var model = new UserTimeZoneViewModel();
+
+        if (await context.Updater.TryUpdateModelAsync(model, Prefix))
+        {
+            userTimeZone.TimeZoneId = model.TimeZoneId;
+
+            // Remove the cache entry, don't update it, as the form might still fail validation for other reasons.
+            await _userTimeZoneService.ClearUserTimeZoneCacheAsync(user);
         }
+
+        return await EditAsync(userTimeZone, context);
     }
 }
--- src/OrchardCore.Modules/OrchardCore.Users/TimeZone/ViewModel/UserTimeZoneViewModel.cs
+++ src/OrchardCore.Modules/OrchardCore.Users/TimeZone/ViewModel/UserTimeZoneViewModel.cs
@@ -1,7 +1,13 @@
-namespace OrchardCore.Users.TimeZone.ViewModels
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace OrchardCore.Users.TimeZone.ViewModels;
+
+public class UserTimeZoneViewModel
 {
-    public class UserTimeZoneViewModel
-    {
-        public string TimeZoneId { get; set; }
-    }
+    public string TimeZoneId { get; set; }
+
+    [BindNever]
+    public IEnumerable<SelectListItem> TimeZones { get; set; } = new List<SelectListItem>();
 }
