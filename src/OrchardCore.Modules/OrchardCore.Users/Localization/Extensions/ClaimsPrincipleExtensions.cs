3f484b477f250ab576c548f79e6f81fe3604f2ef
Jasmin Savard <jasmin.savard@gmail.com>
2024-01-25 00:11:23 -0500

    User Timezone settings refresh
    Originally from Hisham

--- src/OrchardCore.Modules/OrchardCore.Users/Localization/Extensions/ClaimsPrincipleExtensions.cs
+++
@@ -1,9 +0,0 @@
-using OrchardCore.Users.Localization.Providers;
-
-namespace System.Security.Claims;
-
-public static class ClaimsPrincipleExtensions
-{
-    public static string GetCulture(this ClaimsPrincipal principal)
-        => principal.FindFirstValue(UserLocalizationClaimsProvider.CultureClaimType);
-}
--- src/OrchardCore.Modules/OrchardCore.Users/Localization/Models/UserLocalizationSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace OrchardCore.Users.Localization.Models;
-
-/// <summary>
-/// Provides a model for the IEntity property.
-/// </summary>
-public class UserLocalizationSettings
-{
-    public string Culture { get; set; }
-}
