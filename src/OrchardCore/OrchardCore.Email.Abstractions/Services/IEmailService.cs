03d9aca38a5f2ac5cba105a08e2af5a2be84adbd
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2024-01-29 02:21:49 +0300

    Update src/OrchardCore/OrchardCore.Email.Abstractions/Services/IEmailService.cs
    Co-authored-by: Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>

--- src/OrchardCore/OrchardCore.Email.Abstractions/Services/IEmailService.cs
+++ src/OrchardCore/OrchardCore.Email.Abstractions/Services/IEmailService.cs
@@ -8,11 +8,10 @@ namespace OrchardCore.Email.Services;
 public interface IEmailService
 {
     /// <summary>
-    /// Sends the specified message to email server for delivery.
+    /// Sends the specified message in email.
     /// </summary>
-    /// <param name="message">The message to be sent.</param>
-    /// <param name="deliveryMethodName">The delivery method name to be used to send the email. If no method specified the last <see cref="IEmailDeliveryService"/> will be used to deliver the email.</param>
-    /// <returns>A <see cref="EmailResult"/> that holds information about the sent message, for instance, if it was sent successfully or if it has failed.</returns>
-    /// <remarks>By default we are using <see cref="EmailDeliveryServiceName"/> to choose the email delivery service name.</remarks>
+    /// <param name="message">The message to be sent in email.</param>
+    /// <param name="deliveryServiceName">The name of the delivery service to send the email. If no name is specified then `IEmailDeliveryServiceResolver` will select the last registered one.</param>
+    /// <returns>An <see cref="EmailResult"/> that holds information about the message sent, for instance, if it was sent successfully or if it has failed.</returns>
     Task<EmailResult> SendAsync(MailMessage message, string deliveryMethodName = null);
 }
