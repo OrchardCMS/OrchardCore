a2e09e2fdef9b1da0fee89e75fc9a61afa4c6427
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2024-01-29 03:03:35 +0300

    Log warning in NullEmailDeliveryService

--- src/OrchardCore/OrchardCore.Email.Core/Services/NullEmailDeliveryService.cs
+++ src/OrchardCore/OrchardCore.Email.Core/Services/NullEmailDeliveryService.cs
@@ -1,8 +1,21 @@
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace OrchardCore.Email.Services;
 
 public class NullEmailDeliveryService : IEmailDeliveryService
 {
-    public Task<EmailResult> DeliverAsync(MailMessage message) => Task.FromResult(EmailResult.Success);
+    private readonly ILogger _logger;
+
+    public NullEmailDeliveryService(ILogger<NullEmailDeliveryService> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<EmailResult> DeliverAsync(MailMessage message)
+    {
+        _logger.LogWarning("No email delivery service is configured. Please enable an actual implementation so the email can send.");
+
+        return await Task.FromResult(EmailResult.Success);
+    }
 }
