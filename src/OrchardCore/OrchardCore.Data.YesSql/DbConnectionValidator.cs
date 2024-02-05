0cdbc84b11dc14bfe8ace569923b3b19f7f06e63
Jasmin Savard <jasmin.savard@gmail.com>
2024-02-05 10:32:19 -0500

    Pas it through the DbConnectionValidator context

--- src/OrchardCore/OrchardCore.Data.YesSql/DbConnectionValidator.cs
+++ src/OrchardCore/OrchardCore.Data.YesSql/DbConnectionValidator.cs
@@ -32,13 +32,11 @@ public class DbConnectionValidator : IDbConnectionValidator
     private readonly ILogger _logger;
     private readonly SqliteOptions _sqliteOptions;
     private readonly ShellOptions _shellOptions;
-    private readonly ShellSettings _shellSettings;
 
     public DbConnectionValidator(
         IEnumerable<DatabaseProvider> databaseProviders,
         IOptions<SqliteOptions> sqliteOptions,
         IOptions<ShellOptions> shellOptions,
-        ShellSettings shellSettings,
         ITableNameConventionFactory tableNameConventionFactory,
         ILogger<DbConnectionValidator> logger)
     {
@@ -47,7 +45,6 @@ public class DbConnectionValidator : IDbConnectionValidator
         _logger = logger;
         _sqliteOptions = sqliteOptions.Value;
         _shellOptions = shellOptions.Value;
-        _shellSettings = shellSettings;
     }
 
     public async Task<DbConnectionValidatorResult> ValidateAsync(DbConnectionValidatorContext context)
@@ -73,7 +70,7 @@ public class DbConnectionValidator : IDbConnectionValidator
                 return DbConnectionValidatorResult.DocumentTableNotFound;
             }
 
-            connectionString = SqliteHelper.GetConnectionString(_sqliteOptions, _shellOptions, _shellSettings);
+            connectionString = SqliteHelper.GetConnectionString(_sqliteOptions, _shellOptions, context.ShellSettings);
         }
 
         if (string.IsNullOrWhiteSpace(connectionString))
