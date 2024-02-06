1b5e5fe172c0cf992af2d25d0217d74e42c3c8b9
Sára El-Saig <sara.el-saig@lombiq.com>
2024-02-07 00:43:50 +0100

    Update OrchardCore.Menu.

--- src/OrchardCore.Modules/OrchardCore.Menu/Startup.cs
+++ src/OrchardCore.Modules/OrchardCore.Menu/Startup.cs
@@ -1,23 +1,16 @@
-using System;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
-using OrchardCore.Admin;
 using OrchardCore.ContentManagement;
 using OrchardCore.ContentManagement.Display.ContentDisplay;
 using OrchardCore.ContentManagement.Handlers;
 using OrchardCore.ContentTypes.Editors;
 using OrchardCore.Data.Migration;
 using OrchardCore.DisplayManagement.Descriptors;
-using OrchardCore.Menu.Controllers;
 using OrchardCore.Menu.Drivers;
 using OrchardCore.Menu.Handlers;
 using OrchardCore.Menu.Models;
 using OrchardCore.Menu.Settings;
 using OrchardCore.Menu.TagHelpers;
 using OrchardCore.Modules;
-using OrchardCore.Mvc.Core.Utilities;
 using OrchardCore.Navigation;
 using OrchardCore.Security.Permissions;
 
@@ -25,13 +18,6 @@ namespace OrchardCore.Menu
 {
     public class Startup : StartupBase
     {
-        private readonly AdminOptions _adminOptions;
-
-        public Startup(IOptions<AdminOptions> adminOptions)
-        {
-            _adminOptions = adminOptions.Value;
-        }
-
         public override void ConfigureServices(IServiceCollection services)
         {
             services.AddDataMigration<Migrations>();
@@ -60,30 +46,6 @@ namespace OrchardCore.Menu
             services.AddScoped<IContentTypePartDefinitionDisplayDriver, HtmlMenuItemPartSettingsDisplayDriver>();
 
             services.AddTagHelpers<MenuTagHelper>();
-        }
-
-        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
-        {
-            var adminControllerName = typeof(AdminController).ControllerName();
-
-            routes.MapAreaControllerRoute(
-                name: "MenuCreate",
-                areaName: "OrchardCore.Menu",
-                pattern: _adminOptions.AdminUrlPrefix + "/Menu/Create/{id}",
-                defaults: new { controller = adminControllerName, action = nameof(AdminController.Create) }
-            );
-            routes.MapAreaControllerRoute(
-                name: "MenuDelete",
-                areaName: "OrchardCore.Menu",
-                pattern: _adminOptions.AdminUrlPrefix + "/Menu/Delete",
-                defaults: new { controller = adminControllerName, action = nameof(AdminController.Delete) }
-            );
-            routes.MapAreaControllerRoute(
-                name: "MenuEdit",
-                areaName: "OrchardCore.Menu",
-                pattern: _adminOptions.AdminUrlPrefix + "/Menu/Edit",
-                defaults: new { controller = adminControllerName, action = nameof(AdminController.Edit) }
-            );
         }
     }
 }
