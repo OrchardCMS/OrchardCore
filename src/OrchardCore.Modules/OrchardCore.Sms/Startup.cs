ba0642e5ee2896bd9acdf34a918916ebbb5bc664
Sára El-Saig <sara.el-saig@lombiq.com>
2024-02-07 01:55:10 +0100

    Update OrchardCore.Sms.

--- src/OrchardCore.Modules/OrchardCore.Sms/Startup.cs
+++ src/OrchardCore.Modules/OrchardCore.Sms/Startup.cs
@@ -1,19 +1,12 @@
-using System;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
-using OrchardCore.Admin;
 using OrchardCore.DisplayManagement.Handlers;
 using OrchardCore.Modules;
-using OrchardCore.Mvc.Core.Utilities;
 using OrchardCore.Navigation;
 using OrchardCore.Notifications;
 using OrchardCore.Security.Permissions;
 using OrchardCore.Settings;
 using OrchardCore.Sms.Activities;
-using OrchardCore.Sms.Controllers;
 using OrchardCore.Sms.Drivers;
 using OrchardCore.Sms.Services;
 using OrchardCore.Workflows.Helpers;
@@ -23,14 +16,10 @@ namespace OrchardCore.Sms;
 public class Startup : StartupBase
 {
     private readonly IHostEnvironment _hostEnvironment;
-    private readonly AdminOptions _adminOptions;
 
-    public Startup(
-        IHostEnvironment hostEnvironment,
-        IOptions<AdminOptions> adminOptions)
+    public Startup(IHostEnvironment hostEnvironment)
     {
         _hostEnvironment = hostEnvironment;
-        _adminOptions = adminOptions.Value;
     }
 
     public override void ConfigureServices(IServiceCollection services)
@@ -49,16 +38,6 @@ public class Startup : StartupBase
         services.AddScoped<IPermissionProvider, SmsPermissionProvider>();
         services.AddScoped<INavigationProvider, AdminMenu>();
         services.AddScoped<IDisplayDriver<ISite>, SmsSettingsDisplayDriver>();
-    }
-
-    public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
-    {
-        routes.MapAreaControllerRoute(
-            name: "SmsProviderTest",
-            areaName: "OrchardCore.Sms",
-            pattern: _adminOptions.AdminUrlPrefix + "/sms/test",
-            defaults: new { controller = typeof(AdminController).ControllerName(), action = nameof(AdminController.Test) }
-        );
     }
 }
 
