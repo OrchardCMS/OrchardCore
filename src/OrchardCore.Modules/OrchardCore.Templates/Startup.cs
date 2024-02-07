20da3ca83e728945f042b37b7ad6f29c7adb70f4
Sára El-Saig <sara.el-saig@lombiq.com>
2024-02-07 03:56:40 +0100

    Update OrchardCore.Templates.

--- src/OrchardCore.Modules/OrchardCore.Templates/Startup.cs
+++ src/OrchardCore.Modules/OrchardCore.Templates/Startup.cs
@@ -1,19 +1,13 @@
-using System;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
-using OrchardCore.Admin;
 using OrchardCore.ContentTypes.Editors;
 using OrchardCore.Deployment;
 using OrchardCore.DisplayManagement;
 using OrchardCore.Modules;
-using OrchardCore.Mvc.Core.Utilities;
 using OrchardCore.Navigation;
 using OrchardCore.Recipes;
 using OrchardCore.ResourceManagement;
 using OrchardCore.Security.Permissions;
-using OrchardCore.Templates.Controllers;
 using OrchardCore.Templates.Deployment;
 using OrchardCore.Templates.Recipes;
 using OrchardCore.Templates.Services;
@@ -23,13 +17,6 @@ namespace OrchardCore.Templates
 {
     public class Startup : StartupBase
     {
-        private readonly AdminOptions _adminOptions;
-
-        public Startup(IOptions<AdminOptions> adminOptions)
-        {
-            _adminOptions = adminOptions.Value;
-        }
-
         public override void ConfigureServices(IServiceCollection services)
         {
             services.AddTransient<IConfigureOptions<ResourceManagementOptions>, ResourceManagementOptionsConfiguration>();
@@ -51,51 +38,11 @@ namespace OrchardCore.Templates
             services.AddScoped<AdminTemplatesManager>();
             services.AddScoped<IPermissionProvider, AdminTemplatesPermissions>();
         }
-
-        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
-        {
-            var templateControllerName = typeof(TemplateController).ControllerName();
-
-            routes.MapAreaControllerRoute(
-                name: "Templates.Index",
-                areaName: "OrchardCore.Templates",
-                pattern: _adminOptions.AdminUrlPrefix + "/Templates",
-                defaults: new { controller = templateControllerName, action = nameof(TemplateController.Index) }
-            );
-
-            routes.MapAreaControllerRoute(
-                name: "Templates.Admin",
-                areaName: "OrchardCore.Templates",
-                pattern: _adminOptions.AdminUrlPrefix + "/Templates/Admin",
-                defaults: new { controller = templateControllerName, action = nameof(TemplateController.Admin) }
-            );
-
-            routes.MapAreaControllerRoute(
-                name: "Templates.Create",
-                areaName: "OrchardCore.Templates",
-                pattern: _adminOptions.AdminUrlPrefix + "/Templates/Create",
-                defaults: new { controller = templateControllerName, action = nameof(TemplateController.Create) }
-            );
-
-            routes.MapAreaControllerRoute(
-                name: "Templates.Edit",
-                areaName: "OrchardCore.Templates",
-                pattern: _adminOptions.AdminUrlPrefix + "/Templates/Edit/{name}",
-                defaults: new { controller = templateControllerName, action = nameof(TemplateController.Edit) }
-            );
-        }
     }
 
     [Feature("OrchardCore.AdminTemplates")]
     public class AdminTemplatesStartup : StartupBase
     {
-        private readonly AdminOptions _adminOptions;
-
-        public AdminTemplatesStartup(IOptions<AdminOptions> adminOptions)
-        {
-            _adminOptions = adminOptions.Value;
-        }
-
         public override void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IShapeBindingResolver, AdminTemplatesShapeBindingResolver>();
@@ -103,16 +50,6 @@ namespace OrchardCore.Templates
             services.AddScoped<INavigationProvider, AdminTemplatesAdminMenu>();
             services.AddRecipeExecutionStep<AdminTemplateStep>();
             services.AddDeployment<AllAdminTemplatesDeploymentSource, AllAdminTemplatesDeploymentStep, AllAdminTemplatesDeploymentStepDriver>();
-        }
-
-        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
-        {
-            routes.MapAreaControllerRoute(
-                name: "Templates.Admin",
-                areaName: "OrchardCore.Templates",
-                pattern: _adminOptions.AdminUrlPrefix + "/Templates/Admin",
-                defaults: new { controller = typeof(TemplateController).ControllerName(), action = nameof(TemplateController.Admin) }
-            );
         }
     }
 }
