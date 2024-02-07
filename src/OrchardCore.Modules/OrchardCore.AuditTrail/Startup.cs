db87065f0b45a785fe645654bd85846ce77c54cf
Sára El-Saig <sara.el-saig@lombiq.com>
2024-02-07 01:40:56 +0100

    Update OrchardCore.AuditTrail.

--- src/OrchardCore.Modules/OrchardCore.AuditTrail/Startup.cs
+++ src/OrchardCore.Modules/OrchardCore.AuditTrail/Startup.cs
@@ -1,13 +1,7 @@
-using System;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Options;
-using OrchardCore.Admin;
-using OrchardCore.AuditTrail.Controllers;
 using OrchardCore.AuditTrail.Drivers;
 using OrchardCore.AuditTrail.Indexes;
 using OrchardCore.AuditTrail.Models;
@@ -21,7 +15,6 @@ using OrchardCore.Data;
 using OrchardCore.Data.Migration;
 using OrchardCore.DisplayManagement.Handlers;
 using OrchardCore.Modules;
-using OrchardCore.Mvc.Core.Utilities;
 using OrchardCore.Navigation;
 using OrchardCore.Security.Permissions;
 using OrchardCore.Settings;
@@ -32,13 +25,6 @@ namespace OrchardCore.AuditTrail
 {
     public class Startup : StartupBase
     {
-        private readonly AdminOptions _adminOptions;
-
-        public Startup(IOptions<AdminOptions> adminOptions)
-        {
-            _adminOptions = adminOptions.Value;
-        }
-
         public override void ConfigureServices(IServiceCollection services)
         {
             // Add ILookupNormalizer as Singleton because it is needed by Users
@@ -134,25 +120,6 @@ namespace OrchardCore.AuditTrail
                     .ForSort("user-desc-time-asc", b => b
                         .WithQuery((val, query) => query.With<AuditTrailEventIndex>().OrderByDescending(i => i.NormalizedUserName).ThenBy(i => i.CreatedUtc)));
             });
-        }
-
-        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
-        {
-            var adminControllerName = typeof(AdminController).ControllerName();
-
-            routes.MapAreaControllerRoute(
-                name: "AuditTrailIndex",
-                areaName: "OrchardCore.AuditTrail",
-                pattern: _adminOptions.AdminUrlPrefix + "/AuditTrail/{correlationId?}",
-                defaults: new { controller = adminControllerName, action = nameof(AdminController.Index) }
-            );
-
-            routes.MapAreaControllerRoute(
-                name: "AuditTrailDisplay",
-                areaName: "OrchardCore.AuditTrail",
-                pattern: _adminOptions.AdminUrlPrefix + "/AuditTrail/Display/{auditTrailEventId}",
-                defaults: new { controller = adminControllerName, action = nameof(AdminController.Display) }
-            );
         }
     }
 
