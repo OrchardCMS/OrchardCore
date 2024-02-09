1482fd344e55408451186a7fc46bba590fa2312c
Jasmin Savard <jasmin.savard@gmail.com>
2024-02-08 20:28:07 -0500

    move to admin.js

--- src/OrchardCore.Themes/TheAdmin/Assets/js/admin.js
+++ src/OrchardCore.Themes/TheAdmin/Assets/js/admin.js
@@ -193,3 +193,28 @@ $('body').on('submit', 'form.no-multisubmit', function (e) {
         form.removeClass(submittingClass);
     }, 5000);
 });
+
+// Check all checkboxes
+$(function () {
+    $('.check-all-content-container').each(function () {
+        var container = $(this);
+        var master = container.find('input[type="checkbox"].check-all-master');
+        var slaves = container.find('.check-all-content-slave input[type="checkbox"]:not(:disabled)');
+
+        var updateMaster = function () {
+            var allChecked = slaves.filter(':not(:checked)').length == 0;
+            master.prop('checked', allChecked);
+        }
+
+        master.on('change', function () {
+            var isChecked = $(this).is(':checked');
+            slaves.prop('checked', isChecked);
+        });
+
+        slaves.on('change', function () {
+            updateMaster();
+        });
+
+        updateMaster();
+    });
+});
