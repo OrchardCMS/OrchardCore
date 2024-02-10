35e17a9ae00e7395b94e8a2b550ff3526c272ab4
Sára El-Saig <sara.el-saig@lombiq.com>
2024-02-10 01:29:04 +0100

    Revert left-in debug code.

--- src/OrchardCore.Modules/OrchardCore.Recipes/Controllers/AdminController.cs
+++ src/OrchardCore.Modules/OrchardCore.Recipes/Controllers/AdminController.cs
@@ -57,7 +57,7 @@ namespace OrchardCore.Recipes.Controllers
             _logger = logger;
         }
 
-        [Admin("Recipes", "Admin Route for the recipe list.")]
+        [Admin("Recipes", "Recipes")]
         public async Task<ActionResult> Index()
         {
             if (!await _authorizationService.AuthorizeAsync(User, StandardPermissions.SiteOwner))
