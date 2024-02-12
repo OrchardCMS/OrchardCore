3aba6c82f68e961fbb0cab1e1e69e946613ca65e
Mike Alhayek <mike@crestapps.com>
2024-02-12 10:16:27 -0800

    use file-scope namespace

--- src/OrchardCore/OrchardCore.ContentManagement.GraphQL/Extensions/FieldTypeExtensions.cs
+++ src/OrchardCore/OrchardCore.ContentManagement.GraphQL/Extensions/FieldTypeExtensions.cs
@@ -1,32 +1,31 @@
 using GraphQL.Types;
 
-namespace OrchardCore.ContentManagement.GraphQL
+namespace OrchardCore.ContentManagement.GraphQL;
+
+public static class FieldTypeExtensions
 {
-    public static class FieldTypeExtensions
+    public static FieldType WithPartCollapsedMetaData(this FieldType fieldType, bool collapsed = true)
     {
-        public static FieldType WithPartCollapsedMetaData(this FieldType fieldType, bool collapsed = true)
+        return fieldType.WithMetaData("PartCollapsed", collapsed);
+    }
+
+    public static FieldType WithPartNameMetaData(this FieldType fieldType, string partName)
+    {
+        return fieldType.WithMetaData("PartName", partName);
+    }
+
+    private static FieldType WithMetaData(this FieldType fieldType, string name, object value)
+    {
+        if (fieldType == null)
         {
-            return fieldType.WithMetaData("PartCollapsed", collapsed);
+            return null;
         }
 
-        public static FieldType WithPartNameMetaData(this FieldType fieldType, string partName)
+        lock (fieldType.Metadata)
         {
-            return fieldType.WithMetaData("PartName", partName);
+            fieldType.Metadata.TryAdd(name, value);
         }
 
-        private static FieldType WithMetaData(this FieldType fieldType, string name, object value)
-        {
-            if (fieldType == null)
-            {
-                return null;
-            }
-
-            lock (fieldType.Metadata)
-            {
-                fieldType.Metadata.TryAdd(name, value);
-            }
-
-            return fieldType;
-        }
+        return fieldType;
     }
 }
