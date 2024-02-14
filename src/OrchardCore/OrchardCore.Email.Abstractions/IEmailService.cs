66a7478c30a405a6faadd401c3234e5c37fd3327
Mike Alhayek <mike@crestapps.com>
2024-02-13 19:27:31 -0800

    Update IEmailService.cs
    Co-authored-by: Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>

--- src/OrchardCore/OrchardCore.Email.Abstractions/IEmailService.cs
+++ src/OrchardCore/OrchardCore.Email.Abstractions/IEmailService.cs
@@ -8,7 +8,7 @@ public interface IEmailService
     /// Send the given message as email.
     /// </summary>
     /// <param name="message">The message to send.</param>
-    /// <param name="name">The key of the Email provider. When null or empty is provider, the default provider is used.</param>
+    /// <param name="name">The technical name of the Email provider. When null or empty, the default provider is used.</param>
     /// <returns>EmailResult object.</returns>
     Task<EmailResult> SendAsync(MailMessage message, string providerName = null);
 }
