8a58d83372d38b830ef7e07d2db20cb12ad8282d
Mike Alhayek <mike@crestapps.com>
2024-02-13 19:29:20 -0800

    Update IEmailProviderResolver.cs
    Co-authored-by: Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>

--- src/OrchardCore/OrchardCore.Email.Abstractions/IEmailProviderResolver.cs
+++ src/OrchardCore/OrchardCore.Email.Abstractions/IEmailProviderResolver.cs
@@ -8,7 +8,7 @@ public interface IEmailProviderResolver
     /// Gets the Email provider for the given technical name.
     /// When null or empty string is provided, it returns the default email provider.
     /// </summary>
-    /// <param name="name">The key of the Email provider.</param>
+    /// <param name="name">The technical name of the Email provider.</param>
     /// <returns>Instance IEmailProvider or null when no default service is available.</returns>
     /// <exception cref="InvalidEmailProviderException">When invalid provider is given.</exception>
     Task<IEmailProvider> GetAsync(string name = null);
