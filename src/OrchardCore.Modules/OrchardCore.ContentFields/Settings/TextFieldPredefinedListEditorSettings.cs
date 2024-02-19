13f1a7b75adc0fd9804ea76d509ee7257650d515
Mike Alhayek <mike@crestapps.com>
2024-02-19 15:08:05 -0800

    Add multiple constructors for ListValueOption

--- src/OrchardCore.Modules/OrchardCore.ContentFields/Settings/TextFieldPredefinedListEditorSettings.cs
+++ src/OrchardCore.Modules/OrchardCore.ContentFields/Settings/TextFieldPredefinedListEditorSettings.cs
@@ -22,5 +22,21 @@ namespace OrchardCore.ContentFields.Settings
 
         [JsonPropertyName("value")]
         public string Value { get; set; }
+
+        public ListValueOption()
+        {
+        }
+
+        public ListValueOption(string name)
+        {
+            Name = name;
+            Value = name;
+        }
+
+        public ListValueOption(string name, string value)
+        {
+            Name = name;
+            Value = value;
+        }
     }
 }
