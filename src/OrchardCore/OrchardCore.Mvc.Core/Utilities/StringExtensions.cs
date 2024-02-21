e188e8abc6820696c8dbe4ec7d6a029a51927c6f
Mike Alhayek <mike@crestapps.com>
2024-02-21 13:34:28 -0800

    Support comments in recipes Fix #15385

--- src/OrchardCore/OrchardCore.Mvc.Core/Utilities/StringExtensions.cs
+++ src/OrchardCore/OrchardCore.Mvc.Core/Utilities/StringExtensions.cs
@@ -4,6 +4,7 @@ using System.Collections.Immutable;
 using System.Globalization;
 using System.Net;
 using System.Text;
+using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Text.RegularExpressions;
 using Cysharp.Text;
@@ -501,11 +502,11 @@ namespace OrchardCore.Mvc.Utilities
         /// <summary>
         /// Tests if a string is valid json.
         /// </summary>
-        public static bool IsJson(this string json)
+        public static bool IsJson(this string json, JsonNodeOptions? jsonNodeOptions = null, JsonDocumentOptions jsonDocumentOptions = default)
         {
             try
             {
-                JsonNode.Parse(json);
+                JsonNode.Parse(json, jsonNodeOptions, jsonDocumentOptions);
                 return true;
             }
             catch
