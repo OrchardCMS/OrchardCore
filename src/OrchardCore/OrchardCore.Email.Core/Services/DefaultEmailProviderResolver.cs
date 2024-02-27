721f8ad204b0a5176a4d30b19899a532a9cb0d33
Mike Alhayek <mike@crestapps.com>
2024-02-27 11:49:56 -0800

    Azure Email Communication Services (using Provider) (Lombiq Technologies: OCORE-129) (#15254)
    ---------
    
    Co-authored-by: Hisham Bin Ateya <hishamco_2007@yahoo.com>
    Co-authored-by: Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
    Co-authored-by: Benedek Farkas <bfarkas@windowslive.com>

---
+++ src/OrchardCore/OrchardCore.Email.Core/Services/DefaultEmailProviderResolver.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using OrchardCore.Environment.Shell.Builders;
+
+namespace OrchardCore.Email.Core.Services;
+
+public class DefaultEmailProviderResolver : IEmailProviderResolver
+{
+    private readonly EmailOptions _emailOptions;
+    private readonly IServiceProvider _serviceProvider;
+    private readonly EmailProviderOptions _providerOptions;
+
+    public DefaultEmailProviderResolver(
+        IOptions<EmailOptions> emailOptions,
+        IOptions<EmailProviderOptions> providerOptions,
+        IServiceProvider serviceProvider)
+    {
+        _emailOptions = emailOptions.Value;
+        _serviceProvider = serviceProvider;
+        _providerOptions = providerOptions.Value;
+    }
+
+    public ValueTask<IEmailProvider> GetAsync(string name = null)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            name = _emailOptions.DefaultProviderName;
+        }
+
+        if (!string.IsNullOrEmpty(name))
+        {
+            if (_providerOptions.Providers.TryGetValue(name, out var providerType))
+            {
+                return ValueTask.FromResult(_serviceProvider.CreateInstance<IEmailProvider>(providerType.Type));
+            }
+
+            throw new InvalidEmailProviderException(name);
+        }
+
+        return ValueTask.FromResult<IEmailProvider>(null);
+    }
+}
