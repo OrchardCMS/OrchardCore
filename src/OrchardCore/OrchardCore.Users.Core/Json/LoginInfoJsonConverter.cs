8595132b91f77123ffffbccef59d5e75a2825135
Mike Alhayek <mike@crestapps.com>
2024-02-28 12:24:10 -0800

    Update LoginInfoJsonConverter.cs

--- src/OrchardCore/OrchardCore.Users.Core/Json/LoginInfoJsonConverter.cs
+++ src/OrchardCore/OrchardCore.Users.Core/Json/LoginInfoJsonConverter.cs
@@ -3,7 +3,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Identity;
 
-namespace OrchardCore.Users.Json;
+namespace OrchardCore.Json.Serialization;
 
 public class LoginInfoJsonConverter : JsonConverter<UserLoginInfo>
 {
