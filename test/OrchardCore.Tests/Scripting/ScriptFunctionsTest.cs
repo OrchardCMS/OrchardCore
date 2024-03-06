ec29f501eacacc531a92082a525181a3f5e77bdb
hyzx86 <huarui2219@qq.com>
2024-03-07 00:40:11 +0800

    update test case

--- test/OrchardCore.Tests/Scripting/ScriptFunctionsTest.cs
+++ test/OrchardCore.Tests/Scripting/ScriptFunctionsTest.cs
@@ -1,42 +1,34 @@
-using System.Text.Json;
 using System.Text.Json.Nodes;
 using OrchardCore.Scripting;
 using OrchardCore.Tests.Apis.Context;
-using OrchardCore.Users;
-using OrchardCore.Users.Models;
 
 namespace OrchardCore.Tests.Scripting;
 public class ScriptFunctionsTest
 {
     [Fact]
-    public async Task UserShouldBeAllowedConvertedToJsonObjectAndUsedInScripts()
+    public async Task TheScriptingEngineShouldBeAbleToHandleJsonObject()
     {
         using var context = new SiteContext();
         await context.InitializeAsync();
-        await context.UsingTenantScopeAsync(async scope =>
+        await context.UsingTenantScopeAsync(scope =>
         {
             var findUser = new GlobalMethod
             {
-                Name = "getUserByName",
-                Method = sp => (string userName) =>
+                Name = "tryAccessJsonObject",
+                Method = sp => () =>
                 {
-                    var userManager = sp.GetRequiredService<UserManager<IUser>>();
-                    var userInfo = userManager.FindByNameAsync(userName).GetAwaiter().GetResult();
-                    var jobjUser = JObject.FromObject(userInfo, JOptions.CamelCase);
-                    jobjUser.Remove("securityStamp");
-                    jobjUser.Remove("passwordHash");
-                    jobjUser.Remove("resetToken");
-                    jobjUser.Remove("userTokens");
-                    return jobjUser;
+                    return new JsonObject
+                    {
+                        ["name"] = "test-name"
+                    }; ;
                 }
             };
 
             var scriptingEngine = scope.ServiceProvider.GetRequiredService<IScriptingEngine>();
             var scriptingScope = scriptingEngine.CreateScope([findUser], scope.ServiceProvider, null, null);
-            var result = (bool)scriptingEngine.Evaluate(scriptingScope, "var user = getUserByName('admin'); return user.userName == 'admin'");
+            var result = (bool)scriptingEngine.Evaluate(scriptingScope, "var jsonObject = tryAccessJsonObject(); return jsonObject.name == 'test-name'");
             Assert.True(result);
-            var result1 = scriptingEngine.Evaluate(scriptingScope, "var user2 = getUserByName('admin'); return user2.userName");
-            Assert.NotNull(result1);
+            return Task.CompletedTask;
         });
     }
 }
