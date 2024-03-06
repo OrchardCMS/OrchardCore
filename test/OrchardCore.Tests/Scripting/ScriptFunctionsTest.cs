2383d41056a73d13298d91f25d339bf2200ba037
Tony Han <huarui2219@qq.com>
2024-03-07 00:29:55 +0800

    Update ScriptFunctionsTest.cs

--- test/OrchardCore.Tests/Scripting/ScriptFunctionsTest.cs
+++ test/OrchardCore.Tests/Scripting/ScriptFunctionsTest.cs
@@ -9,82 +9,12 @@ namespace OrchardCore.Tests.Scripting;
 public class ScriptFunctionsTest
 {
     [Fact]
-    public async Task ProcessUserInfoTest()
+    public async Task UserShouldBeAllowedConvertedToJsonObjectAndUsedInScripts()
     {
         using var context = new SiteContext();
         await context.InitializeAsync();
         await context.UsingTenantScopeAsync(async scope =>
         {
-            var userProperties = @"{
-            ""UserProfile"": {
-                ""ContentItemId"": ""4fkpnj0fnawmzy3zdc5kx5pnn1"",
-                ""ContentItemVersionId"": null,
-                ""ContentType"": ""UserProfile"",
-                ""DisplayText"": ""admin admin - (admin)"",
-                ""Latest"": true,
-                ""Published"": true,
-                ""ModifiedUtc"": null,
-                ""PublishedUtc"": null,
-                ""CreatedUtc"": null,
-                ""Owner"": ""4fkpnj0fnawmzy3zdc5kx5pnn1"",
-                ""Author"": ""admin"",
-                ""UserProfile"": {
-                    ""UserName"": {
-                        ""Text"": ""admin""
-                    },
-                    ""Email"": {
-                        ""Text"": ""admin@admin.com""
-                    },
-                    ""OwnerUser"": {
-                        ""UserIds"": [
-                            ""4fkpnj0fnawmzy3zdc5kx5pnn1""
-                        ],
-                        ""UserNames"": []
-                    },
-                    ""DisplayName"": {
-                        ""Text"": ""admin admin""
-                    },
-                    ""Department"": {
-                        ""ContentItemIds"": []
-                    },
-                    ""Manager"": {
-                        ""UserIds"": [],
-                        ""UserNames"": []
-                    },
-                    ""FirstName"": {
-                        ""Text"": null
-                    },
-                    ""LastName"": {
-                        ""Text"": null
-                    },
-                    ""Country"": {
-                        ""Text"": ""ID""
-                    },
-                    ""Countries"": {
-                        ""Values"": []
-                    }
-                },
-                ""TitlePart"": {
-                    ""Title"": ""admin - (admin)""
-                },
-                ""DIndexPart"": {}
-            },
-            ""UserNotificationPreferencesPart"": {
-                ""Methods"": [
-                    ""Email""
-                ],
-                ""Optout"": []
-            },
-            ""UserTimeZone"": {
-                ""TimeZoneId"": null
-            }
-        }";
-
-            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IUser>>();
-            var user = await userManager.FindByNameAsync("admin") as User;
-            user.Properties = JObject.Parse(userProperties);
-            await userManager.UpdateAsync(user);
-
             var findUser = new GlobalMethod
             {
                 Name = "getUserByName",
