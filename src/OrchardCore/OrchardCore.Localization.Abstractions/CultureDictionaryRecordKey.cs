9f1dbbe0558a6c38023e897cb53e4f383b293616
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2024-03-09 08:28:21 +0300

    Address feedback

--- src/OrchardCore/OrchardCore.Localization.Abstractions/CultureDictionaryRecordKey.cs
+++ src/OrchardCore/OrchardCore.Localization.Abstractions/CultureDictionaryRecordKey.cs
@@ -1,32 +1,10 @@
-using System;
-
 namespace OrchardCore.Localization
 {
     /// <summary>
     /// Represents a key for <see cref="CultureDictionaryRecord"/>.
     /// </summary>
-    public readonly record struct CultureDictionaryRecordKey : IEquatable<CultureDictionaryRecordKey>
+    public readonly record struct CultureDictionaryRecordKey
     {
-        /// <summary>
-        /// Creates new instance of <see cref="CultureDictionaryRecordKey"/>.
-        /// </summary>
-        /// <param name="messageId">The message Id.</param>
-        public CultureDictionaryRecordKey(string messageId) 
-            : this(messageId, null)
-        {
-        }
-
-        /// <summary>
-        /// Creates new instance of <see cref="CultureDictionaryRecordKey"/>.
-        /// </summary>
-        /// <param name="messageId">The message Id.</param>
-        /// <param name="context">The message context.</param>
-        public CultureDictionaryRecordKey(string messageId, string context)
-        {
-            MessageId = messageId;
-            Context = context;
-        }
-
         /// <summary>
         /// Gets the message Id.
         /// </summary>
@@ -39,13 +17,6 @@ namespace OrchardCore.Localization
 
         public static implicit operator string(CultureDictionaryRecordKey cultureDictionaryRecordKey)
             => cultureDictionaryRecordKey.ToString();
-
-        /// <inheritdoc />
-        public bool Equals(CultureDictionaryRecordKey other)
-            => string.Equals(MessageId, other.MessageId) && string.Equals(Context, other.Context);
-
-        /// <inheritdoc />
-        public override int GetHashCode() => HashCode.Combine(MessageId, Context);
 
         public override string ToString()
             => string.IsNullOrEmpty(Context)
