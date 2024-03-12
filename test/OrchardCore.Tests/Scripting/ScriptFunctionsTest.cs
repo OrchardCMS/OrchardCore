29cfdc73d9fcd9c4f486ad0ca1da0bc8b5c5b027
hyzx86 <yanzhong.han@jizhousoft.com>
2024-03-12 12:42:22 +0800

    update script test

--- test/OrchardCore.Tests/Scripting/ScriptFunctionsTest.cs
+++ test/OrchardCore.Tests/Scripting/ScriptFunctionsTest.cs
@@ -17,16 +17,69 @@ public class ScriptFunctionsTest
                 Name = "tryAccessJsonObject",
                 Method = sp => () =>
                 {
-                    return new JsonObject
+                    const string jsonData = """
                     {
-                        ["name"] = "test-name"
-                    }; ;
+                        "age":33,
+                        "booleanValue":false,
+                        "booleanValue1":true,
+                        "stringValue":"stringTest",
+                        "employees": {
+                            "type": "array",
+                            "value": [
+                                {
+                                    "firstName": "John",
+                                    "lastName": "Doe"
+                                },
+                                {
+                                    "firstName": "Jane",
+                                    "lastName": "Doe"
+                                }
+                            ]
+                        }
+                    }
+                    """;
+
+                    return JObject.Parse(jsonData);
                 }
             };
 
             var scriptingEngine = scope.ServiceProvider.GetRequiredService<IScriptingEngine>();
             var scriptingScope = scriptingEngine.CreateScope([findUser], scope.ServiceProvider, null, null);
-            var result = (bool)scriptingEngine.Evaluate(scriptingScope, "var jsonObject = tryAccessJsonObject(); return jsonObject.name == 'test-name'");
+            var result = (bool)scriptingEngine.Evaluate(scriptingScope,
+                @"var jobj = tryAccessJsonObject();
+                    return jobj.age == 33;
+                ");
+            Assert.True(result);
+
+            result = (bool)scriptingEngine.Evaluate(scriptingScope,
+                  @"var jobj = tryAccessJsonObject();
+                    return jobj.stringValue == ""stringTest"";
+                ");
+            Assert.True(result);
+
+            var result1 = scriptingEngine.Evaluate(scriptingScope,
+                  @"var jobj = tryAccessJsonObject();
+                var steps = [];
+                
+                if(!jobj.booleanValue) steps.push(1);
+
+                // booleanValue should be false 
+                if(jobj.booleanValue == false) steps.push(2);
+                if(jobj.booleanValue1 == true) steps.push(3);
+                if(!jobj.booleanValue) steps.push(4);
+                if(!!jobj.booleanValue1) steps.push(5);
+                steps.push(jobj.booleanValue);
+                steps.push(jobj.booleanValue.toString());
+
+             return steps.join(',')
+            ");
+            Assert.Equal("1,2,3,4,5,false,false", result1);
+
+            result = (bool)scriptingEngine.Evaluate(scriptingScope,
+            @"var jobj = tryAccessJsonObject();
+                            return jobj.employees.type == ""array"" &&
+                                    jobj.employees.value[0].firstName == ""John"";
+                        ");
             Assert.True(result);
             return Task.CompletedTask;
         });
