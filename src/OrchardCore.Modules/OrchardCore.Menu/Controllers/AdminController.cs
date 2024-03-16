262ac7dcbc2241fed5e9c99f56ff911649868dc1
Sára El-Saig <sara.el-saig@lombiq.com>
2024-03-16 04:45:07 +0100

    DRY a little.

--- src/OrchardCore.Modules/OrchardCore.Menu/Controllers/AdminController.cs
+++ src/OrchardCore.Modules/OrchardCore.Menu/Controllers/AdminController.cs
@@ -252,18 +252,10 @@ namespace OrchardCore.Menu.Controllers
                 return Forbid();
             }
 
-            ContentItem menu;
-
             var contentTypeDefinition = await _contentDefinitionManager.GetTypeDefinitionAsync("Menu");
-
-            if (!contentTypeDefinition.IsDraftable())
-            {
-                menu = await _contentManager.GetAsync(menuContentItemId, VersionOptions.Latest);
-            }
-            else
-            {
-                menu = await _contentManager.GetAsync(menuContentItemId, VersionOptions.DraftRequired);
-            }
+            var menu = !contentTypeDefinition.IsDraftable()
+                ? await _contentManager.GetAsync(menuContentItemId, VersionOptions.Latest)
+                : await _contentManager.GetAsync(menuContentItemId, VersionOptions.DraftRequired);
 
             if (menu == null)
             {
