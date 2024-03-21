13953bfe1b2d898d4c6fd62068d862970f515a45
Mike Alhayek <mike@crestapps.com>
2024-03-21 13:14:02 -0700

    Add form alternates

---
+++ src/OrchardCore.Modules/OrchardCore.Forms/FormShapeTableProvider.cs
@@ -0,0 +1,27 @@
+using OrchardCore.ContentManagement;
+using OrchardCore.DisplayManagement;
+using OrchardCore.DisplayManagement.Descriptors;
+using OrchardCore.DisplayManagement.Utilities;
+using OrchardCore.Forms.Models;
+
+namespace OrchardCore.Forms;
+
+public class FormShapeTableProvider : IShapeTableProvider
+{
+    public void Discover(ShapeTableBuilder builder)
+    {
+        builder.Describe("Widget__Form")
+            .OnDisplaying(context =>
+            {
+                if (context.Shape.TryGetProperty<ContentItem>("ContentItem", out var contentItem) && contentItem.ContentType == "Form")
+                {
+                    context.Shape.Metadata.Alternates.Add($"Widget__Form_{contentItem.ContentItemId}");
+
+                    if (contentItem.TryGet<FormElementPart>(out var elementPart) && !string.IsNullOrEmpty(elementPart.Id))
+                    {
+                        context.Shape.Metadata.Alternates.Add($"Widget__Form_{elementPart.Id.EncodeAlternateElement()}");
+                    }
+                }
+            });
+    }
+}
