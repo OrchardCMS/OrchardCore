5720a648d72aa0fe0e4d55f9e2f97fd0245ce006
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2024-04-01 22:35:30 +0300

    Update the comment

--- src/OrchardCore.Modules/OrchardCore.Redis/HealthChecks/Startup.cs
+++ src/OrchardCore.Modules/OrchardCore.Redis/HealthChecks/Startup.cs
@@ -6,7 +6,8 @@ namespace OrchardCore.Redis.HealthChecks;
 [RequireFeatures("OrchardCore.HealthChecks")]
 public class Startup : StartupBase
 {
-    // The order of this startup configuration should run early to register the Redis check.
+    // The order of this startup configuration should be greater than zero to register the Redis check early,
+    // so the health check can be reported alongside with other health chekcs in the system.
     public override int Order => 100;
 
     public override void ConfigureServices(IServiceCollection services)
