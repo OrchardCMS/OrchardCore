b7c24c37b0a3d3804fef00663b32ad1d522851aa
Mike Alhayek <mike@crestapps.com>
2024-04-01 12:18:18 -0700

    undo params

--- src/OrchardCore/OrchardCore.DisplayManagement/Notify/Notifier.cs
+++ src/OrchardCore/OrchardCore.DisplayManagement/Notify/Notifier.cs
@@ -7,18 +7,17 @@ namespace OrchardCore.DisplayManagement.Notify
 {
     public class Notifier : INotifier
     {
-        private readonly List<NotifyEntry> _entries;
+        private readonly List<NotifyEntry> _entries = [];
         private readonly ILogger _logger;
 
         public Notifier(ILogger<Notifier> logger)
         {
-            _entries = [];
             _logger = logger;
         }
 
         public ValueTask AddAsync(NotifyType type, LocalizedHtmlString message)
         {
-            _logger.LogInformation("Notification '{Type}' with message '{Message}'", type, message);
+            _logger.LogInformation("Notification '{NotificationType}' with message '{NotificationMessage}'", type, message);
 
             _entries.Add(new NotifyEntry { Type = type, Message = message });
 
