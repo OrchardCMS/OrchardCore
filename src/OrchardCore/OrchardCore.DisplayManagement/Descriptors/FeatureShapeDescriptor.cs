74d923cf0ec932c31bec7fa307ab21c2ed1ba011
Mike Alhayek <mike@crestapps.com>
2024-04-03 12:43:04 -0700

    Simplify ShapeDescriptorIndex (#15632)

--- src/OrchardCore/OrchardCore.DisplayManagement/Descriptors/FeatureShapeDescriptor.cs
+++ src/OrchardCore/OrchardCore.DisplayManagement/Descriptors/FeatureShapeDescriptor.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Html;
 using OrchardCore.DisplayManagement.Implementation;
@@ -22,63 +21,47 @@ namespace OrchardCore.DisplayManagement.Descriptors
 
     public class ShapeDescriptorIndex : ShapeDescriptor
     {
-        private readonly ConcurrentDictionary<string, FeatureShapeDescriptor> _descriptors;
-        private readonly List<FeatureShapeDescriptor> _alternationDescriptors;
-        private readonly List<string> _wrappers;
-        private readonly List<string> _bindingSources;
-        private readonly Dictionary<string, ShapeBinding> _bindings;
-        private readonly List<Func<ShapeCreatingContext, Task>> _creatingAsync;
-        private readonly List<Func<ShapeCreatedContext, Task>> _createdAsync;
-        private readonly List<Func<ShapeDisplayContext, Task>> _displayingAsync;
-        private readonly List<Func<ShapeDisplayContext, Task>> _processingAsync;
-        private readonly List<Func<ShapeDisplayContext, Task>> _displayedAsync;
+        private readonly List<FeatureShapeDescriptor> _alternationDescriptors = [];
+        private readonly List<string> _wrappers = [];
+        private readonly List<string> _bindingSources = [];
+        private readonly Dictionary<string, ShapeBinding> _bindings = new(StringComparer.OrdinalIgnoreCase);
+        private readonly List<Func<ShapeCreatingContext, Task>> _creatingAsync = [];
+        private readonly List<Func<ShapeCreatedContext, Task>> _createdAsync = [];
+        private readonly List<Func<ShapeDisplayContext, Task>> _displayingAsync = [];
+        private readonly List<Func<ShapeDisplayContext, Task>> _processingAsync = [];
+        private readonly List<Func<ShapeDisplayContext, Task>> _displayedAsync = [];
 
         public ShapeDescriptorIndex(
             string shapeType,
             IEnumerable<string> alterationKeys,
             ConcurrentDictionary<string, FeatureShapeDescriptor> descriptors)
         {
+            ArgumentException.ThrowIfNullOrEmpty(shapeType);
+
             ShapeType = shapeType;
-            _descriptors = descriptors;
 
-            // pre-calculate as much as we can
-            _alternationDescriptors = alterationKeys
-                .Select(key => _descriptors[key])
-                .ToList();
+            // Pre-calculate as much as we can for performance reasons.
+            foreach (var alterationKey in alterationKeys)
+            {
+                if (!descriptors.TryGetValue(alterationKey, out var alternationDescriptor))
+                {
+                    continue;
+                }
 
-            _wrappers = _alternationDescriptors
-                .SelectMany(sd => sd.Wrappers)
-                .ToList();
+                _alternationDescriptors.Add(alternationDescriptor);
+                _wrappers.AddRange(alternationDescriptor.Wrappers);
+                _bindingSources.AddRange(alternationDescriptor.BindingSources);
+                _creatingAsync.AddRange(alternationDescriptor.CreatingAsync);
+                _createdAsync.AddRange(alternationDescriptor.CreatedAsync);
+                _displayingAsync.AddRange(alternationDescriptor.DisplayingAsync);
+                _displayedAsync.AddRange(alternationDescriptor.DisplayedAsync);
+                _processingAsync.AddRange(alternationDescriptor.ProcessingAsync);
 
-            _bindingSources = _alternationDescriptors
-                .SelectMany(sd => sd.BindingSources)
-                .ToList();
-
-            _bindings = _alternationDescriptors
-                .SelectMany(sd => sd.Bindings)
-                .GroupBy(kv => kv.Key, StringComparer.OrdinalIgnoreCase)
-                .Select(kv => kv.Last())
-                .ToDictionary(kv => kv.Key, kv => kv.Value, StringComparer.OrdinalIgnoreCase);
-
-            _creatingAsync = _alternationDescriptors
-                .SelectMany(sd => sd.CreatingAsync)
-                .ToList();
-
-            _createdAsync = _alternationDescriptors
-                .SelectMany(sd => sd.CreatedAsync)
-                .ToList();
-
-            _displayingAsync = _alternationDescriptors
-                .SelectMany(sd => sd.DisplayingAsync)
-                .ToList();
-
-            _processingAsync = _alternationDescriptors
-                .SelectMany(sd => sd.ProcessingAsync)
-                .ToList();
-
-            _displayedAsync = _alternationDescriptors
-                .SelectMany(sd => sd.DisplayedAsync)
-                .ToList();
+                foreach (var binding in alternationDescriptor.Bindings)
+                {
+                    _bindings[binding.Key] = binding.Value;
+                }
+            }
         }
 
         /// <summary>
