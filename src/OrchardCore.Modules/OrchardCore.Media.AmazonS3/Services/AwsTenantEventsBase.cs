5fc658b01af820eb24f518bad92b024e54873f4b
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2024-04-08 01:40:27 +0200

    DRY

--- src/OrchardCore.Modules/OrchardCore.Media.AmazonS3/Services/AwsTenantEventsBase.cs
+++ src/OrchardCore.Modules/OrchardCore.Media.AmazonS3/Services/AwsTenantEventsBase.cs
@@ -130,4 +130,3 @@ public abstract class AwsTenantEventsBase : ModularTenantEvents
         }
     }
 }
-
--- src/OrchardCore.Modules/OrchardCore.Media.Azure/ImageSharpBlobImageCacheOptions.cs
+++ src/OrchardCore.Modules/OrchardCore.Media.Azure/ImageSharpBlobImageCacheOptions.cs
@@ -1,37 +1,5 @@
 namespace OrchardCore.Media.Azure;
 
-public class ImageSharpBlobImageCacheOptions
+public class ImageSharpBlobImageCacheOptions : MediaBlobStorageOptionsBase
 {
-    /// <summary>
-    /// The Azure Blob connection string.
-    /// </summary>
-    public string ConnectionString { get; set; }
-
-    /// <summary>
-    /// The Azure Blob container name.
-    /// </summary>
-    public string ContainerName { get; set; }
-
-    /// <summary>
-    /// Create Blob container on startup if one does not exist.
-    /// </summary>
-    public bool CreateContainer { get; set; } = true;
-
-    /// <summary>
-    /// The base directory path to use inside the container for this store's content.
-    /// </summary>
-    public string BasePath { get; set; } = "";
-
-    /// <summary>
-    /// Remove Blob container on tenant removal if it exists.
-    /// </summary>
-    public bool RemoveContainer { get; set; }
-
-    /// <summary>
-    /// Returns a value indicating whether the basic state of the configuration is valid.
-    /// </summary>
-    public bool IsValid()
-    {
-        return !string.IsNullOrEmpty(ConnectionString) && !string.IsNullOrEmpty(ContainerName);
-    }
 }
