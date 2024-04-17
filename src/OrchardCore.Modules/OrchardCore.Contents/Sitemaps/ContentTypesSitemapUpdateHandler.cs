4fc2a8b9d03f3589fa9af46009eff76e16b7595d
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2024-04-17 02:10:18 +0200

    Lock around the whole deferred task

--- src/OrchardCore.Modules/OrchardCore.Contents/Sitemaps/ContentTypesSitemapUpdateHandler.cs
+++ src/OrchardCore.Modules/OrchardCore.Contents/Sitemaps/ContentTypesSitemapUpdateHandler.cs
@@ -1,36 +1,59 @@
+using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.ContentManagement;
 using OrchardCore.ContentManagement.Handlers;
+using OrchardCore.Environment.Shell.Scope;
+using OrchardCore.Locking.Distributed;
 using OrchardCore.Sitemaps.Handlers;
 
 namespace OrchardCore.Contents.Sitemaps
 {
     public class ContentTypesSitemapUpdateHandler : ContentHandlerBase
     {
-        private readonly ISitemapUpdateHandler _sitemapUpdateHandler;
+        private readonly IDistributedLock _distributedLock;
 
-        public ContentTypesSitemapUpdateHandler(ISitemapUpdateHandler sitemapUpdateHandler)
+        public ContentTypesSitemapUpdateHandler(IDistributedLock distributedLock)
         {
-            _sitemapUpdateHandler = sitemapUpdateHandler;
+            _distributedLock = distributedLock;
         }
 
-        public override Task PublishedAsync(PublishContentContext context)
+        public override Task PublishedAsync(PublishContentContext context) => UpdateSitemapDeferredAsync(context);
+
+        public override Task UnpublishedAsync(PublishContentContext context) => UpdateSitemapDeferredAsync(context);
+
+        // Doing the update in a deferred and synchronized way makes sure that two simultaneous content item updates
+        // don't cause a ConcurrencyException due to the same sitemap document being updated.
+        private async Task UpdateSitemapDeferredAsync(ContentContextBase context)
         {
-            var updateContext = new SitemapUpdateContext
+            var contentItemId = context.ContentItem.ContentItemId;
+
+            var timeout = TimeSpan.FromMilliseconds(20_000);
+            (var locker, var locked) = await _distributedLock.TryAcquireLockAsync("SITEMAPS_UPDATE_LOCK", timeout, timeout);
+            if (!locked)
             {
-                UpdateObject = context.ContentItem,
-            };
-
-            return _sitemapUpdateHandler.UpdateSitemapAsync(updateContext);
-        }
-
-        public override Task UnpublishedAsync(PublishContentContext context)
-        {
-            var updateContext = new SitemapUpdateContext
+                throw new TimeoutException($"Couldn't acquire a lock to update the sitemap within {timeout.Seconds} seconds.");
+            }
+            else
             {
-                UpdateObject = context.ContentItem,
-            };
+                using (locker)
+                {
+                    ShellScope.AddDeferredTask(async scope =>
+                    {
+                        await using var subScope = await ShellScope.CreateChildScopeAsync();
 
-            return _sitemapUpdateHandler.UpdateSitemapAsync(updateContext);
+                        var contentManager = subScope.ServiceProvider.GetRequiredService<IContentManager>();
+                        var sitemapUpdateHandler = subScope.ServiceProvider.GetRequiredService<ISitemapUpdateHandler>();
+
+                        var updateContext = new SitemapUpdateContext
+                        {
+                            UpdateObject = await contentManager.GetAsync(contentItemId),
+                        };
+
+                        await sitemapUpdateHandler.UpdateSitemapAsync(updateContext);
+                    });
+                }
+            }
         }
     }
 }
