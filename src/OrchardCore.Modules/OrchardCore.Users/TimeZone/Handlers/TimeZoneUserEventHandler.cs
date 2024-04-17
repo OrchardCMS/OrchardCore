03ca16624f15d5fc5a9feee4fa1136ad9674484a
Keith Raven <keith@buzzinteractive.co.uk>
2024-04-17 22:09:47 +0100

    Clear user time zone cache entry on update or delete

---
+++ src/OrchardCore.Modules/OrchardCore.Users/TimeZone/Handlers/TimeZoneUserEventHandler.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Users.Handlers;
+using OrchardCore.Users.TimeZone.Services;
+
+namespace OrchardCore.Users.TimeZone.Handlers;
+internal class TimeZoneUserEventHandler : UserEventHandlerBase
+{
+    private readonly UserTimeZoneService _userTimeZoneService;
+
+    public TimeZoneUserEventHandler(UserTimeZoneService userTimeZoneService) => _userTimeZoneService = userTimeZoneService;
+
+    public override async Task DeletedAsync(UserDeleteContext context)
+    {
+        await _userTimeZoneService.UpdateUserTimeZoneAsync(context.User);
+    }
+
+    public override async Task UpdatedAsync(UserUpdateContext context)
+    {
+        await _userTimeZoneService.UpdateUserTimeZoneAsync(context.User);
+    }
+}
