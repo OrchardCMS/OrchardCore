51a1c19f995bc05fd0e3a04ae3a748d2ebdfed8c
Andy <zgofmontreal@gmail.com>
2024-04-18 12:34:48 -0400

    Use Permissions.ViewContent for GetEndpoint

--- src/OrchardCore.Modules/OrchardCore.Contents/Endpoints/Api/GetEndpoint.cs
+++ src/OrchardCore.Modules/OrchardCore.Contents/Endpoints/Api/GetEndpoint.cs
@@ -26,7 +26,7 @@ public static class GetEndpoint
         IAuthorizationService authorizationService,
         HttpContext httpContext)
     {
-        if (!await authorizationService.AuthorizeAsync(httpContext.User, Permissions.AccessContentApi))
+        if (!await authorizationService.AuthorizeAsync(httpContext.User, Permissions.ViewContent))
         {
             return httpContext.ChallengeOrForbid("Api");
         }
