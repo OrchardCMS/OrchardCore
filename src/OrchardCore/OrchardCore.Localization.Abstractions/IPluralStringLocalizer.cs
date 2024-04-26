62a0e0b8357e8c01ff8717604e7b527b370ef395
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2024-04-26 13:52:24 +0300

    IPluralStringLocalizer.GetTranslationAsync()

--- src/OrchardCore/OrchardCore.Localization.Abstractions/IPluralStringLocalizer.cs
+++ src/OrchardCore/OrchardCore.Localization.Abstractions/IPluralStringLocalizer.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Localization;
 
 namespace OrchardCore.Localization;
@@ -13,5 +15,15 @@ public interface IPluralStringLocalizer : IStringLocalizer
     /// <param name="name">The resource name.</param>
     /// <param name="arguments">Optional parameters that can be used inside the resource key.</param>
     /// <returns>A list of localized strings including the plural forms.</returns>
-    (LocalizedString, object[]) GetTranslation(string name, params object[] arguments);
+    [Obsolete("This method is deprecated, please use GetTranslationAsync instead.")]
+    (LocalizedString, object[]) GetTranslation(string name, params object[] arguments)
+        => GetTranslationAsync(name, arguments).GetAwaiter().GetResult();
+
+    /// <summary>
+    /// Gets the localized strings.
+    /// </summary>
+    /// <param name="name">The resource name.</param>
+    /// <param name="arguments">Optional parameters that can be used inside the resource key.</param>
+    /// <returns>A list of localized strings including the plural forms.</returns>
+    Task<(LocalizedString, object[])> GetTranslationAsync(string name, params object[] arguments);
 }
