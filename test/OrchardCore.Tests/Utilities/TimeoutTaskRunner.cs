8f4a2dad519f399578f491561d01cf3024db7e70
hyzx86 <yanzhong.han@jizhousoft.com>
2024-04-30 15:06:42 +0800

    update TimeoutTaskRunner

--- test/OrchardCore.Tests/Utilities/TimeoutTaskRunner.cs
+++ test/OrchardCore.Tests/Utilities/TimeoutTaskRunner.cs
@@ -2,7 +2,7 @@ namespace OrchardCore.Tests.Utilities;
 
 public class TimeoutTaskRunner
 {
-    public static async Task RunAsync(TimeSpan timeout, Func<Task<bool>> checkNextLoop, string failMessage)
+    public static async Task RunAsync(TimeSpan timeout, Func<Task<bool>> checkNextLoop, string failMessage, int interval = 500)
     {
         var timeoutTask = Task.Delay(timeout);
         while (true)
@@ -11,7 +11,7 @@ public class TimeoutTaskRunner
             {
                 Assert.Fail(failMessage);
             }
-            await Task.Delay(500);
+            await Task.Delay(interval);
             if (!await checkNextLoop.Invoke())
             {
                 break;
