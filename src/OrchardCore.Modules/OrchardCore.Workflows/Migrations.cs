4bb9e49b492ccc2b6cf1d4b0f567b54483187713
Tony Han <huarui2219@qq.com>
2024-05-14 01:28:04 +0800

    clear code

--- src/OrchardCore.Modules/OrchardCore.Workflows/Migrations.cs
+++ src/OrchardCore.Modules/OrchardCore.Workflows/Migrations.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http.HttpResults;
 using OrchardCore.Data.Migration;
 using OrchardCore.Modules;
 using OrchardCore.Workflows.Indexes;
