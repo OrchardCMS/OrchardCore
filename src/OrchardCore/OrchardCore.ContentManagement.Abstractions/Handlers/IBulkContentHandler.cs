b736b2fa1e85c1c8c2adac719048449ff59a384b
Tony Han <huarui2219@qq.com>
2024-05-17 14:55:23 +0800

    Update src/OrchardCore/OrchardCore.ContentManagement.Abstractions/Handlers/IBulkContentHandler.cs
    Co-authored-by: Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>

--- src/OrchardCore/OrchardCore.ContentManagement.Abstractions/Handlers/IBulkContentHandler.cs
+++ src/OrchardCore/OrchardCore.ContentManagement.Abstractions/Handlers/IBulkContentHandler.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace OrchardCore.ContentManagement.Handlers;
+
 public interface IBulkContentHandler
 {
     Task ImportingAsync(IEnumerable<ImportContentContext> contentItems);
