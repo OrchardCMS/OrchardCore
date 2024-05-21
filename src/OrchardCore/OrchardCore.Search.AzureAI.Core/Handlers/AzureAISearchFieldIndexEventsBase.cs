b59f7a73218fbe51c2bbec0ba2483f98fdf14acb
Mike Alhayek <mike@crestapps.com>
2024-05-21 13:06:39 -0700

    Update AzureAISearchFieldIndexEventsBase.cs

--- src/OrchardCore/OrchardCore.Search.AzureAI.Core/Handlers/AzureAISearchFieldIndexEventsBase.cs
+++ src/OrchardCore/OrchardCore.Search.AzureAI.Core/Handlers/AzureAISearchFieldIndexEventsBase.cs
@@ -5,9 +5,9 @@ namespace OrchardCore.Search.AzureAI.Handlers;
 
 public class AzureAISearchFieldIndexEventsBase : IAzureAISearchFieldIndexEvents
 {
-    public virtual Task MappedAsync(SearchIndexDefinition context)
-        => Task.CompletedTask;
-
     public virtual Task MappingAsync(SearchIndexDefinition context)
+        => Task.CompletedTask;
+   
+    public virtual Task MappedAsync(SearchIndexDefinition context)
         => Task.CompletedTask;
 }
