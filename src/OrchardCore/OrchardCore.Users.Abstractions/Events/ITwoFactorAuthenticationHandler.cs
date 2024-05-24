4415a6901e08e731104bdb43bc41c872b17b040e
Mike Alhayek <mike@crestapps.com>
2024-05-23 19:31:37 -0700

    docs

--- src/OrchardCore/OrchardCore.Users.Abstractions/Events/ITwoFactorAuthenticationHandler.cs
+++ src/OrchardCore/OrchardCore.Users.Abstractions/Events/ITwoFactorAuthenticationHandler.cs
@@ -7,6 +7,7 @@ public interface ITwoFactorAuthenticationHandler
     /// <summary>
     /// Checks if the two-factor authentication should be required for the given user or not.
     /// </summary>
+    /// <param name="user">An instance of the user to evaluate.</param>
     /// <returns>true if the two-factor authentication is required otherwise false.</returns>
     Task<bool> IsRequiredAsync(IUser user);
 }
