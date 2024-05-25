ff88852c0573807cb66330d7b2c534832ffd23e0
Mike Alhayek <mike@crestapps.com>
2024-05-24 21:34:14 -0700

    Show recipe error instead if throwing an exeption (#16148)
    ---------
    
    Co-authored-by: Hisham Bin Ateya <hishamco_2007@yahoo.com>
    Co-authored-by: Tony Han <huarui2219@qq.com>

--- src/OrchardCore/OrchardCore.Recipes.Abstractions/Models/RecipeStepResult.cs
+++ src/OrchardCore/OrchardCore.Recipes.Abstractions/Models/RecipeStepResult.cs
@@ -1,10 +1,14 @@
-namespace OrchardCore.Recipes.Models
+using System.Collections.Generic;
+
+namespace OrchardCore.Recipes.Models;
+
+public class RecipeStepResult
 {
-    public class RecipeStepResult
-    {
-        public string StepName { get; set; }
-        public bool IsCompleted { get; set; }
-        public bool IsSuccessful { get; set; }
-        public string ErrorMessage { get; set; }
-    }
+    public string StepName { get; set; }
+
+    public bool IsCompleted { get; set; }
+
+    public bool IsSuccessful { get; set; }
+
+    public Dictionary<string, string[]> Errors { get; set; }
 }
