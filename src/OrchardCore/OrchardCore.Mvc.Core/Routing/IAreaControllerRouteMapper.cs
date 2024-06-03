8fc7be66ad674a1a52773f66c4af117270b84e95
Sára El-Saig <sara.el-saig@lombiq.com>
2024-06-03 11:25:22 +0200

    Add IAreaControllerRouteMapper helper methods.

--- src/OrchardCore/OrchardCore.Mvc.Core/Routing/IAreaControllerRouteMapper.cs
+++ src/OrchardCore/OrchardCore.Mvc.Core/Routing/IAreaControllerRouteMapper.cs
@@ -7,5 +7,20 @@ namespace OrchardCore.Mvc.Routing
     {
         int Order { get; }
         bool TryMapAreaControllerRoute(IEndpointRouteBuilder routes, ControllerActionDescriptor descriptor);
+
+        public static string ReplaceMvcPlaceholders(string name, string area, string controller, string action) =>
+            name?
+                .Replace("{area}", area)
+                .Replace("{controller}", controller)
+                .Replace("{action}", action);
+
+        public static (string Area, string Controller, string Action) GetMvcRouteValues(ControllerActionDescriptor descriptor)
+        {
+            var area = descriptor.RouteValues["area"];
+            var controller = descriptor.ControllerName;
+            var action = descriptor.ActionName;
+
+            return (area, controller, action);
+        }
     }
 }
