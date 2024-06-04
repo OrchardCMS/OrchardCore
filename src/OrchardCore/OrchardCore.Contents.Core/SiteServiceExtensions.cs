70275a3465b9e69e06b44ff88ec4c883dd8ad37c
Mike Alhayek <mike@crestapps.com>
2024-06-04 11:20:21 -0700

    Update SiteServiceExtensions.cs
    Co-authored-by: Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>

--- src/OrchardCore/OrchardCore.Contents.Core/SiteServiceExtensions.cs
+++ src/OrchardCore/OrchardCore.Contents.Core/SiteServiceExtensions.cs
@@ -7,11 +7,10 @@ namespace OrchardCore.Settings;
 public static class SiteServiceExtensions
 {
     /// <summary>
-    /// Gets an instance of the specified settings if it exists.
+    /// Gets an instance of the specified custom settings if it exists.
     /// </summary>
-    /// <param name="siteService">The site service.</param>
     /// <param name="name">The entry name.</param>
-    /// <returns>An <see cref="ContentItem" /> instance that matches the given name, if one exists.</returns>
+    /// <returns>A <see cref="ContentItem" /> instance that matches the given name, if one exists.</returns>
     public static async Task<ContentItem> GetCustomSettingsAsync(this ISiteService siteService, string name)
         => (await siteService.GetSiteSettingsAsync()).As<ContentItem>(name);
 }
