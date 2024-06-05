e90c70045a53e016176a9b7d1bc7d8d17d8a8bf4
Mike Alhayek <mike@crestapps.com>
2024-06-05 07:15:39 -0700

    Address feddback

--- src/Templates/OrchardCore.ProjectTemplates/content/OrchardCore.Templates.Mvc.Module/Startup.cs
+++ src/Templates/OrchardCore.ProjectTemplates/content/OrchardCore.Templates.Mvc.Module/Startup.cs
@@ -1,13 +1,17 @@
-namespace OrchardCore.Templates.Mvc.Module
-{
-    public sealed class Startup : StartupBase
-    {
-        public override void ConfigureServices(IServiceCollection services)
-        {
-        }
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Modules;
 
-        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
-        {
-        }
+namespace OrchardCore.Templates.Mvc.Module;
+
+public sealed class Startup : StartupBase
+{
+    public override void ConfigureServices(IServiceCollection services)
+    {
+    }
+
+    public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+    {
     }
 }
