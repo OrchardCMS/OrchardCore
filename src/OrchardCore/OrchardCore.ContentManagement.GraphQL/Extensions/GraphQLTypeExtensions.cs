2f40a25cb7ee1c0f22d17fc4739cb093b7f43fe6
Georg von Kries <gvk@creativbox.net>
2024-06-05 09:23:18 +0200

    Added documentation to HasFieldIgnoreCase().

--- src/OrchardCore/OrchardCore.ContentManagement.GraphQL/Extensions/GraphQLTypeExtensions.cs
+++ src/OrchardCore/OrchardCore.ContentManagement.GraphQL/Extensions/GraphQLTypeExtensions.cs
@@ -12,6 +12,18 @@ public static class GraphQLTypeExtensions
     public static FieldType WithPartNameMetaData(this FieldType fieldType, string partName)
         => fieldType.WithMetaData("PartName", partName);
 
+    /// <summary>
+    /// Checks if the field exists in the GraphQL type in a case-insensitive way.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This is the same as calling <see cref="IComplexGraphType.HasField(string)"/> but in a case-insensitive way. OC
+    /// fields may be added with different casings, and we want to avoid collisions even then.
+    /// </para>
+    /// <para>
+    /// See <see href="https://github.com/OrchardCMS/OrchardCore/pull/16151"/> and its corresponding issues for context.
+    /// </para>
+    /// </remarks>
     public static bool HasFieldIgnoreCase(this IComplexGraphType graphType, string fieldName)
         => graphType.Fields.Any(field => field.Name.Equals(fieldName, StringComparison.OrdinalIgnoreCase));
 
