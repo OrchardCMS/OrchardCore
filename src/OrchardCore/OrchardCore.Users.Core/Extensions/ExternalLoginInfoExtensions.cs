7e50a95783ae21a6676188350c5157a3b56d2b29
tempcode <piemp@hotmail.it>
2024-06-12 08:59:42 +0200

    style: code to new line in ExternalLoginInfoExtensions
    Co-authored-by: Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>

--- src/OrchardCore/OrchardCore.Users.Core/Extensions/ExternalLoginInfoExtensions.cs
+++ src/OrchardCore/OrchardCore.Users.Core/Extensions/ExternalLoginInfoExtensions.cs
@@ -5,6 +5,5 @@ namespace Microsoft.AspNetCore.Identity;
 public static class ExternalLoginInfoExtensions
 {
     public static string GetEmail(this ExternalLoginInfo info)
-        => info.Principal.FindFirstValue(ClaimTypes.Email)
-        ?? info.Principal.FindFirstValue("email");
+        => info.Principal.FindFirstValue(ClaimTypes.Email) ?? info.Principal.FindFirstValue("email");
 }
