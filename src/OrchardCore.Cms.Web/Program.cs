fd79d254aa17dc73f4571f37ed2662eac957addf
Sára El-Saig <sara.el-saig@lombiq.com>
2024-06-19 19:13:57 +0200

    Revert EnableThreadSafetyChecks.

--- src/OrchardCore.Cms.Web/Program.cs
+++ src/OrchardCore.Cms.Web/Program.cs
@@ -1,4 +1,3 @@
-using OrchardCore.Data.YesSql;
 using OrchardCore.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,9 +7,6 @@ builder.Host.UseNLogHost();
 builder.Services
     .AddOrchardCms()
     .AddSetupFeatures("OrchardCore.AutoSetup");
-
-// Temporary.
-builder.Services.Configure<YesSqlOptions>(options => options.EnableThreadSafetyChecks = true);
 
 var app = builder.Build();
 
