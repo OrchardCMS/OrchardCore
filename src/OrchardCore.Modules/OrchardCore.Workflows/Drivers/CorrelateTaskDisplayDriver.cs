f95dc8a2f7250ad8cb1e2c24fbfab86309923073
lampersky <lampersky@gmail.com>
2024-06-21 19:21:17 +0200

    #10849 Liquid syntax support for CorrelateTask (#12021)
    ---------
    
    Co-authored-by: Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
    Co-authored-by: Mike Alhayek <mike@crestapps.com>

--- src/OrchardCore.Modules/OrchardCore.Workflows/Drivers/CorrelateTaskDisplayDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.Workflows/Drivers/CorrelateTaskDisplayDriver.cs
@@ -10,11 +10,13 @@ namespace OrchardCore.Workflows.Drivers
         protected override void EditActivity(CorrelateTask activity, CorrelateTaskViewModel model)
         {
             model.Value = activity.Value.Expression;
+            model.Syntax = activity.Syntax;
         }
 
         protected override void UpdateActivity(CorrelateTaskViewModel model, CorrelateTask activity)
         {
             activity.Value = new WorkflowExpression<string>(model.Value);
+            activity.Syntax = model.Syntax;
         }
     }
 }
