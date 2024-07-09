4f5a75123666ff41c8ce2b0773ec27e235b3b2a5
Mike Alhayek <mike@crestapps.com>
2024-07-09 16:41:38 -0700

    Deserialize compressed documents correctly Fix #16414
    Related Work Items: #16

--- src/OrchardCore/OrchardCore.Infrastructure/Documents/DefaultDocumentSerializer.cs
+++ src/OrchardCore/OrchardCore.Infrastructure/Documents/DefaultDocumentSerializer.cs
@@ -1,6 +1,7 @@
+using System;
 using System.IO;
 using System.IO.Compression;
-using System.Text;
+using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using OrchardCore.Data.Documents;
@@ -14,13 +15,10 @@ namespace OrchardCore.Documents
     {
         public static readonly DefaultDocumentSerializer Instance = new();
 
-        public DefaultDocumentSerializer()
+        public Task<byte[]> SerializeAsync<TDocument>(TDocument document, int compressThreshold = int.MaxValue)
+            where TDocument : class, IDocument, new()
         {
-        }
-
-        public Task<byte[]> SerializeAsync<TDocument>(TDocument document, int compressThreshold = int.MaxValue) where TDocument : class, IDocument, new()
-        {
-            var data = Encoding.UTF8.GetBytes(JConvert.SerializeObject(document));
+            var data = JsonSerializer.SerializeToUtf8Bytes(document, JOptions.Default);
             if (data.Length >= compressThreshold)
             {
                 data = Compress(data);
@@ -29,22 +27,34 @@ namespace OrchardCore.Documents
             return Task.FromResult(data);
         }
 
-        public Task<TDocument> DeserializeAsync<TDocument>(byte[] data) where TDocument : class, IDocument, new()
+        public Task<TDocument> DeserializeAsync<TDocument>(byte[] data)
+            where TDocument : class, IDocument, new()
         {
             if (IsCompressed(data))
             {
                 data = Decompress(data);
             }
 
-            var document = JConvert.DeserializeObject<TDocument>(Encoding.UTF8.GetString(data));
+            using var ms = new MemoryStream(data);
+
+            var document = JsonSerializer.Deserialize<TDocument>(ms, JOptions.Default);
 
             return Task.FromResult(document);
         }
 
         private static readonly byte[] _gZipHeaderBytes = [0x1f, 0x8b];
 
-        internal static bool IsCompressed(byte[] data) =>
-            data.Length < _gZipHeaderBytes.Length && data[0..1] == _gZipHeaderBytes;
+        internal static bool IsCompressed(byte[] data)
+        {
+            // Ensure data is at least as long as the GZip header
+            if (data.Length >= _gZipHeaderBytes.Length)
+            {
+                // Compare the header bytes.
+                return data.Take(_gZipHeaderBytes.Length).SequenceEqual(_gZipHeaderBytes);
+            }
+
+            return false;
+        }
 
         internal static byte[] Compress(byte[] data)
         {
