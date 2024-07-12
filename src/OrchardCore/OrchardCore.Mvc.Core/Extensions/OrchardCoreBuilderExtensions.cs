c8adf4677764c489e93563fab9fa5c8f0d69cc8f
Mike Alhayek <mike@crestapps.com>
2024-07-12 09:21:14 -0700

    Suppress Check For Unhandled Security Metadata Fix #16429

--- src/OrchardCore/OrchardCore.Mvc.Core/Extensions/OrchardCoreBuilderExtensions.cs
+++ src/OrchardCore/OrchardCore.Mvc.Core/Extensions/OrchardCoreBuilderExtensions.cs
@@ -15,6 +15,13 @@ namespace Microsoft.Extensions.DependencyInjection
             {
                 // Allows a tenant to add its own route endpoint schemes for link generation.
                 collection.AddSingleton<IEndpointAddressScheme<RouteValuesAddress>, ShellRouteValuesAddressScheme>();
+
+                collection.Configure<RouteOptions>(options =>
+                {
+                    // The Cors module is designed to deal with CORS, so we skip checking for unhandled security metadata by default.
+                    // In addition to a small performance benefit to skip checking for security metadata on endpoint.
+                    options.SuppressCheckForUnhandledSecurityMetadata = true;
+                });
             },
             // Need to be registered last.
             order: int.MaxValue - 100);
