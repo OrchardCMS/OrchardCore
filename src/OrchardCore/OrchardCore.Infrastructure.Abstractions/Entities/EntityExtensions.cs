6f8045a533717fb236295cb41a73045e86de2b7e
Mike Alhayek <mike@crestapps.com>
2024-07-18 17:15:36 -0700

    rename property to value and use file-scope namespace

--- src/OrchardCore/OrchardCore.Infrastructure.Abstractions/Entities/EntityExtensions.cs
+++ src/OrchardCore/OrchardCore.Infrastructure.Abstractions/Entities/EntityExtensions.cs
@@ -2,130 +2,129 @@ using System;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 
-namespace OrchardCore.Entities
+namespace OrchardCore.Entities;
+
+public static class EntityExtensions
 {
-    public static class EntityExtensions
+    /// <summary>
+    /// Extracts the specified type of property.
+    /// </summary>
+    /// <param name="entity">The <see cref="IEntity"/>.</param>
+    /// <typeparam name="T">The type of the property to extract.</typeparam>
+    /// <returns>A new instance of the requested type if the property was not found.</returns>
+    public static T As<T>(this IEntity entity)
+        where T : new()
+        => entity.As<T>(typeof(T).Name);
+
+    /// <summary>
+    /// Extracts the specified named property.
+    /// </summary>
+    /// <typeparam name="T">The type of the property to extract.</typeparam>
+    /// <param name="entity">The <see cref="IEntity"/>.</param>
+    /// <param name="name">The name of the property to extract.</param>
+    /// <returns>A new instance of the requested type if the property was not found.</returns>
+    public static T As<T>(this IEntity entity, string name)
+        where T : new()
     {
-        /// <summary>
-        /// Extracts the specified type of property.
-        /// </summary>
-        /// <param name="entity">The <see cref="IEntity"/>.</param>
-        /// <typeparam name="T">The type of the property to extract.</typeparam>
-        /// <returns>A new instance of the requested type if the property was not found.</returns>
-        public static T As<T>(this IEntity entity)
-            where T : new()
-            => entity.As<T>(typeof(T).Name);
+        ArgumentNullException.ThrowIfNull(name);
 
-        /// <summary>
-        /// Extracts the specified named property.
-        /// </summary>
-        /// <typeparam name="T">The type of the property to extract.</typeparam>
-        /// <param name="entity">The <see cref="IEntity"/>.</param>
-        /// <param name="name">The name of the property to extract.</param>
-        /// <returns>A new instance of the requested type if the property was not found.</returns>
-        public static T As<T>(this IEntity entity, string name)
-            where T : new()
+        if (entity.Properties.TryGetPropertyValue(name, out var value))
         {
-            ArgumentNullException.ThrowIfNull(name);
-
-            if (entity.Properties.TryGetPropertyValue(name, out var value))
-            {
-                return value.Deserialize<T>(JOptions.Default);
-            }
-
-            return new T();
+            return value.Deserialize<T>(JOptions.Default);
         }
 
-        /// <summary>
-        /// Indicates if the specified type of property is attached to the <see cref="IEntity"/> instance.
-        /// </summary>
-        /// <typeparam name="T">The type of the property to check.</typeparam>
-        /// <param name="entity">The <see cref="IEntity"/>.</param>
-        /// <returns>True if the property was found, otherwise false.</returns>
-        public static bool Has<T>(this IEntity entity)
-            => entity.Has(typeof(T).Name);
+        return new T();
+    }
 
-        /// <summary>
-        /// Indicates if the specified property is attached to the <see cref="IEntity"/> instance.
-        /// </summary>
-        /// <param name="entity">The <see cref="IEntity"/>.</param>
-        /// <param name="name">The name of the property to check.</param>
-        /// <returns>True if the property was found, otherwise false.</returns>
-        public static bool Has(this IEntity entity, string name)
-            => name != null && entity.Properties.ContainsKey(name);
+    /// <summary>
+    /// Indicates if the specified type of property is attached to the <see cref="IEntity"/> instance.
+    /// </summary>
+    /// <typeparam name="T">The type of the property to check.</typeparam>
+    /// <param name="entity">The <see cref="IEntity"/>.</param>
+    /// <returns>True if the property was found, otherwise false.</returns>
+    public static bool Has<T>(this IEntity entity)
+        => entity.Has(typeof(T).Name);
 
-        public static IEntity Put<T>(this IEntity entity, T aspect)
-            where T : new()
-            => entity.Put(typeof(T).Name, aspect);
+    /// <summary>
+    /// Indicates if the specified property is attached to the <see cref="IEntity"/> instance.
+    /// </summary>
+    /// <param name="entity">The <see cref="IEntity"/>.</param>
+    /// <param name="name">The name of the property to check.</param>
+    /// <returns>True if the property was found, otherwise false.</returns>
+    public static bool Has(this IEntity entity, string name)
+        => name != null && entity.Properties.ContainsKey(name);
 
-        public static bool TryGet<T>(this IEntity entity, out T aspect)
-            where T : new()
-            => entity.TryGet(typeof(T).Name, out aspect);
+    public static IEntity Put<T>(this IEntity entity, T aspect)
+        where T : new()
+        => entity.Put(typeof(T).Name, aspect);
 
-        public static bool TryGet<T>(this IEntity entity, string name, out T aspect)
-            where T : new()
+    public static bool TryGet<T>(this IEntity entity, out T aspect)
+        where T : new()
+        => entity.TryGet(typeof(T).Name, out aspect);
+
+    public static bool TryGet<T>(this IEntity entity, string name, out T aspect)
+        where T : new()
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+
+        if (entity.Properties.TryGetPropertyValue(name, out var value))
         {
-            ArgumentException.ThrowIfNullOrEmpty(name);
+            aspect = value.ToObject<T>();
 
-            if (entity.Properties.TryGetPropertyValue(name, out var value))
-            {
-                aspect = value.ToObject<T>();
-
-                return true;
-            }
-
-            aspect = default;
-
-            return false;
+            return true;
         }
 
-        public static IEntity Put(this IEntity entity, string name, object property)
+        aspect = default;
+
+        return false;
+    }
+
+    public static IEntity Put(this IEntity entity, string name, object value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+
+        entity.Properties[name] = JObject.FromObject(value);
+
+        return entity;
+    }
+
+    /// <summary>
+    /// Modifies or create an aspect.
+    /// </summary>
+    /// <param name="entity">The <see cref="IEntity"/>.</param>
+    /// <param name="action">An action to apply on the aspect.</param>
+    /// <returns>The current <see cref="IEntity"/> instance.</returns>
+    public static IEntity Alter<TAspect>(this IEntity entity, Action<TAspect> action)
+        where TAspect : new()
+        => entity.Alter(typeof(TAspect).Name, action);
+
+    /// <summary>
+    /// Modifies or create an aspect.
+    /// </summary>
+    /// <param name="entity">The <see cref="IEntity"/>.</param>
+    /// <param name="name">The name of the aspect.</param>
+    /// <param name="action">An action to apply on the aspect.</param>
+    /// <returns>The current <see cref="IEntity"/> instance.</returns>
+    public static IEntity Alter<TAspect>(this IEntity entity, string name, Action<TAspect> action)
+        where TAspect : new()
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+
+        TAspect obj;
+
+        if (!entity.Properties.TryGetPropertyValue(name, out var value))
         {
-            ArgumentException.ThrowIfNullOrEmpty(name);
-
-            entity.Properties[name] = JObject.FromObject(property);
-
-            return entity;
+            obj = new TAspect();
+        }
+        else
+        {
+            obj = value.Deserialize<TAspect>(JOptions.Default);
         }
 
-        /// <summary>
-        /// Modifies or create an aspect.
-        /// </summary>
-        /// <param name="entity">The <see cref="IEntity"/>.</param>
-        /// <param name="action">An action to apply on the aspect.</param>
-        /// <returns>The current <see cref="IEntity"/> instance.</returns>
-        public static IEntity Alter<TAspect>(this IEntity entity, Action<TAspect> action)
-            where TAspect : new()
-            => entity.Alter(typeof(TAspect).Name, action);
+        action?.Invoke(obj);
 
-        /// <summary>
-        /// Modifies or create an aspect.
-        /// </summary>
-        /// <param name="entity">The <see cref="IEntity"/>.</param>
-        /// <param name="name">The name of the aspect.</param>
-        /// <param name="action">An action to apply on the aspect.</param>
-        /// <returns>The current <see cref="IEntity"/> instance.</returns>
-        public static IEntity Alter<TAspect>(this IEntity entity, string name, Action<TAspect> action)
-            where TAspect : new()
-        {
-            ArgumentException.ThrowIfNullOrEmpty(name);
+        entity.Put(name, obj);
 
-            TAspect obj;
-
-            if (!entity.Properties.TryGetPropertyValue(name, out var value))
-            {
-                obj = new TAspect();
-            }
-            else
-            {
-                obj = value.Deserialize<TAspect>(JOptions.Default);
-            }
-
-            action?.Invoke(obj);
-
-            entity.Put(name, obj);
-
-            return entity;
-        }
+        return entity;
     }
 }
