051f733e1ca23693416ddc2ef4516e8eaf187ca3
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2024-07-21 10:26:05 +0300

    Update src/OrchardCore.Modules/OrchardCore.Resources/Liquid/ResourcesTag.cs

--- src/OrchardCore.Modules/OrchardCore.Resources/Liquid/ResourcesTag.cs
+++ src/OrchardCore.Modules/OrchardCore.Resources/Liquid/ResourcesTag.cs
@@ -18,7 +18,7 @@ namespace OrchardCore.Resources.Liquid
             var services = ((LiquidTemplateContext)context).Services;
             var processors = services.GetRequiredService<IEnumerable<IResourcesTagHelperProcessor>>();
 
-            var processorContext = new ResourcesTagHelperProcessorContext(writer, ResourceTagType.Footer);
+            var processorContext = new ResourcesTagHelperProcessorContext(ResourceTagType.Footer, writer);
 
             foreach (var argument in argumentsList)
             {
