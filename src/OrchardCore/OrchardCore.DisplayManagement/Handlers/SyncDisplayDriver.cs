be5c810a109c02c142a121a1ce9ee38b4143effe
Sára El-Saig <sara.el-saig@lombiq.com>
2024-08-01 22:08:16 +0200

    Let the update stay async.

--- src/OrchardCore/OrchardCore.DisplayManagement/Handlers/SyncDisplayDriver.cs
+++ src/OrchardCore/OrchardCore.DisplayManagement/Handlers/SyncDisplayDriver.cs
@@ -4,8 +4,8 @@ using OrchardCore.DisplayManagement.Views;
 namespace OrchardCore.DisplayManagement.Handlers;
 
 /// <summary>
-/// A version of <see cref="DisplayDriver{TModel,TDisplayContext,TEditorContext,TUpdateContext}"/> with only synchronous
-/// overridable methods.
+/// A version of <see cref="DisplayDriver{TModel,TDisplayContext,TEditorContext,TUpdateContext}"/> where only the update
+/// method is asynchronous.
 /// </summary>
 public abstract class SyncDisplayDriver<TModel, TDisplayContext, TEditorContext, TUpdateContext> :
     DisplayDriver<TModel, TDisplayContext, TEditorContext, TUpdateContext>
@@ -18,22 +18,20 @@ public abstract class SyncDisplayDriver<TModel, TDisplayContext, TEditorContext,
 
     public new virtual IDisplayResult Edit(TModel model, TEditorContext context) => NullShapeResult();
 
-    public virtual IDisplayResult Update(TModel model, TUpdateContext context) => Edit(model, context);
-
     public sealed override Task<IDisplayResult> DisplayAsync(TModel model, TDisplayContext context) =>
         Task.FromResult(Display(model, context));
 
     public sealed override Task<IDisplayResult> EditAsync(TModel model, TEditorContext context) =>
         Task.FromResult(Edit(model, context));
 
-    public sealed override Task<IDisplayResult> UpdateAsync(TModel model, TUpdateContext context) =>
+    public override Task<IDisplayResult> UpdateAsync(TModel model, TUpdateContext context) =>
         Task.FromResult(Edit(model, context));
 
     private static IDisplayResult NullShapeResult() => null;
 }
 
 /// <summary>
-/// A version of <see cref="DisplayDriver{TModel}"/> with only synchronous overridable methods.
+/// A version of <see cref="DisplayDriver{TModel}"/> where only the update method is asynchronous.
 /// </summary>
 /// <typeparam name="TModel"></typeparam>
 public abstract class SyncDisplayDriver<TModel> :
