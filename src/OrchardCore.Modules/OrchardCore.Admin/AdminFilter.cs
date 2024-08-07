edfa684dea59c1da58380be944c61eadb1f307d9
Mike Alhayek <mike@crestapps.com>
2024-08-07 15:20:07 -0700

    test CLI

--- src/OrchardCore.Modules/OrchardCore.Admin/AdminFilter.cs
+++ src/OrchardCore.Modules/OrchardCore.Admin/AdminFilter.cs
@@ -4,62 +4,63 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
-namespace OrchardCore.Admin;
-
-/// <summary>
-/// Intercepts any request to check whether it applies to the admin site.
-/// If so it marks the request as such and ensures the user as the right to access it.
-/// </summary>
-public sealed class AdminFilter : ActionFilterAttribute, IAsyncPageFilter
+namespace OrchardCore.Admin
 {
-    private readonly IAuthorizationService _authorizationService;
-
-    public AdminFilter(IAuthorizationService authorizationService)
+    /// <summary>
+    /// Intercepts any request to check whether it applies to the admin site.
+    /// If so it marks the request as such and ensures the user as the right to access it.
+    /// </summary>
+    public sealed class AdminFilter : ActionFilterAttribute, IAsyncPageFilter
     {
-        _authorizationService = authorizationService;
-    }
+        private readonly IAuthorizationService _authorizationService;
 
-    public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
-    {
-        ArgumentNullException.ThrowIfNull(context);
-
-        if (!await AuthorizeAsync(context.HttpContext))
+        public AdminFilter(IAuthorizationService authorizationService)
         {
-            context.Result = context.HttpContext.User?.Identity?.IsAuthenticated ?? false
-                ? new ForbidResult()
-                : new ChallengeResult();
-            return;
+            _authorizationService = authorizationService;
         }
 
-        await base.OnActionExecutionAsync(context, next);
-    }
-
-    public async Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context, PageHandlerExecutionDelegate next)
-    {
-        ArgumentNullException.ThrowIfNull(context);
-
-        if (!await AuthorizeAsync(context.HttpContext))
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
-            context.Result = new ChallengeResult();
-            return;
+            ArgumentNullException.ThrowIfNull(context);
+
+            if (!await AuthorizeAsync(context.HttpContext))
+            {
+                context.Result = context.HttpContext.User?.Identity?.IsAuthenticated ?? false
+                    ? new ForbidResult()
+                    : new ChallengeResult();
+                return;
+            }
+
+            await base.OnActionExecutionAsync(context, next);
         }
 
-        // Do post work.
-        await next.Invoke();
-    }
-
-    public Task OnPageHandlerSelectionAsync(PageHandlerSelectedContext context)
-    {
-        return Task.CompletedTask;
-    }
-
-    private Task<bool> AuthorizeAsync(Microsoft.AspNetCore.Http.HttpContext context)
-    {
-        if (AdminAttribute.IsApplied(context))
+        public async Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context, PageHandlerExecutionDelegate next)
         {
-            return _authorizationService.AuthorizeAsync(context.User, AdminPermissions.AccessAdminPanel);
+            ArgumentNullException.ThrowIfNull(context);
+
+            if (!await AuthorizeAsync(context.HttpContext))
+            {
+                context.Result = new ChallengeResult();
+                return;
+            }
+
+            // Do post work.
+            await next.Invoke();
         }
 
-        return Task.FromResult(true);
+        public Task OnPageHandlerSelectionAsync(PageHandlerSelectedContext context)
+        {
+            return Task.CompletedTask;
+        }
+
+        private Task<bool> AuthorizeAsync(Microsoft.AspNetCore.Http.HttpContext context)
+        {
+            if (AdminAttribute.IsApplied(context))
+            {
+                return _authorizationService.AuthorizeAsync(context.User, AdminPermissions.AccessAdminPanel);
+            }
+
+            return Task.FromResult(true);
+        }
     }
 }
