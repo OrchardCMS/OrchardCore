81449c720390e6220e6f828b9b9d340eb525c2f1
Andy <zgofmontreal@gmail.com>
2024-08-13 22:34:47 -0400

    Add HttpContextExtensions for GetActionContextAsync

---
+++ src/OrchardCore/OrchardCore.DisplayManagement/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,39 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OrchardCore.DisplayManagement.Extensions;
+public static class HttpContextExtensions
+{
+    public static async Task<ActionContext> GetActionContextAsync(this IHttpContextAccessor httpContextAccessor)
+    {
+        var httpContext = httpContextAccessor.HttpContext;
+        var actionContext = httpContext.RequestServices.GetService<IActionContextAccessor>()?.ActionContext;
+
+        if (actionContext != null)
+        {
+            return actionContext;
+        }
+
+        return await GetActionContextAsync(httpContext);
+    }
+    public static async Task<ActionContext> GetActionContextAsync(this HttpContext httpContext)
+    {
+        var routeData = new RouteData();
+        routeData.Routers.Add(new RouteCollection());
+
+        var actionContext = new ActionContext(httpContext, routeData, new ActionDescriptor());
+        var filters = httpContext.RequestServices.GetServices<IAsyncViewActionFilter>();
+
+        foreach (var filter in filters)
+        {
+            await filter.OnActionExecutionAsync(actionContext);
+        }
+
+        return actionContext;
+    }
+}
