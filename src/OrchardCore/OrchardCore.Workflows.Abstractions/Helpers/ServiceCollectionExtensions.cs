c1f330e005d4f13e333e07b1498dab71f96b1512
Dean Marcussen <dmarcussen@topcon.com>
2024-08-15 21:24:01 +1200

    Return services from workflow registration extension methods (#16568)
    Co-authored-by: Hisham Bin Ateya <hishamco_2007@yahoo.com>

--- src/OrchardCore/OrchardCore.Workflows.Abstractions/Helpers/ServiceCollectionExtensions.cs
+++ src/OrchardCore/OrchardCore.Workflows.Abstractions/Helpers/ServiceCollectionExtensions.cs
@@ -7,8 +7,10 @@ namespace OrchardCore.Workflows.Helpers;
 
 public static class ServiceCollectionExtensions
 {
-    public static void AddActivity<TActivity, TDriver>(this IServiceCollection services) where TActivity : class, IActivity where TDriver : class, IDisplayDriver<IActivity>
+    public static IServiceCollection AddActivity<TActivity, TDriver>(this IServiceCollection services) where TActivity : class, IActivity where TDriver : class, IDisplayDriver<IActivity>
     {
         services.Configure<WorkflowOptions>(options => options.RegisterActivity<TActivity, TDriver>());
+
+        return services;
     }
 }
