796ae4ac40f8b3a4932f0430c903ceae400ff37e
Mike Alhayek <mike@crestapps.com>
2024-08-21 06:10:47 -0700

    add the index to the create

--- src/OrchardCore.Modules/OrchardCore.Notifications/Migrations/NotificationMigrations.cs
+++ src/OrchardCore.Modules/OrchardCore.Notifications/Migrations/NotificationMigrations.cs
@@ -29,7 +29,16 @@ public sealed class NotificationMigrations : DataMigration
             collection: NotificationConstants.NotificationCollection
         );
 
-        return 1;
+        await SchemaBuilder.AlterIndexTableAsync<NotificationIndex>(table => table
+            .CreateIndex("IDX_NotificationIndex_UserId",
+                "DocumentId",
+                "UserId",
+                "IsRead",
+                "CreatedAtUtc"),
+            collection: NotificationConstants.NotificationCollection
+        );
+
+        return 2;
     }
 
     public async Task<int> UpdateFrom1Async()
