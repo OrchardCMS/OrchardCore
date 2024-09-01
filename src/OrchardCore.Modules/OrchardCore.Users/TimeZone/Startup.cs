971e135549708ecffe5d1fab009b3d1495b900ac
Mike Alhayek <mike@crestapps.com>
2024-09-01 12:59:07 -0700

    finalize

--- src/OrchardCore.Modules/OrchardCore.Users/TimeZone/Startup.cs
+++ src/OrchardCore.Modules/OrchardCore.Users/TimeZone/Startup.cs
@@ -13,7 +13,7 @@ public sealed class Startup : StartupBase
     public override void ConfigureServices(IServiceCollection services)
     {
         services.AddScoped<ITimeZoneSelector, UserTimeZoneSelector>();
-        services.AddSingleton<IUserTimeZoneService, UserTimeZoneService>();
+        services.AddScoped<IUserTimeZoneService, UserTimeZoneService>();
         services.AddScoped<IDisplayDriver<User>, UserTimeZoneDisplayDriver>();
     }
 }
