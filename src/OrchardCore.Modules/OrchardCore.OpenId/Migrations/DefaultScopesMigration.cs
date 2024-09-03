1ce9d00038f4e72c12da46417ecb514b2329d638
Mike Alhayek <mike@crestapps.com>
2024-09-03 12:10:13 -0700

    Update DefaultScopesMigration.cs

--- src/OrchardCore.Modules/OrchardCore.OpenId/Migrations/DefaultScopesMigration.cs
+++ src/OrchardCore.Modules/OrchardCore.OpenId/Migrations/DefaultScopesMigration.cs
@@ -25,18 +25,6 @@ public sealed class DefaultScopesMigration : DataMigration
                 existingNames.Add(await scopeManager.GetNameAsync(scope));
             }
 
-            if (!existingNames.Contains("openid"))
-            {
-                var descriptor = new OpenIdScopeDescriptor
-                {
-                    DisplayName = S["OpenID"],
-                    Name = "openid",
-                    Description = S["This scope is required for all OpenID Connect requests. It indicates that the client wants to authenticate the user and obtain an ID token. This scope enables the OpenID Connect protocol and allows for the retrieval of the user's identity information."]
-                };
-
-                await scopeManager.CreateAsync(descriptor);
-            }
-
             if (!existingNames.Contains("profile"))
             {
                 var descriptor = new OpenIdScopeDescriptor
@@ -68,18 +56,6 @@ public sealed class DefaultScopesMigration : DataMigration
                     DisplayName = S["Phone"],
                     Name = "phone",
                     Description = S["Requests access to the user's phone number. This scope includes the phone_number and phone_number_verified claims, which provide the user's phone number and indicate whether it has been verified."]
-                };
-
-                await scopeManager.CreateAsync(descriptor);
-            }
-
-            if (!existingNames.Contains("offline_access"))
-            {
-                var descriptor = new OpenIdScopeDescriptor
-                {
-                    DisplayName = S["Offline Access"],
-                    Name = "offline_access",
-                    Description = S["Requests a refresh token to allow the client to obtain new access tokens without requiring user interaction. This scope is used to obtain long-lived refresh tokens, enabling the client to access resources even when the user is not actively interacting with the application."]
                 };
 
                 await scopeManager.CreateAsync(descriptor);
