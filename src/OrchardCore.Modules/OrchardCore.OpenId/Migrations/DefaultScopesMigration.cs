dc09a545a545efc3faebb58fe2d17e2531dc29fd
Mike Alhayek <mike@crestapps.com>
2024-09-03 12:14:24 -0700

    better code

--- src/OrchardCore.Modules/OrchardCore.OpenId/Migrations/DefaultScopesMigration.cs
+++ src/OrchardCore.Modules/OrchardCore.OpenId/Migrations/DefaultScopesMigration.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Localization;
+using OpenIddict.Abstractions;
 using OrchardCore.Data.Migration;
 using OrchardCore.Environment.Shell.Scope;
 using OrchardCore.OpenId.Abstractions.Descriptors;
@@ -18,43 +19,36 @@ public sealed class DefaultScopesMigration : DataMigration
             var S = shellScope.ServiceProvider.GetService<IStringLocalizer<DefaultScopesMigration>>();
             var scopeManager = shellScope.ServiceProvider.GetRequiredService<IOpenIdScopeManager>();
 
-            var existingNames = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
-
-            await foreach (var scope in scopeManager.ListAsync())
-            {
-                existingNames.Add(await scopeManager.GetNameAsync(scope));
-            }
-
-            if (!existingNames.Contains("profile"))
+            if (await scopeManager.FindByNameAsync(OpenIddictConstants.Scopes.Profile) == null)
             {
                 var descriptor = new OpenIdScopeDescriptor
                 {
                     DisplayName = S["Profile"],
-                    Name = "profile",
+                    Name = OpenIddictConstants.Scopes.Profile,
                     Description = S["Requests access to the user's default profile information."]
                 };
 
                 await scopeManager.CreateAsync(descriptor);
             }
 
-            if (!existingNames.Contains("email"))
+            if (await scopeManager.FindByNameAsync(OpenIddictConstants.Scopes.Email) == null)
             {
                 var descriptor = new OpenIdScopeDescriptor
                 {
                     DisplayName = S["Email"],
-                    Name = "email",
+                    Name = OpenIddictConstants.Scopes.Email,
                     Description = S["Requests access to the user's email address. This scope provides the email and email_verified claims, which indicate the user's email address and whether it has been verified."]
                 };
 
                 await scopeManager.CreateAsync(descriptor);
             }
 
-            if (!existingNames.Contains("phone"))
+            if (await scopeManager.FindByNameAsync(OpenIddictConstants.Scopes.Phone) == null)
             {
                 var descriptor = new OpenIdScopeDescriptor
                 {
                     DisplayName = S["Phone"],
-                    Name = "phone",
+                    Name = OpenIddictConstants.Scopes.Phone,
                     Description = S["Requests access to the user's phone number. This scope includes the phone_number and phone_number_verified claims, which provide the user's phone number and indicate whether it has been verified."]
                 };
 
