2205984844f2985a722199f3799655852dca542c
Mike Alhayek <mike@crestapps.com>
2024-09-14 14:29:50 -0700

    Fix the UI

--- src/OrchardCore.Modules/OrchardCore.Sms.Azure/Extensions/SmsProviderExtensions.cs
+++ src/OrchardCore.Modules/OrchardCore.Sms.Azure/Extensions/SmsProviderExtensions.cs
@@ -1,4 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using OrchardCore.Sms.Azure.Models;
 using OrchardCore.Sms.Azure.Services;
 
 namespace OrchardCore.Sms.Azure;
@@ -6,5 +8,6 @@ namespace OrchardCore.Sms.Azure;
 public static class SmsProviderExtensions
 {
     public static IServiceCollection AddAzureSmsProvider(this IServiceCollection services)
-        => services.AddSmsProviderOptionsConfiguration<AzureSmsProviderOptionsConfigurations>();
+        => services.AddSmsProviderOptionsConfiguration<AzureSmsProviderOptionsConfigurations>()
+        .AddTransient<IConfigureOptions<AzureSmsOptions>, AzureSmsOptionsConfiguration>();
 }
