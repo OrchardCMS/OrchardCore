db2349876a588e12d0b54ae1b1ecf558f50152d7
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2024-09-15 23:15:05 +0300

    Apply suggestions from code review
    Co-authored-by: Mike Alhayek <mike@crestapps.com>

--- src/OrchardCore.Modules/OrchardCore.Sms.Azure/Services/AzureSmsProvider.cs
+++ src/OrchardCore.Modules/OrchardCore.Sms.Azure/Services/AzureSmsProvider.cs
@@ -19,5 +19,5 @@ public sealed class AzureSmsProvider : AzureSmsProviderBase
     }
 
     public override LocalizedString Name
-        => S["Azure Communication Services SMS Provider"];
+        => S["Azure Communication"];
 }
