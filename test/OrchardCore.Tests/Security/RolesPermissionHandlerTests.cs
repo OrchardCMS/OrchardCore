3b77bdf3a075d0808026f3533c56188540a7e1ac
Mike Alhayek <mike@crestapps.com>
2024-09-24 11:02:52 -0700

    Update RolesPermissionHandlerTests.cs
    Co-authored-by: Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>

--- test/OrchardCore.Tests/Security/RolesPermissionHandlerTests.cs
+++ test/OrchardCore.Tests/Security/RolesPermissionHandlerTests.cs
@@ -14,8 +14,6 @@ public class RolesPermissionHandlerTests
 
         var context = PermissionHandlerHelper.CreateTestAuthorizationHandlerContext(required, [adminRolePermission], true);
 
-        var options = new Mock<IOptions<IdentityOptions>>();
-
         var permissionHandler = GetRolesPermissionHandler("Administrator");
 
         // Act
