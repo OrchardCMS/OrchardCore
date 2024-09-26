71bf1bfe93caa0e14a0518e9076be299ae989ed9
Mike Alhayek <mike@crestapps.com>
2024-09-26 09:25:12 -0700

    Update src/OrchardCore/OrchardCore.Recipes.Core/RecipePermissions.cs
    Co-authored-by: Sébastien Ros <sebastienros@gmail.com>

--- src/OrchardCore/OrchardCore.Recipes.Core/RecipePermissions.cs
+++ src/OrchardCore/OrchardCore.Recipes.Core/RecipePermissions.cs
@@ -4,5 +4,5 @@ namespace OrchardCore.Recipes;
 
 public static class RecipePermissions
 {
-    public static readonly Permission ManageRecipes = new Permission("ManageRecipes", "Manage Recipes");
+    public static readonly Permission ManageRecipes = new Permission("ManageRecipes", "Manage Recipes", isSecurityCritical: true);
 }
