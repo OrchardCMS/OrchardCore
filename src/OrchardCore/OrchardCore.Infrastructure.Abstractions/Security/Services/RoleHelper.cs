343086211069582acdcc7217e7cad70f193b58f5
Mike Alhayek <mike@crestapps.com>
2024-10-01 13:51:50 -0700

    Update src/OrchardCore/OrchardCore.Infrastructure.Abstractions/Security/Services/RoleHelper.cs
    Co-authored-by: Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>

--- src/OrchardCore/OrchardCore.Infrastructure.Abstractions/Security/Services/RoleHelper.cs
+++ src/OrchardCore/OrchardCore.Infrastructure.Abstractions/Security/Services/RoleHelper.cs
@@ -2,7 +2,7 @@ namespace OrchardCore.Security.Services;
 
 public static class RoleHelper
 {
-    [Obsolete("This method has been marked as obsolete and will be removed in future releases. Instead of using this helper, please IRoleService and and use IsSystemRoleAsync method for role checks.")]
+    [Obsolete("This method has been marked as obsolete and will be removed in future releases. Instead of using this helper, please use the IRoleService.IsSystemRoleAsync() method.")]
     public static readonly HashSet<string> SystemRoleNames = new(StringComparer.OrdinalIgnoreCase)
     {
         OrchardCoreConstants.Roles.Anonymous,
