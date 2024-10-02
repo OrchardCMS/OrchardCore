1c71b32feadd01385d877f4a928c292a05a197ff
Mike Alhayek <mike@crestapps.com>
2024-10-02 06:20:51 -0700

    Update StandardPermissions.cs
    Co-authored-by: Georg von Kries <gvk@creativbox.net>

--- src/OrchardCore/OrchardCore.Infrastructure.Abstractions/Security/StandardPermissions.cs
+++ src/OrchardCore/OrchardCore.Infrastructure.Abstractions/Security/StandardPermissions.cs
@@ -4,6 +4,6 @@ namespace OrchardCore.Security;
 
 public static class StandardPermissions
 {
-    [Obsolete("This permission is deprecated and will be removed in future releases. Instead, consider changing the role type to 'Owner' for enhanced functionality.")]
+    [Obsolete("This permission is deprecated and will be removed in future releases. Instead, consider adding users to the system administrator role.")]
     public static readonly Permission SiteOwner = new("SiteOwner", "Site Owners Permission", isSecurityCritical: true);
 }
