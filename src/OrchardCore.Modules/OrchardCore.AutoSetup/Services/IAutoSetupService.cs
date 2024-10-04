1573e53359ba6486bf5053cdb5a88bbcf5a50a9c
andy <zgofmontreal@gmail.com>
2024-10-04 12:17:14 -0400

    Update src/OrchardCore.Modules/OrchardCore.AutoSetup/Services/IAutoSetupService.cs
    Co-authored-by: Hisham Bin Ateya <hishamco_2007@yahoo.com>

--- src/OrchardCore.Modules/OrchardCore.AutoSetup/Services/IAutoSetupService.cs
+++ src/OrchardCore.Modules/OrchardCore.AutoSetup/Services/IAutoSetupService.cs
@@ -3,9 +3,12 @@ using OrchardCore.Environment.Shell;
 using OrchardCore.Setup.Services;
 
 namespace OrchardCore.AutoSetup.Services;
+
 public interface IAutoSetupService
 {
     Task<ShellSettings> CreateTenantSettingsAsync(TenantSetupOptions setupOptions);
+
     Task<SetupContext> GetSetupContextAsync(TenantSetupOptions options, ShellSettings shellSettings);
+
     Task<bool> SetupTenantAsync(TenantSetupOptions setupOptions, ShellSettings shellSettings);
 }
