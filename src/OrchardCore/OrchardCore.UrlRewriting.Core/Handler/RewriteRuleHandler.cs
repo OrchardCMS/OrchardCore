5c25ba2fcc617725c77dacfe2b55d6ff85cc6c27
Mike Alhayek <mike@crestapps.com>
2024-10-04 22:20:07 -0700

    fix build

--- src/OrchardCore/OrchardCore.UrlRewriting.Core/Handler/RewriteRuleHandler.cs
+++ src/OrchardCore/OrchardCore.UrlRewriting.Core/Handler/RewriteRuleHandler.cs
@@ -1,9 +1,15 @@
-﻿using OrchardCore.UrlRewriting.Models;
+using OrchardCore.UrlRewriting.Models;
 
 namespace OrchardCore.UrlRewriting.Handler;
 
 public abstract class RewriteRuleHandler : IRewriteRuleHandler
 {
+    public virtual Task DeletedAsync(DeletedRewriteRuleContext context)
+        => Task.CompletedTask;
+
+    public virtual Task DeletingAsync(DeletingRewriteRuleContext context)
+        => Task.CompletedTask;
+
     public virtual Task InitializedAsync(InitializedRewriteRuleContext context)
         => Task.CompletedTask;
 
@@ -11,5 +17,11 @@ public abstract class RewriteRuleHandler : IRewriteRuleHandler
         => Task.CompletedTask;
 
     public virtual Task LoadedAsync(LoadedRewriteRuleContext context)
+        => Task.CompletedTask;
+
+    public virtual Task SavedAsync(SavedRewriteRuleContext context)
+        => Task.CompletedTask;
+
+    public virtual Task SavingAsync(SavingRewriteRuleContext context)
         => Task.CompletedTask;
 }
