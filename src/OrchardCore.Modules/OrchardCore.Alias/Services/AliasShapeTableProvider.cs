f607f012796e72d0d962dd8f45aa8a16a2ac2b13
giannik <giannkyrias@gmail.com>
2024-10-08 22:08:35 +0300

    alias and slug alternates for widgets too

--- src/OrchardCore.Modules/OrchardCore.Alias/Services/AliasShapeTableProvider.cs
+++ src/OrchardCore.Modules/OrchardCore.Alias/Services/AliasShapeTableProvider.cs
@@ -29,6 +29,25 @@ public class AliasShapeTableProvider : ShapeTableProvider
                     displaying.Shape.Metadata.Alternates.Add("Content_" + displaying.Shape.Metadata.DisplayType + "__Alias__" + encodedAlias);
                 }
             });
+        builder.Describe("Widget")
+            .OnDisplaying(displaying =>
+            {
+                var shape = displaying.Shape;
+                var contentItem = shape.GetProperty<ContentItem>("ContentItem");
+
+                var aliasPart = contentItem?.As<AliasPart>();
+
+                if (aliasPart != null)
+                {
+                    var encodedAlias = aliasPart.Alias.EncodeAlternateElement();
+
+                    // Widget__Alias__[Alias] e.g. Widget-Alias-example, Widget-Alias-my-page
+                    displaying.Shape.Metadata.Alternates.Add("Widget__Alias__" + encodedAlias);
+
+                    // Widget_[DisplayType]__Alias__[Alias] e.g. Widget-Alias-example.Summary, Widget-Alias-my-page.Summary
+                    displaying.Shape.Metadata.Alternates.Add("Widget_" + displaying.Shape.Metadata.DisplayType + "__Alias__" + encodedAlias);
+                }
+            });
 
         return ValueTask.CompletedTask;
     }
--- src/OrchardCore.Modules/OrchardCore.Autoroute/Services/AutorouteShapeTableProvider.cs
+++ src/OrchardCore.Modules/OrchardCore.Autoroute/Services/AutorouteShapeTableProvider.cs
@@ -29,6 +29,25 @@ public class AutorouteShapeTableProvider : ShapeTableProvider
                     displaying.Shape.Metadata.Alternates.Add("Content_" + displaying.Shape.Metadata.DisplayType + "__Slug__" + encodedSlug);
                 }
             });
+        builder.Describe("Widget")
+            .OnDisplaying(displaying =>
+            {
+                var shape = displaying.Shape;
+                var contentItem = shape.GetProperty<ContentItem>("ContentItem");
+
+                var autoroutePart = contentItem?.As<AutoroutePart>();
+
+                if (autoroutePart != null)
+                {
+                    var encodedSlug = autoroutePart.Path.EncodeAlternateElement().Replace("/", "__");
+
+                    // Widget__Slug__[Slug] e.g. Widget-Slug-example, Widget-Slug-blog-my-post
+                    displaying.Shape.Metadata.Alternates.Add("Widget__Slug__" + encodedSlug);
+
+                    // Widget_[DisplayType]__Slug__[Slug] e.g. Widget-Slug-example.Summary, Widget-Slug-blog-my-post.Summary
+                    displaying.Shape.Metadata.Alternates.Add("Widget_" + displaying.Shape.Metadata.DisplayType + "__Slug__" + encodedSlug);
+                }
+            });
 
         return ValueTask.CompletedTask;
     }
