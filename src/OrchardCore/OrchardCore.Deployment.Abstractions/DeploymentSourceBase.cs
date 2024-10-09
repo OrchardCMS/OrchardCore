85df02a771359281d7fbbdbb1a619d62e8b7f3ea
Mike Alhayek <mike@crestapps.com>
2024-10-09 14:35:36 -0700

    add release notes

--- src/OrchardCore/OrchardCore.Deployment.Abstractions/DeploymentSourceBase.cs
+++ src/OrchardCore/OrchardCore.Deployment.Abstractions/DeploymentSourceBase.cs
@@ -3,9 +3,9 @@ namespace OrchardCore.Deployment;
 /// <summary>
 /// Represents a base class for deployment source.
 /// </summary>
-/// <typeparam name="TDeploymentStep">The deployment step type.</typeparam>
-public abstract class DeploymentSourceBase<TDeploymentStep>
-    : IDeploymentSource where TDeploymentStep : DeploymentStep
+/// <typeparam name="TStep">The deployment step type.</typeparam>
+public abstract class DeploymentSourceBase<TStep> : IDeploymentSource 
+    where TStep : DeploymentStep
 {
     /// <inheritdoc/>
     public Task ProcessDeploymentStepAsync(DeploymentStep step, DeploymentPlanResult result)
@@ -18,5 +18,5 @@ public abstract class DeploymentSourceBase<TDeploymentStep>
         return ProcessAsync(deploymentStep, result);
     }
 
-    protected abstract Task ProcessAsync(TDeploymentStep step, DeploymentPlanResult result);
+    protected abstract Task ProcessAsync(TStep step, DeploymentPlanResult result);
 }
