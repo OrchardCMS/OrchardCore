42fcffe262990b18e4e0df9b1ed0d5ae6a54915b
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2024-11-06 02:02:56 +0300

    Deprecate LiquidCultureAccessor to avoid breaking changes

---
+++ src/OrchardCore/OrchardCore.DisplayManagement.Liquid/LiquidCultureAccessor.cs
@@ -0,0 +1,9 @@
+namespace OrchardCore.Liquid;
+
+/// <summary>
+/// This is a placeholder class that allows modules to extend the `Culture` property in the current Liquid scope.
+/// </summary>
+[Obsolete("This class is obsolete and will be removed in a future version. Use the `Culture` property in the current Liquid scope instead.", error: true)]
+public class LiquidCultureAccessor
+{
+}
