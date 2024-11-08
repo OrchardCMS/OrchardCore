8c2ac884ebc8536493281acab0bbcb16aeabe11b
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2024-11-09 01:02:38 +0300

    Pass the IHostEnvironment from the DI

--- src/OrchardCore/OrchardCore.DisplayManagement.Liquid/Values/HostingEnvironmentValue.cs
+++ src/OrchardCore/OrchardCore.DisplayManagement.Liquid/Values/HostingEnvironmentValue.cs
@@ -2,20 +2,19 @@ using System.Globalization;
 using System.Text.Encodings.Web;
 using Fluid;
 using Fluid.Values;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using OrchardCore.Liquid;
 
 namespace OrchardCore.DisplayManagement.Liquid.Values;
 
 internal sealed class HostingEnvironmentValue : FluidValue
 {
-    private readonly string _environmentName;
+    private readonly IHostEnvironment _hostEnvironment;
 
-    public HostingEnvironmentValue(string environmentName = null)
-        => _environmentName = environmentName;
+    public HostingEnvironmentValue(IHostEnvironment hostEnvironment = null)
+        => _hostEnvironment = hostEnvironment;
 
-    public override FluidValues Type => FluidValues.String;
+    public override FluidValues Type => FluidValues.Object;
 
     public override bool Equals(FluidValue other)
     {
@@ -31,39 +30,25 @@ internal sealed class HostingEnvironmentValue : FluidValue
 
     public override decimal ToNumberValue() => 0;
 
-    public override object ToObjectValue() => _environmentName;
+    public override object ToObjectValue() => _hostEnvironment;
 
-    public override string ToStringValue() => _environmentName;
+    public override string ToStringValue() => _hostEnvironment.EnvironmentName;
 
 #pragma warning disable CS0672 // Member overrides obsolete member
     public override void WriteTo(TextWriter writer, TextEncoder encoder, CultureInfo cultureInfo)
 #pragma warning restore CS0672 // Member overrides obsolete member
-        => writer.Write(_environmentName);
+        => writer.Write(ToStringValue());
 
     public async override ValueTask WriteToAsync(TextWriter writer, TextEncoder encoder, CultureInfo cultureInfo)
-        => await writer.WriteAsync(_environmentName);
+        => await writer.WriteAsync(ToStringValue());
 
     public override ValueTask<FluidValue> GetValueAsync(string name, TemplateContext context)
-    {
-        var environmentName = _environmentName ?? GetHostingEnvironment(context).ToStringValue();
-
-        return name switch
+        => name switch
         {
-            "IsDevelopment" => BooleanValue.Create(environmentName == Environments.Development),
-            "IsStaging" => BooleanValue.Create(environmentName == Environments.Staging),
-            "IsProduction" => BooleanValue.Create(environmentName == Environments.Production),
-            "Name" => StringValue.Create(environmentName),
+            "IsDevelopment" => BooleanValue.Create(_hostEnvironment.IsDevelopment()),
+            "IsStaging" => BooleanValue.Create(_hostEnvironment.IsStaging()),
+            "IsProduction" => BooleanValue.Create(_hostEnvironment.IsProduction()),
+            "Name" => StringValue.Create(_hostEnvironment.EnvironmentName),
             _ => NilValue.Instance
         };
-    }
-
-    private static HostingEnvironmentValue GetHostingEnvironment(TemplateContext context)
-    {
-        var ctx = context as LiquidTemplateContext
-            ?? throw new InvalidOperationException($"An implementation of '{nameof(LiquidTemplateContext)}' is required");
-
-        var hostEnvironment = ctx.Services.GetRequiredService<IHostEnvironment>();
-
-        return new HostingEnvironmentValue(hostEnvironment.EnvironmentName);
-    }
 }
