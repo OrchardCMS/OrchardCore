f02437da83ab0973149030708722caa52f66ffb9
Emrah Tokalak <emrahtokalak@gmail.com>
2024-11-09 23:37:53 +0300

    Fix JSON parsing errors caused by spaces and special characters in Lu… (#16981)
    Co-authored-by: emrah.tokalak <emrah.tokalak@medyasoft.com.tr>
    Co-authored-by: Hisham Bin Ateya <hishamco_2007@yahoo.com>

--- test/OrchardCore.Tests/Apis/Lucene/LuceneQueryTests.cs
+++ test/OrchardCore.Tests/Apis/Lucene/LuceneQueryTests.cs
@@ -152,4 +152,26 @@ public class LuceneQueryTests
             Assert.Contains("Orchard", contentItems.ElementAt(3).DisplayText, StringComparison.OrdinalIgnoreCase);
         };
     }
+
+    [Fact]
+    public async Task LuceneQueryTemplateWithSpecialCharactersShouldNotThrowError()
+    {
+        using var context = new LuceneContext();
+        await context.InitializeAsync();
+
+        // Act
+        var index = "ArticleIndex";
+        var queryTemplate = "\r\r\n{% assign testVariable = \"48yvsghn194eft8axztaves25h\" %}\n\n{\n  \"query\": {\n    \"bool\": {\n      \"must\": [\n        { \"term\" : { \"Content.ContentItem.ContentType\" : \"Article\" } },\n        { \"term\": { \"Content.ContentItem.Published\" : \"true\" } },\n      ]\n    }\n  }\n}";
+
+        var content = await context.Client.GetAsync($"api/lucene/content?indexName={index}&query={queryTemplate}");
+        var queryResults = await content.Content.ReadAsAsync<LuceneQueryResults>();
+
+        // Assert
+        Assert.NotNull(queryResults);
+        Assert.NotEmpty(queryResults.Items);
+
+        var contentItems = queryResults.Items.Select(x => JObject.FromObject(x).Deserialize<ContentItem>());
+
+        Assert.Contains("Orchard", contentItems.First().DisplayText, StringComparison.OrdinalIgnoreCase);
+    }
 }
