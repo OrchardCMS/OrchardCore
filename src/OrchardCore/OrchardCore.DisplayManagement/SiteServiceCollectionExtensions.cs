5954f9f717a7a126ca974ec6ce8ed58569aea513
Mike Alhayek <mike@crestapps.com>
2024-11-12 07:21:43 -0800

    one file

--- src/OrchardCore/OrchardCore.DisplayManagement/SiteServiceCollectionExtensions.cs
+++ src/OrchardCore/OrchardCore.DisplayManagement/SiteServiceCollectionExtensions.cs
@@ -4,20 +4,15 @@ using OrchardCore.Settings;
 namespace OrchardCore.DisplayManagement.Handlers;
 
 /// <summary>
-/// The <see cref="SiteCollectionExtensions"/> class is scheduled for removal in the next major release.
-/// The <see cref="AddSiteDisplayDriver"/> extension method should be relocated 
-/// to the <see cref="ServiceCollectionExtensions"/> class.
+/// Rename this file to ServiceCollectionExtensions in v3.0.
 /// </summary>
-public static class SiteCollectionExtensions
+public static class SiteServiceCollectionExtensions
 {
     public static IServiceCollection AddSiteDisplayDriver<TDriver>(this IServiceCollection services)
         where TDriver : class, IDisplayDriver<ISite>
         => services.AddDisplayDriver<ISite, TDriver>();
-}
 
-public static class ServiceCollectionExtensions
-{
     public static IServiceCollection AddDisplayDriver<TModel, TDriver>(this IServiceCollection services)
-        where TDriver : class, IDisplayDriver<TModel>
+    where TDriver : class, IDisplayDriver<TModel>
         => services.AddScoped<IDisplayDriver<TModel>, TDriver>();
 }
