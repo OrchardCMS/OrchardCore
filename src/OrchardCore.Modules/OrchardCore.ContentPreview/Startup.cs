9ef4b02434e4648b5a16627457e215b16a82c3e4
Sebastien Ros <sebastienros@gmail.com>
2024-11-12 11:33:38 -0800

    One more warning

--- src/OrchardCore.Modules/OrchardCore.ContentPreview/Startup.cs
+++ src/OrchardCore.Modules/OrchardCore.ContentPreview/Startup.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
 using OrchardCore.ContentManagement;
 using OrchardCore.ContentManagement.Display.ContentDisplay;
