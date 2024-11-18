89b811578cc420bc60dce80290e2fa0d1d38f5ae
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2024-11-18 19:20:48 +0300

    Add PollingTime & MinimumIdelTime to BackgroundServiceOptions

--- src/OrchardCore/OrchardCore.Abstractions/Modules/BackgroundServiceOptions.cs
+++ src/OrchardCore/OrchardCore.Abstractions/Modules/BackgroundServiceOptions.cs
@@ -3,4 +3,14 @@ namespace OrchardCore.Modules;
 public class BackgroundServiceOptions
 {
     public bool ShellWarmup { get; set; }
+
+    /// <summary>
+    /// Gets or sets the time in seconds between each background task execution.
+    /// </summary>
+    public int PollingTime { get; set; } = 60;
+
+    /// <summary>
+    /// Gets or sets the minimum time in seconds that the background task should be idle before being considered for execution.
+    /// </summary>
+    public int MinimumIdelTime { get; set; } = 10;
 }
