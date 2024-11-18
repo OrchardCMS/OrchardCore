3fd066ab139128d922c578185f6a9b682a924e44
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2024-11-18 23:27:10 +0300

    Address the feedback

--- src/OrchardCore/OrchardCore.Abstractions/Modules/BackgroundServiceOptions.cs
+++ src/OrchardCore/OrchardCore.Abstractions/Modules/BackgroundServiceOptions.cs
@@ -2,15 +2,18 @@ namespace OrchardCore.Modules;
 
 public class BackgroundServiceOptions
 {
+    /// <summary>
+    /// Gets or sets a value indicating whether the shell should be warmed up.
+    /// </summary>
     public bool ShellWarmup { get; set; }
 
     /// <summary>
-    /// Gets or sets the time in seconds between each background task execution.
+    /// Gets or sets the polling time for the background task.
     /// </summary>
-    public int PollingTime { get; set; } = 60;
+    public TimeSpan PollingTime { get; set; } = TimeSpan.FromMinutes(1);
 
     /// <summary>
-    /// Gets or sets the minimum time in seconds that the background task should be idle before being considered for execution.
+    /// Gets or set the minimum idle time before the background task is triggered.
     /// </summary>
-    public int MinimumIdelTime { get; set; } = 10;
+    public TimeSpan MinimumIdelTime { get; set; } = TimeSpan.FromSeconds(10);
 }
