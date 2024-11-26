208deb7ac074375942248590fa1e458a45b6301b
Georg von Kries <gvk@creativbox.net>
2024-11-26 09:42:38 +0100

    Update src/OrchardCore.Modules/OrchardCore.Media/GraphQL/MediaAssetObjectType.cs
    Co-authored-by: Mike Alhayek <mike@crestapps.com>

--- src/OrchardCore.Modules/OrchardCore.Media/GraphQL/MediaAssetObjectType.cs
+++ src/OrchardCore.Modules/OrchardCore.Media/GraphQL/MediaAssetObjectType.cs
@@ -22,7 +22,9 @@ public class MediaAssetObjectType : ObjectGraphType<IFileStoreEntry>
                 return mediaFileStore.MapPathToPublicUrl(path);
             });
 
-        Field(file => file.Length).Description(S["The length of the file."]);
+        Field(file => file.Length)
+            .Description(S["The length of the file."]);
+            
         Field<DateTimeGraphType>("lastModifiedUtc")
             .Description(S["The date and time in UTC when the asset was last modified."])
             .Resolve(file => file.Source.LastModifiedUtc);
