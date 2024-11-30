759655ea8988ed644af4981ee4e1563da6b92ece
Sergio Di Fiore <difiores@outlook.com>
2024-11-30 10:00:58 -0300

    Configuration error removed

---
+++ src/OrchardCore.Cms.Web/.config/dotnet-tools.json
@@ -0,0 +1,13 @@
+{
+  "version": 1,
+  "isRoot": true,
+  "tools": {
+    "dotnet-ef": {
+      "version": "9.0.0",
+      "commands": [
+        "dotnet-ef"
+      ],
+      "rollForward": false
+    }
+  }
+}
---
+++ src/OrchardCore/OrchardCore.Search.Elasticsearch.Core/Services/.vshistory/ElasticSearchClientFactory.cs/2024-11-27_07_31_53_088.cs
@@ -0,0 +1,34 @@
+using Elastic.Clients.Elasticsearch;
+using Elastic.Transport;
+using OrchardCore.Search.Elasticsearch.Core.Models;
+
+namespace OrchardCore.Search.Elasticsearch.Core.Services;
+
+public class ElasticsearchClientFactory
+{
+    public static ElasticsearchClient Create(ElasticsearchConnectionOptions elasticConfiguration)
+    {
+        var settings = elasticConfiguration.ConnectionType switch
+        {
+            ElasticsearchConnectionType.CloudConnectionPool => new ElasticsearchClientSettings(elasticConfiguration.CloudId, new BasicAuthentication(elasticConfiguration.Username, elasticConfiguration.Password)),
+            ElasticsearchConnectionType.StaticConnectionPool => new ElasticsearchClientSettings(new StaticNodePool(GetNodeUris(elasticConfiguration))),
+            ElasticsearchConnectionType.SniffingConnectionPool => new ElasticsearchClientSettings(new SniffingNodePool(GetNodeUris(elasticConfiguration))),
+            ElasticsearchConnectionType.StickyConnectionPool => new ElasticsearchClientSettings(new StickyNodePool(GetNodeUris(elasticConfiguration))),
+            _ => new ElasticsearchClientSettings(GetNodeUris(elasticConfiguration).First()),
+        };
+
+        if (!string.IsNullOrWhiteSpace(elasticConfiguration.CertificateFingerprint))
+        {
+            settings.CertificateFingerprint(elasticConfiguration.CertificateFingerprint);
+        }
+
+        settings.EnableHttpCompression();
+
+        return new ElasticsearchClient(settings);
+    }
+
+    private static IEnumerable<Uri> GetNodeUris(ElasticsearchConnectionOptions elasticConfiguration)
+    {
+        return elasticConfiguration.Ports.Select(port => new Uri($"{elasticConfiguration.Url}:{port}")).Distinct();
+    }
+}
