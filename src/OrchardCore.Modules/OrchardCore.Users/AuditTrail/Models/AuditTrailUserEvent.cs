a7a66756d96ff2830bb29d9fb128690212becdf6
Mike Alhayek <mike@crestapps.com>
2024-12-04 11:47:22 -0800

    Improve the Audit Trails views (#17104)

--- src/OrchardCore.Modules/OrchardCore.Users/AuditTrail/Models/AuditTrailUserEvent.cs
+++ src/OrchardCore.Modules/OrchardCore.Users/AuditTrail/Models/AuditTrailUserEvent.cs
@@ -1,9 +1,11 @@
 namespace OrchardCore.Users.AuditTrail.Models;
 
+// TODO a future version should also record the User state, enabling diff against users
 public class AuditTrailUserEvent
 {
-    // TODO a future version should also record the User state, enabling diff against users
     public string Name { get; set; } = "User";
+
     public string UserName { get; set; }
+
     public string UserId { get; set; }
 }
