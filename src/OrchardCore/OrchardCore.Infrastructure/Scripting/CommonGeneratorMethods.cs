2904ef0eaf0a06e885c0b13f6b8ed748fac79cd0
Mike Alhayek <mike@crestapps.com>
2024-12-06 11:58:33 -0800

    undo

--- src/OrchardCore/OrchardCore.Infrastructure/Scripting/CommonGeneratorMethods.cs
+++ src/OrchardCore/OrchardCore.Infrastructure/Scripting/CommonGeneratorMethods.cs
@@ -42,7 +42,6 @@ public class CommonGeneratorMethods : IGlobalMethodProvider
         Method = serviceProvider => (Func<string, string>)(encoded =>
         {
             var compressedStream = Base64.DecodeToStream(encoded);
-            compressedStream.Seek(0, SeekOrigin.Begin);
 
             using var gZip = new GZipStream(compressedStream, CompressionMode.Decompress, leaveOpen: true);
 
