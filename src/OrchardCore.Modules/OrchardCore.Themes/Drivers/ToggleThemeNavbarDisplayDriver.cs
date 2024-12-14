990c2d790d2eeaab62ce1b21d56fb8b2d79da10e
Mike Alhayek <mike@crestapps.com>
2024-12-14 10:10:51 -0800

    Show ThemeToggler on supported themes only

--- src/OrchardCore.Modules/OrchardCore.Themes/Drivers/ToggleThemeNavbarDisplayDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.Themes/Drivers/ToggleThemeNavbarDisplayDriver.cs
@@ -18,7 +18,6 @@ public sealed class ToggleThemeNavbarDisplayDriver : DisplayDriver<Navbar>
     {
         return View("ToggleTheme", model)
             .RenderWhen(async () => (await _siteService.GetSettingsAsync<AdminSettings>()).DisplayThemeToggler)
-            .Location("Detail", "Content:10")
             .Location("DetailAdmin", "Content:10");
     }
 }
