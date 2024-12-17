680d369bea1606c6a6c67e9984738e07fdc81ce7
Mike Alhayek <mike@crestapps.com>
2024-12-17 06:35:24 -0800

    move ToggleThemeNavbarDisplayDriver to TheAdmin theme

--- src/OrchardCore.Themes/TheAdmin/Drivers/ToggleThemeNavbarDisplayDriver.cs
+++ src/OrchardCore.Themes/TheAdmin/Drivers/ToggleThemeNavbarDisplayDriver.cs
@@ -1,23 +1,28 @@
+using OrchardCore.Admin;
 using OrchardCore.Admin.Models;
 using OrchardCore.DisplayManagement.Handlers;
 using OrchardCore.DisplayManagement.Views;
 using OrchardCore.Settings;
 
-namespace OrchardCore.Themes.Drivers;
+namespace OrchardCore.Themes.TheAdmin.Drivers;
 
 public sealed class ToggleThemeNavbarDisplayDriver : DisplayDriver<Navbar>
 {
     private readonly ISiteService _siteService;
+    private readonly IAdminThemeService _adminThemeService;
 
-    public ToggleThemeNavbarDisplayDriver(ISiteService siteService)
+    public ToggleThemeNavbarDisplayDriver(
+        ISiteService siteService,
+        IAdminThemeService adminThemeService)
     {
         _siteService = siteService;
+        _adminThemeService = adminThemeService;
     }
 
     public override IDisplayResult Display(Navbar model, BuildDisplayContext context)
     {
         return View("ToggleTheme", model)
-            .RenderWhen(async () => (await _siteService.GetSettingsAsync<AdminSettings>()).DisplayThemeToggler)
+            .RenderWhen(async () => (await _siteService.GetSettingsAsync<AdminSettings>()).DisplayThemeToggler && await _adminThemeService.GetAdminThemeNameAsync() == "TheAdmin")
             .Location("DetailAdmin", "Content:10");
     }
 }
