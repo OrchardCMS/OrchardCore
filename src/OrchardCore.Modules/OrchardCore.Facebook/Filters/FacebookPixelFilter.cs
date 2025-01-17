2fb0ecf6dac679eb16fac3b81e1d7c8940734891
Sébastien Ros <sebastienros@gmail.com>
2025-01-17 10:10:01 -0800

    Apply suggestions

--- src/OrchardCore.Modules/OrchardCore.Facebook/Filters/FacebookPixelFilter.cs
+++ src/OrchardCore.Modules/OrchardCore.Facebook/Filters/FacebookPixelFilter.cs
@@ -15,7 +15,7 @@ public sealed class FacebookPixelFilter : IAsyncResultFilter
     private readonly ISiteService _siteService;
     private readonly JavaScriptEncoder _jsEncoder;
 
-    private static readonly HtmlString _preamble = new HtmlString("<!-- Meta Pixel Code -->\r\n<script>\r\n!function(f,b,e,v,n,t,s)\r\n{if(f.fbq)return;n=f.fbq=function(){n.callMethod?\r\nn.callMethod.apply(n,arguments):n.queue.push(arguments)};\r\nif(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';\r\nn.queue=[];t=b.createElement(e);t.async=!0;\r\nt.src=v;s=b.getElementsByTagName(e)[0];\r\ns.parentNode.insertBefore(t,s)}(window, document,'script',\r\n'https://connect.facebook.net/en_US/fbevents.js');\r\nfbq('init', '");
+    private static readonly HtmlString _preamble = new("<!-- Meta Pixel Code -->\r\n<script>\r\n!function(f,b,e,v,n,t,s)\r\n{if(f.fbq)return;n=f.fbq=function(){n.callMethod?\r\nn.callMethod.apply(n,arguments):n.queue.push(arguments)};\r\nif(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';\r\nn.queue=[];t=b.createElement(e);t.async=!0;\r\nt.src=v;s=b.getElementsByTagName(e)[0];\r\ns.parentNode.insertBefore(t,s)}(window, document,'script',\r\n'https://connect.facebook.net/en_US/fbevents.js');\r\nfbq('init', '");
     private static readonly HtmlString _end = new HtmlString("');\r\nfbq('track', 'PageView');\r\n</script>\r\n<!-- End Meta Pixel Code -->");
 
     public FacebookPixelFilter(
@@ -30,7 +30,7 @@ public sealed class FacebookPixelFilter : IAsyncResultFilter
 
     public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
     {
-        // Should only run on the front-end for a full view
+        // Should only run on the front-end for a full view.
         if (context.IsViewOrPageResult() && !AdminAttribute.IsApplied(context.HttpContext))
         {
             var canTrack = context.HttpContext.Features.Get<ITrackingConsentFeature>()?.CanTrack ?? true;
--- src/OrchardCore.Modules/OrchardCore.Google/TagManager/GoogleTagManagerFilter.cs
+++ src/OrchardCore.Modules/OrchardCore.Google/TagManager/GoogleTagManagerFilter.cs
@@ -15,7 +15,7 @@ public sealed class GoogleTagManagerFilter : IAsyncResultFilter
     private readonly ISiteService _siteService;
     private readonly JavaScriptEncoder _jsEncoder;
 
-    private static readonly HtmlString _preamble = new HtmlString("<!-- Google Tag Manager -->\n<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\n  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\n  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n  })(window,document,'script','dataLayer','");
+    private static readonly HtmlString _preamble = new("<!-- Google Tag Manager -->\n<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\n  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\n  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n  })(window,document,'script','dataLayer','");
     private static readonly HtmlString _end= new HtmlString("');</script>\n<!-- End Google Tag Manager -->");
 
     public GoogleTagManagerFilter(
