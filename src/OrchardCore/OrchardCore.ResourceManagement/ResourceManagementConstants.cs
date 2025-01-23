ad18ab507feee7ca50b8e5ea3cedc490fb6a3202
Mike Alhayek <mike@crestapps.com>
2025-01-22 18:06:57 -0800

    rename

--- src/OrchardCore/OrchardCore.ResourceManagement/ResourceManagementConstants.cs
+++ src/OrchardCore/OrchardCore.ResourceManagement/ResourceManagementConstants.cs
@@ -2,7 +2,7 @@ namespace OrchardCore.ResourceManagement;
 
 internal static class ResourceManagementConstants
 {
-    public static readonly char[] ParameterValueSeparator = [',', ' '];
+    public static readonly char[] ParameterValuesSeparator = [',', ' '];
 
     public const char VersionSeparator = ':';
 }
