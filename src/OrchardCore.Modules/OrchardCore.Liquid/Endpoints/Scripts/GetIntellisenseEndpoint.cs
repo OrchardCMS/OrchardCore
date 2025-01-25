dfeea37aa625833ed65133e69b0109e9b095a282
James Pretorius <50090107+jbytes1027@users.noreply.github.com>
2025-01-24 22:51:16 -0500

    Disable caching liquid-intellisense.js

--- src/OrchardCore.Modules/OrchardCore.Liquid/Endpoints/Scripts/GetIntellisenseEndpoint.cs
+++ src/OrchardCore.Modules/OrchardCore.Liquid/Endpoints/Scripts/GetIntellisenseEndpoint.cs
@@ -4,14 +4,9 @@ using Fluid;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
-using Microsoft.Extensions.Caching.Memory;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Primitives;
-using Microsoft.Net.Http.Headers;
 using OrchardCore.DisplayManagement.Liquid;
-using OrchardCore.Environment.Shell.Configuration;
 
 #nullable enable
 
@@ -28,54 +23,27 @@ public static class GetIntellisenseEndpoint
         return builder;
     }
 
-    private static byte[] GenerateScript(IServiceProvider serviceProvider)
+    private static IResult HandleRequest(HttpContext context, LiquidViewParser liquidViewParser, IOptions<TemplateOptions> templateOptions)
     {
-        var templateOptions = serviceProvider.GetRequiredService<IOptions<TemplateOptions>>();
-        var liquidViewParser = serviceProvider.GetRequiredService<LiquidViewParser>();
         var filters = string.Join(',', templateOptions.Value.Filters.Select(x => $"'{x.Key}'"));
         var tags = string.Join(',', liquidViewParser.RegisteredTags.Select(x => $"'{x.Key}'"));
-        var script = $@"[{filters}].forEach(value=>{{if(!liquidFilters.includes(value)){{ liquidFilters.push(value);}}}});
-                    [{tags}].forEach(value=>{{if(!liquidTags.includes(value)){{ liquidTags.push(value);}}}});";
 
-        return Encoding.UTF8.GetBytes(script);
-    }
-
-    private static IResult HandleRequest(HttpContext context, IMemoryCache cache, IShellConfiguration shellConfiguration)
-    {
-        context.Response.Headers.CacheControl = shellConfiguration.GetValue(
-                "StaticFileOptions:CacheControl",
-                // Fallback value
-                $"public, max-age={TimeSpan.FromDays(30).TotalSeconds}");
-
-        // Assumes IfNoneMatch has only one ETag for performance
-        var requestETag = context.Request.Headers.IfNoneMatch;
-        if (!StringValues.IsNullOrEmpty(requestETag) && cache.Get(requestETag) != null)
-        {
-            context.Response.Headers.ETag = requestETag;
-
-            return Results.StatusCode(304);
-        }
-
-        const string scriptCacheKey = "OrchardCore.Liquid/Scripts/liquid-intellisense.js";
-
-        var scriptBytes = (byte[]?)cache.Get(scriptCacheKey);
-        if (scriptBytes == null)
-        {
-            scriptBytes = GenerateScript(context.RequestServices);
-
-            cache.Set(scriptCacheKey, scriptBytes);
-        }
+        var scriptBytes = (new byte[][] {
+                "["u8.ToArray(),
+                Encoding.UTF8.GetBytes(filters),
+                "].forEach(value=>{if(!liquidFilters.includes(value)){ liquidFilters.push(value);}});["u8.ToArray(),
+                Encoding.UTF8.GetBytes(tags),
+                "].forEach(value=>{if(!liquidTags.includes(value)){ liquidTags.push(value);}});"u8.ToArray()
+            }).SelectMany(x => x).ToArray();
 
         // Uses cross-processes hashing to enable revalidation after restart
         StringValues eTag = $"\"{XxHash3.HashToUInt64(scriptBytes, 0)}\"";
 
-        // Mark that the eTag corresponds to a fresh file
-        cache.Set(eTag, true);
-
+        context.Response.Headers.CacheControl = "no-cache";
         context.Response.Headers.ETag = eTag;
 
-        // Can be true if the cache was reset after the last client request
-        if (requestETag.Equals(eTag))
+        // Assumes IfNoneMatch has only one ETag for performance
+        if (context.Request.Headers.IfNoneMatch.Equals(eTag))
         {
             return Results.StatusCode(304);
         }
