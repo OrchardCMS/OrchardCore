3b63692d20803ccf37e55acbc190d9626db212ce
Georg von Kries <gvk@creativbox.net>
2025-01-30 10:52:56 +0100

    Fixes some race conditions in the search modules.

--- src/OrchardCore/OrchardCore.Search.AzureAI.Core/AzureAISearchIndexPermissionHelper.cs
+++ src/OrchardCore/OrchardCore.Search.AzureAI.Core/AzureAISearchIndexPermissionHelper.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using OrchardCore.Security.Permissions;
 
 namespace OrchardCore.Search.AzureAI;
@@ -10,23 +11,15 @@ public static class AzureAISearchIndexPermissionHelper
     private static readonly Permission _indexPermissionTemplate =
         new("QueryAzureAISearchIndex_{0}", "Query Azure AI Search '{0}' Index", [ManageAzureAISearchIndexes]);
 
-    private static readonly Dictionary<string, Permission> _permissions = [];
+    private static readonly ConcurrentDictionary<string, Permission> _permissions = [];
 
     public static Permission GetPermission(string indexName)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(indexName);
 
-        if (!_permissions.TryGetValue(indexName, out var permission))
-        {
-            permission = new Permission(
+        return _permissions.GetOrAdd(indexName, indexName => new Permission(
                 string.Format(_indexPermissionTemplate.Name, indexName),
                 string.Format(_indexPermissionTemplate.Description, indexName),
-                _indexPermissionTemplate.ImpliedBy
-            );
-
-            _permissions.Add(indexName, permission);
-        }
-
-        return permission;
+                _indexPermissionTemplate.ImpliedBy));
     }
 }
