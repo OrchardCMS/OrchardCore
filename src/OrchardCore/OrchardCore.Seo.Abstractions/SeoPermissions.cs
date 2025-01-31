610edd2a23c8ea58f407158fec497238c2852930
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2025-01-31 22:06:07 +0300

    Update src/OrchardCore/OrchardCore.Seo.Abstractions/SeoPermissions.cs
    Co-authored-by: Mike Alhayek <mike@crestapps.com>

--- src/OrchardCore/OrchardCore.Seo.Abstractions/SeoPermissions.cs
+++ src/OrchardCore/OrchardCore.Seo.Abstractions/SeoPermissions.cs
@@ -1,6 +1,7 @@
 using OrchardCore.Security.Permissions;
 
 namespace OrchardCore.Seo;
+
 public static class SeoPermissions
 {
     public static readonly Permission ManageSeoSettings = SeoConstants.ManageSeoSettings;
