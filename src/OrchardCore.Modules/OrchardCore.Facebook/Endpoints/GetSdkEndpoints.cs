3a64079a59b7be61145c91439a3048f4d89fa79f
James Pretorius <50090107+jbytes1027@users.noreply.github.com>
2025-01-30 22:19:45 -0500

    Add endpoint versions

--- src/OrchardCore.Modules/OrchardCore.Facebook/Endpoints/GetSdkEndpoints.cs
+++ src/OrchardCore.Modules/OrchardCore.Facebook/Endpoints/GetSdkEndpoints.cs
@@ -14,8 +14,11 @@ namespace OrchardCore.Facebook.Endpoints;
 
 public static class GetSdkEndpoints
 {
-    // Update this version when the script changes to invalidate client caches
-    public static readonly int Version = 1;
+    // Update this version when the fetch script changes to invalidate client caches
+    public static readonly int FetchScriptVersion = 1;
+
+    // Update this version when the init script changes to invalidate client caches
+    public static readonly int InitScriptVersion = 1;
 
     // Scraped from facebook.com
     public static readonly string[] ValidFacebookCultures = { "en-US", "es-LA", "pt-BR", "fr-FR", "de-DE", "so-SO", "af-ZA", "az-AZ", "id-ID", "ms-MY", "jv-ID", "cx-PH", "bs-BA", "br-FR", "ca-ES", "cs-CZ", "co-FR", "cy-GB", "da-DK", "de-DE", "et-EE", "en-GB", "en-US", "es-LA", "es-ES", "eo-EO", "eu-ES", "tl-PH", "fo-FO", "fr-CA", "fr-FR", "fy-NL", "ff-NG", "fn-IT", "ga-IE", "gl-ES", "gn-PY", "ha-NG", "hr-HR", "rw-RW", "iu-CA", "ik-US", "is-IS", "it-IT", "sw-KE", "ht-HT", "ku-TR", "lv-LV", "lt-LT", "hu-HU", "mg-MG", "mt-MT", "nl-NL", "nb-NO", "nn-NO", "uz-UZ", "pl-PL", "pt-BR", "pt-PT", "ro-RO", "sc-IT", "sn-ZW", "sq-AL", "sz-PL", "sk-SK", "sl-SI", "fi-FI", "sv-SE", "vi-VN", "tr-TR", "nl-BE", "zz-TR", "el-GR", "be-BY", "bg-BG", "ky-KG", "kk-KZ", "mk-MK", "mn-MN", "ru-RU", "sr-RS", "tt-RU", "tg-TJ", "uk-UA", "ka-GE", "hy-AM", "he-IL", "ur-PK", "ar-AR", "ps-AF", "fa-IR", "cb-IQ", "sy-SY", "tz-MA", "am-ET", "ne-NP", "mr-IN", "hi-IN", "as-IN", "bn-IN", "pa-IN", "gu-IN", "or-IN", "ta-IN", "te-IN", "kn-IN", "ml-IN", "si-LK", "th-TH", "lo-LA", "my-MM", "km-KH", "ko-KR", "zh-TW", "zh-CN", "zh-HK", "ja-JP", "ja-KS" };
@@ -51,7 +54,7 @@ public static class GetSdkEndpoints
             var encodedCulture = urlEncoder.Encode(culture.Replace('-', '_'));
 
             // Note: If a culture is not found, facebook will use en_US
-            // Note: Update Version constant when the script changes
+            // Note: Update FetchScriptVersion constant when the script changes
             return Task.FromResult(Encoding.UTF8.GetBytes($@"(function(d){{
                 var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {{ return; }}
                 js = d.createElement('script'); js.id = id; js.async = true;
@@ -87,6 +90,7 @@ public static class GetSdkEndpoints
         {
             entry.SetSlidingExpiration(TimeSpan.FromHours(1));
 
+            // Note: Update InitScriptVersion constant when the script changes
             return Encoding.UTF8.GetBytes($@"
                 window.fbAsyncInit = function() {{
                     FB.init({{
