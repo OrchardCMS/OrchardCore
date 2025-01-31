8a921f787a3e7eb1a1e250aad725fdb9d8a19792
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2025-01-31 22:07:30 +0300

    Update src/OrchardCore/OrchardCore.Search.Elasticsearch.Core/ElasticsearchPermissions.cs
    Co-authored-by: Mike Alhayek <mike@crestapps.com>

--- src/OrchardCore/OrchardCore.Search.Elasticsearch.Core/ElasticsearchPermissions.cs
+++ src/OrchardCore/OrchardCore.Search.Elasticsearch.Core/ElasticsearchPermissions.cs
@@ -7,5 +7,4 @@ public static class ElasticsearchPermissions
     public static readonly Permission ManageElasticIndexes = new("ManageElasticIndexes", "Manage Elasticsearch Indexes");
 
     public static readonly Permission QueryElasticApi = new("QueryElasticsearchApi", "Query Elasticsearch Api", [ManageElasticIndexes]);
-
 }
