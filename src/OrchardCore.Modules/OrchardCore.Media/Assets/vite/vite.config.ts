8c366a14c204756933d8cb7747b3f61717ebc743
Jasmin Savard <jasmin.savard@gmail.com>
2025-02-02 23:23:05 -0500

    Add Vite Example in Media module

---
+++ src/OrchardCore.Modules/OrchardCore.Media/Assets/vite/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path';
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    outDir: path.resolve(__dirname, '../wwwroot/Scripts/Media2/'),
+  },
+})
