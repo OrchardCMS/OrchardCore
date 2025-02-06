c1448cc778aa2a2f8986272fd5ac0d1422e07e3b
Mike Alhayek <mike@crestapps.com>
2025-02-06 11:20:07 -0800

    Update event should only be called when updating content item (#17429)

--- src/OrchardCore/OrchardCore.ContentManagement.Abstractions/Handlers/UpdateContentContext.cs
+++ src/OrchardCore/OrchardCore.ContentManagement.Abstractions/Handlers/UpdateContentContext.cs
@@ -4,8 +4,11 @@ public class UpdateContentContext : ContentContextBase
 {
     public UpdateContentContext(ContentItem contentItem) : base(contentItem)
     {
+#pragma warning disable CS0618 // Type or member is obsolete
         UpdatingItem = contentItem;
+#pragma warning restore CS0618 // Type or member is obsolete
     }
 
+    [Obsolete("The property is deprecated and will be removed in a future release. Please use ContentItem instead.")]
     public ContentItem UpdatingItem { get; set; }
 }
