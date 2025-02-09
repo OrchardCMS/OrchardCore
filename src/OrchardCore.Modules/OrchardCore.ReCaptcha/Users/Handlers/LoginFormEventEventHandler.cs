c7ff204722a6f76a13511c3b6327d84423aede7c
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2025-02-09 22:33:15 +0100

    Checking for external logins before ReCaptcha validation

--- src/OrchardCore.Modules/OrchardCore.ReCaptcha/Users/Handlers/LoginFormEventEventHandler.cs
+++ src/OrchardCore.Modules/OrchardCore.ReCaptcha/Users/Handlers/LoginFormEventEventHandler.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Identity;
 using OrchardCore.ReCaptcha.Services;
+using OrchardCore.Users;
 using OrchardCore.Users.Events;
 
 namespace OrchardCore.ReCaptcha.Users.Handlers;
@@ -6,12 +8,32 @@ namespace OrchardCore.ReCaptcha.Users.Handlers;
 public sealed class LoginFormEventEventHandler : LoginFormEventBase
 {
     private readonly ReCaptchaService _reCaptchaService;
+    private readonly UserManager<IUser> _userManager;
 
-    public LoginFormEventEventHandler(ReCaptchaService reCaptchaService)
+    public LoginFormEventEventHandler(ReCaptchaService reCaptchaService, UserManager<IUser> userManager)
     {
         _reCaptchaService = reCaptchaService;
+        _userManager = userManager;
     }
 
-    public override Task LoggingInAsync(string userName, Action<string, string> reportError)
-        => _reCaptchaService.ValidateCaptchaAsync(reportError);
+    public override async Task LoggingInAsync(string userName, Action<string, string> reportError)
+    {
+        var user = await _userManager.FindByNameAsync(userName);
+
+        if (user == null)
+        {
+            return;
+        }
+
+        // Checking if the user has any non-local logins. We shouldn't try to validate the captcha for users that log
+        // in with e.g. Microsoft Entra ID.
+        var logins = await _userManager.GetLoginsAsync(user);
+
+        if (logins.Count > 0)
+        {
+            return;
+        }
+
+        await _reCaptchaService.ValidateCaptchaAsync(reportError);
+    }
 }
