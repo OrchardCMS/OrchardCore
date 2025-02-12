58bce09faa2445e564b3bfc63dca4b1b0d8c2989
Jasmin Savard <jasmin.savard@gmail.com>
2025-02-12 00:15:24 -0500

    Fix admin menu icon picker

--- src/OrchardCore.Modules/OrchardCore.AdminMenu/Assets/js/admin-menu-icon-picker.js
+++ src/OrchardCore.Modules/OrchardCore.AdminMenu/Assets/js/admin-menu-icon-picker.js
@@ -2,10 +2,6 @@
 // It makes easier to use a single picker instance with several input fields.
 // How to use it: Call show() from outside , passing it the id's of the input fields you want the icon classes applied to.
 
-import Vue from "vue";
-import "fontawesome-iconpicker"
-import { Modal } from "bootstrap";
-
 var iconPickerVue = new Vue({
     el: '#iconPickerVue',
     data: {
@@ -16,29 +12,25 @@ var iconPickerVue = new Vue({
     mounted: function () {
         var self = this;
 
-        this.$el.querySelector('.icp-auto').iconpicker({
+        $('.icp-auto').iconpicker({
             title: false,
             templates: {
                 search: '<input type="search" class="form-control iconpicker-search" placeholder="" />' // just to leave empty the placeholder because it is not localized
             }
         });
 
-        this.$el.querySelector('#inline-picker').addEventListener('iconpickerSelected', function (e) {
+        $('#inline-picker').on('iconpickerSelected', function (e) {
             var selected = e.iconpickerInstance.options.fullClassFormatter(e.iconpickerValue);
             
             if (self.targetInputField) {
-                document.getElementById(self.targetInputField).value = selected;
+                $('#' + self.targetInputField).val(selected);
             }
 
             if (self.targetIconTag) {
                 // We need to replace the full tag with the new class.
                 // We could simply apply the new selected class to the i element.
                 // But there is an issue: when the previous class is not a valid fa icon the icon does not refresh.
-                var iconTagElement = document.getElementById(self.targetIconTag);
-                var newIconTagElement = document.createElement('i');
-                newIconTagElement.id = self.targetIconTag;
-                newIconTagElement.className = selected;
-                iconTagElement?.parentNode?.replaceChild(newIconTagElement, iconTagElement);
+                $('#' + self.targetIconTag).replaceWith('<i id="' + self.targetIconTag + '" class="'+ selected + '"></i>')                
             }
 
             if (self.iconPickerModal != null)
@@ -55,7 +47,7 @@ var iconPickerVue = new Vue({
 
             if (this.iconPickerModal == null)
             {
-                this.iconPickerModal = new Modal(this.$el.querySelector("#iconPickerModal"), {
+                this.iconPickerModal = new bootstrap.Modal($("#iconPickerModal"), {
                     keyboard: false
                 });
             }
