c8ceeb8001e73a4f5f453289e459730a7baa7008
Sára El-Saig <sara.el-saig@lombiq.com>
2025-02-23 14:36:56 +0100

    Add prop and docs to AuditTrailUserEvent.

--- src/OrchardCore.Modules/OrchardCore.Users/AuditTrail/Models/AuditTrailUserEvent.cs
+++ src/OrchardCore.Modules/OrchardCore.Users/AuditTrail/Models/AuditTrailUserEvent.cs
@@ -1,9 +1,29 @@
+using OrchardCore.Users.Models;
+
 namespace OrchardCore.Users.AuditTrail.Models;
 
+/// <summary>
+/// Event data for an Audit Trail event related to <see cref="Users.Models.User"/>s.
+/// </summary>
 public class AuditTrailUserEvent
 {
-    // TODO a future version should also record the User state, enabling diff against users
-    public string Name { get; set; } = "User";
+    /// <summary>
+    /// Gets the event name.
+    /// </summary>
+    public string Name => "User";
+    
+    /// <summary>
+    /// Gets or sets a snapshot of the <see cref="Users.Models.User"/> object, if the event modified it somehow.
+    /// </summary>
+    public User User { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the related user's <see cref="Users.Models.User.UserName"/>.
+    /// </summary>
     public string UserName { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the related user's <see cref="Users.Models.User.UserId"/>.
+    /// </summary>
     public string UserId { get; set; }
 }
