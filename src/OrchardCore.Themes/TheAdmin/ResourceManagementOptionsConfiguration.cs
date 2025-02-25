f3b5303a14b7960708c3cd9f8ff4114b2daebc6c
Jasmin Savard <jasmin.savard@gmail.com>
2025-02-25 14:20:09 -0500

    Clean Parcel dest folder before build
    Fixes issue with build and watch not updating files

--- src/OrchardCore.Themes/TheAdmin/ResourceManagementOptionsConfiguration.cs
+++ src/OrchardCore.Themes/TheAdmin/ResourceManagementOptionsConfiguration.cs
@@ -15,12 +15,12 @@ public sealed class ResourceManagementOptionsConfiguration
         _manifest
             .DefineScript("admin")
             .SetDependencies("bootstrap", "admin-main", "theme-manager", "jQuery", "Sortable")
-            .SetUrl("~/TheAdmin/js/TheAdmin.js")
+            .SetUrl("~/TheAdmin/js/the-admin/TheAdmin.js")
             .SetVersion("1.0.0");
 
         _manifest
             .DefineScript("admin-main")
-            .SetUrl("~/TheAdmin/js/TheAdmin-main.js")
+            .SetUrl("~/TheAdmin/js/the-admin-main/TheAdmin-main.js")
             .SetDependencies("theme-head", "js-cookie")
             .SetVersion("1.0.0");
 
