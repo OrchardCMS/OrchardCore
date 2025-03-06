85d650f091bab40e7c9e38f0ca087e498fa19667
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2025-03-06 21:55:04 +0300

    Use file-scoped

--- test/OrchardCore.Tests/Localization/LocalizationServiceCollectionExtensionsTests.cs
+++ test/OrchardCore.Tests/Localization/LocalizationServiceCollectionExtensionsTests.cs
@@ -1,33 +1,32 @@
 using OrchardCore.Localization.Data;
 
-namespace OrchardCore.Localization.Tests
+namespace OrchardCore.Localization.Tests;
+
+public class LocalizationServiceCollectionExtensionsTests
 {
-    public class LocalizationServiceCollectionExtensionsTests
+    [Fact]
+    public void AddDataLocaliztion_RegisterRequiredServices()
     {
-        [Fact]
-        public void AddDataLocaliztion_RegisterRequiredServices()
-        {
-            // Arrange
-            var services = new ServiceCollection();
+        // Arrange
+        var services = new ServiceCollection();
 
-            services.AddLogging();
-            services.AddMemoryCache();
-            services.AddSingleton<ILocalizationManager, LocalizationManager>();
+        services.AddLogging();
+        services.AddMemoryCache();
+        services.AddSingleton<ILocalizationManager, LocalizationManager>();
 
-            // Act
-            services.AddDataLocalization();
+        // Act
+        services.AddDataLocalization();
 
-            // Assert
-            var serviceProvider = services.BuildServiceProvider();
-            var translationProvider = serviceProvider.GetService<IDataTranslationProvider>();
-            var dataResourceManager = serviceProvider.GetService<DataResourceManager>();
-            var dataLocalizerFactory = serviceProvider.GetService<IDataLocalizerFactory>();
-            var dataLocalizer = serviceProvider.GetService<IDataLocalizer>();
+        // Assert
+        var serviceProvider = services.BuildServiceProvider();
+        var translationProvider = serviceProvider.GetService<IDataTranslationProvider>();
+        var dataResourceManager = serviceProvider.GetService<DataResourceManager>();
+        var dataLocalizerFactory = serviceProvider.GetService<IDataLocalizerFactory>();
+        var dataLocalizer = serviceProvider.GetService<IDataLocalizer>();
 
-            Assert.NotNull(translationProvider);
-            Assert.NotNull(dataResourceManager);
-            Assert.NotNull(dataLocalizerFactory);
-            Assert.NotNull(dataLocalizer);
-        }
+        Assert.NotNull(translationProvider);
+        Assert.NotNull(dataResourceManager);
+        Assert.NotNull(dataLocalizerFactory);
+        Assert.NotNull(dataLocalizer);
     }
 }
