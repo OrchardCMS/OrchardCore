9272fd3edb87f3a9094c50c014a36c8d67804c2c
Jasmin Savard <jasmin.savard@gmail.com>
2025-03-07 01:28:03 -0500

    Asset Manager - min action Sourcemaps
    Apply same logic than with Parcel.
    
    min.js --> minified for prod
    .js --> minified with .map reference
    
    This way we don't need to change any script tag.

--- src/OrchardCore.Themes/TheComingSoonTheme/wwwroot/js/scripts.js
+++ src/OrchardCore.Themes/TheComingSoonTheme/wwwroot/js/scripts.js
@@ -3,5 +3,4 @@
 * Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-coming-soon/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+//# sourceMappingURL=scripts.map
