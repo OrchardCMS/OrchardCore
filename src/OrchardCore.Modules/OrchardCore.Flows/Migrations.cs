59a2c3cf3185a87625233b7a99e837c011a58eed
Mike Alhayek <mike@crestapps.com>
2025-03-25 08:16:51 -0700

    Fix settings and add release notes

--- src/OrchardCore.Modules/OrchardCore.Flows/Migrations.cs
+++ src/OrchardCore.Modules/OrchardCore.Flows/Migrations.cs
@@ -1,4 +1,6 @@
 using OrchardCore.ContentManagement.Metadata;
+using OrchardCore.ContentManagement.Metadata.Builders;
+using OrchardCore.ContentManagement.Metadata.Models;
 using OrchardCore.ContentManagement.Metadata.Settings;
 using OrchardCore.Data.Migration;
 using OrchardCore.Flows.Models;
@@ -26,7 +28,7 @@ public sealed class Migrations : DataMigration
             .WithDescription("Provides a collection behavior for your content item where you can place other content items."));
 
         // Shortcut other migration steps on new content definition schemas.
-        return 3;
+        return 4;
     }
 
     // This code can be removed in a later version.
@@ -47,5 +49,56 @@ public sealed class Migrations : DataMigration
         await _contentDefinitionManager.MigratePartSettingsAsync<BagPart, BagPartSettings>();
 
         return 3;
+    }
+
+    public async Task<int> UpdateFrom3Async()
+    {
+        // In Version 3, we introduced the 'CollapseContainedItems' setting for 'BagPartSettings' and 'FlowPartSettings'.
+        // This allows users to control whether contained items are collapsed or expanded by default.
+        // To maintain the behavior for existing tenants, this migration sets 'CollapseContainedItems' to true for 'BagPart'.
+        // For consistency, both 'BagPart' and 'FlowPart' will expand contained items when the page loads by default.
+
+        await _contentDefinitionManager.AlterPartDefinitionAsync("BagPart", part => part
+            .MergeSettings<BagPartSettings>(settings =>
+            {
+                settings.CollapseContainedItems = true;
+            }));
+
+        var typeDefinitions = await _contentDefinitionManager.LoadTypeDefinitionsAsync();
+
+        foreach (var typeDefinition in typeDefinitions)
+        {
+            ContentTypeDefinitionBuilder builder = null;
+
+            foreach (var partDefinition in typeDefinition.Parts)
+            {
+                if (partDefinition.PartDefinition.Name != "BagPart")
+                {
+                    continue;
+                }
+
+                var name = partDefinition.IsNamedPart()
+                    ? partDefinition.Name
+                    : partDefinition.PartDefinition.Name;
+
+                builder ??= new ContentTypeDefinitionBuilder(typeDefinition);
+                builder
+                    .WithPart<BagPart>(name, part => part
+                        .MergeSettings<BagPartSettings>(settings =>
+                        {
+                            settings.CollapseContainedItems = true;
+                        })
+                    );
+            }
+
+            if (builder is not null)
+            {
+                await _contentDefinitionManager.StoreTypeDefinitionAsync(builder.Build());
+
+                builder = null;
+            }
+        }
+
+        return 4;
     }
 }
