52283370830b6d5f2f56ed6fd2c200a5230c8f60
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2025-03-27 21:18:29 +0300

    OrchardHelper content APIs should be accessible without namespace (#17189)

--- src/OrchardCore/OrchardCore.Contents.Core/Extensions/ContentOrchardHelperExtensions.cs
+++ src/OrchardCore/OrchardCore.Contents.Core/Extensions/ContentOrchardHelperExtensions.cs
@@ -1,11 +1,12 @@
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore;
 using OrchardCore.ContentManagement;
 using OrchardCore.ContentManagement.Records;
 using YesSql;
 
-namespace OrchardCore;
-
-[Obsolete("This class has been deprecated, please use ContentOrchardHelperExtensions instead.")]
-public static class ContentRazorHelperExtensions
+#pragma warning disable CA1050 // Declare types in namespaces
+public static class ContentOrchardHelperExtensions
+#pragma warning restore CA1050 // Declare types in namespaces
 {
     /// <summary>
     /// Returns a content item id by its handle.
@@ -15,8 +16,12 @@ public static class ContentRazorHelperExtensions
     /// <example>GetContentItemIdByHandleAsync("alias:carousel").</example>
     /// <example>GetContentItemIdByHandleAsync("slug:myblog/my-blog-post").</example>
     /// <returns>A content item id or <c>null</c> if it was not found.</returns>
-    public static Task<string> GetContentItemIdByHandleAsync(this IOrchardHelper orchardHelper, string handle)
-        => ContentOrchardHelperExtensions.GetContentItemIdByHandleAsync(orchardHelper, handle);
+    public static async Task<string> GetContentItemIdByHandleAsync(this IOrchardHelper orchardHelper, string handle)
+    {
+        var contentHandleManager = orchardHelper.HttpContext.RequestServices.GetService<IContentHandleManager>();
+
+        return await contentHandleManager.GetContentItemIdAsync(handle);
+    }
 
     /// <summary>
     /// Loads a content item by its handle.
@@ -26,7 +31,13 @@ public static class ContentRazorHelperExtensions
     /// <param name="option">A specific version to load or the default version.</param>
     /// <returns>A content item with the specific name, or <c>null</c> if it doesn't exist.</returns>
     public static async Task<ContentItem> GetContentItemByHandleAsync(this IOrchardHelper orchardHelper, string handle, VersionOptions option = null)
-        => await ContentOrchardHelperExtensions.GetContentItemByHandleAsync(orchardHelper, handle, option);
+    {
+        var contentManager = orchardHelper.HttpContext.RequestServices.GetService<IContentManager>();
+
+        var contentItemId = await orchardHelper.GetContentItemIdByHandleAsync(handle);
+
+        return await contentManager.GetAsync(contentItemId, option);
+    }
 
     /// <summary>
     /// Loads a content item by its id.
@@ -35,8 +46,12 @@ public static class ContentRazorHelperExtensions
     /// <param name="contentItemId">The content item id to load.</param>
     /// <param name="option">A specific version to load or the default version.</param>
     /// <returns>A content item with the specific id, or <c>null</c> if it doesn't exist.</returns>
-    public static Task<ContentItem> GetContentItemByIdAsync(this IOrchardHelper orchardHelper, string contentItemId, VersionOptions option = null)
-        => ContentOrchardHelperExtensions.GetContentItemByIdAsync(orchardHelper, contentItemId, option);
+    public static async Task<ContentItem> GetContentItemByIdAsync(this IOrchardHelper orchardHelper, string contentItemId, VersionOptions option = null)
+    {
+        var contentManager = orchardHelper.HttpContext.RequestServices.GetService<IContentManager>();
+
+        return await contentManager.GetAsync(contentItemId, option);
+    }
 
     /// <summary>
     /// Loads a list of content items by their ids.
@@ -45,8 +60,12 @@ public static class ContentRazorHelperExtensions
     /// <param name="contentItemIds">The content item ids to load.</param>
     /// <param name="option">A specific version to load or the default version.</param>
     /// <returns>A list of content items with the specific ids.</returns>
-    public static Task<IEnumerable<ContentItem>> GetContentItemsByIdAsync(this IOrchardHelper orchardHelper, IEnumerable<string> contentItemIds, VersionOptions option = null)
-        => ContentOrchardHelperExtensions.GetContentItemsByIdAsync(orchardHelper, contentItemIds, option);
+    public static async Task<IEnumerable<ContentItem>> GetContentItemsByIdAsync(this IOrchardHelper orchardHelper, IEnumerable<string> contentItemIds, VersionOptions option = null)
+    {
+        var contentManager = orchardHelper.HttpContext.RequestServices.GetService<IContentManager>();
+
+        return await contentManager.GetAsync(contentItemIds, option);
+    }
 
     /// <summary>
     /// Loads a content item by its version id.
@@ -54,14 +73,25 @@ public static class ContentRazorHelperExtensions
     /// <param name="orchardHelper">The <see cref="IOrchardHelper"/>.</param>
     /// <param name="contentItemVersionId">The content item version id to load.</param>
     /// <returns>A content item with the specific version id, or <c>null</c> if it doesn't exist.</returns>
-    public static Task<ContentItem> GetContentItemByVersionIdAsync(this IOrchardHelper orchardHelper, string contentItemVersionId)
-        => ContentOrchardHelperExtensions.GetContentItemByVersionIdAsync(orchardHelper, contentItemVersionId);
+    public static async Task<ContentItem> GetContentItemByVersionIdAsync(this IOrchardHelper orchardHelper, string contentItemVersionId)
+    {
+        var contentManager = orchardHelper.HttpContext.RequestServices.GetService<IContentManager>();
+
+        return await contentManager.GetVersionAsync(contentItemVersionId);
+    }
 
     /// <summary>
     /// Query content items.
     /// </summary>
     public static async Task<IEnumerable<ContentItem>> QueryContentItemsAsync(this IOrchardHelper orchardHelper, Func<IQuery<ContentItem, ContentItemIndex>, IQuery<ContentItem>> query)
-        => await ContentOrchardHelperExtensions.QueryContentItemsAsync(orchardHelper, query);
+    {
+        var contentManager = orchardHelper.HttpContext.RequestServices.GetService<IContentManager>();
+        var session = orchardHelper.HttpContext.RequestServices.GetService<ISession>();
+
+        var contentItems = await query(session.Query<ContentItem, ContentItemIndex>()).ListAsync();
+
+        return await contentManager.LoadAsync(contentItems);
+    }
 
     /// <summary>
     /// Loads content items of a specific type.
@@ -69,6 +99,8 @@ public static class ContentRazorHelperExtensions
     /// <param name="orchardHelper">The <see cref="IOrchardHelper"/>.</param>
     /// <param name="contentType">The content type to load.</param>
     /// <param name="maxContentItems">The maximum content items to return.</param>
-    public static Task<IEnumerable<ContentItem>> GetRecentContentItemsByContentTypeAsync(this IOrchardHelper orchardHelper, string contentType, int maxContentItems = 10)
-        => ContentOrchardHelperExtensions.GetRecentContentItemsByContentTypeAsync(orchardHelper, contentType, maxContentItems);
+    public static async Task<IEnumerable<ContentItem>> GetRecentContentItemsByContentTypeAsync(this IOrchardHelper orchardHelper, string contentType, int maxContentItems = 10)
+        => await orchardHelper.QueryContentItemsAsync(query => query.Where(x => x.ContentType == contentType && x.Published == true)
+        .OrderByDescending(i => i.CreatedUtc)
+        .Take(maxContentItems));
 }
