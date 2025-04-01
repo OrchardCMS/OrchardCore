0441be9d457cd0d7250bddb5dc459458fbb8c678
Jack Telford <wipke9893@yahoo.com>
2025-04-01 14:15:51 -0400

    Revert "t"
    This reverts commit 238de8d520c0816cf7c34ed1ae6f68988119338b.

--- src/OrchardCore.Modules/OrchardCore.Forms/ViewModels/FormVisibilityFieldViewModel.cs
+++ src/OrchardCore.Modules/OrchardCore.Forms/ViewModels/FormVisibilityFieldViewModel.cs
@@ -5,4 +5,10 @@ public class FormVisibilityFieldViewModel
     public string Name { get; set; }
 
     public string Value { get; set; }
+
+    public bool IsPredefinedValues { get; set; }
+
+    public string[] PredefinedValues { get; set; }
+
+    public string[] SelectedValues { get; set; }
 }
