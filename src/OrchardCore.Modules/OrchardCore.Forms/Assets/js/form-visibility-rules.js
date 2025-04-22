b633401b50f6a6f20f2b521b5744c097fc262f7a
Jack Telford <Wipke9893@yahoo.com>
2025-04-22 14:47:03 -0400

    Update src/OrchardCore.Modules/OrchardCore.Forms/Assets/js/form-visibility-rules.js
    Co-authored-by: Mike Alhayek <mike@crestapps.com>

--- src/OrchardCore.Modules/OrchardCore.Forms/Assets/js/form-visibility-rules.js
+++ src/OrchardCore.Modules/OrchardCore.Forms/Assets/js/form-visibility-rules.js
@@ -153,7 +153,6 @@ window.formVisibilityGroupRules = (function () {
                     restoreOriginalState(inputElement);
                 }
             }
-
             else if (data.action === 'Hide') {
 
                 if (anyGroupRuleMet) {
