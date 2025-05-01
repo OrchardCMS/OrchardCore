0f66f8d06bfc9da1580c2bd5c67994de128c5dc6
Jack Telford <Wipke9893@yahoo.com>
2025-05-01 12:24:15 -0400

    Update src/OrchardCore.Modules/OrchardCore.Forms/Models/FormInputElementVisibilityPart.cs
    Co-authored-by: Mike Alhayek <mike@crestapps.com>

--- src/OrchardCore.Modules/OrchardCore.Forms/Models/FormInputElementVisibilityPart.cs
+++ src/OrchardCore.Modules/OrchardCore.Forms/Models/FormInputElementVisibilityPart.cs
@@ -2,7 +2,7 @@ using OrchardCore.ContentManagement;
 
 namespace OrchardCore.Forms.Models;
 
-public class FormInputElementVisibilityPart : ContentPart
+public sealed class FormInputElementVisibilityPart : ContentPart
 {
     public IEnumerable<FormVisibilityRuleGroup> Groups { get; set; }
 
