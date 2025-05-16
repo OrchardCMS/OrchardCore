42d945b0b8d731ee716c6b7061de7864f5dd961f
infofromca <zgofmontreal@gmail.com>
2025-05-16 18:24:28 -0400

    Make IChunkFileUploadService reusable

--- src/OrchardCore/OrchardCore.Media.Abstractions/IChunkFileUploadService.cs
+++ src/OrchardCore/OrchardCore.Media.Abstractions/IChunkFileUploadService.cs
@@ -14,20 +14,6 @@ public interface IChunkFileUploadService
     /// <see cref="MediaOptions"/> and <paramref name="request"/> content.
     /// </summary>
     /// <param name="request">The request to process.</param>
-    /// <param name="chunkAsync">Callback to report the consumer and create the <see cref="IActionResult"/> in case of
-    /// a chunk was uploaded.</param>
-    /// <param name="completedAsync">Callback to report the consumer and create the <see cref="IActionResult"/> in case
-    /// of all chunks were uploaded or a regular file upload happened.</param>
-    Task<IActionResult> ProcessRequestAsync(
-        HttpRequest request,
-        Func<Guid, IFormFile, ContentRangeHeaderValue, Task<IActionResult>> chunkAsync,
-        Func<IEnumerable<IFormFile>, Task<IActionResult>> completedAsync);
-
-    /// <summary>
-    /// Processes the <paramref name="request"/> and handles chunked or regular file uploads depending on the
-    /// <see cref="MediaOptions"/> and <paramref name="request"/> content.
-    /// </summary>
-    /// <param name="request">The request to process.</param>
     /// <param name="chunkAsync">Callback to report the consumer and create the <see cref="object"/> in case of
     /// a chunk was uploaded.</param>
     /// <param name="completedAsync">Callback to report the consumer and create the <see cref="object"/> in case
