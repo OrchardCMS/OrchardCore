22a347f03dc387aa24eb6bd6551e449bbadd416d
Krisztián Németh <krisztian.nemeth@lombiq.com>
2025-06-13 12:21:00 +0200

    Build new wwwroot

---
+++ src/OrchardCore.Themes/TheAgencyTheme/wwwroot/js/scripts.js
@@ -0,0 +1,6 @@
+/*!
+* Start Bootstrap - Agency v7.0.12 (https://startbootstrap.com/theme/agency)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+*/window.addEventListener("DOMContentLoaded",e=>{var n=function(){let e=document.body.querySelector("#mainNav");e&&(0===window.scrollY?e.classList.remove("navbar-shrink"):e.classList.add("navbar-shrink"))};n(),document.addEventListener("scroll",n),document.body.querySelector("#mainNav")&&new bootstrap.ScrollSpy(document.body,{target:"#mainNav",rootMargin:"0px 0px -40%"});let o=document.body.querySelector(".navbar-toggler");[].slice.call(document.querySelectorAll("#navbarResponsive .nav-link")).map(function(e){e.addEventListener("click",()=>{"none"!==window.getComputedStyle(o).display&&o.click()})})});
+//# sourceMappingURL=scripts.map
