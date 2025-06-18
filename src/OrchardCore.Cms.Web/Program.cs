1ca7274e222b90bc971a0a09d4ac9525abd5f2f8
Georg von Kries <gvk@creativbox.net>
2025-06-18 11:18:09 +0200

    Avoid exceptions during startup

--- src/OrchardCore.Cms.Web/Program.cs
+++ src/OrchardCore.Cms.Web/Program.cs
@@ -1,4 +1,9 @@
+using System.Text;
 using OrchardCore.Logging;
+
+// Register the CodePagesEncodingProvider to support additional encodings. This is done to improve performance by preventing
+// several first-chance exceptions thrown by MimeKit when it tries to load encodings that are not available by default in .NET Core.
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
 var builder = WebApplication.CreateBuilder(args);
 
--- test/OrchardCore.Tests/Apis/Context/SiteStartup.cs
+++ test/OrchardCore.Tests/Apis/Context/SiteStartup.cs
@@ -11,6 +11,10 @@ public class SiteStartup
 
     static SiteStartup()
     {
+        // Register the CodePagesEncodingProvider to support additional encodings. This is done to improve performance by preventing
+        // several first-chance exceptions thrown by MimeKit when it tries to load encodings that are not available by default in .NET Core.
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
         PermissionsContexts = new ConcurrentDictionary<string, PermissionsContext>();
     }
 
