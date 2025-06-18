80e43185f811898b71d7ded0b3946c699006d9b5
Mike Alhayek <mike@crestapps.com>
2025-06-18 12:48:41 -0700

    update comments

--- test/OrchardCore.Tests/Apis/GraphQL/Queries/RecentBlogPostsQueryTests.cs
+++ test/OrchardCore.Tests/Apis/GraphQL/Queries/RecentBlogPostsQueryTests.cs
@@ -26,7 +26,7 @@ public class RecentBlogPostsQueryTests
                     });
             });
 
-        // Indexing of the content item happens in the background and may not be immediate available,
+        // Indexing of the content item happens in the deferred-task and may not be immediate available,
         // so we wait until the indexing is done before querying.
         await context.WaitForOutstandingDeferredTasksAsync(TestContext.Current.CancellationToken);
 
