c7fa642a59d106b7e415a8f86be73aab02a0f754
Mike Alhayek <mike@crestapps.com>
2025-06-19 11:18:45 -0700

    rename

--- src/OrchardCore.Modules/OrchardCore.Indexing/DataMigrations/RecordIndexingTaskMigrations.cs
+++ src/OrchardCore.Modules/OrchardCore.Indexing/DataMigrations/RecordIndexingTaskMigrations.cs
@@ -7,10 +7,10 @@ internal sealed class RecordIndexingTaskMigrations : DataMigration
     public async Task<int> CreateAsync()
     {
         await SchemaBuilder.CreateTableAsync("RecordIndexingTask", table => table
-           .Column<int>("Id", col => col.PrimaryKey().Identity())
-           .Column<string>("RecordId", c => c.WithLength(26))
-           .Column<string>("Category", c => c.WithLength(50))
-           .Column<DateTime>("CreatedUtc", col => col.NotNull())
+           .Column<int>("Id", column => column.PrimaryKey().Identity())
+           .Column<string>("RecordId", column => column.WithLength(26))
+           .Column<string>("Category", column => column.WithLength(50))
+           .Column<DateTime>("CreatedUtc", column => column.NotNull())
            .Column<int>("Type")
         );
 
