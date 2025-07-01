cbddf77808c217ced6a4badb03a1d26068c31ee0
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2025-07-02 02:54:11 +0300

    TextField color editor should accept default value with #XXXXXX format

--- src/OrchardCore.Modules/OrchardCore.ContentFields/Settings/TextFieldSettingsDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.ContentFields/Settings/TextFieldSettingsDriver.cs
@@ -1,13 +1,25 @@
+using System.Text.RegularExpressions;
+using Microsoft.Extensions.Localization;
 using OrchardCore.ContentFields.Fields;
 using OrchardCore.ContentManagement.Metadata.Models;
+using OrchardCore.ContentManagement.Metadata.Settings;
 using OrchardCore.ContentTypes.Editors;
 using OrchardCore.DisplayManagement.Handlers;
 using OrchardCore.DisplayManagement.Views;
 
 namespace OrchardCore.ContentFields.Settings;
 
-public sealed class TextFieldSettingsDriver : ContentPartFieldDefinitionDisplayDriver<TextField>
+public sealed partial class TextFieldSettingsDriver : ContentPartFieldDefinitionDisplayDriver<TextField>
 {
+    private const string TextFieldColorEditor = "Color";
+
+    internal readonly IStringLocalizer S;
+
+    public TextFieldSettingsDriver(IStringLocalizer<TextFieldSettingsDriver> localizer)
+    {
+        S = localizer;
+    }
+
     public override IDisplayResult Edit(ContentPartFieldDefinition partFieldDefinition, BuildEditorContext context)
     {
         return Initialize<TextFieldSettings>("TextFieldSettings_Edit", model =>
@@ -22,12 +34,25 @@ public sealed class TextFieldSettingsDriver : ContentPartFieldDefinitionDisplayD
 
     public override async Task<IDisplayResult> UpdateAsync(ContentPartFieldDefinition partFieldDefinition, UpdatePartFieldEditorContext context)
     {
+        var contentPartFieldSettings = partFieldDefinition.GetSettings<ContentPartFieldSettings>();
         var model = new TextFieldSettings();
 
         await context.Updater.TryUpdateModelAsync(model, Prefix);
+
+        // Prevent accepting color with #XXX format, because the color editor will use black color all the time.
+        if (contentPartFieldSettings.Editor == TextFieldColorEditor)
+        {
+            if (!HexColor().IsMatch(model.DefaultValue))
+            {
+                context.Updater.ModelState.AddModelError(Prefix, S["A value for {0} should be in '#XXXXXX' format.", partFieldDefinition.DisplayName()]);
+            }
+        }
 
         context.Builder.WithSettings(model);
 
         return Edit(partFieldDefinition, context);
     }
+
+    [GeneratedRegex(@"^#(?:[0-9a-fA-F]{6})$")]
+    private static partial Regex HexColor();
 }
