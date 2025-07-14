83eadc6ec417708743c2d4d23420dfee1ea1783a
lampersky <lampersky@gmail.com>
2025-07-14 11:08:14 +0200

    js code moved into dedicated file

---
+++ src/OrchardCore.Modules/OrchardCore.Contents/wwwroot/Scripts/expand-cards-with-invalid-inputs.js
@@ -0,0 +1,20 @@
+(function () {
+    function expandCardsWithInvalidInputs(event) {
+        const elements = event.target.form.querySelectorAll(`input`);
+        Array.from(elements).filter(element => !element.checkValidity()).forEach(current => {
+            current = current.parentElement;
+            while (current) {
+                if (current.matches('.widget.widget-editor.card.collapsed')) {
+                    current.classList.remove('collapsed');
+                }
+
+                current = current.parentElement;
+            }
+        });
+    }
+    document.addEventListener("DOMContentLoaded", () => {
+        document.querySelectorAll('button[name="submit.Publish"]').forEach(button => {
+            button.addEventListener('click', (event) => expandCardsWithInvalidInputs(event));
+        });
+    });
+})();
