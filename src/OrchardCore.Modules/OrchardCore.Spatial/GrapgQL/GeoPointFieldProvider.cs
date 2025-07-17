ae823be2422e76b0e061ea09d5e0e56935542c79
Rumen Novachkov <r.novachkov@gmail.com>
2025-07-18 00:32:53 +0300

    Including GraphQL support for the GeoPointField

---
+++ src/OrchardCore.Modules/OrchardCore.Spatial/GrapgQL/GeoPointFieldProvider.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphQL.Resolvers;
+using GraphQL.Types;
+using Microsoft.Extensions.Localization;
+using OrchardCore.ContentManagement;
+using OrchardCore.ContentManagement.GraphQL.Queries.Types;
+using OrchardCore.ContentManagement.Metadata.Models;
+using OrchardCore.Spatial.Fields;
+
+namespace OrchardCore.Spatial.GrapgQL;
+public class GeoPointFieldProvider : IContentFieldProvider
+{
+    protected readonly IStringLocalizer S;
+    public GeoPointFieldProvider(IStringLocalizer<GeoPointFieldProvider> stringLocalizer)
+    {
+        S = stringLocalizer;
+    }
+
+    private static object handleFieldResolving(ContentElement field)
+        => new { Latitude = ((GeoPointField)field).Latitude, Longitude = ((GeoPointField)field).Longitude };
+
+    public FieldType GetField(ISchema schema, ContentPartFieldDefinition field, string namedPartTechnicalName, string customFieldName = null)
+    {
+
+        return new FieldType
+        {
+            Name = customFieldName ?? schema.NameConverter.NameForField(field.Name, null),
+            Description = S["Geo point field"],
+            Type = typeof(GeoPointGraphType),
+            ResolvedType = new GeoPointGraphType(),
+            Resolver = new FuncFieldResolver<ContentElement, object>(context =>
+            {
+                // Check if part has been collapsed by trying to get the parent part.
+                ContentElement contentPart = context.Source.Get<ContentPart>(field.PartDefinition.Name);
+
+                // Part is not collapsed, access field directly.
+                contentPart ??= context.Source;
+
+                var contentField = contentPart?.Get(typeof(GeoPointField), field.Name);
+
+                contentField ??= context.Source.Get(typeof(GeoPointField), field.Name);
+
+                return contentField == null ? null : handleFieldResolving(contentField);
+            }),
+        };
+    }
+
+    public FieldTypeIndexDescriptor GetFieldIndex(ContentPartFieldDefinition field)
+    {
+        return null;
+    }
+
+    public bool HasField(ISchema schema, ContentPartFieldDefinition field)
+    {
+        if (field.FieldDefinition.Name == nameof(GeoPointField)) return true;
+        return false;
+    }
+
+    public bool HasFieldIndex(ContentPartFieldDefinition field)
+        => false;
+}
