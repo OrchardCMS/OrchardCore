3b80fa9d8b878fb5dac79f7e76886c806fcaf14c
John Romberger <jromberger@autolenders.com>
2025-07-28 18:48:13 -0400

    Specify Order for Contents and PublishLater Startups.

---
+++ src/OrchardCore.Modules/OrchardCore.PublishLater/Handlers/PublishLaterContentPartHandler.cs
@@ -0,0 +1,25 @@
+using OrchardCore.ContentManagement;
+using OrchardCore.ContentManagement.Handlers;
+using OrchardCore.PublishLater.Models;
+
+namespace OrchardCore.PublishLater.Handlers;
+
+/// <summary>
+/// When a post was set to PublishLater, we want to set its Published date to the actual scheduled time,
+/// not the time the website just happened to run the background task.
+/// </summary>
+public class PublishLaterContentPartHandler : ContentPartHandler<PublishLaterPart>
+{
+    public override Task PublishingAsync(PublishContentContext context, PublishLaterPart part)
+    {
+        if (part.DoingScheduledPublish && part.ScheduledPublishUtc.HasValue)
+        {
+            context.ContentItem.PublishedUtc = part.ScheduledPublishUtc;
+            part.ScheduledPublishUtc = null;
+            part.DoingScheduledPublish = false;
+            part.Apply();
+        }
+
+        return Task.CompletedTask;
+    }
+}
