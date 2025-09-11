a0ce58000bfcd9991fce2965fecf09eb6263604c
Mike Alhayek <mike@crestapps.com>
2025-09-11 12:41:16 -0700

    remove unwanted changed

--- src/OrchardCore/OrchardCore.DisplayManagement/Shapes/CoreShapes.cs
+++ src/OrchardCore/OrchardCore.DisplayManagement/Shapes/CoreShapes.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using OrchardCore.DisplayManagement.Descriptors;
 using OrchardCore.DisplayManagement.Implementation;
 using OrchardCore.Modules;
-using OrchardCore.Mvc.Utilities;
 
 namespace OrchardCore.DisplayManagement.Shapes;
 
