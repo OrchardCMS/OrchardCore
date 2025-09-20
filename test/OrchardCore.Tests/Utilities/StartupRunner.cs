92ade0b513333fb9ceffa794cbe1a424ab81c116
Antoine Griffard <agriffard@hotmail.com>
2025-09-20 15:53:14 +0200

    Fix unit tests

--- test/OrchardCore.Tests/Utilities/StartupRunner.cs
+++ test/OrchardCore.Tests/Utilities/StartupRunner.cs
@@ -13,25 +13,16 @@ internal static class StartupRunner
 {
     public static async Task Run(Type startupType, string culture, string expected)
     {
-        var builder = WebApplication.CreateBuilder();
-        
-        // Configure the startup type (assuming it implements IStartup or has Configure/ConfigureServices methods)
-        var startupInstance = Activator.CreateInstance(startupType);
-        
-        // Call ConfigureServices if it exists
-        var configureServicesMethod = startupType.GetMethod("ConfigureServices");
-        configureServicesMethod?.Invoke(startupInstance, [builder.Services]);
-        
-        var app = builder.Build();
-        
-        // Call Configure if it exists
-        var configureMethod = startupType.GetMethod("Configure");
-        configureMethod?.Invoke(startupInstance, [app]);
-        
-        // Create TestServer with the app's ServiceProvider (non-obsolete constructor)
-        using var testHost = new TestServer(app.Services);
+        var builder = Host.CreateDefaultBuilder()
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseTestServer()
+                          .UseStartup(startupType);
+            });
 
-        var client = testHost.CreateClient();
+        using var host = await builder.StartAsync();
+        var client = host.GetTestClient();
+        
         var request = new HttpRequestMessage();
         var cookieValue = $"c={culture}|uic={culture}";
         request.Headers.Add("Cookie", $"{CookieRequestCultureProvider.DefaultCookieName}={cookieValue}");
