deabac6fd3cf6e7a54c6cb8bf4f766c1101158b7
Mike Alhayek <mike@crestapps.com>
2025-10-06 13:29:25 -0700

    fix build

--- src/OrchardCore/OrchardCore.DisplayManagement/TagHelpers/BaseShapeTagHelper.cs
+++ src/OrchardCore/OrchardCore.DisplayManagement/TagHelpers/BaseShapeTagHelper.cs
@@ -139,6 +139,8 @@ public abstract class BaseShapeTagHelper : TagHelper
             shape.Metadata.DisplayType = Convert.ToString(displayType.Value);
         }
 
+        await ShapeBuildingAsync(shape);
+
         tagHelperContext.Items[typeof(IShape)] = shape;
 
         if (!string.IsNullOrWhiteSpace(Cache))
@@ -177,4 +179,7 @@ public abstract class BaseShapeTagHelper : TagHelper
         // We don't want any encapsulating tag around the shape
         output.TagName = null;
     }
+
+    protected virtual ValueTask ShapeBuildingAsync(IShape shape)
+        => ValueTask.CompletedTask;
 }
