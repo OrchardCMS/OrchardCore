5a28a0f8ee97e874b885cfe2e8c5df564e1cc0bb
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2025-11-27 17:07:26 +0300

    TextField color editor should accept default value with #RRGGBB format (#18122)

--- src/OrchardCore.Modules/OrchardCore.ContentFields/Settings/TextFieldSettingsDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.ContentFields/Settings/TextFieldSettingsDriver.cs
@@ -1,8 +1,10 @@
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Localization;
 using OrchardCore.ContentFields.Fields;
 using OrchardCore.ContentFields.Settings.Models;
 using OrchardCore.ContentManagement.Metadata.Models;
+using OrchardCore.ContentManagement.Metadata.Settings;
 using OrchardCore.ContentTypes.Editors;
 using OrchardCore.DisplayManagement.Handlers;
 using OrchardCore.DisplayManagement.Views;
@@ -11,8 +13,10 @@ using OrchardCore.Mvc.ModelBinding;
 
 namespace OrchardCore.ContentFields.Settings;
 
-public sealed class TextFieldSettingsDriver : ContentPartFieldDefinitionDisplayDriver<TextField>
+public sealed partial class TextFieldSettingsDriver : ContentPartFieldDefinitionDisplayDriver<TextField>
 {
+    private const string TextFieldColorEditor = "Color";
+
     private readonly ILiquidTemplateManager _liquidTemplateManager;
 
     internal readonly IStringLocalizer S;
@@ -47,6 +51,8 @@ public sealed class TextFieldSettingsDriver : ContentPartFieldDefinitionDisplayD
 
     public override async Task<IDisplayResult> UpdateAsync(ContentPartFieldDefinition partFieldDefinition, UpdatePartFieldEditorContext context)
     {
+        var contentPartFieldSettings = partFieldDefinition.GetSettings<ContentPartFieldSettings>();
+
         var model = new TextFieldSettingsViewModel();
 
         await context.Updater.TryUpdateModelAsync(model, Prefix);
@@ -62,6 +68,13 @@ public sealed class TextFieldSettingsDriver : ContentPartFieldDefinitionDisplayD
                 context.Updater.ModelState.AddModelError(Prefix, nameof(model.Pattern), S["Pattern doesn't contain a valid Liquid expression. Details: {0}", string.Join(' ', errors)]);
             }
         }
+        
+        if (contentPartFieldSettings.Editor == TextFieldColorEditor &&
+            !string.IsNullOrEmpty(model.DefaultValue) &&
+            !HexColorRegex().IsMatch(model.DefaultValue))
+        {
+            context.Updater.ModelState.AddModelError(Prefix, S["A value for {0} should be in #RGB, #RGBA, #RRGGBB, or #RRGGBBAA formats.", partFieldDefinition.DisplayName()]);
+        }
 
         context.Builder.WithSettings(new TextFieldSettings
         {
@@ -74,4 +87,8 @@ public sealed class TextFieldSettingsDriver : ContentPartFieldDefinitionDisplayD
 
         return Edit(partFieldDefinition, context);
     }
+
+    // Supports the hexadecimal color codes in the formats #RGB, #RGBA, #RRGGBB, and #RRGGBBAA.
+    [GeneratedRegex(@"^#(?:[0-9a-fA-F]{3}|[0-9a-fA-F]{4}|[0-9a-fA-F]{6}|[0-9a-fA-F]{8})$")]
+    private static partial Regex HexColorRegex();
 }
