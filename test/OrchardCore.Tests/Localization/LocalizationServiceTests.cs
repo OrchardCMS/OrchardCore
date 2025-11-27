d3b29cb8fec32fc5e49653c529ecbfd4997ea8ae
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2025-11-28 01:12:57 +0300

    Return the unit test back

---
+++ test/OrchardCore.Tests/Localization/LocalizationServiceTests.cs
@@ -0,0 +1,22 @@
+using OrchardCore.Localization;
+using OrchardCore.Localization.Services;
+using OrchardCore.Settings;
+
+namespace OrchardCore.Tests.Localization;
+
+public class LocalizationServiceTest
+{
+    [Fact]
+    public void GetAllCulturesAndAliases_ShouldContainsChineseCultures()
+    {
+        //// Arrange
+        var localizationService = new LocalizationService(Mock.Of<ISiteService>());
+
+        // Act
+        var cultures = localizationService.GetAllCulturesAndAliases();
+
+        // Assert
+        Assert.Single(cultures, c => c.Name == "zh-CN");
+        Assert.Single(cultures, c => c.Name == "zh-TW");
+    }
+}
