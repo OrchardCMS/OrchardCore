43a2bb635b6c7c7401f68350e0778398c1f050b8
Georg von Kries <gvk@creativbox.net>
2025-12-11 09:17:37 +0100

    Update src/OrchardCore/OrchardCore/Modules/ModularBackgroundService.cs
    Co-authored-by: Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>

--- src/OrchardCore/OrchardCore/Modules/ModularBackgroundService.cs
+++ src/OrchardCore/OrchardCore/Modules/ModularBackgroundService.cs
@@ -433,22 +433,25 @@ internal sealed class ModularBackgroundService : BackgroundService
 
     private static void SetBaseUrl(HttpContext context, string baseUrl)
     {
-        if (Uri.TryCreate(baseUrl, UriKind.Absolute, out var uri))
+        if (!Uri.TryCreate(baseUrl, UriKind.Absolute, out var uri))
         {
-            context.Request.Scheme = uri.Scheme;
-            context.Request.Host = new HostString(uri.Host, uri.Port);
-
-            // Set the PathBase only if it's not just "/". Otherwise, the original path base is kept, which may
-            // contain the RequestUrlPrefix if any.
-            if (!string.IsNullOrEmpty(uri.AbsolutePath) && uri.AbsolutePath != "/")
-            {
-                context.Request.PathBase = uri.AbsolutePath;
-            }
-
-            if (!string.IsNullOrWhiteSpace(uri.Query))
-            {
-                context.Request.QueryString = new QueryString(uri.Query);
-            }
+            return;
         }
+
+        context.Request.Scheme = uri.Scheme;
+        context.Request.Host = new HostString(uri.Host, uri.Port);
+
+        // Set the PathBase only if it's not just "/". Otherwise, the original path base is kept, which may
+        // contain the RequestUrlPrefix if any.
+        if (!string.IsNullOrEmpty(uri.AbsolutePath) && uri.AbsolutePath != "/")
+        {
+            context.Request.PathBase = uri.AbsolutePath;
+        }
+
+        if (!string.IsNullOrWhiteSpace(uri.Query))
+        {
+            context.Request.QueryString = new QueryString(uri.Query);
+        }
+    }
     }
 }
