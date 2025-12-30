adfe697bbfd378fc39d5ac50ea7991c38f6043c5
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2025-12-30 07:30:37 +0300

    Add docs

--- src/OrchardCore/OrchardCore.Abstractions/Shell/IFeatureUsageChecker.cs
+++ src/OrchardCore/OrchardCore.Abstractions/Shell/IFeatureUsageChecker.cs
@@ -2,7 +2,16 @@ using OrchardCore.Environment.Extensions.Features;
 
 namespace OrchardCore.Environment.Shell;
 
+/// <summary>
+/// Defines a contract for determining whether a specified feature is currently in use.
+/// </summary>
 public interface IFeatureUsageChecker
 {
+    /// <summary>
+    /// Determines whether the specified feature is currently in use.
+    /// </summary>
+    /// <param name="feature">The feature to check for active usage. Cannot be null.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains <see langword="true"/> if the
+    /// feature is in use; otherwise, <see langword="false"/>.</returns>
     Task<bool> IsFeatureInUseAsync(IFeatureInfo feature);
 }
--- src/OrchardCore/OrchardCore.Deployment.Core/Services/DeploymentFeatureUsageChecker.cs
+++ src/OrchardCore/OrchardCore.Deployment.Core/Services/DeploymentFeatureUsageChecker.cs
@@ -5,11 +5,19 @@ using OrchardCore.Environment.Shell;
 
 namespace OrchardCore.Deployment.Core.Services;
 
+/// <summary>
+/// Provides functionality to determine whether a feature is currently in use by any deployment plan, preventing its
+/// deactivation if it is referenced.
+/// </summary>
+/// <param name="deploymentPlanService">The service used to retrieve deployment plans for feature usage analysis.</param>
+/// <param name="typeFeatureProvider">The provider that maps features to their associated types for inspection.</param>
+/// <param name="logger">The logger used to record warnings or informational messages related to feature usage checks.</param>
 public class DeploymentFeatureUsageChecker(
     IDeploymentPlanService deploymentPlanService,
     ITypeFeatureProvider typeFeatureProvider,
     ILogger<DeploymentFeatureUsageChecker> logger) : IFeatureUsageChecker
 {
+    /// <inheritdoc/>
     public async Task<bool> IsFeatureInUseAsync(IFeatureInfo feature)
     {
         var deploymentStepTypeNames = await GetDeploymentStepNamesAsync();
