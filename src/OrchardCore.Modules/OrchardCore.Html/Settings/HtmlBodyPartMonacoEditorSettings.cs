85ed346c0029a7692326884bfda3dfb46fc0a8fd
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-01-15 14:16:27 +0300

    Set HtmlBodyPartMonacoEditorSettings.Options by default

--- src/OrchardCore.Modules/OrchardCore.Html/Settings/HtmlBodyPartMonacoEditorSettings.cs
+++ src/OrchardCore.Modules/OrchardCore.Html/Settings/HtmlBodyPartMonacoEditorSettings.cs
@@ -2,7 +2,9 @@ namespace OrchardCore.Html.Settings;
 
 public class HtmlBodyPartMonacoEditorSettings
 {
-    public string Options { get; set; }
+    public string Options { get; set; } = "{}";
+
     public bool InsertMediaWithUrl { get; set; }
+
     public bool AllowCustomScripts { get; set; }
 }
