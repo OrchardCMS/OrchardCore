3e0ad7e8fcd0d00ddbace66145780b2b7a4eba2b
Sára El-Saig <sara.el-saig@lombiq.com>
2026-02-20 17:12:24 +0100

    IMediaEventHandler default implementations.

--- src/OrchardCore/OrchardCore.Media.Abstractions/Events/IMediaEventHandler.cs
+++ src/OrchardCore/OrchardCore.Media.Abstractions/Events/IMediaEventHandler.cs
@@ -5,13 +5,13 @@ namespace OrchardCore.Media.Events;
 /// </summary>
 public interface IMediaEventHandler
 {
-    Task MediaDeletingFileAsync(MediaDeletingContext context);
-    Task MediaDeletedFileAsync(MediaDeletedContext context);
-    Task MediaDeletingDirectoryAsync(MediaDeletingContext context);
-    Task MediaDeletedDirectoryAsync(MediaDeletedContext context);
-    Task MediaMovingAsync(MediaMoveContext context);
-    Task MediaMovedAsync(MediaMoveContext context);
-    Task MediaCreatingDirectoryAsync(MediaCreatingContext context);
-    Task MediaCreatedDirectoryAsync(MediaCreatedContext context);
-    Task MediaPermittedStorageAsync(MediaPermittedStorageContext context);
+    Task MediaDeletingFileAsync(MediaDeletingContext context) => Task.CompletedTask;
+    Task MediaDeletedFileAsync(MediaDeletedContext context) => Task.CompletedTask;
+    Task MediaDeletingDirectoryAsync(MediaDeletingContext context) => Task.CompletedTask;
+    Task MediaDeletedDirectoryAsync(MediaDeletedContext context) => Task.CompletedTask;
+    Task MediaMovingAsync(MediaMoveContext context) => Task.CompletedTask;
+    Task MediaMovedAsync(MediaMoveContext context) => Task.CompletedTask;
+    Task MediaCreatingDirectoryAsync(MediaCreatingContext context) => Task.CompletedTask;
+    Task MediaCreatedDirectoryAsync(MediaCreatedContext context) => Task.CompletedTask;
+    Task MediaPermittedStorageAsync(MediaPermittedStorageContext context) => Task.CompletedTask;
 }
