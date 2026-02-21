f79759c0acfe75c284b77cc7743bcc01bbd040dc
Sára El-Saig <sara.el-saig@lombiq.com>
2026-02-21 03:27:02 +0100

    Remove unused code.

--- src/OrchardCore/OrchardCore.FileStorage.Abstractions/FileStoreException.cs
+++ src/OrchardCore/OrchardCore.FileStorage.Abstractions/FileStoreException.cs
@@ -4,13 +4,6 @@ namespace OrchardCore.FileStorage;
 
 public class FileStoreException : Exception
 {
-    public LocalizedString UserMessage { get; }
-
-    public FileStoreException(string message, LocalizedString userMessage) : this(message)
-    {
-        UserMessage = userMessage;
-    }
-
     public FileStoreException(string message) : base(message)
     {
     }
