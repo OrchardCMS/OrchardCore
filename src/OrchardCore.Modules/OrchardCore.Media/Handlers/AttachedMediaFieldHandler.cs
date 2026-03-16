710975d46198c063c4c0ec690eb764b10d120eff
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-03-16 06:42:00 +0300

    Address feedback

--- src/OrchardCore.Modules/OrchardCore.Media/Handlers/AttachedMediaFieldHandler.cs
+++ src/OrchardCore.Modules/OrchardCore.Media/Handlers/AttachedMediaFieldHandler.cs
@@ -1,6 +1,8 @@
 using System.Text.Json.Dynamic;
 using System.Text.Json.Nodes;
 using OrchardCore.ContentManagement.Handlers;
+using OrchardCore.ContentManagement.Metadata;
+using OrchardCore.ContentManagement.Metadata.Models;
 using OrchardCore.Media.Fields;
 using OrchardCore.Media.Services;
 
@@ -9,31 +11,33 @@ namespace OrchardCore.Media.Handlers;
 public class AttachedMediaFieldHandler : ContentFieldHandler<MediaField>
 {
     private readonly AttachedMediaFieldFileService _attachedMediaFieldFileService;
+    private readonly IContentDefinitionManager _contentDefinitionManager;
 
-    public AttachedMediaFieldHandler(AttachedMediaFieldFileService attachedMediaFieldFileService)
+    public AttachedMediaFieldHandler(AttachedMediaFieldFileService attachedMediaFieldFileService, IContentDefinitionManager contentDefinitionManager)
     {
         _attachedMediaFieldFileService = attachedMediaFieldFileService;
+        _contentDefinitionManager = contentDefinitionManager;
     }
 
     public async override Task ClonedAsync(CloneContentFieldContext context, MediaField field)
     {
-        var updatedPaths = await _attachedMediaFieldFileService.CopyFilesAsync(field.Paths, context.CloneContentItem);
+        var mediaFieldNodePath = (field.Content.Node as JsonNode).GetPath();
+        var fieldName = mediaFieldNodePath.Split('.')[1];
 
-        var mediaFieldNodePath = (field.Content.Node as JsonNode).GetNormalizedPath();
-        var mediaFieldNode = (context.CloneContentItem.Content as JsonDynamicObject).SelectNode(mediaFieldNodePath);
+        var contentTypeDefinition = await _contentDefinitionManager.GetTypeDefinitionAsync(context.CloneContentItem.ContentType);
 
-        // Update the paths to the new ones.
-        mediaFieldNode[nameof(MediaField.Paths)] = ToJsonArray(updatedPaths);
-    }
+        var partFieldDefinition = contentTypeDefinition.Parts
+            .SelectMany(tpd => tpd.PartDefinition.Fields)
+            .Single(pfd => pfd.FieldDefinition.Name == nameof(MediaField) && pfd.Name == fieldName);
 
-    private static JsonArray ToJsonArray(string[] paths)
-    {
-        var jsonArray = new JsonArray();
-        foreach (var path in paths)
+        if (partFieldDefinition.Editor() == "Attached")
         {
-            jsonArray.Add(path);
-        }
+            var updatedPaths = await _attachedMediaFieldFileService.CopyFilesAsync((string[])field.Paths.Clone(), context.CloneContentItem);
 
-        return jsonArray;
+            var mediaFieldNode = (context.CloneContentItem.Content as JsonDynamicObject).SelectNode(mediaFieldNodePath);
+
+            // Update the paths to the new ones.
+            mediaFieldNode[nameof(MediaField.Paths)] = JArray.FromObject(updatedPaths);
+        }
     }
 }
