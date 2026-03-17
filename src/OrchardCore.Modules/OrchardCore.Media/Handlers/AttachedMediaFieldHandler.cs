c83d068253d8e57f2af31c8e446ae5eca6e4ed35
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-03-17 23:15:49 +0300

    Use context.ContentPartFieldDefinition

--- src/OrchardCore.Modules/OrchardCore.Media/Handlers/AttachedMediaFieldHandler.cs
+++ src/OrchardCore.Modules/OrchardCore.Media/Handlers/AttachedMediaFieldHandler.cs
@@ -1,7 +1,6 @@
 using System.Text.Json.Dynamic;
 using System.Text.Json.Nodes;
 using OrchardCore.ContentManagement.Handlers;
-using OrchardCore.ContentManagement.Metadata;
 using OrchardCore.ContentManagement.Metadata.Models;
 using OrchardCore.Media.Fields;
 using OrchardCore.Media.Services;
@@ -11,26 +10,17 @@ namespace OrchardCore.Media.Handlers;
 public class AttachedMediaFieldHandler : ContentFieldHandler<MediaField>
 {
     private readonly AttachedMediaFieldFileService _attachedMediaFieldFileService;
-    private readonly IContentDefinitionManager _contentDefinitionManager;
 
-    public AttachedMediaFieldHandler(AttachedMediaFieldFileService attachedMediaFieldFileService, IContentDefinitionManager contentDefinitionManager)
+    public AttachedMediaFieldHandler(AttachedMediaFieldFileService attachedMediaFieldFileService)
     {
         _attachedMediaFieldFileService = attachedMediaFieldFileService;
-        _contentDefinitionManager = contentDefinitionManager;
     }
 
     public async override Task ClonedAsync(CloneContentFieldContext context, MediaField field)
     {
         var mediaFieldNodePath = (field.Content.Node as JsonNode).GetPath();
-        var fieldName = mediaFieldNodePath.Split('.')[1];
 
-        var contentTypeDefinition = await _contentDefinitionManager.GetTypeDefinitionAsync(context.CloneContentItem.ContentType);
-
-        var partFieldDefinition = contentTypeDefinition.Parts
-            .SelectMany(tpd => tpd.PartDefinition.Fields)
-            .Single(pfd => pfd.FieldDefinition.Name == nameof(MediaField) && pfd.Name == fieldName);
-
-        if (partFieldDefinition.Editor() == "Attached")
+        if (context.ContentPartFieldDefinition.Editor() == "Attached")
         {
             var updatedPaths = await _attachedMediaFieldFileService.CopyFilesAsync(field.Paths, context.CloneContentItem);
 
