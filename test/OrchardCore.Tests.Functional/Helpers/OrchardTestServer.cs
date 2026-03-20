701e86800a9fa4e2e6a037308d106d8f45950aa5
Jasmin Savard <jasmin.savard@gmail.com>
2026-03-20 09:13:50 -0400

    fix concurrency issues

--- test/OrchardCore.Tests.Functional/Helpers/OrchardTestServer.cs
+++ test/OrchardCore.Tests.Functional/Helpers/OrchardTestServer.cs
@@ -181,7 +181,7 @@ public sealed class OrchardTestServer : IAsyncDisposable
         // Disable YesSql concurrency checks during setup. Each recipe step runs in a new
         // scope with a new session, but the document cache can serve stale versions, causing
         // ConcurrencyException on SiteSettings with external databases.
-        builder.Configuration["OrchardCore_Documents:CheckConcurrency"] = "false";
+        builder.Configuration["OrchardCore:OrchardCore_Documents:CheckConcurrency"] = "false";
 
         builder.Logging.AddProvider(new InMemoryLoggerProvider(logEntries));
     }
