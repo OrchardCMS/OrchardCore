777306621c2fe67f2dd596107c2a4989aaf03fb5
Jasmin Savard <jasmin.savard@gmail.com>
2026-03-28 04:27:44 -0400

    fix feature selector

--- test/OrchardCore.Tests.Functional/Helpers/FeatureHelper.cs
+++ test/OrchardCore.Tests.Functional/Helpers/FeatureHelper.cs
@@ -7,12 +7,12 @@ public static class FeatureHelper
     public static async Task EnableFeatureAsync(this IPage page, string prefix, string featureName)
     {
         await page.GotoAsync($"{prefix}/Admin/Features");
-        await page.Locator($"#btn-enable-{featureName}").ClickAsync();
+        await page.Locator($"#btn-enable-{featureName.Replace('.', '_')}").ClickAsync();
     }
 
     public static async Task DisableFeatureAsync(this IPage page, string prefix, string featureName)
     {
         await page.GotoAsync($"{prefix}/Admin/Features");
-        await page.Locator($"#btn-disable-{featureName}").ClickAsync();
+        await page.Locator($"#btn-disable-{featureName.Replace('.', '_')}").ClickAsync();
     }
 }
