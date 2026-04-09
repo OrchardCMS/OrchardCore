07a8702eb51ed268cec865f180b50099a7b8a93f
Georg von Kries <gvk@creativbox.net>
2026-04-09 10:11:44 +0200

    Revert "Add SMTP Health Check (#18916)" (#19128)
    This reverts commit b45b52a0d2ea09a8f28a1b64903a83d906b9c6b7.

--- src/OrchardCore.Modules/OrchardCore.Email.Smtp/Extensions/SmtpHealthCheckExtensions.cs
+++
@@ -1,9 +0,0 @@
-using OrchardCore.Email.Smtp.HealthChecks;
-
-namespace Microsoft.Extensions.DependencyInjection;
-
-public static class SmtpHealthCheckExtensions
-{
-    public static IHealthChecksBuilder AddSmtpCheck(this IHealthChecksBuilder healthChecksBuilder)
-        => healthChecksBuilder.AddCheck<SmtpHealthCheck>("SMTP Health Check");
-}
