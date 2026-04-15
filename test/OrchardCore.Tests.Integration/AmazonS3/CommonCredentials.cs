2ec32b25c4bcc58f07b8c038b10afdcdd3212eff
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-04-16 01:37:28 +0300

    Use TestContainers for AWS Local Stack

---
+++ test/OrchardCore.Tests.Integration/AmazonS3/CommonCredentials.cs
@@ -0,0 +1,8 @@
+namespace OrchardCore.Tests.Integration.AmazonS3;
+
+public static class CommonCredentials
+{
+    public const string AwsAccessKey = "AKIAIOSFODNN7EXAMPLE";
+
+    public const string AwsSecretKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+}
