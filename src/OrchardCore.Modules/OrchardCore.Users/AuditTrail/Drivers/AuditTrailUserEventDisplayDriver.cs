a22977885ac1e76e50e13568b60d542fb4386f6f
Sára El-Saig <sara.el-saig@lombiq.com>
2026-05-07 20:51:19 +0200

    Fix IDE0052.

--- src/OrchardCore.Modules/OrchardCore.Users/AuditTrail/Drivers/AuditTrailUserEventDisplayDriver.cs
+++ src/OrchardCore.Modules/OrchardCore.Users/AuditTrail/Drivers/AuditTrailUserEventDisplayDriver.cs
@@ -1,14 +1,11 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 using OrchardCore.AuditTrail.Drivers;
 using OrchardCore.AuditTrail.Models;
 using OrchardCore.DisplayManagement.Handlers;
 using OrchardCore.DisplayManagement.Views;
-using OrchardCore.Modules;
 using OrchardCore.Users.AuditTrail.Models;
 using OrchardCore.Users.AuditTrail.ViewModels;
-using OrchardCore.Users.Models;
 
 namespace OrchardCore.Users.AuditTrail.Drivers;
 
@@ -16,16 +13,13 @@ public sealed class AuditTrailUserEventDisplayDriver : AuditTrailEventSectionDis
 {
     private readonly IAuthorizationService _authorizationService;
     private readonly IHttpContextAccessor _httpContextAccessor;
-    private readonly UserManager<IUser> _userManager;
 
     public AuditTrailUserEventDisplayDriver(
         IAuthorizationService authorizationService,
-        IHttpContextAccessor httpContextAccessor,
-        UserManager<IUser> userManager)
+        IHttpContextAccessor httpContextAccessor)
     {
         _authorizationService = authorizationService;
         _httpContextAccessor = httpContextAccessor;
-        _userManager = userManager;
     }
 
     public override IDisplayResult Display(AuditTrailEvent auditTrailEvent, AuditTrailUserEvent userEvent, BuildDisplayContext context) =>
