78ce0b4eaab51c9d221521e47f6829c2e3bf95e0
Sára El-Saig <sara.el-saig@lombiq.com>
2026-05-07 23:27:51 +0200

    Simplify and document RemoveRedactor.

--- src/OrchardCore.Modules/OrchardCore.Users/AuditTrail/Services/RemoveRedactor.cs
+++ src/OrchardCore.Modules/OrchardCore.Users/AuditTrail/Services/RemoveRedactor.cs
@@ -2,20 +2,16 @@ using Microsoft.Extensions.Compliance.Redaction;
 
 namespace OrchardCore.Users.AuditTrail.Services;
 
+/// <summary>
+/// Similar to <see cref="ErasingRedactor"/>, but it returns <see langword="null"/> when redacting a string, so it can
+/// be used to indicate that the value should be removed instead of just emptied.
+/// </summary>
 public class RemoveRedactor : Redactor
 {
-    public const string RemoveProperty = "!!Remove Property";
+    public override int Redact(ReadOnlySpan<char> source, Span<char> destination) => 0;
 
-    public override int Redact(ReadOnlySpan<char> source, Span<char> destination)
-    {
-        var value = new ReadOnlySpan<char>(RemoveProperty.ToCharArray());
-
-        // Avoid buffer overflows by returning nothing if there is no space. 
-        return !value.TryCopyTo(destination) ? 0 : value.Length;
-    }
-
-    public override int GetRedactedLength(ReadOnlySpan<char> input) => RemoveProperty.Length;
+    public override int GetRedactedLength(ReadOnlySpan<char> input) => 0;
 
     /// <inheritdoc/>
-    public override string Redact(string source) => RemoveProperty;
+    public override string Redact(string source) => null;
 }
