33b44fcfd63fec9d0fb9e4f7d26558193c5f69f5
Sára El-Saig <sara.el-saig@lombiq.com>
2026-06-16 16:20:56 +0200

    Don't break if HTTP context is missing.

--- src/OrchardCore.Modules/OrchardCore.Contents/Liquid/DisplayUrlFilter.cs
+++ src/OrchardCore.Modules/OrchardCore.Contents/Liquid/DisplayUrlFilter.cs
@@ -1,5 +1,6 @@
 using Fluid;
 using Fluid.Values;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Routing;
@@ -14,17 +15,31 @@ public class DisplayUrlFilter : ILiquidFilter
 {
     private readonly AutorouteOptions _autorouteOptions;
     private readonly IContentManager _contentManager;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly LinkGenerator _linkGenerator;
     private readonly IUrlHelperFactory _urlHelperFactory;
 
-    public DisplayUrlFilter(IOptions<AutorouteOptions> autorouteOptions, IContentManager contentManager, IUrlHelperFactory urlHelperFactory)
+    public DisplayUrlFilter(
+        IOptions<AutorouteOptions> autorouteOptions,
+        IContentManager contentManager,
+        IHttpContextAccessor httpContextAccessor,
+        LinkGenerator linkGenerator,
+        IUrlHelperFactory urlHelperFactory)
     {
         _autorouteOptions = autorouteOptions.Value;
         _contentManager = contentManager;
+        _httpContextAccessor = httpContextAccessor;
+        _linkGenerator = linkGenerator;
         _urlHelperFactory = urlHelperFactory;
     }
 
     public async ValueTask<FluidValue> ProcessAsync(FluidValue input, FilterArguments arguments, LiquidTemplateContext context)
     {
+        if (_httpContextAccessor.HttpContext == null)
+        {
+            return new StringValue(string.Empty);
+        }
+
         var contentItem = input.ToObjectValue() as ContentItem;
         RouteValueDictionary routeValues;
 
