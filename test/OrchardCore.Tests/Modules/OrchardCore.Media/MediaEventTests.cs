aa621024a91c776f1785b8edabec078b78191d89
infofromca <zgofmontreal@gmail.com>
2026-07-02 15:26:16 -0400

    Fix the test

--- test/OrchardCore.Tests/Modules/OrchardCore.Media/MediaEventTests.cs
+++ test/OrchardCore.Tests/Modules/OrchardCore.Media/MediaEventTests.cs
@@ -76,6 +76,7 @@ public class MediaEventTests
 
 public class TestMediaEventHandler : IMediaCreatingEventHandler
 {
+    public int Priority => 0;
     public async Task<Stream> MediaCreatingAsync(MediaCreatingContext context, Stream inputStream)
     {
         var outStream = MemoryStreamFactory.GetStream();
