642d0308164b2b963860e0b05c3dfbb9b965021b
Matt Varblow <matt.varblow@advantagecs.com>
2026-07-06 15:55:28 -0400

    Reduce duplicated code.

--- src/OrchardCore.Modules/OrchardCore.Roles/ViewModels/DisplayRoleViewModel.cs
+++ src/OrchardCore.Modules/OrchardCore.Roles/ViewModels/DisplayRoleViewModel.cs
@@ -1,20 +1,13 @@
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using OrchardCore.Security;
-using OrchardCore.Security.Permissions;
 
 namespace OrchardCore.Roles.ViewModels;
 
 public class DisplayRoleViewModel
 {
     [BindNever]
-    public bool IsAdminRole { get; set; }
-
-    [BindNever]
-    public IDictionary<PermissionGroupKey, IEnumerable<Permission>> RoleCategoryPermissions { get; set; }
-
-    [BindNever]
-    public IEnumerable<string> EffectivePermissions { get; set; }
-
-    [BindNever]
     public Role Role { get; set; }
+
+    [BindNever]
+    public RolePermissionsViewModel Permissions { get; set; }
 }
