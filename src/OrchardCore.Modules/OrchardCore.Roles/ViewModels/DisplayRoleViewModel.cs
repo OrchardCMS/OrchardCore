805d0d5ae3e57c1a3f9f9a80e06258ed3babea8d
Matt Varblow <matt.varblow@advantagecs.com>
2026-07-06 14:58:41 -0400

    Add View action (and permission) for roles.

---
+++ src/OrchardCore.Modules/OrchardCore.Roles/ViewModels/DisplayRoleViewModel.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using OrchardCore.Security;
+using OrchardCore.Security.Permissions;
+
+namespace OrchardCore.Roles.ViewModels;
+
+public class DisplayRoleViewModel
+{
+    [BindNever]
+    public bool IsAdminRole { get; set; }
+
+    [BindNever]
+    public IDictionary<PermissionGroupKey, IEnumerable<Permission>> RoleCategoryPermissions { get; set; }
+
+    [BindNever]
+    public IEnumerable<string> EffectivePermissions { get; set; }
+
+    [BindNever]
+    public Role Role { get; set; }
+}
