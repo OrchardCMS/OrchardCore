60dbd992354e3b2c23985d895fbc0c6b402e4770
Mike Alhayek <mike@crestapps.com>
2026-07-09 11:04:43 -0700

    remove file

--- .set_identity.sh
+++
@@ -1,6 +0,0 @@
-if [ "$GIT_AUTHOR_NAME" = "fsfh60" ] || echo "$GIT_AUTHOR_NAME" | grep -qi "fadi" || echo "$GIT_AUTHOR_EMAIL" | grep -qi "fsfh60" || echo "$GIT_COMMITTER_NAME" | grep -qi "fadi" || echo "$GIT_COMMITTER_EMAIL" | grep -qi "fsfh60"; then
-  export GIT_AUTHOR_NAME="'+$targetName+'"
-  export GIT_AUTHOR_EMAIL="'+$targetEmail+'"
-  export GIT_COMMITTER_NAME="'+$targetName+'"
-  export GIT_COMMITTER_EMAIL="'+$targetEmail+'"
-fi
