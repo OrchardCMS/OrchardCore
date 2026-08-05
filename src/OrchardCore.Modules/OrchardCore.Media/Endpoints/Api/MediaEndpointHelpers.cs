4618c0c788ba4522d212e995f31a149fde274966
Georg von Kries <gvk@creativbox.net>
2026-08-05 13:42:28 +0200

    Enhance media folder-level authorization checks
    Require ManageMediaFolder permission for all media endpoints, filtering folders and files based on user access. Update helper methods to enforce folder-level permissions during listing, traversal, and tree-building, ensuring only authorized items are returned and improving security.

--- src/OrchardCore.Modules/OrchardCore.Media/Endpoints/Api/MediaEndpointHelpers.cs
+++ src/OrchardCore.Modules/OrchardCore.Media/Endpoints/Api/MediaEndpointHelpers.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Security.Claims;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
