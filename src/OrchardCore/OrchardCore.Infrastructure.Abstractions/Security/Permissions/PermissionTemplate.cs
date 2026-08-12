148c91dee8a3a4c44e29e5fb4b5b715199d2ce67
Sára El-Saig <sara.el-saig@lombiq.com>
2026-08-12 14:54:57 +0200

    Create PermissionTemplate class.

---
+++ src/OrchardCore/OrchardCore.Infrastructure.Abstractions/Security/Permissions/PermissionTemplate.cs
@@ -0,0 +1,28 @@
+namespace OrchardCore.Security.Permissions;
+
+public record PermissionTemplate(
+    string Name,
+    string Description,
+    string Category,
+    IEnumerable<Permission> ImpliedBy,
+    bool IsSecurityCritical = false)
+{
+    public PermissionTemplate(string name, string description, string category = null, params Permission[] impliedBy)
+        : this(name, description, category, impliedBy, IsSecurityCritical: false)
+    {
+    }
+
+    public PermissionTemplate(string name, string description, params Permission[] impliedBy)
+        : this(name, description, category: null, impliedBy)
+    {
+    }
+    
+    public Permission CreateDynamicPermission(string nameValue, string descriptionValue =  null) => new(
+        string.Format(Name, nameValue),
+        string.Format(Description, descriptionValue ?? nameValue),
+        ImpliedBy ?? [],
+        IsSecurityCritical)
+    {
+        Category = Category,
+    };
+}
