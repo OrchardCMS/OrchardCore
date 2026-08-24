a147ff0df8f9c93f9b7cb1860fd8439f40256df9
Sára El-Saig <sara.el-saig@lombiq.com>
2026-08-25 01:42:29 +0200

    Revert

--- src/OrchardCore.Modules/OrchardCore.ContentFields/Handlers/HtmlFieldHandler.cs
+++ src/OrchardCore.Modules/OrchardCore.ContentFields/Handlers/HtmlFieldHandler.cs
@@ -1,4 +1,5 @@
 using OrchardCore.ContentFields.Fields;
+using OrchardCore.ContentFields.Settings;
 using OrchardCore.ContentManagement.Handlers;
 using OrchardCore.ContentManagement.Metadata;
 using OrchardCore.Infrastructure.Html;
@@ -8,15 +9,27 @@ namespace OrchardCore.ContentFields.Handlers;
 [Obsolete("This handler is no longer used.")]
 public class HtmlFieldHandler : ContentFieldHandler<HtmlField>
 {
+    private readonly IContentDefinitionManager _contentDefinitionManager;
+    private readonly IHtmlSanitizerService _htmlSanitizerService;
+
     public HtmlFieldHandler(
         IContentDefinitionManager contentDefinitionManager,
         IHtmlSanitizerService htmlSanitizerService)
     {
+        _contentDefinitionManager = contentDefinitionManager;
+        _htmlSanitizerService = htmlSanitizerService;
     }
 
     public override Task ImportedAsync(ImportContentFieldContext context, HtmlField field)
     {
-        // The old handler logic has been removed but the method is kept for binary backwards compatibility.
+        var settings = context.ContentPartFieldDefinition.GetSettings<HtmlFieldSettings>();
+
+        if (settings?.SanitizeHtml == true)
+        {
+            context.ContentItem.Content[context.ContentPartFieldDefinition.PartDefinition.Name][context.ContentPartFieldDefinition.Name].Html =
+                _htmlSanitizerService.Sanitize(field.Html);
+        }
+
         return Task.CompletedTask;
     }
 }
--- src/OrchardCore.Modules/OrchardCore.Menu/Handlers/HtmlMenuItemPartHandler.cs
+++ src/OrchardCore.Modules/OrchardCore.Menu/Handlers/HtmlMenuItemPartHandler.cs
@@ -1,20 +1,31 @@
+using OrchardCore.ContentManagement;
 using OrchardCore.ContentManagement.Handlers;
 using OrchardCore.ContentManagement.Metadata;
 using OrchardCore.Infrastructure.Html;
 using OrchardCore.Menu.Models;
+using OrchardCore.Menu.Settings;
 
 namespace OrchardCore.Menu.Handlers;
 
 [Obsolete("This handler is no longer used.")]
 public class HtmlMenuItemPartHandler : ContentPartHandler<HtmlMenuItemPart>
 {
+    private readonly IContentDefinitionManager _contentDefinitionManager;
+    private readonly IHtmlSanitizerService _htmlSanitizerService;
     public HtmlMenuItemPartHandler(IContentDefinitionManager contentDefinitionManager, IHtmlSanitizerService htmlSanitizerService)
     {
+        _contentDefinitionManager = contentDefinitionManager;
+        _htmlSanitizerService = htmlSanitizerService;
     }
     
-    public override Task ImportedAsync(ImportContentContext context, HtmlMenuItemPart part)
+    public override async Task ImportedAsync(ImportContentContext context, HtmlMenuItemPart part)
     {
-        // The old handler logic has been removed but the method is kept for binary backwards compatibility.
-        return Task.CompletedTask;
+        var typeDefinition = await _contentDefinitionManager.GetTypeDefinitionAsync(context.ContentItem.ContentType);
+
+        if (typeDefinition.GetSettings<HtmlMenuItemPartSettings>() is { SanitizeHtml: true })
+        {
+            context.ContentItem.Alter<HtmlMenuItemPart>(part => 
+                part.Html = _htmlSanitizerService.Sanitize(part.Html));
+        }
     }
 }
